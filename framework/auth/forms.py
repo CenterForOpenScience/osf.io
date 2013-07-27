015dbef6c8820deea4d7247135cb92b133c52bf5
Joshua Carp <jm.carp@gmail.com>
2013-07-26 23:03:23 -0400

    fixed typo

--- framework/auth/forms.py
+++ framework/auth/forms.py
@@ -11,7 +11,7 @@ class ResetPasswordForm(Form):
 
 class RegistrationForm(Form):
     fullname = TextField('Full Name', [
-        validators.Required(message=u'Full name i required')
+        validators.Required(message=u'Full name is required')
     ])
     username    = TextField('Email Address', [
         validators.Required(message=u'Email address is required'),
