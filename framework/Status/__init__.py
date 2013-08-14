22f5ae9ec2ca70ccb85a6e57c7efc2d4af37cbc9
Jeffrey Spies <jspies@gmail.com>
2013-08-13 21:50:28 -0400

    bringing status back in; something going on with -status.__init__ and +status__init__

---
+++ framework/Status/__init__.py
@@ -0,0 +1,19 @@
+import framework.Beaker as Session
+
+def pushStatusMessage(message, level=0):
+    statuses = Session.get('status')
+    if not statuses:
+        statuses = []
+    statuses.append(message)
+    Session.set('status', statuses)
+
+def popStatusMessages(level=0):
+    messages = Session.get('status')
+    Session.set('status_prev', messages)
+    Session.unset('status')
+    return messages
+
+def popPreviousStatusMessages(level=0):
+    messages = Session.get('status_prev')
+    Session.unset('status_prev')
+    return messages
