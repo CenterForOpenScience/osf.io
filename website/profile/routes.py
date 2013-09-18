5e32d26e7411a3dec7ef8ff3e9416e57fff95709
Joshua Carp <jm.carp@gmail.com>
2013-09-18 14:10:42 -0400

    strip out render / jsonify

--- website/profile/routes.py
+++ website/profile/routes.py
@@ -12,25 +12,103 @@ from framework import (
 )
 from framework.forms.utils import sanitize
 
-from website.models import ApiKey
+from website.models import User, ApiKey
+from framework.analytics import get_total_activity_count
 
-@get('/profile')
+def _node_info(node):
+    if node.category == 'project':
+        route = '/project/{}'.format(
+            node._id
+        )
+        parent, parent_id = None, None
+    else:
+        parent = node.node__parent[0]
+        parent_id = parent._id
+        route = '/project/{}/node/{}'.format(
+            parent._id,
+            node._id,
+        )
+    return {
+        'node_id' : node._id,
+        'parent_id' : parent_id,
+        'route' : route,
+    }
+
+
+def get_public_projects(uid):
+    user = User.load(uid)
+    return [
+        _node_info(node)
+        for node in user.node__contributed
+        if node.category == 'project'
+        and node.is_public
+        and not node.is_deleted
+    ]
+
+def get_public_components(*args, **kwargs):
+    user = User.load(kwargs['uid'])
+    return [
+        _node_info(node)
+        for node in user.node__contributed
+        if node.category != 'project'
+        and node.is_public
+        and not node.is_deleted
+    ]
+
 @must_be_logged_in
 def profile_view(*args, **kwargs):
     user = kwargs['user']
-    return render(filename="profile.mako", profile=user, user=user)
+    projects = [
+        node
+        for node in user.node__contributed
+        if node.category == 'project'
+        and not node.is_registration
+        and not node.is_deleted
+    ]
+    public_projects = [
+        node
+        for node in projects
+        if node.is_public
+    ]
+    return {
+        'profile' : user,
+        'profile_id' : user._id,
+        'user' : user,
+        'user_id': user._id,
+        'activity_points' : get_total_activity_count(user._primary_key),
+        'number_projects' : len(projects),
+        'number_public_projects' : len(public_projects),
+    }
 
 
-@get('/profile/<user_id>')
-def profile_view_id(user_id):
-    profile = get_user(id=user_id)
+def profile_view_id(uid):
+    profile = get_user(id=uid)
     user = get_current_user()
     if profile:
-        return render(filename="profile.mako", profile=profile, user=user)
+        projects = [
+            node
+            for node in profile.node__contributed
+            if node.category == 'project'
+            and not node.is_registration
+            and not node.is_deleted
+        ]
+        public_projects = [
+            node
+            for node in projects
+            if node.is_public
+        ]
+        return {
+            'profile' : profile,
+            'profile_id' : profile._id,
+            'user' : user,
+            'user_id': user._id,
+            'activity_points' : get_total_activity_count(profile._primary_key),
+            'number_projects' : len(projects),
+            'number_public_projects' : len(public_projects),
+        }
     return abort(404)
 
 
-@post('/profile/<user_id>/edit')
 @must_be_logged_in
 def edit_profile(*args, **kwargs):
     user = kwargs['user']
@@ -41,16 +119,27 @@ def edit_profile(*args, **kwargs):
         user.fullname = sanitize(form['value'])
         user.save()
 
-    return jsonify({'response': 'success'})
+    return {'response' : 'success'}
 
 
-@get('/settings')
 @must_be_logged_in
-def settings(*args, **kwargs):
+def profile_settings(*args, **kwargs):
     user = kwargs['user']
-    return render(filename="settings.mako",user=user,prettify=True)
+    return {
+        'user' : user,
+    }
 
-@post('/settings/create_key/')
+@must_be_logged_in
+def get_keys(*args, **kwargs):
+    user = kwargs['user']
+    return [
+        {
+            'key' : key._id,
+            'label' : key.label,
+        }
+        for key in user.api_keys
+    ]
+
 @must_be_logged_in
 def create_user_key(*args, **kwargs):
 
@@ -64,9 +153,10 @@ def create_user_key(*args, **kwargs):
     user.save()
 
     # Return response
-    return jsonify({'response': 'success'})
+    return {
+        'response' : 'success',
+    }
 
-@post('/settings/remove_key/')
 @must_be_logged_in
 def revoke_user_key(*args, **kwargs):
 
@@ -79,16 +169,23 @@ def revoke_user_key(*args, **kwargs):
     user.save()
 
     # Return response
-    return jsonify({'response': 'success'})
+    return {'response' : 'success'}
 
-@get('/settings/key_history/<kid>')
 @must_be_logged_in
 def user_key_history(*args, **kwargs):
 
     api_key = ApiKey.load(kwargs['kid'])
-    return render(
-        filename='keyhistory.mako',
-        api_key=api_key,
-        user=kwargs['user'],
-        route='/settings',
-    )
+    return {
+        'key' : api_key._id,
+        'label' : api_key.label,
+        'user' : kwargs['user'],
+        'route' : '/settings',
+        'logs' : [
+            {
+                'lid' : log._id,
+                'nid' : log.node__logged[0]._id,
+                'route' : log.node__logged[0].url(),
+            }
+            for log in api_key.nodelog__created
+        ]
+    }
