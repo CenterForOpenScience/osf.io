3ff406048377101d1b2de2a96594e1fd202b63ac
Joshua Carp <jm.carp@gmail.com>
2013-09-25 14:11:29 -0400

    split project views into files

--- website/routes.py
+++ website/routes.py
@@ -1,7 +1,7 @@
 from framework.auth import routes
 
 from website import views
-from website.profile import routes
-from website.project import routes
+# from website.profile import views
+# from website.project import views
 from website.search import routes
 from website.discovery import routes
