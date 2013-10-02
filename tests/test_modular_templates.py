635139427a303d874c0cc95aea4a4cc7dc98c5b9
Lyndsy Simon <lyndsy@lyndsysimon.com>
2013-10-02 10:46:04 -0400

    Adds detailed docstrings for existing tests.

--- tests/test_modular_templates.py
+++ tests/test_modular_templates.py
@@ -20,6 +20,8 @@ class RendererTestCase(OsfTestCase):
         self.r = Renderer()
 
     def test_redirect(self):
+        """When passed a Flask/Werkzeug Response object, it should be returned.
+        """
         resp = flask.make_response('')
 
         self.assertIsInstance(
@@ -28,10 +30,12 @@ class RendererTestCase(OsfTestCase):
         )
 
     def test_http_error_raised(self):
+        """Subclasses of Renderer must implement ``.handle_error()``."""
         with self.assertRaises(NotImplementedError):
             self.r(HTTPError(http.NOT_FOUND))
 
     def test_tuple(self):
+        """Subclasses of Renderer must implement ``.render()``."""
         with self.assertRaises(NotImplementedError):
             self.r(('response text', ))
 
@@ -43,6 +47,8 @@ class JSONRendererTestCase(OsfTestCase):
         self.r = JSONRenderer()
 
     def test_redirect(self):
+        """When passed a Flask/Werkzeug Response object, it should be returned.
+        """
         resp = flask.make_response('')
 
         self.assertIsInstance(
@@ -51,6 +57,14 @@ class JSONRendererTestCase(OsfTestCase):
         )
 
     def test_http_error_raised(self):
+        """When an HTTPError is raised in the view function, it is passed as
+        input to the renderer.
+
+        ``JSONRenderer`` should return a 2-tuple, where the first element is a
+        dict representative of the error passed, and the second element is the
+        associated HTTP status code. This mirrors the tuple format anticipated
+        by Flask.
+        """
         resp = self.r(HTTPError(http.NOT_FOUND))
 
         msg = HTTPError.error_msgs[http.NOT_FOUND]
@@ -77,11 +91,18 @@ class WebRendererTestCase(OsfTestCase):
     def setUp(self):
         super(WebRendererTestCase, self).setUp()
         self.r = WebRenderer(
-            'index.html',
+            '../../tests/templates/main.html',
             render_mako_string
         )
 
     def test_redirect(self):
+        """When passed a Flask-style tuple where the HTTP status code indicates
+        a redirection should take place, a Flask/Werkzeug response object should
+        be returned, with the appropriate ``status_code`` and ``location`` set.
+
+        Note that this behavior is inconsistent with that of raising an
+        ``HTTPError`` in a view function, which serves the same purpose.
+        """
         with self.app.test_request_context():
             self.app.preprocess_request()
 
@@ -101,20 +122,41 @@ class WebRendererTestCase(OsfTestCase):
             self.assertEqual('http://google.com/', resp.location)
 
     def test_input_dict(self):
+        """When only a dict is passed it, a Flask-style tuple is returned, of
+        which the 0th element must be the rendered template, including the dict
+        as part of the context.
+        """
         with self.app.test_request_context():
             self.app.preprocess_request()
 
-            resp = self.r({})
+            input_dict = {'foo': 'bar'}
+
+            resp = self.r(input_dict)
 
             self.assertIsInstance(
                 resp, tuple
             )
 
+            self.assertIn(
+                'foo:bar',
+                resp[0]
+            )
+
     def test_http_error_raised(self):
-        resp = self.r(HTTPError(http.NOT_FOUND))
+        """When an HTTPError is raised in the view function, it is passed as
+        input to the renderer.
+
+        ``WebRenderer`` should return a 2-tuple, where the first element is the
+        rendered error template. Each HTTPError exposes a ``to_data()`` method,
+        which yields the appropriate error message text.
+        """
+
+        err = HTTPError(http.NOT_FOUND)
+
+        resp = self.r(err)
 
         self.assertIn(
-            HTTPError.error_msgs[http.NOT_FOUND]['message_short'],
+            err.to_data()['message_short'],
             resp[0],
         )
         self.assertEqual(
@@ -123,6 +165,20 @@ class WebRendererTestCase(OsfTestCase):
         )
 
     def test_http_error_raise_with_redirect(self):
+        """Some status codes passed to HTTPError may contain a ``resource_uri``
+        which specifies a location to which the user may be redirected. If a
+        ``resource_uri`` is present, the ``WebRenderer`` should redirect the
+        user to that URL.
+
+        Note that some HTTP status codes - for example, 201 (Created) - are not
+        well-supported by browser. As a result, while the 201 should be passed
+        back to an API client, it is translated to a 301 when passed to
+        ``WebRenderer``. This results in a website user being taken directly to
+        a new object upon creation, instead of to an intermediate page.
+
+        This functionality is technically a violation of the HTTP spec, and
+        should be retired once we move to an API-centric web frontend.
+        """
         resp = self.r(
             HTTPError(http.CREATED, resource_uri='http://google.com/')
         )
@@ -138,40 +194,34 @@ class WebRendererTestCase(OsfTestCase):
 class WebRendererTemplateTestCase(OsfTestCase):
 
     def test_nested_templates(self):
+        """When a template passed to ``WebRenderer`` contains references to
+        nested templates, those nested templates should be rendered recursively
+        prior to return."""
+
         with self.app.test_request_context():
             self.app.preprocess_request()
 
+            # Create a ``WebRenderer`` for a nested template file.
             r = WebRenderer(
                 'nested_parent.html',
                 render_mako_string,
                 template_dir='tests/templates',
             )
 
+            # render the template (with an empty context)
             resp = r({})
 
+            # The result should be a tuple
             self.assertIsInstance(resp, tuple)
+            # The contents of the inner template should be present in the page.
             self.assertIn('child template content', resp[0])
 
-    @unittest.skip('Fails with IOError - should replace the template content')
-    def test_broken_template_uri(self):
-        with self.app.test_request_context():
-            self.app.preprocess_request()
-
-            r = WebRenderer(
-                'nested_parent_broken.html',
-                render_mako_string,
-                template_dir='tests/templates',
-            )
-
-            resp = r({})
-
-            self.assertIn(
-                'URI {} not found.'.format(
-                    'test/templates/not_a_valid_file.html'),
-                resp[0],
-            )
-
     def test_render_included_template(self):
+        """``WebRenderer.render_element()`` is the internal method called when
+        a template string is rendered. This test case examines the same
+        functionality as ``test_nested_templates()`` (above), but does so
+        without relying on the parent template being found and processed.
+        """
         with self.app.test_request_context():
             self.app.preprocess_request()
 
@@ -201,7 +251,37 @@ class WebRendererTemplateTestCase(OsfTestCase):
             )
 
     @unittest.skip('Fails with IOError - should replace the template content')
+    def test_broken_template_uri(self):
+        """When a template contains an embedded template that can't be found,
+        a message indicating that should be included in the rendered page.
+
+        NOTE: This functionality is currently failing. Instead of including the
+        appropriate message in the output, an IOError is raised.
+        """
+        with self.app.test_request_context():
+            self.app.preprocess_request()
+
+            r = WebRenderer(
+                'nested_parent_broken.html',
+                render_mako_string,
+                template_dir='tests/templates',
+            )
+
+            resp = r({})
+
+            self.assertIn(
+                'URI {} not found.'.format(
+                    'test/templates/not_a_valid_file.html'),
+                resp[0],
+            )
+
+    @unittest.skip('Fails with IOError - should replace the template content')
     def test_render_included_template_not_found(self):
+        """``WebRenderer.render_element()`` is the internal method called when
+        a template string is rendered. This test case examines the same
+        functionality as ``test_broken_template_uri()`` (above), but does so
+        without relying on the parent template being found and processed.
+        """
         with self.app.test_request_context():
             self.app.preprocess_request()
 
@@ -238,5 +318,3 @@ class CallUriTestCase(OsfTestCase):
             self.app.preprocess_request()
 
             r = call_url('/', wrap=True)
-
-            print r
