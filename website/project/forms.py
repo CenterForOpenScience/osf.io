d566fbdc505f6b34141807dbbfcbb6f2b0082e37
Joshua Carp <jm.carp@gmail.com>
2013-12-02 10:53:17 -0500

    Rephrase error messages in project creation forms

--- website/project/forms.py
+++ website/project/forms.py
@@ -9,16 +9,16 @@ from framework.forms import BootstrapTextArea, BootstrapTextInput
 class NewProjectForm(Form):
     title    = TextField('Title', [
         validators.Required(message=u'Title is required'),
-        validators.Length(min=1, message=u'Title is too short.(It must have at least 1 character.)'),
-        validators.Length(max=200, message=u'Title is too long.(It must have at least 200 characters.)')
+        validators.Length(min=1, message=u'Title must contain at least 1 character.'),
+        validators.Length(max=200, message=u'Title must contain fewer than 200 characters.')
     ], widget=BootstrapTextInput())
     description    = TextAreaField('Description', widget=BootstrapTextArea())
 
 class NewNodeForm(Form):
     title    = TextField('Title', [
         validators.Required(message=u'Title is required'),
-        validators.Length(min=1, message=u'Title is too short.(It must have at least 1 character.)'),
-        validators.Length(max=200, message=u'Title is too long.(It must have at least 200 characters.)')
+        validators.Length(min=1, message=u'Title must contain at least 1 character.'),
+        validators.Length(max=200, message=u'Title must contain fewer than 200 characters.')
     ], widget=BootstrapTextInput())
     description     = TextAreaField('Description', widget=BootstrapTextArea())
     category        = TextAreaField('Category', widget=BootstrapTextArea())
