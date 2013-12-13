4224774149238afc2fb858833b2a0b1556a2b946
Joshua Carp <jm.carp@gmail.com>
2013-12-12 20:29:26 -0500

    Debugging name email

--- scripts/impute_names.py
+++ scripts/impute_names.py
@@ -2,6 +2,8 @@
 Email users to verify citation information.
 """
 
+import re
+
 from framework.auth.utils import parse_name
 from framework.email.tasks import send_email
 
@@ -11,44 +13,67 @@ from website import settings
 
 #app = init_app('website.settings', set_backends=True, routes=True)
 
-email_template = u'''Hello, {fullname},\n
-\n
-Along with a shorter domain name (https://osf.io), the Open Science Framework
-has recently introduced a citation widget on project and component dashboards.\n
-\n
+email_template = u'''Hello, {fullname},
+
+
+Along with a shorter domain name (http://osf.io), the Open Science Framework
+has recently introduced a citation widget on project and component dashboards.
+
+
 As such, we are expanding user settings to include Citation Style Language name
-specifications that will allow us to accurately produce said citations.\n
-\n
-Based upon your full name, "{fullname}", we've done our best to automatically infer the following:\n
-\n
-Given name: {given_name}\n
-Middle name(s): {middle_names}\n
-Family name: {family_name}\n
-Suffix: {suffix}\n
-\n
+specifications that will allow us to accurately produce said citations.
+
+
+Based upon your full name, "{fullname}", we've done our best to automatically infer the following:
+
+
+Given name: {given_name}
+
+Middle name(s): {middle_names}
+
+Family name: {family_name}
+
+Suffix: {suffix}
+
+
 If this information is correct, you don't need to do anything. If you'd like
-to make an adjustment or test the parsing, please browse to\n
-\n
-https://osf.io/settings\n
-\n
-and correct these values. If you have any questions or comments about this, please contact
-us at feedback@osf.io (don't reply to this email).\n
-\n
-I remain sincerely yours,\n
-\n
+to make an adjustment or test the parsing algorithm, please browse to
+
+
+http://osf.io/settings
+
+
+If you have any questions or comments, please contact us at feedback@osf.io (don't reply to this email).
+
+
+I remain sincerely yours,
+
+
 The OSF Robot.
 '''
+
+def clean_template(template):
+    cleaned = ''
+    for line in template.splitlines():
+        cleaned += line or '\n'
+    cleaned = re.sub(' +', ' ', cleaned)
+    return cleaned
+
+email_template = clean_template(email_template)
 
 def email_name(user):
 
     names = {'fullname': user.fullname}
     names.update(parse_name(user.fullname))
 
-    send_email.delay(
+    message=email_template.format(**names).encode('utf-8')
+
+    send_email(
         from_addr=settings.FROM_EMAIL,
         to_addr=user.username,
         subject='OSF: Verify your citation information',
-        message=email_template.format(**names).encode('utf-8'),
+        message=message,
+        mimetype='plain',
     )
 
 #def email_names():
