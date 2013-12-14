8825d67bd8913cafcd1ff3215a9ef145a92b18c2
Joshua Carp <jm.carp@gmail.com>
2013-12-13 22:03:27 -0500

    Add script to impute name parts

---
+++ scripts/impute_names_model.py
@@ -0,0 +1,23 @@
+"""
+
+"""
+
+from framework.auth.utils import parse_name
+
+from website.app import init_app
+from website import models
+
+app = init_app('website.settings', set_backends=True, routes=True)
+
+def impute_names():
+
+    for user in models.User.find():
+
+        parsed = parse_name(user.fullname)
+        for field, value in parsed.items():
+            print field, value
+            setattr(user, field, value)
+        user.save()
+
+if __name__ == '__main__':
+    impute_names()
