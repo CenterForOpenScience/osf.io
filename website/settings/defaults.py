6048f43c7361292b1b699e5d3ddbbabc3bfb1535
Lyndsy Simon <lyndsy@lyndsysimon.com>
2014-01-06 11:27:58 -0500

    cleans up settings

--- website/settings/defaults.py
+++ website/settings/defaults.py
@@ -122,6 +122,6 @@ CELERY_IMPORTS = (
 
 # Piwik (analytics)
 # TODO: Override in local.py in production
-PIWIK_HOST = '' # Domain only - no http:// or trailing slash
-PIWIK_ADMIN_TOKEN = ''
+PIWIK_HOST = 'http://localhost:8000/piwik/'
+PIWIK_ADMIN_TOKEN = '4f5fba03a86607a215fe91bd47735689'
 PIWIK_SITE_ID = 1
--- website/settings/local-dist.py
+++ website/settings/local-dist.py
@@ -11,7 +11,7 @@ DEV_MODE = True
 DEBUG_MODE = True  # Sets app to debug mode, turns off template caching, etc.
 
 # Comment out to use solr in development
-USE_SOLR = False
+USE_SOLR = True
 
 # Email
 MAIL_SERVER = 'localhost:1025'  # For local testing
@@ -33,7 +33,3 @@ USE_CDN_FOR_CLIENT_LIBS = False
 
 # Example of extending default settings
 # defaults.IMG_FMTS += ["pdf"]
-
-PIWIK_HOST = 'http://localhost:8889'
-PIWIK_SITE_ID = 1
-PIWIK_ADMIN_TOKEN = ''
