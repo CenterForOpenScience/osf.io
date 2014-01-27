ec5a99b1325e1de81d18be9784c26c5a8c68001c
Joshua Carp <jm.carp@gmail.com>
2014-01-27 16:54:06 -0500

    Show GitHub URL in add-on configuration

--- website/addons/github/model.py
+++ website/addons/github/model.py
@@ -50,6 +50,13 @@ class AddonGitHubNodeSettings(AddonNodeSettingsBase):
     registration_data = fields.DictionaryField()
 
     @property
+    def repo_url(self):
+        if self.user and self.repo:
+            return 'https://github.com/{0}/{1}/'.format(
+                self.user, self.repo
+            )
+
+    @property
     def short_url(self):
         if self.user and self.repo:
             return '/'.join([self.user, self.repo])
@@ -60,6 +67,7 @@ class AddonGitHubNodeSettings(AddonNodeSettingsBase):
         rv.update({
             'github_user': self.user or '',
             'github_repo': self.repo or '',
+            'github_url': self.repo_url if self.repo_url else '',
             'user_has_authorization': github_user and github_user.has_auth,
             'show_submit': True,
         })
