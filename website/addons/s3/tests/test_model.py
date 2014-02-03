bbcca70905b0133c40066416a0efa580fbe9393f
Chris Seto <Chriskseto@gmail.com>
2014-02-03 13:17:03 -0500

    Added Test and fixed others

--- website/addons/s3/tests/test_model.py
+++ website/addons/s3/tests/test_model.py
@@ -1,4 +1,6 @@
 from nose.tools import *
+import mock
+from boto.s3.connection import *
 
 from tests.base import DbTestCase
 from tests.factories import UserFactory, ProjectFactory
@@ -26,24 +28,33 @@ class TestCallbacks(DbTestCase):
         self.user_settings = self.project.creator.get_addon('s3')
         self.user_settings.access_key = 'We-Will-Rock-You'
         self.user_settings.secret_key = 'Idontknowanyqueensongs'
-        self.node_settings.user = 'Queen'
         self.node_settings.bucket = 'Sheer-Heart-Attack'
+        self.node_settings.node_access_key = 'opensaysme'
+        self.node_settings.node_secret_key = 'secret'
         self.node_settings.save()
 
-    def test_node_settings_empty_bucket(self):
+    @mock.patch('website.addons.s3.model.get_bucket_drop_down')
+    def test_node_settings_empty_bucket(self, mock_drop):
+        mock_drop.return_value = ''
         s3 = AddonS3NodeSettings()
         assert_equals(s3.to_json(self.project.creator)['has_bucket'], 0)
 
-    def test_node_settings_full_bucket(self):
+    @mock.patch('website.addons.s3.model.get_bucket_drop_down')
+    def test_node_settings_full_bucket(self, mock_drop):
+        mock_drop.return_value = ''
         s3 = AddonS3NodeSettings()
         s3.bucket = 'bucket'
         assert_equals(s3.to_json(self.project.creator)['has_bucket'], 1)
 
-    def test_node_settings_user_auth(self):
+    @mock.patch('website.addons.s3.model.get_bucket_drop_down')
+    def test_node_settings_user_auth(self, mock_drop):
+        mock_drop.return_value = ''
         s3 = AddonS3NodeSettings()
         assert_equals(s3.to_json(self.project.creator)['user_has_auth'], 1)
 
-    def test_node_settings_moar_use(self):
+    @mock.patch('website.addons.s3.model.get_bucket_drop_down')
+    def test_node_settings_moar_use(self, mock_drop):
+        mock_drop.return_value = ''
         assert_equals(self.node_settings.to_json(
             self.project.creator)['user_has_auth'], 1)
 
@@ -52,3 +63,47 @@ class TestCallbacks(DbTestCase):
         s3.access_key = "Sherlock"
         s3.secret_key = "lives"
         assert_equals(s3.to_json(self.project.creator)['has_auth'], 1)
+
+    @mock.patch('website.addons.s3.model.get_bucket_drop_down')
+    def test_after_fork_authenticator(self, mock_drop):
+        mock_drop.return_value = ''
+        fork = ProjectFactory()
+        clone, message = self.node_settings.after_fork(
+            self.project, fork, self.project.creator,
+        )
+        assert_equal(
+            self.node_settings.node_access_key,
+            clone.node_access_key,
+        )
+
+    #Expected failure merge develop_s3_forking to fix
+    @mock.patch('website.addons.s3.model.get_bucket_drop_down')
+    def test_after_fork_not_authenticator(self, mock_drop):
+        mock_drop.return_value = ''
+        fork = ProjectFactory()
+        clone, message = self.node_settings.after_fork(
+            self.project, fork, self.non_authenticator,
+        )
+        assert_equal(
+            clone.node_access_key,
+            None,
+        )
+
+    @mock.patch('website.addons.s3.utils.get_bucket_list')
+    def test_drop_down_disabled(self, mock_drop):
+        bucket = mock.create_autospec(Bucket)
+        bucket.name = 'Aticus'
+        mock_drop.return_value = [bucket]
+        drop_list = self.node_settings.to_json(self.project.creator)['bucket_list']
+        assert_true('disabled' in drop_list)
+
+    @mock.patch('website.addons.s3.model.serialize_bucket')
+    @mock.patch('website.addons.s3.model.S3Wrapper.from_addon')
+    def test_registration(self, mock_wrapper, mock_bucket):
+        mock_wrapper.return_value = None
+        mock_bucket.return_value = {'Not None': 'None'}
+        fork = ProjectFactory()
+        clone, message = self.node_settings.after_register(
+            self.project, fork, self.project.creator,
+        )
+        assert_true(clone.is_registration)
