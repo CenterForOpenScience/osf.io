349b8db826d2e664994d35305a88cbce06b409f7
Chris Seto <Chriskseto@gmail.com>
2014-02-04 12:08:23 -0500

    Finished(?) hgrid configs. Untested and unused

--- website/addons/s3/static/hgrid-s3.js
+++ website/addons/s3/static/hgrid-s3.js
@@ -5,68 +5,35 @@
 
     // Public stuff
     FileBrowser.cfg.github = {
-        listeners: [
-            {
-                on: 'change',
-                selector: '.github-branch-select',
-                callback: function(){}
-            }
-        ],
+        uploadMethod = 'PUT';
+        fileName = '';
+        mimeType = '';
         uploadUrl: function(row) {
+            var deferred = $.Deferred();
+
             return $.ajax({
-                type: 'put',
-                url: '/path/to/signed/link',
+                type: 'POST',
+                url: nodeApiUrl + '/s3/upload/',
+                data: JSON.stringify({name: fileName, type:mimeType}),
+                contentType: 'application/json',
+                dataType: 'json'
+            }).success(function (url) {
+                deferred.resolve(url);
             });
+        }
+
+        uploadAdded: function(file, item) {
+            fileName = file.name;
+            mimeType = file.type;
         }
     };
 
 })(FileBrowser);
 
-
-//TODO Dir
-function unused(file)
-{
-  $.ajax({
-      url:  uplUrl + 's3/getsigned/',
-      type: 'POST',
-      data: JSON.stringify({name:file.name,type:file.type}),
-      contentType: 'application/json',
-      dataType: 'json'
-        }).success(function(url) {
-
-            xhr = new XMLHttpRequest();
-
-            if (xhr.withCredentials != null) {
-                xhr.open('PUT', url, true);
-            } else if (typeof XDomainRequest !== "undefined") {
-                xhr = new XDomainRequest();
-                xhr.open('PUT', url);
-            } else {
-                xhr = null;
-            }
-            xhr.onload = function() {
-                if (xhr.status === 200) {
-                    console.log("completed")
-                } else {
-                    console.log('Upload error: ' + xhr.status);
-                }
-            };
-            xhr.onerror = function() {
-                console.log('XHR error.', file);
-            };
-            xhr.upload.onprogress = function(e) {
-                var percentLoaded;
-                if (e.lengthComputable) {
-                    percentLoaded = Math.round((e.loaded / e.total) * 100);
-                    console.log(percentLoaded)
-                }
-            };
-
-            type = file.type || 'application/octet-stream';
-
-            xhr.setRequestHeader('Content-Type', type);
-            xhr.setRequestHeader('x-amz-acl', 'private');
-            return xhr.send(file);
-
-        });
-};
+/*
+ *
+ * xhr.setRequestHeader('Content-Type', type);
+ * xhr.setRequestHeader('x-amz-acl', 'private');
+ *
+ *
+ */
