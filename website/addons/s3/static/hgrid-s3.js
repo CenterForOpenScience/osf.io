55dae8427265237c063612165dc1a468fca6909b
Chris Seto <Chriskseto@gmail.com>
2014-02-04 15:16:05 -0500

    Updated Hgrid config thanks @sloria

--- website/addons/s3/static/hgrid-s3.js
+++ website/addons/s3/static/hgrid-s3.js
@@ -4,28 +4,21 @@
     // Private stuff
 
     // Public stuff
-    FileBrowser.cfg.github = {
+    FileBrowser.cfg.s3 = {
         uploadMethod = 'PUT';
-        //TODO can this be moved up a level?
-        fileName = '';
-        mimeType = '';
-        uploadUrl: function(row) {
+
+        uploadAdded: function(file, item) {
             var deferred = $.Deferred();
 
             return $.ajax({
                 type: 'POST',
                 url: nodeApiUrl + '/s3/upload/',
-                data: JSON.stringify({name: fileName, type:mimeType}),
+                data: JSON.stringify({name: file.name, type: file.type}),
                 contentType: 'application/json',
                 dataType: 'json'
             }).success(function (url) {
                 deferred.resolve(url);
             });
-        }
-
-        uploadAdded: function(file, item) {
-            fileName = file.name;
-            mimeType = file.type;
         }
     };
 
