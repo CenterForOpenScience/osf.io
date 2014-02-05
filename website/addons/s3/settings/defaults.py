6f8c72438604d26aaf34ccb69ad52d081cafe545
Chris Seto <Chriskseto@gmail.com>
2014-02-05 09:38:31 -0500

    Updated allowed_origin

--- website/addons/s3/settings/defaults.py
+++ website/addons/s3/settings/defaults.py
@@ -1,5 +1,5 @@
 MAX_RENDER_SIZE = (1024 ** 2) * 3
-ALLOWED_ORIGIN = '*'  # 'https://osf.io/' change for staging etc
+ALLOWED_ORIGIN = 'http://localhost:5000'  # 'https://osf.io/' change for staging etc
 CORS_RULE_UPLOAD = (
     '<CORSRule><AllowedMethod>PUT</AllowedMethod><AllowedOrigin>'
     + ALLOWED_ORIGIN +
