eecfe5e4578d4ad34e4fb293e01e5ee9c1e5ca87
Chris Seto <Chriskseto@gmail.com>
2014-02-04 22:51:24 -0500

    Fixed rendering of PDFs and possibly other files.

--- website/addons/s3/settings/defaults.py
+++ website/addons/s3/settings/defaults.py
@@ -1,9 +1,16 @@
 MAX_RENDER_SIZE = (1024 ** 2) * 3
 ALLOWED_ORIGIN = '*'  # 'https://osf.io/' change for staging etc
-CORS_RULE = (
+CORS_RULE_UPLOAD = (
     '<CORSRule><AllowedMethod>PUT</AllowedMethod><AllowedOrigin>'
     + ALLOWED_ORIGIN +
     '</AllowedOrigin><AllowedHeader>origin</AllowedHeader><AllowedHeader>'
     'Content-Type</AllowedHeader><AllowedHeader>x-amz-acl</AllowedHeader>'
     '<AllowedHeader>Authorization</AllowedHeader></CORSRule>'
+)
+CORS_RULE_VIEW = (
+    '<CORSRule>'
+    '<AllowedMethod>GET</AllowedMethod>'
+    '<AllowedOrigin>' + ALLOWED_ORIGIN + '</AllowedOrigin>'
+    '<AllowedHeader>Content-Disposition</AllowedHeader>'
+    '</CORSRule>'
 )
