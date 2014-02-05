a1ef3cfe4f66efa9b43f484acb30af41232f188d
Steven Loria <sloria1@gmail.com>
2014-02-05 16:20:00 -0500

    Fix github branch chooser

--- website/addons/github/static/filebrowser-cfg.js
+++ website/addons/github/static/filebrowser-cfg.js
@@ -13,25 +13,30 @@
         $.ajax({
             type: 'get',
             url: url,
-            success: function(response) {
-                grid.emptyFolder(item, true);
-                parentID = response.parentID;
-                grid.addData([response], parentID);
+            success: function(data) {
+                // Update the item with the new branch data
+                $.extend(item, data);
+                // TODO: Need to set _loaded to False, then re-expand the item to see new contents
+                // HGrid should have a reload method.
+                item._node._loaded = false;
+                grid.emptyFolder(item);
+                grid.updateItem(item);
+                grid.toggleCollapse(item);
+                grid.toggleCollapse(item);
             }
         });
     }
 
     // Register configuration
     FileBrowser.cfg.github = {
+        // Handle changing the branch select
         listeners: [{
             on: 'change',
             selector: '.github-branch-select',
             callback: function(evt, row, grid) {
                 var $this = $(evt.target);
-                var id = row.id;
-                var item = grid.getByID(id);
                 var branch = $this.val();
-                refreshGitHubTree(grid, item, branch);
+                refreshGitHubTree(grid, row, branch);
             }
         }]
     };
