2af3b102bfb5fc29b585c147424978af8246a6a6
Steven Loria <sloria1@gmail.com>
2014-02-10 17:20:37 -0500

    Fix deleting S3 files after they are uploaded

--- website/addons/s3/static/s3-rubeus-cfg.js
+++ website/addons/s3/static/s3-rubeus-cfg.js
@@ -22,13 +22,16 @@
             xhr.setRequestHeader('Content-Type', file.type || 'application/octet-stream');
             xhr.setRequestHeader('x-amz-acl', 'private');
         },
-
         uploadSuccess: function(file, item, data) {
+            // Update the added item's urls and permissions
             item.urls = {
                 'delete': nodeApiUrl + 's3/delete/' + file.destination + '/',
                 'download': nodeApiUrl + 's3/download/' + file.destination + '/',
                 'view': '/' + nodeId + '/s3/view/' + file.destination + '/'
-            }
+            };
+            var parent = this.getByID(item.parentID);
+            item.permissions = parent.permissions;
+            this.updateItem(item);
         }
     };
 
