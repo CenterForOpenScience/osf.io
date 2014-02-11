806d2f97e4501ce1b94cdf1c0e6986bad72eb3d1
Chris Seto <Chriskseto@gmail.com>
2014-02-11 16:06:58 -0500

    Custom status messages can now be set in rubeus config files. Might be a bit slow, not 100% certain.

--- website/static/js/rubeus.js
+++ website/static/js/rubeus.js
@@ -73,12 +73,28 @@ this.Rubeus = (function($, HGrid, bootbox) {
     HGrid.Html.statusSelector = '[data-status]';
 
     /**
+     * Get the status message from the addon, is any.
+     */
+     function getStatusCfg(addon, whichStatus, extra) {
+        if(addon && Rubeus.cfg[addon] && Rubeus.cfg[addon][whichStatus]) {
+            if (typeof(Rubeus.cfg[addon][whichStatus]) === 'function')
+                return Rubeus.cfg[addon][whichStatus].call(extra);
+            else
+                return Rubeus.cfg[addon][whichStatus];
+        }
+        if (typeof(default_status[whichStatus]) === 'function')
+            return default_status[whichStatus](extra);
+        else
+            return default_status[whichStatus];
+     };
+
+    /**
      * Changes the html in the status column.
      */
-    HGrid.prototype.changeStatus = function(row, html, fadeAfter) {
+    HGrid.prototype.changeStatus = function(row, html, extra, fadeAfter) {
         var rowElem = this.getRowElement(row.id);
         var $status = $(rowElem).find(HGrid.Html.statusSelector);
-        $status.html(html);
+        $status.html(getStatusCfg(row.addon, html, extra));
         if (fadeAfter) {
             setTimeout(function() {
                 $status.fadeOut('slow');
@@ -87,8 +103,7 @@ this.Rubeus = (function($, HGrid, bootbox) {
         return $status;
     };
 
-    // TODO: This should be configurable by addon devs
-    var status = {
+    var default_status = {
         FETCH_SUCCESS: '',
         FETCH_START: '<span class="text-muted">Fetching contents. . .</span>',
         FETCH_ERROR: '<span class="text-info">Could not retrieve data. Please refresh the page and try again.</span>',
@@ -109,7 +124,18 @@ this.Rubeus = (function($, HGrid, bootbox) {
             return '<span class="text-info">' + Math.floor(progress) + '%</span>';
         }
     };
-
+    var statusType = {
+        FETCH_SUCCESS: 'FETCH_SUCCESS',
+        FETCH_START: 'FETCH_START',
+        FETCH_ERROR: 'FETCH_ERROR',
+        UPLOAD_SUCCESS: 'UPLOAD_SUCCESS',
+        NO_CHANGES: 'NO_CHANGES',
+        UPDATED: 'UPDATED',
+        DELETING: 'DELETING',
+        DELETED: 'DELETED',
+        UPLOAD_ERROR: 'UPLOAD_ERROR',
+        UPLOAD_PROGRESS: 'UPLOAD_PROGRESS'
+    };
     ////////////////////////
     // Listener callbacks //
     ////////////////////////
@@ -119,7 +145,7 @@ this.Rubeus = (function($, HGrid, bootbox) {
             var rowCopy = $.extend({}, row);
             // Show "Deleting..." message in parent folder's status column
             var parent = grid.getByID(rowCopy.parentID);
-            grid.changeStatus(row, status.DELETING(rowCopy));
+            grid.changeStatus(row, statusType.DELETING, rowCopy);
             grid.deleteFile(row, {
                 error: function() {
                     // TODO: This text should be configurable by addon devs
@@ -128,7 +154,7 @@ this.Rubeus = (function($, HGrid, bootbox) {
                 success: function() {
                     grid.getDataView().updateItem(parent.id, parent);
                     // Show 'Successfully deleted' in folder's status column
-                    grid.changeStatus(row, status.DELETED(rowCopy));
+                    grid.changeStatus(row, statusType.DELETED, rowCopy);
                     setTimeout(function(){
                         grid.removeItem(rowCopy.id);
                     }, 1000);
@@ -167,16 +193,16 @@ this.Rubeus = (function($, HGrid, bootbox) {
             return row.urls.fetch;
         },
         fetchSuccess: function(data, row) {
-            this.changeStatus(row, status.FETCH_SUCCESS);
+            this.changeStatus(row, statusType.FETCH_SUCCESS);
         },
         fetchError: function(error, row) {
-            this.changeStatus(row, status.FETCH_ERROR);
+            this.changeStatus(row, statusType.FETCH_ERROR);
         },
         fetchStart: function(row) {
-            this.changeStatus(row, status.FETCH_START);
+            this.changeStatus(row, statusType.FETCH_START);
         },
         uploadProgress: function(file, progress, bytesSent, row) {
-            this.changeStatus(row, status.UPLOAD_PROGRESS(progress));
+            this.changeStatus(row, statusType.UPLOAD_PROGRESS(progress));
         },
         downloadUrl: function(row) {
             return row.urls.download;
@@ -234,14 +260,14 @@ this.Rubeus = (function($, HGrid, bootbox) {
             // is removed and we don't have access to the original row for the file
             var self = this;
             if (data.actionTaken === null) {
-                self.changeStatus(row, status.NO_CHANGES);
+                self.changeStatus(row, statusType.NO_CHANGES);
                 setTimeout(function() {
                     $(self.getRowElement(row)).fadeOut(500, function() {
                         self.removeItem(row.id);
                     });
                 }, 2000);
             } else if (data.actionTaken === 'file_updated') {
-                self.changeStatus(row, status.UPDATED);
+                self.changeStatus(row, statusType.UPDATED);
                 setTimeout(function() {
                     $(self.getRowElement(row)).fadeOut(500, function() {
                         self.removeItem(row.id);
@@ -252,7 +278,7 @@ this.Rubeus = (function($, HGrid, bootbox) {
                 // This is necessary for the download and delete button to work.
                 $.extend(row, data);
                 this.updateItem(row);
-                this.changeStatus(row, status.UPLOAD_SUCCESS, 2000);
+                this.changeStatus(row, statusType.UPLOAD_SUCCESS, 2000);
             }
             var cfgOption = resolveCfgOption.call(this, row, 'uploadSuccess', [file, row, data]);
             return cfgOption || null;
