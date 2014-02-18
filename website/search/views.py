ceefafb9a5b6070d678df74e13426935e318a667
Steven Loria <sloria1@gmail.com>
2014-02-18 12:14:23 -0500

    Don't show inactive (merged, deleted, unregistered) users in search

--- website/search/views.py
+++ website/search/views.py
@@ -1,9 +1,13 @@
+import re
 import time
 from urllib2 import HTTPError
 import logging
 
 from framework import request, status
 from website.search.solr_search import search_solr
+from website import settings
+from website.filters import gravatar
+from website.models import User
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger('search.routes')
@@ -195,11 +199,6 @@ def create_result(highlights, results):
             result_search.append(container)
     return result_search, tags
 
-import re
-
-from website import settings
-from website.filters import gravatar
-from website.models import User
 
 def _search_contributor(query):
     """Search for contributors to add to a project using Solr. Request must
@@ -229,17 +228,18 @@ def _search_contributor(query):
     for doc in docs:
         # TODO: use utils.serialize_user
         user = User.load(doc['id'])
-        users.append({
-            'fullname': doc['user'],
-            'id': doc['id'],
-            'gravatar': gravatar(
-                user,
-                use_ssl=True,
-                size=settings.GRAVATAR_SIZE_ADD_CONTRIBUTOR,
-            ),
-            'registered': user.is_registered,
-            'active': user.is_active()
-        })
+        if user.is_active():  # exclude merged, unregistered, etc.
+            users.append({
+                'fullname': doc['user'],
+                'id': doc['id'],
+                'gravatar': gravatar(
+                    user,
+                    use_ssl=True,
+                    size=settings.GRAVATAR_SIZE_ADD_CONTRIBUTOR,
+                ),
+                'registered': user.is_registered,
+                'active': user.is_active()
+            })
 
     return {'users': users}
 
