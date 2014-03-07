4f0848fdc002b4fe336f8e20c3d63cf22cd0e0c5
Joshua Carp <jm.carp@gmail.com>
2014-03-07 10:29:12 -0500

    catch null contributors

--- scripts/migrate_permissions.py
+++ scripts/migrate_permissions.py
@@ -17,9 +17,12 @@ def main():
 
 def migrate_permissions(node):
     for contributor in node.contributors:
-        node.set_permissions(
-            contributor, ['read', 'write', 'admin'], save=False
-        )
+        if contributor:
+            node.set_permissions(
+                contributor, ['read', 'write', 'admin'], save=False
+            )
+        else:
+            logger.warn('Null contributor found on node {0}'.format(node._id))
     node.save()
     logger.info('Finished migrating node {0}'.format(node._id))
 
