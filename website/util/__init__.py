cb68c88d14df1df806bcee8b5308823be3d9a07e
Steven Loria <sloria1@gmail.com>
2014-03-07 10:30:52 -0500

    Move api_url_for and web_url_for to prevent circular import

--- website/util/__init__.py
+++ website/util/__init__.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
+import framework
 from . import rubeus
 
 
 
+def api_url_for(view_name, *args, **kwargs):
+    return framework.url_for('JSONRenderer__{0}'.format(view_name),
+        *args, **kwargs)
+
+
+def web_url_for(view_name, *args, **kwargs):
+    return framework.url_for('OsfWebRenderer__{0}'.format(view_name),
+        *args, **kwargs)
