a7ac7610f25d71dab5ca43dd2477b395f1270655
Steven Loria <sloria1@gmail.com>
2014-03-18 17:46:01 -0400

    Add language.js module

---
+++ website/static/js/language.js
@@ -0,0 +1,22 @@
+$.osf.Language = {
+
+    // TODO
+    makePublic: null,
+    makePrivate: null,
+
+    Addons: {
+        dropbox: {
+            // Shown on clicking "Delete Access Token for github"
+            confirmDeauth: 'Are you sure you want to delete your Dropbox access' +
+                'key? This will revoke access to Dropbox for all projects you have ' +
+                'authorized delete your access token from Dropbox.'
+        },
+        // TODO
+        github: {
+
+        },
+        s3: {
+
+        }
+    }
+};
