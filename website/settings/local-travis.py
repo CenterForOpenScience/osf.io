22ce03d8f4760ca0a22c912f732779e6926fdb29
Joshua Carp <jm.carp@gmail.com>
2014-03-19 10:38:07 -0400

    no celery on travis

--- website/settings/local-travis.py
+++ website/settings/local-travis.py
@@ -13,6 +13,8 @@ DEBUG_MODE = True  # Sets app to debug mode, turns off template caching, etc.
 # Comment out to use solr in development
 USE_SOLR = False
 
+USE_CELERY = False
+
 # Email
 MAIL_SERVER = 'localhost:1025'  # For local testing
 MAIL_USERNAME = 'osf-smtp'
