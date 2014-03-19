58504cd9ffe2071659de7a159e31977b7a686048
Joshua Carp <jm.carp@gmail.com>
2014-03-19 10:32:41 -0400

    add local.py for travis

---
+++ website/settings/local-travis.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+'''Example settings/local.py file.
+These settings override what's in website/settings/defaults.py
+
+NOTE: local.py will not be added to source control.
+'''
+
+from . import defaults
+
+DEV_MODE = True
+DEBUG_MODE = True  # Sets app to debug mode, turns off template caching, etc.
+
+# Comment out to use solr in development
+USE_SOLR = False
+
+# Email
+MAIL_SERVER = 'localhost:1025'  # For local testing
+MAIL_USERNAME = 'osf-smtp'
+MAIL_PASSWORD = 'CHANGEME'
+
+# Session
+COOKIE_NAME = 'osf'
+SECRET_KEY = "CHANGEME"
+
+##### Celery #####
+## Default RabbitMQ broker
+BROKER_URL = 'amqp://'
+
+# Default RabbitMQ backend
+CELERY_RESULT_BACKEND = 'amqp://'
+
+USE_CDN_FOR_CLIENT_LIBS = False
