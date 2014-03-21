0f6f7b5a76728a1f55e59500e0f6e5586ee51a3b
Steven Loria <sloria1@gmail.com>
2014-03-21 11:36:44 -0400

    Fix hgrid views

--- website/addons/dropbox/views/hgrid.py
+++ website/addons/dropbox/views/hgrid.py
@@ -1,10 +1,14 @@
+# -*- coding: utf-8 -*-
 import os
+import logging
 
 from website.project.decorators import must_be_contributor_or_public, must_have_addon
 from website.util import rubeus, api_url_for
 
+from website.addons.dropbox.client import get_node_client
 
-from ..client import get_node_client
+logger = logging.getLogger(__name__)
+debug = logger.debug
 
 
 @must_be_contributor_or_public
@@ -13,7 +17,9 @@ def dropbox_hgrid_data_contents(**kwargs):
     node_settings = kwargs['node_addon']
     node = node_settings.owner
     auth = kwargs['auth']
-    path = kwargs.get('path', '') + '/'  # Might need to be unquoted
+    path = kwargs.get('path', node_settings.folder)
+    if not path.endswith('/'):  # ensure trailing slash
+        path += '/'
 
     can_edit = node.can_edit(auth) and not node.is_registration
     can_view = node.can_view(auth)
@@ -21,19 +27,18 @@ def dropbox_hgrid_data_contents(**kwargs):
     client = get_node_client(node)
     files = []
     for item in client.metadata(path)['contents']:
-
-        temp_file = {}
-        temp_file['addon'] = 'dropbox'
-        temp_file['permissions'] = {
+        # TODO(sloria): Add a serialization function for a single item
+        serialized = {}
+        serialized['addon'] = 'dropbox'
+        serialized['permissions'] = {
             'edit': can_edit,
             'view': can_view
         }
-        temp_file['name'] = os.path.basename(item['path'])
-        temp_file['ext'] = os.path.splitext(item['path'])[1]
-        temp_file[rubeus.KIND] = rubeus.FOLDER if item['is_dir'] else rubeus.FILE
-        temp_file['urls'] = build_dropbox_urls(item, node.api_url, node._id)
-
-        files.append(temp_file)
+        serialized['name'] = os.path.basename(item['path'])
+        serialized['ext'] = os.path.splitext(item['path'])[1]
+        serialized[rubeus.KIND] = rubeus.FOLDER if item['is_dir'] else rubeus.FILE
+        serialized['urls'] = build_dropbox_urls(item, node.api_url, node._id)
+        files.append(serialized)
 
     return files
 
@@ -42,8 +47,11 @@ def dropbox_addon_folder(node_settings, auth, **kwargs):
     node = node_settings.owner
     return [
         rubeus.build_addon_root(
-            node_settings, node_settings.folder, permissions=auth,
-            nodeUrl=node.url, nodeApiUrl=node.api_url,
+            node_settings=node_settings,
+            name=node_settings.folder,
+            permissions=auth,
+            nodeUrl=node.url,
+            nodeApiUrl=node.api_url,
         )
     ]
 
@@ -58,7 +66,7 @@ def build_dropbox_urls(item, api_url, nid):
         }
     else:
         return {
-            'download': api_url_for('dropbox_download', path=item['path'], pid=nid),#'{0}dropbox{1}/'.format(api_url, item['path']),
+            'download': api_url_for('dropbox_download', path=item['path'], pid=nid),
             'view': '/{0}/dropbox{1}/'.format(nid, item['path']), #TODO Write me
             'delete': api_url_for('dropbox_download', path=item['path'], pid=nid)
         }
