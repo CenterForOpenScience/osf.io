eae31d83c6e523fd893cde713bb41a7520dc1813
Joshua Carp <jm.carp@gmail.com>
2014-03-24 11:18:49 -0400

    fix failing tests

--- website/addons/gitlab/tests/test_utils.py
+++ website/addons/gitlab/tests/test_utils.py
@@ -3,10 +3,26 @@ from nose.tools import *
 
 import os
 import urllib
+import urlparse
+
+from framework.exceptions import HTTPError
 
 from website.addons.gitlab.tests import GitlabTestCase
-
 from website.addons.gitlab import utils
+
+
+def normalize_query_string(url):
+    parsed = urlparse.urlparse(url)
+    query_data = dict(urlparse.parse_qsl(parsed.query))
+    query_str = urllib.urlencode(query_data)
+    return parsed._replace(query=query_str).geturl()
+
+
+def assert_urls_equal(url1, url2):
+    assert_equal(
+        normalize_query_string(url1),
+        normalize_query_string(url2)
+    )
 
 
 class TestTranslatePermissions(unittest.TestCase):
@@ -32,12 +48,42 @@ class TestTranslatePermissions(unittest.TestCase):
 
 class TestKwargsToPath(unittest.TestCase):
 
-    pass
+    def test_kwargs_to_path(self):
+        assert_equal(
+            utils.kwargs_to_path({'path': 'foo/bar/baz'}),
+            urllib.unquote_plus('foo/bar/baz')
+        )
+
+    def test_kwargs_to_path_required(self):
+        with assert_raises(HTTPError):
+            utils.kwargs_to_path({}, required=True)
 
 
 class TestRefsToParams(unittest.TestCase):
 
-    pass
+    def test_no_kwargs(self):
+        assert_equal(
+            utils.refs_to_params(),
+            ''
+        )
+
+    def test_branch(self):
+        assert_equal(
+            utils.refs_to_params(branch='master'),
+            '?' + urllib.urlencode({'branch': 'master'})
+        )
+
+    def test_sha(self):
+        assert_equal(
+            utils.refs_to_params(sha='12345'),
+            '?' + urllib.urlencode({'sha': '12345'})
+        )
+
+    def test_branch_sha(self):
+        assert_equal(
+            utils.refs_to_params(branch='master', sha='12345'),
+            '?' + urllib.urlencode({'branch': 'master', 'sha': '12345'})
+        )
 
 
 class TestSlugify(unittest.TestCase):
@@ -53,15 +99,6 @@ class TestSlugify(unittest.TestCase):
             utils.gitlab_slugify('foo.git'),
             'foo'
         )
-
-
-class TestTypeToKind(unittest.TestCase):
-
-    def test_blob(self):
-        pass
-
-    def test_tree(self):
-        pass
 
 
 class TestBuildUrls(GitlabTestCase):
@@ -90,14 +127,14 @@ class TestBuildUrls(GitlabTestCase):
             set(output.keys()),
             {'upload', 'fetch'}
         )
-        assert_equal(
+        assert_urls_equal(
             output['upload'],
             self.node_lookup(
                 'api', 'gitlab_upload_file',
                 path=quote_path, branch=branch
             )
         )
-        assert_equal(
+        assert_urls_equal(
             output['fetch'],
             self.node_lookup(
                 'api', 'gitlab_list_files',
@@ -125,25 +162,25 @@ class TestBuildUrls(GitlabTestCase):
             set(output.keys()),
             {'view', 'download', 'delete'}
         )
-        assert_equal(
+        assert_urls_equal(
             output['view'],
             self.node_lookup(
                 'web', 'gitlab_view_file',
                 path=quote_path, branch=branch, sha=sha
             )
         )
-        assert_equal(
+        assert_urls_equal(
             output['download'],
             self.node_lookup(
                 'web', 'gitlab_download_file',
                 path=quote_path, branch=branch, sha=sha
             )
         )
-        assert_equal(
+        assert_urls_equal(
             output['delete'],
             self.node_lookup(
                 'api', 'gitlab_delete_file',
-                path=quote_path, branch=branch, sha=sha
+                path=quote_path, branch=branch
             )
         )
 
