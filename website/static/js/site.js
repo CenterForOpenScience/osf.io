efc232ae12f850f2df0431b0e6199d2d635ce36b
Steven Loria <sloria1@gmail.com>
2014-03-29 00:49:02 -0400

    Move tag cloud initializtion to search template

--- website/static/js/site.js
+++ website/static/js/site.js
@@ -232,18 +232,6 @@
         $('[rel=tooltip]').tooltip({
             placement:'bottom'
         });
-
-        //  Initiate tag cloud (on search page)
-        $.fn.tagcloud.defaults = {
-          size: {start: 14, end: 18, unit: 'pt'},
-          color: {start: '#cde', end: '#f52'}
-        };
-
-        $(function () {
-          $('#tagCloud a').tagcloud();
-        });
-
-
     });
 
 }).call(this, jQuery, window);
