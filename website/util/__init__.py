bdf84d8e6c4251eaf6a5b67e15153d123265e939
Steven Loria <sloria1@gmail.com>
2014-03-31 16:10:23 -0400

    Add _absolute parameter to url_for helpers

--- website/util/__init__.py
+++ website/util/__init__.py
@@ -2,15 +2,31 @@
 import framework
 from . import rubeus
 
+from website import settings
 
 
-def api_url_for(view_name, *args, **kwargs):
+
+def api_url_for(view_name, _absolute=False, *args, **kwargs):
+    if _absolute:
+        external = True
+        scheme = 'http' if settings.DEBUG_MODE else 'https'
+    else:
+        external = False
+        scheme = 'http'
     return framework.url_for('JSONRenderer__{0}'.format(view_name),
+        _external=external, _scheme=scheme,
         *args, **kwargs)
 
 
-def web_url_for(view_name, *args, **kwargs):
+def web_url_for(view_name, _absolute=False, *args, **kwargs):
+    if _absolute:
+        external = True
+        scheme = 'http' if settings.DEBUG_MODE else 'https'
+    else:
+        external = False
+        scheme = 'http'
     return framework.url_for('OsfWebRenderer__{0}'.format(view_name),
+        _external=external, _scheme=scheme,
         *args, **kwargs)
 
 
