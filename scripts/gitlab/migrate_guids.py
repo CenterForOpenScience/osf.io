4f5ad9ac85b593c81c8972e403d737ca8d19d2c0
Joshua Carp <jm.carp@gmail.com>
2014-04-04 16:49:26 -0400

    migration creates new file object and resets guid referent

--- scripts/gitlab/migrate_guids.py
+++ scripts/gitlab/migrate_guids.py
@@ -1,4 +1,8 @@
+from modularodm import Q
+from modularodm.exceptions import ModularOdmException
+
 from website.app import init_app
+from website.models import Guid
 
 from website.addons.osffiles.model import OsfGuidFile
 from website.addons.gitlab.model import GitlabGuidFile
@@ -9,12 +13,24 @@ app = init_app('website.settings', set_backends=True, routes=True)
 
 def migrate_file_obj(file_obj):
 
-    node_settings = file_obj.node.get_addon('gitlab')
-    gitlab_obj = GitlabGuidFile.get_or_create(
-        node_settings=node_settings,
-        path=file_obj.path,
-        client=None
-    )
+    guid_obj = Guid.load(file_obj._id)
+
+    try:
+        gitlab_obj = GitlabGuidFile.find_one(
+            Q('_id', 'eq', file_obj._id)
+        )
+    except ModularOdmException:
+        gitlab_obj = GitlabGuidFile(_id=file_obj._id)
+
+    gitlab_obj.node = file_obj.node
+    gitlab_obj.path = file_obj.name
+
+    gitlab_obj.save()
+
+    guid_obj.referent = gitlab_obj
+    guid_obj.save()
+
+    return gitlab_obj
 
 
 def migrate_file_objs():
