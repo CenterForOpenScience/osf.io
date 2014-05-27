f416c1de109b09d35c7029a7a4bd6cc10e2b62b3
Robert Liebowitz <rliebz@gmail.com>
2014-05-27 16:27:16 -0400

    Adds and improves dataverse utils tests.

--- website/addons/dataverse/tests/test_utils.py
+++ website/addons/dataverse/tests/test_utils.py
@@ -4,8 +4,9 @@ import mock
 from website.addons.dataverse.dvn.dataverse import Dataverse
 from website.addons.dataverse.dvn.file import DvnFile, ReleasedFile
 from website.addons.dataverse.dvn.study import Study
-from website.addons.dataverse.tests.utils import create_mock_connection, \
-    create_mock_dataverse, create_mock_study, DataverseAddonTestCase
+from website.addons.dataverse.tests.utils import (create_mock_connection,
+    create_mock_dataverse, create_mock_study, create_mock_dvn_file,
+    create_mock_released_file, DataverseAddonTestCase)
 
 
 class TestUtils(DataverseAddonTestCase):
@@ -20,7 +21,7 @@ class TestUtils(DataverseAddonTestCase):
         assert_equal(mock_connection.username, 'snowman')
         assert_equal(mock_connection.password, 'frosty')
         assert_equal(len(mock_connection.get_dataverses()), 3)
-        assert_equal(mock_connection.get_dataverses()[0].__class__, Dataverse)
+        assert_is_instance(mock_connection.get_dataverses()[0], Dataverse)
         assert_equal(
             mock_connection.get_dataverse(mock_connection.get_dataverses()[1].alias),
             mock_connection.get_dataverses()[1],
@@ -32,7 +33,7 @@ class TestUtils(DataverseAddonTestCase):
         assert_true(mock_dv.is_released)
         assert_equal(mock_dv.alias, 'ALIAS1')
         assert_equal(len(mock_dv.get_studies()), 3)
-        assert_equal(mock_dv.get_studies()[0].__class__, Study)
+        assert_is_instance(mock_dv.get_studies()[0], Study)
         assert_equal(mock_dv.get_study_by_hdl(mock_dv.get_studies()[1].get_id()),
                      mock_dv.get_studies()[1])
 
@@ -46,12 +47,26 @@ class TestUtils(DataverseAddonTestCase):
         assert_equal(mock_study.doi, 'doi:12.3456/{0}'.format(sid))
         assert_equal(mock_study.get_state(), 'DRAFT')
         assert_equal(len(mock_study.get_files()), 1)
-        assert_equal(mock_study.get_files()[0].__class__, DvnFile)
-        assert_equal(mock_study.get_files(released=True)[0].__class__,
-                     ReleasedFile)
-        assert_equal(mock_study.get_file('name.txt').__class__, DvnFile)
-        assert_equal(mock_study.get_file('name.txt', released=True).__class__,
-                     ReleasedFile)
-        assert_equal(mock_study.get_file_by_id('123').__class__, DvnFile)
-        assert_equal(mock_study.get_file_by_id('123', released=True).__class__,
-                     ReleasedFile)
+        assert_is_instance(mock_study.get_files()[0], DvnFile)
+        assert_is_instance(mock_study.get_files(released=True)[0], ReleasedFile)
+        assert_is_instance(mock_study.get_file('name.txt'), DvnFile)
+        assert_is_instance(mock_study.get_file('name.txt', released=True),
+                           ReleasedFile)
+        assert_is_instance(mock_study.get_file_by_id('123'), DvnFile)
+        assert_is_instance(mock_study.get_file_by_id('123', released=True),
+                           ReleasedFile)
+
+    def test_mock_dvn_file(self):
+        fid = '65432'
+        mock_file = create_mock_dvn_file(fid)
+        assert_equal(mock_file.name, 'file.txt')
+        assert_equal(mock_file.id, fid)
+        assert_is_instance(mock_file, DvnFile)
+
+    def test_mock_dvn_file(self):
+        fid = '65432'
+        mock_file = create_mock_released_file(fid)
+        assert_equal(mock_file.name, 'released.txt')
+        assert_equal(mock_file.id, fid)
+        assert_is_instance(mock_file, ReleasedFile)
+
