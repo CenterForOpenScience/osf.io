f992729745883a91bf8884c6755d7b866b03247f
Robert Liebowitz <rliebz@gmail.com>
2014-05-29 11:38:21 -0400

    Merges with develop.

--- website/project/model.py
+++ website/project/model.py
@@ -476,7 +476,6 @@ class Node(GuidStoredObject, AddonModelMixin):
     # TODO: Add validator
     comment_level = fields.StringField(default='private')
 
-    # TODO: move these to NodeFile
     files_current = fields.DictionaryField()
     files_versions = fields.DictionaryField()
     wiki_pages_current = fields.DictionaryField()
@@ -622,6 +621,9 @@ class Node(GuidStoredObject, AddonModelMixin):
         :returns: User has required permission
 
         """
+        if user is None:
+            logger.error('User is ``None``.')
+            return False
         try:
             return permission in self.permissions[user._id]
         except KeyError:
