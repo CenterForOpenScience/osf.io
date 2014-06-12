f870855c245dcc77fa07ae00c89639a67187a769
Jim Witschey <jim.witschey@gmail.com>
2014-06-12 14:52:00 -0400

    moves error-processing code to its own file

--- website/app.py
+++ website/app.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import importlib
 import logging
-import re
 
 from framework import storage, db, app
 from framework.mongo import set_up_storage
@@ -10,6 +9,8 @@ from framework.sentry import sentry
 import website.models
 from website.routes import make_url_map
 from website.addons.base import init_addon
+from website.util.errors import flask_endpoint_overwrite
+
 
 logger = logging.getLogger(__name__)
 
@@ -31,24 +32,6 @@ def init_addons(settings, routes=True):
 
     settings.ADDON_CAPABILITIES = render_addon_capabilities(settings.ADDONS_AVAILABLE)
 
-
-def human_message(error):
-    m = re.match(
-        'View function mapping is overwriting an existing endpoint function: (.*)__(.*)',
-        error.message)
-    if not m:
-        return None
-
-    clobbered_renderer = m.group(1)
-    clobbered_function = m.group(2)
-
-    human_message = ' '.join(["You're trying to overwrite an existing",
-                    "Flask endpoint with a renderer named `{}`".format(clobbered_renderer),
-                    "and a view function named `{}`.".format(clobbered_function),
-                    "You can't do that. Rename your view function or add a postfix."])
-    return human_message
-
-
 def init_app(settings_module='website.settings', set_backends=True, routes=True):
     """Initializes the OSF. A sort of pseudo-app factory that allows you to
     bind settings, set up routing, and set storage backends, but only acts on
@@ -64,9 +47,9 @@ def init_app(settings_module='website.settings', set_backends=True, routes=True)
         init_addons(settings, routes)
     except AssertionError as error:  # Addon Route map has already been created
         logger.error(error)
-        message = human_message(error)
+        message = flask_endpoint_overwrite(error)
         if message:
-            logger.debug(human_message(error))
+            logger.debug(flask_endpoint_overwrite(error))
 
     app.debug = settings.DEBUG_MODE
     if set_backends:
@@ -79,7 +62,7 @@ def init_app(settings_module='website.settings', set_backends=True, routes=True)
         try:
             make_url_map(app)
         except AssertionError as error:  # Route map has already been created
-            message = human_message(error)
+            message = flask_endpoint_overwrite(error)
             if message:
                 logger.debug(message)
 
---
+++ website/util/errors.py
@@ -0,0 +1,18 @@
+import re
+
+# produces a human-helpful debug message if you try to overwrite an existing endpoint function
+def flask_endpoint_overwrite(error):
+    m = re.match(
+        'View function mapping is overwriting an existing endpoint function: (.*)__(.*)',
+        error.message)
+    if not m:
+        return None
+
+    clobbered_renderer = m.group(1)
+    clobbered_function = m.group(2)
+
+    human_message = ' '.join(["You're trying to overwrite an existing",
+                    "Flask endpoint with a renderer named `{}`".format(clobbered_renderer),
+                    "and a view function named `{}`.".format(clobbered_function),
+                    "You can't do that. Rename your view function or add a postfix."])
+    return human_message
