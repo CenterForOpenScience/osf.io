b1769f5dc4eee948152262577300a4bcc6eb7d28
Joshua Carp <jm.carp@gmail.com>
2014-06-13 10:03:17 -0400

    Adds Dataverse to `ADDONS_REQUESTED`

--- website/settings/defaults.py
+++ website/settings/defaults.py
@@ -131,7 +131,7 @@ CELERY_IMPORTS = (
 ADDONS_REQUESTED = [
     'wiki', 'osffiles',
     'github', 's3', 'figshare',
-    'dropbox',
+    'dropbox', 'dataverse',
     # 'badges',
     'forward',
 ]
