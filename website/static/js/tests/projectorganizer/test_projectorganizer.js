7f3c7543be1615fa8ef1b324d1ae2c9219940892
Brian J. Geiger <bgeiger@pobox.com>
2014-06-16 10:52:14 -0400

    Added scaffolding for javascript unit tests. Integrated new version of Hgrid with support for expand/collapse state. General cleaning of code. Improved user interactions.

--- website/static/js/tests/projectorganizer/test_projectorganizer.js
+++ website/static/js/tests/projectorganizer/test_projectorganizer.js
@@ -1,7 +1,46 @@
-var projectbrowser = new ProjectOrganizer('#project-grid');
+describe("Jasmine works at all", function () {
+    beforeEach(function () {
+        jasmine.Ajax.install();
+    });
 
-describe("A suite", function() {
-  it("contains spec with an expectation", function() {
-    expect(true).toBe(true);
-  });
+    afterEach(function () {
+        jasmine.Ajax.uninstall();
+    });
+
+    it("true equals true", function () {
+        expect(true).toBe(true);
+    });
+
+    it("ajax specifying response when you need it", function () {
+        var doneFn = jasmine.createSpy("success");
+
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function (arguments) {
+            if (this.readyState == this.DONE) {
+                doneFn(this.responseText);
+            }
+        };
+        xhr.open("GET", "/some/cool/url");
+        xhr.send();
+        expect(jasmine.Ajax.requests.mostRecent().url).toBe('/some/cool/url');
+        expect(doneFn).not.toHaveBeenCalled();
+        jasmine.Ajax.requests.mostRecent().response({
+            "status": 200,
+            "contentType": 'text/plain',
+            "responseText": 'awesome response'
+        });
+        expect(doneFn).toHaveBeenCalledWith('awesome response');
+    });
+});
+
+describe("Dahsboard has a project organizer", function () {
+    beforeEach(function (done) {
+        var projectbrowser = new ProjectOrganizer('#project-grid');
+    });
+
+    it("appears on the dashboard", function () {
+
+            expect($('#project-grid').html()).not.toEqual('');
+
+    });
 });
