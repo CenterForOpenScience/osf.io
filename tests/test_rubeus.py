8e84eae8dff69174f6d253427506232ed5de1da3
Jim Witschey <jim.witschey@gmail.com>
2014-07-15 14:01:58 -0400

    fixes broken tests to test correct behavior

--- tests/test_rubeus.py
+++ tests/test_rubeus.py
@@ -450,7 +450,7 @@ class TestSerializingFolders(OsfTestCase):
 
         folder_hgrid = rubeus.to_project_hgrid(folder, self.auth)
 
-        assert_valid_hgrid_folder(folder_hgrid)
+        assert_equal(folder_hgrid, [])
 
     def test_serialize_folder_containing_folder(self):
         """
@@ -492,11 +492,14 @@ class TestSmartFolderViews(OsfTestCase):
             url = api_url_for('get_dashboard')
 
         res = self.app.get(url + self.AMP_ID)
-        init_len = len(res.json)
+
+        import pprint;pp = pprint.PrettyPrinter()
+
+        init_len = len(res.json[u'data'])
 
         ProjectFactory(creator=self.user)
         res = self.app.get(url + self.AMP_ID)
-        assert_equal(len(res.json), init_len + 1)
+        assert_equal(len(res.json[u'data']), init_len + 1)
 
 
     @mock.patch('website.project.decorators.get_api_key')
@@ -513,11 +516,11 @@ class TestSmartFolderViews(OsfTestCase):
             url = api_url_for('get_dashboard')
 
         res = self.app.get(url + self.AMR_ID)
-        init_len = len(res.json)
+        init_len = len(res.json[u'data'])
 
         RegistrationFactory(creator=self.user)
         res = self.app.get(url + self.AMR_ID)
-        assert_equal(len(res.json), init_len + 1)
+        assert_equal(len(res.json[u'data']), init_len + 1)
 
 
 def assert_valid_hgrid_folder(node_hgrid):
@@ -547,22 +550,28 @@ def assert_valid_hgrid_folder(node_hgrid):
         'type': 'folder'
     }
 
-    assert_is_instance(node_hgrid, list)
+    print node_hgrid
+    if isinstance(node_hgrid, list):
+        node_hgrid = node_hgrid[0]['data']
+    else:
+        assert_is_instance(node_hgrid, dict)
 
-    # for key, correct_value in folder_values.items():
-    #     assert_equal(node_hgrid[key], correct_value)
-    #
-    # for key, correct_type in folder_types.items():
-    #     assert_is_instance(node_hgrid[key], correct_type)
-    #
-    # for key, correct_type in keys_types.items():
-    #     for inner_key, inner_value in node_hgrid[key].items():
-    #         assert_is_instance(inner_value, correct_type)
-    #
-    #
-    # valid_keys = set(folder_types.keys()).union(folder_values.keys())
-    # for key in node_hgrid.keys():
-    #     assert_in(key, valid_keys)
+
+
+    for key, correct_value in folder_values.items():
+        assert_equal(node_hgrid[key], correct_value)
+
+    for key, correct_type in folder_types.items():
+        assert_is_instance(node_hgrid[key], correct_type)
+
+    for key, correct_type in keys_types.items():
+        for inner_key, inner_value in node_hgrid[key].items():
+            assert_is_instance(inner_value, correct_type)
+
+
+    valid_keys = set(folder_types.keys()).union(folder_values.keys())
+    for key in node_hgrid.keys():
+        assert_in(key, valid_keys)
 
 
 def assert_valid_hgrid_smart_folder(node_hgrid):
