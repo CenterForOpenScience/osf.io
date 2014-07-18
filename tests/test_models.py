7960aed5c0e26a9d89543c61f941a19d07e01914
Saman Ehsan <saman@Saman-Ehsans-MacBook-Pro.local>
2014-07-18 16:15:23 -0400

    added test for get projects in common method

--- tests/test_models.py
+++ tests/test_models.py
@@ -466,6 +466,17 @@ class TestUser(OsfTestCase):
         project.save()
         assert_equal(u.display_full_name(node=project), name)
 
+    def test_get_projects_in_common(self):
+        user2 = UserFactory()
+        user3 = UserFactory()
+        project = ProjectFactory(creator=self.user)
+
+        project.add_contributor(contributor=user2, auth=self.consolidate_auth)
+        project.save()
+
+        assert_equal(self.user.get_projects_in_common(user2), 1)
+        assert_equal(self.user.get_projects_in_common(user3), 0)
+
 
 class TestUserParse(unittest.TestCase):
 
