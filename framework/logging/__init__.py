f6ab8f4c1d582a961da642e7dcc16a5f6c6e57c9
Lyndsy Simon <lyndsy@lyndsysimon.com>
2014-07-30 10:39:02 -0400

    Make colorlog an optional dependency

--- framework/logging/__init__.py
+++ framework/logging/__init__.py
@@ -1,13 +1,17 @@
 import logging
 import logging.config
 
-import colorlog
+try:
+    import colorlog
+    use_color = True
+except ImportError:
+    use_color = False
 
 from website import settings
 
 handler = logging.StreamHandler()
 
-if settings.DEBUG_MODE:
+if settings.DEBUG_MODE and use_color:
     log_colors = colorlog.default_log_colors
     log_colors['DEBUG'] = 'cyan'
 
