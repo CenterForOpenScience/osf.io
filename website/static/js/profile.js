0a95aff655269c14a45322794f888709e5dda1b1
Brian J. Geiger <bgeiger@pobox.com>
2014-08-07 16:49:36 -0400

    Removes redundant date observable extension.
    https://github.com/CenterForOpenScience/openscienceframework.org/issues/838
    
    One of the two problems, and the one that made these controls the most unusable, is that the date validator required YYYY-MM-DD, but putting in a date that didn't naturally have double-digits on both MM and DD would strip out the leading 0, thus causing the validator to fail.
    
    There were two extensions to the observable that were date related. One was going to the validator, and the other… I'm not sure what it was doing. Taking it out didn't seem to break anything, and fixed at least the most important part of the issue, so we'll start with that.

--- website/static/js/profile.js
+++ website/static/js/profile.js
@@ -458,11 +458,9 @@
         self.title = ko.observable('');
 
         self.start = ko.observable().extend({
-            asDate: true,
             date: true
         });
         self.end = ko.observable().extend({
-            asDate: true,
             date: true,
             minDate: self.start
         });
