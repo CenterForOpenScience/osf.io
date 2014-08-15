8e8f675ccc7f9315c7db6ed2957f1db61b0531fd
Chris Seto <chriskseto@gmail.com>
2014-08-15 15:48:21 -0400

    Guis are the most important trust me Im a programmer

--- website/addons/app/static/appNodeConfig.js
+++ website/addons/app/static/appNodeConfig.js
@@ -16,16 +16,30 @@
         var self = this;
         self.url = url;
         // TODO: Initialize observables, computes, etc. here
-        self.nodeHasAuth = ko.observable(false);
-        self.userHasAuth = ko.observable(false);
+        self.customRoutes = ko.observableArray([]);
         // Flashed messages
         self.message = ko.observable('');
+        self.customUrl = ko.observable('');
+        self.customQuery = ko.observable('');
+
+        self.createCustomRoute = function() {
+            $.osf.postJSON(self.url, {route: self.customUrl(),  query: self.customQuery()}, function(resp) {
+                self.customRoutes.push({url: self.customUrl(),  query: self.customQuery()});
+                self.customUrl('');
+                self.customQuery('');
+            });
+        };
 
         // Get data from the config GET endpoint
         function onFetchSuccess(response) {
             // Update view model
-            self.nodeHasAuth(response.result.nodeHasAuth);
-            self.userHasAuth(resposne.result.userHasAuth);
+            self.customRoutes(ko.utils.arrayMap(Object.keys(response), function(key) {
+                return {
+                    url: key,
+                    query: response[key]
+                };
+            }));
+
         }
         function onFetchError(xhr, textstatus, error) {
             self.message('Could not fetch settings.');
@@ -37,7 +51,7 @@
             });
         }
 
-
+        fetch();
     };
 
     function AppNodeConfig(selector, url) {
@@ -46,5 +60,7 @@
         self.viewModel = new ViewModel(url);
         $.osf.applyBindings(self.viewModel, selector);
     }
+
+    return AppNodeConfig
 
 }));
--- website/addons/app/templates/app_node_settings.mako
+++ website/addons/app/templates/app_node_settings.mako
@@ -1,6 +1,6 @@
 <!-- TODO -->
 <h4 class="addon-title">
-    Application
+    Application <small>${application_url}</small>
 </h4>
 
 
@@ -9,11 +9,38 @@
     <!-- <pre data-bind="text: ko.toJSON($data, null, 2)"></pre> -->
 
     <!-- TODO: Settings Interface here -->
-    <input data-bind="value: message" type="text" class="form-control">
-
+    <h4>Custom Routes</h4>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Route</th>
+                <th>Query</th>
+            </tr>
+        </thead>
+        <tbody data-bind="foreach: customRoutes">
+            <tr>
+                <td>
+                    <a data-bind="attr: { href: url}">{{url}}</a>
+                </td>
+                <td>{{query}}</td>
+            </tr>
+        </tbody>
+    </table>
+    <hr>
+    <form class="form-inline" role="form">
+        <input class="form-control" type="text" data-bind="value: customUrl" placeholder="Url">
+        <input class="form-control" type="text" data-bind="value: customQuery" placeholder="Query">
+        <button class="btn btn-success" data-bind="click: createCustomRoute">Create</button>
+    </form>
     <!-- Flashed Messages -->
     <div class="help-block">
         <p data-bind="html: message"></p>
     </div>
 
 </div>
+
+<script>
+    $script('/static/addons/app/appNodeConfig.js', function() {
+        AppNodeConfig('#appScope', '${'/api/v1/%s/app/q/' % node['id']}');
+    });
+</script>
