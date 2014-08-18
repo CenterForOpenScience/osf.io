83b5ed08b8fafeec6de9df34064972e903e924e3
Casey Rollins <cmr3rm@virginia.edu>
2014-08-18 13:20:54 -0400

    Added function that returns the affected nodes

--- scripts/migrate_addons.py
+++ scripts/migrate_addons.py
@@ -8,12 +8,19 @@ from tests.factories import NodeFactory
 from website.app import init_app
 from website.project.model import Node
 
+from website.addons.wiki.model import AddonWikiNodeSettings
+from website.addons.osffiles.model import AddonFilesNodeSettings
+
 logger = logging.getLogger(__name__)
+
+ADDONS = {AddonFilesNodeSettings, AddonWikiNodeSettings}
 
 
 def main():
-    init_app()
-    migrate_nodes()
+    from framework.mongo import db
+    init_app(routes=False)
+    migrate_nodes(db)
+
 
 def migrate_addons(node):
     ret = False
@@ -25,46 +32,75 @@ def migrate_addons(node):
         ret = True
     return ret
 
-def migrate_nodes():
-    migrated_count = 0
-    nodes = []
-    for node in Node.find():
-        was_migrated = migrate_addons(node)
-        if was_migrated:
-            node.save()
-            nodes.append(node)
-            logger.info('Migrated {0}'.format(node._id))
-            migrated_count += 1
-    logger.info('Finished migrating {0} nodes.'.format(migrated_count))
-    return nodes
+
+def migrate_nodes(db):
+    for addon_class in ADDONS:
+        print('Processing ' + addon_class.__name__)
+
+        for node in get_affected_nodes(db, addon_class):
+            print(' - ' + node._id)
+            migrate_addons(node)
+
+        print('')
+
+    print('-----\nDone.')
+
+
+def get_affected_nodes(db, addon_class):
+    """Generate affected nodes."""
+    query = db['node'].find({
+        '.'.join(
+            ('__backrefs',
+                'addons',
+                addon_class.__name__.lower(),
+                'owner',
+                '0'
+            )
+        ): {'$exists': False}
+    })
+    return (Node.load(node['_id']) for node in query)
 
 
 class TestMigratingAddons(OsfTestCase):
 
     def test_migrate_wiki(self):
         node = NodeFactory()
-        (node.get_addon('wiki')).delete(save=True)
+        wiki_addon = node.get_addon('wiki')
+        AddonWikiNodeSettings.remove_one(wiki_addon)
         assert_false(node.has_addon('wiki'))
         was_migrated = migrate_addons(node)
         assert_true(was_migrated)
-        node.save()
         assert_true(node.has_addon('wiki'))
 
     def test_migrate_osffiles(self):
         node = NodeFactory()
-        (node.get_addon('osffiles')).delete(save=True)
+        osf_addon = node.get_addon('osffiles')
+        AddonFilesNodeSettings.remove_one(osf_addon)
         assert_false(node.has_addon('osffiles'))
         was_migrated = migrate_addons(node)
         assert_true(was_migrated)
-        node.save()
         assert_true(node.has_addon('osffiles'))
 
     def test_no_migration_if_addon_exists(self):
         node = NodeFactory()
         assert_true(node.has_addon('wiki'))
         assert_true(node.has_addon('osffiles'))
-        was_migrated = migrate_addons(node)
-        assert_false(was_migrated)
+        migrate_nodes(self.db)
+        assert_false(migrate_addons(node))
+
+    def test_affected_nodes(self):
+        affected_node = NodeFactory()
+        AddonWikiNodeSettings.remove_one(affected_node.get_addon('wiki'))
+        assert_false(affected_node.has_addon('wiki'))
+
+        unaffected_node = NodeFactory()
+        assert_true(unaffected_node.has_addon('wiki'))
+
+        affected_nodes = get_affected_nodes(self.db, AddonWikiNodeSettings)
+
+        assert affected_node in affected_nodes
+        assert unaffected_node not in affected_nodes
+
 
 if __name__ == '__main__':
     main()
