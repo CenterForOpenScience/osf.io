05ea8a0872ec1af56f411fc4279361369ecd38de
erinspace <erin.braswell@gmail.com>
2014-08-19 09:28:53 -0400

    a bit more changes for rss feed

--- website/publishers/rss.py
+++ website/publishers/rss.py
@@ -1,2 +1,8 @@
 from website.search import views as search_views
 
+
+## TODO
+## use the search_search function from website/search/views.py
+## use the publsihers folder just like the search folder
+## display as json
+## display the 10 most recent public projects that aren't registrations
