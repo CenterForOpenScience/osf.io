334a2ad544bc4f0ba0c803200f23ca5e9d7682b4
erinspace <erin.braswell@gmail.com>
2014-08-20 10:16:29 -0400

    adding more files for the trial RSS feed

--- website/publishers/rss.py
+++ website/publishers/rss.py
@@ -1,10 +1,46 @@
-from website.search import views as search_views
+import logging
+import datetime
+from cStringIO import StringIO
 
-search_views.search_search()
+import PyRSS2Gen as pyrss
+
+from website import search
+
+logger = logging.getLogger(__name__)
 
 
-## TODO
-## use the search_search function from website/search/views.py
-## use the publsihers folder just like the search folder
-## display as json
-## display the 10 most recent public projects that aren't registrations
+@search.requires_search
+def gen_rss_feed(raw_query):
+    results, count = search.search('scrapi', raw_query, start=0, size=100)
+    logger.info('{n} results returned from search'.format(n=len(results)))
+    xml = dict_to_rss(results, count, raw_query)
+    return xml
+
+
+def dict_to_rss(results, count, query):
+    if not query:
+        query = '*'
+    docs = results
+
+    items = [
+        pyrss.RSSItem(
+            title=str(doc.get('title')),
+            link='http://' + settings.URL + '/' + doc.get('location')[0],
+            description=format_description(doc),
+            guid=str(doc.get('id')),
+            pubDate=str(doc.get('timestamp'))
+        ) for doc in docs if doc.get('location') is not None
+    ]
+    logger.info("{n} documents added to RSS feed".format(n=len(items)))
+    rss = pyrss.RSS2(
+        title='scrAPI: RSS feed for documents retrieved from query: "{query}"'.format(query=query),
+        link='{base_url}/rss?q={query}'.format(base_url=settings.URL, query=query),
+        items=items,
+        description='{n} results, {m} most recent displayed in feed'.format(n=count, m=len(items)),
+        lastBuildDate=str(datetime.datetime.now()),
+    )
+
+    f = StringIO()
+    rss.write_xml(f)
+
+    return f.getvalue()
