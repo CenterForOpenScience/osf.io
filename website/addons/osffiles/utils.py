b17e39436bde57558c1a9d6e70330a51dd1d0d19
sloria <sloria1@gmail.com>
2014-08-29 16:02:24 -0400

    Clarify documentation for get_versions and get_latest_version_number.

--- website/addons/osffiles/utils.py
+++ website/addons/osffiles/utils.py
@@ -2,7 +2,11 @@ from website.addons.osffiles.exceptions import FileNotFoundError
 
 
 def get_versions(filename, node):
-    """Return file versions for a :class:`NodeFile`.
+    """Return IDs for a file's version records.
+
+    :param str filename: The name of the file.
+    :param Node node: The node which has the requested file.
+    :return: List of ids (strings) for :class:`NodeFile` records.
 
     :raises: FileNotFoundError if file does not exists for the node.
     """
@@ -15,7 +19,10 @@ def get_versions(filename, node):
 
 
 def get_latest_version_number(filename, node):
-    """Return the current version number (0-indexed) for a NodeFile.
+    """Return the current version number (0-indexed) for a file.
+
+    :param str filename: The name of the file.
+    :param Node node: The node which has the requested file.
 
     :raises: FileNotFoundError if file does not exists for the node.
     """
