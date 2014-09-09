4eb62b792eaa55a67112a34f4bee8c74e7b43d4a
Robert Liebowitz <rliebz@gmail.com>
2014-09-09 10:00:49 -0400

    Remove unnecessary files.

--- shareServer.js
+++ shareServer.js
@@ -1,10 +1,8 @@
 var connect = require('connect'),
     sharejs = require('share').server;
 
-var server = connect(
-    connect.logger(),
-    connect.static(__dirname + '/static/website/addons/wiki/static/')
-);
+var server = connect(connect.logger());
+
 var options = {
     db: {type: 'none'},
     browserChannel: { cors: "http://localhost:5000" }
@@ -13,6 +11,6 @@ var options = {
 // Attach the sharejs REST and Socket.io interfaces to the server
 sharejs.attach(server, options);
 
-server.listen(7007, function(){
+server.listen(7007, function() {
     console.log('Server running at http://127.0.0.1:7007/');
 });
