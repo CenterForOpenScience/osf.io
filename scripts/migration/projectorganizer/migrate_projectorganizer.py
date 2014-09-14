2d7fdeda0b0c8eac87a62e68a069c3754ef6ae7a
Brian J. Geiger <bgeiger@pobox.com>
2014-09-14 11:20:29 -0400

    Moved migration script into a main() function to avoid side-effects.

--- scripts/migration/projectorganizer/migrate_projectorganizer.py
+++ scripts/migration/projectorganizer/migrate_projectorganizer.py
@@ -5,17 +5,19 @@ This script must be run from the OSF root directory for the imports to work.
 
 from pymongo import MongoClient
 from website.app import init_app
-
-app = init_app()
-
 from website.settings import DB_USER, DB_PASS, DB_PORT
 
-client = MongoClient('localhost', DB_PORT)
-client.osf20130903.authenticate(DB_USER, DB_PASS)
 
-db = client.osf20130903
-node = db['node']
-node.update({"is_folder": {'$exists': False}}, {'$set': {'is_folder': False}}, multi=True)
+def main():
+    init_app()
+    client = MongoClient('localhost', DB_PORT)
+    client.osf20130903.authenticate(DB_USER, DB_PASS)
 
-print('-----\nDone.')
+    db = client.osf20130903
+    node = db['node']
+    node.update({"is_folder": {'$exists': False}}, {'$set': {'is_folder': False}}, multi=True)
 
+    print('-----\nDone.')
+
+if __name__ == '__main__':
+    main()
