854aa1576053482cbf69cf8e1e993fc523c4fa2e
erinspace <erin.braswell@gmail.com>
2014-09-17 16:24:12 -0400

    Add a basic resourcesyc.py module that will serve as a template.
    Right now, it's only a copy of the rss version, but will serve as a jumping off point for starting to build the resourcesync base!

--- website/publishers/views.py
+++ website/publishers/views.py
@@ -3,6 +3,11 @@
 import bleach
 import logging
 
+from resync.resource import Resource
+from resync.resource_list import ResourceList
+from resync.change_list import ChangeList
+from resync.capability_list import CapabilityList
+
 from website.publishers import rss
 
 from framework.flask import request
@@ -25,3 +30,24 @@ def recent_rss():
     feed = rss.gen_rss_feed(query)
 
     return feed
+
+
+def gen_resourcelist(): 
+    ''' Right now this only returns the most recent 100 
+    scrapi results, but could be modified if I knew better
+    how ... '''
+
+    results, count = get_scrapi_resources('')
+
+    rl = ResourceList()
+
+    for result in results:
+        location = result.get('location')[0]
+        url = 'http://' + settings.URL + '/' + location,
+        resource = Resource(url)
+        rl.add(resource)
+
+    for item in rl:
+        item.uri = item.uri[0]
+
+    return rl.as_xml()
