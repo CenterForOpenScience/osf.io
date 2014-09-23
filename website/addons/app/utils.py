094aa53f6d600629b72c1ad66a42d8518030d064
Chris Seto <chriskseto@gmail.com>
2014-09-23 16:19:45 -0400

    Proper collision detection Fixed an issue where queries ending in ';' returned nothing Catch the issue of elastic search throwing a 404 Now we peel off the first search result and compair titles rather than trying to get down to a single result

--- website/addons/app/utils.py
+++ website/addons/app/utils.py
@@ -5,35 +5,23 @@ from website.project import new_node, Node
 from website.search.search import search
 
 def find_or_create_from_report(report, app):
-    search_map = {
-        'doi': ['id', 'doi'],
-        'title': ['title']
-    }
     # @chrisseto TODO: Find a better way to do this
     # The parent projects need to be marked, currently
     # this could be overwritten
-    search_string = 'is_project:true;'
+    search_string = 'is_project:true;doi:"{doc[id][doi]}";title:"{doc[title]}"'
+    search_string = search_string.format(doc=report)
 
-    for key, val in search_map.items():
-        tmp = report
-        for _key in val:
-            tmp = tmp[_key]
-
-        search_string += '{}:{};'.format(key, tmp)
-
+    try:
         ret = search(search_string, _type=app.namespace, index='metadata')
+    except Exception:
+        ret = None
 
-        if not ret:
-            break
-
-        if ret['hits']['total'] == 1:
+    if ret and ret['hits']['total'] > 0:
+        if ret['hits']['hits'][0]['_source']['title'] == report['title']:
             return Node.load(ret['hits']['hits'][0]['_source']['guid'])
-        elif ret['hits']['total'] == 0:
-            break
 
     resource = new_node('project', report['title'], app.system_user, description=report.get('description'))
     resource.set_privacy('public')
-    # resource.set_visible(app.system_user, False, log=False)
     resource.save()
     # TODO Address this issue
     app.attach_data(resource._id, {'is_project': 'true'})
