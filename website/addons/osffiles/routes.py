f45f325206934cdfb73c4f93533824e4f8b71188
Cailey Fitzgerald <cef3fu@virginia.edu>
2014-09-26 14:50:21 -0400

    Change project.api_url_for to project.web_url_for in tests and views Remove TODO associated with this change

--- website/addons/osffiles/routes.py
+++ website/addons/osffiles/routes.py
@@ -48,8 +48,6 @@ web_routes = {
         ),
 
         # Download file by version
-        # TODO: This should use OsfWebRenderer. Will need change any usages of
-        # project.api_url_for('download_file_by_version') -> project.web_url_for
         Rule(
             [
                 '/project/<pid>/osffiles/<fid>/version/<vid>/download/',
