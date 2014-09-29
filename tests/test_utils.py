cfdd044ce39453ed46e2a565aa29997ffb9f36b5
Brian J. Geiger <bgeiger@pobox.com>
2014-09-29 15:29:25 -0400

    Skip test if libmagic is not available.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,6 +9,12 @@ from website.routes import process_rules, OsfWebRenderer
 from website.util import web_url_for, api_url_for, is_json_request
 from website.util.mimetype import get_mimetype
 
+try:
+    import magic
+    LIBMAGIC_AVAILABLE = True
+except ImportError:
+    LIBMAGIC_AVAILABLE = False
+
 class TestUrlForHelpers(unittest.TestCase):
 
     def setUp(self):
@@ -65,6 +71,7 @@ class TestGetMimeTypes(unittest.TestCase):
         mimetype = get_mimetype(name)
         assert_equal(None, mimetype)
 
+    @unittest.skipIf(LIBMAGIC_AVAILABLE, 'Must have python-magic and libmagic installed')
     def test_unknown_extension_with_python_contents_results_in_python_mimetype(self):
         name = 'test.thisisnotarealextensionidonotcarwhatyousay'
         python_file = os.path.abspath(__file__)
