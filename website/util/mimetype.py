c6d949cbb32e095e5859aa22d11aa1566f5bc63f
Brian J. Geiger <bgeiger@pobox.com>
2014-09-29 15:32:40 -0400

    Make better name for argument.

--- website/util/mimetype.py
+++ website/util/mimetype.py
@@ -5,14 +5,14 @@ HERE = os.path.dirname(os.path.abspath(__file__))
 MIMEMAP = os.path.join(HERE, 'mime.types')
 
 
-def get_mimetype(path, data=None):
+def get_mimetype(path, file_contents=None):
     mimetypes.init([MIMEMAP])
     mimetype, _ = mimetypes.guess_type(path)
 
-    if mimetype is None and data is not None:
+    if mimetype is None and file_contents is not None:
         try:
             import magic
-            mimetype = magic.from_buffer(data, mime=True)
+            mimetype = magic.from_buffer(file_contents, mime=True)
         except ImportError:
             return mimetype
 
