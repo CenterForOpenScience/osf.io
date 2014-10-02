80b62ced82d40b1db73b47d4b5dc67395184a8cc
Nan Chen <nc6r@virginia.edu>
2014-10-02 15:32:34 -0400

    prototype migrate

--- scripts/migrate_github_oauth_settings.py
+++ scripts/migrate_github_oauth_settings.py
@@ -3,10 +3,11 @@
 """Script to migrate nodes with invalid categories."""
 
 import sys
-
+import mock
 from modularodm import Q
 from website.app import init_app
 from tests.base import OsfTestCase
+
 from framework.auth import User
 from website.addons.github.api import GitHub
 from website.addons.github.model import AddonGitHubOauthSettings, AddonGitHubUserSettings
@@ -43,22 +44,77 @@ def get_user_settings():
     return AddonGitHubUserSettings.find()
 
 def main():
-    init_app(routes=False)  # Sets the storage backends on all models
+    init_app(set_backends=True, routes=True)  # Sets the storage backends on all models
+    user_settings = get_user_settings()
     if 'dry' in sys.argv:
         # print list of affected nodes, totals, etc.
-        print "===AddonGithubUserSettings==="
-        
+        for user_setting in user_settings:
+            print "===AddonGithubUserSettings==="
+            print "github_user:"
+            print (user_setting.github_user)
 
     else:
         do_migration(get_user_settings())
 
-class TestMigrateNodeCategories(OsfTestCase):
+
+class TestMigrateGitHubOauthSettings(OsfTestCase):
+    def setUp(self):
+        super(TestMigrateGitHubOauthSettings, self).setUp()
+        self.user_settings = AddonGitHubUserSettings()
+        self.user_settings._id = "testing user settings"
+        self.user_settings.save()
+        self.user_settings.oauth_access_token = "testing acess token"
+        self.user_settings.oauth_token_type = "testing token type"
+        self.user_settings.oauth_state = "no state"
+        self.user_settings.github_user = "testing user"
+        self.user_settings.save()
+
 
     def test_get_targets(self):
-        # ...
+        records = list(get_user_settings())
 
-    def test_do_migration(self):
-        # ...
+        assert_equal(1, len(records))
+        assert_equal(
+            records[0].github_user,
+            self.user_settings.github_user
+        )
+        assert_equal(
+            records[0].oauth_state,
+            self.user_settings.oauth_state
+        )
+        assert_equal(
+            records[0].oauth_access_token,
+            self.user_settings.oauth_access_token
+        )
+        assert_equal(
+            records[0].oauth_token_type,
+            self.user_settings.oauth_token_type
+        )
+
+    @mock.patch('website.addons.github.api.GitHub.user')
+    @mock.patch('website.addons.github.api.GitHub')
+    def test_do_migration(self, mock_github, mock_github_user):
+        mock_github_user.id.return_value = "testing user id"
+        do_migration(get_user_settings())
+        self.user_settings.reload()
+        assert_true(self.user_settings.oauth_settings)
+        assert_true(self.user_settings.oauth_state)
+        assert_equal(
+            self.user_settings.oauth_settings.github_user_name,
+            "testing user"
+        )
+        assert_equal(
+            self.user_settings.oauth_settings.oauth_access_token,
+            "testing acess token"
+        )
+        assert_equal(
+            self.user_settings.oauth_settings.oauth_token_type,
+            "testing token type"
+        )
+        assert_equal(
+            self.user_settings.oauth_settings.github_user_id,
+            "testing user id"
+        )
 
 if __name__ == '__main__':
     main()
