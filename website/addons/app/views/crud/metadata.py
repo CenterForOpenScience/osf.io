b35dc00c8e97bae2d4d4736f4d0f3dc497a89aaa
Chris Seto <chriskseto@gmail.com>
2014-10-03 16:53:54 -0400

    Update metadata crud views

--- website/addons/app/views/crud/metadata.py
+++ website/addons/app/views/crud/metadata.py
@@ -4,83 +4,91 @@ import httplib as http
 
 from flask import request
 
-from framework.guid.model import Metadata
 from framework.exceptions import HTTPError
 
-from website.project.decorators import (
-    must_have_addon, must_have_permission,
-    must_not_be_registration, must_be_contributor_or_public
-)
-from website.addons.app.utils import create_orphaned_metadata
+from website.addons.app.model import Metadata
+from website.project.decorators import must_have_addon
+from website.project.decorators import must_have_permission
+from website.project.decorators import must_be_contributor_or_public
+
+
+
+@must_be_contributor_or_public
+@must_have_addon('app', 'node')
+def get_metadatums(node_addon, mid, **kwargs):
+    return {
+        'ids': [m._id for m in node_addon.Metadata__owner]
+    }
 
 # GET
 @must_be_contributor_or_public
 @must_have_addon('app', 'node')
-def get_metadata(node_addon, guid, **kwargs):
+def get_metadata(node_addon, mid, **kwargs):
     try:
-        return node_addon.get_data(guid)
-    except TypeError:
-        pass
-
-    try:
-        return Metadata.load(guid).to_json()
+        return Metadata.load(mid).to_json()
     except TypeError:
         raise HTTPError(http.NOT_FOUND)
 
 
-# POST, PUT
+# PUT
 @must_have_permission('write')
 @must_have_addon('app', 'node')
-def add_metadata(node_addon, guid, **kwargs):
+def create_metadata(node_addon, **kwargs):
     metadata = request.json
 
     if not metadata:
         raise HTTPError(http.BAD_REQUEST)
-    try:
-        node_addon.attach_data(guid, metadata)
-        node_addon.save()
-    except TypeError:
+
+    metastore = Metadata(app=node_addon, data=metadata)
+
+    return {
+        'id': metastore._id
+    }, http.CREATED
+
+
+@must_have_permission('write')
+@must_have_addon('app', 'node')
+def promote_metadata(node_addon, **kwargs):
+    pass  # TODO
+
+# PUT
+@must_have_permission('write')
+@must_have_addon('app', 'node')
+def update_metadata(node_addon, mid, **kwargs):
+    metadata = request.json
+
+    if not metadata:
         raise HTTPError(http.BAD_REQUEST)
+
+    try:
+        Metadata.load(mid).update(metadata)
+        return http.OK
+    except TypeError:
+        raise HTTPError(http.NOT_FOUND)
 
 
 # DELETE
 @must_have_permission('admin')
 @must_have_addon('app', 'node')
-def delete_metadata(node_addon, guid, **kwargs):
+def delete_metadata(node_addon, mid, **kwargs):
+    metastore = Metadata.load(mid)
+
+    if not metastore:
+        return HTTPError(http.NOT_FOUND)
+
     key = request.args.get('key', None)
 
     if key:
-        key = key.split(',')
+        for k in key.split(','):
+            try:
+                del metastore[k]
+            except KeyError:
+                pass
 
-    metastore = Metadata.load(guid)
-    # Note: You cannot delete keys from orphan metadata
-    if metastore:
-        Metadata.remove_one(metastore, True)
-        return HTTPError(http.NO_CONTENT)
-
-    try:
-        node_addon.delete_data(guid, keys=key)
-    except KeyError:
-        raise HTTPError(http.BAD_REQUEST)
-
-    if key:
         return {
             'deleted': key
         }, http.OK
 
+    Metadata.remove_one(metastore, True)
+
     return HTTPError(http.NO_CONTENT)
-
-
-@must_have_permission('write')
-@must_have_addon('app', 'node')
-def create_ophan_metadata(node_addon, **kwargs):
-    metadata = request.json
-
-    if not metadata:
-        raise HTTPError(http.BAD_REQUEST)
-    try:
-        metastore = create_ophan_metadata(node_addon, metadata)
-        metastore.save()
-        node_addon.save()
-    except TypeError:
-        raise HTTPError(http.BAD_REQUEST)
