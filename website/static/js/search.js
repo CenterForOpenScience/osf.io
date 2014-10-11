0f0ad900784321c017b9551926637494afb32962
Chris Seto <chriskseto@gmail.com>
2014-10-10 23:45:40 -0400

    BEST SEARCH INTERFACE EVar 100% my code i promise

---
+++ website/static/js/search.js
@@ -0,0 +1,136 @@
+;(function (global, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(['knockout', 'jquery', 'knockoutpunches'], factory);
+    } else {
+        global.Search  = factory(ko, jQuery);
+    }
+}(this, function(ko, $) {
+    // Enable knockout punches
+    ko.punches.enableAll();
+
+    //https://stackoverflow.com/questions/7731778/jquery-get-query-string-parameters
+    function qs(key) {
+        key = key.replace(/[*+?^$.\[\]{}()|\\\/]/g, "\\$&"); // escape RegEx meta chars
+        var match = location.search.match(new RegExp("[?&]"+key+"=([^&]+)(&|$)"));
+        return match && decodeURIComponent(match[1].replace(/\+/g, " "));
+    }
+
+    var ViewModel = function(url) {
+        var self = this;
+
+        self.searchStarted = ko.observable(false);
+        self.queryUrl = url;
+        self.totalResults = ko.observable(0);
+        self.resultsPerPage = ko.observable(10);
+        self.currentPage = ko.observable(1);
+        self.query = ko.observable('');
+        self.results = ko.observableArray([]);
+        self.searching = ko.observable(false);
+        self.startDate = ko.observable(Date.now());
+        self.endDate = ko.observable(Date('1970-01-01'));
+
+        self.totalPages = ko.computed(function() {
+            var pageCount = 1;
+            var resultsCount = Math.max(self.resultsPerPage(),1); // No Divide by Zero
+            pageCount = Math.ceil(self.totalResults() / resultsCount);
+            return pageCount;
+        });
+
+        self.nextPageExists = ko.computed(function() {
+            return ((self.totalPages() > 1) && (self.currentPage() < self.totalPages()));
+        });
+
+        self.prevPageExists = ko.computed(function() {
+            return self.totalPages() > 1 && self.currentPage() > 1;
+        });
+
+        self.currentIndex = ko.computed(function() {
+            return Math.max(self.resultsPerPage() * (self.currentPage()-1),0);
+        });
+
+        self.navLocation = ko.computed(function() {
+            return 'Page ' + self.currentPage() + ' of ' + self.totalPages();
+        });
+
+        self.queryObject = ko.computed(function(){
+            return {
+                'query_string': {
+                    'default_field': '_all',
+                    'query': self.query(),
+                    'analyze_wildcard': true,
+                    'lenient': true
+                }
+            };
+        });
+
+        self.dateFilter = ko.computed(function() {
+            return {
+                'range': {
+                    'consumeFinished': {
+                        'gte': self.startDate(),
+                        'lte': self.endDate()
+                    }
+                }
+            };
+        });
+        self.fullQuery = ko.computed(function() {
+            return {
+                'filtered': {
+                    'query': self.queryObject()
+//                    'filter': self.dateFilter()
+                }
+            };
+        });
+
+        self.submit = function() {
+            self.currentPage(1);
+            self.results.removeAll();
+            self.search();
+        };
+
+        self.search = function() {
+            self.searchStarted(true);
+            var jsonData = {'query': self.fullQuery(), 'from': self.currentIndex(), 'size': self.resultsPerPage()};
+            $.osf.postJSON(self.queryUrl , jsonData).success(function(data) {
+                self.totalResults(data.counts.total);
+                self.results.removeAll();
+
+                data.results.forEach(function(result){
+                    self.results.push(result);
+                });
+            }).fail(function(){
+                console.log("error");
+                self.totalResults(0);
+                self.currentPage(0);
+                self.results.removeAll();
+            });
+        };
+
+        self.pageNext = function() {
+            self.currentPage(self.currentPage() + 1);
+            self.search();
+        };
+
+        self.pagePrev = function() {
+            self.currentPage(self.currentPage() - 1);
+            self.search();
+        };
+
+    };
+
+    function Search(selector, url) {
+        // Initialization code
+        var self = this;
+        var query = qs('q');
+        self.viewModel = new ViewModel(url);
+        if (query !== null) {
+            self.viewModel.query(query);
+            self.viewModel.search();
+        }
+        element = $(selector).get();
+        ko.applyBindings(self.viewModel, element[0]);
+    }
+
+    return Search;
+
+}));
