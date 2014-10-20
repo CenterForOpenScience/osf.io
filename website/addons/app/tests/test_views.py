379365abfc32c904968b2c818891a22fbd4ad534
Chris Seto <chriskseto@gmail.com>
2014-10-20 11:54:09 -0400

    Test the majority of metadata views

---
+++ website/addons/app/tests/test_views.py
@@ -0,0 +1,183 @@
+# -*- coding: utf-8 -*-
+
+from nose.tools import *  # noqa (PEP8 asserts)
+
+from tests.base import OsfTestCase
+from tests.factories import AuthUserFactory
+from tests.factories import ProjectFactory
+
+from framework.auth import User, Auth
+
+from website.project import Node
+from website.util import api_url_for, web_url_for
+from website.addons.app.model import Metadata, AppNodeSettings
+
+
+class TestMetadataViews(OsfTestCase):
+
+    def setUp(self):
+        super(TestMetadataViews, self).setUp()
+        self.user = AuthUserFactory()
+        self.auth = Auth(self.user)
+        self.project = ProjectFactory(creator=self.user)
+
+        self.project.add_addon('app', self.auth)
+        self.appAddon = self.project.get_addon('app')
+
+        # Log user in
+        self.app.authenticate(*self.user.auth)
+
+    def test_post_metadata(self):
+        num = Metadata.find().count()
+        url = self.project.api_url_for('create_metadata')
+        ret = self.app.post_json(url, {'foo':'bar'})
+        assert_equals(ret.status_code, 201)
+        assert_true(ret.json['id'])
+        assert_equals(Metadata.find().count(), num + 1)
+
+    def test_get_metadata(self):
+        meta = Metadata(app=self.app)
+        meta.save()
+
+        url = self.project.api_url_for('get_metadata', mid=meta._id)
+        ret = self.app.get(url)
+        assert_equals(ret.status_code, 200)
+        assert_equals(ret.json, {u'_id': meta._id})
+
+    def test_get_metadata_404(self):
+        url = self.project.api_url_for('get_metadata', mid='nickelback')
+        ret = self.app.get(url, expect_errors=True)
+        assert_equals(ret.status_code, 404)
+
+    def test_post_metadata_no_json(self):
+        num = Metadata.find().count()
+        url = self.project.api_url_for('create_metadata')
+        ret = self.app.post_json(url, expect_errors=True)
+
+        assert_equals(ret.status_code, 400)
+        assert_equals(Metadata.find().count(), num)
+
+    def test_update_metadata(self):
+        meta = Metadata(app=self.app)
+        meta['best'] = 'savinme'
+        meta.save()
+
+        num = Metadata.find().count()
+
+        url = self.project.api_url_for('update_metadata', mid=meta._id)
+        ret = self.app.put_json(url, {'band': 'nickelback'})
+
+        assert_equals(ret.status_code, 200)
+
+        meta.reload()
+        assert_equals(meta.to_json(), {
+            u'_id': meta._id,
+            u'best': 'savinme',
+            u'band': 'nickelback'
+        })
+
+        assert_equals(Metadata.find().count(), num)
+
+    def test_update_metadata_no_json(self):
+        meta = Metadata(app=self.app)
+        meta['best'] = 'savinme'
+        meta.save()
+
+        num = Metadata.find().count()
+
+        url = self.project.api_url_for('update_metadata', mid=meta._id)
+        ret = self.app.put_json(url, expect_errors=True)
+
+        assert_equals(ret.status_code, 400)
+        assert_equals(Metadata.find().count(), num)
+
+    def test_update_metadata_no_meta(self):
+        num = Metadata.find().count()
+
+        url = self.project.api_url_for('update_metadata', mid='rockstar')
+        ret = self.app.put_json(url, {'needsmore': 'nickelback'}, expect_errors=True)
+
+        assert_equals(ret.status_code, 404)
+        assert_equals(Metadata.find().count(), num)
+
+    def test_delete_metadata(self):
+        meta = Metadata(app=self.app)
+        meta['best'] = 'savinme'
+        meta.save()
+
+        num = Metadata.find().count()
+
+        url = self.project.api_url_for('delete_metadata', mid=meta._id)
+        ret = self.app.delete(url)
+
+        assert_equals(ret.status_code, 204)
+        assert_equals(Metadata.load(meta._id), None)
+        assert_equals(Metadata.find().count(), num - 1)
+
+    def test_delete_metadata(self):
+        meta = Metadata(app=self.app)
+        meta['best'] = 'creed'
+        meta.save()
+
+        num = Metadata.find().count()
+
+        url = self.project.api_url_for('delete_metadata', mid=meta._id)
+        url += '?key=best'
+        ret = self.app.delete(url)
+
+        meta.reload()
+
+        assert_equals(ret.status_code, 200)
+        assert_equals(ret.json['deleted'], 'best')
+        assert_equals(Metadata.find().count(), num)
+        assert_equals(meta.get('best'), None)
+
+    def test_delete_metadata_no_meta(self):
+        num = Metadata.find().count()
+
+        url = self.project.api_url_for('delete_metadata', mid='cakeordeath')
+        ret = self.app.delete(url, expect_errors=True)
+
+        assert_equals(ret.status_code, 404)
+        assert_equals(Metadata.find().count(), num)
+
+    def test_promote_metadata_no_meta(self):
+        num = Metadata.find().count()
+        nodes = Node.find().count()
+
+        url = self.project.api_url_for('promote_metadata', mid='cakeordeath')
+        ret = self.app.post(url, expect_errors=True)
+
+        assert_equals(ret.status_code, 404)
+        assert_equals(Metadata.find().count(), num)
+        assert_equals(Node.find().count(), nodes)
+
+    # TODO Write more tests
+    def test_promote_metadata_no_data(self):
+        meta = Metadata(app=self.app)
+        meta.save()
+
+        num = Metadata.find().count()
+        nodes = Node.find().count()
+
+        url = self.project.api_url_for('promote_metadata', mid=meta._id)
+        ret = self.app.post(url)
+
+        assert_equals(ret.status_code, 201)
+        assert_true(Node.load(ret.json['id']))
+        assert_equals(Metadata.find().count(), num)
+        assert_equals(Node.find().count(), nodes + 1)
+
+class TestMetadataViews(OsfTestCase):
+
+    def setUp(self):
+        super(TestMetadataViews, self).setUp()
+        self.user = AuthUserFactory()
+        self.auth = Auth(self.user)
+        self.project = ProjectFactory(creator=self.user)
+
+        self.project.add_addon('app', self.auth)
+        self.appAddon = self.project.get_addon('app')
+
+        # Log user in
+        self.app.authenticate(*self.user.auth)
