7364a96071cdcfbe4da4670409189da386e0acdb
Chris Seto <chriskseto@gmail.com>
2014-10-22 16:02:39 -0400

    Final flake8 fixes

--- website/addons/app/settings/__init__.py
+++ website/addons/app/settings/__init__.py
@@ -1,1 +1,7 @@
-from .defaults import *
+import logging
+from .defaults import *  # noqa
+
+try:
+    from .local import *  # noqa
+except ImportError as error:
+    logging.warn('No local.py settings file found')
