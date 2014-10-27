1fad45edf799343498b9588ba768fd3d22169e61
Brian J. Geiger <bgeiger@pobox.com>
2014-10-27 16:55:35 -0400

    Flake8

--- website/search/util.py
+++ website/search/util.py
@@ -5,12 +5,13 @@ def build_query(q='*', start='0', size='10'):
         'size': size,
     }
 
+
 def build_query_string(q):
     return {
         'query_string': {
             'default_field': '_all',
             'query': q,
             'analyze_wildcard': True,
-            'lenient': True # TODO, may not want to do this
-            }
+            'lenient': True  # TODO, may not want to do this
         }
+    }
