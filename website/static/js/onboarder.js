c70fc5cf339c97fdfb34bb84bc6878e2da6a3d33
Steven Loria <sloria1@gmail.com>
2014-10-28 19:53:52 -0400

    Prevent unnecessary typeahead instantiations

--- website/static/js/onboarder.js
+++ website/static/js/onboarder.js
@@ -86,6 +86,8 @@
         }
         // Attach $typeahead element to viewModel
         viewModel.$typeahead = $inputElem;
+        // Set a flag on the viewModel to prevent multiple typeahead instantiations
+        viewModel._taInitialized = true;
         return $inputElem;
     }
 
@@ -127,8 +129,7 @@
             var params = valueAccessor() || {};
             // Either an Array of nodes or a URL
             var nodesOrURL = ko.unwrap(params.data);
-
-            if (Array.isArray(nodesOrURL)) {
+            if (!viewModel._taInitialized && Array.isArray(nodesOrURL)) {
                 var nodes = params.data;
                 // Compute relevant URLs for each search result
                 initTypeahead(element, nodes, viewModel, params);
