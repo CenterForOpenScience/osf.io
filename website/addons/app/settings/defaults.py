b6660e2835b96d17633dee0b031542698081bab1
Chris Seto <chriskseto@gmail.com>
2014-11-04 13:53:13 -0500

    Adds the default type map

--- website/addons/app/settings/defaults.py
+++ website/addons/app/settings/defaults.py
@@ -1,1 +1,17 @@
+from datetime import date
+from datetime import datetime
+
+
 SYSTEM_USERS_UNCRACKABLE_PASSWORD = '12'
+
+TYPE_MAP = {
+    'string': str,
+    'int': int,
+    'num': int,
+    'number': int,
+    'date': date,
+    'datetime': datetime,
+    'bool': bool,
+    'dict': dict,
+    'object': dict
+}
