c9cb2b901812dedac3ac09b54429958a3d92da3d
kushG <imkushagra@gmail.com>
2014-11-05 11:15:07 -0500

    removing Dropbox key & secret fro local-dist.py & default.py files

--- website/addons/dropbox/settings/local-dist.py
+++ website/addons/dropbox/settings/local-dist.py
@@ -3,5 +3,5 @@
 these settings.
 """
 # Get an app key and secret at https://www.dropbox.com/developers/apps
-DROPBOX_KEY = '2x64l7wag1218md'
-DROPBOX_SECRET = '2x64l7wag1218md'
+DROPBOX_KEY = 'changeme'
+DROPBOX_SECRET = 'changeme'
