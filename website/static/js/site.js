97fcaadca2866cdfa76ff5c9fd23fe5ca61d392a
Chris Seto <chriskseto@gmail.com>
2014-11-05 13:36:05 -0500

    Decode url parameters when getting them

--- website/static/js/site.js
+++ website/static/js/site.js
@@ -153,7 +153,7 @@
      */
     $.osf.urlParams = function(str) {
         return (str || document.location.search).replace(/(^\?)/,'').split('&')
-            .map(function(n){return n = n.split('='),this[n[0]] = n[1],this;}.bind({}))[0];
+            .map(function(n){return n = n.split('='),this[n[0]] = decodeURIComponent(n[1]).replace(/\+/g, ' '),this;}.bind({}))[0];
     };
 
     ///////////
