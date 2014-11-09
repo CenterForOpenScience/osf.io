d4edf18d5471fb5009ecb3122b595fc3d9995adb
Steven Loria <sloria1@gmail.com>
2014-11-09 10:37:18 -0500

    Reindent webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,49 @@
 var webpack = require('webpack');
 
 module.exports = {
-  // This is the main file that should include all other JS files
-  entry: {
-    profile: './website/static/js/app/profile.js',
-    project: './website/static/js/app/project.js',
-    dashboard: './website/static/js/app/dashboard.js'
-  },
-  debug: true,
-  output: {
-    path: './website/static/public/js/',
-    // publicPath: '/static/', // used to generate urls to e.g. images
-    // If you want to generate a filename with a hash of the content (for cache-busting)
-    // filename: "main-[hash].js",
-    filename: '[name].js'
-  },
-  resolve: {
-    // Tell webpack to look for required files in bower and node
-    modulesDirectories: ['./website/static/vendor/bower_components', 'node_modules'],
-  },
-  module: {
-    loaders: [
-      { test: /\.css/, loader: 'style-loader!css-loader' },
-      { test: /\.gif/, loader: 'url-loader?limit=10000&minetype=image/gif' },
-      { test: /\.jpg/, loader: 'url-loader?limit=10000&minetype=image/jpg' },
-      { test: /\.png/, loader: 'url-loader?limit=10000&minetype=image/png' }
-    ]
-  },
-  plugins: [
-    // Bundle common code between modules
-    new webpack.optimize.CommonsChunkPlugin('common.js'),
-    // Bower support
-    new webpack.ResolverPlugin(
-        new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin("bower.json", ["main"])
-    ),
-    new webpack.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery'
-    })
-  ],
-  externals: {
-      // require("jquery") is external and available
-      //  on the global var jQuery
-      'jquery': 'jQuery',
-      'jquery-ui': 'jQuery.ui'
-  }
+    entry: {
+        profile: './website/static/js/app/profile.js',
+        project: './website/static/js/app/project.js',
+        dashboard: './website/static/js/app/dashboard.js'
+    },
+    debug: true,
+    output: {
+        path: './website/static/public/js/',
+        // publicPath: '/static/', // used to generate urls to e.g. images
+        filename: '[name].js'
+    },
+    resolve: {
+        // Look for required files in bower and node
+        modulesDirectories: ['./website/static/vendor/bower_components', 'node_modules'],
+        alias: {
+            'knockout-punches': '../vendor/knockout-punches/knockout.punches.min.js',
+            'knockout-sortable': '../vendor/knockout-sortable/knockout-sortable.js'
+        }
+    },
+    module: {
+        loaders: [
+            { test: /\.css/, loader: 'style-loader!css-loader' },
+            { test: /\.gif/, loader: 'url-loader?limit=10000&minetype=image/gif' },
+            { test: /\.jpg/, loader: 'url-loader?limit=10000&minetype=image/jpg' },
+            { test: /\.png/, loader: 'url-loader?limit=10000&minetype=image/png' }
+        ]
+    },
+    plugins: [
+        // Bundle common code between modules
+        new webpack.optimize.CommonsChunkPlugin('common.js'),
+        // Bower support
+        new webpack.ResolverPlugin(
+            new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin('bower.json', ['main'])
+        ),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        })
+    ],
+    externals: {
+        // require("jquery") is external and available
+        //  on the global var jQuery
+        'jquery': 'jQuery',
+        'jquery-ui': 'jQuery.ui'
+    }
 };
