1862326feb1938ec76ab4a01a509fff9a8ef9f9f
Brian J. Geiger <bgeiger@pobox.com>
2014-11-13 16:19:31 -0500

    Replace Dropbox's revisions.js bootboxes with GrowlBoxes

--- website/addons/dropbox/static/revisions.js
+++ website/addons/dropbox/static/revisions.js
@@ -74,7 +74,7 @@
                             });
                             request.fail(function( jqXHR, textStatus ) {
                                 $('#deletingAlert').removeClass('in');
-                                bootbox.alert( 'Could not delete: ' + textStatus );
+                                new GrowlBox( 'Could not delete', textStatus );
                             });
                         }
                     }
