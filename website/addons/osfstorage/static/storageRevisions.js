e964047d318d56b2d53762646204565aba81ccf6
Brian J. Geiger <bgeiger@pobox.com>
2014-11-13 16:22:31 -0500

    Replace Osfstorage's storageRevisions.js bootboxes with GrowlBoxes

--- website/addons/osfstorage/static/storageRevisions.js
+++ website/addons/osfstorage/static/storageRevisions.js
@@ -59,7 +59,7 @@
         }).done(function() {
             window.location = self.urls.files;
         }).fail(function() {
-            bootbox.alert('Could not delete file.');
+            new GrowlBox('Error', 'Could not delete file.');
         });
     };
 
