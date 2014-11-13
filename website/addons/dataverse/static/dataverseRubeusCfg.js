c00b1a89f4d0f80f489d955673249094818575ad
Brian J. Geiger <bgeiger@pobox.com>
2014-11-13 16:11:42 -0500

    Replace dataverseRubeusCfg's bootboxes with GrowlBoxes

--- website/addons/dataverse/static/dataverseRubeusCfg.js
+++ website/addons/dataverse/static/dataverseRubeusCfg.js
@@ -44,16 +44,16 @@
                             url,
                             {}
                         ).done(function() {
-                            bootbox.alert('Your study has been released. Please ' +
+                            new GrowlBox('Your study has been released.', 'Please ' +
                             'allow up to 24 hours for the released version to ' +
                             'appear on your OSF project\'s file page.');
                             self.updateItem(row);
                         }).fail( function(args) {
                             var message = args.responseJSON.code === 400 ?
-                                'Error: Something went wrong when attempting to ' +
+                                'Something went wrong when attempting to ' +
                                 'release your study.' :
-                                'Error: This version has already been released.'
-                            bootbox.alert(message);
+                                'This version has already been released.';
+                            bootbox.alert('Error', message);
                             self.updateItem(row);
                         });
                     }
