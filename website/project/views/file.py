59e94158f527c32da34128cdcebb664929430c2d
Saman Ehsan <se8ea@virginia.edu>
2014-11-19 16:09:11 -0500

    Show privacy alerts on 'files' and 'sharing' project pages
    Pass 'primary=True' to _view_project where it
    is first called when rendering the files and
    sharing pages.
    
    Closes CenterForOpenScience/openscienceframework.org#1263

--- website/project/views/file.py
+++ website/project/views/file.py
@@ -23,7 +23,7 @@ def collect_file_trees(**kwargs):
     node = kwargs['node'] or kwargs['project']
     auth = kwargs['auth']
 
-    serialized = _view_project(node, auth)
+    serialized = _view_project(node, auth, primary=True)
     # Add addon static assets
     serialized.update(rubeus.collect_addon_assets(node))
     return serialized
