ba9749fa0da6d33536a7a678199a139abc327947
rliebz <rliebz@gmail.com>
2014-11-24 17:09:23 -0500

    Add user interface for doc lock/unlock.

--- website/addons/wiki/static/ShareJSDoc.js
+++ website/addons/wiki/static/ShareJSDoc.js
@@ -11,48 +11,12 @@ var ShareJSDoc = function(viewModel, url, metadata) {
     editor.setShowPrintMargin(false);           // Hides print margin
     editor.setReadOnly(true); // Read only until initialized
 
+    // Configure connection
     var socket = new ReconnectingWebSocket('ws://localhost:7007');
     var sjs = new sharejs.Connection(socket);
     var doc = sjs.get('docs', metadata.docId);
 
-    sjs.on('disconnected', function() {
-        // TODO: Inform user of disconnect
-        console.log('disconnected');
-    });
-
-    // Handle our custom messages separately
-    var onmessage = socket.onmessage;
-    socket.onmessage = function (message) {
-        var data = JSON.parse(message.data);
-        // Meta type is not built into sharejs; we pass it manually
-        if (data.type === 'meta') {
-            // Convert users object into knockout array
-            activeUsers = [];
-            for (var user in data.users) {
-                var userMeta = data.users[user];
-                userMeta.id = user;
-                activeUsers.push(userMeta);
-            }
-            viewModel.activeUsers(activeUsers);
-        } else if (data.type === 'lock') {
-            editor.setReadOnly(true);
-        } else if (data.type === 'unlock') {
-                editor.setReadOnly(false);
-        } else {
-            onmessage(message);
-        }
-    };
-
-    // This will be called on both connect and reconnect
-    doc.on('subscribe', function () {
-
-        // Send user metadata
-        socket.send(JSON.stringify(metadata));
-
-    });
-
-    // This will be called when we have a live copy of the server's data.
-    doc.whenReady(function () {
+    function whenReady() {
 
         // Create a text document if one does not exist
         if (!doc.type) {
@@ -77,7 +41,51 @@ var ShareJSDoc = function(viewModel, url, metadata) {
             editor.setReadOnly(false);
         }
 
+    }
+
+    // Send user metadata
+    function register() {
+        socket.send(JSON.stringify(metadata));
+    }
+
+    sjs.on('disconnected', function() {
+        // TODO: Inform user of disconnect
+        console.log('disconnected');
     });
+
+    // Handle our custom messages separately
+    var onmessage = socket.onmessage;
+    socket.onmessage = function (message) {
+        var data = JSON.parse(message.data);
+        // Meta type is not built into sharejs; we pass it manually
+        if (data.type === 'meta') {
+            // Convert users object into knockout array
+            activeUsers = [];
+            for (var user in data.users) {
+                var userMeta = data.users[user];
+                userMeta.id = user;
+                activeUsers.push(userMeta);
+            }
+            viewModel.activeUsers(activeUsers);
+        } else if (data.type === 'lock') {
+            editor.setReadOnly(true);
+            $('#refresh-modal').modal({
+                backdrop: 'static',
+                keyboard: false
+            });
+        } else if (data.type === 'unlock') {
+            // TODO: Wait a certain number of seconds so they can read it?
+            location.reload();
+        } else {
+            onmessage(message);
+        }
+    };
+
+    // This will be called on both connect and reconnect
+    doc.on('subscribe', register);
+
+    // This will be called when we have a live copy of the server's data.
+    doc.whenReady(whenReady);
 
     // Subscribe to changes
     doc.subscribe();
--- website/addons/wiki/templates/edit.mako
+++ website/addons/wiki/templates/edit.mako
@@ -62,6 +62,19 @@
     </div><!-- end row -->
 </div><!-- end wiki -->
 
+<div class="modal fade" id="refresh-modal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title">The permissions for this page have changed</h4>
+      </div>
+      <div class="modal-body">
+        <p>Your browser should refresh shortly&hellip;</p>
+      </div>
+    </div>
+  </div>
+</div>
+
 <script src="/static/vendor/bower_components/ace-builds/src-noconflict/ace.js"></script>
 <script src="/static/vendor/pagedown-ace/Markdown.Converter.js"></script>
 <script src="/static/vendor/pagedown-ace/Markdown.Sanitizer.js"></script>
