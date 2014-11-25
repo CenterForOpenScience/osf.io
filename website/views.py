87b9b8080879bfc9bae88144be13e82920a90775
Brian J. Geiger <bgeiger@pobox.com>
2014-11-25 16:58:55 -0500

    Remove unneeded check for Auth

--- website/views.py
+++ website/views.py
@@ -88,7 +88,7 @@ def _render_nodes(nodes, user=None):
 
 
 @collect_auth
-def index(auth, **kwargs):
+def index(auth):
     """Redirect to dashboard if user is logged in, else show homepage.
 
     """
@@ -112,8 +112,6 @@ def find_dashboard(user):
 
 @must_be_logged_in
 def get_dashboard(auth, nid=None, **kwargs):
-    if auth is None:
-        return None
     user = auth.user
 
     if nid is None:
@@ -130,9 +128,10 @@ def get_dashboard(auth, nid=None, **kwargs):
         return_value = {'data': dashboard_projects}
     return return_value
 
+
 @must_be_logged_in
 def get_all_projects_smart_folder(auth, **kwargs):
-    #TODO: Unit tests
+    # TODO: Unit tests
     user = auth.user
 
     contributed = user.node__contributed
@@ -169,9 +168,10 @@ def get_all_projects_smart_folder(auth, **kwargs):
     return_value.extend([rubeus.to_project_root(node, auth, **kwargs) for node in nodes])
     return return_value
 
+
 @must_be_logged_in
 def get_all_registrations_smart_folder(auth, **kwargs):
-    #TODO: Unit tests
+    # TODO: Unit tests
     user = auth.user
     contributed = user.node__contributed
 
@@ -207,8 +207,9 @@ def get_all_registrations_smart_folder(auth, **kwargs):
     return_value.extend([rubeus.to_project_root(node, auth, **kwargs) for node in nodes])
     return return_value
 
+
 @must_be_logged_in
-def get_dashboard_nodes(auth, **kwargs):
+def get_dashboard_nodes(auth):
     """Get summary information about the current user's dashboard nodes.
 
     :param-query no_components: Exclude components from response.
@@ -326,7 +327,7 @@ def reset_password_form():
     return form_utils.jsonify(ResetPasswordForm())
 
 
-### GUID ###
+# GUID ###
 
 def _build_guid_url(url, prefix=None, suffix=None):
     if not url.startswith('/'):
