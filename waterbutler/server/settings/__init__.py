4c9cab70799afe42d5f6ca2f9d2b98ffb2bd4add
Chris Seto <chriskseto@gmail.com>
2014-12-09 17:05:19 -0500

    Warning message for lacking a local.py

--- waterbutler/server/settings/__init__.py
+++ waterbutler/server/settings/__init__.py
@@ -1,8 +1,14 @@
 # encoding: utf-8
+import logging
 
 from .defaults import *
+
+
+logger = logging.getLogger()
+
 
 try:
     from .local import *
 except ImportError:
-    pass
+    logger.warning('No local.py found.')
+    logger.warning('Using defaults.')
