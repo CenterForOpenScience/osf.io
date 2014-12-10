8323b2d6e6b81e244eba1ebf1775535d5e29f235
jmcarp <jm.carp@gmail.com>
2014-12-10 15:16:09 -0500

    Add travis support.

--- tasks.py
+++ tasks.py
@@ -6,12 +6,25 @@ from waterbutler import settings
 
 
 @task
-def tornado(port=settings.PORT, address=settings.ADDRESS, debug=settings.DEBUG):
-    from waterbutler.server import serve
-    serve(port, address, debug)
+def install(upgrade=False):
+    cmd = 'pip install -r dev-requirements.txt'
+    if upgrade:
+        cmd += ' --upgrade'
+    run(cmd)
+
+
+@task
+def flake():
+    run('flake8 .')
 
 
 @task
 def test():
     cmd = 'py.test --cov-report term-missing --cov waterbutler tests'
     run(cmd, pty=True)
+
+
+@task
+def tornado(port=settings.PORT, address=settings.ADDRESS, debug=settings.DEBUG):
+    from waterbutler.server import serve
+    serve(port, address, debug)
