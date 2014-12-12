5099e1f79e49836cb5abdd77b969c407531ca5af
Saman Ehsan <se8ea@virginia.edu>
2014-12-12 17:56:58 -0500

    Make MAILCHIMP_WEBHOOK_SECRET_KEY default to string 'CHANGEME'
    Fix mailchimp webhook tests that were failing because
    the fake key was None instead of a string.

--- website/settings/defaults.py
+++ website/settings/defaults.py
@@ -64,7 +64,7 @@ MAIL_PASSWORD = ''  # Set this in local.py
 
 # Mailchimp
 MAILCHIMP_API_KEY = None
-MAILCHIMP_WEBHOOK_SECRET_KEY = None  # OSF secret key to ensure webhook is secure
+MAILCHIMP_WEBHOOK_SECRET_KEY = 'CHANGEME'  # OSF secret key to ensure webhook is secure
 ENABLE_EMAIL_SUBSCRIPTIONS = True
 
 # TODO: Override in local.py
