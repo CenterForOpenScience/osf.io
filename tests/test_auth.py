80dd989568368ad8a86e70604338659bea287101
Saman Ehsan <se8ea@virginia.edu>
2014-12-12 10:15:03 -0500

    Move mailchimp helper tests into separate file

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -22,7 +22,6 @@ from tests.factories import (
 from framework.auth import User, Auth
 from framework.auth.decorators import must_be_logged_in
 
-from website import mailchimp_helpers
 from website.util import web_url_for
 from website.project.decorators import (
     must_have_permission, must_be_contributor,
@@ -93,48 +92,6 @@ class TestAuthUtils(OsfTestCase):
         user.save()
         with assert_raises(auth.PasswordIncorrectError):
             auth.login(user.username, 'wrongpassword')
-
-    @mock.patch('website.mailchimp_helpers.get_mailchimp_api')
-    def test_get_list_id_from_name(self, mock_get_mailchimp_api):
-        list_name = 'foo'
-        mock_client = mock.MagicMock()
-        mock_get_mailchimp_api.return_value = mock_client
-        mock_client.lists.list.return_value = {'data': [{'id': 1, 'list_name': list_name}]}
-        list_id = mailchimp_helpers.get_list_id_from_name(list_name)
-        mock_client.lists.list.assert_called_with(filters={'list_name': list_name})
-        assert_equal(list_id, 1)
-
-    @mock.patch('website.mailchimp_helpers.get_mailchimp_api')
-    def test_get_list_name_from_id(self, mock_get_mailchimp_api):
-        list_id = '12345'
-        mock_client = mock.MagicMock()
-        mock_get_mailchimp_api.return_value = mock_client
-        mock_client.lists.list.return_value = {'data': [{'id': list_id, 'name': 'foo'}]}
-        list_name = mailchimp_helpers.get_list_name_from_id(list_id)
-        mock_client.lists.list.assert_called_with(filters={'list_id': list_id})
-        assert_equal(list_name, 'foo')
-
-    @mock.patch('website.mailchimp_helpers.get_mailchimp_api')
-    def test_subscribe_called_with_correct_arguments(self, mock_get_mailchimp_api):
-        list_name = 'foo'
-        username = 'foo@example.com'
-        mock_client = mock.MagicMock()
-        mock_get_mailchimp_api.return_value = mock_client
-        mock_client.lists.list.return_value = {'data': [{'id': 1, 'list_name': list_name}]}
-        list_id = mailchimp_helpers.get_list_id_from_name(list_name)
-        mailchimp_helpers.subscribe(list_id, username)
-        mock_client.lists.subscribe.assert_called_with(id=list_id, email={'email': username}, double_optin=False, update_existing=True)
-
-    @mock.patch('website.mailchimp_helpers.get_mailchimp_api')
-    def test_unsubscribe_called_with_correct_arguments(self, mock_get_mailchimp_api):
-        list_name = 'foo'
-        username = 'foo@example.com'
-        mock_client = mock.MagicMock()
-        mock_get_mailchimp_api.return_value = mock_client
-        mock_client.lists.list.return_value = {'data': [{'id': 1, 'list_name': list_name}]}
-        list_id = mailchimp_helpers.get_list_id_from_name(list_name)
-        mailchimp_helpers.unsubscribe(list_id, username)
-        mock_client.lists.unsubscribe.assert_called_with(id=list_id, email={'email': username})
 
 
 class TestAuthObject(OsfTestCase):
