9dadeb848d49a08a00c0e73f198ff2c3f44e2e79
icereval <icereval@gmail.com>
2014-12-15 14:23:09 -0500

    BaseMetadata and S3MetadataFile/Folder

--- waterbutler/providers/core.py
+++ waterbutler/providers/core.py
@@ -162,3 +162,53 @@ class BaseProvider(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def metadata(self, **kwargs):
         pass
+
+
+class BaseMetadata(metaclass=abc.ABCMeta):
+
+    def __init__(self, raw):
+        self.raw = raw
+
+    def serialized(self):
+        return {
+            'content': self.content,
+            'provider': self.provider,
+            'kind': self.kind,
+            'name': self.name,
+            'size': self.size,
+            'path': self.path,
+            'modified': self.modified,
+            'extra': self.extra,
+        }
+
+    @abc.abstractproperty
+    def content(self):
+        pass
+
+    @abc.abstractproperty
+    def provider(self):
+        pass
+
+    @abc.abstractproperty
+    def kind(self):
+        pass
+
+    @abc.abstractproperty
+    def name(self):
+        pass
+
+    @abc.abstractproperty
+    def path(self):
+        pass
+
+    @abc.abstractproperty
+    def modified(self):
+        pass
+
+    @abc.abstractproperty
+    def size(self):
+        pass
+
+    @property
+    def extra(self):
+        return {}
