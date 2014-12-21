b9fc5ae5395aabc0c8cbe6bed066bcefcac7956e
icereval <icereval@gmail.com>
2014-12-21 17:07:01 -0500

    aiohttpretty

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,18 +4,18 @@ import aiohttpretty
 def pytest_configure(config):
     config.addinivalue_line(
         'markers',
-        'aiopretty: mark tests to activate aiopretty'
+        'aiohttpretty: mark tests to activate aiohttpretty'
     )
 
 
 def pytest_runtest_setup(item):
-    marker = item.get_marker('aiopretty')
+    marker = item.get_marker('aiohttpretty')
     if marker is not None:
         aiohttpretty.clear()
         aiohttpretty.activate()
 
 
 def pytest_runtest_teardown(item, nextitem):
-    marker = item.get_marker('aiopretty')
+    marker = item.get_marker('aiohttpretty')
     if marker is not None:
         aiohttpretty.deactivate()
