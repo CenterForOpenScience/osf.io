74ba2da1c6f7d9dd721d2c05895a16771f00ffbe
icereval <icereval@gmail.com>
2014-12-22 10:29:08 -0500

    settings consolidation

---
+++ waterbutler/settings.py
@@ -0,0 +1,21 @@
+import os
+import json
+
+PROJECT_NAME = 'waterbutler'
+PROJECT_CONFIG_PATH = '~/.cos'
+
+try:
+    config_path = os.environ['{}_CONFIG'.format(PROJECT_NAME.upper())]
+except KeyError:
+    env = os.environ.get('ENV', 'test')
+    config_path = '{}/{}-{}.json'.format(PROJECT_CONFIG_PATH, PROJECT_NAME, env)
+
+config_path = os.path.expanduser(config_path)
+if not os.path.exists(config_path):
+    raise Exception('Configuration file \'{}\' could not be found.'.format(config_path))
+with open(os.path.expanduser(config_path)) as fp:
+    config = json.load(fp)
+
+
+OSFSTORAGE_PROVIDER_CONFIG = config.get('OSFSTORAGE_PROVIDER_CONFIG')
+SERVER_CONFIG = config.get('SERVER_CONFIG')
