80ae34b9737d1a8938dcc525e827abf1be5e78e6
erinspace <erin.braswell@gmail.com>
2014-12-23 14:38:35 -0500

    remove unecessary method for extended rss

--- website/addons/app/views/crud/__init__.py
+++ website/addons/app/views/crud/__init__.py
@@ -149,31 +149,6 @@ def query_app_rss(node_addon, **kwargs):
 # GET
 @must_be_contributor_or_public
 @must_have_addon('app', 'node')
-def query_app_extended_rss(node_addon, **kwargs):
-    q = request.args.get('q', '*')
-    size = request.args.get('size')
-    start = request.args.get('from')
-    query = args_to_query(q, size, start)
-
-    try:
-        ret = search(query, doc_type=node_addon.namespace, index='metadata')
-    except MalformedQueryError:
-        raise HTTPError(http.BAD_REQUEST)
-    except IndexNotFoundError:
-        ret = {
-            'count': 0,
-            'results': []
-        }
-
-    node = node_addon.owner
-    name = node_addon.system_user.username
-
-    rss_url = node.api_url_for('query_app_rss', _xml=True, _absolute=True)
-
-
-# GET
-@must_be_contributor_or_public
-@must_have_addon('app', 'node')
 def query_app_atom(node_addon, **kwargs):
     q = request.args.get('q', '*')
     size = request.args.get('size')
