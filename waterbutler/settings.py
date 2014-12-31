154dc97fccaa2ba125d000ae1259f8678e3ce4c2
icereval <icereval@gmail.com>
2014-12-30 21:34:42 -0500

    optional config

--- waterbutler/settings.py
+++ waterbutler/settings.py
@@ -1,8 +1,11 @@
 import os
 import json
+import logging
+
 
 PROJECT_NAME = 'waterbutler'
 PROJECT_CONFIG_PATH = '~/.cos'
+
 
 try:
     config_path = os.environ['{}_CONFIG'.format(PROJECT_NAME.upper())]
@@ -10,11 +13,14 @@ except KeyError:
     env = os.environ.get('ENV', 'test')
     config_path = '{}/{}-{}.json'.format(PROJECT_CONFIG_PATH, PROJECT_NAME, env)
 
+
+config = {}
 config_path = os.path.expanduser(config_path)
 if not os.path.exists(config_path):
-    raise Exception('Configuration file \'{}\' could not be found.'.format(config_path))
-with open(os.path.expanduser(config_path)) as fp:
-    config = json.load(fp)
+    logging.warn('No \'{}\' configuration file found'.format(config_path))
+else:
+    with open(os.path.expanduser(config_path)) as fp:
+        config = json.load(fp)
 
 
 OSFSTORAGE_PROVIDER_CONFIG = config.get('OSFSTORAGE_PROVIDER_CONFIG')
