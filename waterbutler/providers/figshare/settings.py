f0e009c0c8932eec14d66a266c06f3d93859d355
icereval <icereval@gmail.com>
2015-01-01 19:01:35 -0700

    figshare settings base file

--- waterbutler/providers/figshare/settings.py
+++ waterbutler/providers/figshare/settings.py
@@ -1,9 +1,6 @@
 try:
-    from waterbutler.settings import S3_PROVIDER_CONFIG
+    from waterbutler.settings import FIGSHARE_PROVIDER_CONFIG
 except ImportError:
     S3_PROVIDER_CONFIG = {}
 
-config = S3_PROVIDER_CONFIG
-
-
-TEMP_URL_SECS = config.get('TEMP_URL_SECS', 100)
+config = FIGSHARE_PROVIDER_CONFIG or {}
