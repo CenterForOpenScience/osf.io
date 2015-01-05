2f9f9babbad8080f11449ebfb0b815df1b5a00c7
Chris Seto <chriskseto@gmail.com>
2015-01-05 17:31:30 -0500

    Refactor dropbox url building

--- website/static/js/waterbutler.js
+++ website/static/js/waterbutler.js
@@ -12,34 +12,50 @@ function getViewOnly() {
   return $osf.urlParams().view_only;
 }
 
-function buildUrl(metadata, path, provider, file) {
-    path = path || '/';
-
+function getDefaultOptions(path, provider) {
     var nid = window.nodeId || contextVars.node.id;
-    var baseUrl = settings.WATERBUTLER_URL + (metadata ? 'data?': 'file?');
 
-    if (file) {
-        path += file.name;
-    }
-
-
-    return baseUrl + $.param({
+    return {
         nid: nid,
         token: '',
         path: path,
         provider: provider,
         cookie: getCookie(),
         viewOnly: getViewOnly()
-    });
+    };
 }
 
-function buildFromTreebeard(metadata, item, file) {
-    return buildUrl(metadata, item.data.path, item.data.provider, file);
+function buildUrl(suffix, path, provider, options) {
+    path = path || '/';
+    var baseUrl = settings.WATERBUTLER_URL + suffix;
+
+    return baseUrl + $.param($.extend(getDefaultOptions(path, provider), options));
+}
+
+var buildCrudUrl = buildUrl.bind(this, 'file?');
+var buildMetadataUrl = buildUrl.bind(this, 'data?');
+
+
+function buildUploadUrl(path, provider, file, options) {
+    path = (path || '/') + file.name;
+    return buildUrl('file?', path, provider, options);
+}
+
+function buildFromTreebeard(suffix, item, options) {
+    return buildUrl(suffix, item.data.path, item.data.provider, options);
+}
+
+function buildFromTreebeardFile(item, file, options) {
+    return buildUploadUrl(item.data.path, item.data.provider, file, options);
 }
 
 module.exports = {
-    buildFileUrlFromPath: buildUrl.bind(this, false),
-    buildFileUrl: buildFromTreebeard.bind(this, false),
-    buildMetadataUrlFromPath: buildUrl.bind(this, true),
-    buildMetadataUrl: buildFromTreebeard.bind(this, true),
+    buildDeleteUrl: buildCrudUrl,
+    buildUploadUrl: buildUploadUrl,
+    buildDownloadUrl: buildCrudUrl,
+    buildMetadataUrl: buildMetadataUrl,
+    buildTreeBeardUpload: buildFromTreebeardFile,
+    buildTreeBeardDelete: buildFromTreebeard.bind(this, 'file?'),
+    buildTreeBeardDownload: buildFromTreebeard.bind(this, 'file?'),
+    buildTreeBeardMetadata: buildFromTreebeard.bind(this, 'data?'),
 };
