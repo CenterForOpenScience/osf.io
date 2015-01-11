9ddb02abdc7e7db732797f832f7a52a63ea8b912
icereval <icereval@gmail.com>
2015-01-11 18:35:12 -0500

    adding docker compatibility

--- waterbutler/tasks/settings.py
+++ waterbutler/tasks/settings.py
@@ -1,4 +1,7 @@
+import os
+
 from stevedore import extension
+
 
 try:
     from waterbutler.settings import TASKS_CONFIG
@@ -8,8 +11,14 @@ except ImportError:
 config = TASKS_CONFIG or {}
 
 
-BROKER_URL = config.get('BROKER_URL', 'amqp://')
-CELERY_RESULT_BACKEND = config.get('CELERY_RESULT_BACKEND', 'redis://')
+BROKER_URL = config.get('BROKER_URL', 'amqp://{}:{}'.format(
+    os.environ.get('RABBITMQ_PORT_5672_TCP_ADDR', ''),
+    os.environ.get('RABBITMQ_PORT_5672_TCP_PORT', ''),
+))
+CELERY_RESULT_BACKEND = config.get('CELERY_RESULT_BACKEND', 'redis://{}:{}'.format(
+    os.environ.get('REDIS_PORT_6379_TCP_ADDR', ''),
+    os.environ.get('REDIS_PORT_6379_TCP_PORT', ''),
+))
 CELERY_DISABLE_RATE_LIMITS = config.get('CELERY_DISABLE_RATE_LIMITS', True)
 CELERY_TASK_RESULT_EXPIRES = config.get('CELERY_TASK_RESULT_EXPIRES', 60)
 # CELERY_ALWAYS_EAGER = config.get('CELERY_ALWAYS_EAGER', True)
