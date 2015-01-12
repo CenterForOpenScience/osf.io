12df5c60ef2d2a34aca7239caab89b809bb2ea68
icereval <icereval@gmail.com>
2015-01-11 20:23:28 -0500

    cleanup

--- waterbutler/tasks/settings.py
+++ waterbutler/tasks/settings.py
@@ -10,14 +10,20 @@ except ImportError:
 config = settings.get('TASKS_CONFIG', {})
 
 
-BROKER_URL = config.get('BROKER_URL', 'amqp://{}:{}//'.format(
-    os.environ.get('RABBITMQ_PORT_5672_TCP_ADDR', ''),
-    os.environ.get('RABBITMQ_PORT_5672_TCP_PORT', ''),
-))
-CELERY_RESULT_BACKEND = config.get('CELERY_RESULT_BACKEND', 'redis://{}:{}'.format(
-    os.environ.get('REDIS_PORT_6379_TCP_ADDR', ''),
-    os.environ.get('REDIS_PORT_6379_TCP_PORT', ''),
-))
+BROKER_URL = config.get(
+    'BROKER_URL',
+    'amqp://{}:{}//'.format(
+        os.environ.get('RABBITMQ_PORT_5672_TCP_ADDR', ''),
+        os.environ.get('RABBITMQ_PORT_5672_TCP_PORT', ''),
+    )
+)
+CELERY_RESULT_BACKEND = config.get(
+    'CELERY_RESULT_BACKEND',
+    'redis://{}:{}'.format(
+        os.environ.get('REDIS_PORT_6379_TCP_ADDR', ''),
+        os.environ.get('REDIS_PORT_6379_TCP_PORT', ''),
+    )
+)
 CELERY_DISABLE_RATE_LIMITS = config.get('CELERY_DISABLE_RATE_LIMITS', True)
 CELERY_TASK_RESULT_EXPIRES = config.get('CELERY_TASK_RESULT_EXPIRES', 60)
 # CELERY_ALWAYS_EAGER = config.get('CELERY_ALWAYS_EAGER', True)
