2806b58bd87728420f583ae1025b9dd181cd8bd5
Chris Seto <chriskseto@gmail.com>
2015-01-16 15:52:27 -0500

    Update to commonjs

--- website/addons/app/static/appPage.js
+++ website/addons/app/static/appPage.js
@@ -1,77 +1,74 @@
-;(function (global, factory) {
-    if (typeof define === 'function' && define.amd) {
-        define(['knockout', 'jquery', 'osfutils', 'knockoutpunches'], factory);
-    } else {
-        global.ApplicationView  = factory(ko, jQuery);
-    }
-}(this, function(ko, $) {
-    // Enable knockout punches
-    ko.punches.enableAll();
+var $ = require('jQuery');
+require('knockout-punches');
+var ko = require('knockout');
+var $osf = require('osfHelpers');
 
-    var ViewModel = function(url) {
-        var self = this;
 
-        self.url = url;
-        self.total = ko.observable(0);
-        self.chosen = ko.observable();
-        self.query = ko.observable('');
-        self.metadata = ko.observable('');
-        self.currentIndex = ko.observable(0);
-        self.results = ko.observableArray([]);
+// Enable knockout punches
+ko.punches.enableAll();
 
-        self.search = function() {
-            $.ajax({
-                url: self.url + '?q=' + self.query() + '&page=' + self.currentIndex(),
-                type: 'GET',
-                success: self.searchRecieved
-            });
-        };
+var ViewModel = function(url) {
+    var self = this;
 
-        self.pageNext = function() {
-            if (self.currentIndex() >= self.total()) return;
+    self.url = url;
+    self.total = ko.observable(0);
+    self.chosen = ko.observable();
+    self.query = ko.observable('');
+    self.metadata = ko.observable('');
+    self.currentIndex = ko.observable(0);
+    self.results = ko.observableArray([]);
 
-            self.currentIndex(self.currentIndex() + 1);
-            self.search();
-        }
-
-        self.pagePrev = function() {
-            if (self.currentIndex() == 0) return;
-
-            self.currentIndex(self.currentIndex() - 1);
-            self.search();
-        }
-
-        self.getMetadata = function(id) {
-            $.ajax({
-                url: self.url + 'metadata/' + id + '/',
-                type: 'GET',
-                success: self.metadataRecieved
-            });
-        }
-
-        self.searchRecieved = function(data) {
-            self.total(Math.ceil(data.total / 10));
-            self.results(data.results);
-        };
-
-        self.metadataRecieved = function(data) {
-            self.metadata('\n' + jsl.format.formatJson(JSON.stringify(data)));
-        };
-
-        self.setSelected = function(datum) {
-            self.chosen(datum);
-            self.getMetadata(datum._id)
-        };
-
+    self.search = function() {
+        $.ajax({
+            url: self.url + '?q=' + self.query() + '&page=' + self.currentIndex(),
+            type: 'GET',
+            success: self.searchRecieved
+        });
     };
 
-    function ApplicationView(selector, url) {
-        // Initialization code
-        var self = this;
-        self.viewModel = new ViewModel(url);
-        $.osf.applyBindings(self.viewModel, selector);
+    self.pageNext = function() {
+        if (self.currentIndex() >= self.total()) return;
+
+        self.currentIndex(self.currentIndex() + 1);
+        self.search();
     }
 
-    return ApplicationView
+    self.pagePrev = function() {
+        if (self.currentIndex() === 0) return;
 
-}));
+        self.currentIndex(self.currentIndex() - 1);
+        self.search();
+    }
+
+    self.getMetadata = function(id) {
+        $.ajax({
+            url: self.url + 'metadata/' + id + '/',
+            type: 'GET',
+            success: self.metadataRecieved
+        });
+    }
+
+    self.searchRecieved = function(data) {
+        self.total(Math.ceil(data.total / 10));
+        self.results(data.results);
+    };
+
+    self.metadataRecieved = function(data) {
+        self.metadata('\n' + jsl.format.formatJson(JSON.stringify(data)));
+    };
+
+    self.setSelected = function(datum) {
+        self.chosen(datum);
+        self.getMetadata(datum._id)
+    };
+
+};
+
+function ApplicationView(selector, url) {
+    // Initialization code
+    var self = this;
+    self.viewModel = new ViewModel(url);
+    $.osf.applyBindings(self.viewModel, selector);
+}
+
+module.exports = ApplicationView;
