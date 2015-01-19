face35ca50248e90eacd094423096a33aa087449
Chris Seto <chriskseto@gmail.com>
2015-01-19 12:46:10 -0500

    Fix api key management

--- website/static/js/apiKeyManager.js
+++ website/static/js/apiKeyManager.js
@@ -1,81 +1,78 @@
-;(function (global, factory) {
-    if (typeof define === 'function' && define.amd) {
-        define(['knockout', 'jquery', 'osfutils', 'knockoutpunches'], factory);
-    } else {
-        global.ApiKeyView  = factory(ko, jQuery);
-    }
-}(this, function(ko, $) {
-    // Enable knockout punches
-    ko.punches.enableAll();
+var $ = require('jQuery');
+require('knockout-punches');
+var ko = require('knockout');
+var $osf = require('osfHelpers');
 
-    var ViewModel = function(url) {
-        var self = this;
-        self.url = url;
-        self.keys = ko.observableArray([]);
-        self.label = ko.observable('');
 
-        self.keysRecieved = function(data) {
-            self.keys(data.keys);
-        };
+// Enable knockout punches
+ko.punches.enableAll();
 
-        self.createKey = function() {
-            $.ajax({
-                url: self.url,
-                method: 'POST',
-                data: JSON.stringify({label: self.label()}),
-                contentType: 'application/json',
-                success: self.keyCreated,
-                error: self.ajaxError.bind(this, 'create an API key')
-            });
-        };
+var ViewModel = function(url) {
+    var self = this;
+    self.url = url;
+    self.keys = ko.observableArray([]);
+    self.label = ko.observable('');
 
-        self.keyCreated = function(data) {
-            self.keys.push({label: self.label(), key: data.key});
-            self.label('');
-        };
-
-        self.deleteKey = function(key) {
-            bootbox.confirm('Are you sure you want to delete this API key?', function(result) {
-                if (result) {
-                    $.ajax({
-                        url: self.url + '?key=' + key.key,
-                        type: 'DELETE',
-                        success: function() {
-                            self.keys.splice(self.keys.indexOf(key), 1);
-                        },
-                        error: self.ajaxError.bind(this, 'delete this API key')
-                    });
-                }
-            });
-        };
-
-        self.ajaxError = function(item) {
-            bootbox.alert('Could not ' + item + '. Please refresh the page or ' +
-                'contact <a href="mailto: support@cos.io">support@cos.io</a> if the ' +
-                'problem persists.');
-        };
-
-        function fetch() {
-            $.ajax({
-                url: self.url,
-                type: 'GET',
-                success: self.keysRecieved,
-                error: self.ajaxError.bind(this, 'fetch your API keys')
-            });
-        }
-
-        fetch();
-
+    self.keysRecieved = function(data) {
+        self.keys(data.keys);
     };
 
-    function ApiKeyView(selector, url) {
-        // Initialization code
-        var self = this;
-        self.viewModel = new ViewModel(url);
-        window.model = self.viewModel;
-        $.osf.applyBindings(self.viewModel, selector);
+    self.createKey = function() {
+        $.ajax({
+            url: self.url,
+            method: 'POST',
+            data: JSON.stringify({label: self.label()}),
+            contentType: 'application/json',
+            success: self.keyCreated,
+            error: self.ajaxError.bind(this, 'create an API key')
+        });
+    };
+
+    self.keyCreated = function(data) {
+        self.keys.push({label: self.label(), key: data.key});
+        self.label('');
+    };
+
+    self.deleteKey = function(key) {
+        bootbox.confirm('Are you sure you want to delete this API key?', function(result) {
+            if (result) {
+                $.ajax({
+                    url: self.url + '?key=' + key.key,
+                    type: 'DELETE',
+                    success: function() {
+                        self.keys.splice(self.keys.indexOf(key), 1);
+                    },
+                    error: self.ajaxError.bind(this, 'delete this API key')
+                });
+            }
+        });
+    };
+
+    self.ajaxError = function(item) {
+        bootbox.alert('Could not ' + item + '. Please refresh the page or ' +
+            'contact <a href="mailto: support@cos.io">support@cos.io</a> if the ' +
+            'problem persists.');
+    };
+
+    function fetch() {
+        $.ajax({
+            url: self.url,
+            type: 'GET',
+            success: self.keysRecieved,
+            error: self.ajaxError.bind(this, 'fetch your API keys')
+        });
     }
 
-    return ApiKeyView;
+    fetch();
 
-}));
+};
+
+function ApiKeyView(selector, url) {
+    // Initialization code
+    var self = this;
+    self.viewModel = new ViewModel(url);
+    window.model = self.viewModel;
+    $osf.applyBindings(self.viewModel, selector);
+}
+
+module.exports = ApiKeyView;
---
+++ website/static/js/pages/apikey-page.js
@@ -0,0 +1,4 @@
+var ApiKeyView = require('../apiKeyManager.js');
+
+var url = '/api/v1/settings/keys/';
+ApiKeyView('#apiKey', url);
