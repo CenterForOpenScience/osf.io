11b7b71ec747da9d29cb216b87a6e90900901f3b
icereval <icereval@gmail.com>
2015-01-19 11:54:14 -0500

    waterbulter should access osf by default on a separate port (5001) to avoid a deadlock in synchronous osf when requesting a file from waterbutler to render via mfr

--- waterbutler/server/settings.py
+++ waterbutler/server/settings.py
@@ -15,7 +15,7 @@ DEBUG = config.get('DEBUG', True)
 CHUNK_SIZE = config.get('CHUNK_SIZE', 65536)  # 64KB
 
 IDENTITY_METHOD = config.get('IDENTITY_METHOD', 'rest')
-IDENTITY_API_URL = config.get('IDENTITY_API_URL', 'http://127.0.0.1:5000/api/v1/files/auth/')
+IDENTITY_API_URL = config.get('IDENTITY_API_URL', 'http://127.0.0.1:5001/api/v1/files/auth/')
 
 HMAC_ALGORITHM = getattr(hashlib, config.get('HMAC_ALGORITHM', 'sha256'))
 HMAC_SECRET = config.get('HMAC_SECRET', 'changeme').encode('utf-8')
