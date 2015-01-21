1f25276bad190635d75bdd2a49b30b435d1904ec
Joshua Carp <jm.carp@gmail.com>
2015-01-21 11:11:05 -0500

    Fix brittle test.

--- tests/webtest_tests.py
+++ tests/webtest_tests.py
@@ -456,7 +456,7 @@ class TestComponents(OsfTestCase):
 
     def test_sees_parent(self):
         res = self.app.get(self.component.url, auth=self.user.auth).maybe_follow()
-        parent_title = res.html.find_all('h1', class_='node-parent-title')
+        parent_title = res.html.find_all('h2', class_='node-parent-title')
         assert_equal(len(parent_title), 1)
         assert_in(self.project.title, parent_title[0].text)
 
