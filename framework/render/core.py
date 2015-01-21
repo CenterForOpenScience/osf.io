209f42b4bb1de1480aa6b0dfc2510a4248844eb5
Steven Loria <sloria1@gmail.com>
2015-01-21 10:15:46 -0500

    Get up to date with latest mfr; use ALL_HANDLERS

--- framework/render/core.py
+++ framework/render/core.py
@@ -2,37 +2,14 @@
 import os
 
 import mfr
-import mfr_audio
-import mfr_code_pygments
-import mfr_docx
-import mfr_image
-import mfr_ipynb
-import mfr_movie
-import mfr_pdb
-import mfr_pdf
-import mfr_rst
-import mfr_tabular
-
-HANDLERS = [
-    mfr_audio.Handler,
-    mfr_image.Handler,
-    mfr_movie.Handler,
-    mfr_pdb.Handler,
-    mfr_pdf.Handler,
-    mfr_rst.Handler,
-    mfr_tabular.Handler,
-    mfr_ipynb.Handler,
-    mfr_docx.Handler,
-    mfr_code_pygments.Handler,
-]
-
+from mfr.ext import ALL_HANDLERS
 
 def init_mfr(app):
     """Register all available FileHandlers and collect each
     plugin's static assets to the app's static path.
     """
     # Available file handlers
-    mfr.register_filehandlers(HANDLERS)
+    mfr.register_filehandlers(ALL_HANDLERS)
 
     # Update mfr config with static path and url
     mfr.config.update({
