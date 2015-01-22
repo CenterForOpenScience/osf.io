21e120e41e06cc1ab0f344ce7d94256d56da0d44
Joshua Carp <jm.carp@gmail.com>
2015-01-22 10:58:30 -0500

    Remove task args and kwargs from Celery logs.

--- waterbutler/tasks/app.py
+++ waterbutler/tasks/app.py
@@ -1,14 +1,28 @@
 from celery import Celery
+from celery.signals import task_failure
+
+from raven import Client
 
 from waterbutler import settings
 from waterbutler.tasks import settings as tasks_settings
 
-from raven import Client
-from raven.contrib.celery import register_signal
-
 
 app = Celery()
 app.config_from_object(tasks_settings)
+
+
+def register_signal(client):
+    """Adapted from `raven.contrib.celery.register_signal`. Remove args and
+    kwargs from logs so that keys aren't leaked to Sentry.
+    """
+    def process_failure_signal(sender, task_id, *args, **kwargs):
+        client.captureException(
+            extra={
+                'task_id': task_id,
+                'task': sender,
+            }
+        )
+    task_failure.connect(process_failure_signal, weak=False)
 
 
 sentry_dsn = settings.get('SENTRY_DSN', None)
