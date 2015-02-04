72044e13f0040ef4c198447aaf317ba7903ad893
Chris Seto <chriskseto@gmail.com>
2015-02-04 15:15:56 -0500

    Dont make any parellel requests

--- website/static/js/filerenderer.js
+++ website/static/js/filerenderer.js
@@ -8,23 +8,37 @@ FileRenderer = {
         this.url = url;
         this.element = $(selector);
         this.tries = 0;
-        this.refreshContent = window.setInterval(this.getCachedFromServer.bind(this), 1000);
+        // this.refreshContent = window.setInterval(this.getCachedFromServer.bind(this), 1000);
+        this.getCachedFromServer();
     },
 
     getCachedFromServer: function() {
         var self = this;
-        $.get( self.url, function(data) {
+        var fut = $.ajax({
+            url: self.url,
+        });
+
+        fut.done(function(data) {
             if (data) {
                 self.element.html(data);
                 clearInterval(self.refreshContent);
             } else {
-                self.tries += 1;
-                if(self.tries > 10){
-                    clearInterval(self.refreshContent);
-                    self.element.html('Timeout occurred while loading, please refresh the page');
-                }
+                self.handleRetry();
             }
         });
+
+        fut.fail(self.handleRetry);
+    },
+
+    handleRetry: function() {
+        var self = this;
+        self.tries += 1;
+        if(self.tries > 10){
+            clearInterval(self.refreshContent);
+            self.element.html('Timeout occurred while loading, please refresh the page');
+        } else {
+            self.getCachedFromServer();
         }
+    }
 };
 module.exports = FileRenderer;
