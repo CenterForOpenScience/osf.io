aecc659b80d7f7dac076fcdd1c3bfde8d6e14cf2
Chris Seto <chriskseto@gmail.com>
2015-02-05 11:33:53 -0500

    Better error message for figshare

--- website/addons/figshare/exceptions.py
+++ website/addons/figshare/exceptions.py
@@ -1,8 +1,16 @@
 from website.addons.base.exceptions import AddonEnrichmentError
+from website.util.sanitize import escape_html
 
 class FigshareIsDraftError(AddonEnrichmentError):
 
+    def __init__(self, file_guid):
+        self.file_guid = file_guid
+
     @property
     def renderable_error(self):
-        return '''<p>This File is still a draft on Figshare, to view it publish it or go here</p>'''
-
+        return '''
+        <div class="alert alert-info" role="alert">
+        The file "{name}" is still a draft on Figshare. <br>
+        To view it  on the OSF <a href="http://figshare.com/faqs">publish</a> it on Figshare.
+        </div>
+        '''.format(name=escape_html(self.file_guid.name))
