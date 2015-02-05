c50b461759a3bdbb2e76e69a336dd8a88b485c9f
Steven Loria <sloria1@gmail.com>
2015-02-05 10:58:47 -0500

    Refactor citation widget initialization into a class
    Only initialize it if not viewing through an anonymized VOL

--- website/static/js/citations.js
+++ website/static/js/citations.js
@@ -1,3 +1,6 @@
+/**
+ * Controller for the rich citation widget on the project overview page.
+ */
 'use strict';
 
 var $ = require('jquery');
@@ -28,46 +31,56 @@ var formatSelection = function(state) {
     return state.title;
 };
 
-var input = $('#citationStyleInput');
-var citationElement = $('#citationText');
+// Public API
 
-input.select2({
-    allowClear: true,
-    formatResult: formatResult,
-    formatSelection: formatSelection,
-    placeholder: 'Citation Style (e.g. "APA")',
-    minimumInputLength: 1,
-    ajax: {
-        url: '/api/v1/citations/styles/',
-        quietMillis: 200,
-        data: function(term, page) {
-            return {
-                'q': term
-            };
-        },
-        results: function(data, page) {
-            return {results: data.styles};
-        },
-        cache: true
-    }
-}).on('select2-selecting', function(e) {
-    var url = ctx.node.urls.api + 'citation/' + e.val;
-    var request = $.ajax({
-        url: url
-    });
-    request.done(function (data) {
-        citationElement.text(data.citation).slideDown();
-    });
-    request.fail(function(jqxhr, status, error) {
-        $osf.growl(
-            'Citation render failed',
-            'The requested citation format generated an error.',
-            'danger'
-        );
-        Raven.captureMessage('Unexpected error when fetching citation', {
-            url: url, citationStyle: e.val, status: status, error: error
+function CitationWidget(inputSelector, displaySelector) {
+    this.$input = $(inputSelector || '#citationStyleInput');
+    this.$citationElement = $(displaySelector || '#citationText');
+    this.init();
+}
+CitationWidget.prototype.init = function() {
+    var self = this;
+    // Initialize select2 for selecting citation style
+    self.$input.select2({
+        allowClear: true,
+        formatResult: formatResult,
+        formatSelection: formatSelection,
+        placeholder: 'Citation Style (e.g. "APA")',
+        minimumInputLength: 1,
+        ajax: {
+            url: '/api/v1/citations/styles/',
+            quietMillis: 200,
+            data: function(term, page) {
+                return {
+                    'q': term
+                };
+            },
+            results: function(data, page) {
+                return {results: data.styles};
+            },
+            cache: true
+        }
+    }).on('select2-selecting', function(e) {
+        var url = ctx.node.urls.api + 'citation/' + e.val;
+        var request = $.ajax({
+            url: url
         });
+        request.done(function (data) {
+            self.$citationElement.text(data.citation).slideDown();
+        });
+        request.fail(function(jqxhr, status, error) {
+            $osf.growl(
+                'Citation render failed',
+                'The requested citation format generated an error.',
+                'danger'
+            );
+            Raven.captureMessage('Unexpected error when fetching citation', {
+                url: url, citationStyle: e.val, status: status, error: error
+            });
+        });
+    }).on('select2-removed', function (e) {
+        self.$citationElement.slideUp().text();
     });
-}).on('select2-removed', function (e) {
-    citationElement.slideUp().text();
-});
+
+};
+module.exports = CitationWidget;
