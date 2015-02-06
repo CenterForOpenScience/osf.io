553bce29a41e2fb2d0d0d83fc489f7149ba77438
Chris Seto <chriskseto@gmail.com>
2015-02-06 09:28:57 -0500

    Better error message

--- website/addons/base/exceptions.py
+++ website/addons/base/exceptions.py
@@ -15,4 +15,10 @@ class AddonEnrichmentError(AddonError):
 
     @property
     def renderable_error(self):
-        return 'TODO'
+        return '''
+        <div class="alert alert-info" role="alert">
+        This file is currently unable to be rendered. <br>
+        If this should not have occurred and the issue persists,
+        please report it to <a href="mailto:support@osf.io">support@osf.io
+        </div>
+        '''
