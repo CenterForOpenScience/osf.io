7b8e4619cfad157b9ed68497963e991c238e3df4
Chris Seto <chriskseto@gmail.com>
2015-02-06 10:21:48 -0500

    Removal of unused figshare code

--- website/addons/figshare/__init__.py
+++ website/addons/figshare/__init__.py
@@ -8,7 +8,7 @@ MODELS = [
 USER_SETTINGS_MODEL = model.AddonFigShareUserSettings
 NODE_SETTINGS_MODEL = model.AddonFigShareNodeSettings
 
-ROUTES = [routes.settings_routes, routes.page_routes, routes.api_routes]
+ROUTES = [routes.settings_routes, routes.api_routes]
 
 SHORT_NAME = 'figshare'
 FULL_NAME = 'FigShare'
