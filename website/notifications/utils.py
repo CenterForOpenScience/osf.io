51479f53b1bcf661752bce7869d0034e6538d685
Saman Ehsan <se8ea@virginia.edu>
2015-02-10 11:09:14 -0500

    Remove unused SubscriptionsDict helper class
    This class was used to format subscriptions
    on the user settings page but is no longer
    being used.

--- website/notifications/utils.py
+++ website/notifications/utils.py
@@ -21,23 +21,6 @@ class NotificationsDict(dict):
         return True
 
 
-class SubscriptionsDict(dict):
-    def __init__(self):
-        super(dict, self).__init__()
-        self.update(subscriptions=collections.defaultdict(SubscriptionsDict),
-                    children=collections.defaultdict(SubscriptionsDict))
-
-    def add_subscription(self, keys, subscription):
-        d_to_use = self
-        for key in keys:
-            d_to_use = d_to_use['children'][key]
-        for notification_type in settings.NOTIFICATION_TYPES:
-            if getattr(subscription, notification_type):
-                d_to_use['subscriptions'] = {subscription.event_name: []}
-                d_to_use['subscriptions'][subscription.event_name].append(notification_type)
-        return True
-
-
 def get_configured_projects(user):
     configured_project_ids = []
     user_subscriptions = get_all_user_subscriptions(user)
@@ -125,6 +108,7 @@ def get_parent_notification_type(uid, event, user):
             for notification_type in settings.NOTIFICATION_TYPES:
                 if user in getattr(subscription, notification_type):
                     return notification_type
+
 
 def format_user_and_project_subscriptions(user):
     return [
