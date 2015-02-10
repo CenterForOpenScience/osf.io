5b7abfe39a5e47d3436f040e6b0911046feac967
Chris Seto <chriskseto@gmail.com>
2015-02-10 15:42:02 -0500

    Add More render exceptions

--- framework/render/exceptions.py
+++ framework/render/exceptions.py
@@ -13,11 +13,23 @@ STATUS_CODE_ERROR_MAP = {
 
 
 class RenderFailureException(Exception):
+    '''An exception for temporary errors when attempting to render a file
+    IE 500s 400s etc etc
+    '''
     def __init__(self, http_status_code, **additional):
         additional['status_code'] = http_status_code
 
         msg = json.dumps(additional)
         super(Exception, self).__init__(msg)
+
+
+class RenderNotPossibleException(Exception):
+    '''An exception indicating an an avoidable render error
+    that should therefore be cached. IE dropbox's DMCA take downs
+    '''
+    def __init__(self, msg):
+        self.renderable_error = msg
+        super(RenderNotPossibleException, self).__init__(msg)
 
 
 def error_message_or_exception(http_status_code, **info):
@@ -26,4 +38,4 @@ def error_message_or_exception(http_status_code, **info):
     if not err:
         raise RenderFailureException(http_status_code)
 
-    return err.format(**info)
+    raise RenderNotPossibleException(err.format(**info))
