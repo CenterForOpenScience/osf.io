096d7a90de5a4e302739da34a031eb9228a92a76
Chris Seto <chriskseto@gmail.com>
2015-02-10 23:23:37 -0500

    More exceptions!

--- website/addons/base/exceptions.py
+++ website/addons/base/exceptions.py
@@ -14,11 +14,54 @@ class HookError(AddonError):
 class AddonEnrichmentError(AddonError):
 
     @property
+    def can_delete(self):
+        return False
+
+    @property
+    def can_download(self):
+        return False
+
+    @property
     def renderable_error(self):
         return '''
         <div class="alert alert-info" role="alert">
         This file is currently unable to be rendered. <br>
         If this should not have occurred and the issue persists,
         please report it to <a href="mailto:support@osf.io">support@osf.io
+        </div>
+        '''
+
+    @property
+    def render_error(self):
+        # TODO Refactor me to be all in the front end
+        # 2/10/14 ping @chrisseto when refactoring
+        additional = ''
+        if not self.can_download:
+            additional += "<style>.fileDownload{display: none;}</style>"
+
+        if not self.can_delete:
+            additional += "<style>.fileDelete{display: none;}</style>"
+
+        return self.renderable_error + additional
+
+
+class FileDeletedError(AddonEnrichmentError):
+
+    @property
+    def renderable_error(self):
+        return '''
+        <div class="alert alert-info" role="alert">
+        This file has been deleted.
+        </div>
+        '''
+
+
+class FileDoesntExistError(AddonEnrichmentError):
+
+    @property
+    def renderable_error(self):
+        return '''
+        <div class="alert alert-info" role="alert">
+        This file does not exist.
         </div>
         '''
