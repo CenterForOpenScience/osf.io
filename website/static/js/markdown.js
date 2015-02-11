59dd78a752c282b17b8db205d6725697bcc534c0
rliebz <rliebz@gmail.com>
2015-02-11 14:36:07 -0500

    Add video and sanitizer plugins for markdown-it

--- website/static/js/markdown.js
+++ website/static/js/markdown.js
@@ -1,3 +1,5 @@
-var markdown = require('markdown-it')();
+var markdown = require('markdown-it')('commonmark')
+    .use(require('markdown-it-video'))
+    .use(require('markdown-it-sanitizer'));
 
 module.exports = markdown;
