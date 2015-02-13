6cbe32ab786995f3cae3340658408adc895ad0e3
Brian J. Geiger <bgeiger@pobox.com>
2015-02-13 15:40:10 -0500

    Make quick vs full version of markdown renderer for live editing vs. nearly live editing and normal rendering

--- website/static/js/markdown.js
+++ website/static/js/markdown.js
@@ -1,6 +1,8 @@
 var hljs = require('highlight.js');
 require('highlight-css');
-var markdown = require('markdown-it')('commonmark', {
+var MarkdownIt = require('markdown-it')
+
+var markdown = new MarkdownIt('commonmark', {
     highlight: function (str, lang) {
         if (lang && hljs.getLanguage(lang)) {
             try {
@@ -18,4 +20,23 @@ var markdown = require('markdown-it')('commonmark', {
     .use(require('markdown-it-video'))
     .use(require('markdown-it-sanitizer'));
 
+var markdown_quick = new MarkdownIt(('commonmark'), {
+     highlight: function (str, lang) {
+        if (lang && hljs.getLanguage(lang)) {
+            try {
+                return hljs.highlight(lang, str).value;
+            } catch (__) {}
+        }
+
+        try {
+            return hljs.highlightAuto(str).value;
+        } catch (__) {}
+
+        return ''; // use external default escaping
+    }
+})
+    .use(require('markdown-it-sanitizer'))
+    .disable('image');
+
 module.exports = markdown;
+module.exports = markdown_quick;
