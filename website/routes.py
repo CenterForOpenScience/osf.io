e3c0b07fc1525a61c9f6f5666d9071ecf45c79bf
Fabian von Feilitzsch <fabian@fabianism.us>
2015-02-13 16:29:55 -0500

    Add post route for share search

--- website/routes.py
+++ website/routes.py
@@ -631,7 +631,7 @@ def make_url_map(app):
 
         Rule(['/search/', '/search/<type>/'], ['get', 'post'], search_views.search_search, json_renderer),
         Rule('/search/projects/', 'get', search_views.search_projects_by_title, json_renderer),
-        Rule('/share/', 'get', search_views.search_share, json_renderer),
+        Rule('/share/', ['get', 'post'], search_views.search_share, json_renderer),
         Rule('/share/count/', ['get', 'post'], search_views.count_share, json_renderer)
 
     ], prefix='/api/v1')
