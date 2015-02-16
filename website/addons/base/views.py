b33e4676f7b0be5cb47240cf433fc361055e515d
Chris Seto <chriskseto@gmail.com>
2015-02-16 01:04:42 -0500

    default to osfstorage as a provider for cases such as /pid/files/fid/

--- website/addons/base/views.py
+++ website/addons/base/views.py
@@ -243,8 +243,10 @@ def addon_view_or_download_file_legacy(**kwargs):
 
     if 'osffiles' in request.path:
         provider = 'osfstorage'
-    else:
+    elif 'provider' in kwargs:
         provider = kwargs['provider']
+    else:
+        provider = 'osfstorage'
 
     if 'download' in request.path:
         action = 'download'
