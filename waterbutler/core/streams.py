577ce0c90bc54d04469f5b7a17d10262e5f54b54
Chris Seto <chriskseto@gmail.com>
2015-02-15 19:45:19 -0500

    Complete FormDataStream

--- waterbutler/core/streams.py
+++ waterbutler/core/streams.py
@@ -1,5 +1,6 @@
 import os
 import abc
+import uuid
 import asyncio
 
 
@@ -148,7 +149,6 @@ class MultiStream(asyncio.StreamReader):
     etc. Used to build streaming form data for Figshare uploads.
     """
     def __init__(self, *streams):
-        self._streams = streams
         self.streams = list(streams)
         self.cycle()
 
@@ -176,49 +176,103 @@ class FormDataStream(MultiStream):
     Reads from the current stream until exhausted, then continues to the next,
     etc. Used to build streaming form data for Figshare uploads.
     """
+    FORM_DATA_HEADER = 'Content-Disposition: form-data; name="{}"\r\n\r\n'
+    FILE_HEADER = 'Content-Disposition: file; {}filename="{}"\r\nContent-Type: {}\r\nContent-Transfer-Encoding: {}\r\n\r\n'
 
     @classmethod
     def make_boundary(self):
-        return uuid.uuid4().hex.encode('utf-8')
-
-    @classmethod
-    def make_header(key, value):
-        return b'Content-Disposition: form-data; name="{1}"\r\n\r\n'.format(key, value)
+        return uuid.uuid4().hex
 
     def __init__(self, **fields):
-        streams = [self.make_boundary_stream()]
+        self.can_add_more = True
+        self.boundary = self.make_boundary()
+        super().__init__()
 
-        for key, value in fields:
-            stream += self.make_header(key, value)
+        for key, value in fields.items():
+            if isinstance(value, tuple):
+                self.add_file(key, *value)
+            elif isinstance(value, asyncio.StreamReader):
+                self.add_file(key, value)
+            else:
+                self.add_field(key, value)
 
-        super().__init__(*streams)
+    @property
+    def headers(self):
+        assert not self.can_add_more, 'Must call finalize or read before getting headers.'
 
-    def make_boundary_stream(self):
-        return StringStream(b'--{}\r\n'.format(self.boundary))
+        return {
+            'Content-Length': self.size,
+            'Content-Type': 'multipart/form-data, boundry={}'.format(self.boundry)
+        }
+
+    @asyncio.coroutine
+    def read(self, n=-1):
+        if self.can_add_more:
+            self.finalize()
+        return super().read(n=n)
+
+    def finalize(self):
+        if self.can_add_more:
+            if not self.stream and self.streams:
+                self.cycle()
+            self.can_add_more = False
+            self.streams.append(self.end_boundary)
+            self.size = sum([x.size for x in self.streams])
+
+    def add_file(self, file_name, file_stream, field_name=None, mime='application/octet-stream', transcoding='binary'):
+        assert self.can_add_more, 'Cannot add more fields after calling finalize or read'
+
+        if field_name:
+            field_name = 'name="{}"; '.format(field_name)
+        else:
+            field_name = ''
+
+        self.streams.extend([
+            self._make_boundary_stream(),
+            StringStream(
+                self.FILE_HEADER.format(
+                    field_name,
+                    file_name,
+                    mime,
+                    transcoding
+                )
+            ),
+            file_stream
+        ])
+
+    def add_field(self, key, value):
+        assert self.can_add_more, 'Cannot add more fields after calling finalize or read'
+
+        self.streams.extend([
+            self._make_boundary_stream(),
+            StringStream(self.FORM_DATA_HEADER.format(key) + value + '\r\n')
+        ])
+
+    def _make_boundary_stream(self):
+        return StringStream('--{}\r\n'.format(self.boundary))
 
     @property
     def end_boundary(self):
-        return StringStream(b'--{}--\r\n'.format(self.boundary))
+        return StringStream('--{}--\r\n'.format(self.boundary))
 
 
 class StringStream(asyncio.StreamReader):
     def __init__(self, data):
-        self.size = len(data)
+        super().__init__()
+        if isinstance(data, str):
+            data = data.encode('UTF-8')
+        elif not isinstance(data, bytes):
+            raise TypeError('Data must be either str or bytes, found {!r}'.format(type(data)))
+
         self.feed_data(data)
+        self.size = len(data)
+
         self.feed_eof()
 
 
 def make_headers(**options):
     fields = '; '.join('{0}="{1}"'.format(key, value) for key, value in options.items())
     return 'Content-Disposition: form-data; {0}\r\n\r\n'.format(fields).encode('utf-8')
-
-
-def make_boundary_streams(boundary, **options):
-    headers = make_headers(**options)
-    return (
-        wrap_stream(b'--' + boundary + b'\r\n' + headers),
-        wrap_stream(b'\r\n--' + boundary + b'--\r\n'),
-    )
 
 
 def make_upload_data(stream, **options):
