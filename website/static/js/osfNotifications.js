fe2843d046ad73bea7a8ae3fbcbc9dd753f6df63
Saman Ehsan <se8ea@virginia.edu>
2015-02-17 15:40:20 -0500

    Remove unused imports in osfNotifications.js

--- website/static/js/osfNotifications.js
+++ website/static/js/osfNotifications.js
@@ -1,7 +1,3 @@
-var $ = require('jquery');
-var $osf = require('osfHelpers');
-var bootbox = require('bootbox');
-
 //initialize treebeard
 var ProjectNotifications = require('./project-settings-treebeard.js');
 new ProjectNotifications(window.contextVars.notificationsData);
