65c85d1367461a98026bdccc14a5aa04284aef68
kushG <imkushagra@gmail.com>
2015-02-17 20:16:19 -0500

    Deleted Unneccessary files

--- website/addons/gdrive/static/gdriveRubeusConfig.js
+++
@@ -1,9 +0,0 @@
-
-var Rubeus = require('rubeus');
-
-Rubeus.cfg.gdrive = {
-    // Custom error message for when folder contents cannot be fetched
-    FETCH_ERROR: '<span class="text-danger">This Google Drive folder may ' +
-                    'have been renamed or deleted. ' +
-                    'Please select a folder at the settings page.</span>'
-};
