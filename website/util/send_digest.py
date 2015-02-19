9a1a908ce18da3654c567a0e55690179f6c6e996
Saman Ehsan <se8ea@virginia.edu>
2015-02-19 16:36:48 -0500

    Remove duplicate TestSendDigest added in merge

--- website/util/send_digest.py
+++ website/util/send_digest.py
@@ -15,10 +15,6 @@ from website import mails, settings
 from website.app import init_app
 from website.notifications.model import DigestNotification
 from website.notifications.utils import NotificationsDict
-from tests.base import OsfTestCase
-from tests.factories import DigestNotificationFactory, UserFactory, ProjectFactory
-import nose.tools
-
 
 from scripts import utils as script_utils
 
@@ -87,81 +83,6 @@ def group_digest_notifications_by_user():
                     };
                     """))
 
-class TestSendDigest(OsfTestCase):
-    def test_group_digest_notifications_by_user(self):
-        user = UserFactory()
-        user2 = UserFactory()
-        project = ProjectFactory()
-        timestamp = (datetime.datetime.utcnow() - datetime.timedelta(hours=1)).replace(microsecond=0)
-        d = DigestNotificationFactory(
-            user_id=user._id,
-            timestamp=timestamp,
-            message='Hello',
-            node_lineage=[project._id]
-        )
-        d.save()
-        d2 = DigestNotificationFactory(
-            user_id=user2._id,
-            timestamp=timestamp,
-            message='Hello',
-            node_lineage=[project._id]
-        )
-        d2.save()
-        user_groups = group_digest_notifications_by_user()
-        info = [{
-                u'message': {
-                    u'message': u'Hello',
-                    u'timestamp': timestamp,
-                },
-                u'node_lineage': [unicode(project._id)]
-                }]
-        expected = [{
-                    u'user_id': user._id,
-                    u'info': info
-                    },
-                    {
-                    u'user_id': user2._id,
-                    u'info': info
-                    }]
-        assert_equal(len(user_groups), 2)
-        assert_equal(user_groups, expected)
-
-    @unittest.skipIf(settings.USE_CELERY, 'Digest emails must be sent synchronously for this test')
-    @mock.patch('website.mails.send_mail')
-    def test_send_digest_called_with_correct_args(self, mock_send_mail):
-        d = DigestNotificationFactory(
-            user_id=UserFactory()._id,
-            timestamp=datetime.datetime.utcnow(),
-            message='Hello',
-            node_lineage=[ProjectFactory()._id]
-        )
-        d.save()
-        user_groups = group_digest_notifications_by_user()
-        send_digest(user_groups)
-        assert_true(mock_send_mail.called)
-
-        user = User.load(user_groups[2]['user_id'])
-        mock_send_mail.assert_called_with(
-            to_addr=user.username,
-            mail=mails.DIGEST,
-            name=user.fullname,
-            message=group_messages(user_groups[2]['info']),
-            url=urlparse.urljoin(settings.DOMAIN, web_url_for('user_notifications'))
-        )
-
-    @unittest.skipIf(settings.USE_CELERY, 'Digest emails must be sent synchronously for this test')
-    def test_send_digest_deletes_sent_digest_notifications(self):
-        d = DigestNotificationFactory(
-            user_id=UserFactory()._id,
-            timestamp=datetime.datetime.utcnow(),
-            message='Hello',
-            node_lineage=[ProjectFactory()._id]
-        )
-        id = d._id
-        user_groups = group_digest_notifications_by_user()
-        send_digest(user_groups)
-        with assert_raises(NoResultsFound):
-            DigestNotification.find_one(Q('_id', 'eq', id))
 
 if __name__ == '__main__':
     main()
