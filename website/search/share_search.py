642267c42ab46030554da1bdc4c419cdc522b3a9
erinspace <erin.braswell@gmail.com>
2015-02-23 09:57:41 -0500

    pep8 fixes to search code

--- website/search/share_search.py
+++ website/search/share_search.py
@@ -13,6 +13,7 @@ share_es = Elasticsearch(
     request_timeout=settings.ELASTIC_TIMEOUT
 )
 
+
 def search(query):
     # Run the real query and get the results
     raw_results = share_es.search(index='share', doc_type=None, body=query)
@@ -22,6 +23,7 @@ def search(query):
         'results': results,
         'count': raw_results['hits']['total'],
     }
+
 
 def count(query):
     if query.get('from') is not None:
