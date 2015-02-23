aa447cf0240b9dacaadad3d5f81787985fcfdfac
icereval <icereval@gmail.com>
2015-02-23 16:45:51 -0500

    remove mock

--- tests/server/handlers/test_crud.py
+++ tests/server/handlers/test_crud.py
@@ -53,7 +53,6 @@ class TestCrudHandler(utils.HandlerTestCase):
     @testing.gen_test
     def test_download_stream(self, mock_make_provider):
         stream = asyncio.StreamReader()
-        stream.response = mock.Mock(headers={})
         data = b'freddie brian john roger'
         stream.feed_data(data)
         stream.feed_eof()
