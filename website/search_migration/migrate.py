b98bcfb3872c17358cd101211b23f5085750d89d
Fabian von Feilitzsch <fabian@fabianism.us>
2015-02-23 16:30:30 -0500

    use enumerate instead of maintaining a count

--- website/search_migration/migrate.py
+++ website/search_migration/migrate.py
@@ -17,21 +17,19 @@ app = init_app("website.settings", set_backends=True, routes=True)
 
 
 def migrate_nodes():
-    count = 0
     nodes = Node.find(Q('is_public', 'eq', True) & Q('is_deleted', 'eq', False))
-    for node in nodes:
+    for i, node in enumerate(nodes):
         node.update_search()
-        count += 1
-    return count
+
+    return i + 1  # Started counting from 0
 
 
 def migrate_users():
-    count = 0
-    for user in User.find():
+    for i, user in enumerate(User.find()):
         if user.is_active:
             user.update_search()
-            count += 1
-    return count
+
+    return i + 1  # Started counting from 0
 
 
 def main():
