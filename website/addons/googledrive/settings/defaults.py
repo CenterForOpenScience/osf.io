c61847e6cbae36e70ea2a9ab958c0b3eb1be79ea
Chris Seto <chriskseto@gmail.com>
2015-02-24 11:15:23 -0500

    Add refresh endpoint and refresh time to settings

--- website/addons/googledrive/settings/defaults.py
+++ website/addons/googledrive/settings/defaults.py
@@ -3,5 +3,8 @@
 CLIENT_ID = 'chaneme'
 CLIENT_SECRET = 'changeme'
 
+REFRESH_TIME = 5 * 60  # 5 minutes
+
 # Check https://developers.google.com/drive/scopes for all available scopes
 OAUTH_SCOPE = ['https://www.googleapis.com/auth/drive']
+REFRESH_ENDPOINT = 'https://www.googleapis.com/oauth2/v3/token'
