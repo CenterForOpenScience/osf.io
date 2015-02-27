983a8180ef646759a92ed69db411e34bf49f182b
Chris Seto <chriskseto@gmail.com>
2015-02-27 18:23:58 -0500

    Code clean up

--- website/notifications/model.py
+++ website/notifications/model.py
@@ -7,10 +7,11 @@ class Subscription(StoredObject):
     _id = fields.StringField(primary=True)  # pxyz_wiki_updated, uabc_comment_replies
     object_id = fields.StringField()     # pid, user._id
     event_name = fields.StringField()      # wiki_updated, comment_replies
+
     # Notification types
-    email_transactional = fields.ForeignField('user', list=True, backref='email_transactional')
-    email_digest = fields.ForeignField('user', list=True, backref='email_digest')
     none = fields.ForeignField('user', list=True, backref='none')
+    email_digest = fields.ForeignField('user', list=True, backref='email_digest')
+    email_transactional = fields.ForeignField('user', list=True, backref='email_transactional')
 
     def remove_user_from_subscription(self, user):
         for n in NOTIFICATION_TYPES:
