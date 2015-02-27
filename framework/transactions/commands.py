4319adfbc71adc75cf934401387300af78220edf
Steven Loria <sloria1@gmail.com>
2015-02-26 23:39:43 -0500

    Remove unused imports

--- framework/transactions/commands.py
+++ framework/transactions/commands.py
@@ -1,11 +1,6 @@
 # -*- coding: utf-8 -*-
-
-from pymongo.errors import OperationFailure
-
 from framework.mongo import database as proxy_database
-from flask import current_app
 
-from website import settings
 
 def begin(database=None):
     database = database or proxy_database
