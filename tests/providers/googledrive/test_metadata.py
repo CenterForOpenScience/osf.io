6e487ad60bdb58fc162601ac91626fde1616e86f
Chris Seto <chriskseto@gmail.com>
2015-02-26 22:11:02 -0500

    Add tests for forward slashes in file names

--- tests/providers/googledrive/test_metadata.py
+++ tests/providers/googledrive/test_metadata.py
@@ -1,6 +1,7 @@
 import pytest
 
 import os
+from urllib.parse import quote
 
 from waterbutler.providers.googledrive.metadata import GoogleDriveFileMetadata
 from waterbutler.providers.googledrive.metadata import GoogleDriveFolderMetadata
@@ -19,8 +20,22 @@ def test_file_metadata_drive():
     assert parsed.size == item['fileSize']
     assert parsed.modified == item['modifiedDate']
     assert parsed.content_type == item['mimeType']
-    assert parsed.path == os.path.join(path, item['title'])
     assert parsed.extra == {'revisionId': item['version']}
+    assert parsed.path == os.path.join(path, quote(item['title'], safe=''))
+
+
+def test_file_metadata_drive_slashes():
+    path = '/conrad'
+    item = fixtures.file_forward_slash
+    parsed = GoogleDriveFileMetadata(item, path)
+    assert parsed.provider == 'googledrive'
+    assert parsed.id == item['id']
+    assert parsed.name == item['title']
+    assert parsed.size == item['fileSize']
+    assert parsed.modified == item['modifiedDate']
+    assert parsed.content_type == item['mimeType']
+    assert parsed.extra == {'revisionId': item['version']}
+    assert parsed.path == os.path.join(path, quote(item['title'], safe=''))
 
 
 def test_file_metadata_docs():
@@ -38,8 +53,19 @@ def test_folder_metadata():
     assert parsed.provider == 'googledrive'
     assert parsed.id == item['id']
     assert parsed.name == item['title']
-    assert parsed.path == os.path.join(path, item['title'] + '/')
     assert parsed.extra == {'revisionId': item['version']}
+    assert parsed.path == os.path.join(path, quote(item['title'], safe='') + '/')
+
+
+def test_folder_metadata_slash():
+    path = '/we/love/you/conrad'
+    item = fixtures.folder_metadata_forward_slash
+    parsed = GoogleDriveFolderMetadata(item, path)
+    assert parsed.provider == 'googledrive'
+    assert parsed.id == item['id']
+    assert parsed.name == item['title']
+    assert parsed.extra == {'revisionId': item['version']}
+    assert parsed.path == os.path.join(path, quote(item['title'], safe='') + '/')
 
 
 def test_revision_metadata():
