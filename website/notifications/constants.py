2b8a18e1e4ca7177c7e2bcdcba0ea2e854e6a15e
Chris Seto <chriskseto@gmail.com>
2015-02-28 14:46:37 -0500

    Add note

--- website/notifications/constants.py
+++ website/notifications/constants.py
@@ -6,6 +6,7 @@ USER_SUBSCRIPTIONS_AVAILABLE = {
     'comment_replies': 'Replies to your comments'
 }
 
+# Note: the python value None mean inherit from parent
 NOTIFICATION_TYPES = {
     'email_transactional': 'Email when a change occurs',
     'email_digest': 'Daily email digest of all changes to this project',
