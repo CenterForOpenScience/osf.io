030c8f771bfad6d6229cb9fe6703ef0226f07be7
Steven Loria <sloria1@gmail.com>
2015-03-04 14:56:24 -0500

    Add more logging to audit script
    [skip ci]

--- scripts/find_guids_without_referents.py
+++ scripts/find_guids_without_referents.py
@@ -3,27 +3,38 @@
 E.g. a node was created and given a guid but an error caused the node to
 get deleted, leaving behind a guid that points to nothing.
 """
+import sys
 
-from framework import Q
+from modularodm import Q
 from framework.guid.model import Guid
 from website.app import init_app
 from website.project.model import Node
 from tests.base import OsfTestCase
 from tests.factories import NodeFactory
+from scripts import utils as scripts_utils
 from nose.tools import *
+
+import logging
+logger = logging.getLogger(__name__)
 
 
 def main():
+    if 'dry' not in sys.argv:
+        scripts_utils.add_file_logger(logger, __file__, suffix=worker_id)
     # Set up storage backends
     init_app(routes=False)
-    print ('{n} invalid GUID objects found'.format(n=len(get_targets())))
+    logger.info('{n} invalid GUID objects found'.format(n=len(get_targets())))
 
 
 def get_targets():
     """Find GUIDs with no referents and GUIDs with referents that no longer exist."""
-    # Use a list comp because querying MODM with Guid.find(Q('referent', 'eq', None))
+    # Use a loop because querying MODM with Guid.find(Q('referent', 'eq', None))
     # only catches the first case.
-    return [each for each in Guid.find() if each.referent is None]
+    ret = []
+    for each in Guid.find():
+        logger.info('GUID {} has no referent.'.format(each._id))
+        ret.append(each)
+    return ret
 
 
 class TestFindGuidsWithoutReferents(OsfTestCase):
@@ -31,7 +42,7 @@ class TestFindGuidsWithoutReferents(OsfTestCase):
     def setUp(self):
         super(TestFindGuidsWithoutReferents, self).setUp()
         self.node = NodeFactory()
-        self.nontarget_guid= Guid(referent=self.node)
+        self.nontarget_guid = Guid(referent=self.node)
         self.nontarget_guid.save()
 
     def test_get_targets_referent_is_none(self):
