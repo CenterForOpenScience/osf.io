5ead1b39fbef2623cdac42f8c8f3d5152679cd4d
Sam Chrisinger <s.chrisinger@gmail.com>
2015-03-04 11:56:58 -0500

    Add a 'share' link to Box serialized settings
    This allows the folderpicker to work as expected when a user clicks
    "Share on Box".

--- website/addons/box/settings/defaults.py
+++ website/addons/box/settings/defaults.py
@@ -6,3 +6,5 @@ REFRESH_TIME = 5 * 60  # 5 minutes
 
 BOX_OAUTH_TOKEN_ENDPOINT = 'https://www.box.com/api/oauth2/token'
 BOX_OAUTH_AUTH_ENDPOINT = 'https://www.box.com/api/oauth2/authorize'
+
+BOX_SHARE_URL_TEMPLATE = 'https://app.box.com/files/0/f/{0}'
