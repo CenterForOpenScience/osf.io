c53183a11f49c73322079ef25fc71e2b6387d4ca
Steven Loria <sloria1@gmail.com>
2015-03-05 09:53:20 -0500

    Add more verbose logging; only log to file if not a dry run

--- scripts/osfstorage/migrate_dates.py
+++ scripts/osfstorage/migrate_dates.py
@@ -24,13 +24,14 @@ from scripts.osfstorage.utils import ensure_osf_files
 
 
 logger = logging.getLogger(__name__)
-script_utils.add_file_logger(logger, __file__)
-logging.basicConfig(level=logging.INFO)
 
 
-def migrate_version(idx, file_data, record):
+def migrate_version(idx, file_data, record, dry_run=True):
     version = record.versions[idx]
-    version._fields['date_created'].__set__(version, file_data['date_modified'], safe=True)
+    logger.info('Setting OsfStorageFileVersion {} date_created to {}'.format(version._id, file_data['date_modified']))
+    if not dry_run:
+        version._fields['date_created'].__set__(version, file_data['date_modified'], safe=True)
+        version.save()
 
 
 def migrate_node(node, dry_run=True):
@@ -38,14 +39,12 @@ def migrate_node(node, dry_run=True):
     for path, versions in node.files_versions.iteritems():
         for idx, version in enumerate(versions):
             logger.info('Migrating file {0}, version {1} on node {2}'.format(path, idx, node._id))
-            if dry_run:
-                continue
             try:
                 # Note: Use direct mongo lookup to handle deprecation of `NodeFile`
                 # collection
                 file_data = database['nodefile'].find_one({'_id': version})
                 record = OsfStorageFileRecord.find_by_path(file_data['path'], node_settings)
-                migrate_version(idx, file_data, record)
+                migrate_version(idx, file_data, record, dry_run=dry_run)
             except Exception as error:
                 logger.error('Could not migrate object {0} on node {1}'.format(version, node._id))
                 logger.exception(error)
@@ -71,6 +70,8 @@ def main(dry_run=True):
 if __name__ == '__main__':
     import sys
     dry_run = 'dry' in sys.argv
+    if not dry_run:
+        script_utils.add_file_logger(logger, __file__)
     ensure_osf_files(settings)
     init_app(set_backends=True, routes=False)
     main(dry_run=dry_run)
