62b166cd952523c3ca7152bc317056f18453cc24
Steven Loria <sloria1@gmail.com>
2015-03-06 02:12:57 -0500

    Add sinon support and our first working JS test!

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,30 @@
+var webpack = require('webpack');
+var BowerWebpackPlugin = require('bower-webpack-plugin');
+var webpackCommon = require('./webpack.common.config.js');
+
+// Put in separate file?
+var webpackTestConfig = {
+    devtool: 'inline-source-map',
+    plugins: [
+       new webpack.ResolverPlugin(
+            new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin('bower.json', ['main'])
+        ),
+
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery',
+            'window.$': 'jquery'
+        }),
+    ],
+    resolve: webpackCommon.resolve
+};
+
+
 module.exports = function (config) {
     config.set({
         browsers: ['Chrome'],
-        frameworks: ['mocha'],
+        frameworks: ['mocha', 'sinon'],
         files: [
             // Only need to target one file, which will load all files in tests/ that
             // match *.test.js
@@ -12,9 +35,7 @@ module.exports = function (config) {
             // add webpack as preprocessor
             'website/static/js/tests/tests.webpack.js': ['webpack', 'sourcemap'],
         },
-        webpack: {
-            devtool: 'inline-source-map',
-        },
+        webpack: webpackTestConfig,
         webpackServer: {
             noInfo: true // don't spam the console
         },
@@ -23,7 +44,8 @@ module.exports = function (config) {
             require('karma-mocha'),
             require('karma-sourcemap-loader'),
             require('karma-chrome-launcher'),
-            require('karma-spec-reporter')
+            require('karma-spec-reporter'),
+            require('karma-sinon')
         ]
     });
 };
--- website/static/js/tests/osfHelpers.test.js
+++ website/static/js/tests/osfHelpers.test.js
@@ -1,10 +1,18 @@
-/*global describe, it, expect, example, beforeEach, mocha */
+/*global describe, it, expect, example, beforeEach, mocha, sinon*/
 'use strict';
 
 var assert = require('chai').assert;
+var $ = require('jquery');
 
-describe('root', function() {
-    it('is ok', function() {
-        assert.equal(2 + 2, 4);
+var $osf = require('../osfHelpers.js');
+
+describe('growl', function() {
+    it('calls $.growl with correct arguments', function() {
+        var spy = new sinon.spy($, 'growl');
+        $osf.growl('The one', 'the only', 'danger');
+        sinon.assert.calledOnce(spy);
+
+        sinon.assert.calledWith(spy,
+            {title: '<strong>The one<strong><br />', message: 'the only'});
     });
 });
