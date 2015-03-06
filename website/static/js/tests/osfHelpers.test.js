285156c99782820800b95bd100d8cc1a06c7b981
Steven Loria <sloria1@gmail.com>
2015-03-06 02:36:23 -0500

    Add tests for putJSON and postJSON

--- website/static/js/tests/osfHelpers.test.js
+++ website/static/js/tests/osfHelpers.test.js
@@ -1,4 +1,4 @@
-/*global describe, it, expect, example, beforeEach, mocha, sinon*/
+/*global describe, it, expect, example, beforeEach, afterEach, mocha, sinon*/
 'use strict';
 
 var assert = require('chai').assert;
@@ -16,3 +16,46 @@ describe('growl', function() {
             {title: '<strong>The one<strong><br />', message: 'the only'});
     });
 });
+
+describe('ajax helpers', function() {
+    var spy;
+    beforeEach(function() {
+        spy = new sinon.spy($, 'ajax');
+    });
+    afterEach(function() { spy.restore(); });
+
+    describe('postJSON', function() {
+        it('calls $.ajax with correct args', function() {
+            var url = '/foo';
+            var payload = {'bar': 42};
+            $osf.postJSON(url, payload);
+
+            sinon.assert.calledOnce(spy);
+            sinon.assert.calledWith(spy, {
+                url: url,
+                type: 'post',
+                data: JSON.stringify(payload),
+                contentType: 'application/json',
+                dataType: 'json'
+            });
+        });
+    });
+
+    describe('putJSON', function() {
+        it('calls $.ajax with correct args', function() {
+            var url = '/foo';
+            var payload = {'bar': 42};
+            $osf.putJSON(url, payload);
+
+            sinon.assert.calledOnce(spy);
+            sinon.assert.calledWith(spy, {
+                url: url,
+                type: 'put',
+                data: JSON.stringify(payload),
+                contentType: 'application/json',
+                dataType: 'json'
+            });
+        });
+    });
+});
+
