4c6369137d8daf409778a75bec6ac5bc34980734
harryrybacki <hrybacki@gmail.com>
2015-03-06 10:17:16 -0500

    Added initial tests
    Testing complete for models, views, and decorators. Will likely pull
    decorator testing into it's own module as neither decorator was
    previously tested and they touch swathes of the site. Search tests
    are stubbed and need to be completed.

---
+++ tests/test_registration_retractions.py
@@ -0,0 +1,213 @@
+"""Tests related to retraction of public registrations"""
+
+from nose.tools import *  # noqa
+
+from tests.base import OsfTestCase
+from tests.factories import ProjectFactory, NodeFactory, RegistrationFactory, UserFactory, AuthUserFactory
+
+import werkzeug
+from modularodm.exceptions import ValidationTypeError, ValidationValueError
+
+from framework.auth.core import Auth
+from framework.exceptions import HTTPError
+
+from website.project.decorators import must_be_valid_project
+
+
+class RegistrationRetractionModelsTestCase(OsfTestCase):
+    def setUp(self):
+        super(RegistrationRetractionModelsTestCase, self).setUp()
+        self.user = UserFactory()
+        self.consolidate_auth = Auth(user=self.user)
+        self.project = ProjectFactory(creator=self.user)
+        self.registration = RegistrationFactory(project=self.project)
+        self.justification = 'I loathe openness...'
+
+    def test_retract(self):
+        self.registration.is_public = True
+        self.registration.retract_registration(self.justification)
+
+        self.registration.reload()
+        assert_true(self.registration.is_retracted)
+        assert_equal(self.registration.retracted_justification, self.justification)
+
+    def test_retract_private_registration_throws_type_error(self):
+        with assert_raises(ValidationTypeError):
+            self.registration.retract_registration(self.justification)
+
+        self.registration.reload()
+        assert_false(self.registration.is_retracted)
+        assert_is_none(self.registration.retracted_justification)
+
+    def test_retract_public_non_registration_throws_type_error(self):
+        self.project.is_public = True
+        with assert_raises(ValidationTypeError):
+            self.project.retract_registration(self.justification)
+
+        self.registration.reload()
+        assert_false(self.registration.is_retracted)
+        assert_is_none(self.registration.retracted_justification)
+
+    def test_retract_invalid_justification_throw_value_error(self):
+        self.registration.is_public = True
+        with assert_raises(ValidationValueError):
+            self.registration.retract_registration("")
+        self.registration.reload()
+        assert_false(self.registration.is_retracted)
+        assert_is_none(self.registration.retracted_justification)
+
+
+class RegistrationRetractionViewsTestCase(OsfTestCase):
+    def setUp(self):
+        super(RegistrationRetractionViewsTestCase, self).setUp()
+        self.admin_user = AuthUserFactory()
+        self.admin_user.save()
+        self.auth = self.admin_user.auth
+        self.project = ProjectFactory(is_public=False, creator=self.admin_user)
+        self.registration = RegistrationFactory(project=self.project)
+        self.registration.is_public = True
+        self.registration.save()
+
+        self.retraction_url = self.registration.api_url_for('node_registration_retraction')
+        self.justification = 'I loathe openness...'
+
+    def test_retract_private_registration_raises_400(self):
+        self.registration.is_public = False
+        self.registration.save()
+
+        res = self.app.post_json(
+            self.retraction_url,
+             auth=self.auth,
+             expect_errors=True,
+        )
+
+        assert_equal(res.status_code, 400)
+        self.registration.reload()
+        assert_false(self.registration.is_retracted)
+        assert_is_none(self.registration.retracted_justification)
+
+    def test_non_admin_retract_raises_401(self):
+        res = self.app.post_json(self.retraction_url, expect_errors=True)
+
+        assert_equals(res.status_code, 401)
+        self.registration.reload()
+        assert_false(self.registration.is_retracted)
+        assert_is_none(self.registration.retracted_justification)
+
+    def test_retract_without_justification_raises_400(self):
+        res = self.app.post_json(
+            self.retraction_url,
+             {'justification': ''},
+             auth=self.auth,
+             expect_errors=True,
+        )
+
+        assert_equal(res.status_code, 400)
+        self.registration.reload()
+        assert_false(self.registration.is_retracted)
+        assert_is_none(self.registration.retracted_justification)
+
+    def test_redirect_if_retracted(self):
+        expected_redirect_url = self.registration.web_url_for('view_project')
+        res = self.app.post_json(
+            self.retraction_url,
+             {'justification': self.justification},
+             auth=self.auth,
+             expect_errors=True,
+        )
+
+        assert_equal(res.status_code, 200)
+        assert_equal(res.json['redirectUrl'], expected_redirect_url)
+        self.registration.reload()
+        assert_true(self.registration.is_retracted)
+        assert_equal(self.registration.retracted_justification, self.justification)
+
+
+class RegistrationRetractionSearchTestCase(OsfTestCase):
+
+    def test_retract_wiki_not_in_search(self):
+        pass
+
+    def test_retract_search_indicator_in_results(self):
+        pass
+
+
+@must_be_valid_project
+def valid_project_helper(**kwargs):
+    return kwargs
+
+
+@must_be_valid_project(are_retractions_valid=True)
+def as_factory_allow_retractions(**kwargs):
+    return kwargs
+
+
+@must_be_valid_project(are_retractions_valid=False)
+def as_factory_do_not_allow_retractions(**kwargs):
+    return kwargs
+
+
+class TestValidProject(OsfTestCase):
+
+    def setUp(self):
+        super(TestValidProject, self).setUp()
+        self.project = ProjectFactory()
+        self.node = NodeFactory(project=self.project)
+
+    def test_populates_kwargs_project(self):
+        res = valid_project_helper(pid=self.project._id)
+        assert_equal(res['project'], self.project)
+        assert_is_none(res['node'])
+
+    def test_populates_kwargs_node(self):
+        res = valid_project_helper(pid=self.project._id, nid=self.node._id)
+        assert_equal(res['project'], self.project)
+        assert_equal(res['node'], self.node)
+
+    def test_project_not_found(self):
+        with assert_raises(HTTPError) as exc_info:
+            valid_project_helper(pid='fakepid')
+        assert_equal(exc_info.exception.code, 404)
+
+    def test_project_category_mismatch(self):
+        with assert_raises(HTTPError) as exc_info:
+            valid_project_helper(pid=self.node._id)
+        assert_equal(exc_info.exception.code, 400)
+
+    def test_project_deleted(self):
+        self.project.is_deleted = True
+        self.project.save()
+        with assert_raises(HTTPError) as exc_info:
+            valid_project_helper(pid=self.project._id)
+        assert_equal(exc_info.exception.code, 410)
+
+    def test_node_not_found(self):
+        with assert_raises(HTTPError) as exc_info:
+            valid_project_helper(pid=self.project._id, nid='fakenid')
+        assert_equal(exc_info.exception.code, 404)
+
+    def test_node_deleted(self):
+        self.node.is_deleted = True
+        self.node.save()
+        with assert_raises(HTTPError) as exc_info:
+            valid_project_helper(pid=self.project._id, nid=self.node._id)
+        assert_equal(exc_info.exception.code, 410)
+
+    def test_valid_project_as_factory_allow_retractions_is_retracted(self):
+        self.project.is_registration = True
+        self.project.is_retracted = True
+        self.project.save()
+        res = as_factory_allow_retractions(pid=self.project._id)
+        assert_equal(res['project'], self.project)
+
+    def test_valid_project_as_factory_disallow_retractions_is_retracted(self):
+        self.project.is_registration = True
+        self.project.is_retracted = True
+        self.project.save()
+        res = as_factory_do_not_allow_retractions(pid=self.project._id)
+        assert_true(isinstance(res, werkzeug.Response))
+        assert_equal(res.status_code, 302)
+        assert_equal(
+            res.headers['Location'],
+            self.project.web_url_for('node_registration_retracted'),
+        )
