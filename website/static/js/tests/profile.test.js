6b60c13c888885f80e1b98e73c6c0a6875a508d9
Steven Loria <sloria1@gmail.com>
2015-03-07 18:48:28 -0500

    Initial tests for profile._NameViewModel

--- website/static/js/tests/profile.test.js
+++ website/static/js/tests/profile.test.js
@@ -7,34 +7,6 @@ var faker = require('faker');
 var utils = require('./utils');
 var profile = require('../profile');
 
-var assign = require('object-assign');
-
-/**
- * Utility to create a fake server with sinon.
- */
-var defaultHeaders = {'Content-Type': 'application/json'};
-function createServer(endpoints) {
-    var server = sinon.fakeServer.create();
-    for (var url in endpoints) {
-        var endpoint = endpoints[url];
-        var headers = assign(
-            {},
-            defaultHeaders,
-            endpoints.headers
-        );
-        server.respondWith(
-            endpoint.method || 'GET',
-            url,
-            [
-                endpoint.status || 200,
-                headers,
-                JSON.stringify(endpoint.response)
-            ]
-        );
-    }
-    return server;
-}
-
 // Add sinon asserts to chai.assert, so we can do assert.calledWith instead of sinon.assert.calledWith
 sinon.assert.expose(assert, {prefix: ''});
 
@@ -44,7 +16,7 @@ describe('ViewModels', () => {
         crud: '/settings/names/',
         impute: '/settings/names/impute/'
     };
-    var vm, server;
+    var server;
 
     var names = {
         full: faker.name.findName(),
@@ -53,20 +25,20 @@ describe('ViewModels', () => {
         family: faker.name.lastName(),
         suffix: faker.name.suffix()
     };
+    var imputedNames = {
+        given: faker.name.firstName(),
+        middle: [faker.name.lastName()],
+        family: faker.name.lastName(),
+        suffix: faker.name.suffix()
+    };
 
     before(() => {
         // Set up fake server
-        var endpoints = {};
-
-        // Set up responses for names URLs
-        endpoints[nameURLs.crud]  = {
-            response: names
-        };
-        endpoints[nameURLs.impute] = {
-            response: names
-        };
-
-        server = utils.createServer(endpoints);
+        var endpoints = [
+            {url: nameURLs.crud, response: names},
+            {url: /\/settings\/names\/impute\/.+/, response: imputedNames}
+        ];
+        server = utils.createServer(sinon, endpoints);
     });
 
     after(() => {
@@ -75,8 +47,14 @@ describe('ViewModels', () => {
 
 
     describe('NameViewModel', () => {
+        var vm;
+        beforeEach(() => {
+            vm = new profile._NameViewModel(nameURLs, ['view', 'edit'], false);
+            server.respond(); // initialization fetches initial data
+        });
+
         it('should fetch and update names upon instantiation', (done) => {
-            var vm = new profile._NameViewModel(nameURLs, ['view', 'edit'], function() {
+            var vm = new profile._NameViewModel(nameURLs, ['view', 'edit'], false, function() {
                 // Observables have been updated
                 assert.equal(this.full(), names.full);
                 assert.equal(this.given(), names.given);
@@ -84,13 +62,16 @@ describe('ViewModels', () => {
                 assert.equal(this.suffix(), names.suffix);
                 done();
             });
-            window.onbeforeunload = null; // hack to disable BaseViewModel's onbeforeunload handler
             server.respond();
         });
 
         describe('impute', () => {
-            it('should send request and update imputed names', () => {
-
+            it('should send request and update imputed names', (done) => {
+                vm.impute(function() {
+                    assert.equal(vm.given(), imputedNames.given);
+                    done();
+                });
+                server.respond();
             });
         });
     });
