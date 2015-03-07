d48012fa631035957572add09999aef9b46b22d7
Steven Loria <sloria1@gmail.com>
2015-03-07 11:06:49 -0500

    Expand test coverage of osfHelpers

--- website/static/js/tests/osfHelpers.test.js
+++ website/static/js/tests/osfHelpers.test.js
@@ -1,12 +1,14 @@
-/*global describe, it, expect, example, beforeEach, afterEach, mocha, sinon*/
+/*global describe, it, expect, example, before, after, beforeEach, afterEach, mocha, sinon*/
 'use strict';
-
 var assert = require('chai').assert;
-// Add sinon asserts to chai.assert, so we can do assert.calledWith instead of sinon.assert.calledWith
-sinon.assert.expose(assert, {prefix: ''});
 var $ = require('jquery');
+var moment = require('moment');
+var Raven = require('raven-js');
 
 var $osf = require('../osfHelpers.js');
+
+// Add sinon asserts to chai.assert, so we can do assert.calledWith instead of sinon.assert.calledWith
+sinon.assert.expose(assert, {prefix: ''});
 
 describe('growl', () => {
     it('calls $.growl with correct arguments', () => {
@@ -16,6 +18,97 @@ describe('growl', () => {
 
         assert.calledWith(spy,
             {title: '<strong>The one<strong><br />', message: 'the only'});
+        spy.restore();
+    });
+});
+
+describe('handleJSONError', () => {
+    var response = {
+        responseJSON: {message_short: 'Oh no!', message_long: 'Something went wrong'}
+    };
+    it('uses the response body if available', () => {
+        var spy = new sinon.spy($osf, 'growl');
+        $osf.handleJSONError(response);
+        assert.called(spy);
+    });
+
+    it('logs error with Raven', () => {
+        var spy = new sinon.spy(Raven, 'captureMessage');
+        $osf.handleJSONError(response);
+        assert.called(spy);
+    });
+});
+
+describe('block', () => {
+    it('calls $.blockUI with correct arguments', () => {
+        var spy = new sinon.spy($, 'blockUI');
+        $osf.block();
+        assert.calledOnce(spy);
+        assert.calledWith(spy, {
+            css: {
+                border: 'none',
+                padding: '15px',
+                backgroundColor: '#000',
+                '-webkit-border-radius': '10px',
+                '-moz-border-radius': '10px',
+                opacity: 0.5,
+                color: '#fff'
+            },
+            message: 'Please wait'
+        });
+    });
+});
+
+describe('unblock', () => {
+    it('calls unblockUI', () => {
+        var spy = new sinon.spy($, 'unblockUI');
+        $osf.unblock();
+        assert.calledOnce(spy);
+    });
+});
+
+describe('mapByProperty', () => {
+    it('returns the given property for every item in a list of objects', () => {
+        var fixture = [{foo: 42}, {foo: 24}, {foo: 424, bar:242}, {bar: 2424}];
+        var ret = $osf.mapByProperty(fixture, 'foo');
+        assert.deepEqual(ret, [42, 24, 424]);
+    });
+});
+
+describe('isEmail', () => {
+    it('returns true for valid emails', () => {
+        var emails = [
+            'niceandsimple@example.com',
+            'NiCeAnDsImPlE@eXaMpLe.CoM',
+            'very.common@example.com',
+            'a.little.lengthy.but.fine@a.iana-servers.net',
+            'disposable.style.email.with+symbol@example.com',
+            '"very.unusual.@.unusual.com"@example.com',
+            "!#$%&'*+-/=?^_`{}|~@example.org", // jshint ignore: line
+        ];
+        emails.forEach((email) => {
+            assert.isTrue($osf.isEmail(email), email + ' not recognized as a valid email');
+        });
+    });
+
+    it('returns false for invalid emails', () => {
+        var invalids = [
+            'a"b(c)d,e:f;g<h>i[j\\k]l@example.com',
+            'just"not"right@example.com',
+            'this is"not\allowed@example.com',
+            'this\\ still\\"not\\\\allowed@example.com',
+            '"very.(),:;<>[]\".VERY.\"very@\\ \"very\".unusual"@strange.example.com',
+            'user@example',
+            '@nouser.com',
+            'example.com',
+            'user',
+            '',
+            null,
+            undefined
+        ];
+        invalids.forEach((invalid) => {
+            assert.isFalse($osf.isEmail(invalid), invalid + ' not recognized as a invalid email');
+        });
     });
 });
 
@@ -65,3 +158,29 @@ describe('ajax helpers', () => {
     });
 });
 
+describe('urlParams', () => {
+    it('should parse query params', () => {
+        assert.deepEqual($osf.urlParams('?foo=bar'), {foo: 'bar'});
+        assert.deepEqual($osf.urlParams('?foo=bar'), {foo: 'bar'});
+        assert.deepEqual($osf.urlParams('?foo=bar&baz=42'), {foo: 'bar', baz: '42'});
+    });
+});
+
+describe('htmlEscape', () => {
+    it('should escape html entities', () => {
+        assert.equal($osf.htmlEscape('safe'), 'safe');
+        assert.equal($osf.htmlEscape('<b>'), '&lt;b&gt;');
+        assert.equal($osf.htmlEscape('<script>alert("lol")</script>'), '&lt;script&gt;alert("lol")&lt;/script&gt;');
+    });
+});
+
+describe('FormattableDate', () => {
+    it('should have local and utc time', () => {
+        var date = new Date();
+        var fd = new $osf.FormattableDate(date);
+        var expectedLocal = moment(date).format('YYYY-MM-DD hh:mm A');
+        assert.equal(fd.local, expectedLocal);
+        var expectedUTC = moment.utc(date).format('YYYY-MM-DD HH:mm UTC');
+        assert.equal(fd.utc, expectedUTC);
+    });
+});
