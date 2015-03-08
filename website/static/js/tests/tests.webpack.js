67113660c68cf9a164b110be6ed63cd136971db3
Steven Loria <sloria1@gmail.com>
2015-03-08 17:13:49 -0400

    Configure Raven with a fake DSN to prevent errors in output

--- website/static/js/tests/tests.webpack.js
+++ website/static/js/tests/tests.webpack.js
@@ -1,6 +1,10 @@
 // Generate a single webpack bundle for all tests for faster testing
 // See https://github.com/webpack/karma-webpack#alternative-usage
 
+// Configure raven with a fake DSN to avoid errors in test output
+var Raven = require('raven-js');
+Raven.config('http://abc@example.com:80/2');
+
 // Include all files that end with .test.js (core tests)
 var context = require.context('.', true, /.*test\.js$/); //make sure you have your directory and regex test set correctly!
 context.keys().forEach(context);
