c057b1f570d8c20027aaa84246558012cacaa024
Sam Chrisinger <s.chrisinger@gmail.com>
2015-03-09 10:28:02 -0400

    Revert "More cleanup getting S3 node settings async and using knockout."
    This reverts commit 0f8c2de7d768f8878cbb5b9b1b620bb93cbd4f1e.

--- website/addons/s3/views/config.py
+++ website/addons/s3/views/config.py
@@ -151,9 +151,7 @@ def s3_get_node_settings(auth, node_addon, **kwargs):
 @must_not_be_registration
 def s3_bucket_list(auth, node_addon, user_addon, **kwargs):
 
-    return {
-        'buckets': get_bucket_drop_down(user_addon)
-    }
+    return get_bucket_drop_down(user_addon)
 
 @must_have_permission('write')
 @must_have_addon('s3', 'node')
