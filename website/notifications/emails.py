47d63ab3807f54156b76c98b8d0867fc5d446ee3
Saman Ehsan <se8ea@virginia.edu>
2015-03-11 16:04:31 -0400

    Fix direct comment replies in components
    Only use the "comment replies" template if
    the comment is a direct reply to the user's
    comment. Otherwise, use the general "comments"
    template.
    
    Fix in check_parent so that this check occurs
    for indirect project subscriptions as well.

--- website/notifications/emails.py
+++ website/notifications/emails.py
@@ -116,7 +116,7 @@ def check_parent(uid, event, node_subscribers, **context):
             for u in subscribed_users:
                 if u not in node_subscribers and node.has_permission(u, 'read'):
                     if notification_type != 'none':
-                        event = 'comment_replies' if target_user else event
+                        event = 'comment_replies' if target_user == u else event
                         send([u._id], notification_type, uid, event, **context)
                     node_subscribers.append(u)
 
