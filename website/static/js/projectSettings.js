0ad0ab4540be151b390109283ef9d93e97a32fba
Lyndsy Simon <lyndsy@lyndsysimon.com>
2015-03-13 18:02:08 -0400

    Add $osf.confirmDangerousACtion() to osf helpers; moved from project js.

--- website/static/js/projectSettings.js
+++ website/static/js/projectSettings.js
@@ -5,55 +5,6 @@ var $osf = require('osfHelpers');
 
 var ProjectSettings = {};
 
-/**
-*  returns a random name from this list to use as a confirmation string
-*/
-function randomScientist() {
-    var scientists = [
-    'Anning',
-    'Banneker',
-    'Cannon',
-    'Carver',
-    'Chappelle',
-    'Curie',
-    'Divine',
-    'Emeagwali',
-    'Fahlberg',
-    'Forssmann',
-    'Franklin',
-    'Herschel',
-    'Hodgkin',
-    'Hopper',
-    'Horowitz',
-    'Jemison',
-    'Julian',
-    'Kovalevsky',
-    'Lamarr',
-    'Lavoisier',
-    'Lovelace',
-    'Massie',
-    'McClintock',
-    'Meitner',
-    'Mitchell',
-    'Morgan',
-    'Nosek',
-    'Odum',
-    'Pasteur',
-    'Pauling',
-    'Payne',
-    'Pearce',
-    'Pollack',
-    'Rillieux',
-    'Sanger',
-    'Somerville',
-    'Tesla',
-    'Tyson',
-    'Turing'
-    ];
-
-    return scientists[Math.floor(Math.random() * scientists.length)];
-}
-
 
 // Request the first 5 contributors, for display in the deletion modal
 var contribs = [];
@@ -84,54 +35,40 @@ request.fail(function(xhr, textStatus, err) {
 *  Ignores case and whitespace
 */
 ProjectSettings.getConfirmationCode = function(nodeType) {
-    var key = randomScientist();
-    function successHandler(response) {
-        // Redirect to either the parent project or the dashboard
-        window.location.href = response.url;
-    }
-
-    var promptMsg = 'Are you sure you want to delete this ' + nodeType + '?' +
-        '<div class="bootbox-node-deletion-modal"><p>It will no longer be available to other contributors on the project.';
+    var message = '<p>It will no longer be available to other contributors on the project.';
 
     // It's possible that the XHR request for contributors has not finished before getting to this
     // point; only construct the HTML for the list of contributors if the contribs list is populated
-    var contribsMsg = '';
+    console.log(contribs);
     if (contribs.length) {
-        // Build contributor unordered list
-        var contriblist = '';
+        message += ' These include:<ul>';
         $.each(contribs, function(i, b){
-            contriblist += '<li>' + b.fullname + '</li>';
+            message += '<li>' + b.fullname + '</li>';
         });
-        contribsMsg = ' Contributors include:</p>' +
-            '<ol>' + contriblist +'</ol>' +
-            '<p style="font-weight: normal; font-size: medium; line-height: normal;">' +
-            ((moreContribs > 0) ? 'and <strong>' + moreContribs + '</strong> others.</p>' : '');
+        message += '</ul>';
+
+        if (moreContribs) {
+            message += '<p>and <strong>' + moreContribs + '</strong> others.';
+        }
+        message += '</p>';
     }
 
-    var promptMsgEnd = '<p style="font-weight: normal; font-size: medium; line-height: normal;">' +
-        'If you want to continue, type <strong>' + key + '</strong> and click OK.</p></div>';
-
-    var fullMsg = [promptMsg, contribsMsg, promptMsgEnd].join('');
-    bootbox.prompt(
-        fullMsg,
-        function(result) {
-            if (result != null) {
-                result = result.toLowerCase();
-            }
-            if ($.trim(result) === key.toLowerCase()) {
-                var request = $.ajax({
-                    type: 'DELETE',
-                    dataType: 'json',
-                    url: nodeApiUrl
-                });
-                request.done(successHandler);
-                request.fail($osf.handleJSONError);
-            } else if (result != null) {
-                $osf.growl('Incorrect confirmation',
-                    'The confirmation string you provided was incorrect. Please try again.');
-            }
+    $osf.confirmDangerousAction({
+        title: 'Are you sure you want to delete this ' + nodeType + '?',
+        message: message,
+        callback: function () {
+            var request = $.ajax({
+                type: 'DELETE',
+                dataType: 'json',
+                url: nodeApiUrl
+            });
+            request.done(function(response) {
+                // Redirect to either the parent project or the dashboard
+                window.location.href = response.url;
+            });
+            request.fail($osf.handleJSONError);
         }
-    );
+    });
 };
 
 module.exports = ProjectSettings;
