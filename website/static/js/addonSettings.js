a80b3d3849050eb2eafc99469730da129d72697b
Sam Chrisinger <s.chrisinger@gmail.com>
2015-03-18 13:35:35 -0400

    Use oop.defClass

--- website/static/js/addonSettings.js
+++ website/static/js/addonSettings.js
@@ -4,6 +4,7 @@ var $ = require('jquery');
 var ko = require('knockout');
 var bootbox = require('bootbox');
 var Raven = require('raven-js');
+var oop = require('js/oop');
 
 var ConnectedProject = function(data) {
     var self = this;
@@ -12,73 +13,76 @@ var ConnectedProject = function(data) {
     self.urls = data.urls;
 };
 
-var ExternalAccount = function(data) {
-    var self = this;
-    self.name = data.display_name;
-    self.id = data.id;
-    self.profileUrl = data.profile_url;
-    self.providerName = data.provider_name;
+var ExternalAccount = oop.defClass({
+    constructor: function(data) {
+        var self = this;
+        self.name = data.display_name;
+        self.id = data.id;
+        self.profileUrl = data.profile_url;
+        self.providerName = data.provider_name;
 
-    self.connectedNodes = ko.observableArray();
+        self.connectedNodes = ko.observableArray();
 
-    ko.utils.arrayMap(data.nodes, function(item) {
-        self.connectedNodes.push(new ConnectedProject(item));
-    });
-};
-ExternalAccount.prototype._deauthorizeNodeConfirm = function(node) {
-    var self = this;
-    var url = node.urls.deauthorize;
-    var request = $.ajax({
-            url: url,
-            type: 'DELETE'
-        })
-        .done(function(data) {
-            self.connectedNodes.remove(node);
-        })
-        .fail(function(xhr, status, error) {
-            Raven.captureMessage('Error deauthorizing node: ' + node.id, {
-                url: url,
-                status: status,
-                error: error
-            });
+        ko.utils.arrayMap(data.nodes, function(item) {
+            self.connectedNodes.push(new ConnectedProject(item));
         });
-};
-ExternalAccount.prototype.deauthorizeNode = function(node) {
-    var self = this;
-    bootbox.confirm({
-        title: 'Remove addon?',
-        message: 'Are you sure you want to remove the ' + self.providerName + ' authorization from this project?',
-        callback: function(confirm) {
-            if (confirm) {
-                self._deauthorizeNodeConfirm(node);
+    },
+    _deauthorizeNodeConfirm: function(node) {
+        var self = this;
+        var url = node.urls.deauthorize;
+        var request = $.ajax({
+                url: url,
+                type: 'DELETE'
+            })
+            .done(function(data) {
+                self.connectedNodes.remove(node);
+            })
+            .fail(function(xhr, status, error) {
+                Raven.captureMessage('Error deauthorizing node: ' + node.id, {
+                    url: url,
+                    status: status,
+                    error: error
+                });
+            });
+    },
+    deauthorizeNode: function(node) {
+        var self = this;
+        bootbox.confirm({
+            title: 'Remove addon?',
+            message: 'Are you sure you want to remove the ' + self.providerName + ' authorization from this project?',
+            callback: function(confirm) {
+                if (confirm) {
+                    self._deauthorizeNodeConfirm(node);
+                }
             }
-        }
-    });
-};
+        });
+    }
+});
 
-
-var OAuthAddonSettingsViewModel = function(name, displayName) {
-    var self = this;
-    self.name = name;
-    self.properName = displayName;
-    self.accounts = ko.observableArray();
-    self.message = ko.observable('');
-    self.messageClass = ko.observable('');
-
-    self.setMessage = function(msg, cls) {
+var OAuthAddonSettingsViewModel = oop.defClass({
+    constructor: function(name, displayName) {
+        var self = this;
+        self.name = name;
+        self.properName = displayName;
+        self.accounts = ko.observableArray();
+        self.message = ko.observable('');
+        self.messageClass = ko.observable('');
+    },
+    setMessage: function(msg, cls) {
+        var self = this;
         self.message(msg);
         self.messageClass(cls || 'text-info');
-    };
-
-    self.connectAccount = function() {
+    },
+    connectAccount: function() {
+        var self = this;
         window.oauthComplete = function() {
             self.updateAccounts();
             self.setMessage('Add-on successfully authorized. To link this add-on to an OSF project, go to the settings page of the project, enable ' + self.properName + ', and choose content to connect.', '.text-success');
         };
         window.open('/oauth/connect/' + self.name + '/');
-    };
-
-    self.askDisconnect = function(account) {
+    },
+    askDisconnect: function(account) {
+        var self = this;
         bootbox.confirm({
             title: 'Delete account?',
             message: '<p class="overflow">' +
@@ -91,9 +95,9 @@ var OAuthAddonSettingsViewModel = function(name, displayName) {
                 }
             }
         });
-    };
-
-    self.disconnectAccount = function(account) {
+    },
+    disconnectAccount: function(account) {
+        var self = this;
         var url = '/api/v1/oauth/accounts/' + account.id + '/';
         var request = $.ajax({
             url: url,
@@ -110,9 +114,9 @@ var OAuthAddonSettingsViewModel = function(name, displayName) {
             });
         });
         return request;
-    };
-
-    self.updateAccounts = function() {
+    },
+    updateAccounts: function() {
+        var self = this;
         var url = '/api/v1/settings/' + self.name + '/accounts/';
         var request = $.get(url);
         request.done(function(data) {
@@ -128,8 +132,8 @@ var OAuthAddonSettingsViewModel = function(name, displayName) {
             });
         });
         return request;
-    };
-};
+    }
+});
 
 module.exports = {
     ConnectedProject: ConnectedProject,
