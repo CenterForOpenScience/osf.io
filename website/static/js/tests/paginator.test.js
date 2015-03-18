e6d76b9803842bbca375f2584f9015a1bd211164
Nan Chen <nc6r@virginia.edu>
2015-03-18 10:08:30 -0400

    prototype version

--- website/static/js/tests/paginator.test.js
+++ website/static/js/tests/paginator.test.js
@@ -6,38 +6,34 @@ var ko = require('knockout');
 var osfHelpers = require('osfHelpers');
 var Paginator = require('js/paginator');
 var oop = require('js/oop');
+var assert = require('chai').assert;
 
-describe('paginator', () => {
+describe('Paginator', () => {
+    var vm;
+    var numberOfPage = 5;
+    var currentPage = 1;
+    var paginators = [];
 
-    var numberofPages = function(){
-        return 5;
-    };
-
-    var currentPage = function(){
-        return 1;
-    };
-
-    var paginators = function(){
-        return [];
-    };
+    beforeEach(() => {
+        vm = new Paginator;
+    });
 
     it('nextPage', () => {
-        var newCurrentPage = Paginator.nextPage();
+        var newCurrentPage = vm.nextPage();
         assert.equal(newCurrentPage, currentPage + 1);
     });
 
     it('previousPage', () => {
-        var newCurrentPage = Paginator.previousPage();
+        var newCurrentPage = vm.previousPage();
         assert.equal(newCurrentPage, currentPage - 1);
     });
 
     it('search', () => {
-        var error = Paginator.previousPage();
-        assert.equal(error, 'Paginator subclass must define a "search" method.');
+        assert.throw(vm.previousPage(), 'Paginator subclass must define a "search" method.');
     });
 
     it('addNewPaginator', () => {
-        var newPaginator = Paginator.addNewPaginators();
+        var newPaginator = vm.addNewPaginators();
         assert.equal(paginators.length(), 1);
     });
 });
