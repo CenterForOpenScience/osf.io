875f46cd8152e1584dd99facba0bfd67fbc81a33
Chris Seto <chriskseto@gmail.com>
2015-03-19 13:03:26 -0400

    Mount FolderHandler as /folders

--- waterbutler/server/app.py
+++ waterbutler/server/app.py
@@ -7,6 +7,7 @@ from waterbutler import settings
 from waterbutler.core.utils import AioSentryClient
 from waterbutler.server.handlers import crud
 from waterbutler.server.handlers import status
+from waterbutler.server.handlers import folders
 from waterbutler.server.handlers import metadata
 from waterbutler.server.handlers import revisions
 from waterbutler.server import settings as server_settings
@@ -19,6 +20,7 @@ def make_app(debug):
             (r'/file', crud.CRUDHandler),
             (r'/data', metadata.MetadataHandler),
             (r'/status', status.StatusHandler),
+            (r'/folders', folders.FolderHandler),
             (r'/revisions', revisions.RevisionHandler),
         ],
         debug=debug,
