58c036d9e15c4e3699177b242233e083cbb809fd
Sam Chrisinger <s.chrisinger@gmail.com>
2015-03-23 11:44:31 -0400

    Fix dataverseNodeConfig formatting

--- website/addons/dataverse/static/dataverseNodeConfig.js
+++ website/addons/dataverse/static/dataverseNodeConfig.js
@@ -12,183 +12,183 @@ ko.punches.enableAll();
 
 function ViewModel(url) {
     var self = this;
-        self.url = url;
-        self.urls = ko.observable();
-        self.dataverseUsername = ko.observable();
-        self.dataversePassword = ko.observable();
+    self.url = url;
+    self.urls = ko.observable();
+    self.dataverseUsername = ko.observable();
+    self.dataversePassword = ko.observable();
 
-        self.ownerName = ko.observable();
-        self.nodeHasAuth = ko.observable(false);
-        self.userHasAuth = ko.observable(false);
-        self.userIsOwner = ko.observable(false);
-        self.connected = ko.observable(false);
-        self.loadedSettings = ko.observable(false);
-        self.loadedStudies = ko.observable(false);
-        self.submitting = ko.observable(false);
+    self.ownerName = ko.observable();
+    self.nodeHasAuth = ko.observable(false);
+    self.userHasAuth = ko.observable(false);
+    self.userIsOwner = ko.observable(false);
+    self.connected = ko.observable(false);
+    self.loadedSettings = ko.observable(false);
+    self.loadedStudies = ko.observable(false);
+    self.submitting = ko.observable(false);
 
-        self.dataverses = ko.observableArray([]);
-        self.studies = ko.observableArray([]);
-        self.badStudies = ko.observableArray([]);
+    self.dataverses = ko.observableArray([]);
+    self.studies = ko.observableArray([]);
+    self.badStudies = ko.observableArray([]);
 
-        self.savedStudyHdl = ko.observable();
-        self.savedStudyTitle = ko.observable();
-        self.savedDataverseAlias = ko.observable();
-        self.savedDataverseTitle = ko.observable();
-        self.studyWasFound = ko.observable(false);
+    self.savedStudyHdl = ko.observable();
+    self.savedStudyTitle = ko.observable();
+    self.savedDataverseAlias = ko.observable();
+    self.savedDataverseTitle = ko.observable();
+    self.studyWasFound = ko.observable(false);
 
-        self.messages = {
-            USER_SETTINGS_ERROR: ko.pureComputed(function() {
-                return 'Could not retrieve settings. Please refresh the page or ' +
-                    'contact <a href="mailto: support@osf.io">support@osf.io</a> if the ' +
-                    'problem persists.';
-            }),
-            CONFIRM_NODE_DEAUTH: ko.pureComputed(function() {
-                return 'Are you sure you want to unlink this Dataverse account? This will ' +
-                    'revoke the ability to view, download, modify, and upload files ' +
-                    'to studies on the Dataverse from the OSF. This will not remove your ' +
-                    'Dataverse authorization from your <a href="' + self.urls().settings + '">user settings</a> ' +
-                    'page.';
-            }),
-            CONFIRM_IMPORT_AUTH: ko.pureComputed(function() {
-                return 'Are you sure you want to authorize this project with your Dataverse credentials?';
-            }),
-            DEAUTH_ERROR: ko.pureComputed(function() {
-                return 'Could not unlink Dataverse at this time. Please refresh the page or ' +
-                    'contact <a href="mailto: support@osf.io">support@osf.io</a> if the ' +
-                    'problem persists.';
-            }),
-            DEAUTH_SUCCESS: ko.pureComputed(function() {
-                return 'Successfully unlinked your Dataverse account.';
-            }),
-            AUTH_INVALID: ko.pureComputed(function() {
-                return 'Your Dataverse username or password is invalid.';
-            }),
-            IMPORT_AUTH_SUCCESS: ko.pureComputed(function() {
-                return 'Successfully linked your Dataverse account';
-            }),
-            IMPORT_AUTH_ERROR: ko.pureComputed(function() {
-                return 'There was a problem connecting to the Dataverse. Please refresh the page or ' +
-                    'contact <a href="mailto: support@osf.io">support@osf.io</a> if the ' +
-                    'problem persists.';
-            }),
-            STUDY_DAACCESSIONED: ko.pureComputed(function() {
-                return 'This study has already been deaccessioned on the Dataverse ' +
-                    'and cannot be connected to the OSF.';
-            }),
-            FORBIDDEN_CHARACTERS: ko.pureComputed(function() {
-                return 'This study cannot be connected due to forbidden characters ' +
-                    'in one or more of the study\'s file names. This issue has been forwarded to our ' +
-                    'development team.';
-            }),
-            SET_INFO_SUCCESS: ko.pureComputed(function() {
-                var filesUrl = window.contextVars.node.urls.web + 'files/';
-                return 'Successfully linked study \'' + self.savedStudyTitle() + '\'. Go to the <a href="' +
-                    filesUrl + '">Files page</a> to view your content.';
-            }),
-            SET_STUDY_ERROR: ko.pureComputed(function() {
-                return 'Could not connect to this study. Please refresh the page or ' +
-                    'contact <a href="mailto: support@osf.io">support@osf.io</a> if the ' +
-                    'problem persists.';
-            }),
-            GET_STUDIES_ERROR: ko.pureComputed(function() {
-                return 'Could not load studies. Please refresh the page or ' +
-                    'contact <a href="mailto: support@osf.io">support@osf.io</a> if the ' +
-                    'problem persists.';
-            })
-        };
+    self.messages = {
+        USER_SETTINGS_ERROR: ko.pureComputed(function() {
+            return 'Could not retrieve settings. Please refresh the page or ' +
+                'contact <a href="mailto: support@osf.io">support@osf.io</a> if the ' +
+                'problem persists.';
+        }),
+        CONFIRM_NODE_DEAUTH: ko.pureComputed(function() {
+            return 'Are you sure you want to unlink this Dataverse account? This will ' +
+                'revoke the ability to view, download, modify, and upload files ' +
+                'to studies on the Dataverse from the OSF. This will not remove your ' +
+                'Dataverse authorization from your <a href="' + self.urls().settings + '">user settings</a> ' +
+                'page.';
+        }),
+        CONFIRM_IMPORT_AUTH: ko.pureComputed(function() {
+            return 'Are you sure you want to authorize this project with your Dataverse credentials?';
+        }),
+        DEAUTH_ERROR: ko.pureComputed(function() {
+            return 'Could not unlink Dataverse at this time. Please refresh the page or ' +
+                'contact <a href="mailto: support@osf.io">support@osf.io</a> if the ' +
+                'problem persists.';
+        }),
+        DEAUTH_SUCCESS: ko.pureComputed(function() {
+            return 'Successfully unlinked your Dataverse account.';
+        }),
+        AUTH_INVALID: ko.pureComputed(function() {
+            return 'Your Dataverse username or password is invalid.';
+        }),
+        IMPORT_AUTH_SUCCESS: ko.pureComputed(function() {
+            return 'Successfully linked your Dataverse account';
+        }),
+        IMPORT_AUTH_ERROR: ko.pureComputed(function() {
+            return 'There was a problem connecting to the Dataverse. Please refresh the page or ' +
+                'contact <a href="mailto: support@osf.io">support@osf.io</a> if the ' +
+                'problem persists.';
+        }),
+        STUDY_DAACCESSIONED: ko.pureComputed(function() {
+            return 'This study has already been deaccessioned on the Dataverse ' +
+                'and cannot be connected to the OSF.';
+        }),
+        FORBIDDEN_CHARACTERS: ko.pureComputed(function() {
+            return 'This study cannot be connected due to forbidden characters ' +
+                'in one or more of the study\'s file names. This issue has been forwarded to our ' +
+                'development team.';
+        }),
+        SET_INFO_SUCCESS: ko.pureComputed(function() {
+            var filesUrl = window.contextVars.node.urls.web + 'files/';
+            return 'Successfully linked study \'' + self.savedStudyTitle() + '\'. Go to the <a href="' +
+                filesUrl + '">Files page</a> to view your content.';
+        }),
+        SET_STUDY_ERROR: ko.pureComputed(function() {
+            return 'Could not connect to this study. Please refresh the page or ' +
+                'contact <a href="mailto: support@osf.io">support@osf.io</a> if the ' +
+                'problem persists.';
+        }),
+        GET_STUDIES_ERROR: ko.pureComputed(function() {
+            return 'Could not load studies. Please refresh the page or ' +
+                'contact <a href="mailto: support@osf.io">support@osf.io</a> if the ' +
+                'problem persists.';
+        })
+    };
 
-        self.savedStudyUrl = ko.pureComputed(function() {
-            return (self.urls()) ? self.urls().studyPrefix + self.savedStudyHdl() : null;
-        });
-        self.savedDataverseUrl = ko.pureComputed(function() {
-            return (self.urls()) ? self.urls().dataversePrefix + self.savedDataverseAlias() : null;
-        });
+    self.savedStudyUrl = ko.pureComputed(function() {
+        return (self.urls()) ? self.urls().studyPrefix + self.savedStudyHdl() : null;
+    });
+    self.savedDataverseUrl = ko.pureComputed(function() {
+        return (self.urls()) ? self.urls().dataversePrefix + self.savedDataverseAlias() : null;
+    });
 
-        self.selectedDataverseAlias = ko.observable();
-        self.selectedStudyHdl = ko.observable();
-        self.selectedDataverseTitle = ko.pureComputed(function() {
-            for (var i = 0; i < self.dataverses().length; i++) {
-                var data = self.dataverses()[i];
-                if (data.alias === self.selectedDataverseAlias()) {
-                    return data.title;
-                }
+    self.selectedDataverseAlias = ko.observable();
+    self.selectedStudyHdl = ko.observable();
+    self.selectedDataverseTitle = ko.pureComputed(function() {
+        for (var i = 0; i < self.dataverses().length; i++) {
+            var data = self.dataverses()[i];
+            if (data.alias === self.selectedDataverseAlias()) {
+                return data.title;
             }
-            return null;
-        });
-        self.selectedStudyTitle = ko.pureComputed(function() {
-            for (var i = 0; i < self.studies().length; i++) {
-                var data = self.studies()[i];
-                if (data.hdl === self.selectedStudyHdl()) {
-                    return data.title;
-                }
+        }
+        return null;
+    });
+    self.selectedStudyTitle = ko.pureComputed(function() {
+        for (var i = 0; i < self.studies().length; i++) {
+            var data = self.studies()[i];
+            if (data.hdl === self.selectedStudyHdl()) {
+                return data.title;
             }
-            return null;
-        });
-        self.dataverseHasStudies = ko.pureComputed(function() {
-            return self.studies().length > 0;
-        });
+        }
+        return null;
+    });
+    self.dataverseHasStudies = ko.pureComputed(function() {
+        return self.studies().length > 0;
+    });
 
-        self.showStudySelect = ko.pureComputed(function() {
-            return self.loadedStudies() && self.dataverseHasStudies();
-        });
-        self.showNoStudies = ko.pureComputed(function() {
-            return self.loadedStudies() && !self.dataverseHasStudies();
-        });
-        self.showLinkedStudy = ko.pureComputed(function() {
-            return self.savedStudyHdl();
-        });
-        self.showLinkDataverse = ko.pureComputed(function() {
-            return self.userHasAuth() && !self.nodeHasAuth() && self.loadedSettings();
-        });
-        self.credentialsChanged = ko.pureComputed(function() {
-            return self.nodeHasAuth() && !self.connected();
-        });
-        self.showInputCredentials = ko.pureComputed(function() {
-            return (self.credentialsChanged() && self.userIsOwner()) ||
-                (!self.userHasAuth() && !self.nodeHasAuth() && self.loadedSettings());
-        });
-        self.hasDataverses = ko.pureComputed(function() {
-            return self.dataverses().length > 0;
-        });
-        self.hasBadStudies = ko.pureComputed(function() {
-            return self.badStudies().length > 0;
-        });
-        self.showNotFound = ko.pureComputed(function() {
-            return self.savedStudyHdl() && self.loadedStudies() && !self.studyWasFound();
-        });
-        self.showSubmitStudy = ko.pureComputed(function() {
-            return self.nodeHasAuth() && self.connected() && self.userIsOwner();
-        });
-        self.enableSubmitStudy = ko.pureComputed(function() {
-            return !self.submitting() && self.dataverseHasStudies() &&
-                self.savedStudyHdl() !== self.selectedStudyHdl();
-        });
+    self.showStudySelect = ko.pureComputed(function() {
+        return self.loadedStudies() && self.dataverseHasStudies();
+    });
+    self.showNoStudies = ko.pureComputed(function() {
+        return self.loadedStudies() && !self.dataverseHasStudies();
+    });
+    self.showLinkedStudy = ko.pureComputed(function() {
+        return self.savedStudyHdl();
+    });
+    self.showLinkDataverse = ko.pureComputed(function() {
+        return self.userHasAuth() && !self.nodeHasAuth() && self.loadedSettings();
+    });
+    self.credentialsChanged = ko.pureComputed(function() {
+        return self.nodeHasAuth() && !self.connected();
+    });
+    self.showInputCredentials = ko.pureComputed(function() {
+        return (self.credentialsChanged() && self.userIsOwner()) ||
+            (!self.userHasAuth() && !self.nodeHasAuth() && self.loadedSettings());
+    });
+    self.hasDataverses = ko.pureComputed(function() {
+        return self.dataverses().length > 0;
+    });
+    self.hasBadStudies = ko.pureComputed(function() {
+        return self.badStudies().length > 0;
+    });
+    self.showNotFound = ko.pureComputed(function() {
+        return self.savedStudyHdl() && self.loadedStudies() && !self.studyWasFound();
+    });
+    self.showSubmitStudy = ko.pureComputed(function() {
+        return self.nodeHasAuth() && self.connected() && self.userIsOwner();
+    });
+    self.enableSubmitStudy = ko.pureComputed(function() {
+        return !self.submitting() && self.dataverseHasStudies() &&
+            self.savedStudyHdl() !== self.selectedStudyHdl();
+    });
 
-        // Flashed messages
-        self.message = ko.observable('');
-        self.messageClass = ko.observable('text-info');
+    // Flashed messages
+    self.message = ko.observable('');
+    self.messageClass = ko.observable('text-info');
 
-        // Update above observables with data from the server
-        $.ajax({
+    // Update above observables with data from the server
+    $.ajax({
+        url: url,
+        type: 'GET',
+        dataType: 'json'
+    }).done(function(response) {
+        // Update view model
+        self.updateFromData(response.result);
+        self.loadedSettings(true);
+    }).fail(function(xhr, textStatus, error) {
+        self.changeMessage(self.messages.USER_SETTINGS_ERROR, 'text-warning');
+        Raven.captureMessage('Could not GET dataverse settings', {
             url: url,
-            type: 'GET',
-            dataType: 'json'
-        }).done(function(response) {
-            // Update view model
-            self.updateFromData(response.result);
-            self.loadedSettings(true);
-        }).fail(function(xhr, textStatus, error) {
-            self.changeMessage(self.messages.USER_SETTINGS_ERROR, 'text-warning');
-            Raven.captureMessage('Could not GET dataverse settings', {
-                url: url,
-                textStatus: textStatus,
-                error: error
-            });
+            textStatus: textStatus,
+            error: error
         });
-    }
-    /**
-     * Update the view model from data returned from the server.
-     */
+    });
+};
+/**
+ * Update the view model from data returned from the server.
+ */
 
 ViewModel.prototype.updateFromData = function(data) {
     var self = this;
@@ -204,7 +204,7 @@ ViewModel.prototype.updateFromData = function(data) {
         self.savedDataverseAlias(data.savedDataverse.alias);
         self.savedDataverseTitle(data.savedDataverse.title);
         self.selectedDataverseAlias(data.savedDataverse.alias);
-        self.savedStudyHdl(data.savedStudy.hdl);
+        self.savendStudyHdl(data.savedStudy.hdl);
         self.savedStudyTitle(data.savedStudy.title);
         self.connected(data.connected);
         if (self.userIsOwner()) {
