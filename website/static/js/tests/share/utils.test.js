06ecd7760805f3976310c6cd61c7ebf8597c8733
Fabian von Feilitzsch <fabian@fabianism.us>
2015-03-25 15:48:27 -0400

    Finally making some progress on js tests

--- website/static/js/tests/share/utils.test.js
+++ website/static/js/tests/share/utils.test.js
@@ -5,39 +5,112 @@ var assert = require('chai').assert;
 var $osf = require('js/osfHelpers');
 
 var utils = require('js/share/utils');
+var testUtils = require('tests/utils');
 
 var noop = function(){};
 
 var resultsLoadingSpy = new sinon.spy();
 describe('share/utils', () => {
+    var query = '';
+    var sort = 'sort';
+
     var vm = {
         page: 0,
         sort: function(){
-            return 'sort';
+            return sort;
         },
         sortMap: {
             sort: 'sort'
         },
-        resultsLoading: resultsLoadingSpy
+        resultsLoading: resultsLoadingSpy,
+        query: function(){return query;},
+        optionalFilters: [],
+        requiredFilters: []
     };
 
     describe('#loadMore', () => {
+        var server;
         var stub;
+
+        var emptyResponse = {
+            count: 0,
+            time: 0,
+            results: []
+        };
+
+        var endpoints = [{
+            method: 'GET',
+            url: /\/api\/v1\/share\/.*/,
+            response: emptyResponse
+        }];
+
         before(() => {
+            query = '';
+
+            server = testUtils.createServer(sinon, endpoints);
             stub = sinon.stub(utils, 'buildQuery', function(){
-                return '';
+                return query;
             });
         });
         after(() => {
             utils.buildQuery.restore();
+            server.restore();
         });
 
-        it('returns a promise that resolves to null when buildQuery returns and empty query', (done) => {
+        it('returns a promise that resolves to null when buildQuery returns an empty query', (done) => {
             utils.loadMore(vm)
                 .then(function(data){
                     assert.isNull(data);
                     done();
                 });
         });
+
+        it('returns a promise that resolves to new data when called with a query', (done) => {
+            query = 'q=toast';
+            utils.loadMore(vm)
+                .then(function(data){
+                    assert.deepEqual(data, emptyResponse);
+                    done();
+                });
+        });
+    });
+
+    describe('#buildURLParams', () => {
+
+        afterEach(() => {
+            vm.optionalFilters = [];
+            vm.requiredFilters = [];
+            query = '';
+            sort = 'sort';
+        });
+
+        it('build url parameters of the form ?q=&required=&optional&sort=', () => {
+            query = '1';
+            vm.requiredFilters.push('2');
+            vm.optionalFilters.push('3');
+            sort = '4';
+            assert.deepEqual('q=1&required=2&optional=3&sort=4', utils.buildURLParams(vm));
+        });
+
+        it('doesn\'t display parameters unless they have meaningful values', () => {
+            query = '1';
+            vm.requiredFilters.push('2');
+            sort = '4';
+            assert.deepEqual('q=1&required=2&sort=4', utils.buildURLParams(vm));
+
+            vm.optionalFilters.push('3');
+            vm.requiredFilters = [];
+            assert.deepEqual('q=1&optional=3&sort=4', utils.buildURLParams(vm));
+
+            vm.requiredFilters.push('2');
+            sort = null;
+            assert.deepEqual('q=1&required=2&optional=3', utils.buildURLParams(vm));
+
+            sort = '4';
+            query = null;
+            assert.deepEqual('required=2&optional=3&sort=4', utils.buildURLParams(vm));
+
+        });
+
     });
 });
