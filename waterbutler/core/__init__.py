caab96114964a1c9154df67d97c66c701cede8d9
Chris Seto <chriskseto@gmail.com>
2015-04-01 13:01:39 -0400

    Allow make_provider to be imported from waterbutler core

--- waterbutler/core/__init__.py
+++ waterbutler/core/__init__.py
@@ -1,7 +1,7 @@
 from waterbutler.core.utils import async_retry
-from waterbutler.core.utils import backgrounded
+from waterbutler.core.utils import make_provider
 
 __all__ = [
-    'backgrounded',
-    'async_retry'
+    'async_retry',
+    'make_provider',
 ]
