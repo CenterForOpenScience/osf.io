9168742e45eb4fd2e3f814c99785afaa053875d0
Steven Loria <sloria1@gmail.com>
2015-04-05 18:08:52 -0400

    Remove test db

--- api/api/settings.py
+++ api/api/settings.py
@@ -101,12 +101,12 @@ WSGI_APPLICATION = 'api.api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Internationalization
