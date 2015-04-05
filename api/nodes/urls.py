98818b415d940abb2ef6a10f420f9640cef54546
Steven Loria <sloria1@gmail.com>
2015-04-05 19:10:50 -0400

    Filter nodes based on current user

--- api/nodes/urls.py
+++ api/nodes/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import include, url
+from django.conf.urls import url
 from api.nodes import views as node_views
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'api.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', node_views.NodeList.as_view()),
+    url(r'^$', node_views.NodeList.as_view(), name='node-list'),
 ]
--- api/nodes/views.py
+++ api/nodes/views.py
@@ -1,14 +1,18 @@
-from website.models import Node
 from rest_framework import generics, permissions
-from rest_framework.response import Response
 
 from .serializers import NodeSerializer
 
 class NodeList(generics.ListAPIView):
+    """Return a list of nodes. By default, a GET
+    will return a list of nodes the current user contributes
+    to.
+    """
     permission_classes = (
         permissions.IsAuthenticated,
     )
     serializer_class = NodeSerializer
 
     def get_queryset(self):
-        return Node.find()
+        user = self.request.user
+        # Return list of nodes that current user contributes to
+        return user.node__contributed
