c4e1ea11fc93c416dfe42cf88b5c791739322ad3
Caner Uguz <caneruguz@gmail.com>
2015-04-06 16:26:21 -0400

    Undo changes to profile-settings page. remove affix

--- website/static/js/pages/profile-settings-page.js
+++ website/static/js/pages/profile-settings-page.js
@@ -6,19 +6,3 @@ new profile.Names('#names', ctx.nameUrls, ['edit']);
 new profile.Social('#social', ctx.socialUrls, ['edit']);
 new profile.Jobs('#jobs', ctx.jobsUrls, ['edit']);
 new profile.Schools('#schools', ctx.schoolsUrls, ['edit']);
-
-// Reusable function to fix affix widths to columns.  
-function fixAffixWidth() {
-    $('.affix, .affix-top, .affix-bottom').each(function (){
-        var el = $(this);
-        var colsize = el.parent('.affix-parent').width();
-        el.outerWidth(colsize);
-    });
-}
-
-$(document).ready(function() {
-
-    $(window).resize(function (){ fixAffixWidth(); });
-    $('.profile-settings-page .panel').on('affixed.bs.affix', function(){ fixAffixWidth(); });
-
-});
