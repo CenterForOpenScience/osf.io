5957beb7f5313453a2958be5fbade250c83aae75
Chris Seto <chriskseto@gmail.com>
2015-04-06 09:38:14 -0400

    Move tests to scripts/tests

--- scripts/osfstorage/migrate_from_oldels.py
+++ scripts/osfstorage/migrate_from_oldels.py
@@ -1,7 +1,7 @@
 import copy
 import logging
 
-from modularodm import Q, fields
+from modularodm import Q
 from modularodm.exceptions import NoResultsFound
 
 from framework.mongo import database
@@ -82,78 +82,3 @@ if __name__ == '__main__':
     dry = 'dry' in sys.argv
     init_app(set_backends=True, routes=False)
     main(dry=dry)
-
-
-from framework.auth import Auth
-from tests.base import OsfTestCase
-from tests.factories import ProjectFactory
-
-class TestMigrateOldels(OsfTestCase):
-
-    def setUp(self):
-        super(TestMigrateOldels, self).setUp()
-        # Hack to avoid calling on add
-        self.osf_storage_on_add = model.OsfStorageNodeSettings.on_add
-        model.OsfStorageNodeSettings.on_add = lambda *_, **__: None
-
-        self.project = ProjectFactory()
-        self.user = self.project.creator
-        self.auth = Auth(user=self.user)
-
-        self.project.add_addon('osfstorage', None)
-        self.node_settings = self.project.get_addon('osfstorage')
-        tree, _ = oldels.OsfStorageFileTree.get_or_create('', self.node_settings)
-        tree.save()
-        self.node_settings.file_tree = tree
-        self.node_settings.save()
-        model.OsfStorageNodeSettings.on_add = self.osf_storage_on_add
-
-    def test_creates_root_node(self):
-        assert self.node_settings.root_node is None
-        migrate_node_settings(self.node_settings, dry=False)
-        assert self.node_settings.root_node is not None
-        assert not self.node_settings._dirty
-
-    def test_migrates_files(self):
-        names = []
-        for num in range(10):
-            names.append('DEAR GOD! {} CARPNADOS'.format(num))
-            oldels.OsfStorageFileRecord.get_or_create(names[-1], self.node_settings)
-
-        assert len(self.node_settings.file_tree.children) == 10
-
-        migrate_node_settings(self.node_settings, dry=False)
-        migrate_children(self.node_settings, dry=False)
-
-        children = self.node_settings.root_node.children
-
-        assert not self.node_settings._dirty
-        assert self.node_settings.root_node is not None
-        assert not self.node_settings.root_node._dirty
-
-        assert len(children) == 10
-
-        for child in children:
-            names.remove(child.name)
-
-        assert len(names) == 0
-
-    def test_migrates_guids(self):
-        names = []
-        for num in range(10):
-            names.append('DEAR GOD! {} CARPNADOS'.format(num))
-            guid = model.OsfStorageGuidFile(node=self.project, path=names[-1])
-            guid.save()
-            oldels.OsfStorageFileRecord.get_or_create(names[-1], self.node_settings)
-
-        assert len(model.OsfStorageGuidFile.find()) == 10
-
-        migrate_node_settings(self.node_settings, dry=False)
-        migrate_children(self.node_settings, dry=False)
-
-        guids = model.OsfStorageGuidFile.find()
-        paths = [x.path for x in model.OsfStorageFileNode.find(Q('kind', 'eq', 'file') & Q('node_settings', 'eq', self.node_settings))]
-        assert len(guids) == 10
-        for guid in guids:
-            paths.remove(guid.path)
-        assert len(paths) == 0
---
+++ scripts/tests/test_osfstorage_oldels.py
@@ -0,0 +1,81 @@
+import mock
+
+from modularodm import Q
+
+from framework.auth import Auth
+
+from tests.base import OsfTestCase
+from tests.factories import ProjectFactory
+
+from website.addons.osfstorage import model, oldels
+
+from scripts.osfstorage import migrate_from_oldels as migration
+
+
+class TestMigrateOldels(OsfTestCase):
+
+    def setUp(self):
+        super(TestMigrateOldels, self).setUp()
+
+        with mock.patch.object(model.OsfStorageNodeSettings, 'on_add'):
+            self.project = ProjectFactory()
+
+        self.user = self.project.creator
+        self.auth = Auth(user=self.user)
+
+        self.node_settings = self.project.get_addon('osfstorage')
+
+        tree, _ = oldels.OsfStorageFileTree.get_or_create('', self.node_settings)
+        tree.save()
+        self.node_settings.file_tree = tree
+        self.node_settings.save()
+
+    def test_creates_root_node(self):
+        assert self.node_settings.root_node is None
+        migration.migrate_node_settings(self.node_settings, dry=False)
+        assert self.node_settings.root_node is not None
+        assert not self.node_settings._dirty
+
+    def test_migrates_files(self):
+        names = []
+        for num in range(10):
+            names.append('DEAR GOD! {} CARPNADOS'.format(num))
+            oldels.OsfStorageFileRecord.get_or_create(names[-1], self.node_settings)
+
+        assert len(self.node_settings.file_tree.children) == 10
+
+        migration.migrate_node_settings(self.node_settings, dry=False)
+        migration.migrate_children(self.node_settings, dry=False)
+
+        children = self.node_settings.root_node.children
+
+        assert not self.node_settings._dirty
+        assert self.node_settings.root_node is not None
+        assert not self.node_settings.root_node._dirty
+
+        assert len(children) == 10
+
+        for child in children:
+            names.remove(child.name)
+
+        assert len(names) == 0
+
+    def test_migrates_guids(self):
+        names = []
+        for num in range(10):
+            names.append('DEAR GOD! {} CARPNADOS'.format(num))
+            guid = model.OsfStorageGuidFile(node=self.project, path=names[-1])
+            guid.save()
+            oldels.OsfStorageFileRecord.get_or_create(names[-1], self.node_settings)
+
+        assert len(model.OsfStorageGuidFile.find()) == 10
+
+        migration.migrate_node_settings(self.node_settings, dry=False)
+        migration.migrate_children(self.node_settings, dry=False)
+
+        guids = model.OsfStorageGuidFile.find()
+        paths = [x.path for x in model.OsfStorageFileNode.find(Q('kind', 'eq', 'file') & Q('node_settings', 'eq', self.node_settings))]
+        assert len(guids) == 10
+        for guid in guids:
+            paths.remove(guid.path)
+        assert len(paths) == 0
