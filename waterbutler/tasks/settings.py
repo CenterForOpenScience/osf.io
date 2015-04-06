ef82cc0e80db2d027ef761e76202867d18738cb8
Chris Seto <chriskseto@gmail.com>
2015-04-06 15:30:38 -0400

    read eager from settings and default to false

--- waterbutler/tasks/settings.py
+++ waterbutler/tasks/settings.py
@@ -17,16 +17,15 @@ BROKER_URL = config.get(
         os.environ.get('RABBITMQ_PORT_5672_TCP_PORT', ''),
     )
 )
+CELERY_ALWAYS_EAGER = config.get('CELERY_ALWAYS_EAGER', False)
 CELERY_RESULT_BACKEND = config.get('CELERY_RESULT_BACKEND', None)
 CELERY_DISABLE_RATE_LIMITS = config.get('CELERY_DISABLE_RATE_LIMITS', True)
 CELERY_TASK_RESULT_EXPIRES = config.get('CELERY_TASK_RESULT_EXPIRES', 60)
-# CELERY_ALWAYS_EAGER = config.get('CELERY_ALWAYS_EAGER', True)
 CELERY_IMPORTS = [
     entry.module_name
     for entry in iter_entry_points(group='waterbutler.providers.tasks', name=None)
 ]
 
-CELERY_ALWAYS_EAGER = True
 CELERY_EAGER_PROPAGATES_EXCEPTIONS = True
 REDIS_HOST = config.get('REDIS_HOST', 'localhost')
 REDIS_PORT = int(config.get('REDIS_PORT', 6379))
