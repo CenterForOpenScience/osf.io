1e5c593ef0dc38c12bd987f9b2f37f9bfc3c71e1
Steven Loria <sloria1@gmail.com>
2015-04-07 19:44:41 -0400

    Make paginated response items ordered

--- api/base/pagination.py
+++ api/base/pagination.py
@@ -1,14 +1,18 @@
+from collections import OrderedDict
+
 from rest_framework import pagination
 from rest_framework.response import Response
 
 class JSONAPIPagination(pagination.PageNumberPagination):
+    """Custom paginator that formats responses in a JSON-API compatible format."""
 
     def get_paginated_response(self, data):
-        return Response({
-            'links': {
-                'next': self.get_next_link(),
-                'previous': self.get_previous_link(),
-            },
-            # 'count': self.page.paginator.count,
-            'data': data
-        })
+        response_dict = OrderedDict([
+            ('data', data),
+            ('links', OrderedDict([
+                ('next', self.get_next_link()),
+                ('previous', self.get_previous_link()),
+            ])),
+            ('count', self.page.paginator.count),
+        ])
+        return Response(response_dict)
