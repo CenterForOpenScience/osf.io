98c1875d544cd3287b1df91f6216f57d09b93bdc
Chris Seto <chriskseto@gmail.com>
2015-04-07 10:51:29 -0400

    Send callback when finished moving

--- waterbutler/tasks/move.py
+++ waterbutler/tasks/move.py
@@ -1,17 +1,36 @@
-import asyncio
+import os
+import time
 
 from waterbutler.core import utils
 from waterbutler.tasks import core
-from waterbutler.tasks import settings
 
 @core.celery_task
-def move(src_bundle, dest_bundle):
+def move(src_bundle, dest_bundle, callback_url, auth):
     src_args, src_provider = src_bundle.pop('args'), utils.make_provider(**src_bundle.pop('provider'))
     dest_args, dest_provider = dest_bundle.pop('args'), utils.make_provider(**dest_bundle.pop('provider'))
 
-    core.ensure_event_loop().run_until_complete(
+    metadata, _ = core.ensure_event_loop().run_until_complete(
         src_provider.move(dest_provider, src_args, dest_args)
     )
+
+    if callback:
+        callback(metadata)
+
+    return (yield from utils.send_signed_request('PUT', callback_url, {
+        'action': 'move',
+        'source': {
+            'path': self.json['source']['path'],
+            'name': os.path.split(self.json['source']['path'])[1],
+            'provider': source_provider.NAME,
+        },
+        'destination': {
+            'path': data['path'],
+            'name': data['name'],
+            'provider': destination_provider.NAME,
+        },
+        'auth': auth['auth'],
+        'time': time.time() + 60
+    }))
     # dest_provider.move()
     # stream = src_provider.download(**src_args)
     # progress = stream.ProgressStreamWriter(stream.size)
