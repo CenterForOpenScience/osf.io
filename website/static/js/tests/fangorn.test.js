589a913c2fc7392eade25c21ccb3be2d39d60737
Caner Uguz <caneruguz@gmail.com>
2015-04-08 17:09:55 -0400

    Add initial tests to fangorn.test [skip ci]

--- website/static/js/tests/fangorn.test.js
+++ website/static/js/tests/fangorn.test.js
@@ -13,12 +13,39 @@ sinon.assert.expose(assert, {prefix: ''});
 
 var smallSample = [ {kind : 'folder', name : 'test folder'}, { kind : 'file', name : 'test file'}];
 
+var fangornInstance = function() {
+    var fan =  new Fangorn({
+        divID: 'fangorn',
+        filesData: smallSample
+    });
+    return fan.grid.tbController;
+};
+
 describe('fangornToolbars', () => {
 // Tests related to the toolbar
 	describe('toolbarOptions', () => {
-	// header template is defined 
+	  var fangorn; 
+	  beforeEach(function() {
+	    // runs before all tests in this block
+	    $('body').append('<div id="fangorn"></div>');
+	    fangorn = fangornInstance();
+	  });
+	  afterEach(function(){
+	    // runs after all tests in this block
+	    $('#fangorn').remove();
+	  });
+
+	  it('Should have fangorn toolbar template', function() {
+	  	assert.ok(fangorn.options.headerTemplate);
+	  });
+	  it('fangorn toolbar template should returns an object', function() {
+	  	var template = fangorn.options.headerTemplate.call(fangorn);
+	  	assert.equal(typeof template, 'object');
+	  });
+
 
 
 	});
 
 });
+  
