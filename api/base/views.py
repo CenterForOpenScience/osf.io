87778eec6425c9bc8ae80f6ad8a0264986d1e7c1
Steven Loria <sloria1@gmail.com>
2015-04-08 07:47:24 -0400

    Use UserSerializer to serialize current user in the root response

--- api/base/views.py
+++ api/base/views.py
@@ -2,6 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from .utils import absolute_reverse
+from api.users.serializers import UserSerializer
 
 @api_view(('GET',))
 def root(request, format=None):
@@ -9,15 +10,14 @@ def root(request, format=None):
     if request.user and not request.user.is_anonymous():
         user = request.user
         # TODO: Use user serializer
-        current_user = {
-            'id': user.pk,
-            'fullname': user.fullname,
-        }
+        current_user = UserSerializer(user).data
     else:
         current_user = None
     return Response({
-        'message': 'Welcome to the OSF API v2',
-        'current_user': current_user,
+        'meta': {
+            'message': 'Welcome to the OSF API v2',
+            'current_user': current_user,
+        },
         'links': {
             'nodes': absolute_reverse('nodes:node-list'),
             'users': absolute_reverse('users:user-list'),
