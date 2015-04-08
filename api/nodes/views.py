61dc5dcd644fcc54fe4af47643b73eab37b460a2
Steven Loria <sloria1@gmail.com>
2015-04-08 07:57:01 -0400

    Reduce duplication of node loading logic

--- api/nodes/views.py
+++ api/nodes/views.py
@@ -7,6 +7,20 @@ from .serializers import NodeSerializer
 from api.users.serializers import UserSerializer
 from .permissions import ContributorOrPublic, ReadOnlyIfRegistration
 
+
+class NodeMixin(object):
+    """Mixin with convenience methods for retrieving the current node based on the
+    current URL. By default, fetches the current node based on the pk kwarg.
+    """
+
+    node_lookup_url_kwarg = 'pk'
+
+    def get_node(self):
+        obj = get_object_or_404(Node, self.kwargs[self.node_lookup_url_kwarg])
+        # May raise a permission denied
+        self.check_object_permissions(self.request, obj)
+        return obj
+
 class NodeList(generics.ListCreateAPIView):
     """Return a list of nodes. By default, a GET
     will return a list of nodes the current user contributes
@@ -31,7 +45,7 @@ class NodeList(generics.ListCreateAPIView):
         serializer.save(creator=user)
 
 
-class NodeDetail(generics.RetrieveUpdateAPIView):
+class NodeDetail(generics.RetrieveUpdateAPIView, NodeMixin):
 
     permission_classes = (
         ContributorOrPublic,
@@ -40,20 +54,15 @@ class NodeDetail(generics.RetrieveUpdateAPIView):
     serializer_class = NodeSerializer
 
     # override
-    # TODO: Generalize this.
     def get_object(self):
-        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
-        obj = get_object_or_404(Node, self.kwargs[lookup_url_kwarg])
-        # May raise a permission denied
-        self.check_object_permissions(self.request, obj)
-        return obj
+        return self.get_node()
 
     # override
     def get_serializer_context(self):
         # Serializer needs the request in order to make an update to privacy
         return {'request': self.request}
 
-class NodeContributorsList(generics.ListAPIView):
+class NodeContributorsList(generics.ListAPIView, NodeMixin):
     """Return the contributors (users) fora node."""
 
     permissions_classes = (
@@ -63,21 +72,13 @@ class NodeContributorsList(generics.ListAPIView):
     serializer_class = UserSerializer
 
     def get_queryset(self):
-        # TODO: Duplication here. Rethink.
-        node = get_object_or_404(Node, self.kwargs['pk'])
-        # May raise a permission denied
-        self.check_object_permissions(self.request, node)
-        return node.visible_contributors
+        return self.get_node().visible_contributors
 
-class NodeRegistrationsList(generics.ListAPIView):
+class NodeRegistrationsList(generics.ListAPIView, NodeMixin):
     permissions_classes = (
         ContributorOrPublic,
     )
     serializer_class = NodeSerializer
 
     def get_queryset(self):
-        # TODO: Duplication here. Rethink.
-        node = get_object_or_404(Node, self.kwargs['pk'])
-        # May raise a permission denied
-        self.check_object_permissions(self.request, node)
-        return node.node__registrations
+        return self.get_node().node__registrations
