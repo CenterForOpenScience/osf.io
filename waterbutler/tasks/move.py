b0a7fbe608d99ebb2a76a50b1dd9ae970937a628
Chris Seto <chriskseto@gmail.com>
2015-04-09 15:17:33 -0400

    Clean up code

--- waterbutler/tasks/move.py
+++ waterbutler/tasks/move.py
@@ -4,33 +4,30 @@ import time
 from waterbutler.core import utils
 from waterbutler.tasks import core
 
+
 @core.celery_task
 def move(src_bundle, dest_bundle, callback_url, auth):
     src_args, src_provider = src_bundle.pop('args'), utils.make_provider(**src_bundle.pop('provider'))
     dest_args, dest_provider = dest_bundle.pop('args'), utils.make_provider(**dest_bundle.pop('provider'))
 
-    metadata, _ = core.ensure_event_loop().run_until_complete(
-        src_provider.move(dest_provider, src_args, dest_args)
-    )
-
-    if callback:
-        callback(metadata)
+    metadata, _ = yield from src_provider.move(dest_provider, src_args, dest_args)
 
     return (yield from utils.send_signed_request('PUT', callback_url, {
         'action': 'move',
         'source': {
-            'path': self.json['source']['path'],
-            'name': os.path.split(self.json['source']['path'])[1],
-            'provider': source_provider.NAME,
+            'path': src_args['path'],
+            'name': os.path.split(src_args['path'])[1],
+            'provider': src_provider.NAME,
         },
         'destination': {
-            'path': data['path'],
-            'name': data['name'],
-            'provider': destination_provider.NAME,
+            'path': metadata['path'],
+            'name': metadata['name'],
+            'provider': dest_provider.NAME,
         },
         'auth': auth['auth'],
         'time': time.time() + 60
     }))
+
     # dest_provider.move()
     # stream = src_provider.download(**src_args)
     # progress = stream.ProgressStreamWriter(stream.size)
