ec735d5c12fd526fc6cc3f21dfdda5a6552aebf3
Caner Uguz <caneruguz@gmail.com>
2015-04-09 11:11:27 -0400

    Complete initial functioning tests

--- website/static/js/tests/fangorn.test.js
+++ website/static/js/tests/fangorn.test.js
@@ -3,43 +3,64 @@
 
 var assert = require('chai').assert;
 var $ = require('jquery');
-var m = require('mithril');
-var URI = require('URIjs');
-var Treebeard = require('treebeard');
+// var m = require('mithril');
+// var URI = require('URIjs');
+// var Treebeard = require('treebeard');
 var Fangorn = require('js/fangorn');
 
 // Add sinon asserts to chai.assert, so we can do assert.calledWith instead of sinon.assert.calledWith
 sinon.assert.expose(assert, {prefix: ''});
 
-var smallSample = [ {kind : 'folder', name : 'test folder'}, { kind : 'file', name : 'test file'}];
+var smallSample = [
+	{ 
+		kind : 'folder', 
+		name : 'test folder',
+		permissions : {
+			view : true,
+			edit : true
+		},
+		downloads : 0
+	}, 
+	{ 
+		kind : 'file', 
+		name : 'test file',
+		permissions : {
+			view : true,
+			edit : true
+		},
+		downloads : 1
+	}
+];
 
 var fangornInstance = function() {
-    var fan =  new Fangorn({
+    var fangorn =  new Fangorn({
         divID: 'fangorn',
-        filesData: smallSample
+        filesData: smallSample,
+        uploads : false
     });
-    return fan.grid.tbController;
+    return fangorn;
 };
 
 describe('fangornToolbars', () => {
 // Tests related to the toolbar
 	describe('toolbarOptions', () => {
 	  var fangorn; 
-	  beforeEach(function() {
+	  before(function() {
 	    // runs before all tests in this block
 	    $('body').append('<div id="fangorn"></div>');
 	    fangorn = fangornInstance();
 	  });
-	  afterEach(function(){
+	  after(function(){
 	    // runs after all tests in this block
 	    $('#fangorn').remove();
+	    fangorn.grid.tbController.destroy();
 	  });
 
 	  it('Should have fangorn toolbar template', function() {
-	  	assert.ok(fangorn.options.headerTemplate);
+	  	assert.ok(fangorn.grid.tbController.options.headerTemplate);
 	  });
-	  it('fangorn toolbar template should returns an object', function() {
-	  	var template = fangorn.options.headerTemplate.call(fangorn);
+	  it('fangorn toolbar template should return an object', function() {
+	  	var template = fangorn.tests.fangornToolbar.call(fangorn.grid.tbController);
 	  	assert.equal(typeof template, 'object');
 	  });
 
