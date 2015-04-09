60eed2f30ad2d817e5159ca1893b7e682c1214f3
Sam Chrisinger <s.chrisinger@gmail.com>
2015-04-09 11:46:22 -0400

    WIP adding new tests

--- website/addons/twofactor/tests/test_views.py
+++ website/addons/twofactor/tests/test_views.py
@@ -1,10 +1,12 @@
 import mock
-from nose.tools import *
+from nose.tools import *  # noqa
 
 from tests.base import OsfTestCase
 from tests.factories import AuthUserFactory
 from website.app import init_app
 from website.addons.twofactor.tests import _valid_code
+
+from website.util import api_url_for
 
 app = init_app(
     routes=True,
@@ -20,10 +22,17 @@ class TestViews(OsfTestCase):
         self.user.add_addon('twofactor')
         self.user_settings = self.user.get_addon('twofactor')
 
+    def test_serialize_urls(self):
+        pass
+
+    def test_serialize_settings(self):
+        pass
+
     def test_confirm_code(self):
         # Send a valid code to the API endpoint for the user settings.
+        url = api_url_for('twofactor_settings_put')
         res = self.app.post_json(
-            '/api/v1/settings/twofactor/',
+            url,
             {'code': _valid_code(self.user_settings.totp_secret)},
             auth=self.user.auth
         )
@@ -35,8 +44,9 @@ class TestViews(OsfTestCase):
         assert_equal(res.status_code, 200)
 
     def test_confirm_code_failure(self):
-        res = self.app.post_json(
-            '/api/v1/settings/twofactor/',
+        url = api_url_for('twofactor_settings_put')
+        res = self.app.put_json(
+            url,
             {'code': '0000000'},
             auth=self.user.auth,
             expect_errors=True
