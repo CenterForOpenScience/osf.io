2e2303c62cb2d0ebf5793687c485f23733781a92
Chris Seto <chriskseto@gmail.com>
2015-04-09 15:15:12 -0400

    Better handling of async celery tasks

--- waterbutler/tasks/core.py
+++ waterbutler/tasks/core.py
@@ -21,8 +21,12 @@ def ensure_event_loop():
     return asyncio.get_event_loop()
 
 
-def __coroutine_unwrapper(func, *args, **kwargs):
-    return ensure_event_loop().run_until_complete(func(*args, **kwargs))
+def __coroutine_unwrapper(func):
+    @functools.wraps(func)
+    def wrapped(*args, **kwargs):
+        return ensure_event_loop().run_until_complete(func(*args, **kwargs))
+    wrapped.as_async = func
+    return wrapped
 
 
 def backgrounded(func, *args, **kwargs):
@@ -31,24 +35,27 @@ def backgrounded(func, *args, **kwargs):
     """
     loop = asyncio.get_event_loop()
     if asyncio.iscoroutinefunction(func):
-        args = (func, ) + args
-        func = __coroutine_unwrapper
+        func = __coroutine_unwrapper(func)
 
     return loop.run_in_executor(
         None,
         functools.partial(func, *args, **kwargs)
     )
 
+def backgroundify(func):
+    @asyncio.coroutine
+    @functools.wraps(func)
+    def wrapped(*args, **kwargs):
+        return (yield from backgrounded(func, *args, **kwargs))
+    return wrapped
 
-def celery_task(*args, **kwargs):
+
+def celery_task(func, *args, **kwargs):
     """A wrapper around Celery.task.
     When the wrapped method is called it will be called using
     Celery's Task.delay function and run in a background thread
     """
-    func = app.task(*args, **kwargs)
+    task = app.task(__coroutine_unwrapper(func), **kwargs)
+    task.adelay = backgroundify(task.delay)
 
-    @asyncio.coroutine
-    @functools.wraps(func)
-    def wrapped(*args, **kwargs):
-        return (yield from backgrounded(func.delay, *args, **kwargs))
-    return wrapped
+    return task
