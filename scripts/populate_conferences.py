12c3ea3c59b1ed05601b5e40331a057ad41f704f
jolene-esposito <jolene-esposito@users.noreply.github.com>
2015-04-09 11:57:01 -0400

    Change 'name' field
    Change 'name' field to spell out the name of several conferences

--- scripts/populate_conferences.py
+++ scripts/populate_conferences.py
@@ -20,7 +20,7 @@ def main():
 
 MEETING_DATA = {
     'spsp2014': {
-        'name': 'SPSP 2014',
+        'name': 'Society for Personality and Social Psychology 2014',
         'info_url': None,
         'logo_url': None,
         'active': False,
@@ -28,7 +28,7 @@ MEETING_DATA = {
         'public_projects': True,
     },
     'asb2014': {
-        'name': 'ASB 2014',
+        'name': 'Association of Southeastern Biologists 2014',
         'info_url': 'http://www.sebiologists.org/meetings/talks_posters.html',
         'logo_url': None,
         'active': False,
@@ -36,7 +36,7 @@ MEETING_DATA = {
         'public_projects': True,
     },
     'aps2014': {
-        'name': 'APS 2014',
+        'name': 'Association for Psychological Science 2014',
         'info_url': 'http://centerforopenscience.org/aps/',
         'logo_url': '/static/img/2014_Convention_banner-with-APS_700px.jpg',
         'active': False,
@@ -52,7 +52,7 @@ MEETING_DATA = {
         'public_projects': True,
     },
     'cpa2014': {
-        'name': 'CPA 2014',
+        'name': 'Canadian Psychological Association 2014',
         'info_url': None,
         'logo_url': None,
         'active': False,
@@ -60,7 +60,7 @@ MEETING_DATA = {
         'public_projects': True,
     },
     'filaments2014': {
-        'name': 'Filaments 2014',
+        'name': 'National Radio Astronomy Observatory Filaments 2014',
         'info_url': None,
         'logo_url': 'https://science.nrao.edu/science/meetings/2014/'
                     'filamentary-structure/images/filaments2014_660x178.png',
@@ -73,7 +73,7 @@ MEETING_DATA = {
         'public_projects': True,
     },
     'bitss2014': {
-        'name': 'BITSS Research Transparency Forum 2014',
+        'name': 'Berkeley Initiative for Transparency in the Social Sciences Research Transparency Forum 2014',
         'info_url': None,
         'logo_url': os.path.join(
             settings.STATIC_URL_PATH,
@@ -90,7 +90,7 @@ MEETING_DATA = {
         'public_projects': True,
     },
     'spsp2015': {
-        'name': 'SPSP 2015',
+        'name': 'Society for Personality and Social Psychology 2015',
         'info_url': None,
         'logo_url': 'http://spspmeeting.org/CMSPages/SPSPimages/spsp2015banner.jpg',
         'active': True,
@@ -100,7 +100,7 @@ MEETING_DATA = {
         ],
     },
     'aps2015': {
-        'name': 'APS 2015',
+        'name': 'Association for Psychological Science 2015',
         'info_url': None,
         'logo_url': 'http://www.psychologicalscience.org/images/APS_2015_Banner_990x157.jpg',
         'active': True,
@@ -110,7 +110,7 @@ MEETING_DATA = {
         'public_projects': True,
     },
     'icps2015': {
-        'name': 'ICPS 2015',
+        'name': 'International Convention of Psychological Science 2015',
         'info_url': None,
         'logo_url': 'http://icps.psychologicalscience.org/wp-content/themes/deepblue/images/ICPS_Website-header_990px.jpg',
         'active': True,
@@ -120,7 +120,7 @@ MEETING_DATA = {
         'public_projects': True,
     },
     'mpa2015': {
-        'name': 'MPA 2015',
+        'name': 'Midwestern Psychological Association 2015',
         'info_url': None,
         'logo_url': 'http://www.midwesternpsych.org/resources/Pictures/MPA%20logo.jpg',
         'active': True,
@@ -131,7 +131,7 @@ MEETING_DATA = {
         'public_projects': True,
     },
     'NCCC2015': {
-        'name': '2015 NC Cognition Conference',
+        'name': 'North Carolina Cognition Conference 2015',
         'info_url': None,
         'logo_url': None,
         'active': True,
@@ -142,7 +142,7 @@ MEETING_DATA = {
         'public_projects': True,
     },
     'VPRSF2015': {
-        'name': 'Virginia Piedmont Regional Science Fair',
+        'name': 'Virginia Piedmont Regional Science Fair 2015',
         'info_url': None,
         'logo_url': 'http://vprsf.org/wp-content/themes/VPRSF/images/logo.png',
         'active': True,
@@ -164,7 +164,7 @@ MEETING_DATA = {
         'public_projects': True,
     },
     'ASB2015': {
-        'name': 'Association of Southeastern Biologists  2015',
+        'name': 'Association of Southeastern Biologists 2015',
         'info_url': None,
         'logo_url': 'http://www.sebiologists.org/wp/wp-content/uploads/2014/09/banner_image_Large.png',
         'active': True,
@@ -233,7 +233,7 @@ MEETING_DATA = {
         'public_projects': True,
     },
     'VaACS2015': {
-        'name': 'VA Section ACS Student Poster Session 2015',
+        'name': 'Virginia Section American Chemical Society Student Poster Session 2015',
         'info_url': 'http://virginia.sites.acs.org/',
         'logo_url': 'http://virginia.sites.acs.org/Bulletin/15/UVA.jpg',
         'active': True,
