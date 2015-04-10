926a40bf129445ba5012099023b7c2c9c8d8afcd
Sam Chrisinger <s.chrisinger@gmail.com>
2015-04-10 17:26:28 -0400

    Remove whitespace

--- website/static/js/folderPickerNodeConfig.js
+++ website/static/js/folderPickerNodeConfig.js
@@ -157,7 +157,7 @@ var FolderPickerViewModel = oop.defclass({
             var loaded = self.loadedSettings();
             return loaded && !userHasAuth && !nodeHasAuth;
         });
-        
+
         /** Computed functions for the linked and selected folders' display text.*/
         self.folderName = ko.pureComputed(function() {
             var nodeHasAuth = self.nodeHasAuth();
