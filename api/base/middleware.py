1941cba46978fb7f8182ddb3eddf2d77002b28f7
Steven Loria <sloria1@gmail.com>
2015-04-12 23:41:24 -0400

    Make sure transaction is rolled back if an exception is raised

--- api/base/middleware.py
+++ api/base/middleware.py
@@ -1,10 +1,13 @@
-from framework.transactions import handlers
+from framework.transactions import handlers, commands
 
 class TokuTransactionsMiddleware(object):
     """TokuMX transaction middleware."""
 
     def process_request(self, request):
         handlers.transaction_before_request()
+
+    def process_exception(self, request, exception):
+        commands.rollback()
 
     def process_response(self, request, response):
         return handlers.transaction_after_request(response)
