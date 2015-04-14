43be704d2b3103c0b4d0d26b5931c632ed3086cc
rliebz <rliebz@gmail.com>
2015-04-14 15:02:51 -0400

    Remove unusable dataverse scripts and move remaining tests to tests directory

--- scripts/dataverse/migrate_study.py
+++ scripts/dataverse/migrate_study.py
@@ -10,20 +10,14 @@ Changes:
 
 """
 
+import sys
 import logging
 from modularodm import Q
-
-import mock
-from nose.tools import *
-from tests.base import OsfTestCase
-from tests.factories import ProjectFactory
 
 from website.app import init_app
 from scripts import utils as script_utils
 
-from website.addons.dataverse import client
 from website.addons.dataverse.model import AddonDataverseNodeSettings
-from website.addons.dataverse.tests.utils import create_mock_connection
 
 logger = logging.getLogger(__name__)
 
@@ -54,43 +48,6 @@ def get_targets():
 def main(dry=True):
     init_app(set_backends=True, routes=False, mfr=False)  # Sets the storage backends on all models
     do_migration(get_targets(), dry=dry)
-
-
-class TestDatasetMigration(OsfTestCase):
-
-    def setUp(self):
-        super(TestDatasetMigration, self).setUp()
-        self.project = ProjectFactory()
-        self.project.creator.add_addon('dataverse')
-        self.user_addon = self.project.creator.get_addon('dataverse')
-        self.project.add_addon('dataverse', None)
-        self.node_addon = self.project.get_addon('dataverse')
-        self.node_addon.study_hdl = 'doi:12.3456/DVN/00003'
-        self.node_addon.study = 'Example (DVN/00003)'
-        self.node_addon.user_settings = self.user_addon
-        self.node_addon.save()
-
-    def test_migration(self):
-
-        do_migration([self.node_addon], dry=False)
-        self.node_addon.reload()
-
-        assert_equal(self.node_addon.dataset_doi, 'doi:12.3456/DVN/00003')
-        assert_equal(self.node_addon.dataset, 'Example (DVN/00003)')
-
-    def test_get_targets(self):
-        AddonDataverseNodeSettings.remove()
-        addons = [
-            AddonDataverseNodeSettings(),
-            AddonDataverseNodeSettings(study_hdl='foo'),
-            AddonDataverseNodeSettings(user_settings=self.user_addon),
-            AddonDataverseNodeSettings(study_hdl='foo', user_settings=self.user_addon),
-        ]
-        for addon in addons:
-            addon.save()
-        targets = get_targets()
-        assert_equal(targets.count(), 1)
-        assert_equal(targets[0]._id, addons[-1]._id)
 
 
 if __name__ == '__main__':
