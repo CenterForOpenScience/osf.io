793880342f9155bd40fb22ba95c7f07f03c8dc3f
Caner Uguz <caneruguz@gmail.com>
2015-04-21 16:59:51 -0400

    Change cancel upload circle to denote removal [skip ci]

--- website/static/js/fangorn.js
+++ website/static/js/fangorn.js
@@ -90,7 +90,7 @@ var cancelUploadTemplate = function(row){
             'onclick' : function (e) {
                 cancelUploads.call(treebeard, row);
             }},
-        m('.fa.fa-times'));
+        m('.fa.fa-times-circle.text-danger', { style : 'display:block;font-size:18px'}));
 };
 
 
