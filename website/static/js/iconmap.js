b615b2fb0d35bf813867433fbf0d6b5b22bf7333
Sam Chrisinger <s.chrisinger@gmail.com>
2015-04-28 16:03:00 -0400

    Use class names in iconmap

--- website/static/js/iconmap.js
+++ website/static/js/iconmap.js
@@ -2,31 +2,24 @@
 require('css/projectorganizer.css');
 ///////////////////////////////////////////////
 
-function makeIcon(className) {
-    var icon = document.createElement('i');
-    icon.className = className;
-    return icon;
-}
-
 module.exports = {
     componentIcons: {
-        hypothesis: makeIcon('fa fa-lightbulb-o'),
-        methods_and_measures: makeIcon('fa fa-pencil'),
-        procedure: makeIcon('fa fa-cogs'),
-        instrumentation: makeIcon('fa fa-flask'),
-        data: makeIcon('fa fa-database'),
-        analysis: makeIcon('fa fa-bar-chart'),
-        communication: makeIcon('fa fa-comment'),
-        other: makeIcon('fa fa-question'),
-        '': makeIcon('fa fa-circle-thin')
+        hypothesis: 'fa fa-lightbulb-o',
+        methods_and_measures: 'fa fa-pencil',
+        procedure: 'fa fa-cogs',
+        instrumentation: 'fa fa-flask',
+        data: 'fa fa-database',
+        analysis: 'fa fa-bar-chart',
+        communication: 'fa fa-comment',
+        other: 'fa fa-question',
+        '': 'fa fa-circle-thin'
     },
     projectIcons: {
-        folder: makeIcon('project-organizer-icon-folder'),
-        smartFolder: makeIcon('project-organizer-icon-smart-folder'),
-        project: makeIcon('project-organizer-icon-project'),
-        registration: makeIcon( 'project-organizer-icon-reg-project'),
-        component: makeIcon( 'project-organizer-icon-component'),
-        registeredComponent: makeIcon( 'project-organizer-icon-reg-component'),
-        link: makeIcon( 'project-organizer-icon-pointer')
-    }
+        folder: 'project-organizer-icon-folder',
+        smartFolder: 'project-organizer-icon-smart-folder',
+        project: 'project-organizer-icon-project',
+        registration:  'project-organizer-icon-reg-project',
+        link:  'project-organizer-icon-pointer'
+    },
+    info: 'fa fa-question'
 };
