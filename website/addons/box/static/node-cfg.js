ab2fd2dbb81dcda731f8fb6edfde6cc7550a9e04
Matt Frazier <maf7sm@virginia.edu>
2015-04-28 13:18:41 +1200

    Get JS mostly working for new oauth   -FolderPicker is currently nonfunctional

--- website/addons/box/static/node-cfg.js
+++ website/addons/box/static/node-cfg.js
@@ -1,16 +1,7 @@
 'use strict';
 
-var AddonNodeConfig = require('js/addonNodeConfig').AddonNodeConfig;
-var url = window.contextVars.node.urls.api + 'box/config/';
-new AddonNodeConfig('Box', '#boxScope', url, '#boxGrid', {
-    onPickFolder: function(evt, item) {
-        evt.preventDefault();
-        var name = item.data.path === 'All Files' ? '/ (Full Box)' : item.data.path.replace('All Files', '');
-        this.selected({
-            name: name,
-            path: item.data.path,
-            id: item.data.id
-        });
-        return false; // Prevent event propagation
-    }
-});
+require('./box.css')
+var BoxNodeConfig = require('./boxNodeConfig').BoxNodeConfig;
+
+var url = window.contextVars.node.urls.api + 'box/settings/';
+new BoxNodeConfig('Box', '#boxScope', url, '#boxGrid');
