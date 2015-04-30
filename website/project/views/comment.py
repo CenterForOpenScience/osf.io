0e4c045bada86fe2f450f5c9cda725d11f417786
Saman Ehsan <se8ea@virginia.edu>
2015-04-29 20:26:04 -0400

    Refactor collect_discussion to return list
    Instead of modifying the dictionary
    of users' comments in collect_discussion,
    refactor to make it return a list of
    comments.

--- website/project/views/comment.py
+++ website/project/views/comment.py
@@ -79,13 +79,15 @@ def resolve_target(node, page, guid):
     return target.referent
 
 
-def collect_discussion(target, comments_dict):
+def collect_discussion(target, comments=None):
+    if not comments:
+        comments = []
     if not getattr(target, 'commented', None) is None:
         for comment in getattr(target, 'commented', []):
             if not (comment.is_deleted or comment.is_hidden):
-                comments_dict[comment.user].append(comment)
-            collect_discussion(comment, comments_dict=comments_dict)
-    return comments_dict
+                comments.append(comment)
+            collect_discussion(comment, comments=comments)
+    return comments
 
 
 def comment_discussion(comments, node, anonymous=False, widget=False):
@@ -95,7 +97,7 @@ def comment_discussion(comments, node, anonymous=False, widget=False):
         if not (comment.is_deleted or comment.is_hidden):
             comments_dict[comment.user].append(comment)
         if not widget:
-            collect_discussion(comment, comments_dict=comments_dict)
+            comments_dict[comment.user].extend(collect_discussion(comment))
 
     sorted_users_frequency = sorted(
         comments_dict.keys(),
