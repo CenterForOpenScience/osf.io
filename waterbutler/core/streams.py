93c576c6a0215dd9c72fa6de0f7196a5d464abe9
Chris Seto <chriskseto@gmail.com>
2015-05-05 17:04:16 -0400

    Fix sizing of Base64Stream

--- waterbutler/core/streams.py
+++ waterbutler/core/streams.py
@@ -372,15 +372,26 @@ class FormDataStream(MultiStream):
 
 class Base64EncodeStream(asyncio.StreamReader):
 
+    @staticmethod
+    def calculate_encoded_size(size):
+        size = 4 * size / 3
+        if size % 4:
+            size += 4 - size % 4
+        return int(size)
+
     def __init__(self, stream, **kwargs):
         self.extra = b''
         self.stream = stream
-        self.size = int(stream.size)
-        self.size = (4 * self.size / 3)
-        if self.size % 4:
-            self.size += (4 - self.size % 4)
-        self.size = str(int(self.size))
+        if stream.size is None:
+            self._size = None
+        else:
+            self._size = Base64EncodeStream.calculate_encoded_size(stream.size)
+
         super().__init__(**kwargs)
+
+    @property
+    def size(self):
+        return self._size
 
     @asyncio.coroutine
     def read(self, n=-1):
