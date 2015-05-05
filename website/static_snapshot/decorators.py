324ac0a1c74aed556115bf2e879dc863cceb8e0a
kushG <imkushagra@gmail.com>
2015-05-05 17:12:00 -0400

    decorator update

--- website/static_snapshot/decorators.py
+++ website/static_snapshot/decorators.py
@@ -8,6 +8,7 @@ from werkzeug.contrib.cache import SimpleCache
 from website.static_snapshot import tasks
 from website.static_snapshot.utils import get_path
 from website.models import Node
+from website import settings
 
 
 cache = SimpleCache()
@@ -27,44 +28,46 @@ def gets_static_snapshot(page_name):
         @functools.wraps(func)
         def wrapped(*args, **kwargs):
 
-            # Do not allow phantom or API calls to run this task
-            if not ('Phantom' in request.user_agent.string
-                    or 'api/v1' in request.url):
+            if settings.USE_CELERY:
 
-                if cache.get(page_name) == 'pending':
-                    logger.warn('SEO Background task in progress')
+                # Do not allow phantom or API calls to run this task
+                if not ('Phantom' in request.user_agent.string
+                        or 'api/v1' in request.url):
 
-                else:
-                    id = ''
-                    category = ''
-                    # Only public projects
-                    if kwargs.get('pid') or kwargs.get('nid'):
-                        node = Node.load(kwargs.get('pid', kwargs.get('nid')))
-                        if node.is_public:
-                            id = kwargs.get('pid') or kwargs.get('nid')
-                            category = 'node'
-                        else:
-                            logger.warn('Private Projects are not exposed for SEO')
-                            return func(*args, **kwargs)
-
-                    if kwargs.get('uid'):
-                        id = kwargs.get('uid')
-                        category = 'user'
-
-                    path = get_path(page_name, id, category)
-                    if not os.path.exists(path['full_path']):
-                        task = tasks.get_static_snapshot.apply_async(args=[request.url, path['path']])
-
-                        # Retrieve these cache values in snapshot handler
-                        cache.set(page_name, 'pending')
-                        cache.set('task_id', task.id)
-                        cache.set('current_page', page_name)
+                    if cache.get(page_name) == 'pending':
+                        logger.warn('SEO Background task in progress')
 
                     else:
-                        # Retrieving from cache, if already available
-                        with open(path['full_path'], 'r') as fp:
-                            file_content = fp.read().decode('utf-8')
-                            cache.set('cached_content', file_content)
+                        id = ''
+                        category = ''
+                        # Only public projects
+                        if kwargs.get('pid') or kwargs.get('nid'):
+                            node = Node.load(kwargs.get('pid', kwargs.get('nid')))
+                            if node.is_public:
+                                id = kwargs.get('pid') or kwargs.get('nid')
+                                category = 'node'
+                            else:
+                                logger.warn('Private Projects are not exposed for SEO')
+                                return func(*args, **kwargs)
+
+                        if kwargs.get('uid'):
+                            id = kwargs.get('uid')
+                            category = 'user'
+
+                        path = get_path(page_name, id, category)
+                        if not os.path.exists(path['full_path']):
+                            task = tasks.get_static_snapshot.apply_async(args=[request.url, path['path']])
+
+                            # Retrieve these cache values in snapshot handler
+                            cache.set(page_name, 'pending')
+                            cache.set('task_id', task.id)
+                            cache.set('current_page', page_name)
+
+                        else:
+                            # Retrieving from cache, if already available
+                            with open(path['full_path'], 'r') as fp:
+                                file_content = fp.read().decode('utf-8')
+                                cache.set('cached_content', file_content)
 
             return func(*args, **kwargs)
 
