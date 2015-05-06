d7b8ec1c978d72df68e1a166beaf730793e4944a
kushG <imkushagra@gmail.com>
2015-05-05 21:38:16 -0400

    Adds celery checks in decorator

--- website/static_snapshot/views.py
+++ website/static_snapshot/views.py
@@ -3,6 +3,7 @@ import logging
 
 from framework.render.tasks import ensure_path
 from website.static_snapshot import tasks
+from website import settings
 
 logger = logging.getLogger(__name__)
 
@@ -11,34 +12,39 @@ def get_static_snapshot(cache):
     """
     View function to handle the url call made by google bot crawler
     """
-    response = {}
-    task_id = cache.get('task_id')
-    task = tasks.get_static_snapshot.AsyncResult(task_id)
-    if task.id:
+    if settings.USE_CELERY:
 
-        if task.state == 'PENDING':
-            logger.debug('Waiting for response from Phantom Server')
+        response = {}
+        task_id = cache.get('task_id')
+        task = tasks.get_static_snapshot.AsyncResult(task_id)
+        if task.id:
 
-        elif task.state == 'SUCCESS':
-            logger.debug('Static snapshot received. Caching the response in website/seocache')
-            path = task.result['path']
-            ensure_path(path)
-            current_page = cache.get('current_page')
-            file_name = os.path.join(path, current_page) + '.html'
-            file_content = task.result['content'].encode('utf-8')
-            with open(file_name, 'wb') as fp:
-                fp.write(file_content)
+            if task.state == 'PENDING':
+                logger.debug('Waiting for response from Phantom Server')
+
+            elif task.state == 'SUCCESS':
+                logger.debug('Static snapshot received. Caching the response in website/seocache')
+                path = task.result['path']
+                ensure_path(path)
+                current_page = cache.get('current_page')
+                file_name = os.path.join(path, current_page) + '.html'
+                file_content = task.result['content'].encode('utf-8')
+                with open(file_name, 'wb') as fp:
+                    fp.write(file_content)
+                cache.clear()
+                response = {'content': task.result['content']}
+            else:
+                logger.warn('Invalid Celery task')
+
+        elif cache.get('cached_content'):
+            logger.debug('Cached Already')
+            response = {'content': cache.get('cached_content')}
             cache.clear()
-            response = {'content': task.result['content']}
-        else:
-            logger.warn('Invalid Celery task')
 
-    elif cache.get('cached_content'):
-        logger.debug('Cached Already')
-        response = {'content': cache.get('cached_content')}
-        cache.clear()
+        else:
+            logger.warn('No celery task id found')
+
+        return response
 
     else:
-        logger.warn('No celery task id found')
-
-    return response
+        return None
