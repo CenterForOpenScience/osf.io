50d23b655b01fe5a71f13ffe445d03dc515b6e8c
Sam Chrisinger <s.chrisinger@gmail.com>
2015-05-06 15:45:36 -0400

    Block pointer auto-expansion on frontend, hopefully

--- website/static/js/projectorganizer.js
+++ website/static/js/projectorganizer.js
@@ -1,4 +1,4 @@
-/**
+**
  * Handles Project Organizer on dashboard page of OSF.
  * For Treebeard and _item API's check: https://github.com/caneruguz/treebeard/wiki
  */
@@ -742,7 +742,11 @@ function expandStateLoad(item) {
         item.data.childrenCount = 0;
         tb.updateFolder(null, item);
     }
-    if (item.children.length > 0 && item.depth > 0) {
+    if (item.data.isPointer) {
+        item.data.expand = false;
+    }
+
+    if ((!item.data.isPointer) && (item.children.length > 0 && item.depth > 0)) {
         for (i = 0; i < item.children.length; i++) {
             if (item.children[i].data.expand) {
                 tb.updateFolder(null, item.children[i]);
