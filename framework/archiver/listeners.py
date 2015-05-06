88a50aed6e6ebe94aca4c8cecf67c961df99858f
Sam Chrisinger <s.chrisinger@gmail.com>
2015-05-06 16:28:57 -0400

    WIP archiver

--- framework/archiver/listeners.py
+++ framework/archiver/listeners.py
@@ -3,7 +3,7 @@ from framework.archiver.tasks import ArchiveTask
 from website.project import signals as project_signals
 
 @project_signals.after_create_registration.connect
-def _before_register_node(src, dst, user):
+def before_register_node(src, dst, user):
     src.archiving = True
     task = ArchiveTask().delay(src._id, dst._id, user._id)
     src.archive_task_id = task.task_id
