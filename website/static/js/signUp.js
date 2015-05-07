0709c64c3065af09b2038c7d401a0a8662b1fec7
kmjungersen <kmjungersen@gmail.com>
2015-05-07 16:47:37 -0400

    added google analytics event for the new user form's submit button

--- website/static/js/signUp.js
+++ website/static/js/signUp.js
@@ -112,7 +112,8 @@ var ViewModel = function(submitUrl) {
             });
             return false;
         }
-        // Else submit
+        // Else submit, and send Google Analytics event
+        ga('send', 'event', 'signup-submit', 'click', 'new_user_submit');
         $osf.postJSON(
             submitUrl,
             ko.toJS(self)
