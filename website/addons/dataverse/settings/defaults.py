68bda6e05a3cdb64319e82adeb8aab2926dc81c5
rliebz <rliebz@gmail.com>
2015-05-12 11:08:44 -0400

    Update Dataverse default/local settings

--- website/addons/dataverse/settings/defaults.py
+++ website/addons/dataverse/settings/defaults.py
@@ -1,5 +1,5 @@
 DEFAULT_HOSTS = [
-    'dataverse.harvard.edu',
-    'dataverse-demo.iq.harvard.edu',
-    'apitest.dataverse.org',
+    'dataverse.harvard.edu',            # Harvard PRODUCTION server
+    'dataverse-demo.iq.harvard.edu',    # Harvard DEMO server
+    'apitest.dataverse.org',            # Dataverse TEST server
 ]
