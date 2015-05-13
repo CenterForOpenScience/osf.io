80fe6ddd4c2dfdb311ff2817e55e7f11baff4937
Chris Seto <chriskseto@gmail.com>
2015-05-12 20:44:52 -0400

    Better decorators and no more webargs

--- website/addons/osfstorage/decorators.py
+++ website/addons/osfstorage/decorators.py
@@ -1,64 +1,18 @@
 import httplib
 import functools
 
-from webargs import Arg
-from webargs import core
-
 from modularodm.exceptions import NoResultsFound
-from modularodm.exceptions import ValidationValueError
 from modularodm.storage.base import KeyExistsException
 from framework.auth.decorators import must_be_signed
+
+from framework.exceptions import HTTPError
 
 from website.models import User
 from website.models import Node
 from website.addons.osfstorage import model
-from framework.exceptions import HTTPError
-from website.addons.osfstorage import utils
 from website.project.decorators import (
     must_not_be_registration, must_have_addon,
 )
-
-
-USER_ARG = Arg(None, required=True, dest='user', use=User.from_cookie, validate=lambda x: x is not None)
-
-class JSONParser(core.Parser):
-    def __init__(self, data):
-        self._data = data
-
-    def parse(self, args):
-        return super(JSONParser, self).parse(args, None, ('json',))
-
-    def parse_json(self, _, name, arg):
-        if self._data:
-            return core.get_value(self._data, name, arg.multiple)
-        else:
-            return core.Missing
-
-    def error_callback(self, err):
-        raise HTTPError(err.status_code, data={
-            'message_long': err.message
-        })
-
-def path_validator(path):
-    return (
-        path.startswith('/') and
-        len(path.strip('/').split('/')) < 3
-    )
-
-file_opt_args = {
-    'user': Arg(None, required=True, use=User.load, validate=lambda x: x is not None),
-    'source': Arg(unicode, required=True),
-    'destination': Arg({
-        'name': Arg(unicode, required=True, validate=lambda x: '/' not in x),
-        'parent': Arg(unicode, required=True, validate=lambda x: '/' not in x),
-        'node': Arg(None, required=True, dest='node', use=Node.load, validate=lambda x: x is not None),
-    })
-}
-
-waterbutler_crud_args = {
-    'cookie': USER_ARG,
-    'path': Arg(str, required=True, validate=path_validator),
-}
 
 
 def handle_odm_errors(func):
@@ -73,6 +27,29 @@ def handle_odm_errors(func):
     return wrapped
 
 
+def autoload_filenode(must_be=None, default_root=False):
+    def _autoload_filenode(func):
+        @handle_odm_errors
+        @functools.wraps(func)
+        def wrapped(*args, **kwargs):
+            node_addon = kwargs['node_addon']
+
+            if 'fid' not in kwargs and default_root:
+                file_node = node_addon.root_node
+            else:
+                file_node = model.OsfStorageFileNode.get(kwargs.get('fid'), node_addon)
+
+            if must_be and file_node.kind != must_be:
+                raise HTTPError(httplib.BAD_REQUEST)
+
+            kwargs['file_node'] = file_node
+
+            return func(*args, **kwargs)
+
+        return wrapped
+    return _autoload_filenode
+
+
 def waterbutler_opt_hook(func):
 
     @must_be_signed
@@ -81,41 +58,20 @@ def waterbutler_opt_hook(func):
     @must_have_addon('osfstorage', 'node')
     @functools.wraps(func)
     def wrapped(payload, *args, **kwargs):
-        kwargs.update(JSONParser(payload).parse(file_opt_args))
-        destination = kwargs['destination']
-        kwargs.update({
-            'source': model.OsfStorageFileNode.get(
-                kwargs['source'],
-                kwargs['node_addon']
-            ),
-            'destination': model.OsfStorageFileNode.get_folder(
-                destination['parent'],
-                destination['node'].get_addon('osfstorage')
-            ),
-            'name': destination['name'],
-        })
-        return func(*args, **kwargs)
-    wrapped.undecorated = func  # For testing
-    return wrapped
+        try:
+            user = User.load(payload['user'])
+            dest_node = Node.load(payload['destination']['node'])
+            source = model.OsfStorageFileNode.get(payload['source'], kwargs['node_addon'])
+            dest_parent = model.OsfStorageFileNode.get_folder(payload['destination']['parent'], dest_node.get_addon('osfstorage'))
 
-
-@must_be_signed
-@handle_odm_errors
-@must_not_be_registration
-@must_have_addon('osfstorage', 'node')
-def waterbutler_crud_hook(func):
-
-    @functools.wraps(func)
-    def wrapped(payload, *args, **kwargs):
-        kwargs.update(JSONParser(payload)).parse({
-            'cookie': USER_ARG,
-            'path': Arg(
-                None,
-                required=True,
-                dest='file_node',
-                validate=lambda x: model.OsfStorageFileNode.get(x, kwargs.get('node_addon'))
-            ),
-        })
+            kwargs.update({
+                'user': user,
+                'source': source,
+                'destination': dest_parent,
+                'name': payload['destination']['name'],
+            })
+        except KeyError:
+            raise HTTPError(httplib.BAD_REQUEST)
 
         return func(*args, **kwargs)
     return wrapped
