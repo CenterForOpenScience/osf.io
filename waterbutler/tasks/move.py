061f9385d87cedc2d07545e879a66f435c4e7a1a
Chris Seto <chriskseto@gmail.com>
2015-05-15 15:21:00 -0400

    Try except both moves and copies

--- waterbutler/tasks/move.py
+++ waterbutler/tasks/move.py
@@ -11,7 +11,29 @@ def move(src_bundle, dest_bundle, callback_url, auth, **kwargs):
     src_path, src_provider = src_bundle.pop('path'), utils.make_provider(**src_bundle.pop('provider'))
     dest_path, dest_provider = dest_bundle.pop('path'), utils.make_provider(**dest_bundle.pop('provider'))
 
-    metadata, created = yield from src_provider.move(dest_provider, src_path, dest_path, **kwargs)
+    try:
+        metadata, created = yield from src_provider.move(dest_provider, src_path, dest_path, **kwargs)
+    except Exception as e:
+        yield from utils.send_signed_request('PUT', callback_url, {
+            'action': 'move',
+            'errors': [e.__repr__()],
+            'source': {
+                'path': src_path.path,
+                'name': src_path.name,
+                'materialized': str(src_path),
+                'provider': src_provider.NAME,
+            },
+            'destination': {
+                'path': dest_path.path,
+                'name': dest_path.name,
+                'materialized': str(dest_path),
+                'provider': dest_provider.NAME,
+            },
+            'auth': auth['auth'],
+            'time': time.time() + 60,
+            'email': start_time - time.time() > settings.WAIT_TIMEOUT
+        })
+        raise
 
     yield from utils.send_signed_request('PUT', callback_url, {
         'action': 'move',
