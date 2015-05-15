8e1442c51f711a6cba74ae7d0472e88d3537f877
Chris Seto <chriskseto@gmail.com>
2015-05-15 00:05:30 -0400

    More stream tests

--- tests/core/streams/test_multistream.py
+++ tests/core/streams/test_multistream.py
@@ -1,5 +1,6 @@
 import pytest
 import random
+import string
 import functools
 from unittest import mock
 
@@ -10,11 +11,37 @@ from waterbutler.core import streams
 
 @pytest.fixture
 def blob():
-    pass
+    return ''.join(random.sample(string.printable, 50)).encode('utf-8')
+
 
 class TestMultiStream:
 
-    def test_single_stream(self):
-        pass
+    @async
+    def test_single_stream(self, blob):
+        stream = streams.MultiStream(streams.StringStream(blob))
+        data = yield from stream.read()
+        assert data == blob
 
+    @async
+    def test_double_same_stream(self, blob):
+        stream = streams.MultiStream(
+            streams.StringStream(blob),
+            streams.StringStream(blob)
+        )
+        data = yield from stream.read()
+        assert data == (blob * 2)
 
+    @async
+    def test_1_at_a_time_single_stream(self, blob):
+        stream = streams.MultiStream(streams.StringStream(blob))
+        for i in range(len(blob)):
+            assert blob[i:i + 1] == (yield from stream.read(1))
+
+    @async
+    def test_1_at_a_time_many_stream(self, blob):
+        count = 4
+        stream = streams.MultiStream(*[streams.StringStream(blob) for _ in range(count)])
+
+        for _ in range(count):
+            for i in range(len(blob)):
+                assert blob[i:i + 1] == (yield from stream.read(1))
