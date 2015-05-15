1e0cbdd95d047091f9d569c5961d6e42c3289ac7
Chris Seto <chriskseto@gmail.com>
2015-05-14 23:43:00 -0400

    Split up streams tests

--- tests/core/streams/test_formdatastream.py
+++ tests/core/streams/test_formdatastream.py
@@ -1,12 +1,11 @@
-import base64
+import pytest
 import functools
 from unittest import mock
-
-import pytest
 
 from tests.utils import async
 
 from waterbutler.core import streams
+
 
 @pytest.fixture(autouse=True)
 def boundary(monkeypatch):
@@ -140,49 +139,3 @@ class TestFormDataStream:
         ]).encode('utf-8')
 
         assert expected == data
-
-
-class TestBase64Stream:
-
-    @async
-    def test_doesnt_crash_with_none(self):
-        stream = streams.Base64EncodeStream(streams.StringStream(b''))
-        data = yield from stream.read()
-
-        assert data == b''
-
-    @async
-    def test_read(self):
-        data = b'this is a test'
-        expected = base64.b64encode(data)
-        stream = streams.Base64EncodeStream(streams.StringStream(data))
-
-        actual = yield from stream.read()
-
-        assert expected == actual
-
-    @async
-    def test_chunking(self):
-        for chunk_size in range(1, 10):
-            data = b'the ode to carp'
-            expected = streams.StringStream(base64.b64encode(data))
-            stream = streams.Base64EncodeStream(streams.StringStream(data))
-
-            hoped = yield from expected.read(chunk_size)
-
-            while hoped:
-                actual = yield from stream.read(chunk_size)
-                assert actual == hoped
-                hoped = yield from expected.read(chunk_size)
-
-            left_overs = yield from stream.read()
-
-            assert left_overs == b''
-
-    def test_size(self):
-        data = b'the ode to carp'
-        expected = base64.b64encode(data)
-        stream = streams.Base64EncodeStream(streams.StringStream(data))
-
-        assert len(expected) == int(stream.size)
-
---
+++ tests/core/streams/test_multistream.py
@@ -0,0 +1,20 @@
+import pytest
+import random
+import functools
+from unittest import mock
+
+from tests.utils import async
+
+from waterbutler.core import streams
+
+
+@pytest.fixture
+def blob():
+    pass
+
+class TestMultiStream:
+
+    def test_single_stream(self):
+        pass
+
+
