c0bcc00a09525bfdb49774045b4d533485ee9ce4
Caner Uguz <caneruguz@gmail.com>
2015-05-15 15:15:57 -0400

    Update treebeard commit to fix modals and svae naturalscrolllimit option [skip ci]

--- website/static/js/fangorn.js
+++ website/static/js/fangorn.js
@@ -1690,7 +1690,8 @@ tbOptions = {
         var item = tb.find(row.id);
         _fangornMultiselect.call(tb,null,item);
     },
-    hScroll : 400
+    hScroll : 400,
+    naturalScrollLimit : 1000
 };
 
 /**
