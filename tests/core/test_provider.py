55802522c4b836f07afa30155c00a7b4878f092d
Chris Seto <chriskseto@gmail.com>
2015-05-15 11:34:59 -0400

    More tests for base provider

--- tests/core/test_provider.py
+++ tests/core/test_provider.py
@@ -102,3 +102,236 @@ class TestBaseProvider:
         assert new_path.is_file
         assert str(path) == str(new_path.parent)
         assert new_path.name == 'text_file.txt'
+
+
+class TestHandleNameConflict:
+
+    @async
+    def test_no_replace_or_existing(self, provider1):
+        path = yield from provider1.validate_path('/test/path')
+        provider1.exists = utils.MockCoroutine(return_value=False)
+
+        handled, exists = yield from provider1.handle_name_conflict(path)
+
+        assert handled is path
+        assert exists is False
+
+    @async
+    def test_replace(self, provider1):
+        path = yield from provider1.validate_path('/test/path')
+        provider1.exists = utils.MockCoroutine(return_value=True)
+
+        handled, exists = yield from provider1.handle_name_conflict(path, conflict='replace')
+
+        assert handled is path
+        assert exists is True
+        assert str(handled) == '/test/path'
+
+    @async
+    def test_replace_not_existing(self, provider1):
+        path = yield from provider1.validate_path('/test/path')
+        provider1.exists = utils.MockCoroutine(return_value=False)
+
+        handled, exists = yield from provider1.handle_name_conflict(path, conflict='replace')
+
+        assert handled is path
+        assert str(handled) == '/test/path'
+        assert exists is False
+
+    @async
+    def test_renames(self, provider1):
+        path = yield from provider1.validate_path('/test/path')
+        provider1.exists = utils.MockCoroutine(side_effect=(True, False))
+
+        handled, exists = yield from provider1.handle_name_conflict(path, conflict='keep')
+
+        assert handled is path
+        assert exists is False
+        assert str(handled) == '/test/path (1)'
+        assert handled.name == 'path (1)'
+
+    @async
+    def test_renames_twice(self, provider1):
+        path = yield from provider1.validate_path('/test/path')
+        provider1.exists = utils.MockCoroutine(side_effect=(True, True, False))
+
+        handled, exists = yield from provider1.handle_name_conflict(path, conflict='keep')
+
+        assert handled is path
+        assert exists is False
+        assert str(handled) == '/test/path (2)'
+        assert handled.name == 'path (2)'
+
+
+class TestHandleNaming:
+
+    @async
+    def test_no_problem(self, provider1):
+        src_path = yield from provider1.validate_path('/test/path/')
+        dest_path = yield from provider1.validate_path('/test/path/')
+        provider1.exists = utils.MockCoroutine(return_value=False)
+
+        handled = yield from provider1.handle_naming(src_path, dest_path)
+
+        assert handled == src_path.child('path', folder=True)
+        assert handled.is_dir is True
+        assert len(handled.parts) == 4  # Includes root
+        assert handled.name == 'path'
+
+    @async
+    def test_rename_via_path(self, provider1):
+        src_path = yield from provider1.validate_path('/test/name1')
+        dest_path = yield from provider1.validate_path('/test/name2')
+        provider1.exists = utils.MockCoroutine(return_value=False)
+
+        handled = yield from provider1.handle_naming(src_path, dest_path)
+
+        assert handled.name == 'name2'
+        assert handled.is_file is True
+
+    @async
+    def test_rename_explicit(self, provider1):
+        dest_path = yield from provider1.validate_path('/test/')
+        src_path = yield from provider1.validate_path('/test/name1')
+        provider1.exists = utils.MockCoroutine(return_value=False)
+
+        handled = yield from provider1.handle_naming(src_path, dest_path, rename='name2')
+
+        assert handled.name == 'name2'
+        assert handled.is_file is True
+
+    @async
+    def test_no_problem_file(self, provider1):
+        src_path = yield from provider1.validate_path('/test/path')
+        dest_path = yield from provider1.validate_path('/test/path')
+        provider1.exists = utils.MockCoroutine(return_value=False)
+
+        handled = yield from provider1.handle_naming(src_path, dest_path)
+
+        assert handled == dest_path  # == not is
+        assert handled.is_file is True
+        assert len(handled.parts) == 3  # Includes root
+        assert handled.name == 'path'
+
+
+class TestCopy:
+
+    @async
+    def test_handles_naming_false(self, provider1):
+        src_path = yield from provider1.validate_path('/source/path')
+        dest_path = yield from provider1.validate_path('/destination/path')
+
+        provider1.handle_naming = utils.MockCoroutine()
+
+        yield from provider1.copy(provider1, src_path, dest_path, handle_naming=False)
+
+        assert provider1.handle_naming.called is False
+
+    @async
+    def test_handles_naming(self, provider1):
+        src_path = yield from provider1.validate_path('/source/path')
+        dest_path = yield from provider1.validate_path('/destination/path')
+
+        provider1.handle_naming = utils.MockCoroutine()
+
+        yield from provider1.copy(provider1, src_path, dest_path)
+
+        provider1.handle_naming.assert_called_once_with(
+            src_path,
+            dest_path,
+            rename=None,
+            conflict='replace',
+        )
+
+    @async
+    def test_passes_on_conflict(self, provider1):
+        src_path = yield from provider1.validate_path('/source/path')
+        dest_path = yield from provider1.validate_path('/destination/path')
+
+        provider1.handle_naming = utils.MockCoroutine()
+
+        yield from provider1.copy(provider1, src_path, dest_path, conflict='keep')
+
+        provider1.handle_naming.assert_called_once_with(
+            src_path,
+            dest_path,
+            rename=None,
+            conflict='keep',
+        )
+
+    @async
+    def test_passes_on_rename(self, provider1):
+        src_path = yield from provider1.validate_path('/source/path')
+        dest_path = yield from provider1.validate_path('/destination/path')
+
+        provider1.handle_naming = utils.MockCoroutine()
+
+        yield from provider1.copy(provider1, src_path, dest_path, rename='Baz')
+
+        provider1.handle_naming.assert_called_once_with(
+            src_path,
+            dest_path,
+            rename='Baz',
+            conflict='replace',
+        )
+
+
+class TestMove:
+    @async
+    def test_handles_naming_false(self, provider1):
+        src_path = yield from provider1.validate_path('/source/path')
+        dest_path = yield from provider1.validate_path('/destination/path')
+
+        provider1.handle_naming = utils.MockCoroutine()
+
+        yield from provider1.move(provider1, src_path, dest_path, handle_naming=False)
+
+        assert provider1.handle_naming.called is False
+
+    @async
+    def test_handles_naming(self, provider1):
+        src_path = yield from provider1.validate_path('/source/path')
+        dest_path = yield from provider1.validate_path('/destination/path')
+
+        provider1.handle_naming = utils.MockCoroutine()
+
+        yield from provider1.move(provider1, src_path, dest_path)
+
+        provider1.handle_naming.assert_called_once_with(
+            src_path,
+            dest_path,
+            rename=None,
+            conflict='replace',
+        )
+
+    @async
+    def test_passes_on_conflict(self, provider1):
+        src_path = yield from provider1.validate_path('/source/path')
+        dest_path = yield from provider1.validate_path('/destination/path')
+
+        provider1.handle_naming = utils.MockCoroutine()
+
+        yield from provider1.move(provider1, src_path, dest_path, conflict='keep')
+
+        provider1.handle_naming.assert_called_once_with(
+            src_path,
+            dest_path,
+            rename=None,
+            conflict='keep',
+        )
+
+    @async
+    def test_passes_on_rename(self, provider1):
+        src_path = yield from provider1.validate_path('/source/path')
+        dest_path = yield from provider1.validate_path('/destination/path')
+
+        provider1.handle_naming = utils.MockCoroutine()
+
+        yield from provider1.move(provider1, src_path, dest_path, rename='Baz')
+
+        provider1.handle_naming.assert_called_once_with(
+            src_path,
+            dest_path,
+            rename='Baz',
+            conflict='replace',
+        )
