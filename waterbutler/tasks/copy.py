55f1aee5c2948ab8e30baacb8ffa1ec6e1a0e333
Chris Seto <chriskseto@gmail.com>
2015-05-18 13:34:44 -0400

    Clean up the logic for move and copy a bit

--- waterbutler/tasks/copy.py
+++ waterbutler/tasks/copy.py
@@ -11,42 +11,35 @@ def copy(src_bundle, dest_bundle, callback_url, auth, **kwargs):
     src_path, src_provider = src_bundle.pop('path'), utils.make_provider(**src_bundle.pop('provider'))
     dest_path, dest_provider = dest_bundle.pop('path'), utils.make_provider(**dest_bundle.pop('provider'))
 
-    try:
-        metadata, created = yield from src_provider.copy(dest_provider, src_path, dest_path, **kwargs)
-    except Exception as e:
-        yield from utils.send_signed_request('PUT', callback_url, {
-            'action': 'copy',
-            'errors': [e.__repr__()],
-            'source': {
-                'path': src_path.path,
-                'name': src_path.name,
-                'materialized': str(src_path),
-                'provider': src_provider.NAME,
-            },
-            'destination': {
-                'path': dest_path.path,
-                'name': dest_path.name,
-                'materialized': str(dest_path),
-                'provider': dest_provider.NAME,
-            },
-            'auth': auth['auth'],
-            'time': time.time() + 60,
-            'email': start_time - time.time() > settings.WAIT_TIMEOUT
-        })
-        raise  # Ensure sentry sees this
-
-    yield from utils.send_signed_request('PUT', callback_url, {
+    data = {
+        'errors': [],
         'action': 'copy',
-        'source': {
+        'source': dict(src_bundle, **{
             'path': src_path.path,
             'name': src_path.name,
             'materialized': str(src_path),
             'provider': src_provider.NAME,
-        },
-        'destination': metadata,
+        }),
+        'destination': dict(dest_bundle, **{
+            'path': dest_path.path,
+            'name': dest_path.name,
+            'materialized': str(dest_path),
+            'provider': dest_provider.NAME,
+        }),
         'auth': auth['auth'],
-        'time': time.time() + 60,
-        'email': start_time - time.time() > settings.WAIT_TIMEOUT
-    })
+    }
+
+    try:
+        metadata, created = yield from src_provider.copy(dest_provider, src_path, dest_path, **kwargs)
+    except Exception as e:
+        data.update({'errors': [e.__repr__()]})
+        raise  # Ensure sentry sees this
+    else:
+        data.update({'destination': dict(src_bundle, **metadata)})
+    finally:
+        yield from utils.send_signed_request('PUT', callback_url, dict(data, **{
+            'time': time.time() + 60,
+            'email': time.time() - start_time > settings.WAIT_TIMEOUT
+        }))
 
     return metadata, created
--- waterbutler/tasks/move.py
+++ waterbutler/tasks/move.py
@@ -11,43 +11,36 @@ def move(src_bundle, dest_bundle, callback_url, auth, **kwargs):
     src_path, src_provider = src_bundle.pop('path'), utils.make_provider(**src_bundle.pop('provider'))
     dest_path, dest_provider = dest_bundle.pop('path'), utils.make_provider(**dest_bundle.pop('provider'))
 
-    try:
-        metadata, created = yield from src_provider.move(dest_provider, src_path, dest_path, **kwargs)
-    except Exception as e:
-        yield from utils.send_signed_request('PUT', callback_url, {
-            'action': 'move',
-            'errors': [e.__repr__()],
-            'source': {
-                'path': src_path.path,
-                'name': src_path.name,
-                'materialized': str(src_path),
-                'provider': src_provider.NAME,
-            },
-            'destination': {
-                'path': dest_path.path,
-                'name': dest_path.name,
-                'materialized': str(dest_path),
-                'provider': dest_provider.NAME,
-            },
-            'auth': auth['auth'],
-            'time': time.time() + 60,
-            'email': start_time - time.time() > settings.WAIT_TIMEOUT
-        })
-        raise
-
-    yield from utils.send_signed_request('PUT', callback_url, {
+    data = {
+        'errors': [],
         'action': 'move',
-        'source': {
+        'source': dict(src_bundle, **{
             'path': src_path.path,
             'name': src_path.name,
             'materialized': str(src_path),
             'provider': src_provider.NAME,
-        },
-        'destination': metadata,
+        }),
+        'destination': dict(dest_bundle, **{
+            'path': dest_path.path,
+            'name': dest_path.name,
+            'materialized': str(dest_path),
+            'provider': dest_provider.NAME,
+        }),
         'auth': auth['auth'],
-        'time': time.time() + 60,
-        'email': start_time - time.time() > settings.WAIT_TIMEOUT
-    })
+    }
+
+    try:
+        metadata, created = yield from src_provider.move(dest_provider, src_path, dest_path, **kwargs)
+    except Exception as e:
+        data.update({'errors': [e.__repr__()]})
+        raise  # Ensure sentry sees this
+    else:
+        data.update({'destination': dict(src_bundle, **metadata)})
+    finally:
+        yield from utils.send_signed_request('PUT', callback_url, dict(data, **{
+            'time': time.time() + 60,
+            'email': time.time() - start_time > settings.WAIT_TIMEOUT
+        }))
 
     return metadata, created
 
