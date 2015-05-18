40bca3bfedce8239634127a34cf6cdfce9aaaf59
Harry Rybacki <hrybacki@gmail.com>
2015-05-18 08:42:03 -0400

    Remove redundant text

--- website/static/js/nodeControl.js
+++ website/static/js/nodeControl.js
@@ -37,11 +37,7 @@ var MESSAGES = {
                         'but will have no impact on external sites, including Google\'s cache. ' +
                         'Would you like to continue?',
     // TODO(hrybacki): Remove once Retraction/Embargoes goes is merged into production
-    makeRegistrationPublicWarning: 'Once a registration is made public, there is no way to guarantee that ' +
-                        'access to the data it contains can be completely prevented. Users ' +
-                        'should assume that once a project is made public, it will always ' +
-                        'be public. Are you absolutely sure you would like to continue?' +
-                        '<hr /> \n<b>Important Note:</b> Effective <u>29 May 2015</u>, new registrations ' +
+    makeRegistrationPublicWarning: '<b>Important Note:</b> Effective <u>29 May 2015</u>, new registrations ' +
                         'will be made public immediately or can be embargoed for up to one year. There ' +
                         'will no longer be the option of creating a permanently private registration. This ' +
                         'registration occurred before 29 May 2015, so you do retain the option of keeping it ' +
