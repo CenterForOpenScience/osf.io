305ecb790bee091c5bc931f8e6b046e02b28d3db
harryrybacki <hrybacki@gmail.com>
2015-05-18 12:22:24 -0400

    Script didn't update User.security_messages
    Added tests

--- scripts/email_registration_contributors.py
+++ scripts/email_registration_contributors.py
@@ -1,23 +1,64 @@
 """Script for sending OSF emails to all users who contribute to registrations."""
 
+import datetime
+import logging
+
 from modularodm import Q
 
 from website import models
 from website.app import init_app
 from website.mails import Mail, send_mail
 
+from scripts import utils as script_utils
 
-def main():
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+MESSAGE_NAME = 'retraction_and_embargo_addition'
+MAILER = Mail(
+    'email_contributors_of_registrations',
+    'Important Update to Registrations on OSF'
+)
+
+
+def send_retraction_and_embargo_addition_message(contrib, label, mail, dry_run=True):
+    if label in contrib.security_messages:
+        return
+    send_mail(contrib.username, mail, user=contrib)
+    if not dry_run:
+        contrib.security_messages[MESSAGE_NAME] = datetime.datetime.utcnow()
+        contrib.save()
+
+
+def get_registration_contributors():
+    """Returns set of users that contribute to registrations."""
     registrations = models.Node.find(Q('is_registration', 'eq', True))
     contributors = []
-    for node in registrations:
-        contributors.extend([contrib for contrib in node.contributors if contrib not in contributors])
 
-    mailer = Mail('email_contributors_of_registrations', 'Important Update to Registrations on OSF')
+    def has_received_message(contrib):
+        query = (Q('_id', 'eq', contrib._id) & Q('security_messages.{0}'.format(MESSAGE_NAME), 'exists', False))
+        return False if len(models.User.find(query)) > 0 else True
+
+    for node in registrations:
+        contributors.extend([contrib for contrib in node.contributors if contrib not in contributors and not has_received_message(contrib)])
+
+    return contributors
+
+
+def main(dry_run):
+    contributors = get_registration_contributors()
 
     for contrib in contributors:
-        send_mail(contrib.username, mailer, user=contrib)
+        if dry_run:
+            logger.info('Dry run mode')
+        logger.info('Sending message to user {0!r}'.format(contrib))
+        send_retraction_and_embargo_addition_message(contrib, MESSAGE_NAME, MAILER, dry_run=dry_run)
+
 
 if __name__ == '__main__':
+    import sys
+    script_utils.add_file_logger(logger, __file__)
+    dry_run = 'dry' in sys.argv
     init_app(routes=False, mfr=False)
-    main()
+    main(dry_run)
