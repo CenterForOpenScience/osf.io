7638f03dfe347866d62ede55d0163e7593f9e6c9
Chris Seto <chriskseto@gmail.com>
2015-05-19 16:47:06 -0400

    Remove __all__ as it was not used and causing circular import errors when attempting to configure logging

--- waterbutler/core/__init__.py
+++ waterbutler/core/__init__.py
@@ -1,7 +1,7 @@
-from waterbutler.core.utils import async_retry
-from waterbutler.core.utils import make_provider
+# from waterbutler.core.utils import async_retry
+# from waterbutler.core.utils import make_provider
 
-__all__ = [
-    'async_retry',
-    'make_provider',
-]
+# __all__ = [
+#     'async_retry',
+#     'make_provider',
+# ]
