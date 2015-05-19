20c7ac1f95f3812a691b46795d64cd05abbb41a9
Saman Ehsan <se8ea@virginia.edu>
2015-05-19 15:48:25 -0400

    Fix TestApiBaseViews
    - Use ApiTestCase
    - Change route to '/v2/'

--- tests/api_tests/base/test_views.py
+++ tests/api_tests/base/test_views.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 from nose.tools import *  # flake8: noqa
 
-from tests.base import OsfTestCase
+from tests.base import ApiTestCase
 
-class TestApiBaseViews(OsfTestCase):
+class TestApiBaseViews(ApiTestCase):
 
     def test_root_returns_200(self):
-        res = self.app.get('/api/v2/')
+        res = self.app.get('/v2/')
         assert_equal(res.status_code, 200)
 
 
