42e628d1ec8f615d2de93f37f873248fa8a0996b
Chris Seto <chriskseto@gmail.com>
2015-05-19 17:44:56 -0400

    Catch Runtimeerrors as well as assertion errors

--- waterbutler/tasks/core.py
+++ waterbutler/tasks/core.py
@@ -19,7 +19,7 @@ def ensure_event_loop():
     """
     try:
         return asyncio.get_event_loop()
-    except AssertionError:
+    except (AssertionError, RuntimeError):
         asyncio.set_event_loop(asyncio.new_event_loop())
 
     # Note: No clever tricks are used here to dry up code
