ccd4b4f2fb02e5037e7a8c06a7643c38d1eaff22
Steven Loria <sloria1@gmail.com>
2015-05-20 11:08:53 -0400

    Fix JS raised when Forward or Dataverse aren't configured
    [skip ci]

--- website/addons/forward/static/node-cfg.js
+++ website/addons/forward/static/node-cfg.js
@@ -1,4 +1,6 @@
 var ForwardConfig = require('./forwardConfig.js');
 
 var url = window.contextVars.node.urls.api + 'forward/config/';
-new ForwardConfig('#forwardScope', url, window.contextVars.node.id);
+if ($('#forwardScope')[0]) {
+    new ForwardConfig('#forwardScope', url, window.contextVars.node.id);
+}
