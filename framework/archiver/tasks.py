9f05fb1284c01abc8a9a146603978a8aeebd2f5c
Sam Chrisinger <s.chrisinger@gmail.com>
2015-05-21 09:58:46 -0400

    Set application context for archiver tasks, and cosmetic cleanup

--- framework/archiver/tasks.py
+++ framework/archiver/tasks.py
@@ -1,6 +1,8 @@
-from celery import group, chain
 import requests
 import json
+from urllib import quote
+from celery import group, chain
+from celery.utils.log import get_task_logger
 
 from framework.tasks import app as celery_app
 from framework.auth.core import User
@@ -16,17 +18,37 @@ from website.project import signals as project_signals
 from website.mails import send_mail
 
 from website import settings
+from website.app import init_addons, do_set_backends
 
 from framework.archiver import (
     ARCHIVER_PENDING,
     ARCHIVER_CHECKING,
     ARCHIVER_SUCCESS,
+    ARCHIVER_SENDING,
+    ARCHIVER_SENT
 )
 from framework.archiver.settings import (
     ARCHIVE_PROVIDER,
     MAX_ARCHIVE_SIZE,
 )
 from framework.archiver.utils import catch_archive_addon_error
+
+logger = get_task_logger(__name__)
+
+def create_app_context():
+    try:
+        init_addons(settings)
+        do_set_backends(settings)
+    except AssertionError:
+        # ignore AssertionErrors
+        pass
+
+def update_status(node, addon, status, meta={}):
+    tmp = node.archived_providers.get(addon) or {}
+    tmp['status'] = status
+    tmp.update(meta)
+    node.archived_providers[addon] = tmp
+    node.save()
 
 def stat_file_tree(addon_short_name, fileobj_metadata, user):
     """Traverse the addon's file tree and collect metadata in AggregateStatResult
@@ -58,7 +80,7 @@ def stat_file_tree(addon_short_name, fileobj_metadata, user):
             meta=fileobj_metadata,
         )
 
-@celery_app.task
+@celery_app.task(name="archiver.stat_addon")
 def stat_addon(addon_short_name, src_pk, dst_pk, user_pk):
     """Collect metadata about the file tree of a given addon
 
@@ -68,12 +90,10 @@ def stat_addon(addon_short_name, src_pk, dst_pk, user_pk):
     :param user_pk: primary key of registration initatior
     :return: AggregateStatResult containing file tree metadata
     """
+    create_app_context()
     src = Node.load(src_pk)
     dst = Node.load(dst_pk)
-    dst.archived_providers[addon_short_name] = {
-        'status': ARCHIVER_CHECKING,
-    }
-    dst.save()
+    update_status(dst, addon_short_name, ARCHIVER_CHECKING)
     src_addon = src.get_addon(addon_short_name)
     user = User.load(user_pk)
     file_tree = src_addon._get_file_tree(user=user)
@@ -84,7 +104,7 @@ def stat_addon(addon_short_name, src_pk, dst_pk, user_pk):
     )
     return result
 
-@celery_app.task
+@celery_app.task(name="archiver.stat_node")
 def stat_node(src_pk, dst_pk, user_pk):
     """Create a celery.group group of #stat_addon subtasks
 
@@ -94,20 +114,21 @@ def stat_node(src_pk, dst_pk, user_pk):
     :return: celery.result.GroupResult containing the collected return
     values of a #stat_addon
     """
+    logger.info("Statting node: {0}".format(src_pk))
+    create_app_context()
     src = Node.load(src_pk)
-    subtasks = group(
+    targets = [src.get_addon(name) for name in settings.ADDONS_ARCHIVABLE]
+    return group(
         stat_addon.si(
             addon.config.short_name,
             src_pk,
             dst_pk,
             user_pk,
         )
-        for addon in src.get_addons()
-        if addon.config.short_name in settings.ADDONS_ARCHIVABLE
-    )
-    return subtasks.apply_async()
+        for addon in targets if addon
+    )()
 
-@celery_app.task
+@celery_app.task(name="archiver.make_copy_request")
 def make_copy_request(dst_pk, url, data):
     """Make the copy request to the WaterBulter API and handle
     successful and fauled responses
@@ -119,16 +140,19 @@ def make_copy_request(dst_pk, url, data):
     """
     dst = Node.load(dst_pk)
     provider = data['source']['provider']
+    update_status(dst, provider, ARCHIVER_SENDING)
+    logger.info("Sending copy request for addon: {0} on node: {1}".format(provider, dst_pk))
     res = requests.post(url, data=json.dumps(data))
+    logger.info("Copy request responded with {2} for addon: {0} on node: {1}".format(provider, dst_pk, res.status_code))
+    update_status(dst, provider, ARCHIVER_SENT)
     if res.status_code not in (200, 201, 202):
         catch_archive_addon_error(dst, provider, errors=[res.json()])
     elif res.status_code in (200, 201):
-        dst.archived_providers[provider]['status'] = ARCHIVER_SUCCESS
-    dst.save()
+        update_status(dst, provider, ARCHIVER_SUCCESS)
     project_signals.archive_callback.send(dst)
 
 
-@celery_app.task
+@celery_app.task(name="archiver.archive_addon")
 def archive_addon(addon_short_name, src_pk, dst_pk, user_pk, stat_result):
     """Archive the contents of an addon by making a copy request to the
     WaterBulter API
@@ -139,18 +163,18 @@ def archive_addon(addon_short_name, src_pk, dst_pk, user_pk, stat_result):
     :param user_pk: primary key of registration initatior
     :return: None
     """
+    logger.info("Archiving addon: {1} on node: {0}".format(src_pk, addon_short_name))
+    create_app_context()
     src = Node.load(src_pk)
     dst = Node.load(dst_pk)
-    dst.archived_providers[addon_short_name] = {
-        'status': ARCHIVER_PENDING,
+    update_status(dst, addon_short_name, ARCHIVER_PENDING, meta={
         'stat_result': str(stat_result),
-    }
-    dst.save()
+    })
     user = User.load(user_pk)
     src_provider = src.get_addon(addon_short_name)
     parent_name = "Archive of {addon}".format(addon=src_provider.config.full_name)
     if hasattr(src_provider, 'folder'):
-        parent_name = parent_name + " ({folder})".format(folder=src_provider.folder)
+        parent_name = parent_name + " ({folder})".format(folder=src_provider.folder.replace('/', '%2F'))
     provider = src_provider.config.short_name
     cookie = user.get_or_create_cookie()
     data = dict(
@@ -169,22 +193,23 @@ def archive_addon(addon_short_name, src_pk, dst_pk, user_pk, stat_result):
         rename=parent_name
     )
     copy_url = settings.WATERBUTLER_URL + '/ops/copy'
-    make_copy_request.si(dst_pk, copy_url, data).apply_async()
+    make_copy_request.si(dst_pk, copy_url, data)()
 
 
-@celery_app.task
+@celery_app.task(name="archiver.archive_node")
 def archive_node(group_result, src_pk, dst_pk, user_pk):
     """First use the result of #stat_node to check disk usage of the
     initated registration, then either fail the registration or
     create a celery.group group of subtasks to archive addons
 
-    :param group_result: a celery.result.GroupResulf containing the collected
+    :param group_result: a celery.result.GroupResult containing the collected
     results from the #stat_addon subtasks spawned in #stat_node
     :param src_pk: primary key of node being registered
     :param dst_pk: primary key of registration node
     :param user_pk: primary key of registration initatior
     :return: None
     """
+    logger.info("Archiving node: {0}".format(src_pk))
     src = Node.load(src_pk)
     dst = Node.load(dst_pk)
     user = User.load(user_pk)
@@ -209,7 +234,7 @@ def archive_node(group_result, src_pk, dst_pk, user_pk):
             result,
         )
         for result in stat_result.targets.values()
-    ).apply_async()
+    )()
 
 @celery_app.task(bind=True, name='archiver.archive')
 def archive(self, src_pk, dst_pk, user_pk):
@@ -225,7 +250,7 @@ def archive(self, src_pk, dst_pk, user_pk):
     dst.archiving = True
     dst.archive_task_id = self.request.id
     dst.save()
-    chain(stat_node.si(src_pk, dst_pk, user_pk), archive_node.s(src_pk, dst_pk, user_pk)).apply_async()
+    chain(stat_node.si(src_pk, dst_pk, user_pk), archive_node.s(src_pk, dst_pk, user_pk))()
 
 
 @celery_app.task
@@ -240,7 +265,8 @@ def send_success_message(dst_pk):
 
     send_mail(
         to_addr=user.username,
-        mail=mails.ARCHIVE_SIZE_EXCEEDED_USER,
+        mail=mails.ARCHIVE_SUCCESS,
         user=user,
         src=dst,
+        mimetype='html',
     )
