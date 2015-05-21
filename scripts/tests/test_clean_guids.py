7e2d9459940762cb13820f65f7aa508edd1288e4
Saman Ehsan <se8ea@virginia.edu>
2015-05-21 13:24:59 -0400

    Add tests for clean_guids script

---
+++ scripts/tests/test_clean_guids.py
@@ -0,0 +1,24 @@
+from modularodm import Q
+from modularodm.exceptions import NoResultsFound
+
+from framework.guid.model import CleanGuid, Guid
+from tests.base import OsfTestCase
+from scripts.clean_guids import remove_current_guids, create_clean_guid_objects
+from nose.tools import *  # noqa PEP8 asserts
+
+
+class TestCleanGuids(OsfTestCase):
+
+    def test_remove_current_guids(self):
+        guid = Guid(_id='guid1')
+        guid.save()
+        clean_guids = remove_current_guids('test_clean_guids.csv')
+        assert_not_in('guid1', clean_guids)
+
+    def test_create_whitelist_db_items(self):
+        clean_list = ['clean']
+        create_clean_guid_objects(clean_list)
+        try:
+            guid = CleanGuid.find_one(Q('_id', 'eq', 'clean'))
+        except NoResultsFound:
+            assert False
