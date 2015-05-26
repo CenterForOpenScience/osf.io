a3422f79367838ac30ddceaadf26dbd045381f20
Sam Chrisinger <s.chrisinger@gmail.com>
2015-05-26 12:07:33 -0400

    Use ** instead of math.pow

--- framework/archiver/settings.py
+++ framework/archiver/settings.py
@@ -1,9 +1,8 @@
-import math
 from datetime import timedelta
 
 ARCHIVE_PROVIDER = 'osfstorage'
 
-MAX_ARCHIVE_SIZE = math.pow(1024, 3)  # 1 GB
+MAX_ARCHIVE_SIZE = 1024 ** 3  # == math.pow(1024, 3) == 1 GB
 MAX_FILE_SIZE = MAX_ARCHIVE_SIZE  # TODO limit file size?
 
 ARCHIVE_TIMEOUT_TIMEDELTA = timedelta(1)  # 24 hours
