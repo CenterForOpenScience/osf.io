89938a772edd389b790b20fc3eccec0cafde06b6
Bryan Gorges <bgorges@cos.io>
2015-05-26 09:26:51 -0400

    Just Intermediate tests
    [skip ci]
    
    Each file has a wb path plus two guids dependent on "/".
    This needs to be removed.

--- website/addons/base/notifications.py
+++ website/addons/base/notifications.py
@@ -3,6 +3,7 @@
 from furl import furl
 from datetime import datetime
 
+from website.models import Guid
 from website.notifications.emails import notify
 
 
@@ -10,21 +11,29 @@ def file_notify(user, node, event, metadata, provider):
     f_url = furl(node.absolute_url)
     path = metadata['path']
     timestamp = datetime.utcnow()
+    f_url.path = node.web_url_for('collect_file_trees')
     if event == 'delete':
         message = 'deleted <strong>"{}"</strong>.'.format(path)  # only has path
-        f_url.path = node.web_url_for('collect_file_trees')
+    elif event == 'create_folder':
+        message = 'added folder <strong>"{}"</strong>.'.format(path)
     else:
         try:
             name = metadata['name']
         except KeyError:
             name = path
-        f_url.path = node.web_url_for('addon_view_or_download_file', path=path, provider=provider)
+        addon = node.get_addon(provider)
+        file_guid, created = addon.find_or_create_file_guid(path)
+        guid_object = Guid.load(file_guid)
+        print guid_object
+
+        file_guid, created = addon.find_or_create_file_guid('/' + path)
+        guid_object = Guid.load(file_guid)
+        print guid_object
+        f_url.path = file_guid.guid_url
         if event == 'create':
             message = 'uploaded file <strong>"{}"</strong>.'.format(name)
         elif event == 'update':
             message = 'updated file <strong>"{}"</strong>.'.format(name)
-        elif event == 'create_folder':
-            message = 'added folder <strong>"{}"</strong>.'.format(name)
 
     notify(
         uid=node._id,
