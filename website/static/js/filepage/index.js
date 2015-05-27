f8b516e4e717a3ddb2fb2af72453edd138d8d6dc
Chris Seto <chriskseto@gmail.com>
2015-05-27 18:42:47 -0400

    Gui updates add download and upload buttons

--- website/static/js/filepage/index.js
+++ website/static/js/filepage/index.js
@@ -1,5 +1,7 @@
 var $ = require('jquery');
 var m = require('mithril');
+var bootbox = require('bootbox');
+var $osf = require('js/osfHelpers');
 var waterbutler = require('js/waterbutler');
 
 // Local requires
@@ -36,17 +38,58 @@ var FileViewPage = {
             });
         };
 
+        self.deleteFile = function() {
+            bootbox.confirm({
+                title: 'Delete file?',
+                message: '<p class="overflow">' +
+                        'Are you sure you want to delete <strong>' +
+                        self.file.safeName + '</strong>?' +
+                    '</p>',
+                callback: function(confirm) {
+                    if (!confirm) return;
+                    $.ajax({
+                        type: 'DELETE',
+                        url: self.file.urls.delete,
+                        beforeSend: $osf.setXHRAuthorization
+                    }).done(function() {
+                        window.location = self.node.urls.files;
+                    }).fail(function() {
+                        $osf.growl('Error', 'Could not delete file.');
+                    });
+                }
+            });
+        };
+
+        self.downloadFile = function() {
+            window.location = self.file.urls.content;
+            return false;
+        };
+
+        revisionsHeader = m('.row', [
+            m('.col-md-6', 'Revisions'),
+            m('.col-md-6', [
+                m('.pull-right', [
+                    m('button.btn.btn-danger.btn-sm', {onclick: self.deleteFile}, 'Delete'),
+                    m('button.btn.btn-success.btn-sm', {onclick: self.downloadFile}, 'Download')
+                ])
+            ])
+        ]);
+
+        treeHeader = m('.row', m('.col-md-12', m('#filesSearch')));
+        viewHeader = [m('i.fa.fa-eye'), ' View'];
+        editHeader = [m('i.fa.fa-pencil-square-o'), ' Edit'];
+
         self.panels = [
-            Panel('Tree', FileTree, [self.node.urls.api]),
-            Panel('Edit', FileEditor, [self.file.urls.content, self.reloadFile]),
-            Panel('View', FileRenderer, [self.file.urls.render, self.file.urls.sharejs, self.context.editorMeta, self.reloadFile], true),
-            Panel('Revisions', FileRevisionsTable, [self.file, self.node], true),
+            new Panel('Tree', treeHeader, FileTree, [self.node.urls.api], true),
+            new Panel('Edit', editHeader, FileEditor, [self.file.urls.content, self.reloadFile]),
+            new Panel('View', viewHeader, FileRenderer, [self.file.urls.render, self.file.urls.sharejs, self.context.editorMeta, self.reloadFile], true),
+            new Panel('Revisions', revisionsHeader, FileRevisionsTable, [self.file, self.node], true),
         ];
 
     },
     view: function(ctrl) {
         return m('.file-view-page', [
-            m.component(PanelToggler, ctrl.panels)
+            m.component(PanelToggler, m('h3', ctrl.file.name), ctrl.panels)
         ]);
     }
 };
