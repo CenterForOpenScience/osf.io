c392955e71eab6dc0111a97414436feb532c2fe0
Chris Seto <chriskseto@gmail.com>
2015-05-27 06:36:10 -0400

    Attempt to bind treebeard as if it was a jquery plugin

--- website/static/js/filepage/tree.js
+++ website/static/js/filepage/tree.js
@@ -1,10 +1,45 @@
 var m = require('mithril');
+var $osf = require('js/osfHelpers');
+var fileBrowser = require('js/fileViewTreebeard');
 
 var FileTree = {
-    controller: function() {
+    controller: function(nodeApiUrl) {
+        var self = this;
+        self.load = false;
+        self.url = nodeApiUrl + 'files/grid/';
+
+        $.ajax({
+            method: 'GET',
+            url: self.url,
+            beforeSend: $osf.setXHRAuthorization,
+        })
+        .done(function (data) {
+            self.data = data;
+            self.loaded = true;
+        });
+
+        self.bindFangorn = function(element, isInitialized, context) {
+            if (isInitialized) return;
+            new fileBrowser(self.data);
+        };
+
     },
-    view: function() {
-        return m('', 'TODO');
+    view: function(ctrl) {
+        if (!ctrl.loaded) {
+            return m('#grid', [
+                m('.fangorn-loading', [
+                    m('.logo-spin.text-center', [
+                        m('img[src="/static/img/logo_spin.png"alt=loader]')
+                    ]),
+                    m('p.m-t-sm-fg.load-message', 'Loading files...  ')
+                ])
+            ]);
+        }
+
+        return m('', [
+            m('#filesSearch'),
+            m('#grid', {config: ctrl.bindFangorn})
+        ]);
     }
 };
 
