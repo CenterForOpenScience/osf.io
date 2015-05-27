921728f1efd93395c3fd44f764d6951ca7bc128b
Chris Seto <chriskseto@gmail.com>
2015-05-27 01:27:57 -0400

    Add a helper for turning bytes in to a human readable size

--- website/static/js/osfHelpers.js
+++ website/static/js/osfHelpers.js
@@ -498,6 +498,23 @@ ko.bindingHandlers.listing = {
     }
 };
 
+// Thanks to https://stackoverflow.com/questions/10420352/converting-file-size-in-bytes-to-human-readable
+function humanFileSize(bytes, si) {
+    var thresh = si ? 1000 : 1024;
+    if(Math.abs(bytes) < thresh) {
+        return bytes + ' B';
+    }
+    var units = si ?
+        ['kB','MB','GB','TB','PB','EB','ZB','YB'] :
+        ['KiB','MiB','GiB','TiB','PiB','EiB','ZiB','YiB'];
+    var u = -1;
+    do {
+        bytes /= thresh;
+        ++u;
+    } while(Math.abs(bytes) >= thresh && u < units.length - 1);
+    return bytes.toFixed(1) + ' ' + units[u];
+}
+
 
 // Also export these to the global namespace so that these can be used in inline
 // JS. This is used on the /goodbye page at the moment.
@@ -520,5 +537,6 @@ module.exports = window.$.osf = {
     throttle: throttle,
     debounce: debounce,
     htmlEscape: htmlEscape,
-    tableResize: tableResize
+    tableResize: tableResize,
+    humanFileSize: humanFileSize
 };
