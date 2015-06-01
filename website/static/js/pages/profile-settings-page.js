56d758078c24f41b799a16b629fa4622c021dafa
Patrick Gorman <gormanpe@vcu.edu>
2015-06-01 16:01:02 -0400

    Removed menu methods from profile-settings-page and made separate file

--- website/static/js/pages/profile-settings-page.js
+++ website/static/js/pages/profile-settings-page.js
@@ -7,17 +7,4 @@ new profile.Social('#social', ctx.socialUrls, ['edit']);
 new profile.Jobs('#jobs', ctx.jobsUrls, ['edit']);
 new profile.Schools('#schools', ctx.schoolsUrls, ['edit']);
 
-// Reusable function to fix affix widths to columns.
-function fixAffixWidth() {
-    $('.affix, .affix-top, .affix-bottom').each(function (){
-        var el = $(this);
-        var colsize = el.parent('.affix-parent').width();
-        el.outerWidth(colsize);
-    });
-}
 
-
-$(document).ready(function() {
-    $(window).resize(function (){ fixAffixWidth(); });
-    $('.profile-page .panel').on('affixed.bs.affix', function(){ fixAffixWidth();});
-});
