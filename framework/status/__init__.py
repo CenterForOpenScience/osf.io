e9e99594a667b8795c93a852a00e55f5007bb56d
Andy Boughton <abought@gmail.com>
2015-06-02 11:14:24 -0400

    Add support for safe/unsafe status alert messages in alert.mako

--- framework/status/__init__.py
+++ framework/status/__init__.py
@@ -4,7 +4,7 @@ from collections import namedtuple
 
 from framework.sessions import session
 
-Status = namedtuple('Status', ['message', 'css_class', 'dismissible'])
+Status = namedtuple('Status', ['message', 'css_class', 'dismissible', 'safe'])
 
 #: Status_type => bootstrap css class
 TYPE_MAP = {
@@ -16,12 +16,16 @@ TYPE_MAP = {
     'danger': 'danger',
 }
 
-def push_status_message(message, kind='warning', dismissible=True):
+def push_status_message(message, kind='warning', dismissible=True, safe=True):
+    # TODO: Change the default to safe=False once conversion to markupsafe rendering is complete
     statuses = session.data.get('status')
     if not statuses:
         statuses = []
     css_class = TYPE_MAP.get(kind, 'warning')
-    statuses.append(Status(message=message, css_class=css_class, dismissible=dismissible))
+    statuses.append(Status(message=message,
+                           css_class=css_class,
+                           dismissible=dismissible,
+                           safe=safe))
     session.data['status'] = statuses
 
 def pop_status_messages(level=0):
--- website/templates/alert.mako
+++ website/templates/alert.mako
@@ -1,12 +1,17 @@
 <div id="alert-container">
-% for message, css_class, dismissible in status:
+% for message, css_class, dismissible, safe in status:
       <div class='alert alert-block alert-${css_class} fade in'>
         % if dismissible:
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
         % endif
-        <p>${message}</p>
+
+        % if safe:
+          <p>${message | n}</p>
+        % else:
+          <p>${message}</p>
+        % endif
       </div>
 % endfor
 </div>
