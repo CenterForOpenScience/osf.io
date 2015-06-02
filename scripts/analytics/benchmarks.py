9d3e14eb8d140fb5408179d0c6bef01f7023e090
John Tordoff <johnetordoff@gmail.com>
2015-06-02 09:21:27 -0400

    This merges the two scripts and removes the now redundant metrics.py

--- scripts/analytics/benchmarks.py
+++ scripts/analytics/benchmarks.py
@@ -16,6 +16,7 @@ from website.app import init_app
 from website.models import User, Node, PrivateLink
 from website.addons.dropbox.model import DropboxUserSettings
 from website.addons.osfstorage.model import OsfStorageFileNode
+from website.addons.osfstorage.model import OsfStorageTrashedFileNode
 
 from scripts.analytics import profile, tabulate_emails, tabulate_logs
 
@@ -139,6 +140,93 @@ def get_log_counts(users):
 
 
 def main():
+
+    number_users = User.find().count()
+
+    projects = Node.find(
+        Q('category', 'eq', 'project') &
+        Q('is_deleted', 'eq', False) &
+        Q('is_folder', 'ne', True)
+    )
+    projects_forked = list(Node.find(
+        Q('category', 'eq', 'project') &
+        Q('is_deleted', 'eq', False) &
+        Q('is_folder', 'ne', True) &
+        Q('is_fork', 'eq', True)
+    ))
+    projects_registered = Node.find(
+        Q('category', 'eq', 'project') &
+        Q('is_deleted', 'eq', False) &
+        Q('is_folder', 'ne', True) &
+        Q('is_registration', 'eq', True)
+    )
+
+    pf = []
+    for p in projects_forked:
+        if not p.contributors[0]:
+            continue
+        name = p.contributors[0].fullname
+        if unicode(name) not in [u'Jeffres R. Spies', 'Brian A. Nosek']:
+            pf.append(p)
+
+    pr = []
+    for p in projects_registered:
+        name = p.contributors[0].fullname
+        if not p.contributors[0]:
+            continue
+        if not unicode(name)==u'Jeffrey R. Spies' and not unicode(name)==u'Brian A. Nosek':
+            pr.append(p)
+
+    number_projects = len(projects)
+    number_projects_public = Node.find(
+        Q('category', 'eq', 'project') &
+        Q('is_deleted', 'eq', False) &
+        Q('is_folder', 'ne', True) &
+        Q('is_public', 'eq', True)
+    ).count()
+    number_projects_forked = len(pf)
+
+    number_projects_registered = len(pr)
+
+    ##############
+
+    number_downloads_total = 0
+    number_downloads_unique = 0
+
+    contributors_per_project = []
+
+    contrib = {}
+
+    for project in projects:
+        contributors_per_project.append(len(project.contributors))
+        for person in project.contributors:
+            if not person:
+                continue
+            if person._id not in contrib:
+                contrib[person._id] = []
+            for neighbor in project.contributors:
+                if not neighbor:
+                    continue
+                if neighbor._id not in contrib[person._id]:
+                    contrib[person._id].append(neighbor._id)
+
+        addon = project.get_addon('osfstorage')
+
+        for filenode in OsfStorageFileNode.find(Q('node_settings', 'eq', addon) & Q('kind', 'eq', 'file')):
+            for idx, version in enumerate(filenode.versions):
+                page = ':'.join(['download', project._id, filenode._id, str(idx)])
+                unique, total = get_basic_counters(page)
+                number_downloads_total += total or 0
+                number_downloads_unique += unique or 0
+
+        for filenode in OsfStorageTrashedFileNode.find(Q('node_settings', 'eq', addon) & Q('kind', 'eq', 'file')):
+            for idx, version in enumerate(filenode.versions):
+                page = ':'.join(['download', project._id, filenode._id, str(idx)])
+                unique, total = get_basic_counters(page)
+                number_downloads_total += total or 0
+                number_downloads_unique += unique or 0
+
+
     active_users = get_active_users()
     active_users_invited = get_active_users(Q('is_invited', 'eq', True))
     dropbox_metrics = get_dropbox_metrics()
@@ -152,6 +240,15 @@ def main():
     nodes_at_least_3 = count_at_least(node_counts, 3)
 
     rows = [
+        ['number_users', number_users],
+        ['number_projects', number_projects],
+        ['number_projects_public', number_projects_public],
+        ['number_projects_forked', number_projects_forked],
+        ['number_projects_registered', number_projects_registered],
+        ['number_downloads_total', number_downloads_total],
+        ['number_downloads_unique', number_downloads_unique],
+
+
         ['active-users', active_users.count()],
         ['active-users-invited', active_users_invited.count()],
         ['dropbox-users-enabled', len(dropbox_metrics['enabled'])],
