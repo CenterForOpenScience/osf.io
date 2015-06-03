7936edbd5497f2eb1e32ac264b9837bd5b7d318e
Revanth <revanth@Admins-MacBook-Air-2.local>
2015-06-03 11:45:21 -0400

    Comparing email addresses during registration is now case insensitive

--- framework/auth/views.py
+++ framework/auth/views.py
@@ -241,7 +241,7 @@ def register_user(**kwargs):
 
     """
     # Verify email address match
-    if request.json['email1'] != request.json['email2']:
+    if str(request.json['email1']).lower() != str(request.json['email2']).lower():
         raise HTTPError(
             http.BAD_REQUEST,
             data=dict(message_long='Email addresses must match.')
