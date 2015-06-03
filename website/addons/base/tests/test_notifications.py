b7b51e5c673231196e4af6fa235422d28aaafb87
Bryan Gorges <bgorges@cos.io>
2015-06-03 15:51:26 -0400

    Outline tests
    [skip ci]

--- website/addons/base/tests/test_notifications.py
+++ website/addons/base/tests/test_notifications.py
@@ -4,35 +4,71 @@ import unittest
 from nose.tools import *
 import mock
 
-
+from framework.auth import Auth
 from website.util import api_url_for, web_url_for
 from tests.base import OsfTestCase
+from tests import factories
 
+from furl import furl
 from website.addons.base import notifications
 
-class TestNotifications(OsfTestCase):
 
+class TestNotifications(OsfTestCase):
     def setUp(self):
-        pass
+        super(TestNotifications, self).setUp()
+        self.user = factories.UserFactory()
+        self.consolidate_auth = Auth(user=self.user)
+        self.node = factories.ProjectFactory(creator=self.user)
+        self.move_copy_payload = dict(
+            destination=dict(
+                node={"title": self.node.title},
+                path='string',
+                provider='osfstorage',
+                materialized='other/that.txt',
+                addon='OSF Storage'
+            ),
+            source=dict(
+                node={"_id": self.node._id, "title": self.node.title},
+                provider='osfstorage',
+                materialized='this/that.txt',
+                addon='OSF Storage'
+            )
+        )
+        self.other_payload = dict(
+            provider='osfstorage',
+            metadata=dict(
+                path='string',
+                materialized='this/that.txt'
+            )
+        )
+        self.f_url = furl(self.node.absolute_url)
+
+    # Should test all the event options.
+    @mock.patch('website.notifications.emails.notify')
+    def test_file_notify(self, notify):
+        event = 'file_added'
+        payload = self.other_payload
+        notifications.file_notify(self.user, self.node, event, payload)
+        assert_true(notify.called)
 
     def test_file_info(self):
-        pass
+        notifications.file_info(self.node, self.other_payload['metadata']['path'], self.other_payload['provider'])
 
     def test_file_created(self):
-        pass
+        notifications.file_created(self.node, self.f_url, self.other_payload)
 
     def test_file_updated(self):
-        pass
+        notifications.file_updated(self.node, self.f_url, self.other_payload)
 
     def test_file_deleted(self):
-        pass
+        notifications.file_deleted(self.node, self.f_url, self.other_payload)
 
-    def test_file_added(self):
-        pass
+    def test_folder_added(self):
+        notifications.folder_added(self.node, self.f_url, self.other_payload)
 
     # mock call to updating subscription
     def test_file_moved(self):
-        pass
+        notifications.file_moved(self.node, self.f_url, self.move_copy_payload, self.user)
 
     def test_file_copied(self):
-        pass
+        notifications.file_copied(self.node, self.f_url, self.move_copy_payload)
