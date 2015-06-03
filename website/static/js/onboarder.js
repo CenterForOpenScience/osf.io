fd32db975467769e259368c1c5e714c2d9de522c
geeksnglitter <geeksnglitter@gmail.com>
2015-06-03 11:07:47 -0400

    Add warning if user tries to leave page without finishing upload. Use beforeunload. Should be switched to a bootbox.

--- website/static/js/onboarder.js
+++ website/static/js/onboarder.js
@@ -576,6 +576,14 @@ function OBUploaderViewModel(params) {
         }
     };
     self.dropzone = new Dropzone(self.selector, dropzoneOpts);
+
+        //stop user from leaving if file is staged for upload
+        $(window).on('beforeunload', function() {
+            if(!self.enableUpload()) {
+                return 'You have a pending upload. If you leave' +
+                    'the page now, your file will not be stored.';
+            }
+        });
 }
 
 ko.components.register('osf-ob-uploader', {
