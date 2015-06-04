0bf88c740cb707a139269efd0f00cf8fc4e41016
Billy Hunt <Billy@billyhunt.com>
2015-06-04 14:31:28 -0400

    added permissions file for custom permissions

--- api/users/permissions.py
+++ api/users/permissions.py
@@ -1,24 +1,13 @@
 from website.models import User
 from rest_framework import permissions
 
-from framework.auth import Auth
 
-
-def get_user_auth(request):
-    user = request.user
-    if user.is_anonymous():
-        auth = Auth(None)
-    else:
-        auth = Auth(user)
-    return auth
-
-
-class AuthorizedUserOrPublic(permissions.BasePermission):
+class ReadOnlyOrCurrentUser(permissions.BasePermission):
 
     def has_user_permission(self, request, view, obj):
         assert isinstance(obj, User), 'obj must be a User, got {}'.format(obj)
-        auth = get_user_auth(request)
+        request_user = request.user
         if request.method in permissions.SAFE_METHODS:
-            return obj.is_public
+            return True
         else:
-            return obj.can_edit(auth)
+            return obj == request_user
