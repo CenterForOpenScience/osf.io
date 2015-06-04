990e87a1569330fccd4ad1048ccd5d580bb2d594
Patrick Gorman <gormanpe@vcu.edu>
2015-06-04 11:33:03 -0400

    Removed fixAffixWidth() from profile-settings-addons-page.js

--- website/static/js/pages/profile-settings-addons-page.js
+++ website/static/js/pages/profile-settings-addons-page.js
@@ -104,18 +104,3 @@ $('.addon-oauth').each(function(index, elem) {
     viewModel.updateAccounts();
 });
 
-//Fixes profile settings side menu to left column
-function fixAffixWidth() {
-    $('.affix, .affix-top, .affix-bottom').each(function (){
-        var el = $(this);
-        var colsize = el.parent('.affix-parent').width();
-        el.outerWidth(colsize);
-    });
-}
-
-
-$(document).ready(function() {
-    $(window).resize(function (){ fixAffixWidth(); });
-    $('.profile-page .panel').on('affixed.bs.affix', function(){ fixAffixWidth();});
-});
-
