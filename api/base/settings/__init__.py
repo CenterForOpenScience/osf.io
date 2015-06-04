8a778750c2284045566c6f67b2aedffd2811f1ce
Brian J. Geiger <bgeiger@pobox.com>
2015-06-04 14:08:13 -0400

    Put in at least two spaces before inline comment

--- api/base/settings/__init__.py
+++ api/base/settings/__init__.py
@@ -6,10 +6,10 @@
     >>> settings.API_BASE
     'v2/'
 '''
-from .defaults import * # noqa
+from .defaults import *  # noqa
 
 try:
-    from .local import * # noqa
+    from .local import *  # noqa
 except ImportError as error:
     raise ImportError("No api/base/settings/local.py settings file found. Did you remember to "
                         "copy local-dist.py to local.py?")
