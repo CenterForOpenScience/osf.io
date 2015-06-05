1aa96c29eb2f51522d2c691ef7c7a6825fbf35dc
Gage Gaskins <tgg8ec@virginia.edu>
2015-06-05 15:44:00 -0400

    Added initializeResponsiveAffix() to Getting Started page

--- website/static/js/pages/gettingstarted-page.js
+++ website/static/js/pages/gettingstarted-page.js
@@ -1,17 +1,7 @@
 'use strict';
 
-// Reusable function to fix affix widths to columns.
-function fixAffixWidth() {
-    $('.affix, .affix-top, .affix-bottom').each(function (){
-        var el = $(this);
-        var colsize = el.parent('.affix-parent').width();
-        el.outerWidth(colsize);
-    });
-}
+var $osf = require('js/osfHelpers.js')
 
 $(document).ready(function () {
-
-    $(window).resize(function (){ fixAffixWidth(); });
-    $('.scrollspy').on('affixed.bs.affix', function(){ fixAffixWidth(); });
-
-  });
+    $osf.initializeResponsiveAffix();
+})
