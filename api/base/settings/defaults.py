080ca8f63ffba7d6a971e530e418ca60508fa118
Brian J. Geiger <bgeiger@pobox.com>
2015-06-05 11:20:44 -0400

    Revert "Feature/apiv2 url"

--- api/base/settings/defaults.py
+++ api/base/settings/defaults.py
@@ -118,8 +118,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/vendor')
 API_PATH = ''
 API_BASE = 'v2/'
 
-API_PREFIX = '{}/{}'.format(API_PATH, API_BASE)
-STATIC_URL = '{}static/'.format(API_PREFIX)
+
+STATIC_URL = '{}/{}static/'.format(API_PATH, API_BASE)
 
 STATICFILES_DIRS = (
     ('rest_framework_swagger/css', os.path.join(BASE_DIR, 'static/css')),
