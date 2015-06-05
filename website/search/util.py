3fc7b18c8697e57a173c22ea290eb97343ed3972
Brandon Purvis <bcp7cc@virginia.edu>
2015-06-05 08:49:41 -0400

    Move field weight boosts into constants.

--- website/search/util.py
+++ website/search/util.py
@@ -22,6 +22,10 @@ RE_XML_ILLEGAL = u'([\u0000-\u0008\u000b-\u000c\u000e-\u001f\ufffe-\uffff])' + \
 RE_XML_ILLEGAL_COMPILED = re.compile(RE_XML_ILLEGAL)
 
 
+TITLE_WEIGHT = 2
+DESCRIPTION_WEIGHT = 1.2
+
+
 def build_query(qs='*', start=0, size=10, sort=None):
     query = {
         'query': build_query_string(qs),
@@ -40,10 +44,17 @@ def build_query(qs='*', start=0, size=10, sort=None):
 
 # Match queryObject in search.js
 def build_query_string(qs):
+    field_boosts = {
+        'title': TITLE_WEIGHT,
+        'description': DESCRIPTION_WEIGHT,
+        '_all': 1,
+    }
+
+    fields = ['{}^{}'.format(k, v) for k,v in field_boosts.iteritems()]
     return {
         'query_string': {
             'default_field': '_all',
-            'fields': ['title^2', '_all', 'description^1.2'],
+            'fields': fields,
             'query': qs,
             'analyze_wildcard': True,
             'auto_generate_phrase_queries': True,
