9b5dfe5b9fb9d90a255ce62cd59dea86d3c91ccf
Sam Chrisinger <s.chrisinger@gmail.com>
2015-06-08 11:47:00 -0400

    Use celery.group instead of celery.chain, and use apply_async
    Celery chains fail (i.e. stops execution) if any of the subtasks
    fail. This is likely not the desired behavior here. Additionally (as far
    as I understand the Celery API {@chrisseto comments here?}) the
    explicit call of celery.chain(...)() will call the task directly in a
    blocking
    fashion. http://celery.readthedocs.org/en/latest/userguide/canvas.html#signatures
    Again, this does not seem like the correct behavior here.

--- framework/tasks/handlers.py
+++ framework/tasks/handlers.py
@@ -4,7 +4,7 @@ import logging
 import functools
 
 from flask import g
-from celery import chain
+from celery import group
 
 from website import settings
 
@@ -22,7 +22,7 @@ def celery_teardown_request(error=None):
     try:
         tasks = g._celery_tasks
         if tasks:
-            chain(*tasks)()
+            group(task for task in tasks).apply_async()
     except AttributeError:
         if not settings.DEBUG_MODE:
             logger.error('Task queue not initialized')
