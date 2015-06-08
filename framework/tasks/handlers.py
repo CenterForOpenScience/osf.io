af22c61d207914cf39ebf8b38817a6e32dfc4596
Sam Chrisinger <s.chrisinger@gmail.com>
2015-06-08 14:54:43 -0400

    Use explicit iterator

--- framework/tasks/handlers.py
+++ framework/tasks/handlers.py
@@ -22,7 +22,7 @@ def celery_teardown_request(error=None):
     try:
         tasks = g._celery_tasks
         if tasks:
-            group(task for task in tasks).apply_async()
+            group(tasks.iter()).apply_async()
     except AttributeError:
         if not settings.DEBUG_MODE:
             logger.error('Task queue not initialized')
