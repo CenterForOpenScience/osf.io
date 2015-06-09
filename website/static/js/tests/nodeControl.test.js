588638c4cb9b857f095e7bfb36d1ee6b7fe45425
Rohan Bajaj <rb3kn@virginia.edu>
2015-06-09 13:05:18 -0400

    Update nodeControl.test.js

--- website/static/js/tests/nodeControl.test.js
+++ website/static/js/tests/nodeControl.test.js
@@ -71,7 +71,7 @@ describe('nodeControl', () => {
                 assert.equal(vm.arkUrl(), 'http://ezid.cdlib.org/id/ark:/24601');
             });
             it('creates new identifiers', (done) => {
-                vm.createIdentifiers( => {
+                vm.createIdentifiers()always( => {
                     assert.equal(vm.doi(), '24601');
                     assert.equal(vm.ark(), '24601');
                     done();
