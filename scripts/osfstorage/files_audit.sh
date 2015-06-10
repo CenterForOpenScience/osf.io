cbf82051e15aaeae9b84163528d38365d7f8695a
icereval <icereval@gmail.com>
2015-06-10 12:19:19 -0400

    osf storage audit scripts, standardize cron.py on shell scripts

---
+++ scripts/osfstorage/files_audit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export HOME=$(mktemp)
+cd /opt/apps/osf
+source /opt/data/envs/osf/bin/activate
+
+python -m scripts.osfstroage.files_audit 4 0 &
+python -m scripts.osfstroage.files_audit 4 1 &
+python -m scripts.osfstroage.files_audit 4 2 &
+python -m scripts.osfstroage.files_audit 4 3 &
+wait
---
+++ scripts/osfstorage/glacier_audit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export HOME=$(mktemp)
+cd /opt/apps/osf
+source /opt/data/envs/osf/bin/activate
+
+python -m scripts.osfstorage.glacier_audit
