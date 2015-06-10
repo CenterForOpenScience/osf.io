96355fcabf302fcbfb5ca49f59f9a6abc756cc2e
icereval <icereval@gmail.com>
2015-06-10 13:30:22 -0400

    typo

--- scripts/osfstorage/files_audit.sh
+++ scripts/osfstorage/files_audit.sh
@@ -7,8 +7,8 @@ export HOME=$TEMPDIR
 cd /opt/apps/osf
 source /opt/data/envs/osf/bin/activate
 
-python -m scripts.osfstroage.files_audit 4 0 &
-python -m scripts.osfstroage.files_audit 4 1 &
-python -m scripts.osfstroage.files_audit 4 2 &
-python -m scripts.osfstroage.files_audit 4 3 &
+python -m scripts.osfstorage.files_audit 4 0 &
+python -m scripts.osfstorage.files_audit 4 1 &
+python -m scripts.osfstorage.files_audit 4 2 &
+python -m scripts.osfstorage.files_audit 4 3 &
 wait
