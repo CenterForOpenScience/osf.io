CenterForOpenScience/osf.io

commit 7d8e492c4838daca0e15ef082678ea62c407607f

Changed how csl returns the citation, changed css so that it would show in the widget, and changed what is copied to clipboard in order to preserve formatting such as italics

website/static/js/citationGrid.js
@@ -117,7 +117,7 @@ var makeButtons = function(item, col, buttons) {
                         class: button.css,
                         'data-toggle': 'tooltip',
                         'data-placement': 'bottom',
-                        'data-clipboard-text': button.clipboard,
+                        'data-clipboard-target': item.data.csl.id,
                         config: mergeConfigs(button.config, tooltipConfig),
                         onclick: button.onclick ?
                             function(event) {
@@ -355,7 +355,7 @@ CitationGrid.prototype.makeBibliography = function(folder) {
             return child.data.csl;
         })
     );
-    var citeproc = citations.makeCiteproc(this.styleXml, data, 'text');
+    var citeproc = citations.makeCiteproc(this.styleXml, data, 'html');
     var bibliography = citeproc.makeBibliography();
     if (bibliography[0].entry_ids) {
         return utils.reduce(
@@ -400,7 +400,9 @@ CitationGrid.prototype.resolveRowAux = function(item) {
                 return item.data.name;
             }
             else {
-                return self.getCitation(item);
+                return m("span", {id: item.data.csl.id}, [
+                    m.trust(self.getCitation(item))
+                        ]);
             }
         }
     }, {

website/static/css/site.css
@@ -1988,6 +1988,10 @@ div.profile-fullname{
 /* Project Dashboard */
 
 
+.csl-entry {
+    display: inline;
+}
+
 .addon-widget-container {
     border:1px solid #ddd;
     margin-bottom: 20px;

commit 00a24e690bea8ad2e938cefbcc74850c16dc427b

Fix regression preventing subfolder expansion in citationgrids.
Regression was introduced in 7d8e492c4838daca0e15ef082678ea62c407607f.
The assumption was made that all items in the returned dataset would
have an item[data][csl] value - but folders do not. To resolve, I've
added a conditional that first checks for the existence of the key, and
falls back to the previous method of action.

website/static/js/citationGrid.js
@@ -117,7 +117,7 @@ var makeButtons = function(item, col, buttons) {
                         class: button.css,
                         'data-toggle': 'tooltip',
                         'data-placement': 'bottom',
-                        'data-clipboard-target': item.data.csl.id,
+                        'data-clipboard-target': item.data.csl ? item.data.csl.id : button.clipboard,
                         config: mergeConfigs(button.config, tooltipConfig),
                         onclick: button.onclick ?
                             function(event) {