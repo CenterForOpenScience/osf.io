e2af7cc5f2f0f9129cbd6e1191c7254ee12a7fa3
haoyuchen1992 <hc6as@virginia.edu>
2015-06-12 11:06:13 -0400

    Add home-page css

--- website/static/js/pages/home-page.js
+++ website/static/js/pages/home-page.js
@@ -1,3 +1,7 @@
+// for public activity page under explore nav-tag
+require('../../css/pages/home-page.css');
+
+
 var SignUp = require('../signUp.js');
 
 new SignUp('#signUpScope', '/api/v1/register/');
