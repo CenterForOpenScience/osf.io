0ca7bcb1654efe162067595cdff18ae9aa3f3b3f
Caner Uguz <caneruguz@gmail.com>
2015-06-15 11:33:07 -0400

    Fix broken file references [skip ci]

--- website/static/js/pages/wiki-edit-page.js
+++ website/static/js/pages/wiki-edit-page.js
@@ -4,7 +4,7 @@ var $ = require('jquery');
 var Raven = require('raven-js');
 require('bootstrap-editable');
 require('osf-panel');
-require('../../css/page/wiki-page.css');
+require('../../css/pages/wiki-page.css');
 
 var WikiPage = require('wikiPage');
 
