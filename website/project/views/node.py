bb706d68f7189ac371c7c327aa6182d74353a928
Sam Chrisinger <s.chrisinger@gmail.com>
2015-06-17 14:22:47 -0400

    Filter out deleted registrations

--- website/project/views/node.py
+++ website/project/views/node.py
@@ -1007,7 +1007,7 @@ def get_forks(auth, node, **kwargs):
 
 @must_be_contributor_or_public
 def get_registrations(auth, node, **kwargs):
-    registrations = node.node__registrations  # get all registratioms, including archiving
+    registrations = [n for n in node.node__registrations if not n.is_deleted]  # get all registrations, including archiving
     return _render_nodes(registrations, auth)
 
 
