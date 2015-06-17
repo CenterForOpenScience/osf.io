2c612a1fb40f0de1a3b4b53525e2cbc861c3803b
Gage Gaskins <tgg8ec@virginia.edu>
2015-06-17 15:17:13 -0400

    More jshint fixes

--- website/static/js/pages/render-nodes.js
+++ website/static/js/pages/render-nodes.js
@@ -1,9 +1,12 @@
 'use strict';
 
 var $osf = require('js/osfHelpers');
+var $ = require('jquery');
 
 // model for components, due to simplicity did not create a new file
 var ComponentControl = {};
 
 // binds to component scope in render_nodes.mako
-$osf.applyBindings(ComponentControl, '#componentScope');
+$('.render-nodes-list').each(function() {
+    $osf.applyBindings(ComponentControl, this);
+});
