96c75f3079a327ec4f8dc8fe90153efd48b72082
Steven Loria <sloria1@gmail.com>
2015-06-18 17:11:56 -0400

    Fixes to cleanup script
    [skip ci]

--- scripts/cleanup_failed_registrations.py
+++ scripts/cleanup_failed_registrations.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
 from datetime import datetime
+import logging
 
 from modularodm import Q
 
@@ -10,23 +11,29 @@ from website.archiver import (
 )
 from website.settings import ARCHIVE_TIMEOUT_TIMEDELTA
 from website.archiver.utils import handle_archive_fail
-
+from website.app import init_app
 from website.project.model import Node
 
+from scripts import utils as script_utils
+
+
+logger = logging.getLogger(__name__)
+
 def find_failed_registrations():
-    expired_if_before = datetime.now() - ARCHIVE_TIMEOUT_TIMEDELTA
+    expired_if_before = datetime.utcnow() - ARCHIVE_TIMEOUT_TIMEDELTA
     query = (
         Q('is_deleted', 'eq', False) &
         Q('is_registration', 'eq', True) &
         Q('registered_date', 'lt', expired_if_before) &
         Q('__backrefs.active.archivejob', 'exists', True)
-    )    
+    )
     return [node for node in Node.find(query) if node.archive_job.status != ARCHIVER_SUCCESS]
 
 def remove_failed_registrations(dry_run=True):
     failed = find_failed_registrations()
     if not dry_run:
         for f in failed:
+            logging.info('Cleaning {}'.format(f))
             handle_archive_fail(
                 ARCHIVER_NETWORK_ERROR,
                 f.registered_from,
@@ -34,10 +41,14 @@ def remove_failed_registrations(dry_run=True):
                 f.creator,
                 f.archived_providers
             )
+    logging.info('Cleaned {} registrations'.format(len(failed)))
 
 def main():
+    init_app(set_backends=True, routes=False)
     flags = ['dry_run']
-    args = {arg: True for arg in sys.argv if arg.lstrip('--') in flags}
+    args = {arg.lstrip('--'): True for arg in sys.argv if arg.lstrip('--') in flags}
+    if not args['dry']:
+        script_utils.add_file_logger(logger, __file__)
     remove_failed_registrations(*args)
 
 if __name__ == '__main__':
