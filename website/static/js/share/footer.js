1feedbe7435128d5c1b662ca79e588698fd47c4c
Fabian von Feilitzsch <fabian@fabianism.us>
2015-06-18 09:53:27 -0400

    add Footer with provider list to SHARE search page

---
+++ website/static/js/share/footer.js
@@ -0,0 +1,53 @@
+var m = require('mithril');
+var utils = require('./utils');
+var MESSAGES = JSON.parse(require('raw!./messages.json'));
+
+var Footer = {};
+
+Footer.view = function(ctrl) {
+    return m('', [
+        m(
+            'ul.provider-footer.col-xs-12.col-lg-10.col-lg-offset-2',
+            {
+                style: {
+                    'list-style-type': 'none'
+                }
+            },
+            ctrl.renderProviders()
+        ),
+        m('.row', m('.col-md-12', {style: 'padding-top: 30px;'}, m('span', m.trust(MESSAGES.ABOUTSHARE))))
+    ]);
+};
+
+Footer.controller = function(vm) {
+    var self = this;
+    self.vm = vm;
+
+    self.renderProvider = function(result, index) {
+
+        return m('li.provider-filter', {
+                onclick: function(cb){
+                    self.vm.query(self.vm.query() === '' ? '*' : self.vm.query());
+                    self.vm.showFooter = false;
+                    utils.updateFilter(self.vm, 'source:' + result.short_name);
+                }
+            },
+            m('', [
+                m('img', {
+                    src: result.favicon,
+                    style: {
+                        width: '16px',
+                        height: '16px'
+                    }
+                }), ' ',
+                result.long_name]
+             )
+        );
+    };
+
+    self.renderProviders = function() {
+        return self.vm.showFooter ? $.map(self.vm.sortProviders(), self.renderProvider) : null;
+    };
+};
+
+module.exports = Footer;
