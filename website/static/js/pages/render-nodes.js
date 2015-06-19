ad8e9500ff6c2f79dae456c4a02bb60d8ec060e6
haoyuchen1992 <hc6as@virginia.edu>
2015-06-19 13:57:05 -0400

    Display anonymous user name by knockout

--- website/static/js/pages/render-nodes.js
+++ website/static/js/pages/render-nodes.js
@@ -2,9 +2,12 @@
 
 var $osf = require('js/osfHelpers');
 var $ = require('jquery');
+var ko = require('knockout');
 
 // model for components, due to simplicity did not create a new file
-var ComponentControl = {};
+var ComponentControl = {
+     anonymousUserName:ko.observable("<em>A user</em>")
+};
 
 // binds to component scope in render_nodes.mako
 $('.render-nodes-list').each(function() {
