0f126e16fb7ab8e149145d70235fdd272d136d9d
Dawn Pattison <pattison.dawn.r@gmail.com>
2015-06-22 12:51:17 -0400

    Return only registrations and reg drafts for GET 'v2'registrations/registration_id}/children, files, contributors, and pointers

--- api/registrations/views.py
+++ api/registrations/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics, permissions as drf_permissions
 from modularodm import Q
 
+from framework.auth.core import Auth
 from api.base.utils import get_registration_or_404
 from website.models import Node
 from api.base.filters import ODMFilterMixin
@@ -23,6 +24,7 @@ class RegistrationMixin(NodeMixin):
         # May raise a permission denied
         self.check_object_permissions(self.request, obj)
         return obj
+
 
 class RegistrationList(generics.ListAPIView, ODMFilterMixin):
     """All node registrations"""
@@ -72,11 +74,29 @@ class RegistrationContributorsList(NodeContributorsList, RegistrationMixin):
     """
     Contributors(users) for a registration
     """
+    def get_default_queryset(self):
+        node = self.get_registration()
+        visible_contributors = node.visible_contributor_ids
+        contributors = []
+        for contributor in node.contributors:
+            contributor.bibliographic = contributor._id in visible_contributors
+            contributors.append(contributor)
+        return contributors
+
 
 class RegistrationChildrenList(NodeChildrenList, RegistrationMixin):
     """
     Children of the current registration
     """
+    def get_queryset(self):
+        nodes = self.get_registration().nodes
+        user = self.request.user
+        if user.is_anonymous():
+            auth = Auth(None)
+        else:
+            auth = Auth(user)
+        children = [node for node in nodes if node.can_view(auth) and node.primary]
+        return children
 
 
 class RegistrationPointersList(generics.ListAPIView, RegistrationMixin):
@@ -100,3 +120,50 @@ class RegistrationFilesList(NodeFilesList, RegistrationMixin):
     """
     Files attached to a registration
     """
+    def get_queryset(self):
+        query_params = self.request.query_params
+
+        addons = self.get_registration().get_addons()
+        user = self.request.user
+        cookie = None if self.request.user.is_anonymous() else user.get_or_create_cookie()
+        node_id = self.get_registration()._id
+        obj_args = self.request.parser_context['args']
+
+        provider = query_params.get('provider')
+        path = query_params.get('path', '/')
+        files = []
+
+        if provider is None:
+            valid_self_link_methods = self.get_valid_self_link_methods(True)
+            for addon in addons:
+                if addon.config.has_hgrid_files:
+                    files.append({
+                        'valid_self_link_methods': valid_self_link_methods['folder'],
+                        'provider': addon.config.short_name,
+                        'name': addon.config.short_name,
+                        'path': path,
+                        'node_id': node_id,
+                        'cookie': cookie,
+                        'args': obj_args,
+                        'waterbutler_type': 'file',
+                        'item_type': 'folder',
+                        'metadata': {},
+                    })
+        else:
+            url = waterbutler_url_for('data', provider, path, self.kwargs['node_id'], cookie, obj_args)
+            waterbutler_request = requests.get(url)
+            if waterbutler_request.status_code == 401:
+                raise PermissionDenied
+            try:
+                waterbutler_data = waterbutler_request.json()['data']
+            except KeyError:
+                raise ValidationError(detail='detail: Could not retrieve files information.')
+
+            if isinstance(waterbutler_data, list):
+                for item in waterbutler_data:
+                    file = self.get_file_item(item, cookie, obj_args)
+                    files.append(file)
+            else:
+                files.append(self.get_file_item(waterbutler_data, cookie, obj_args))
+
+        return files
