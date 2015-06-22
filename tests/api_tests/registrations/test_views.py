c6c6dc5561b81ff255a57fe8d01cb5802b98c11f
Dawn Pattison <pattison.dawn.r@gmail.com>
2015-06-22 17:22:01 -0400

    Add tests for updating registration_drafts (tentative)

--- tests/api_tests/registrations/test_views.py
+++ tests/api_tests/registrations/test_views.py
@@ -115,3 +115,122 @@ class TestRegistrationDetail(ApiTestCase):
         res = self.app.get(self.private_url, auth=self.basic_auth_two, expect_errors=True)
         assert_equal(res.status_code, 403)
 
+class TestRegistrationUpdate(ApiTestCase):
+
+    def setUp(self):
+        ApiTestCase.setUp(self)
+        self.user = UserFactory.build()
+        password = fake.password()
+        self.password = password
+        self.user.set_password(password)
+        self.user.save()
+        self.basic_auth = (self.user.username, password)
+
+        self.user_two = UserFactory.build()
+        self.user_two.set_password(password)
+        self.user_two.save()
+        self.basic_auth_two = (self.user_two.username, password)
+
+        #TODO ADD registration drafts to test.  User should only be able to update registration DRAFT, never registration.
+
+        self.public_project = ProjectFactory(creator=self.user, is_public=True)
+        self.public_registration = RegistrationFactory(creator=self.user, project=self.public_project)
+        self.public_url = '/{}registrations/{}'.format(API_BASE, self.public_registration._id)
+
+        self.private_project = ProjectFactory(creator=self.user, is_private=True)
+        self.private_registration = RegistrationFactory(creator=self.user, project=self.private_project)
+        self.private_url = '/{}registrations/{}'.format(API_BASE, self.private_registration._id)
+
+        self.new_title = "Updated registration title"
+        self.new_description = "Updated registration description"
+        self.new_category = 'project'
+
+    def test_update_node_that_is_not_registration_draft(self):
+        url = '/{}registrations/{}'.format(API_BASE, self.private_project)
+        res = self.app.put(url, {
+            'title': self.new_title,
+            'description': self.new_description,
+            'category': self.new_category,
+            'public': False,
+        }, auth=self.basic_auth, expect_errors=True)
+        assert_equal(res.status_code, 400)
+
+    def test_update_registration(self):
+        res = self.app.put(self.private_url, {
+            'title': self.new_title,
+            'description': self.new_description,
+            'category': self.new_category,
+            'public': False,
+        }, auth=self.basic_auth, expect_errors=True)
+        assert_equal(res.status_code, 403)
+
+    def test_update_node_that_does_not_exist(self):
+        url = '/{}registrations/{}'.format(API_BASE, '12345')
+        res = self.app.put(url, {
+            'title': self.new_title,
+            'description': self.new_description,
+            'category': self.new_category,
+            'public': False,
+        }, auth=self.basic_auth, expect_errors=True)
+        assert_equal(res.status_code, 404)
+
+    def test_update_public_registration_draft_logged_out(self):
+        res = self.app.put(self.public_url, {
+            'title': self.new_title,
+            'description': self.new_description,
+            'category': self.new_category,
+            'public': False,
+        }, expect_errors=True)
+        assert_equal(res.status_code, 403)
+
+    def test_update_public_registration_draft_logged_in(self):
+        #TODO test updating public registration DRAFT, not registration
+        res = self.app.put(self.public_url, {
+            'title': self.new_title,
+            'description': self.new_description,
+            'category': self.new_category,
+            'public': False,
+        }, auth=self.basic_auth, expect_errors=True)
+        assert_equal(res.status_code, 200)
+
+        res = self.app.put(self.public_url, {
+            'title': self.new_title,
+            'description': self.new_description,
+            'category': self.new_category,
+            'public': False,
+        }, auth=self.basic_auth_two, expect_errors=True)
+        assert_equal(res.status_code, 403)
+
+    def test_update_private_registration_draft_logged_out(self):
+        #TODO test updating private registration DRAFT, not registration
+        res = self.app.put(self.public_url, {
+            'title': self.new_title,
+            'description': self.new_description,
+            'category': self.new_category,
+            'public': False,
+        }, expect_errors=True)
+        assert_equal(res.status_code, 403)
+
+    def test_update_private_registration_draft_logged_in_contributor(self):
+        #TODO test updating private registration DRAFT, not registration
+        res = self.app.put(self.public_url, {
+            'title': self.new_title,
+            'description': self.new_description,
+            'category': self.new_category,
+            'public': False,
+        }, auth=self.basic_auth)
+        assert_equal(res.status_code, 200)
+        assert_equal(res.json['data']['id'], self.public_registration._id)
+
+    def test_update_private_registration_draft_logged_in_non_contributor(self):
+        #TODO test updating private registration DRAFT, not registration
+        res = self.app.put(self.public_url, {
+            'title': self.new_title,
+            'description': self.new_description,
+            'category': self.new_category,
+            'public': False,
+        }, auth=self.basic_auth_two, expect_errors=True)
+        assert_equal(res.status_code, 403)
+
+
+
