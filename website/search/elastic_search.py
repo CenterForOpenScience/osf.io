f60c3917e45ce8e7d7a8740f7b8778c33479310b
Brandon Purvis <bcp7cc@virginia.edu>
2015-06-23 16:32:55 -0400

    Clean update_project_files function.

--- website/search/elastic_search.py
+++ website/search/elastic_search.py
@@ -259,7 +259,7 @@ def update_node(node, index=None):
             'is_registration': node.is_registration,
             'registered_date': node.registered_date,
             'wikis': {},
-            'files': {},
+            'files': '',
             'parent_id': parent_id,
             'date_created': node.date_created,
             'boost': int(not node.is_registration) + 1,  # This is for making registered projects less relevant
@@ -267,41 +267,21 @@ def update_node(node, index=None):
         for wiki in [NodeWikiPage.load(x) for x in node.wiki_pages_current.values()]:
             elastic_document['wikis'][wiki.page_name] = wiki.raw_text(node)
 
-        for file_ in index_file.collect_files(node._id):
-            elastic_document['files'][file_['name']] = file_['content']
         es.index(index=index, doc_type=category, id=elastic_document_id, body=elastic_document, refresh=True)
+        update_project_files(node, category, index=index)
 
 
-# TODO: Not yet called by anything.
 @requires_search
-def update_project_files(node, index=None):
+def update_project_files(node, category, index=None):
     index = index or INDEX
 
-    component_categories = [k for k in Node.CATEGORY_MAP.keys() if not k == 'project']
-    category = 'component' if node.category in component_categories else node.category
-    if category == 'project':
-        elastic_document_id = node._id
-        parent_id = None
-        category = 'registration' if node.is_registration else category
-    else:
-        try:
-            elastic_document_id = node._id
-            parent_id = node.parent_id
-            category = 'registration' if node.is_registration else category
-        except IndexError:
-            # Skip orphaned components
-            return
     files = index_file.collect_files(node._id)
-    file_dict = {file_['name']: file_['content'] for file_ in files if file_['content']}
-    body = {
-        'doc': {
-            'files': file_dict
-        }
-    }
+    file_text = ' '.join([file_['content'] for file_ in files if file_['content']])
+    update_body = {'doc': {'files': file_text}}
     #TODO: REMOVE DEBUGGING
     logger.info('\nES UPDATING FILES OF NODE {}...\n'.format(node))
-    es.update(index=index, doc_type=category, id=elastic_document_id, body=body)
-
+    response = es.update(index=index, doc_type=category, id=node._id, body=update_body)
+    logging.info('\nUPDATE RESPONSE: {}\n'.format(response))
 
 @requires_search
 def update_user(user, index=None):
