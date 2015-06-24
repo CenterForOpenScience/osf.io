2e721606132cf97be803b5eea3b0ce020591d2b3
Billy Hunt <Billy@billyhunt.com>
2015-06-24 17:06:20 -0400

    Add Comments to Permissions

--- api/users/permissions.py
+++ api/users/permissions.py
@@ -3,6 +3,8 @@ from rest_framework import permissions
 
 
 class ReadOnlyOrCurrentUser(permissions.BasePermission):
+    # Checks to see if the request is coming from the currently logged in user, and allows
+    # non safe actions if so
 
     def has_object_permission(self, request, view, obj):
         assert isinstance(obj, User), 'obj must be a User, got {}'.format(obj)
