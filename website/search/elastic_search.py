115c688952ad6c54e84a13a9550bf7d395cc9edd
Brandon Purvis <bcp7cc@virginia.edu>
2015-06-26 11:16:40 -0400

    Break adding files to document into separate function.

--- website/search/elastic_search.py
+++ website/search/elastic_search.py
@@ -282,12 +282,15 @@ def update_node(node, index=None):
             ]:
                 elastic_document['wikis'][wiki.page_name] = wiki.raw_text(node)
 
-            for file_ in index_file.collect_files(node):
-                logging.info(file_)
-                elastic_document['files'][file_['name']] = file_['content']
-
+            elastic_document = add_files_to_document(node, elastic_document, index=index)
         es.index(index=index, doc_type=category, id=elastic_document_id, body=elastic_document, refresh=True)
 
+
+def add_files_to_document(node, doc, index=None):
+    for file_ in index_file.collect_files(node):
+        logging.info(file_)
+        doc['files'][file_['name']] = file_['content']
+    return doc
 
 @requires_search
 def update_user(user, index=None):
