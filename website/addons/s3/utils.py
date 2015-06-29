812290e9963272066bbc7630f1aca640c316c0e1
Harry Rybacki <hrybacki@Harrys-MacBook-Pro-2.local>
2015-06-29 11:07:51 -0400

    Default location is empty string

--- website/addons/s3/utils.py
+++ website/addons/s3/utils.py
@@ -40,7 +40,7 @@ def validate_bucket_name(name):
     return bool(validate_name.match(name))
 
 
-def create_bucket(user_settings, bucket_name, location='DEFAULT'):
+def create_bucket(user_settings, bucket_name, location=''):
     return connect_s3(user_settings=user_settings).create_bucket(bucket_name, location=location)
 
 
