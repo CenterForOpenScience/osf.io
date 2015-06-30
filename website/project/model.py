5e4270ce94ce3fdbc6bdc697eb2bb4ae9115409a
Saman Ehsan <se8ea@virginia.edu>
2015-06-30 11:07:46 -0400

    Move contrib_added signal to node.add_contributors
    Node.add_contributor is used in a variety of places
    where a user is added before the node is saved for
    the first time (e.g. forking, templating, testd) - this
    causes errors in building urls for the email
    because the node._id is None.
    
    Move signal to add_contributors, which is only used
    when adding users to an existing project.

--- website/project/model.py
+++ website/project/model.py
@@ -2326,7 +2326,6 @@ class Node(GuidStoredObject, AddonModelMixin, IdentifierMixin):
             if save:
                 self.save()
 
-            project_signals.contributor_added.send(self, contributor=contributor)
             return True
 
         #Permissions must be overridden if changed when contributor is added to parent he/she is already on a child of.
@@ -2348,10 +2347,12 @@ class Node(GuidStoredObject, AddonModelMixin, IdentifierMixin):
         :param save: Save after adding contributor
         """
         for contrib in contributors:
-            self.add_contributor(
+            added = self.add_contributor(
                 contributor=contrib['user'], permissions=contrib['permissions'],
                 visible=contrib['visible'], auth=auth, log=False, save=False,
             )
+            if added:
+                project_signals.contributor_added.send(self, contributor=contrib['user'])
         if log and contributors:
             self.add_log(
                 action=NodeLog.CONTRIB_ADDED,
