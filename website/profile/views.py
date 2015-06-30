d6bc396bafb9dccf84fcd7e17a21395385d4ea76
Saman Ehsan <se8ea@virginia.edu>
2015-06-30 13:33:47 -0400

    Remove unused view function

--- website/profile/views.py
+++ website/profile/views.py
@@ -376,13 +376,6 @@ def collect_user_config_js(addon_configs):
             js_modules.append(js_path)
     return js_modules
 
-@must_be_logged_in
-def profile_addons(**kwargs):
-    user = kwargs['auth'].user
-    return {
-        'user_id': user._primary_key,
-    }
-
 
 @must_be_logged_in
 def user_choose_addons(**kwargs):
