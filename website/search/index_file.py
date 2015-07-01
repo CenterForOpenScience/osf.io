0d776170fd00c530524c4f64bf07393f671885fd
Brandon Purvis <bcp7cc@virginia.edu>
2015-07-01 11:10:29 -0400

    Add function to collect files from github addon and include in collect files.

--- website/search/index_file.py
+++ website/search/index_file.py
@@ -11,6 +11,20 @@ def collect_from_osfstorage(addon, tree):
         yield {'name': name, 'content': content}
 
 
+def collect_from_github(addon, tree):
+    children = tree['children']
+    for child in children:
+        if child.get('children'):
+            for file_ in collect_from_github(addon, child):
+                yield file_
+        else:
+            path, name = child['path'], child['name']
+            file_, created = addon.find_or_create_file_guid(path)
+            response = requests.get(file_.download_url)
+            content = unicode(response.text).encode('utf-8')
+            yield {'name': name, 'content': content}
+
+
 def collect_files(node):
     """ Generate the contents of a projects.
     :param node: node
@@ -32,3 +46,6 @@ def collect_files(node):
                 logging.info('File from {}:\n {}\n'.format(addon_name, pformat(file_)))
                 yield file_
 
+        elif addon_name == 'github':
+            for file_ in collect_from_github(addon, file_tree):
+                yield file_
