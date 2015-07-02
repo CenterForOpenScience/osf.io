15e4782d47f4c48e7f827bed7d7785ceeb3712ce
Brandon Purvis <bcp7cc@virginia.edu>
2015-07-02 15:56:25 -0400

    Remove global functions.

--- tests/test_index_file.py
+++ tests/test_index_file.py
@@ -1,3 +1,4 @@
+import unittest
 import factories
 import mock
 
@@ -5,13 +6,6 @@ from nose.tools import *  # flake8: noqa (PEP8 asserts)
 
 from tests.base import OsfTestCase
 from website.search.index_file import collect_files
-
-FILE_TREE = {
-    'name': 'fake_file_tree',
-    'children': [],
-}
-
-DOWNLOAD_HANDLERS = {}
 
 # fakes
 
@@ -24,87 +18,84 @@ class FakeResponse():
         return self.content
 
 
-def fake_request_get(*args, **kwargs):
-    url = args[0]
-    path_start = url.rfind('/')
-    argument_start = url.find('&') if url.find('&') > 0 else None
-    path = url[path_start:argument_start] if argument_start else url[path_start:]
-    if url[:11] == 'http://fake':
-        for file_path, contents in DOWNLOAD_HANDLERS.iteritems():
-            if path == file_path:
-                return FakeResponse(contents)
-    raise ValueError('{} not mocked'.format(url))
-
-
 @property
 def fake_download_url(*args, **kwargs):
     path = args[0].path
     return 'http://fake{}'.format(path)
 
-
-def fake_file_tree(*args, **kwargs):
-    return FILE_TREE
-
-# global file functions
-
-def add_file(name, path, contents):
-    global FILE_TREE
-    global DOWNLOAD_HANDLERS
-    FILE_TREE['children'].append({'type': 'file', 'name': name, 'path': path})
-    DOWNLOAD_HANDLERS[path] = contents
-
-
-def add_folder(name):
-    global FILE_TREE
-    FILE_TREE['children'].append({
-        'type': 'folder',
-        'name': name,
-        'children': [],
-    })
-
-
-def add_file_to_folder(folder_name, file_name, path, contents):
-    global FILE_TREE
-    global DOWNLOAD_HANDLERS
-
-    for child in FILE_TREE['children']:
-        if child['type'] == 'folder':
-            if child['name'] == folder_name:
-                child['children'].append({
-                    'type': 'file',
-                    'name': file_name,
-                    'path': path
-                })
-                DOWNLOAD_HANDLERS[path] = contents
-                return
-    raise ValueError('Folder {} does not exist'.format(folder_name))
-
-def reset_files():
-    global FILE_TREE
-    global DOWNLOAD_HANDLERS
-
-    FILE_TREE['children'] = []
-    DOWNLOAD_HANDLERS = {}
-
 # Tests
 
-class TestCollectFiles(OsfTestCase):
+class FileTreeTestCase(unittest.TestCase):
+    def setUp(self):
+        super(FileTreeTestCase, self).setUp()
+        self.FILE_TREE = {
+            'name': 'fake_file_tree',
+            'children': [],
+        }
+        self.DOWNLOAD_HANDLERS = {}
+
+    def get_fake_file_tree(self, *args, **kwargs):
+        return self.FILE_TREE
+
+    def make_fake_request_get(self, *args, **kwargs):
+        url = args[0]
+        path_start = url.rfind('/')
+        argument_start = url.find('&') if url.find('&') > 0 else None
+        path = url[path_start:argument_start] if argument_start else url[path_start:]
+        if url[:11] == 'http://fake':
+            for file_path, contents in self.DOWNLOAD_HANDLERS.iteritems():
+                if path == file_path:
+                    return FakeResponse(contents)
+        raise ValueError('{} not mocked'.format(url))
+
+    def tearDown(self):
+        super(FileTreeTestCase, self).tearDown()
+        self.reset_files()
+
+    def add_file(self, name, path, contents):
+        self.FILE_TREE['children'].append({'type': 'file', 'name': name, 'path': path})
+        self.DOWNLOAD_HANDLERS[path] = contents
+
+    def add_folder(self, name):
+        self.FILE_TREE['children'].append({
+            'type': 'folder',
+            'name': name,
+            'children': [],
+        })
+
+    def add_file_to_folder(self, folder_name, file_name, path, contents):
+        for child in self.FILE_TREE['children']:
+            if child['type'] == 'folder':
+                if child['name'] == folder_name:
+                    child['children'].append({
+                        'type': 'file',
+                        'name': file_name,
+                        'path': path
+                    })
+                    self.DOWNLOAD_HANDLERS[path] = contents
+                    return
+        raise ValueError('Folder {} does not exist'.format(folder_name))
+
+    def reset_files(self):
+        self.FILE_TREE['children'] = []
+        self.DOWNLOAD_HANDLERS = {}
+
+
+class TestCollectFiles(OsfTestCase, FileTreeTestCase):
     def setUp(self):
         super(TestCollectFiles, self).setUp()
         self._start_mocks()
         self.fake_project_with_addon = factories.ProjectWithAddonFactory()
         self.fake_project_with_addon.add_addon('github', None)
-        self.fake_project_with_addon.add_addon('brandons_addon', None)
 
     def tearDown(self):
         super(TestCollectFiles, self).tearDown()
         self._stop_mocks()
-        reset_files()
 
     def _start_mocks(self):
-        self.file_tree_patch = mock.patch('website.addons.base.StorageAddonBase._get_file_tree', fake_file_tree)
+        self.file_tree_patch = mock.patch('website.addons.base.StorageAddonBase._get_file_tree', self.get_fake_file_tree)
         self.download_url_patch = mock.patch('website.addons.base.GuidFile.download_url', fake_download_url)
-        self.request_patch = mock.patch('website.search.index_file.requests.get', fake_request_get)
+        self.request_patch = mock.patch('website.search.index_file.requests.get', self.make_fake_request_get)
 
         self.file_tree_patch.start()
         self.download_url_patch.start()
@@ -116,14 +107,14 @@ class TestCollectFiles(OsfTestCase):
         self.request_patch.stop()
 
     def test_collect_single_file(self):
-        add_file('file_one.txt', '/file_one', 'This is file one.')
+        self.add_file('file_one.txt', '/file_one', 'This is file one.')
         for file_ in collect_files(self.fake_project_with_addon):
             assert_equal(file_['name'], 'file_one.txt')
             assert_equal(file_['content'], 'This is file one.')
 
     def test_collect_multiple_files(self):
-        add_file('file_one.txt', '/file_one', 'This is file one.')
-        add_file('file_two.txt', '/file_two', 'This is file two.')
+        self.add_file('file_one.txt', '/file_one', 'This is file one.')
+        self.add_file('file_two.txt', '/file_two', 'This is file two.')
         for i, file_ in enumerate(collect_files(self.fake_project_with_addon)):
             if i == 0:
                 assert_equal(file_['name'], 'file_one.txt')
@@ -133,17 +124,17 @@ class TestCollectFiles(OsfTestCase):
                 assert_equal(file_['content'], 'This is file two.')
 
     def test_collect_with_folder(self):
-        add_folder('folder')
-        add_file_to_folder('folder', 'file_one.txt', '/file_one', 'This is file one.')
+        self.add_folder('folder')
+        self.add_file_to_folder('folder', 'file_one.txt', '/file_one', 'This is file one.')
         for file_ in collect_files(self.fake_project_with_addon):
             assert_equal(file_['name'], 'file_one.txt', 'This is file one.')
             assert_equal(file_['content'], 'This is file one.')
 
     def test_does_not_include_images(self):
-        add_file('file_one.txt', '/f_one', 'No one expects the finches swim audition!')
-        add_file('file_two.png', '/f_two', '*A picture of the moon*')
-        add_file('file_two.jpg', '/f_two', '*A picture of the moon*')
-        add_file('file_two.jpeg', '/f_two', '*A picture of the moon*')
-        add_file('file_two.bmp', '/f_two', '*A picture of the moon*')
+        self.add_file('file_one.txt', '/f_one', 'No one expects the finches swim audition!')
+        self.add_file('file_two.png', '/f_two', '*A picture of the moon*')
+        self.add_file('file_two.jpg', '/f_two', '*A picture of the moon*')
+        self.add_file('file_two.jpeg', '/f_two', '*A picture of the moon*')
+        self.add_file('file_two.bmp', '/f_two', '*A picture of the moon*')
         for file_ in collect_files(self.fake_project_with_addon):
             assert_equal(file_['name'], 'file_one.txt')
