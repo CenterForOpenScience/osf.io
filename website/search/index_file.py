6edcaf75fd215ad36f20d64c14454a41da40e045
Brandon Purvis <bcp7cc@virginia.edu>
2015-07-02 13:15:57 -0400

    Combine identical methods for collecting files from addons into one function.

--- website/search/index_file.py
+++ website/search/index_file.py
@@ -2,21 +2,8 @@ from framework.exceptions import HTTPError
 import requests
 
 
-def collect_from_osfstorage(addon, tree):
-    children = tree['children']
-    for child in children:
-        if child.get('children'):
-            for file_ in collect_from_osfstorage(addon, child):
-                yield file_
-        else:
-            path, name = child['path'], child['name']
-            file_, created = addon.find_or_create_file_guid(path)
-            response = requests.get(file_.download_url + '&mode=render')
-            content = unicode(response.text).encode('utf-8')
-            yield {'name': name, 'content': content}
-
-
 def collect_from_addon(addon, tree):
+    addon_name = addon.config.short_name
     children = tree['children']
     for child in children:
         if child.get('children'):
@@ -25,7 +12,9 @@ def collect_from_addon(addon, tree):
         else:
             path, name = child['path'], child['name']
             file_, created = addon.find_or_create_file_guid(path)
-            response = requests.get(file_.download_url)
+            download_url = file_.download_url
+            download_url += '&mode=render' if addon_name == 'osfstorage' else ''
+            response = requests.get(download_url)
             content = unicode(response.text).encode('utf-8')
             yield {'name': name, 'content': content}
 
@@ -37,16 +26,9 @@ def collect_files(node):
     """
     addons = node.get_addons()
     for addon in addons:
-        addon_name = addon.config.short_name
         try:
             file_tree = addon._get_file_tree()
         except (AttributeError, HTTPError):
             continue
-
-        if addon_name == 'osfstorage':
-            for file_ in collect_from_osfstorage(addon, file_tree):
-                yield file_
-
-        else:
-            for file_ in collect_from_addon(addon, file_tree):
-                yield file_
+        for file_ in collect_from_addon(addon, file_tree):
+            yield file_
