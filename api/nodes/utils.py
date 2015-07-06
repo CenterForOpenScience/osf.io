368019bfedb8414dc302dac4520dcec808418e28
Patrick Gorman <gormanpe@vcu.edu>
2015-07-06 14:27:04 -0400

    Refactored to allow for more efficient processing and testing of valid/invalid data

--- api/nodes/utils.py
+++ api/nodes/utils.py
@@ -16,22 +16,26 @@ allowed_query_keys = {
 class IncludeParamsProcessor(object):
     #todo move into base/utils?
     def __init__(self, include):
+
         # Checks and cuts off include value if '/' is found
         include = include.split('/')[0]
-        query_keys = {}
+        query_params = {}
+
         # Processes include string into ',' separated parameters with '.' marking relationships
         for raw_parameter in include.split(','):
             sub_query_list = raw_parameter.split('.')
             query = {}
-            for subquery in reversed(sub_query_list):
-                query = {subquery: query}
-            query_keys[sub_query_list[0]] = query[sub_query_list[0]]
-        self.query_params = query_keys
-        for key in query_keys:
-            if key not in allowed_query_keys:
-                raise NotFound('Key {} is not a valid query parameter for node object.'.format(key))
-            else:
-                self.query_params[key] = self.process_key(key, self.query_params[key])
+            allowed_keys = allowed_query_keys
+            for sub_query in reversed(sub_query_list):
+                query = {sub_query: query}
+            for sub_query_test in sub_query_list:
+                if sub_query_test in allowed_keys:
+                    allowed_keys = allowed_keys[sub_query_test]
+                else:
+                    raise NotFound('{} is not a valid property of the Node object.'.format(query))
+            query = self.process_query(query)
+            query_params[sub_query_list[0]] = query[sub_query_list[0]]
+        self.additional_query_params = query_params
 
-    def process_key(self, key, default_value):
-        return default_value
+    def process_query(self, query, allowed_keys=None):
+        return query
