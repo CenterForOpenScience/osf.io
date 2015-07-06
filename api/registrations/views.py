1f69ae775f3d09809bbd200efebc008306afd412
Dawn Pattison <pattison.dawn.r@gmail.com>
2015-07-06 11:15:14 -0400

    Change GET registrations to include just registrations, not draft registrations

--- api/registrations/views.py
+++ api/registrations/views.py
@@ -1,21 +1,17 @@
-import requests
 
 from rest_framework import status
-from framework.auth.core import Auth
 from rest_framework.response import Response
 from django.utils.translation import ugettext_lazy as _
 from rest_framework import generics, permissions as drf_permissions
-from rest_framework.exceptions import PermissionDenied, ValidationError
+from rest_framework.exceptions import ValidationError
 
 from modularodm import Q
 from website.models import Node
 from api.base.filters import ODMFilterMixin
 from website.language import REGISTER_WARNING
-from api.base.utils import waterbutler_url_for
-from api.nodes.serializers import NodePointersSerializer
 from api.base.utils import token_creator, absolute_reverse
 from api.nodes.permissions import ContributorOrPublic, ReadOnlyIfRegistration
-from api.nodes.views import NodeMixin, NodeFilesList, NodeChildrenList, NodeContributorsList, NodeDetail
+from api.nodes.views import NodeMixin, NodeDetail
 from api.registrations.serializers import RegistrationSerializer, RegistrationCreateSerializer, RegistrationCreateSerializerWithToken
 
 
@@ -46,7 +42,7 @@ class RegistrationList(generics.ListAPIView, ODMFilterMixin):
         base_query = (
             Q('is_deleted', 'ne', True) &
             Q('is_folder', 'ne', True) &
-            (Q('is_registration', 'eq', True) | Q('is_registration_draft', 'eq', True))
+            (Q('is_registration', 'eq', True))
         )
         user = self.request.user
         permission_query = Q('is_public', 'eq', True)
@@ -116,106 +112,3 @@ class RegistrationCreate(generics.CreateAPIView, RegistrationMixin):
     serializer_class = RegistrationCreateSerializerWithToken
 
 
-class RegistrationContributorsList(NodeContributorsList, RegistrationMixin):
-    """
-    Contributors(users) for a registration
-    """
-    def get_default_queryset(self):
-        node = self.get_node()
-        registration_enforcer(node)
-        visible_contributors = node.visible_contributor_ids
-        contributors = []
-        for contributor in node.contributors:
-            contributor.bibliographic = contributor._id in visible_contributors
-            contributors.append(contributor)
-        return contributors
-
-
-class RegistrationChildrenList(NodeChildrenList, RegistrationMixin):
-    """
-    Children of the current registration
-    """
-    def get_queryset(self):
-        reg_node = self.get_node()
-        registration_enforcer(reg_node)
-        nodes = reg_node.nodes
-        user = self.request.user
-        if user.is_anonymous():
-            auth = Auth(None)
-        else:
-            auth = Auth(user)
-        children = [node for node in nodes if node.can_view(auth) and node.primary]
-        return children
-
-
-class RegistrationPointersList(generics.ListAPIView, RegistrationMixin):
-    """
-    Registration pointers
-    """
-
-    permission_classes = (
-        drf_permissions.IsAuthenticatedOrReadOnly,
-        ContributorOrPublic,
-    )
-
-    serializer_class = NodePointersSerializer
-
-    def get_queryset(self):
-        node = self.get_node()
-        registration_enforcer(node)
-        pointers = node.nodes_pointer
-        return pointers
-
-
-class RegistrationFilesList(NodeFilesList, RegistrationMixin):
-    """
-    Files attached to a registration
-    """
-    def get_queryset(self):
-        query_params = self.request.query_params
-        node = self.get_node()
-        registration_enforcer(node)
-        addons = node.get_addons()
-        user = self.request.user
-        cookie = None if self.request.user.is_anonymous() else user.get_or_create_cookie()
-        node_id = node._id
-        obj_args = self.request.parser_context['args']
-
-        provider = query_params.get('provider')
-        path = query_params.get('path', '/')
-        files = []
-
-        if provider is None:
-            valid_self_link_methods = self.get_valid_self_link_methods(True)
-            for addon in addons:
-                if addon.config.has_hgrid_files:
-                    files.append({
-                        'valid_self_link_methods': valid_self_link_methods['folder'],
-                        'provider': addon.config.short_name,
-                        'name': addon.config.short_name,
-                        'path': path,
-                        'node_id': node_id,
-                        'cookie': cookie,
-                        'args': obj_args,
-                        'waterbutler_type': 'file',
-                        'item_type': 'folder',
-                        'metadata': {},
-                    })
-        else:
-            url = waterbutler_url_for('data', provider, path, self.kwargs['node_id'], cookie, obj_args)
-            waterbutler_request = requests.get(url)
-            if waterbutler_request.status_code == 401:
-                raise PermissionDenied
-            try:
-                waterbutler_data = waterbutler_request.json()['data']
-            except KeyError:
-                raise ValidationError(detail='detail: Could not retrieve files information.')
-
-            if isinstance(waterbutler_data, list):
-                for item in waterbutler_data:
-                    file = self.get_file_item(item, cookie, obj_args)
-                    files.append(file)
-            else:
-                files.append(self.get_file_item(waterbutler_data, cookie, obj_args))
-
-        return files
