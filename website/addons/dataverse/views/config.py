0b528d79f44e5159e900957a7c2b970b933972be
Nan Chen <nc6r@virginia.edu>
2015-07-06 15:45:50 -0400

    fix the import dataverse token doesn't enable addon_user_settings

--- website/addons/dataverse/views/config.py
+++ website/addons/dataverse/views/config.py
@@ -60,6 +60,9 @@ def dataverse_add_user_account(auth, **kwargs):
         user.external_accounts.append(provider.account)
         user.save()
 
+    user_settings = auth.user.get_addon('dataverse')
+    if not user_settings:
+        auth.user.add_addon(addon_name='dataverse', auth=auth)
     return {}
 
 
