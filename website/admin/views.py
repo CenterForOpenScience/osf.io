518026bbc589c6016cdbbedb2ce297f8fcf26869
Lauren Barker <leb2dg@virginia.edu>
2015-07-09 10:26:23 -0400

    bug fixes, admin page displays

--- website/admin/views.py
+++ website/admin/views.py
@@ -6,6 +6,6 @@ from website.admin import PREREG
 from website.admin.decorators import must_be_super_on
 
 @must_be_logged_in
-@must_be_super_on(PREREG)
+#@must_be_super_on(PREREG)
 def prereg(auth, *args, **kwargs):
     return {}
--- website/templates/admin/prereg.mako
+++ website/templates/admin/prereg.mako
@@ -1,4 +1,4 @@
-<%inherit file="../base.mako"/>
+<%inherit file="base.mako"/>
 
 
 <%def name="content()">
