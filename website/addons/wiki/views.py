f531cca8386862e1bde4fb17d3272fa4c4f8055a
Matt Frazier <maf7sm@virginia.edu>
2015-07-09 09:52:16 -0400

    Save newly created wiki page     -upon successful name validation

--- website/addons/wiki/views.py
+++ website/addons/wiki/views.py
@@ -430,6 +430,7 @@ def project_wiki_rename(auth, wname, **kwargs):
 @must_have_addon('wiki', 'node')
 def project_wiki_validate_name(wname, **kwargs):
     node = kwargs['node'] or kwargs['project']
+    auth = kwargs['auth']
     wiki_name = wname.strip()
     wiki_key = to_mongo_key(wiki_name)
 
@@ -438,4 +439,6 @@ def project_wiki_validate_name(wname, **kwargs):
             message_short='Wiki page name conflict.',
             message_long='A wiki page with that name already exists.'
         ))
+    else:
+        node.update_node_wiki(wiki_name, '', auth)
     return {'message': wiki_name}
