10d5efc9ba5178ce264f22e0a316ce0f60a326d8
Lauren Barker <leb2dg@virginia.edu>
2015-07-09 10:06:11 -0400

    added route and files for admin page to get drafts

--- website/static/js/pages/prereg-admin-page.js
+++ website/static/js/pages/prereg-admin-page.js
@@ -1,1 +1,26 @@
-// TODO
+'use strict';
+
+var $osf = require('js/osfHelpers');
+var ko = require('knockout');
+var $ = require('jquery');
+var Raven = require('raven-js');
+
+$(document).ready(function() {
+    var test = '/api/v1/drafts/' + window.contextVars.currentUser.id
+
+    var request = $.ajax({
+        url: test
+    });
+    request.done(function(data) {
+        console.log(data);
+    });
+    request.fail(function(xhr, textStatus, error) {
+        console.log(xhr);
+        Raven.captureMessage('Failed to populate data', {
+            url: test,
+            textStatus: textStatus,
+            error: error
+        });
+    });
+
+});
