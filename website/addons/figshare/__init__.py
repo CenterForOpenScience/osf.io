a02a421e41320c1dc07dfdd08e8e05b61088c52f
Chris Seto <chriskseto@gmail.com>
2015-07-14 14:49:33 -0400

    Set figshare's MAX_FILE_SIZE to 50mb

--- website/addons/figshare/__init__.py
+++ website/addons/figshare/__init__.py
@@ -31,6 +31,8 @@ INCLUDE_CSS = {}
 HAS_HGRID_FILES = True
 GET_HGRID_DATA = views.hgrid.figshare_hgrid_data
 
+MAX_FILE_SIZE = 50
+
 HERE = os.path.dirname(os.path.abspath(__file__))
 NODE_SETTINGS_TEMPLATE = None  # use default nodes settings templates
 USER_SETTINGS_TEMPLATE = os.path.join(HERE, 'templates', 'figshare_user_settings.mako')
