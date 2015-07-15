d8569043c3ce2b6d1b4c0f50d9a4aa035b770bcc
reinaH <reh4fd@virginia.edu>
2015-07-15 14:15:23 -0400

    bug fixed and respective unit tests added: return deactivated users

--- api/users/views.py
+++ api/users/views.py
@@ -7,38 +7,27 @@ from api.base.utils import get_object_or_404
 from api.base.filters import ODMFilterMixin
 from api.nodes.serializers import NodeSerializer
 from .serializers import UserSerializer
-from django.contrib.auth.models import AnonymousUser
-from rest_framework.exceptions import PermissionDenied
 
+from rest_framework.exceptions import NotFound
 
 class UserMixin(object):
     """Mixin with convenience methods for retrieving the current node based on the
-    current URL. By default, fetches the user based on the user_id kwarg.
+    current URL. By default, fetches the user based on the pk kwarg.
     """
 
     serializer_class = UserSerializer
     node_lookup_url_kwarg = 'user_id'
 
     def get_user(self, check_permissions=True):
-        key = self.kwargs[self.node_lookup_url_kwarg]
-        current_user = self.request.user
+        obj = get_object_or_404(User, self.kwargs[self.node_lookup_url_kwarg])
 
-        if key == 'me':
-            # TODO: change exception from PermissionDenied to NotAuthenticated/AuthenticationFailed
-            # TODO: for unauthorized users
+        if check_permissions & obj.is_disabled == False:
 
-            if isinstance(current_user, AnonymousUser):
-                raise PermissionDenied
-            else:
-                return self.request.user
-
-        obj = get_object_or_404(User, key)
-
-        if check_permissions:
             # May raise a permission denied
             self.check_object_permissions(self.request, obj)
-        return obj
-
+            return obj
+        else:
+            raise NotFound
 
 class UserList(generics.ListAPIView, ODMFilterMixin):
     """Users registered on the OSF.
