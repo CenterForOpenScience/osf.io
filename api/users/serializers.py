3b6b36f12cdad47c58cc9b735c72f8fab7207a65
Dawn Pattison <pattison.dawn.r@gmail.com>
2015-07-16 14:28:38 -0400

    Add serializer method field for attributes

--- api/users/serializers.py
+++ api/users/serializers.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 from rest_framework import serializers as ser
 
 from api.base.serializers import JSONAPISerializer, LinksField, Link
@@ -12,20 +13,7 @@ class UserSerializer(JSONAPISerializer):
         'id'
     ])
     id = ser.CharField(read_only=True, source='_id')
-    fullname = ser.CharField(help_text='Display name used in the general user interface')
-    given_name = ser.CharField(help_text='For bibliographic citations')
-    middle_name = ser.CharField(source='middle_names', help_text='For bibliographic citations')
-    family_name = ser.CharField(help_text='For bibliographic citations')
-    suffix = ser.CharField(help_text='For bibliographic citations')
-    date_registered = ser.DateTimeField(read_only=True)
-    gravatar_url = ser.CharField(help_text='URL for the icon used to identify the user. Relies on http://gravatar.com ')
-    employment_institutions = ser.ListField(source='jobs', help_text='An array of dictionaries representing the '
-                                                                     'places the user has worked')
-    educational_institutions = ser.ListField(source='schools', help_text='An array of dictionaries representing the '
-                                                                         'places the user has attended school')
-    social_accounts = ser.DictField(source='social', help_text='A dictionary of various social media account '
-                                                               'identifiers including an array of user-defined URLs')
-
+    attributes = ser.SerializerMethodField(help_text='A dictionary containing user properties')
     links = LinksField({
         'html': 'absolute_url',
         'nodes': {
@@ -35,6 +23,21 @@ class UserSerializer(JSONAPISerializer):
 
     class Meta:
         type_ = 'users'
+
+    @staticmethod
+    def get_attributes(obj):
+        ret = OrderedDict((
+            ('fullname', obj.fullname),
+            ('given_name', obj.given_name),
+            ('middle_name', obj.middle_names),
+            ('family_name', obj.family_name),
+            ('suffix', obj.suffix),
+            ('date_registered', obj.date_registered),
+            ('gravatar_url', obj.gravatar_url),
+            ('employment_institutions', obj.jobs),
+            ('educational_institutions', obj.schools),
+            ('social_accounts', obj.social)))
+        return ret
 
     def absolute_url(self, obj):
         return obj.absolute_url
