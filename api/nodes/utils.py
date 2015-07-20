0f7de8e462a1f4e34a79d2e31a041a921e7384b2
Patrick Gorman <gormanpe@vcu.edu>
2015-07-19 20:03:50 -0400

    Renamed obj into node for clarity

--- api/nodes/utils.py
+++ api/nodes/utils.py
@@ -6,8 +6,8 @@ from rest_framework.exceptions import PermissionDenied, ValidationError
 
 class IncludeAdditionalQueryNode(object):
 
-    def __init__(self, obj, request):
-        self.obj = obj
+    def __init__(self, node, request):
+        self.node = node
         self.request = request
 
     # todo: make simple serializers for parameters?
@@ -37,22 +37,22 @@ class IncludeAdditionalQueryNode(object):
 
     def get_children(self):
         nodes = {}
-        for node in self.obj.nodes:
+        for node in self.node.nodes:
             if node.can_view(Auth(self.request.user)) or node.is_public:
                 nodes[node._id] = {
                     'title': node.title,
                     'description': node.description,
-                    'is_public': node.is_public
+                    'is_public': node.is_public,
                 }
         return nodes
 
     def get_contributors(self):
         contributors = {}
-        for contributor in self.obj.contributors:
+        for contributor in self.node.contributors:
             contributors[contributor._id] = {
                 'username': contributor.username,
-                'bibliographic': self.obj.get_visible(contributor),
-                'permissions': self.obj.get_permissions(contributor)
+                'bibliographic': self.node.get_visible(contributor),
+                'permissions': self.node.get_permissions(contributor)
             }
         return contributors
 
@@ -69,7 +69,7 @@ class IncludeAdditionalQueryNode(object):
 
     def get_pointers(self):
         pointers = {}
-        for pointer in self.obj.nodes_pointer:
+        for pointer in self.node.nodes_pointer:
             pointers[pointer._id] = {
                 'title': pointer.title,
                 'description': pointer.description,
@@ -79,12 +79,13 @@ class IncludeAdditionalQueryNode(object):
 
     def get_registrations(self):
         registrations = {}
-        for registration in self.obj.node__registrations:
-            registrations[registration._id] = {
-                'title': registration.title,
-                'description': registration.description,
-                'is_public': registration.is_public
-            }
+        for registration in self.node.node__registrations:
+            if registration.can_view(Auth(self.request.user)) or registration.is_public:
+                registrations[registration._id] = {
+                    'title': registration.title,
+                    'description': registration.description,
+                    'is_public': registration.is_public
+                }
         return registrations
 
     # todo configure this after issue 3060 is resolved
@@ -92,9 +93,9 @@ class IncludeAdditionalQueryNode(object):
     def get_file_list(self):
         query_params = self.request.query_params
 
-        addons = self.obj.get_addons()
+        addons = self.node.get_addons()
         user = self.request.user
-        obj_args = self.request.parser_context['args']
+        node_args = self.request.parser_context['args']
 
         provider = query_params.get('provider')
         path = query_params.get('path', '/')
@@ -107,11 +108,11 @@ class IncludeAdditionalQueryNode(object):
                         'provider': addon.config.short_name,
                         'name': addon.config.short_name,
                         'path': path,
-                        'args': obj_args,
+                        'args': node_args,
                     })
         else:
             cookie = None if self.request.user.is_anonymous() else user.get_or_create_cookie()
-            url = waterbutler_url_for('data', provider, path, self.kwargs['node_id'], cookie, obj_args)
+            url = waterbutler_url_for('data', provider, path, self.kwargs['node_id'], cookie, node_args)
             waterbutler_request = self.request.get(url)
             if waterbutler_request.status_code == 401:
                 raise PermissionDenied
@@ -122,19 +123,19 @@ class IncludeAdditionalQueryNode(object):
 
             if isinstance(waterbutler_data, list):
                 for item in waterbutler_data:
-                    file = self.get_file_item(item, obj_args)
+                    file = self.get_file_item(item, node_args)
                     files.append(file)
             else:
-                files.append(self.get_file_item(waterbutler_data, obj_args))
+                files.append(self.get_file_item(waterbutler_data, node_args))
 
         return files
 
-    def get_file_item(self, item, obj_args):
+    def get_file_item(self, item, node_args):
         file_item = {
             'provider': item['provider'],
             'path': item['path'],
             'name': item['name'],
-            'args': obj_args,
+            'args': node_args,
         }
         if file_item['item_type'] == 'folder':
             file_item['metadata'] = {}
