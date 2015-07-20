717825fe39c1e1e7e0b949823d388d1f57c46116
Gage Gaskins <tgg8ec@virginia.edu>
2015-07-20 16:59:00 -0400

    Begin implementation of date change/Piwik statistics update

--- website/static/js/pages/piwikStats-page.js
+++ website/static/js/pages/piwikStats-page.js
@@ -1,47 +1,46 @@
 'use strict';
+
 var c3 = require('c3');
 require('c3/c3.css');
 
-var statistics = {};
+var pikaday = require('pikaday');
+require('pikaday-css');
 
+var moment = require('moment');
+
+var statistics = {};
 $(document).ready(function() {
 
-    $.when(
-        $.get('http://localhost:6969/'+nodeId+'/nodeData', function (data) {
-            return data;
-        }),
-
-        $.get('http://localhost:6969/fileData', {'files': nodeFiles}, function(data) {
-            return data;
-        })
-    ).then(function(nodeData, fileData){
-            statisticsSchema(nodeData[0], fileData[0]);
-            renderPiwikChart(statistics, 'visits');
-        },
-        function(){
-            console.log(arguments)
+    var picker = new pikaday({
+        field: document.getElementById('datepicker'),
+        trigger: document.getElementById('datepicker-btn'),
+        onSelect: function(){
+            selectDate(picker.toString());
         }
-    )
+    });
+
+    loadPiwikData();
+
 });
 
 function statisticsSchema(nodeData, fileData){
 
-    statistics = {
-        node: formatNodeData(nodeData['node']),
-        children: formatChildrenData(nodeData['children']),
-        files: formatFileData(fileData['files'])
-    };
+    statistics.dates = nodeData['dates'];
+    statistics.node = formatNodeData(nodeData['node']);
+    statistics.children = formatChildrenData(nodeData['children']);
+    statistics.files = formatFileData(fileData['files']);
 
 }
 
-function extractDataType(piwikType, piwikData) {
-    var data = [];
+function dataTypeToColumn(label, piwikType, piwikData) {
+    var data = [label];
 
-    for (var date in piwikData){
+    for (var i=0; i < statistics['dates'].length; i++){
+        var date = statistics['dates'][i];
         if($.isPlainObject(piwikData[date])) {
-            data.push({date: date, data: piwikData[date][piwikType]})
+            data.push(piwikData[date][piwikType])
         } else {
-            data.push({date: date, data: 0})
+            data.push(0)
         }
     }
 
@@ -49,17 +48,16 @@ function extractDataType(piwikType, piwikData) {
 }
 
 function formatNodeData(nodeData) {
-    var node = {};
+    var data = {};
 
-    var visits = extractDataType('nb_visits', nodeData[nodeId]);
-    var uniqueVisitors = extractDataType('nb_uniq_visitors', nodeData[nodeId]);
-
-    node[nodeId] = {
-        visits: visits,
+    var visits = dataTypeToColumn('Visits','nb_visits', nodeData[nodeId]);
+    var uniqueVisitors = dataTypeToColumn('Unique Visitors','nb_uniq_visitors', nodeData[nodeId]);
+    data[nodeId] = {
+        visits : visits,
         uniqueVisitors: uniqueVisitors
     };
 
-    return node
+    return data;
 }
 
 function formatChildrenData(childrenData) {
@@ -69,8 +67,8 @@ function formatChildrenData(childrenData) {
 
         for (var child in childrenData){
             children[child] = {
-                visits: extractDataType('nb_visits', childrenData[child]),
-                uniqueVisitors: extractDataType('nb_uniq_visitors', childrenData[child])
+                visits: dataTypeToColumn('Visits', 'nb_visits', childrenData[child]),
+                uniqueVisitors: dataTypeToColumn('Unique Visitors', 'nb_uniq_visitors', childrenData[child])
             }
         }
 
@@ -87,8 +85,8 @@ function formatFileData(fileData) {
 
         for (var file in fileData){
             files[file] = {
-                visits: extractDataType('nb_visits', fileData[file]),
-                uniqueVisitors: extractDataType('nb_uniq_visitors', fileData[file])
+                visits: dataTypeToColumn('Visits', 'nb_visits', fileData[file]),
+                uniqueVisitors: dataTypeToColumn('Unique Visitors', 'nb_uniq_visitors', fileData[file])
             }
         }
 
@@ -98,27 +96,102 @@ function formatFileData(fileData) {
     return [];
 }
 
-function renderPiwikChart(dataSchema, dataType){
+function renderPiwikChart(stats, dataType){
 
     $('.piwikChart').height(200);
+
+    var dates = statistics['dates'];
+    dates.unshift('x');
 
     var chart = c3.generate({
         bindto: '.piwikChart',
         data: {
-            json: dataSchema['node'][nodeId][dataType],
-            keys: {
-                x: 'date',
-                value: ['data']
-            }
+            x: 'x',
+            columns: [
+                dates,
+                stats['node'][nodeId][dataType]
+            ]
         },
         axis: {
             x: {
                 type: 'timeseries',
                 tick: {
-                    format: '%Y-%m-%d'
+                    format: '%b %d %Y',
+                    culling: {
+                        max: 5
+                    }
+                },
+                padding: {
+                    left: 0
+                }
+            },
+            y: {
+                padding: {
+                    bottom: 0
                 }
             }
+        },
+        padding: {
+            right: 50,
+            bottom: 20
+        },
+        legend: {
+            show: false
         }
     });
 }
 
+function renderStatsTable(table, data, dataType){
+
+    var table = $('#'+table);
+
+    if(!$.isPlainObject(data)){
+        table.append('<h4>This project has no public components for statistics.</h4>');
+        return
+    }
+
+    table.append('<thead><th>Name</th><th> </th><th>' + dataType + '</th></thead>');
+
+    for (var compGuid in data){
+
+        var componentRow = $('<tr>');
+        var total = 0;
+
+        var compData = data[compGuid][dataType];
+        for(var i=1; i < compData.length; i++){
+            total += compData[i];
+        }
+
+        componentRow.append('<td>' + compGuid + '</td><td>Null</td><td>' + total + '</td>');
+        table.append(componentRow);
+    }
+
+}
+
+function selectDate(date){
+    var endDate = moment(date);
+    var startDate = moment(endDate).subtract(7, 'days');
+
+    loadPiwikData()
+
+}
+
+function loadPiwikData(){
+
+    $.when(
+        $.get('http://localhost:7000/'+nodeId+'/nodeData', function(data){
+        console.log(data);
+    }),
+        $.get('http://localhost:7000/fileData', {'files': nodeFiles})
+    ).then(function(nodeData, fileData){
+            statisticsSchema(nodeData[0], fileData[0]);
+            renderPiwikChart(statistics, 'visits');
+            renderStatsTable('componentStats', statistics['children'], 'visits');
+            renderStatsTable('fileStats', statistics['files'], 'visits');
+        },
+        function(){
+            console.log(arguments)
+        }
+    )
+
+}
--- website/templates/project/piwik_stats.mako
+++ website/templates/project/piwik_stats.mako
@@ -3,13 +3,40 @@
 <%def name="title()">${node['title']} Statistics</%def>
 <script type="text/javascript" src="http://code.highcharts.com/highcharts.js"></script>
 
+
+<div class="row">
+    <input type="text" id="datepicker" class="form-control">
+    <button class="btn btn-default" id="datepicker-btn">Select Date</button>
+</div>
 <div class="row">
     <div class="col-md-12">
         <div class="panel panel-default">
-             <div class="panel-heading">
-                <h3 class="panel-title">C3 Pageviews</h3>
+             <div class="panel-heading clearfix">
+                <h3 class="panel-title">Statistics for ${node['title']}</h3>
             </div>
-            <div class="panel-body piwikChart"></div>
+            <div class="panel-body">
+                <div class="piwikChart"></div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-6">
+        <div class="panel panel-default">
+            <div class="panel-heading clearfix">
+                <h3 class="panel-title">Components </h3>
+            </div>
+            <div class="panel-body">
+                <table id="componentStats" class="table"></table>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-6">
+        <div class="panel panel-default">
+            <div class="panel-heading clearfix">
+                <h3 class="panel-title">Files</h3>
+            </div>
+            <div class="panel-body">
+                <table id="fileStats" class="table"></table>
+            </div>
         </div>
     </div>
 </div>
