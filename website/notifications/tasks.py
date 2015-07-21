2343da123f792728a8e81316b77b9f1fd4a598e8
Bryan Gorges <bgorges@cos.io>
2015-07-21 17:43:06 -0400

    Start of thoughts on celery tasks

---
+++ website/notifications/tasks.py
@@ -0,0 +1,7 @@
+"""
+Tasks for making even transactional emails consolidated.
+"""
+
+from framework.tasks import app as celery_app
+
+# TODO: Not sure whether to make this a class and use some of the same ideas as archives/tasks
