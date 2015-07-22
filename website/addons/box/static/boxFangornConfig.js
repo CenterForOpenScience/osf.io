eb90b405e05b32f06db3027bb9806df45cb90617
Caner Uguz <caneruguz@gmail.com>
2015-07-22 13:26:37 -0400

    Remove notify from box fangorn [skip ci]

--- website/addons/box/static/boxFangornConfig.js
+++ website/addons/box/static/boxFangornConfig.js
@@ -3,11 +3,4 @@
 var m = require('mithril');
 var Fangorn = require('js/fangorn');
 
-function _fangornLazyLoadError (item) {
-    item.notify.update('Box couldn\'t load, please try again later.', 'deleting', undefined, 3000);
-    return true;
-}
-
-Fangorn.config.box = {
-    lazyLoadError : _fangornLazyLoadError
-};
+Fangorn.config.box = {};
