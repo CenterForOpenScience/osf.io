fc03f5f65d40405c6b322c19114479bcc3e3cf12
Caner Uguz <caneruguz@gmail.com>
2015-07-22 13:25:58 -0400

    Remove notify from dropbox fangorn [skip ci]

--- website/addons/dropbox/static/dropboxFangornConfig.js
+++ website/addons/dropbox/static/dropboxFangornConfig.js
@@ -3,11 +3,4 @@
 var m = require('mithril');
 var Fangorn = require('js/fangorn');
 
-function _fangornLazyLoadError (item) {
-    item.notify.update('Dropbox couldn\'t load, please try again later.', 'deleting', undefined, 3000);
-    return true;
-}
-
-Fangorn.config.dropbox = {
-    lazyLoadError : _fangornLazyLoadError
-};
+Fangorn.config.dropbox = {};
