8a14db5b97884c1e30989c8addb54434a608759f
Caner Uguz <caneruguz@gmail.com>
2015-07-22 13:27:58 -0400

    Remove notify from google drive fangorn [skip ci]

--- website/addons/googledrive/static/googleDriveFangornConfig.js
+++ website/addons/googledrive/static/googleDriveFangornConfig.js
@@ -3,11 +3,4 @@
 var m = require('mithril');
 var Fangorn = require('js/fangorn');
 
-function _fangornLazyLoadError(item) {
-    item.notify.update('Google Drive couldn\'t load, please try again later.', 'deleting', undefined, 3000);
-    return true;
-}
-
-Fangorn.config.googledrive = {
-    lazyLoadError : _fangornLazyLoadError
-};
+Fangorn.config.googledrive = {};
