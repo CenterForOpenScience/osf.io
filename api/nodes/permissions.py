fd9fb35af4236d6c4e5e0cd3ecbc1104625c1020
Patrick Gorman <gormanpe@vcu.edu>
2015-07-29 16:27:21 -0400

    Updated permissions to be consistent with of permissions

--- api/nodes/permissions.py
+++ api/nodes/permissions.py
@@ -52,18 +52,8 @@ class Contributor(permissions.BasePermission):
             return node.is_public or node.can_view(auth)
         elif request.method == 'DELETE':
             return is_admin or is_current_user
-        elif request.method == 'PUT':
-            return self.put_permission_check(node, user, is_admin, is_current_user)
         else:
-            return False
-
-    def put_permission_check(self, node, user, is_admin, is_current):
-        if is_admin:
-            return True
-        elif len(node.get_permissions(user)) > 1 and is_current:
-            return True
-        else:
-            return False
+            return is_admin
 
 
 class ContributorOrPublicForPointers(permissions.BasePermission):
