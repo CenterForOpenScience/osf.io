a12cb148cf31ef0c2dabb56c1285cf5578014c8f
Caner Uguz <caneruguz@gmail.com>
2015-08-03 15:16:22 -0400

    Create a jQuery version of toggle height [skip ci]

---
+++ website/static/js/osfToggleheight.js
@@ -0,0 +1,84 @@
+
+'use strict';
+var jQuery = require('jquery');
+
+(function ( $, window, document, undefined ) {
+
+	// Create the defaults once
+	var pluginName = 'osfToggleHeight';
+	var defaults = {
+			height:  50,
+			iconDown : 'fa fa-angle-down',
+			iconUp : 'fa fa-angle-up'
+	};
+
+	// The actual plugin constructor
+	function ToggleHeight ( element, options ) {
+		var self = this;
+		self.element = element;
+		self.$el = $(element);
+		self.settings = $.extend( {}, defaults, options );
+		self._defaults = defaults;
+		self._name = pluginName;
+
+		self.init =  function () {
+		    self.collapsed = true;
+			self.showToggle = false;
+			self.gradientDiv = $('<div class="' + self.element.id + '-gradient toggle-height-gradient" style="display:none"></div>').appendTo(self.element);
+			self.toggleDiv = $('<div class="' + self.element.id + '-toggle toggle-height-toggle text-center" style="display:none"></div>').insertAfter(self.element);
+		    $('.' + self.element.id + '-toggle.toggle-height-toggle').click(self.toggle);
+		    self.checkCollapse();
+		    $(window).resize(self.checkCollapse);
+		};
+		self.noToggle = function () {
+	        self.$el.css('height', 'auto');
+	        self.gradientDiv.hide();
+	        self.toggleDiv.hide();
+	        self.showToggle = false;
+	        self.$el.removeClass('toggle-height-parent');
+	    },
+	    self.toggleCollapse = function () {
+	        self.$el.css('height', self.settings.height + 'px');
+	        self.gradientDiv.show();
+	        self.toggleDiv.html('<i class="' + self.settings.iconDown +'"></i>').show();
+	    };
+	    self.toggleOpen = function (){
+	        self.$el.css('height', 'auto');
+	        self.gradientDiv.hide();
+	        self.toggleDiv.html('<i class="' + self.settings.iconUp + '"></i>').show();
+	    };
+	    self.checkCollapse = function _checkCollapse () {
+	        if (self.element.scrollHeight <= self.settings.height){
+	            self.noToggle();
+	        } else if (!self.showToggle){
+	            self.showToggle = true;
+	            self.$el.addClass('toggle-height-parent');
+	            if (self.collapsed){
+	                this.toggleCollapse();
+	            } else {
+	                self.toggleOpen();
+	            }
+	        }
+	    };
+	    self.toggle = function () {
+	        if (self.collapsed) {
+	            self.toggleOpen();
+	        } else {
+	            self.toggleCollapse();
+	        }
+	        self.collapsed = !self.collapsed;
+	    };
+
+		self.init();
+	}
+
+
+	$.fn.osfToggleHeight = function ( options ) {
+		return this.each(function() {
+			if ( !$.data( this, 'plugin_' + pluginName ) ) {
+					$.data( this, 'plugin_' + pluginName, new ToggleHeight( this, options ) );
+			}
+		});
+	};
+
+})( jQuery, window, document );
