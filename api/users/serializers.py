0761d204443c927e3595851535a2047a444aa915
Billy Hunt <billy@cos.io>
2015-08-04 16:11:21 -0400

    allowed blank fields when necessary and made gravatar field read only

--- api/users/serializers.py
+++ api/users/serializers.py
@@ -39,25 +39,25 @@ class UserSerializer(JSONAPISerializer):
     parser_classes = (JSONParser,)
     id = ser.CharField(read_only=True, source='_id')
     fullname = ser.CharField(help_text='Display name used in the general user interface')
-    given_name = ser.CharField(required=False, help_text='For bibliographic citations')
-    middle_name = ser.CharField(required=False, source='middle_names', help_text='For bibliographic citations')
-    family_name = ser.CharField(required=False, help_text='For bibliographic citations')
-    suffix = ser.CharField(required=False, help_text='For bibliographic citations')
+    given_name = ser.CharField(required=False, allow_blank=True, help_text='For bibliographic citations')
+    middle_name = ser.CharField(required=False, allow_blank=True, source='middle_names', help_text='For bibliographic citations')
+    family_name = ser.CharField(required=False, allow_blank=True, help_text='For bibliographic citations')
+    suffix = ser.CharField(required=False, allow_blank=True, help_text='For bibliographic citations')
     date_registered = ser.DateTimeField(read_only=True)
-    gravatar_url = ser.CharField(required=False, help_text='URL for the icon used to identify the user. Relies on http://gravatar.com ')
+    gravatar_url = ser.CharField(required=False, read_only=True, help_text='URL for the icon used to identify the user. Relies on http://gravatar.com ')
     employment_institutions = JobsSerializer(read_only=True, required=False, source='jobs', help_text='An array of dictionaries representing the '
                                                                                                       'places the user has worked')
     educational_institutions = SchoolsSerializer(read_only=True, required=False, source='schools', help_text='An array of dictionaries representing the '
                                                                                                              'places the user has worked')
     # Social Fields are broken out to get around DRF complex object bug and to make API updating more user friendly.
-    github = ser.CharField(required=False, source='social.github', help_text='Github Handle')
-    scholar = ser.CharField(required=False, source='social.scholar', help_text='Google Scholar Account')
-    personal_website = ser.CharField(required=False, source='social.personal', help_text='Personal Website')
-    twitter = ser.CharField(required=False, source='social.twitter', help_text='Twitter Handle')
-    linkedIn = ser.CharField(required=False, source='social.linkedIn', help_text='LinkedIn Account')
-    impactStory = ser.CharField(required=False, source='social.impactStory', help_text='ImpactStory Account')
-    orcid = ser.CharField(required=False, source='social.orcid', help_text='orcid Account Number ex 1111 1111 1111 1111')
-    researcherId = ser.CharField(required=False, source='social.researcherId', help_text='ResearcherId Account')
+    github = ser.CharField(required=False, source='social.github', allow_blank=True, help_text='Github Handle')
+    scholar = ser.CharField(required=False, source='social.scholar', allow_blank=True, help_text='Google Scholar Account')
+    personal_website = ser.CharField(required=False, source='social.personal', allow_blank=True, help_text='Personal Website')
+    twitter = ser.CharField(required=False, source='social.twitter', allow_blank=True, help_text='Twitter Handle')
+    linkedIn = ser.CharField(required=False, source='social.linkedIn', allow_blank=True, help_text='LinkedIn Account')
+    impactStory = ser.CharField(required=False, source='social.impactStory', allow_blank=True, help_text='ImpactStory Account')
+    orcid = ser.CharField(required=False, source='social.orcid', allow_blank=True, help_text='orcid Account Number ex 1111 1111 1111 1111')
+    researcherId = ser.CharField(required=False, source='social.researcherId', allow_blank=True, help_text='ResearcherId Account')
 
     links = LinksField({
         'html': 'absolute_url',
