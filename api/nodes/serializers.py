9b224a4d072ebb22080725803d0e694e80d22ec3
Dawn Pattison <pattison.dawn.r@gmail.com>
2015-08-06 10:04:39 -0400

    Change files and parent into HyperLinkedIdentityFields because not supplying count information

--- api/nodes/serializers.py
+++ api/nodes/serializers.py
@@ -48,16 +48,13 @@ class NodeSerializer(JSONAPISerializer):
     #         'self': Link('nodes:node-detail', kwargs={'node_id': '<parent_id>'})
     #     }
     # })
+
     children = HyperLinkedIdentityFieldWithMeta(view_name='nodes:node-children', lookup_field='pk', lookup_url_kwarg='node_id', count='get_node_count')
     contributors = HyperLinkedIdentityFieldWithMeta(view_name='nodes:node-contributors', lookup_field='pk', lookup_url_kwarg='node_id', count='get_contrib_count')
     pointers = HyperLinkedIdentityFieldWithMeta(view_name='nodes:node-pointers', lookup_field='pk', lookup_url_kwarg='node_id', count='get_pointers_count')
     registrations = HyperLinkedIdentityFieldWithMeta(view_name='nodes:node-registrations', lookup_field='pk', lookup_url_kwarg='node_id', count='get_registration_count')
-    files = HyperLinkedIdentityFieldWithMeta(view_name='nodes:node-files', lookup_field='pk', lookup_url_kwarg='node_id')
-    parent = HyperLinkedIdentityFieldWithMeta(view_name='nodes:node-detail', lookup_field='parent_id', lookup_url_kwarg='node_id')
-    # children_count = ser.SerializerMethodField()
-    # contributors_count = ser.SerializerMethodField()
-    # pointers_count = ser.SerializerMethodField()
-    # registrations_count = ser.SerializerMethodField()
+    files = ser.HyperlinkedIdentityField(view_name='nodes:node-files', lookup_field='pk', lookup_url_kwarg='node_id')
+    parent = ser.HyperlinkedIdentityField(view_name='nodes:node-detail', lookup_field='parent_id', lookup_url_kwarg='node_id')
 
     # TODO: When we have 'admin' permissions, make this writable for admins
     public = ser.BooleanField(source='is_public', read_only=True,
