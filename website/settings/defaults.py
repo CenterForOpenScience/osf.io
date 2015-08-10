de3883b8e162d21b58f3afcaac78a5ec2a82a276
Bryan Gorges <bgorges@cos.io>
2015-08-10 10:16:50 -0400

    Move celery settings into own file

--- website/settings/defaults.py
+++ website/settings/defaults.py
@@ -9,7 +9,6 @@ import os
 import json
 import hashlib
 from datetime import timedelta
-from celery.schedules import crontab
 
 os_env = os.environ
 
@@ -176,38 +175,6 @@ WIKI_WHITELIST = {
         'list-style',
     ]
 }
-
-##### Celery #####
-## Default RabbitMQ broker
-BROKER_URL = 'amqp://'
-
-# Default RabbitMQ backend
-CELERY_RESULT_BACKEND = 'amqp://'
-
-# CELERYBEAT_SCHEDULE_FILENAME = 'celery_beat.py'
-# Setting up a scheduler
-CELERYBEAT_SCHEDULE = {
-    '5-minute-emails': {
-        'task': 'notify.send_users_email',
-        'schedule': crontab(minute='*/5'),
-        'args': ('email_transactional',),
-    },
-    'daily-emails': {
-        'task': 'notify.send_users_email',
-        'schedule': crontab(minute=0, hour=0),
-        'args': ('email_digest',),
-    },
-}
-
-# Modules to import when celery launches
-CELERY_IMPORTS = (
-    'framework.tasks',
-    'framework.tasks.signals',
-    'framework.email.tasks',
-    'framework.analytics.tasks',
-    'website.mailchimp_utils',
-    'website.notifications.tasks'
-)
 
 # Add-ons
 # Load addons from addons.json
