80dc64527983aa94619a6492171a5a3110b60b67
Fabian von Feilitzsch <fabian@fabianism.us>
2015-08-10 11:44:52 -0400

    Turn loadingIcon into a component

--- website/static/js/share/utils.js
+++ website/static/js/share/utils.js
@@ -31,7 +31,11 @@ utils.formatNumber = function(num) {
     return num;
 };
 
-var loadingIcon = m('img[src=/static/img/loading.gif]',{style: {margin: 'auto', display: 'block'}});
+utils.loadingIcon = {
+    view: function(ctrl) {
+        return m('img', {src: '/static/img/loading.gif'});
+    }
+};
 
 /* This resets the state of the vm on error */
 utils.errorState = function(vm){
