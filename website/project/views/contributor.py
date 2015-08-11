49bd064a57cd9ad5f72bf51de85f64e71351efb6
Saman Ehsan <se8ea@virginia.edu>
2015-08-11 17:53:09 -0400

    Check whether parent_node exists
    Check if parent_node exists before
    checking whether the user is a
    contributor on the parent_node.

--- website/project/views/contributor.py
+++ website/project/views/contributor.py
@@ -526,7 +526,7 @@ def notify_added_contributor(node, contributor, throttle=None):
     # via 'add_contributor' but does not need to get notified.
     # Only email users for projects, or for components where they are not contributors on the parent node.
     if (contributor.is_registered and not node.template_node and not node.is_fork
-            and not node.parent_node or not node.parent_node.is_contributor(contributor)):
+            and not node.parent_node or (node.parent_node and not node.parent_node.is_contributor(contributor))):
         contributor_record = contributor.contributor_added_email_records.get(node._id, {})
         if contributor_record:
             timestamp = contributor_record.get('last_sent', None)
