25cf2c6b1be8c33de8cf54f1023870eeabf5a7b2
haoyuchen1992 <hc6as@virginia.edu>
2015-08-11 09:46:27 -0400

    refine error message

--- website/addons/github/static/node-cfg.js
+++ website/addons/github/static/node-cfg.js
@@ -7,7 +7,7 @@ var AddonHelper = require('js/addonHelper');
 $(window.contextVars.githubSettingsSelector).on('submit',
     function(){
         AddonHelper.onSubmitSettings({
-            successUpdateMsg: 'Github add-on successfully authorized',
-            failUpdateMsg: 'Github add-on doesn\'t  authorized'
+            successUpdateMsg: 'Github add-on successfully updated.',
+            failUpdateMsg: 'Github add-on doesn\'t updated successfully. Please try again or check your internet connection.'
         });
     });
