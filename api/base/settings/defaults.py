917672f9fdc89ca59aa17c2973849f9c7450a376
Dawn Pattison <pattison.dawn.r@gmail.com>
2015-08-12 11:27:41 -0400

    Restore JSONRenderer to DEFAULT_RENDERER_CLASSES

--- api/base/settings/defaults.py
+++ api/base/settings/defaults.py
@@ -67,6 +67,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework_json_api.renderers.JSONRenderer',
+        'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
