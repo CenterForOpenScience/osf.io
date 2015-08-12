9f79aee81ac48160fb2c24491538e176e399e758
Dawn Pattison <pattison.dawn.r@gmail.com>
2015-08-12 11:38:54 -0400

    Update docstring for custom select_renderer.

--- api/base/content_negotiation.py
+++ api/base/content_negotiation.py
@@ -5,15 +5,14 @@ class CustomClientContentNegotiation(DefaultContentNegotiation):
 
     def select_renderer(self, request, renderers, format_suffix=None):
         """
-        If 'application/json' in acceptable media types, use the JSONAPIRenderer with
-        media_type "application/vnd.api+json".  Otherwise, check media types against
-        each renderer.  Returns a tuple (renderer, media_type).
+        If 'application/json' in acceptable media types, use the first renderer in
+        DEFAULT_RENDERER_CLASSES which should be 'rest_framework_json_api.renderers.JSONRenderer'.
+        Media_type "application/vnd.api+json".  Otherwise, use default select_renderer.
+        Returns a tuple (renderer, media_type).
         """
         accepts = self.get_accept_list(request)
 
         if 'application/json' in accepts:
             return (renderers[0], renderers[0].media_type)
 
-        return DefaultContentNegotiation.select_renderer(self,request, renderers)
-
-
+        return DefaultContentNegotiation.select_renderer(self, request, renderers)
