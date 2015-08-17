5801329319e51ad4bba5bb84718c49f20b616959
Ben Yetton <bdyetton@gmail.com>
2015-08-17 13:26:28 -0400

    changed to american date

--- website/static/js/share/stats.js
+++ website/static/js/share/stats.js
@@ -129,8 +129,7 @@ Stats.sourcesByDatesAgg = function () {
 /* Helper function for dealing with epoch times returned by elasticsearch */
 Stats.timeSinceEpochInMsToMMDDYY = function (timeSinceEpochInMs) {
     var d = new Date(timeSinceEpochInMs);
-    return (d.getMonth()+1).toString() + '/' + (d.getDate()+1).toString() + '/' + 
-        d.getFullYear().toString().substring(2);
+    return (d.getMonth()+1).toString() + '/' + (d.getDate()+1).toString() + '/' + d.getFullYear().toString().substring(2);
 };
 
 /* Parses elasticsearch data so that it can be fed into a c3 donut graph */
