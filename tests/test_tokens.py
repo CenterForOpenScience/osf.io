87a955eb4eab3fdede6a3dbf660bf5083c4dedf2
Harry Rybacki <hrybacki@gmail.com>
2015-08-17 14:40:39 -0400

    Begin testing TokenHandler class

---
+++ tests/test_tokens.py
@@ -0,0 +1,47 @@
+from website import settings
+from website.tokens import TokenHandler
+from website.tokens.exceptions import TokenHandlerNotFound
+
+from nose.tools import *  # noqa
+from tests.base import OsfTestCase
+
+import jwt
+
+class TestTokenHandler(OsfTestCase):
+
+    def setUp(self):
+        super(TestTokenHandler, self).setUp()
+
+        self.payload = {
+            'user_id': 'abc123',
+            'field_x': 'xyzert'
+        }
+        self.secret = settings.JWT_SECRET
+        self.encoded_token = jwt.encode(self.payload, self.secret, algorithm='HS256')
+
+    def test_encode(self):
+        assert_equal(TokenHandler.encode(self.payload), self.encoded_token)
+
+    def test_decode(self):
+        assert_equal(TokenHandler.decode(self.encoded_token), self.payload)
+
+    def test_from_string(self):
+        token = TokenHandler.from_string(self.encoded_token)
+        assert_equal(token.encoded_token, self.encoded_token)
+        assert_equal(token.payload, self.payload)
+
+    def test_from_token(self):
+        token = TokenHandler.from_token(self.payload)
+        assert_equal(token.encoded_token, self.encoded_token)
+        assert_equal(token.payload, self.payload)
+
+    def test_token_process_for_invalid_action_raises_TokenHandlerNotFound(self):
+        self.payload['action'] = 'not a handler'
+        token = TokenHandler.from_token(self.payload)
+        with assert_raises(TokenHandlerNotFound):
+            token.process()
+
+    def test_token_process_with_valid_action(self):
+        self.payload['action'] = 'approve_registration_approval'
+        token = TokenHandler.from_token(self.payload)
+        assert_is_none(token.process())
