b8dfb9fe2459f3f9139b3774e69fe51240a15205
Dawn Pattison <pattison.dawn.r@gmail.com>
2015-08-20 09:31:42 -0400

    Checkout COS/develop

--- api/nodes/views.py
+++ api/nodes/views.py
@@ -308,12 +308,15 @@ class NodeFilesList(generics.ListAPIView, NodeMixin):
             'waterbutler_type': 'file',
             'item_type': item['kind'],
         }
-
-        if file_item['item_type'] != 'folder':
-            file_item['content_type'] = item['contentType']
-            file_item['modified'] = item['modified']
-            file_item['size'] = item['size']
-            file_item['extra'] = item['extra']
+        if file_item['item_type'] == 'folder':
+            file_item['metadata'] = {}
+        else:
+            file_item['metadata'] = {
+                'content_type': item['contentType'],
+                'modified': item['modified'],
+                'size': item['size'],
+                'extra': item['extra'],
+            }
         return file_item
 
     def get_queryset(self):
