1fa0eb2c792b3cc89d27b322c80548f022b7fbb9
Dawn Pattison <pattison.dawn.r@gmail.com>
2015-08-21 13:03:42 -0400

    Modify exception handler to cover multiple data types i.e. dict and list and handle when more than one error returned

--- api/base/exceptions.py
+++ api/base/exceptions.py
@@ -1,18 +1,34 @@
-from rest_framework.exceptions import APIException
-from rest_framework import status
 
-def jsonapi_exception_handler(exc, context):
+from rest_framework import status
+from rest_framework.exceptions import APIException
+
+
+def json_api_exception_handler(exc, context):
     """
-    Custom exception handler that returns errors object as an array with a 'detail' member
+    Custom exception handler that returns errors object as an array
     """
     from rest_framework.views import exception_handler
     response = exception_handler(exc, context)
 
+    # Title removed to avoid clash with node "title" errors
+    acceptable_members = ['id', 'links', 'status', 'code', 'detail', 'source', 'meta']
+    errors = []
+
     if response is not None:
-        if 'detail' in response.data:
-            response.data = {'errors': [response.data]}
+        message = response.data
+        if isinstance(message, dict):
+            for key, value in message.iteritems():
+                if key in acceptable_members:
+                    errors.append({key: value})
+                else:
+                    errors.append({'detail': {key: value}})
+        elif isinstance(message, list):
+            for error in message:
+                errors.append({'detail': error})
         else:
-            response.data = {'errors': [{'detail': response.data}]}
+            errors.append({'detail': message})
+
+    response.data = {'errors': errors}
 
     return response
 
