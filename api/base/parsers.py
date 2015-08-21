63918dd053fe05511c539a9eac7f9333e2555dab
Dawn Pattison <pattison.dawn.r@gmail.com>
2015-08-21 13:09:08 -0400

    Add docstring to class

--- api/base/parsers.py
+++ api/base/parsers.py
@@ -3,5 +3,8 @@ from rest_framework.parsers import JSONParser
 from api.base.renderers import JSONAPIRenderer
 
 class JSONAPIParser(JSONParser):
+    """
+    Parses JSON-serialized data. Overrides media_type.
+    """
     media_type = 'application/vnd.api+json'
     renderer_class = JSONAPIRenderer
