3ae6c1cd2b728724b26da5215f4518528e5529ed
Sam Chrisinger <s.chrisinger@gmail.com>
2015-08-24 15:13:26 -0400

    Add untracked licenses.js:

---
+++ website/static/js/licenses.js
@@ -0,0 +1,26 @@
+var $ = require('jquery');
+
+var licenses = $.map(require('list-of-licenses'), function(value, key) {
+    value.id = key;
+    return value;
+});
+
+var DEFAULT_LICENSE = {
+    id: 'NONE',
+    name: 'None Selected',
+    text: 'Copyright {{year}} {{copyrightHolders}}',
+    properties: ['Year', 'Copyright Holders']
+};
+var OTHER_LICENSE = {
+    id: 'OTHER',
+    name: 'Other',
+    text: 'Please see the "license.txt" uploaded in this project\'s OSF Storage'
+};
+licenses.unshift(DEFAULT_LICENSE);
+licenses.push(OTHER_LICENSE);
+
+module.exports = {
+    list: licenses,
+    DEFAULT_LICENSE: DEFAULT_LICENSE
+};
+    
