ea9c48b13801cd78bd1aa99f927d56446a43557f
Chris Seto <chriskseto@gmail.com>
2015-08-27 10:37:46 -0400

    Whitelisted nodes no longer count against users, add comments

--- scripts/osfstorage/usage_audit.py
+++ scripts/osfstorage/usage_audit.py
@@ -33,27 +33,26 @@ WHITE_LIST_PATH = os.path.join(os.path.dirname(__file__), 'usage_whitelist.json'
 
 try:
     with open(WHITE_LIST_PATH, 'r') as fobj:
-        WHITE_LIST = json.load(fobj)
+        WHITE_LIST = set(json.load(fobj))  # Cast to set for constant time look ups
     logger.info('Loaded whitelist.json from {}'.format(WHITE_LIST_PATH))
 except IOError:
-    WHITE_LIST = []
+    WHITE_LIST = set()
     logger.warning('No whitelist found')
 
 
 def add_to_white_list(gtg):
     gtg = set(gtg).difference(WHITE_LIST)
     logger.info('Adding {} to whitelist'.format(gtg))
-    WHITE_LIST.extend(gtg)
     with open(WHITE_LIST_PATH, 'w') as fobj:
-        json.dump(WHITE_LIST, fobj)
+        json.dump(list(WHITE_LIST.union(gtg)), fobj)  # Sets are not JSON serializable
     logger.info('Whitelist updated to {}'.format(WHITE_LIST))
 
 
 def get_usage(node):
     addon = node.get_addon('osfstorage')
-    calculate_usage = lambda m: sum([v.size or 0 for fn in m.find(Q('node_settings', 'eq', addon)) for v in fn.versions])
-    usage = (calculate_usage(model.OsfStorageFileNode), calculate_usage(model.OsfStorageTrashedFileNode))
-    return map(sum, zip(*([usage] + [get_usage(child) for child in node.nodes])))
+    calculate_usage = lambda m: sum([v.size or 0 for fn in m.find(Q('node_settings', 'eq', addon)) for v in fn.versions])  # Sum all versions of all files of this node
+    usage = (calculate_usage(model.OsfStorageFileNode), calculate_usage(model.OsfStorageTrashedFileNode))  # Sum both deleted and not deleted files, keep as a tuple (files, deletedfiles)
+    return map(sum, zip(*([usage] + [get_usage(child) for child in node.nodes])))  # Adds tuples together, map(sum, zip((a, b), (c, d))) -> (a+c, b+d)
 
 
 def main(send_email=False):
@@ -61,18 +60,18 @@ def main(send_email=False):
     init_app(set_backends=True, routes=False)
 
     lines = []
-    users = defaultdict(lambda: (0, 0))
     projects = {}
+    users = defaultdict(lambda: (0, 0))
 
-    for node in Node.find():
-        if node.parent_id:
-            continue  # Dont bother with children
+    for node in Node.find(Q('__backrefs.parent.node.nodes', 'eq', None)):  # ODM hack to ignore all nodes with parents
+        if node._id in WHITE_LIST:
+            continue  # Dont count whitelisted nodes against users
         projects[node] = get_usage(node)
         for contrib in node.contributors:
             if node.can_edit(user=contrib):
-                users[contrib] = tuple(map(sum, zip(users[contrib], projects[node])))
+                users[contrib] = tuple(map(sum, zip(users[contrib], projects[node])))  # Adds tuples together, map(sum, zip((a, b), (c, d))) -> (a+c, b+d)
 
-    filter_factory = lambda x: lambda (i, (u, d)): i._id not in WHITE_LIST and (u + d) >= x
+    filter_factory = lambda x: lambda (i, (u, d)): i._id not in WHITE_LIST and (u + d) >= x  # Returns a lambda that returns if i is in the whitelist and its storage usage is >= x
 
     for collection, limit in ((users, USER_LIMIT), (projects, PROJECT_LIMIT)):
         for item, (used, deleted) in filter(filter_factory(limit), collection.items()):
