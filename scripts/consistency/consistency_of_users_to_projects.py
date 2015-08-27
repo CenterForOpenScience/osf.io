b07f56b1010cc46c734cacae835bc9296da40cd0
Steven Loria <sloria1@gmail.com>
2015-08-27 10:10:21 -0400

    Improve consistency script
    - Handle nodes without a creator
    - Handle "null" nodes
    - Allow script to be run without file logging ("dry" arg)
    - Allow function to be called in shell
    
    [skip ci]

--- scripts/consistency/consistency_of_users_to_projects.py
+++ scripts/consistency/consistency_of_users_to_projects.py
@@ -2,12 +2,14 @@
 
 """
 import logging
+import sys
+
+from modularodm import Q
+
 from website.app import init_app
 from website import models
-from modularodm import Q
 from scripts import utils as script_utils
 
-app = init_app()
 logger = logging.getLogger(__name__)
 
 
@@ -16,22 +18,29 @@ def check_consistency_of_users_to_projects():
     nodes = models.Node.find(
         Q('is_deleted', 'eq', False)
     )
-    count = 0
+    no_creator, creator_ref, contrib_ref = [], [], []
     for node in nodes:
-        if node._id not in node.creator.created:
+        if not node.creator:
+            logger.info('Node {} has no creator'.format(node._id))
+            no_creator.append(node)
+        elif node and node._id not in node.creator.created:
             logger.info('Creator {} is inconsistent in reference with node {}'.format(node.creator._id, node._id))
-            count += 1
+            creator_ref.append(node)
         for contributor in node.contributors:
-            if node._id not in contributor.contributed:
+            if node and node._id not in contributor.contributed:
                 logger.info('Contributor {} is inconsistent in reference with node {}'.format(contributor._id, node._id))
-                count += 1
+                contrib_ref.append(node)
 
+    count = len(no_creator) + len(creator_ref) + len(contrib_ref)
     if count == 0:
         logger.info("Consistency of users to projects' check is done. There is no inconsistency found.")
     else:
         logger.info("Consistency of users to projects' check is done. There are {} inconsistency found.".format(count))
+    return no_creator, creator_ref, contrib_ref
 
 
 if __name__ == '__main__':
-    script_utils.add_file_logger(logger, __file__)
+    app = init_app(routes=False)
+    if 'dry' not in sys.argv:
+        script_utils.add_file_logger(logger, __file__)
     check_consistency_of_users_to_projects()
