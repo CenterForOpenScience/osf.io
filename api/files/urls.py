9856361b48bb481f7913eaf69be668225c5bb818
Chris Seto <chriskseto@gmail.com>
2015-09-04 12:01:56 -0400

    Remove the files list endpoint

--- api/files/urls.py
+++ api/files/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import url
 from api.files import views
 
 urlpatterns = [
-    url(r'^$', views.FileList.as_view(), name='file-list'),
     url(r'^(?P<file_id>\w+)/$', views.FileDetail.as_view(), name='file-detail'),
     url(r'^(?P<file_id>\w+)/versions/$', views.FileVersionsList.as_view(), name='file-versions'),
     url(r'^(?P<file_id>\w+)/versions/(?P<version_id>\w+)/$', views.FileVersionDetail.as_view(), name='version-detail'),
