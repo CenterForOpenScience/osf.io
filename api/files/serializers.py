27568c1a5526fc3850835a5d81ba4afeb03668c0
Chris Seto <chriskseto@gmail.com>
2015-09-04 12:01:44 -0400

    Add the kind field, update links

--- api/files/serializers.py
+++ api/files/serializers.py
@@ -2,10 +2,9 @@ import furl
 
 from rest_framework import serializers as ser
 
-from website import util
 from website import settings
 from api.base.utils import absolute_reverse
-from api.base.serializers import JSONAPISerializer, LinksField, JSONAPIHyperlinkedIdentityField
+from api.base.serializers import Link, JSONAPISerializer, LinksField, NodeFileHyperLink, WaterbutlerLink
 
 
 class FileSerializer(JSONAPISerializer):
@@ -13,28 +12,32 @@ class FileSerializer(JSONAPISerializer):
         'id',
         'name',
         'node',
+        'kind',
         'path',
         'provider',
         'last_touched',
     ])
     id = ser.CharField(read_only=True, source='_id')
     name = ser.CharField(help_text='Display name used in the general user interface')
+    kind = ser.CharField(help_text='Either folder or file')
     path = ser.CharField(help_text='The unique path used to reference this object')
     provider = ser.CharField(help_text='The Add-on service this file originates from')
     last_touched = ser.DateTimeField(help_text='The last time this file had information fetched about it via the OSF')
     # history = ser.ListField(ser.DictField(), help_text='A raw dump of the metadata recieved whenever infomation is fetched about it')
 
-    links = LinksField({'self': 'waterbutler_link'})
+    files = NodeFileHyperLink(kind='folder', read_only=True, link_type='related', view_name='nodes:node-files', kwargs=('node_id', 'path', 'provider'))
+    versions = NodeFileHyperLink(kind='file', read_only=True, link_type='related', view_name='files:file-versions', kwargs=(('file_id', '_id'), ))
 
-    node = JSONAPIHyperlinkedIdentityField(view_name='nodes:node-detail', lookup_field='node_id', lookup_url_kwarg='node_id',
-                                             link_type='related')
+    links = LinksField({
+        'info': Link('files:file-detail', kwargs={'file_id': '<_id>'}),
+        'move': WaterbutlerLink(),
+        'upload': WaterbutlerLink(),
+        'download': WaterbutlerLink(must_be_file=True),
+        'new_folder': WaterbutlerLink(must_be_folder=True, kind='folder')
+    })
 
     class Meta:
         type_ = 'files'
-
-    @staticmethod
-    def waterbutler_link(obj):
-        return util.waterbutler_api_url_for(obj.node._id, obj.provider, obj.path)
 
 
 class FileVersionSerializer(JSONAPISerializer):
@@ -68,6 +71,3 @@ class FileVersionSerializer(JSONAPISerializer):
             path=(fobj.node._id, 'files', fobj.provider, fobj.path.lstrip('/')),
             query={fobj.version_idenifier: obj.identifier}
         ).url
-
-    def update(self, instance, validated_data):
-        pass
