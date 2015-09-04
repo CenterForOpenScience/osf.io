159ae7cce37fbfbfef731e0284565fe54fafb04d
Sam Chrisinger <s.chrisinger@gmail.com>
2015-09-04 10:36:25 -0400

    Add missing import

--- api/base/utils.py
+++ api/base/utils.py
@@ -5,6 +5,7 @@ from modularodm.exceptions import NoResultsFound
 from rest_framework.exceptions import NotFound
 from rest_framework.reverse import reverse
 import furl
+import collections
 
 from website import util as website_util  # noqa
 from website import settings as website_settings
