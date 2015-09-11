80638b2070f578408f00d7a263ccfb27fea5b1d4
Dawn Pattison <pattison.dawn.r@gmail.com>
2015-09-11 10:43:58 -0400

    Update delete warning to include instructions that same query parameters need to be in request

--- api/base/language.py
+++ api/base/language.py
@@ -1,4 +1,5 @@
 from django.utils.translation import ugettext_lazy as _
 
 BEFORE_BULK_DELETE = _('Are you sure you want to delete these projects? They will no longer be '
-                       'available to other contributors.  Send delete request to new URL to confirm.')
+                       'available to other contributors.  Send delete request to new URL with same '
+                       'query parameters to confirm.')
