b819c2c5c086a356b87d2fca5c946b36e423cc8d
Chris Seto <chriskseto@gmail.com>
2015-09-14 09:52:39 -0400

    Remove duplicate code

--- api/files/permissions.py
+++ api/files/permissions.py
@@ -1,16 +1,7 @@
 from rest_framework import permissions
 
-from framework.auth import Auth
+from api.base.utils import get_user_auth
 from website.files.models import FileNode
-
-
-def get_user_auth(request):
-    user = request.user
-    if user.is_anonymous():
-        auth = Auth(None)
-    else:
-        auth = Auth(user)
-    return auth
 
 
 class ContributorOrPublic(permissions.BasePermission):
