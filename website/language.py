c8235554867f24f5aae83b1bd5920dc96d3bd86e
Harry Rybacki <hrybacki@gmail.com>
2015-09-16 15:43:24 -0400

    Fix broken registration warning rendering
    - Removed <p> tags to ensure styling matched the rest of the page
    - Added `n` filter to allow formatting of large text blob

--- website/language.py
+++ website/language.py
@@ -129,18 +129,18 @@ of the registration.</p>
 '''
 
 REGISTRATION_EMBARGO_INFO = '''
-<p>You can choose whether to make your registration public immediately or
+You can choose whether to make your registration public immediately or
 embargo it for up to four years. At the end of the embargo period the registration
 is automatically made public. After becoming public, the only way to remove a
 registration is to retract it. Retractions show only the registration title,
 contributors, and description to indicate that a registration was made and
-later retracted.</p>
-
-<p>If you choose to embargo your registration, a notification will be sent to
+later retracted.
+<br /><br />
+If you choose to embargo your registration, a notification will be sent to
 all other project contributors. Other administrators will have 48 hours to
 approve or cancel creating the registration. If any other administrator rejects the
 registration, it will be canceled. If all other administrators approve or do
-nothing, the registration will be confirmed and enter its embargo period.</p>
+nothing, the registration will be confirmed and enter its embargo period.
 '''
 
 BEFORE_REGISTRATION_INFO = '''
