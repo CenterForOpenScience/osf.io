3e32117455d7ba9a3c86aa084e319fe2b46fc67c
Fitz Elliott <fitz@cos.io>
2015-09-17 15:10:54 -0400

    second draft of files api documentation

--- api/files/views.py
+++ api/files/views.py
@@ -57,10 +57,35 @@ class FileList(generics.ListAPIView, ODMFilterMixin):
 
 
 class FileDetail(generics.RetrieveUpdateAPIView, FileMixin):
-    """File and folder details.
+    """Details about files and folders
+
+    ##Attributes
 
     Both files and folders are accessed through this endpoint and may be distinguished by the `kind` attribute. `size`
     will be `null` for folders.
+
+        name:         name of the file or folder
+        kind:         "file" or "folder"
+        path:         path to this entity, used in "move" actions
+        size:         size of file in bytes, null for folders
+        provider:     storage provider for this file. "osfstorage" if stored on the OSF.  Other examples include
+                      "s3" for Amazon S3, "googledrive" for Google Drive, "box" for Box.com.
+        last_touched: last time the metadata for the file was retrieved. only applies to non-OSF storage
+                      providers.
+
+    ##Relationships
+
+    ###Checkout
+
+    **TODO**
+
+    ###Files (*folders*)
+
+    The `files` endpoint lists all of the subfiles and folders of the current folder. Will be `null` for files.
+
+    ###Versions (*files*)
+
+    The `versions` endpoint provides version history for files.  Will be `null` for folders.
 
     ##Actions
 
@@ -69,38 +94,78 @@ class FileDetail(generics.RetrieveUpdateAPIView, FileMixin):
 
     ###Get Info (*files, folders*)
 
-        GET /v2/files/55ea00a9cfe1915f0e57d67d/
+        Method: GET
+        URL:    data.links.info
 
     The details of a particular file can be retreived by performing a GET request against the url in the `info` property.
 
     ###Download (*files*)
+
+        Method: GET
+        URL:    data.links.download
 
     To download a file, issue a GET request against the url in the `download` property.  The response will have the
     Content-Disposition header set, which will will trigger a download in a browser.
 
     ###Create Subfolder (*folders*)
 
+        Method:       PUT
+        URL:          data.links.new_folder
+        Query Params: ?name=<new folder name>
+        Body:         <empty>
+
     You can create a subfolder of an existing folder by issuing a PUT request against the `new_folder` url.  The name of
     the new subfolder should be provided in the `name` query parameter.
 
     ###Upload New File (*folders*)
+
+        Method:       PUT
+        URL:          data.links.upload
+        Query Params: ?kind=file&name=<new file name>
+        Body (Raw):   <file data (not form-encoded)>
+        Body:         <empty>
 
     To upload a file to a folder, issue a PUT request to the folder's `upload` url with the raw file data in the request
     body, and the `kind` and `name` query parameters set to `'file'` and the desired name of the file.
 
     ###Update Existing File (*file*)
 
+        Method:       PUT
+        URL:          data.links.upload
+        Query Params: kind=file&name=<new file name>
+        Body (Raw):   <file data (not form-encoded)>
+
     To update an existing file, issue a PUT request to the file's `upload` url with the raw file data in the request
     body, and the `kind` and `name` query parameters set to `'file'` and the desired name of the file.
 
     ###Rename (*files, folders*)
+
+        Method:        POST
+        URL:           data.links.move
+        Query Params:  <none>
+        Body (JSON):   {
+                        "action": "rename",
+                        "rename": <new file name>
+                       }
 
     To rename a file or folder, issue a POST request to the `move` url with the `action` body parameter set to
     `'rename'` and the `rename` body parameter set to the desired name.
 
     ###Move & Copy (*files, folders*)
 
-    Move and copy actions both use the same request structure, with different values for the `action` body parameters.
+        Method:        POST
+        URL:           data.links.move
+        Query Params:  <none>
+        Body (JSON):   {
+                        // mandatory
+                        "action":   "move|copy",
+                        "path":     <path attribute of target folder>,
+                        // optional
+                        "rename":   <new name>,
+                        "conflict": "replace|keep" // defaults to 'replace'
+                       }
+
+    Move and copy actions both use the same request structure, a POST to the `move` url, but with different values for the `action` body parameters.
     The `path` parameter is also required and should be the `path` attribute of the folder being written to.  The
     `rename` and `conflict` parameters are optional.  If you wish to change the file's or folder's name at its
     destination, set the `rename` parameter to the new name.  The `conflict` param governs how name clashes are
@@ -110,8 +175,11 @@ class FileDetail(generics.RetrieveUpdateAPIView, FileMixin):
 
     ###Delete (*file, folders*)
 
-    To delete a file, send a DELETE request to the `download` url for files, or the `upload` url for folders.
+        Method:       DELETE
+        URL (file):   data.links.download
+        URL (folder): data.links.upload
 
+    To delete a file, send a DELETE request to the `download` url for files, or the `upload` url for folders.
     """
     serializer_class = FileSerializer
     permission_classes = (
