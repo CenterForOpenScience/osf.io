78c08fc6ed8ae656de1d52ed39bd283676e810f8
Billy Hunt <Billy@billyhunt.com>
2015-09-18 16:16:55 -0400

    add tests to migration

--- scripts/migration/migrate_personal_to_profile_websites.py
+++ scripts/migration/migrate_personal_to_profile_websites.py
@@ -3,16 +3,19 @@
 """Script to migrate nodes with invalid categories."""
 
 import logging
-import sys
 
 from modularodm import Q
 from nose.tools import *
+from tests.base import OsfTestCase
 
 from website import models
 from website.app import init_app
 from scripts import utils as scripts_utils
+from tests.factories import AuthUserFactory
+
 
 logger = logging.getLogger(__name__)
+
 
 def main():
     init_app(routes=False)
@@ -35,12 +38,84 @@ def main():
 
 def get_users_with_personal_websites():
     return models.User.find(
-        Q('social', 'ne', None)
+        Q('social', 'ne', {})
     )
 
 
 def migrate_personal_to_profile_websites(user):
-    user.social['profileWebsites'][0] = user.social.get('personal')
+    user.social['profileWebsites'].append(user.social.get('personal'))
+
+
+class TestMigrateProfileWebsites(OsfTestCase):
+
+    def setUp(self):
+        super(TestMigrateProfileWebsites, self).setUp()
+        self.user_one = AuthUserFactory.build(
+            fullname='Martin Luther King',
+            social=dict(
+                github='userOneGithub',
+                scholar='userOneScholar',
+                personal='http://www.useronewebsite.com',
+                twitter='userOneTwitter',
+                linkedIn='userOneLinkedIn',
+                impactStory='userOneImpactStory',
+                orcid='userOneOrcid',
+                researcherId='userOneResearcherId',
+            ),
+        )
+        self.user_one.save()
+
+        self.user_two = AuthUserFactory.build(
+            fullname='el-Hajj Malik el-Shabazz',
+            social=dict(
+                github='userTwoGithub',
+                scholar='userTwoScholar',
+                profileWebsites=['http://www.usertwowebsite.com'],
+                twitter='userTwoTwitter',
+                linkedIn='userTwoLinkedIn',
+                impactStory='userTwoImpactStory',
+                orcid='userTwoOrcid',
+                researcherId='userTwoResearcherId'
+            )
+        )
+        self.user_two.save()
+        self.user_three = AuthUserFactory()
+
+    def test_get_users_with_personal_website(self):
+        users = []
+        for user in get_users_with_personal_websites():
+            users.append(user)
+
+        assert_equal(self.user_one._id, users[0]._id)
+        assert_equal(self.user_two._id, users[1]._id)
+        assert_equal(len(users), 2)
+
+    def test_migrate_profile_websites(self):
+        main()
+        self.user_one.reload()
+        assert_equal(self.user_one.fullname, 'Martin Luther King')
+        assert_equal(self.user_one.given_name, 'Martin')
+        assert_equal(self.user_one.middle_names, 'Luther')
+        assert_equal(self.user_one.family_name, 'King')
+        assert_equal(self.user_one.social['scholar'], 'userOneScholar')
+        assert_equal(self.user_one.social['profileWebsites'], ['http://www.useronewebsite.com'])
+        assert_equal(self.user_one.social['twitter'], 'userOneTwitter')
+        assert_equal(self.user_one.social['linkedIn'], 'userOneLinkedIn')
+        assert_equal(self.user_one.social['impactStory'], 'userOneImpactStory')
+        assert_equal(self.user_one.social['orcid'], 'userOneOrcid')
+        assert_equal(self.user_one.social['researcherId'], 'userOneResearcherId')
+        self.user_two.reload()
+        assert_equal(self.user_two.fullname, 'el-Hajj Malik el-Shabazz')
+        assert_equal(self.user_two.social['scholar'], 'userTwoScholar')
+        assert_equal(self.user_two.social['profileWebsites'], ['http://www.usertwowebsite.com'])
+        assert_equal(self.user_two.social['twitter'], 'userTwoTwitter')
+        assert_equal(self.user_two.social['linkedIn'], 'userTwoLinkedIn')
+        assert_equal(self.user_two.social['impactStory'], 'userTwoImpactStory')
+        assert_equal(self.user_two.social['orcid'], 'userTwoOrcid')
+        assert_equal(self.user_two.social['researcherId'], 'userTwoResearcherId')
+
+        assert_equal(self.user_three.social, {})
+
 
 if __name__ == '__main__':
     main()
