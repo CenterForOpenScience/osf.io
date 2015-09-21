aaedcb0c5540f87607717cbc31c1e1566201bc3b
Nan Chen <nc6r@virginia.edu>
2015-09-21 15:36:01 -0400

    fix the logic for sending email to notify contributor added. Closes [#OSF-4573]

--- website/project/views/contributor.py
+++ website/project/views/contributor.py
@@ -549,8 +549,9 @@ def notify_added_contributor(node, contributor, throttle=None):
     # Exclude forks and templates because the user forking/templating the project gets added
     # via 'add_contributor' but does not need to get notified.
     # Only email users for projects, or for components where they are not contributors on the parent node.
-    if (contributor.is_registered and not node.template_node and not node.is_fork
-            and not node.parent_node or (node.parent_node and not node.parent_node.is_contributor(contributor))):
+    if (contributor.is_registered and not node.template_node and not node.is_fork and
+            (not node.parent_node or
+                (node.parent_node and not node.parent_node.is_contributor(contributor)))):
         contributor_record = contributor.contributor_added_email_records.get(node._id, {})
         if contributor_record:
             timestamp = contributor_record.get('last_sent', None)
