008cc980db2228f4ac517b8a6872077c43703258
Dawn Pattison <pattison.dawn.r@gmail.com>
2015-09-21 00:38:54 -0400

    Remove attributes property from Node model

--- website/project/model.py
+++ website/project/model.py
@@ -686,21 +686,6 @@ class Node(GuidStoredObject, AddonModelMixin, IdentifierMixin):
             return None
 
     @property
-    def attributes(self):
-        return OrderedDict([
-            ('title', self.title),
-            ('description', self.description),
-            ('category', self.category),
-            ('date_created', self.date_created),
-            ('date_modified', self.date_modified),
-            ('tags', self.tags),
-            ('registration', self.is_registration),
-            ('collection', self.is_folder),
-            ('dashboard', self.is_dashboard),
-            ('public', self.is_public)
-        ])
-
-    @property
     def node_links_attributes(self):
         return {'target_node_id': self._id}
 
