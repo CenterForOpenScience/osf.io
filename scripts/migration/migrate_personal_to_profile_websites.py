8896d9811f46d71193a6bbe0ccb1c929cfbc4194
Billy Hunt <Billy@billyhunt.com>
2015-09-22 12:04:06 -0400

    remove dry run and clean up test

--- scripts/migration/migrate_personal_to_profile_websites.py
+++ scripts/migration/migrate_personal_to_profile_websites.py
@@ -19,9 +19,6 @@ logger = logging.getLogger(__name__)
 
 def main():
     init_app(routes=False)
-    dry_run = 'dry' in sys.argv
-    if not dry_run:
-        scripts_utils.add_file_logger(logger, __file__)
     logger.info("migrating personal to profileWebsites")
             
     for user in get_users_with_personal_websites():
@@ -32,8 +29,7 @@ def main():
             if user.social.get('personal'):
                 migrate_personal_to_profile_websites(user)
         logger.info(repr(user.social))
-        if not dry_run:
-            user.save()
+        user.save()
 
 
 def get_users_with_personal_websites():
@@ -84,10 +80,9 @@ class TestMigrateProfileWebsites(OsfTestCase):
     def test_get_users_with_personal_website(self):
         users = []
         for user in get_users_with_personal_websites():
-            users.append(user)
-
-        assert_equal(self.user_one._id, users[0]._id)
-        assert_equal(self.user_two._id, users[1]._id)
+            users.append(user._id)
+        assert_in(self.user_one._id, users)
+        assert_in(self.user_two._id, users)
         assert_equal(len(users), 2)
 
     def test_migrate_profile_websites(self):
