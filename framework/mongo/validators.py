6b1b6fbcbd9db1a57d2456890c689cc68385a548
Andy Boughton <abought@gmail.com>
2015-09-23 18:32:11 -0400

    Add validator to restrict modularODM field to a set of pre-defined choices

--- framework/mongo/validators.py
+++ framework/mongo/validators.py
@@ -7,3 +7,31 @@ from modularodm.exceptions import ValidationValueError
 def string_required(value):
     if value is None or value == '':
         raise ValidationValueError('Value must not be empty.')
+    return True
+
+
+def choice_in(choices, match_case=True):
+    """
+    Validate that the option provided is from a restricted set of choices
+
+    Returns a callable that can be used as a validator in ModularODM
+    :param choices: An iterable of choices allowed for use
+    :param bool match_case: If false, perform case-insensitive comparison (for strings)
+    :return:
+    """
+
+    if match_case:
+        choice_set = set(e.upper() if isinstance(e, basestring) else e
+                         for e in choices)
+    else:
+        choice_set = set(choices)
+
+    def validator(value):
+        if match_case is True and isinstance(value, basestring):
+            value = value.upper()
+
+        if value in choice_set:
+            return True
+        else:
+            raise ValidationValueError('Value must be one of these options: {}'.format(choices))
+    return validator
---
+++ tests/framework_tests/test_mongo.py
@@ -0,0 +1,44 @@
+"""
+Tests related to functions in framework.mongo
+"""
+from unittest import TestCase
+
+from nose.tools import *  # flake8: noqa
+
+from modularodm.exceptions import ValidationValueError
+
+from framework.mongo import validators
+
+class TestValidators(TestCase):
+    def test_string_required_passes_with_string(self):
+        assert_true(validators.string_required('Hi!'))
+
+    def test_string_required_fails_when_empty(self):
+
+        with assert_raises(ValidationValueError):
+            validators.string_required(None)
+            validators.string_required('')
+
+    def test_choice_validator_finds_only_exact_match(self):
+        new_validator = validators.choice_in(('value1', 'value2', 'VaLuE3'),
+                                             match_case=True)
+
+        assert_true(new_validator('value1'))
+        assert_true(new_validator('VaLuE3'))
+
+        with assert_raises(ValidationValueError):
+            new_validator('Value2')
+
+    def choice_validator_fails_when_option_not_present(self):
+        new_validator = validators.choice_in(('value1', 'value2', 'VaLuE3'),
+                                             match_case=True)
+        with assert_raises(ValidationValueError):
+            new_validator('You')
+            new_validator(123)
+
+    def choice_validator_case_insensitive(self):
+        new_validator = validators.choice_in(('value1', 'value2', 'VaLuE3'),
+                                             match_case=False)
+        assert_true(new_validator('VaLuE3'))
+        assert_true(new_validator('value3'))
+
