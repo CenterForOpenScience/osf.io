91229ab93609f66af866f7ef87b576a84546aeab
Dawn Pattison <pattison.dawn.r@gmail.com>
2015-09-24 15:10:28 -0400

    Raise Parse Error if request data is not a dictionary

--- api/base/parsers.py
+++ api/base/parsers.py
@@ -1,4 +1,5 @@
 from rest_framework.parsers import JSONParser
+from rest_framework.exceptions import ParseError
 
 from api.base.renderers import JSONAPIRenderer
 from api.base.exceptions import JSONAPIException
@@ -15,16 +16,18 @@ class JSONAPIParser(JSONParser):
         Parses the incoming bytestream as JSON and returns the resulting data
         """
         result = super(JSONAPIParser, self).parse(stream, media_type=media_type, parser_context=parser_context)
+        if not isinstance(result, dict):
+            raise ParseError()
         data = result.get('data', {})
 
         if data:
             if 'attributes' not in data:
                 raise JSONAPIException(source={'pointer': '/data/attributes'}, detail='This field is required.')
             id = data.get('id')
-            type = data.get('type')
+            object_type = data.get('type')
             attributes = data.get('attributes')
 
-            parsed = {'id': id, 'type': type}
+            parsed = {'id': id, 'type': object_type}
             parsed.update(attributes)
 
             return parsed
