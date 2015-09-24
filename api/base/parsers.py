61a23918dc458b9d7cfdff96cf40f45500c8d18b
Dawn Pattison <pattison.dawn.r@gmail.com>
2015-09-24 11:54:16 -0400

    Have parser handle bulk data

--- api/base/parsers.py
+++ api/base/parsers.py
@@ -17,17 +17,29 @@ class JSONAPIParser(JSONParser):
         result = super(JSONAPIParser, self).parse(stream, media_type=media_type, parser_context=parser_context)
         data = result.get('data', {})
 
-        if data:
-            if 'attributes' not in data:
-                raise JSONAPIException(source={'pointer': '/data/attributes'}, detail='This field is required.')
-            id = data.get('id')
-            type = data.get('type')
-            attributes = data.get('attributes')
+        def data_flattener(resource_object, stream):
+            if "attributes" not in resource_object and stream.method != 'DELETE':
+                    raise JSONAPIException(source={'pointer': '/data/attributes'}, detail='This field is required.')
+            id = resource_object.get('id')
+            type = resource_object.get('type')
+            attributes = resource_object.get('attributes')
 
             parsed = {'id': id, 'type': type}
-            parsed.update(attributes)
+            if attributes:
+                parsed.update(attributes)
 
             return parsed
+
+        if data:
+            if isinstance(data, list):
+                data_collection = []
+                for object in data:
+                    parsed_data = data_flattener(object, stream)
+                    data_collection.append(parsed_data)
+                return data_collection
+
+            else:
+                return data_flattener(data, stream)
 
         else:
             raise JSONAPIException(source={'pointer': '/data'}, detail='This field is required.')
