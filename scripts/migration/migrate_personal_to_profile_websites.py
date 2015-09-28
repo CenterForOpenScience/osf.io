0531ce7b8c7ad3a796a188f033a89a39b16f5d27
Billy Hunt <Billy@billyhunt.com>
2015-09-28 16:34:29 -0400

    remove tests and improve logging

--- scripts/migration/migrate_personal_to_profile_websites.py
+++ scripts/migration/migrate_personal_to_profile_websites.py
@@ -5,13 +5,8 @@
 import logging
 
 from modularodm import Q
-from nose.tools import *
-from tests.base import OsfTestCase
-
 from website import models
 from website.app import init_app
-from scripts import utils as scripts_utils
-from tests.factories import AuthUserFactory
 
 
 logger = logging.getLogger(__name__)
@@ -20,16 +15,19 @@ logger = logging.getLogger(__name__)
 def main():
     init_app(routes=False)
     logger.info("migrating personal to profileWebsites")
-            
+    all_users = 0
+    migrated_users = 0
     for user in get_users_with_social_field():
-        logger.info(repr(user))
-        logger.info("Migrating User: %s" % repr(user._id))
+        all_users += 1
+        logger.info("Migrating User: %s" % repr(user.fullname))
         if not user.social.get('profileWebsites', None):
             user.social['profileWebsites'] = []
             if user.social.get('personal'):
                 migrate_personal_to_profile_websites(user)
-        logger.info(repr(user.social))
+                migrated_users += 1
+        logger.info("%s/'s social dictionary is now %s" % (repr(user.social), repr(user.fullname)))
         user.save()
+    logger.info("merged %d users to profileWebsites out of %d total users" % (migrated_users, all_users))
 
 
 def get_users_with_social_field():
@@ -40,72 +38,6 @@ def get_users_with_social_field():
 
 def migrate_personal_to_profile_websites(user):
     user.social['profileWebsites'].append(user.social.get('personal'))
-
-
-class TestMigrateProfileWebsites(OsfTestCase):
-
-    def setUp(self):
-        super(TestMigrateProfileWebsites, self).setUp()
-        self.user_one = AuthUserFactory.build(
-            fullname='Martin Luther King',
-            social=dict(
-                github='userOneGithub',
-                scholar='userOneScholar',
-                personal='http://www.useronewebsite.com',
-                twitter='userOneTwitter',
-                linkedIn='userOneLinkedIn',
-                impactStory='userOneImpactStory',
-                orcid='userOneOrcid',
-                researcherId='userOneResearcherId',
-            ),
-        )
-        self.user_one.save()
-
-        self.user_two = AuthUserFactory.build(
-            fullname='el-Hajj Malik el-Shabazz',
-            social=dict(
-                github='userTwoGithub',
-                scholar='userTwoScholar',
-                profileWebsites=['http://www.usertwowebsite.com'],
-                twitter='userTwoTwitter',
-                linkedIn='userTwoLinkedIn',
-                impactStory='userTwoImpactStory',
-                orcid='userTwoOrcid',
-                researcherId='userTwoResearcherId'
-            )
-        )
-        self.user_two.save()
-        self.user_three = AuthUserFactory()
-
-    def test_get_users_with_personal_website(self):
-        users = []
-        for user in get_users_with_personal_websites():
-            users.append(user._id)
-        assert_in(self.user_one._id, users)
-        assert_in(self.user_two._id, users)
-        assert_equal(len(users), 2)
-
-    def test_migrate_profile_websites(self):
-        main()
-        self.user_one.reload()
-        assert_equal(self.user_one.social['scholar'], 'userOneScholar')
-        assert_equal(self.user_one.social['profileWebsites'], ['http://www.useronewebsite.com'])
-        assert_equal(self.user_one.social['twitter'], 'userOneTwitter')
-        assert_equal(self.user_one.social['linkedIn'], 'userOneLinkedIn')
-        assert_equal(self.user_one.social['impactStory'], 'userOneImpactStory')
-        assert_equal(self.user_one.social['orcid'], 'userOneOrcid')
-        assert_equal(self.user_one.social['researcherId'], 'userOneResearcherId')
-        self.user_two.reload()
-        assert_equal(self.user_two.social['scholar'], 'userTwoScholar')
-        assert_equal(self.user_two.social['profileWebsites'], ['http://www.usertwowebsite.com'])
-        assert_equal(self.user_two.social['twitter'], 'userTwoTwitter')
-        assert_equal(self.user_two.social['linkedIn'], 'userTwoLinkedIn')
-        assert_equal(self.user_two.social['impactStory'], 'userTwoImpactStory')
-        assert_equal(self.user_two.social['orcid'], 'userTwoOrcid')
-        assert_equal(self.user_two.social['researcherId'], 'userTwoResearcherId')
-
-        assert_equal(self.user_three.social, {})
-
 
 if __name__ == '__main__':
     main()
