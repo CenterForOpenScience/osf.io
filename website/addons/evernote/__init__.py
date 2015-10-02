d79aad50667220236bc0e277671c2020eee7d298
Raymond Yee <raymond.yee@gmail.com>
2015-10-01 18:32:07 -0700

    minimal __init__.py to get evernote add-on to show up as an option...can't persist the selection of the add-on yet.

---
+++ website/addons/evernote/__init__.py
@@ -0,0 +1,31 @@
+# MUST
+
+SHORT_NAME = 'evernote'
+FULL_NAME = 'Evernote'
+
+ROUTES = []
+
+MODELS = []
+
+ADDED_DEFAULT = []
+ADDED_MANDATORY = []
+
+VIEWS = []
+
+# SHOULD be one of documentation, storage, citations, security, bibliography, and other
+# Additional categories can be added to ADDON_CATEGORIES in website.settings.defaults
+# evernote is the documentation category, according to https://cos.io/integrationgrants/ (https://www.evernote.com/l/AAG7vFn4MqFHd7g6tDChuwk2DKPWhp0oNCM)
+
+CATEGORIES = ['documentation']
+
+# https://github.com/CenterForOpenScience/COSDev/blame/9cbf2db92fca22796c2c68593bd18bdcca97a0ed/docs/osf/addons.rst#L95
+# Deprecated field, define as empty dict (``{}``)
+
+INCLUDE_JS = {}
+INCLUDE_CSS = {}
+
+# I think the following are musts too
+
+#OWNERS = ['user', 'node']
+OWNERS = ['user', 'node']
+
