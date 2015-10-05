8f427800d8d1ff99a7284f86b078f1aedf531db5
Steven Loria <sloria1@gmail.com>
2015-10-05 14:07:35 -0400

    Use NO_LOGIN_TYPE constant
    h/t @hmoco
    
    [skip ci]

--- website/mails/presends.py
+++ website/mails/presends.py
@@ -8,8 +8,8 @@ def no_addon(email):
     return len(email.user.get_addons()) == 0
 
 def no_login(email):
-    from website.models import QueuedMail
-    sent = QueuedMail.find(Q('user', 'eq', email.user) & Q('email_type', 'eq', 'no_login'))
+    from website.models import QueuedMail, NO_LOGIN_TYPE
+    sent = QueuedMail.find(Q('user', 'eq', email.user) & Q('email_type', 'eq', NO_LOGIN_TYPE))
     if sent.count():
         return False
     return email.user.date_last_login > datetime.utcnow() - settings.NO_LOGIN_WAIT_TIME
