01ccb8cee9fc1a9bf4501c8326f306b401a0f1f2
Matt Frazier <maf7sm@virginia.edu>
2015-10-05 15:28:25 -0400

    Combine scripts, update 'dry' template

--- scripts/box/migrate_to_external_account.py
+++ scripts/box/migrate_to_external_account.py
@@ -8,6 +8,14 @@ Changes:
  - Create external account for authorized user settings
  - Attach external account to user settings
  - Attach external account to all node settings
+
+
+First run the following in a mongo shell:
+
+    db.getCollection('boxnodesettings').update({'user_settings': {'$type': 2}}, {$rename: { user_settings: 'foreign_user_settings'}}, {multi: true})
+
+Then change the user_settings field of BoxNodeSettings to foreign_user_settings
+
 """
 
 import sys
@@ -26,57 +34,49 @@ from website.oauth.models import ExternalAccount
 logger = logging.getLogger(__name__)
 
 
-def do_migration(records, dry):
+def do_migration(records):
     for user_addon in records:
         user = user_addon.owner
         old_account = user_addon.oauth_settings
 
         logger.info('Record found for user {}'.format(user._id))
 
-        if not dry:
-            # Create/load external account and append to user
-            try:
-                account = ExternalAccount(
-                    provider='box',
-                    provider_name='Box',
-                    display_name=old_account.username,
-                    oauth_key=old_account.access_token,
-                    refresh_token=old_account.refresh_token,
-                    provider_id=old_account.user_id,
-                    expires_at=old_account.expires_at,
-                )
-                account.save()
-            except KeyExistsException:
-                # ... or get the old one
-                account = ExternalAccount.find_one(
-                    Q('provider', 'eq', 'box') &
-                    Q('provider_id', 'eq', old_account.user_id)
-                )
-                assert account is not None
-            user.external_accounts.append(account)
-            user.save()
+        # Create/load external account and append to user
+        try:
+            account = ExternalAccount(
+                provider='box',
+                provider_name='Box',
+                display_name=old_account.username,
+                oauth_key=old_account.access_token,
+                refresh_token=old_account.refresh_token,
+                provider_id=old_account.user_id,
+                expires_at=old_account.expires_at,
+            )
+            account.save()
+        except KeyExistsException:
+            # ... or get the old one
+            account = ExternalAccount.find_one(
+                Q('provider', 'eq', 'box') &
+                Q('provider_id', 'eq', old_account.user_id)
+            )
+            assert account is not None
+        user.external_accounts.append(account)
+        user.save()
 
-            # Remove oauth_settings from user settings object
-            user_addon.clear()
-            user_addon.save()
+        # Remove oauth_settings from user settings object
+        user_addon.clear()
+        user_addon.save()
 
-            logger.info('Added external account {0} to user {1}'.format(
-                account._id, user._id,
-            ))
+        logger.info('Added external account {0} to user {1}'.format(
+            account._id, user._id,
+        ))
 
-        if dry:
-            logger.info('[Dry] Creating Box ExternalAccount:\n\tdisplay_name={0}\n\toauth_key={1}\n\trefresh_token={2}\n\tprovider_id={3}'.format(
-                old_account.username, old_account.access_token, old_account.refresh_token, old_account.user_id
-            ))
-            logger.info('[Dry] Added external account to user {0}'.format(
-                user._id,
-            ))
-
-            for node_addon in get_authorized_node_settings(user_addon):
-                logger.info('[Dry] Added external account to node {0}'.format(
-                    node_addon.owner._id,
-                ))
-
+        for node in BoxNodeSettings.find():
+            if node.foreign_user_settings is None:
+                continue
+            logger.info('Migrating user_settings for box {}'.format(node._id))
+            node.user_settings = node.foreign_user_settings
+            node.save()
 
 
 def get_targets():
@@ -85,17 +85,12 @@ def get_targets():
     )
 
 
-def get_authorized_node_settings(user_addon):
-    """Returns node settings authorized by a given user settings object"""
-    return BoxNodeSettings.find(
-        Q('user_settings', 'eq', user_addon)
-    )
-
-
 def main(dry=True):
     init_app(set_backends=True, routes=False)  # Sets the storage backends on all models
     with TokuTransaction():
         do_migration(get_targets(), dry=dry)
+        if dry:
+            raise Exception('Abort Transaction - Dry Run')
 
 
 if __name__ == '__main__':
