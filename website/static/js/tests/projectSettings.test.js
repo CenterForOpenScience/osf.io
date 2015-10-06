c5de0b0b1a7eb0554a36208fbe7046a0e1d6ecee
Zak <zik3qe@virginia.edu>
2015-10-06 01:51:05 -0400

    update tests

--- website/static/js/tests/projectSettings.test.js
+++ website/static/js/tests/projectSettings.test.js
@@ -6,6 +6,7 @@ var faker = require('faker');
 
 var $osf = require('js/osfHelpers');
 var Raven = require('raven-js');
+var language = require('js/osfLanguage').ProjectSettings;
 
 /*
  * Dear sloria,
@@ -25,7 +26,7 @@ window.contextVars = {
 
 var ProjectSettings = require('js/projectSettings.js');
 
-var ProjectSettings= ProjectSettings.ProjectSettings;
+var ProjectSettings = ProjectSettings.ProjectSettings;
 
 describe('ProjectSettings', () => {
     var category = faker.internet.domainWord();
@@ -40,7 +41,7 @@ describe('ProjectSettings', () => {
             var broken = function() {
                 new ProjectSettings({category: category, categoryOptions: categoryOptions});
             };
-            assert.throws(broken , vm.INSTANTIATION_ERROR_MESSAGE);
+            assert.throws(broken , language.instantiationErrorMessage);
         });
         it('implements the changeMessage interface', () => {
             assert.isTrue(vm.hasOwnProperty('message'));
@@ -49,20 +50,7 @@ describe('ProjectSettings', () => {
             assert.isTrue(Boolean(vm.resetMessage) || false);
         });
     });
-    describe('#updateSuccess', () => {
-        var changeMessageSpy;
-        before(() => {
-            changeMessageSpy = sinon.spy(vm, 'changeMessage');
-        });
-        after(() => {
-            vm.changeMessage.restore();
-        });
-        it('updates the message', () => {
-            vm.updateSuccess();
-            assert.calledWith(changeMessageSpy, vm.UPDATE_SUCCESS_MESSAGE, vm.MESSAGE_SUCCESS_CLASS);
-        });
-    });
-    describe('#updateCategoryError', () => {
+    describe('#updateError', () => {
         var changeMessageSpy;
         var ravenStub;
         before(() => {
@@ -75,32 +63,18 @@ describe('ProjectSettings', () => {
         });
         it('updates the message, and captures the error with Raven', () => {
             var error = faker.lorem.sentence();
-            vm.updateCategoryError({}, error, {});
-            assert.calledWith(changeMessageSpy, vm.UPDATE_CATEGORY_ERROR_MESSAGE, vm.MESSAGE_ERROR_CLASS);
-            assert.calledWith(ravenStub, vm.UPDATE_CATEGORY_ERROR_MESSAGE_RAVEN, {
+            vm.updateError({}, error, {});
+            assert.calledWith(changeMessageSpy, language.updateErrorMessage, 'text-error');
+            assert.calledWith(ravenStub, language.updateErrorMessage, {
                 url: updateUrl,
                 textStatus: error,
                 err: {},
             });
         });
     });
-    describe('#updateDescriptionError', () => {
-        var changeMessageSpy;
-        before(() => {
-            changeMessageSpy = sinon.spy(vm, 'changeMessage');
-        });
-        after(() => {
-            vm.changeMessage.restore();
-        });
-        it('updates the message', () => {
-            vm.updateDescriptionError();
-            assert.calledWith(changeMessageSpy, vm.UPDATE_DESCRIPTION_ERROR_MESSAGE, vm.MESSAGE_ERROR_CLASS);
-        });
-    });
-    describe('#updateCategory', () => {
+    describe('#updateAll', () => {
         var server;
         var serverSpy = sinon.spy();
-        var updateTitleSpy = sinon.spy(vm, 'updateTitle');
         before(() => {
             server = sinon.fakeServer.create();
             server.respondWith(
@@ -122,82 +96,14 @@ describe('ProjectSettings', () => {
         after(() => {
             server.restore();
         });
-        it('sends a put to the updateUrl with the selected category, and updates the category on success', (done) => {
+        it('sends a put to the updateUrl with the settings inputs and updates them on success', (done) => {
             var newcategory = categoryOptions[0];
-            vm.selectedCategory(newcategory);
-            vm.updateCategory()
-                .always(function() {
-                    assert.called(serverSpy);
-                    assert.calledWith(updateTitleSpy, newcategory);
-                    done();
-                });
-            server.respond();
-        });
-    });
-    describe('#updateTitle', () => {
-        var server;
-        var serverSpy = sinon.spy();
-        var updateDescriptionSpy = sinon.spy(vm, 'updateDescription');
-        before(() => {
-            server = sinon.fakeServer.create();
-            server.respondWith(
-                'PUT',
-                updateUrl,
-                function(xhr) {
-                    serverSpy();
-                    var response = {
-                        'updated_fields': JSON.parse(xhr.requestBody)
-                    };
-                    xhr.respond(
-                        200,
-                        {'Content-Type': 'application/json'},
-                        JSON.stringify(response)
-                    );
-                }
-            );
-        });
-        after(() => {
-            server.restore();
-        });
-        it('sends a put to the updateUrl with user input, and updates the project title on success', (done) => {
+            vm.categoryPlaceholder(newcategory);
             vm.title('New title');
-            vm.updateTitle()
-                .always(function() {
-                    assert.called(serverSpy);
-                    assert.calledWith(updateDescriptionSpy);
-                    done();
-                });
-            server.respond();
-        });
-    });
-    describe('#updateDescription', () => {
-        var server;
-        var serverSpy = sinon.spy();
-        var updateSuccessSpy = sinon.spy(vm, 'updateSuccess');
-        before(() => {
-            server = sinon.fakeServer.create();
-            server.respondWith(
-                'PUT',
-                updateUrl,
-                function(xhr) {
-                    serverSpy();
-                    var response = {
-                        'updated_fields': JSON.parse(xhr.requestBody)
-                    };
-                    xhr.respond(
-                        200,
-                        {'Content-Type': 'application/json'},
-                        JSON.stringify(response)
-                    );
-                }
-            );
-        });
-        it('sends a put to the updateUrl with user input, and updates the project description on success', (done) => {
             vm.description('New description');
-            vm.updateDescription()
+            vm.updateAll()
                 .always(function() {
                     assert.called(serverSpy);
-                    assert.calledWith(updateSuccessSpy);
                     done();
                 });
             server.respond();
@@ -211,13 +117,13 @@ describe('ProjectSettings', () => {
         after(() => {
             vm.resetMessage.restore();
         });
-        it('restores the selectedCategory, title, and description to those of the VM, and resets the message', () => {
-            vm.selectedCategory(categoryOptions[0]);
+        it('restores the category, title, and description to those of the VM, and resets the message', () => {
+            vm.categoryPlaceholder(categoryOptions[0]);
             vm.changeMessage('Some message', 'some-class');
             vm.cancelAll();
-            assert.equal(vm.selectedCategory(), vm.category);
-            assert.equal(vm.title(), vm.decodedTitle);
-            assert.equal(vm.description(), vm.decodedDescription);
+            assert.equal(vm.categoryPlaceholder(), vm.category);
+            assert.equal(vm.title(), vm.titlePlaceholder);
+            assert.equal(vm.description(), vm.descriptionPlaceholder);
             assert.called(resetMessageSpy);
         });
     });
