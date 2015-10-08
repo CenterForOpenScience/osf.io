6f8ce858ec0977bfc84eada46ff364c026f492a1
Eric Barbour <emb4gu@virginia.edu>
2015-10-08 09:27:32 -0400

    Fix: use iterObject and tests for it

--- website/static/js/tests/osfHelpers.test.js
+++ website/static/js/tests/osfHelpers.test.js
@@ -415,6 +415,27 @@ describe('osfHelpers', () => {
             assert.calledOnce(bootboxStub);
         });
     });
+    
+    describe('iterObject', () => {
+        var get = function(obj, key) {
+            return obj[key];
+        };
+
+        it('maps an object to an array {key: KEY, value: VALUE} pairs', () => {
+            var obj = {
+                foo: 'bar',
+                cat: 'dog'
+            };
+            var keys = Object.keys(obj);
+            var values = keys.map(get.bind(null, obj));
+            var iterable = $osf.iterObject(obj);
+            for(var i = 0; i < iterable.length; i++) {
+                var item = iterable[i];
+                assert.include(keys, item.key);
+                assert.include(values, item.value);
+            }
+        });
+    });
 
     describe('indexOf', () => {
         it('returns a positive integer index if it finds a matching item', () => {
