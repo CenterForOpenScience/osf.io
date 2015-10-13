01b891b729f73563240b62cb3a76756b53bc76d0
Dawn Pattison <pattison.dawn.r@gmail.com>
2015-10-13 14:06:23 -0400

    Move NodeRegistrationSerializer to RegistrationSerializer

--- api/nodes/serializers.py
+++ api/nodes/serializers.py
@@ -264,34 +264,6 @@ class NodeContributorDetailSerializer(NodeContributorsSerializer):
         return contributor
 
 
-class NodeRegistrationSerializer(NodeSerializer):
-
-    retracted = ser.BooleanField(source='is_retracted', read_only=True,
-        help_text='Whether this registration has been retracted.')
-    date_registered = ser.DateTimeField(source='registered_date', read_only=True, help_text='Date time of registration.')
-
-    registered_by = JSONAPIHyperlinkedIdentityField(
-        view_name='users:user-detail',
-        lookup_field='registered_user_id',
-        link_type='related',
-        lookup_url_kwarg='user_id'
-    )
-
-    registered_from = JSONAPIHyperlinkedIdentityField(
-        view_name='nodes:node-detail',
-        lookup_field='registered_from_id',
-        link_type='related',
-        lookup_url_kwarg='node_id'
-    )
-
-    # TODO: Finish me
-
-    # TODO: Override create?
-
-    def update(self, *args, **kwargs):
-        raise exceptions.ValidationError('Registrations cannot be modified.')
-
-
 class NodeLinksSerializer(JSONAPISerializer):
 
     id = IDField(source='_id', read_only=True)
--- api/registrations/serializers.py
+++ api/registrations/serializers.py
@@ -2,40 +2,45 @@ from rest_framework import serializers as ser
 from rest_framework import exceptions
 
 from api.base.serializers import JSONAPISerializer
-from api.base.serializers import IDField, JSONAPIHyperlinkedRelatedField, LinksField
+from api.base.serializers import IDField, JSONAPIHyperlinkedIdentityField, LinksField
 
 
 class RegistrationSerializer(JSONAPISerializer):
 
     id = IDField(source='_id', read_only=True)
-    registration = ser.BooleanField(read_only=True, source='is_registration')
     retracted = ser.BooleanField(source='is_retracted', read_only=True,
         help_text='Whether this registration has been retracted.')
-    registered_date = ser.DateTimeField(read_only=True)
-    registered_meta = ser.CharField(read_only=True)
-    embargo_end_date = ser.CharField(read_only=True, source='embargo.end_date')
-    registered_schema = ser.CharField(read_only=True)
+    registration = ser.BooleanField(read_only=True, source='is_registration')
+    date_registered = ser.DateTimeField(source='registered_date', read_only=True, help_text='Date time of registration.')
 
-    branched_from = JSONAPIHyperlinkedRelatedField(view_name='nodes:node-detail', lookup_field='pk', link_type='related',
-                                              lookup_url_kwarg='node_id', read_only=True, source='registered_from')
-    initiator = JSONAPIHyperlinkedRelatedField(view_name='users:user-detail', lookup_field='pk', link_type='related',
-                                              lookup_url_kwarg='user_id', read_only=True, source='registered_user')
+    registered_by = JSONAPIHyperlinkedIdentityField(
+        view_name='users:user-detail',
+        lookup_field='registered_user_id',
+        link_type='related',
+        lookup_url_kwarg='user_id'
+    )
 
-    links = LinksField({'html': 'get_absolute_url'})
-
-    class Meta:
-        type_ = 'registrations'
+    registered_from = JSONAPIHyperlinkedIdentityField(
+        view_name='nodes:node-detail',
+        lookup_field='registered_from_id',
+        link_type='related',
+        lookup_url_kwarg='node_id'
+    )
 
     # TODO: Finish me
 
     # TODO: Override create?
 
-    def update(self, *args, **kwargs):
-        raise exceptions.ValidationError('Registrations cannot be modified.')
-
+    links = LinksField({'html': 'get_absolute_url'})
 
     def get_absolute_url(self, obj):
         return obj.absolute_url
+
+    def update(self, *args, **kwargs):
+        raise exceptions.ValidationError('Registrations cannot be modified.')
+
+    class Meta:
+        type_ = 'registrations'
 
 
 class RegistrationDetailSerializer(RegistrationSerializer):
