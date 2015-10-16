72eb6fdb8ddc720d235d1dacd9e57ac51a55b5fb
Dawn Pattison <pattison.dawn.r@gmail.com>
2015-10-15 21:34:37 -0400

    remove Permissions classes

--- api/nodes/views.py
+++ api/nodes/views.py
@@ -49,11 +49,6 @@ class ListBulkCreateJSONAPIView(bulk_generics.ListBulkCreateAPIView):
     in accordance with the JSON API spec
     """
 
-    permission_classes = (
-        drf_permissions.IsAuthenticatedOrReadOnly,
-        base_permissions.TokenHasScope,
-    )
-
     # overrides ListBulkCreateAPIView
     def create(self, request, *args, **kwargs):
         """
@@ -84,10 +79,6 @@ class BulkUpdateJSONAPIView(bulk_generics.BulkUpdateAPIView):
     Custom BulkUpdateAPIView that properly formats bulk update responses in accordance with
     the JSON API spec
     """
-    permission_classes = (
-        drf_permissions.IsAuthenticatedOrReadOnly,
-        base_permissions.TokenHasScope,
-    )
 
     # overrides BulkUpdateAPIView
     def bulk_update(self, request, *args, **kwargs):
@@ -103,11 +94,6 @@ class BulkDestroyJSONAPIView(bulk_generics.BulkDestroyAPIView):
     Custom BulkDestroyAPIView that handles validation and permissions for
     bulk delete
     """
-
-    permission_classes = (
-        drf_permissions.IsAuthenticatedOrReadOnly,
-        base_permissions.TokenHasScope,
-    )
 
     # Overrides BulkDestroyAPIView
     def bulk_destroy(self, request, *args, **kwargs):
