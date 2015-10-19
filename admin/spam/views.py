d256febce974d9ac6e13099271c09291b94071ea
Bryan Gorges <bgorges@cos.io>
2015-10-19 15:59:35 -0400

    Add templates
    Start to list comments in table.
    
    [#ADM-25]

--- admin/spam/views.py
+++ admin/spam/views.py
@@ -1,10 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 from .serializers import serialize_comments
 
 
 def spam_list(request):
-    return HttpResponse('This is a list of spam:{}'.format(' *** '.join(serialize_comments())))
+    comments = serialize_comments()
+    context = {'comments': comments}
+    return render(request, 'spam/spam.html', context)
+    # return HttpResponse('This is a list of spam:{}'.format(' *** '.join(serialize_comments())))
 
 
 def spam_detail(request, spam_id):
