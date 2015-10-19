fbae3139b1af2c4d9b62176021370b39c42efad2
Bryan Gorges <bgorges@cos.io>
2015-10-19 11:26:33 -0400

    Add very basic view of list of comments
    Main issue is whether or not the serializer is a good name for
    this sort of app, or should we go with a more standard name.

--- admin/spam/views.py
+++ admin/spam/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 
+from .serializers import serialize_comments
+
 
 def spam_list(request):
-    return HttpResponse('This is a list of spam')
+    return HttpResponse('This is a list of spam:{}'.format(' *** '.join(serialize_comments())))
 
 
 def spam_detail(request, spam_id):
