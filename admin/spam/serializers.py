27529496e7dc2c4f0b5df45b0134e6a1d58bcf44
Bryan Gorges <bgorges@cos.io>
2015-10-19 15:59:35 -0400

    Add templates
    Start to list comments in table.
    
    [#ADM-25]

--- admin/spam/serializers.py
+++ admin/spam/serializers.py
@@ -1,5 +1,7 @@
 from website.project.model import Comment
+from modularodm import Q
 
 
 def serialize_comments():
-    return [c.content for c in Comment.find()]
+    query = Q('reports', 'size gte', '1')
+    return [c.content for c in Comment.find(query)]
