ef3473890c9b9d10146d2d40e22610718da849cc
Bryan Gorges <bgorges@cos.io>
2015-10-20 11:44:05 -0400

    Show filtered spam
    Display some of the necessary fields.

--- admin/spam/serializers.py
+++ admin/spam/serializers.py
@@ -1,7 +1,46 @@
-from website.project.model import Comment
+from website.project.model import Comment, User
+from website.profile.utils import serialize_user
 from modularodm import Q
+
+
+def serialize_comment(comment):
+    comment = {
+        'id': comment._id,
+        'author': serialize_user(comment.user),
+        'date_created': comment.date_created.isoformat(),
+        'date_modified': comment.date_modified.isoformat(),
+        'content': comment.content,
+        'has_children': bool(getattr(comment, 'commented', [])),
+        'modified': comment.modified,
+        'is_deleted': comment.is_deleted,
+        'reports': serialize_reports(comment.reports),
+        'node': comment.node,
+    }
+    comment.update(
+        number_of_reports=len(comment['reports']),
+        category=comment['reports'][0]['category'],
+    )
+    return comment
 
 
 def serialize_comments():
     query = Q('reports', 'ne', {})
-    return [c.content for c in Comment.find(query)]
+    return [
+        serialize_comment(c)
+        for c in Comment.find(query)
+    ]
+
+
+def serialize_reports(reports):
+    return [
+        serialize_report(user, report)
+        for user, report in reports.iteritems()
+    ]
+
+
+def serialize_report(user, report):
+    return {
+        'reporter': serialize_user(User.load(user)),
+        'category': report.get('category', None),
+        'reason': report.get('text', None),
+    }
