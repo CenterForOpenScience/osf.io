d19fc41d5f741109507cfcf1984d67c610831628
Sam Chrisinger <s.chrisinger@gmail.com>
2015-10-26 14:41:12 -0400

    Add StorageAddonSerializer and update base serializer classes

--- website/addons/box/serializer.py
+++ website/addons/box/serializer.py
@@ -1,33 +1,31 @@
-from website.addons.base.serializer import OAuthAddonSerializer
+from website.addons.base.serializer import StorageAddonSerializer
 
-from website.util import api_url_for, web_url_for
+from website.util import api_url_for
 
 from box.client import BoxClient, BoxClientException
 
 
-class BoxSerializer(OAuthAddonSerializer):
+class BoxSerializer(StorageAddonSerializer):
 
+    # overrides OAuthAddonSerializer
     def credentials_owner(self, user_settings=None):
         return user_settings.owner or self.user_settings.owner
 
-    @property
-    def user_is_owner(self):
-        if self.user_settings is None or self.node_settings is None:
-            return False
+    def creditials_are_valid(self, user_settings, client):
+        if user_settings:
+            client = client or BoxClient(user_settings.external_accounts[0].oauth_key)
+            try:
+                client.get_user_info()
+            except (BoxClientException, IndexError):
+                return False
+        return True
 
-        user_accounts = self.user_settings.external_accounts
-        return bool(
-            (
-                self.node_settings.has_auth and
-                (self.node_settings.external_account in user_accounts)
-            ) or len(user_accounts)
-        )
-
-    @property
-    def serialized_urls(self):
-        ret = self.addon_serialized_urls
-        ret.update({'settings': web_url_for('user_addons')})
-        return ret
+    def serialized_folder(self, node_settings):
+        path = node_settings.fetch_full_folder_path()
+        return {
+            'path': path,
+            'name': path.replace('All Files', '', 1) if path != 'All Files' else '/ (Full Box)'
+        }
 
     @property
     def addon_serialized_urls(self):
@@ -45,50 +43,3 @@ class BoxSerializer(OAuthAddonSerializer):
             'deauthorize': node.api_url_for('box_remove_user_auth'),
             'accounts': node.api_url_for('box_get_user_settings'),
         }
-
-    def serialize_settings(self, node_settings, current_user, client=None):
-        """View helper that returns a dictionary representation of a
-        BoxNodeSettings record. Provides the return value for the
-        box config endpoints.
-        """
-        valid_credentials = True
-        user_settings = node_settings.user_settings
-        self.node_settings = node_settings
-        current_user_settings = current_user.get_addon('box')
-        user_is_owner = user_settings is not None and user_settings.owner == current_user
-
-        if user_settings:
-            try:
-                client = client or BoxClient(user_settings.external_accounts[0].oauth_key)
-                client.get_user_info()
-            except (BoxClientException, IndexError):
-                valid_credentials = False
-
-        result = {
-            'userIsOwner': user_is_owner,
-            'nodeHasAuth': node_settings.has_auth,
-            'urls': self.addon_serialized_urls,
-            'validCredentials': valid_credentials,
-            'userHasAuth': current_user_settings is not None and current_user_settings.has_auth,
-        }
-
-        if node_settings.has_auth:
-            # Add owner's profile URL
-            result['urls']['owner'] = web_url_for(
-                'profile_view_id',
-                uid=user_settings.owner._id
-            )
-            result['ownerName'] = user_settings.owner.fullname
-            # Show available folders
-            # path = node_settings.folder
-
-            if node_settings.folder_id is None:
-                result['folder'] = {'name': None, 'path': None}
-            elif valid_credentials:
-                path = node_settings.fetch_full_folder_path()
-
-                result['folder'] = {
-                    'path': path,
-                    'name': path.replace('All Files', '', 1) if path != 'All Files' else '/ (Full Box)'
-                }
-        return result
--- website/addons/dropbox/serializer.py
+++ website/addons/dropbox/serializer.py
@@ -1,10 +1,28 @@
-from website.util import api_url_for, web_url_for
+from dropbox.rest import ErrorResponse
 
-from website.addons.base.serializer import OAuthAddonSerializer
-
+from website.util import api_url_for
+from website.addons.base.serializer import StorageAddonSerializer
 from website.addons.dropbox.client import get_client_from_user_settings
 
-class DropboxSerializer(OAuthAddonSerializer):
+class DropboxSerializer(StorageAddonSerializer):
+
+    addon_short_name = 'dropbox'
+
+    def credentials_are_valid(self, user_settings, client):
+        if user_settings:
+            client = client or get_client_from_user_settings(user_settings)
+            try:
+                client.account_info()
+            except (ValueError, IndexError, ErrorResponse):
+                return False
+        return True
+
+    def serialized_folder(self, node_settings):
+        path = node_settings.folder
+        return {
+            'name': path if path != '/' else '/ (Full Dropbox)',
+            'path': path
+        }
 
     @property
     def addon_serialized_urls(self):
@@ -18,54 +36,6 @@ class DropboxSerializer(OAuthAddonSerializer):
             'folders': node.api_url_for('dropbox_get_folders'),
             'config': node.api_url_for('dropbox_config_put'),
             'emails': node.api_url_for('dropbox_get_share_emails'),
-            'deauthorize': node.api_url_for('dropbox_oauth_delete_user'),
+            'deauthorize': node.api_url_for('dropbox_remove_user_auth'),
             'accounts': node.api_url_for('dropbox_user_config_get'),
-            'share': None
         }
-
-    def serialize_settings(self, node_settings, current_user, client=None):
-        """View helper that returns a dictionary representation of a
-        BoxNodeSettings record. Provides the return value for the
-        box config endpoints.
-        """
-        valid_credentials = True
-        user_settings = node_settings.user_settings
-        self.node_settings = node_settings
-        current_user_settings = current_user.get_addon('box')
-        user_is_owner = user_settings is not None and user_settings.owner == current_user
-
-        if user_settings:
-            try:
-                client = client or get_client_from_user_settings(user_settings)
-                client.get_user_info()
-            except (ValueError, IndexError):
-                valid_credentials = False
-
-        result = {
-            'userIsOwner': user_is_owner,
-            'nodeHasAuth': node_settings.has_auth,
-            'urls': self.addon_serialized_urls,
-            'validCredentials': valid_credentials,
-            'userHasAuth': current_user_settings is not None and current_user_settings.has_auth,
-        }
-
-        if node_settings.has_auth:
-            # Add owner's profile URL
-            result['urls']['owner'] = web_url_for(
-                'profile_view_id',
-                uid=user_settings.owner._id
-            )
-            result['ownerName'] = user_settings.owner.fullname
-            # Show available folders
-            # path = node_settings.folder
-
-            if node_settings.folder_id is None:
-                result['folder'] = {'name': None, 'path': None}
-            elif valid_credentials:
-                path = node_settings.fetch_full_folder_path()
-
-                result['folder'] = {
-                    'path': path,
-                    'name': path.replace('All Files', '', 1) if path != 'All Files' else '/ (Full Dropbox)'
-                }
-        return result
