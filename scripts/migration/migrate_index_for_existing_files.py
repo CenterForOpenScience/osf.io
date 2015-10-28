faf067ec4f5189a7a0b12fc78b62373a8f997ac8
Henrique Moco <hmoco94@vt.edu>
2015-10-28 13:24:18 -0400

    Change migration to update_file rather than save it

--- scripts/migration/migrate_index_for_existing_files.py
+++ scripts/migration/migrate_index_for_existing_files.py
@@ -5,6 +5,7 @@ import sys
 import logging
 
 from website.app import init_app
+from website.search import search
 from website.files.models.osfstorage import OsfStorageFile
 
 logger = logging.getLogger(__name__)
@@ -19,7 +20,7 @@ def main():
     for file_ in OsfStorageFile.find():
         logger.info('File with _id {0} and name {1} has been saved.'.format(file_._id, file_.name))
         if not dry_run:
-            file_.save()
+            search.update_file(file_)
 
 if __name__ == '__main__':
     main()
