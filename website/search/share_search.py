8627afcbf05440c4ea4196a59dd1ab2c5c34b935
Fabian von Feilitzsch <fabian@fabianism.us>
2015-11-09 13:37:50 -0500

    Be a little less optimistic about the quality of the data

--- website/search/share_search.py
+++ website/search/share_search.py
@@ -36,8 +36,9 @@ def search(query, raw=False, index='share'):
 
     for hit in results['hits']['hits']:
         hit['_source']['highlight'] = hit.get('highlight', {})
-        hit['shareProperties']['docID'] = hit['shareProperties'].get('docID') or hit['_id']
-        hit['shareProperties']['source'] = hit['shareProperties'].get('source') or hit['_type']
+        if hit.get('shareProperties'):
+            hit['shareProperties']['docID'] = hit['shareProperties'].get('docID') or hit['_id']
+            hit['shareProperties']['source'] = hit['shareProperties'].get('source') or hit['_type']
     return results if raw else {
         'results': [hit['_source'] for hit in results['hits']['hits']],
         'count': results['hits']['total'],
