e9031ab6091e3b9d7866c300c8e21b9e81e7e935
Brian J. Geiger <bgeiger@pobox.com>
2015-11-09 17:18:27 -0500

    Remove DEV ONLY on the sub view since the super already has it

--- api/collections/urls.py
+++ api/collections/urls.py
@@ -1,16 +1,11 @@
 from django.conf.urls import url
 
 from api.collections import views
-from website import settings
 
-urlpatterns = []
-
-# Routes only active in local/staging environments
-if settings.DEV_MODE:
-    urlpatterns.extend([
-        url(r'^$', views.CollectionList.as_view(), name='collection-list'),
-        url(r'^(?P<collection_id>\w+)/$', views.CollectionDetail.as_view(), name='collection-detail'),
-        url(r'^(?P<collection_id>\w+)/linked_nodes/$', views.LinkedNodesList.as_view(), name='linked-nodes'),
-        url(r'^(?P<collection_id>\w+)/node_links/$', views.NodeLinksList.as_view(), name='node-pointers'),
-        url(r'^(?P<collection_id>\w+)/node_links/(?P<node_link_id>\w+)/', views.NodeLinksDetail.as_view(), name='node-pointer-detail'),
-    ])
+urlpatterns = [
+    url(r'^$', views.CollectionList.as_view(), name='collection-list'),
+    url(r'^(?P<collection_id>\w+)/$', views.CollectionDetail.as_view(), name='collection-detail'),
+    url(r'^(?P<collection_id>\w+)/linked_nodes/$', views.LinkedNodesList.as_view(), name='linked-nodes'),
+    url(r'^(?P<collection_id>\w+)/node_links/$', views.NodeLinksList.as_view(), name='node-pointers'),
+    url(r'^(?P<collection_id>\w+)/node_links/(?P<node_link_id>\w+)/', views.NodeLinksDetail.as_view(), name='node-pointer-detail'),
+]
