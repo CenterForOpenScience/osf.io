820d42cb6c08e8d1136e9bf16eaa2bd5035dfdf1
Dawn Pattison <pattison.dawn.r@gmail.com>
2015-11-10 15:01:13 -0500

    Add to query_param_generator.  Use method to modify first, last, prev, and next links.

--- api/base/pagination.py
+++ api/base/pagination.py
@@ -49,56 +49,57 @@ class JSONAPIPagination(pagination.PageNumberPagination):
 class EmbeddedPagination(JSONAPIPagination):
 
     def query_param_generator(self, url, page_number):
-        embedded = self.request.parser_context['kwargs'].get('embedded')
+        embedded = self.request.parser_context['kwargs'].get('no_embeds')
         if 'embed' in self.request.query_params:
             if not embedded:
                 url = replace_query_param(url, 'embed', self.request.query_params['embed'])
 
         url = replace_query_param(url, self.page_query_param, page_number)
 
+        if page_number == 1:
+            return remove_query_param(url, self.page_query_param)
+
         return url
 
     def get_first_real_link(self, url):
         if not self.page.has_previous():
             return None
-        url = self.query_param_generator(self.request.build_absolute_uri(url))
-        return remove_query_param(url, self.page_query_param)
+        return self.query_param_generator(self.request.build_absolute_uri(url), 1)
 
     def get_last_real_link(self, url):
         if not self.page.has_next():
             return None
         url = self.request.build_absolute_uri(url)
         page_number = self.page.paginator.num_pages
-        return replace_query_param(url, self.page_query_param, page_number)
+        return self.query_param_generator(url, page_number)
 
     def get_previous_real_link(self, url):
         if not self.page.has_previous():
             return None
         url = self.request.build_absolute_uri(url)
         page_number = self.page.previous_page_number()
-        if page_number == 1:
-            return remove_query_param(url, self.page_query_param)
-        return replace_query_param(url, self.page_query_param, page_number)
+        return self.query_param_generator(url, page_number)
 
     def get_next_real_link(self, url):
         if not self.page.has_next():
             return None
         url = self.request.build_absolute_uri(url)
         page_number = self.page.next_page_number()
-        return replace_query_param(url, self.page_query_param, page_number)
+        return self.query_param_generator(url, page_number)
 
     def get_paginated_response(self, data):
         kwargs = self.request.parser_context['kwargs'].copy()
         kwargs.pop('no_embeds', None)
         view_name = self.request.parser_context['view'].view_name
+        reversed_url = reverse(view_name, kwargs=kwargs)
 
         response_dict = OrderedDict([
-             ('data', data),
+            ('data', data),
             ('links', OrderedDict([
-                ('first', self.get_first_real_link(reverse(view_name, kwargs=kwargs))),
-                ('last', self.get_last_real_link(reverse(view_name, kwargs=kwargs))),
-                ('prev', self.get_previous_real_link(reverse(view_name, kwargs=kwargs))),
-                ('next', self.get_next_real_link(reverse(view_name, kwargs=kwargs))),
+                ('first', self.get_first_real_link(reversed_url)),
+                ('last', self.get_last_real_link(reversed_url)),
+                ('prev', self.get_previous_real_link(reversed_url)),
+                ('next', self.get_next_real_link(reversed_url)),
                 ('meta', OrderedDict([
                     ('total', self.page.paginator.count),
                     ('per_page', self.page.paginator.per_page),
