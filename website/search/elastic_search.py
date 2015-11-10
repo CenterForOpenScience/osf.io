6a84ea008440a4d41006f52b96b7524258553af5
Steven Loria <sloria1@gmail.com>
2015-11-10 11:59:13 -0500

    Fix incorrect query for osf storage files
    h/t @chrisseto

--- website/search/elastic_search.py
+++ website/search/elastic_search.py
@@ -291,8 +291,8 @@ def update_node(node, index=None, bulk=False):
             # Skip orphaned components
             return
 
-    from website.files.models.osfstorage import OsfStorageFileNode
-    for file_ in OsfStorageFileNode.find(Q('node', 'eq', node)):
+    from website.files.models.osfstorage import OsfStorageFile
+    for file_ in OsfStorageFile.find(Q('node', 'eq', node)):
         update_file(file_)
 
     if node.is_deleted or not node.is_public or node.archiving:
