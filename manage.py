fb708f78dcdb166173aa2af05f5f4120a4ee6878
Zak <zik3qe@virginia.edu>
2015-11-10 10:50:30 -0500

    remove line from manage.py that sets django_settings_module to prevent breaking api/v2

--- manage.py
+++ manage.py
@@ -2,9 +2,7 @@
 import sys
 import os
 
-
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "admin.base.settings")
 
     from django.core.management import execute_from_command_line
     from website.app import init_app
