765433723f51652d8af26018749d9a8a91941626
Chris Wisecarver <cwisecarver@cos.io>
2015-11-10 16:53:25 -0500

    Order in python if queryset doesn't support ordering
    [#OSF-5161]

--- api/base/filters.py
+++ api/base/filters.py
@@ -27,6 +27,11 @@ class ODMOrderingFilter(OrderingFilter):
     def filter_queryset(self, request, queryset, view):
         ordering = self.get_ordering(request, queryset, view)
         if ordering:
+            if isinstance(queryset, list) and isinstance(ordering, tuple):
+                # for lists call sorted, list.sort doesn't take a key
+                order_key = ordering[0]
+                sorted_list = sorted(list(queryset), key=operator.attrgetter(order_key))
+                return sorted_list
             return queryset.sort(*ordering)
         return queryset
 
