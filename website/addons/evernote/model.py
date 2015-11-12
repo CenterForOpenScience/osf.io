65ea9223a25a1b324a14c027d0c1510327deff71
Raymond Yee <raymond.yee@gmail.com>
2015-11-12 15:33:30 -0800

    First cut at Evernote OAuth1 -- I'm able to issue an approval at Evernote.  No persistence of the authentication token yet. I had to fix what seems to be a bug in the OAuth1 implementation:  I had to pass in the callback_uri to OAuth1Session.

--- website/addons/evernote/model.py
+++ website/addons/evernote/model.py
@@ -1,13 +1,51 @@
 
 from website.addons.base import AddonUserSettingsBase, AddonNodeSettingsBase
 from website.addons.base import StorageAddonBase
-from website.oauth.models import ExternalProvider
+from website.addons.evernote import settings
+from website.oauth.models import (ExternalProvider, OAUTH1)
+
 
 class Evernote(ExternalProvider):
-    pass
+    """
+    First cut at the Evernote provider
+
+    """
+    name = 'Evernote'
+    short_name = 'evernote'
+
+    # Evernote has a sandbox and a production service
+    # with different endpoints
+
+    client_id = settings.EVERNOTE_CLIENT_ID
+    client_secret = settings.EVERNOTE_CLIENT_SECRET
+    _sandbox = settings.EVERNOTE_SANDBOX
+
+    BASE_URL = "https://www.evernote.com" if not _sandbox  \
+           else "https://sandbox.evernote.com"
+
+    auth_url_base = '{}/OAuth.action'.format(BASE_URL)
+    request_token_url = '{}/oauth'.format(BASE_URL)
+    callback_url = '{}/oauth'.format(BASE_URL)
+
+    _oauth_version = OAUTH1
+
+
+    def handle_callback(self, response):
+        """
+        Placeholder:
+        I don't think this callback does anything meaningful yet
+        in the absence of a way to store the auth token.
+        """
+
+        return {
+            'provider_id': "[provider_id]",
+            'display_name': "[display_name]",
+            'profile_url': "https://example.com/profile_url",
+        }
 
 class EvernoteUserSettings(AddonUserSettingsBase):
-    pass
+    oauth_provider = Evernote
+
 
 class EvernoteNodeSettings(StorageAddonBase, AddonNodeSettingsBase):
     pass
