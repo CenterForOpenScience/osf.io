4d9efd892c3d60385de944a072ede389b120c635
Raymond Yee <raymond.yee@gmail.com>
2015-11-12 08:17:33 -0800

    minor changes: added evernote requirement and skeleton for Evernote(ExternalProvider)

--- website/addons/evernote/model.py
+++ website/addons/evernote/model.py
@@ -1,6 +1,10 @@
 
 from website.addons.base import AddonUserSettingsBase, AddonNodeSettingsBase
 from website.addons.base import StorageAddonBase
+from website.oauth.models import ExternalProvider
+
+class Evernote(ExternalProvider):
+    pass
 
 class EvernoteUserSettings(AddonUserSettingsBase):
     pass
