d9f58ce77b50d3a8ad9abf38e4956b27edfc36d5
Zak <zik3qe@virginia.edu>
2015-11-13 15:11:01 -0500

    merge admin/user_login and fix merge conflicts

--- admin/base/views.py
+++ admin/base/views.py
@@ -1,3 +1,5 @@
+from django.contrib import messages
+from django.contrib.auth import views
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
@@ -7,3 +9,21 @@ from django.shortcuts import render
 @login_required
 def home(request):
     return render(request, 'home.html', {'user': request.user})
+
+def password_reset_done(request, **kwargs):
+    messages.success(request, 'You have successfully reset your password and activated your admin account. Thank you')
+    return redirect('/admin/auth/login/')
+
+def password_reset_confirm_custom(request, **kwargs):
+    response = views.password_reset_confirm(request, **kwargs)
+    # i.e. if the user successfully resets their password
+    if response.status_code == 302:
+        try:
+            uid = urlsafe_base64_decode(kwargs['uidb64'])
+            user = User.objects.get(pk=uid)
+        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+            pass
+        else:
+            user.is_active = True
+            user.save()
+    return response
