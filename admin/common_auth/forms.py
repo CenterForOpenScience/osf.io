b69f1d6b47bbabc7dd1a754529f81dfac7d33d97
Zak <zik3qe@virginia.edu>
2015-11-13 12:41:17 -0500

    refactor login view

--- admin/common_auth/forms.py
+++ admin/common_auth/forms.py
@@ -2,16 +2,5 @@ from django import forms
 from django.contrib.auth import authenticate
 
 class LoginForm(forms.Form):
-    username = forms.CharField(label=(u'User Name'))
-    password = forms.CharField(label=(u'Password'), widget=forms.PasswordInput(render_value=False))
-
-    def clean(self):
-        username = self.cleaned_data.get('username')
-        password = self.cleaned_data.get('password')
-        if not username or not password:
-            raise forms.ValidationError("Please enter the missing information.")
-            return self.cleaned_data
-        user = authenticate(username=username, password=password)
-        if not user:
-            raise forms.ValidationError("Invalid username/password combination. Please try again.")
-        return self.cleaned_data
+    username = forms.CharField(label=(u'User Name'), required=True)
+    password = forms.CharField(label=(u'Password'), widget=forms.PasswordInput(render_value=False), required=True)
--- admin/common_auth/views.py
+++ admin/common_auth/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth import authenticate, logout as logout_user, login as auth_login
 from django.shortcuts import render, redirect
 
@@ -8,13 +9,14 @@ def login(request):
         return redirect('/admin/')
     form = LoginForm(request.POST or None)
     if request.POST and form.is_valid():
-        username = form.cleaned_data.get('username')
-        password = form.cleaned_data.get('password')
-        admin_user = authenticate(username=username, password=password)
-        if admin_user:
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
             auth_login(request, admin_user)
             return redirect('/admin/')
         else:
+            messages.error(request, 'Username and/or Password incorrect. Please try again.')
             return redirect('/admin/auth/login/')
     context = {'form': form}
     return render(request, 'login.html', context)
