3fec35358ec56de41051e45d38dd595b0e5c0b87
Dawn Pattison <pattison.dawn.r@gmail.com>
2015-11-16 15:34:16 -0500

    Inline construction of view_fqn().

--- api/base/views.py
+++ api/base/views.py
@@ -13,14 +13,8 @@ class JSONAPIBaseView(generics.GenericAPIView):
     def __init__(self, **kwargs):
         assert getattr(self, 'view_name', None), 'Must specify view_name on view.'
         assert getattr(self, 'view_category', None), 'Must specify view_category on view.'
-        self._view_fqn = self.view_fqn()
+        self.view_fqn = ':'.join([self.view_category, self.view_name])
         super(JSONAPIBaseView, self).__init__(**kwargs)
-
-    # lazily build view_fqn
-    def view_fqn(self):
-        if not getattr(self, '_view_fqn', None):
-            self._view_fqn = ':'.join([self.view_category, self.view_name])
-        return self._view_fqn
 
     def _get_embed_partial(self, field_name, field):
         """Create a partial function to fetch the values of an embedded field. A basic
