1a2879f95b726568c96bd47be7a42b7c64daf253
Raymond Yee <raymond.yee@gmail.com>
2015-11-16 10:55:48 -0800

    start to adding api for evernote accounts

---
+++ website/addons/evernote/routes.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from framework.routing import Rule, json_renderer
+
+from website.addons.evernote import views
+
+api_routes = {
+    'rules': [
+        Rule(
+            [
+                '/settings/evernote/accounts/',
+            ],
+            'get',
+            views.evernote_get_user_accounts,
+            json_renderer,
+        ),
+    ],
+    'prefix': '/api/v1',
+}
