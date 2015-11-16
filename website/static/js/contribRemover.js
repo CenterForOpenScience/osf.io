fb59658d5e5f630d8112e0ff3b17022eda41e842
Billy Hunt <Billy@billyhunt.com>
2015-11-16 14:46:48 -0500

    add v1 call info

--- website/static/js/contribRemover.js
+++ website/static/js/contribRemover.js
@@ -14,42 +14,57 @@ var Paginator = require('./paginator');
 
 var API_BASE = 'http://localhost:8000/v2/nodes/';
 
-var contributorsToDelete = [];
+function getNodesOriginal(nodeTree, nodesOriginal) {
+    /**
+     * take treebeard tree structure of nodes and get a dictionary of parent node and all its
+     * children
+     */
+    var i;
+    var nodeId = nodeTree.node.id;
+    nodesOriginal[nodeId] = {
+        public: nodeTree.node.is_public,
+        id: nodeTree.node.id,
+        addons: nodeTree.node.addons,
+        title: nodeTree.node.title,
+        changed: false
+    };
 
-
-var getChildren = function(nodeId) {
-
+    if (nodeTree.children) {
+        for (i in nodeTree.children) {
+            nodesOriginal = getNodesOriginal(nodeTree.children[i], nodesOriginal);
+        }
+    }
+    return nodesOriginal;
 }
 
-
-var getNodeChildrenList = function(nodeId, contributorId) {
-    var self = this;
-    var contribUrl = API_BASE + nodeId + '/contributors/';
-    debugger;
-        $.ajax({
-            url: contribUrl,
-            type: 'GET',
-            crossOrigin: true,
-            xhrFields: {
-                withCredentials: true
-            },
-        }).done(function (response) {
-            for (var i = 0; i < response.data.length; i++) {
-                var node = response.data.length
-                debugger;
-
-            }
-
-            //window.location.reload();
-        }).fail(function (xhr, status, error) {
-            $osf.growl('Error', 'Unable to delete Contributor');
-            Raven.captureMessage('Could not DELETE Contributor.' + error, {
-                API_BASE: url, status: status, error: error
-            });
-        });
-}
-
-
+//var getNodeChildrenList = function(nodeId, contributorId) {
+//    var self = this;
+//    var contribUrl = API_BASE + nodeId + '/contributors/';
+//    debugger;
+//        $.ajax({
+//            url: contribUrl,
+//            type: 'GET',
+//            crossOrigin: true,
+//            xhrFields: {
+//                withCredentials: true
+//            },
+//        }).done(function (response) {
+//            for (var i = 0; i < response.data.length; i++) {
+//                var node = response.data.length
+//                debugger;
+//
+//            }
+//
+//            //window.location.reload();
+//        }).fail(function (xhr, status, error) {
+//            $osf.growl('Error', 'Unable to delete Contributor');
+//            Raven.captureMessage('Could not DELETE Contributor.' + error, {
+//                API_BASE: url, status: status, error: error
+//            });
+//        });
+//}
+//
+//
 
 
 var RemoveContributorViewModel = oop.extend(Paginator, {
@@ -61,11 +76,10 @@ var RemoveContributorViewModel = oop.extend(Paginator, {
         self.nodeApiUrl = '/api/v1/project/' + self.nodeId + '/';
         self.parentId = parentId;
         self.parentTitle = parentTitle;
-
         self.contributorToRemove = ko.observable();
         shouter.subscribe(function(newValue) {
             self.contributorToRemove(newValue);
-        }, self, "messageToPublish");
+        }, self, 'messageToPublish');
 
         self.page = ko.observable('remove');
         self.pageTitle = ko.computed(function() {
@@ -77,41 +91,72 @@ var RemoveContributorViewModel = oop.extend(Paginator, {
         self.userName = ko.observable(userName);
         self.deleteAll = ko.observable(false);
         self.nodeHasChildren = ko.observable(nodeHasChildren);
-    },
-    selectRemove: function() {
-        var self = this;
-        self.page('remove');
-    },
-    clear: function() {
-        var self = this;
-        self.selectRemove();
-        self.deleteAll(false);
-    },
-    deleteOneNode: function() {
-        var self = this;
-        var url = API_BASE + self.nodeId + '/contributors/' + self.contributorToRemove().id + '/';
+        var nodesOriginal = {};
+        self.nodesOriginal = ko.observableArray();
+
+        /**
+         * original node state on page load
+         */
+        self.nodesOriginal.subscribe(function(newValue) {
+        });
+
+        /**
+         * get node free for treebeard from API V1
+         */
         $.ajax({
-            url: url,
-            type: 'DELETE',
-            crossOrigin: true,
-            xhrFields: {
-                withCredentials: true
-            },
-        }).done(function (response) {
-            window.location.reload();
-        }).fail(function (xhr, status, error) {
-            $osf.growl('Error', 'Unable to delete Contributor');
-            Raven.captureMessage('Could not DELETE Contributor.' + error, {
-                API_BASE: url, status: status, error: error
+            url: self.nodeApiUrl,
+            type: 'GET',
+            dataType: 'json'
+        }).done(function(response) {
+            nodesOriginal = getNodesOriginal(response[0], nodesOriginal);
+            self.nodesOriginal(nodesOriginal);
+            debugger;
+            ////change node state to reflect button push by user on project page (make public | make private)
+            //nodesState[nodeParent].public = !parentIsPublic;
+            //nodesState[nodeParent].changed = true;
+            //self.nodesState(nodesState);
+            //new NodesPrivacyTreebeard(response, self.nodesState, nodesOriginal);
+        }).fail(function(xhr, status, error) {
+            $osf.growl('Error', 'Unable to retrieve projects and components');
+            Raven.captureMessage('Unable to retrieve projects and components', {
+                url: self.nodeApiUrl, status: status, error: error
             });
         });
-        self.page('remove');
-    },
-    deleteAllNodes: function() {
-        var self = this;
-        self.page('removeAll');
-        getNodeContributorList(self.nodeId, self.contributorToRemove().id);
-    }
+        },
+        selectRemove: function() {
+            var self = this;
+            self.page('remove');
+        },
+        clear: function() {
+            var self = this;
+            self.selectRemove();
+            self.deleteAll(false);
+        },
+        deleteOneNode: function() {
+            var self = this;
+            var url = API_BASE + self.nodeId + '/contributors/' + self.contributorToRemove().id + '/';
+            $.ajax({
+                url: url,
+                type: 'DELETE',
+                crossOrigin: true,
+                xhrFields: {
+                    withCredentials: true
+                },
+            }).done(function (response) {
+                window.location.reload();
+            }).fail(function (xhr, status, error) {
+                $osf.growl('Error', 'Unable to delete Contributor');
+                Raven.captureMessage('Could not DELETE Contributor.' + error, {
+                    API_BASE: url, status: status, error: error
+                });
+            });
+            self.page('remove');
+        },
+        deleteAllNodes: function() {
+            var self = this;
+            self.page('removeAll');
+            //getNodeContributorList(self.nodeId, self.contributorToRemove().id);
+        }
 });
 
 
