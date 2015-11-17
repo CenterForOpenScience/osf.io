7be5de7a55c86cbb1c813d92ac19eea0a51bf94e
Dawn Pattison <pattison.dawn.r@gmail.com>
2015-11-17 17:02:23 -0500

    Continued reconciliation of RelationshipsField with Sam's ensure links are valid URI's

--- api/base/serializers.py
+++ api/base/serializers.py
@@ -239,7 +239,7 @@ class RelationshipField(ser.HyperlinkedIdentityField):
             if key == 'count':
                 show_related_counts = self.context['request'].query_params.get('related_counts', False)
                 if utils.is_truthy(show_related_counts):
-                    meta[key] = website_utils.rapply(self.meta[key], _url_val, obj=value, serializer=self.parent)
+                    meta[key] = website_utils.rapply(meta_data[key], _url_val, obj=value, serializer=self.parent)
                 elif utils.is_falsy(show_related_counts):
                     continue
                 if not utils.is_truthy(show_related_counts):
@@ -248,7 +248,7 @@ class RelationshipField(ser.HyperlinkedIdentityField):
                         parameter='related_counts'
                     )
             else:
-                meta[key] = website_utils.rapply(self.meta[key], _url_val, obj=value, serializer=self.parent)
+                meta[key] = website_utils.rapply(meta_data[key], _url_val, obj=value, serializer=self.parent)
         return meta
 
     def lookup_attribute(self, obj, lookup_field):
