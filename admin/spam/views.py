c71ec2c26ef9c873048ea35e636569a877be165a
Bryan Gorges <bgorges@cos.io>
2015-11-18 13:34:08 -0500

    Add pagination to spam instances

--- admin/spam/views.py
+++ admin/spam/views.py
@@ -1,13 +1,21 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .serializers import serialize_comments, retrieve_comment
 
 
 def spam_list(request):
-    comments = serialize_comments()
-    context = {'comments': comments}
-    return render(request, 'spam/spam.html', context)
+    comment_list = serialize_comments()
+    paginator = Paginator(comment_list, 10)
+
+    page = request.GET.get('page', 1)
+    try:
+        comments = paginator.page(page)
+    except PageNotAnInteger:
+        comments = paginator.page(1)
+    except EmptyPage:
+        comments = paginator.page(paginator.num_pages)
+    return render_to_response('spam/spam.html', {'comments': comments})
 
 
 def spam_detail(request, spam_id):
