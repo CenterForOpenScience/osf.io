ccf0f5286592fb86015ec8b56621f0fbf71de847
Lauren Barker <leb2dg@virginia.edu>
2015-11-20 16:49:15 -0500

    move require to base js page & clean up admin config

--- admin/webpack.admin.config.js
+++ admin/webpack.admin.config.js
@@ -4,20 +4,16 @@ var common = require('../webpack.common.config.js');
 var assign = require('object-assign');
 var BundleTracker = require('webpack-bundle-tracker');
 
-var websiteRoot = path.join(__dirname, 'websiteRoot', 'static');
+var websiteRoot = path.join(__dirname, '..', 'website', 'static');
 
 var adminRoot = path.join(__dirname, 'static');
-
-var staticWebsitePath = function(dir) {
-    return path.join(websiteRoot, dir);
-};
 
 var staticAdminPath = function(dir) {
     return path.join(adminRoot, dir);
 };
 
 // Adding bundle tracker to plugins
-common.plugins.concat([
+var plugins = common.plugins.concat([
     // for using webpack with Django
     new BundleTracker({filename: './webpack-stats.json'}),
 ]);
@@ -32,6 +28,9 @@ common.output = {
 module.exports = assign({}, common, {
     entry: {
         'admin-base-page': staticAdminPath('js/pages/base-page.js')
+    },
+    resolve: {
+        root: [websiteRoot, adminRoot]
     },
     debug: true,
     devtool: 'source-map'
