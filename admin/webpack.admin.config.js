cc5b59b42dfd42b909857665443f8992b65e1aba
Lauren Barker <leb2dg@virginia.edu>
2015-11-20 16:19:30 -0500

    remove in-place modification of data structures

--- admin/webpack.admin.config.js
+++ admin/webpack.admin.config.js
@@ -16,28 +16,11 @@ var staticAdminPath = function(dir) {
     return path.join(adminRoot, dir);
 };
 
-common.entry['admin-base-page'] = staticAdminPath('js/pages/base-page.js')
-
 // Adding bundle tracker to plugins
-common.plugins = [
-    // Bundle common code between modules
-    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js'),
-    // Bower support
-    new webpack.ResolverPlugin(
-        new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin('bower.json', ['main'])
-    ),
-    // Make jQuery available in all modules without having to do require('jquery')
-    new webpack.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery'
-    }),
-    // Slight hack to make sure that CommonJS is always used
-    new webpack.DefinePlugin({
-        'define.amd': false
-    }),
+common.plugins.concat([
     // for using webpack with Django
     new BundleTracker({filename: './webpack-stats.json'}),
-];
+]);
 
 common.output = {
     path: './static/public/js/',
@@ -46,7 +29,10 @@ common.output = {
     sourcePrefix: ''
 };
 
-module.exports = assign(common, {
+module.exports = assign({}, common, {
+    entry: {
+        'admin-base-page': staticAdminPath('js/pages/base-page.js')
+    },
     debug: true,
     devtool: 'source-map'
 });
