b9d9b6343f342d47b04358c92f2eefbf7bb81fa6
Ryan Casey <rcasey@growingtechnologies.com>
2015-11-21 15:08:12 -0500

    added onedriveClient to handle fetching tokens, data from one drive; todo:refresh access token and get folder list

---
+++ website/addons/onedrive/client.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+
+from requests_oauthlib import OAuth2Session
+from oauthlib.oauth2 import InvalidGrantError
+
+from framework.exceptions import HTTPError
+
+from website.util.client import BaseClient
+#from website.addons.googledrive import settings
+from website.addons.googledrive import exceptions
+from website.addons.onedrive import settings
+
+class OneDriveAuthClient(BaseClient):
+
+#     def refresh(self, access_token, refresh_token):
+#         client = OAuth2Session(
+#             settings.CLIENT_ID,
+#             token={
+#                 'access_token': access_token,
+#                 'refresh_token': refresh_token,
+#                 'token_type': 'Bearer',
+#                 'expires_in': '-30',
+#             }
+#         )
+# 
+#         extra = {
+#             'client_id': settings.CLIENT_ID,
+#             'client_secret': settings.CLIENT_SECRET,
+#         }
+# 
+#         try:
+#             return client.refresh_token(
+#                 self._build_url(settings.API_BASE_URL, 'oauth2', 'v3', 'token'),
+#                 # ('love')
+#                 **extra
+#             )
+#         except InvalidGrantError:
+#             raise exceptions.ExpiredAuthError()
+# 
+    def userinfo(self, access_token):
+        return self._make_request(
+            'GET',
+            self._build_url(settings.MSLIVE_API_URL, 'oauth2', 'v3', 'userinfo'),
+            params={'access_token': access_token},
+            expects=(200, ),
+            throws=HTTPError(401)
+        ).json()
+
+
+class OneDriveClient(BaseClient):
+
+    def __init__(self, access_token=None):
+        self.access_token = access_token
+
+    @property
+    def _default_headers(self):
+        if self.access_token:
+            return {'Authorization': 'bearer {}'.format(self.access_token)}
+        return {}
+
+    def about(self):
+        return self._make_request(
+            'GET',
+            self._build_url(settings.ONEDRIVE_API_URL, 'drive', 'v2', 'about', ),
+            expects=(200, ),
+            throws=HTTPError(401)
+        ).json()
+
+    def folders(self, folder_id='root'):
+#         query = ' and '.join([
+#             "'{0}' in parents".format(folder_id),
+#             'trashed = false',
+#             "mimeType = 'application/vnd.google-apps.folder'",
+#         ])
+        res = self._make_request(
+            'GET',
+            self._build_url(settings.ONEDRIVE_API_URL, 'drive'),
+            params={}, #'q': query
+            expects=(200,401),
+            throws=HTTPError(401)
+        )
+        return res.json()#['items']
