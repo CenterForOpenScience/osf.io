5ff836a6a1040e2f4a09f69ab3c574af61e8d5a3
Caner Uguz <caneruguz@gmail.com>
2015-11-24 11:31:10 -0500

    Remove unnecessary request --  see init()

--- website/static/js/fileBrowser.js
+++ website/static/js/fileBrowser.js
@@ -88,14 +88,6 @@ var FileBrowser = {
             new LinkObject('collection', { path : 'registrations/', query : { 'related_counts' : true }, systemCollection : true}, 'All My Registrations'),
         ];
 
-        var collectionsUrl = $osf.apiV2Url('collections/', { query : {'related_counts' : true, 'sort' : 'date_created'}});
-        m.request({method : 'GET', url : collectionsUrl, config : xhrconfig}).then(function(result){
-            console.log(result);
-           result.data.forEach(function(node){
-               self.collections.push(new LinkObject('collection', { path : 'collections/' + node.id + '/linked_nodes/', query : { 'related_counts' : true }, systemCollection : false, node : node }, node.attributes.title));
-           });
-        });
-
         self.breadcrumbs = m.prop([
             new LinkObject('collection', { path : 'users/me/nodes/', query : { 'related_counts' : true }, systemCollection : true}, 'All My Projects'),
         ]);
