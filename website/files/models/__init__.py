722fa5b8af32b6eb4f8470e86c484f6a58c82e00
Kevin Wierman <kwierman@gmail.com>
2015-12-01 14:31:05 -0500

    [dryad]: Basic API settings added

---
+++ website/files/models/__init__.py
@@ -0,0 +1,11 @@
+from website.files.models.base import *  # noqa
+
+from website.files.models.s3 import *  # noqa
+from website.files.models.box import *  # noqa
+from website.files.models.dryad import *  # noqa
+from website.files.models.github import *  # noqa
+from website.files.models.dropbox import *  # noqa
+from website.files.models.figshare import *  # noqa
+from website.files.models.dataverse import *  # noqa
+from website.files.models.osfstorage import *  # noqa
+from website.files.models.googledrive import *  # noqa
