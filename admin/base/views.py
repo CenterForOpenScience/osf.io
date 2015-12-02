0722ff001be73b6cd35c534a546ca439c276d64d
Zak <zik3qe@virginia.edu>
2015-12-02 12:52:02 -0500

    -remove bits of unneeded code

--- admin/base/views.py
+++ admin/base/views.py
@@ -5,9 +5,6 @@ from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.utils.http import urlsafe_base64_decode
 
-#def root(request):
-#   return HttpResponse("Will probably need to put some front end Auth on this.")
-
 @login_required
 def home(request):
     context = {'user': request.user}
@@ -15,7 +12,7 @@ def home(request):
 
 def password_reset_done(request, **kwargs):
     messages.success(request, 'You have successfully reset your password and activated your admin account. Thank you')
-    return redirect('/admin/auth/login/')
+    return redirect('auth:login')
 
 def password_reset_confirm_custom(request, **kwargs):
     response = views.password_reset_confirm(request, **kwargs)
