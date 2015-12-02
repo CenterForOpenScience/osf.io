0d88e88a10c380bb5b7fe3ce43c5502d869fbed3
Henrique Moco <hmoco94@vt.edu>
2015-12-02 11:58:36 -0500

    InstitutionList work

--- api/institutions/serializers.py
+++ api/institutions/serializers.py
@@ -1,5 +1,12 @@
-class InstitutionSerializer():
-    pass
+from rest_framework import serializers as ser
+from api.base.serializers import JSONAPISerializer
 
-class InstitutionDetailSerializer():
+class InstitutionSerializer(JSONAPISerializer):
+    name = ser.CharField(required=True)
+    id = ser.CharField(required=True, source='_id')
+
+    class Meta:
+        type_ = 'institutions'
+
+class InstitutionDetailSerializer(InstitutionSerializer):
     pass
