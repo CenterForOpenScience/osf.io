73083ea3068b426d28c19248709eda7bdb37094d
Henrique Moco <hmoco94@vt.edu>
2015-12-02 12:35:53 -0500

    Removed node relationships

--- api/nodes/serializers.py
+++ api/nodes/serializers.py
@@ -117,10 +117,10 @@ class NodeSerializer(JSONAPISerializer):
         related_meta={'count': 'get_registration_count'}
     ))
 
-    primary_institution = RelationshipField(
+    '''primary_institution = RelationshipField(
         related_view='nodes:node-institution-detail',
         related_view_kwargs={'node_id': '<pk>'}
-    )
+    )'''
 
     class Meta:
         type_ = 'nodes'
