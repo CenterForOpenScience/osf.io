8500e1ab6acb52df4ca2c66ef322817b46224903
Sam Chrisinger <s.chrisinger@gmail.com>
2015-12-03 14:29:29 -0500

    Use OSF version of registrationUtils

--- admin/static/js/pages/admin-registration-edit-page.js
+++ admin/static/js/pages/admin-registration-edit-page.js
@@ -1,4 +1,4 @@
-var registrationUtils = require('js/adminRegistrationUtils');
+var registrationUtils = require('js/registrationUtils');
 var RegistrationEditor = registrationUtils.RegistrationEditor;
 var $ = require('jquery');
 var $osf = require('js/osfHelpers');
@@ -8,13 +8,13 @@ $(document).ready(function() {
 	var params = context[0];
 
 	var draftEditor = new RegistrationEditor({
-	    schemas: '/get-schemas/',
-	    update: '/update-draft/{draft_pk}/',
-	    approve: '/approve-draft/{draft_pk}/',
-	    reject: '/reject-draft/{draft_pk}/',
-	    request_revisions: '/reject-draft/{draft_pk}/',
-	    home: '/prereg/'
-	}, 'registrationEditor');
+	    schemas: '/admin/pre-reg/get_schemas/',
+	    update: '/admin/pre-reg/update_draft/{draft_pk}/',
+	    approve: '/admin/pre-reg/approve_draft/{draft_pk}/',
+	    reject: '/admin/pre-reg/reject_draft/{draft_pk}/',
+	    request_revisions: '/admin/pre-reg/reject_draft/{draft_pk}/',
+	    home: '/admin/pre-reg/prereg/'
+	}, 'registrationEditor', true);
 
 	var draft = new registrationUtils.Draft(params);
 	draftEditor.init(draft);
