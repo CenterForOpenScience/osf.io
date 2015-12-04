2cd3b66dc09c36f3b194264b8c6ff29b0eb71359
Caner Uguz <caneruguz@gmail.com>
2015-12-04 16:41:52 -0500

    Move template builder to controller to run once

--- website/static/js/logTextParser.js
+++ website/static/js/logTextParser.js
@@ -8,38 +8,40 @@ var logActions = require('js/logActionsList');
 
 var LogText = {
     controller : function (logObject){
+        var self = this;
+        self.buildTemplate = function () {   // Build template in controller to run only once
+            var text = logActions[logObject.attributes.action];
+            var list = text.split(/(\${.*?})/);
+            return m('.osf-log-item',[
+                list.map(function(piece){
+                    if(piece === '') { return; }
+                    if(piece === '${user}' && logObject.embeds.user) {
+                        return m.component(UserLink, logObject.embeds.user);
+                    }
+                    if(piece === '${node}' && logObject.embeds.nodes){
+                        return m.component(NodeLink, logObject.embeds.nodes);
+                    }
+
+                    return piece;
+                })
+            ]);
+        };
+        self.finalTemplate = self.buildTemplate();
 
     },
     view : function(ctrl, logObject) {
-        var text = logActions[logObject.attributes.action];
-        var list = text.split(/(\${.*?})/);
-        return m('.osf-log-item',[
-            list.map(function(piece){
-                if(piece === '') { return; }
-                if(piece === '${user}') {
-                    return m.component(UserLink, logObject.embeds.user);
-                }
-                if(piece === '${node}'){
-                    return m.component(NodeLink, logObject.embeds.nodes);
-                }
-
-                return piece;
-            })
-        ]);
-
+        return ctrl.finalTemplate;
     }
 };
 
 var UserLink = {
     view : function(ctrl, userObject){
-        console.log(userObject);
         return m('a', { href : userObject.data.links.html}, userObject.data.attributes.full_name);
     }
 };
 
 var NodeLink = {
     view : function (ctrl, nodeObject) {
-        console.log(nodeObject);
         return m('a', { href : nodeObject.data[0].links.html}, nodeObject.data[0].attributes.title);
     }
 };
