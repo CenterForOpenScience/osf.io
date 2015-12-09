46976380f7fe0c1bb905f4e09b74cd03bdb64fb2
Caner Uguz <caneruguz@gmail.com>
2015-12-09 15:21:10 -0500

    Add contributor and tag pieces to log parser

--- website/static/js/logTextParser.js
+++ website/static/js/logTextParser.js
@@ -12,7 +12,7 @@ var LogText = {
         self.buildTemplate = function () {   // Build template in controller to run only once
             var text = logActions[logObject.attributes.action];
             var list = text.split(/(\${.*?})/);
-            return m('.osf-log-item',[
+            return m('span.osf-log-item',[
                 list.map(function(piece){
                     if(piece === '') { return; }
                     if(piece === '${user}' && logObject.embeds.user) {
@@ -21,7 +21,12 @@ var LogText = {
                     if(piece === '${node}' && logObject.embeds.nodes){
                         return m.component(NodeLink, logObject.embeds.nodes);
                     }
-
+                    if(piece === '${contributors}'){
+                        return m.component(Contributors, logObject);
+                    }
+                     if(piece === '${tag}'){
+                        return m.component(Tag, logObject);
+                    }
                     return piece;
                 })
             ]);
@@ -45,5 +50,19 @@ var NodeLink = {
         return m('a', { href : nodeObject.data[0].links.html}, nodeObject.data[0].attributes.title);
     }
 };
+
+var Contributors = {
+    view : function (ctrl, logObject) {
+        return m('span');
+        //return m('a', { href : logObject.attributes.links.html}, nodeObject.data[0].attributes.title);
+    }
+};
+
+var Tag = {
+    view : function (ctrl, logObject) {
+        return m('a', { href : '/search/?q=%22' + logObject.attributes.params.tag + '%22'}, logObject.attributes.params.tag);
+    }
+};
+
 
 module.exports = LogText;
