f535710095b6258ea403f5699a65ae766d691c02
Bryan Gorges <bgorges@cos.io>
2015-12-10 16:31:58 -0500

    Separate buttons, add login_required

--- admin/spam/views.py
+++ admin/spam/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, render_to_response
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.contrib.auth.decorators import login_required
 
 from .serializers import serialize_comments, retrieve_comment
 
 
+@login_required
 def spam_list(request):
     comment_list = serialize_comments()
     paginator = Paginator(comment_list, 10)
@@ -18,12 +20,14 @@ def spam_list(request):
     return render_to_response('spam/spam.html', {'comments': comments})
 
 
+@login_required
 def spam_detail(request, spam_id):
     comment = retrieve_comment(spam_id)
     context = {'comment': comment}
     return render(request, 'spam/comment.html', context)
 
 
+@login_required
 def spam_sub_list(request, spam_ids):
     comments = None
     context = {'comments': comments}
@@ -31,6 +35,7 @@ def spam_sub_list(request, spam_ids):
     return render(request, 'spam/sub_list.html', context)
 
 
+@login_required
 def email(request, spam_id):
     comment = retrieve_comment(spam_id, full_user=True)
     context = {'comment': comment}
