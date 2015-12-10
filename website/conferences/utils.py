7489489b629e9779846c47ad9b751df48c6d193e
Steven Loria <sloria1@gmail.com>
2015-12-10 09:33:11 -0500

    Make sure users created through OSF4m are subscribed...
    to general notifications list

--- website/conferences/utils.py
+++ website/conferences/utils.py
@@ -41,8 +41,8 @@ def get_or_create_user(fullname, address, is_spam):
         user.verification_key = security.random_string(20)
         if is_spam:
             user.system_tags.append('is_spam')
-        user.save()
-        signals.user_confirmed.send(user)
+        # user.save()
+        # signals.user_confirmed.send(user)
         return user, True
 
 
