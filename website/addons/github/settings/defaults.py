cf7017db5eacad461b61f14839ea149e8f2aae3c
Matt Frazier <maf7sm@virginia.edu>
2015-12-14 09:51:16 -0500

    GH OAuth urls added

--- website/addons/github/settings/defaults.py
+++ website/addons/github/settings/defaults.py
@@ -12,6 +12,10 @@ SET_PRIVACY = False
 HOOK_DOMAIN = None
 HOOK_CONTENT_TYPE = 'json'
 
+# OAuth related urls
+OAUTH_AUTHORIZE_URL = 'https://github.com/login/oauth/authorize'
+OAUTH_ACCESS_TOKEN_URL = 'https://github.com/login/oauth/access_token'
+
 # Max render size in bytes; no max if None
 MAX_RENDER_SIZE = None
 
