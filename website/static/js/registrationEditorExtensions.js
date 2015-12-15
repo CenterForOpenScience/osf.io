e0e8b949526f19d168f45f14d4436cb939a3132a
Sam Chrisinger <s.chrisinger@gmail.com>
2015-12-15 15:34:24 -0500

    Only clean node and instantiate ContribAdder if the corresponding node exists

--- website/static/js/registrationEditorExtensions.js
+++ website/static/js/registrationEditorExtensions.js
@@ -249,15 +249,17 @@ var AuthorImport = function(data, $root) {
         self.value(self.serializeContributors(data));
     };
 
-    ko.cleanNode($('#addContributors')[0]);
-    var adder = new ContribAdder(
-        '#addContributors',
-        node.title,
-        node.id,
-        null,
-        null,
-        {async: true, callback: callback}
-    );
+    if ($('#addContributors').length > 0) {
+        ko.cleanNode($('#addContributors')[0]);
+        var adder = new ContribAdder(
+            '#addContributors',
+            node.title,
+            node.id,
+            null,
+            null,
+            {async: true, callback: callback}
+        );
+    }
 
     $.extend(self, data);
 };
