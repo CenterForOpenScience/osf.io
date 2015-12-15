12841308778be28f71fffacf5d45d46c275f3b5b
Dawn Pattison <pattison.dawn.r@gmail.com>
2015-12-15 15:32:16 -0500

    Respond to migration suggestions -
    Use TokuTransaction for migration, add a file logger to logger when not dry mode, add logger statement after node successfully migrated, formatting changes.

--- scripts/migration/migrate_date_modified_for_existing_nodes.py
+++ scripts/migration/migrate_date_modified_for_existing_nodes.py
@@ -3,35 +3,45 @@ This will add a date_modified field to all nodes.  Date_modified will be equival
 """
 import sys
 import logging
-
-from website import models
 from website.app import init_app
+from website import models
+from scripts import utils as script_utils
+from framework.transactions.context import TokuTransaction
 
 logger = logging.getLogger(__name__)
 
-def date_updated(self):
-        """
-        The most recent datetime when this node was modified, based on
-        the logs.
-        """
-        try:
-            return self.logs[-1].date
-        except IndexError:
-            return self.date_created
+def date_updated(node):
+    """
+    The most recent datetime when this node was modified, based on
+    the logs.
+    """
+    try:
+        return node.logs[-1].date
+    except IndexError:
+        return node.date_created
 
 def main():
     init_app(routes=False)
-    dry_run = 'dry' in sys.argv
+
     logger.warn('Date_modified field will be added to all nodes.')
-    if dry_run:
-        logger.warn('Dry_run mode')
+
     for node in models.Node.find():
+        node.date_modified = date_updated(node)
+        node.save()
         logger.info('Node {0} "date_modified" added'.format(node._id))
-        if not dry_run:
-            node.date_modified = date_updated(node)
-            node.save()
+
 
 if __name__ == '__main__':
-    main()
+
+    dry_run = 'dry' in sys.argv
+    if not dry_run:
+        script_utils.add_file_logger(logger, __file__)
+
+    with TokuTransaction():
+        main()
+        if dry_run:
+            raise RuntimeError('Dry run, rolling back transaction.')
+
+
 
 
