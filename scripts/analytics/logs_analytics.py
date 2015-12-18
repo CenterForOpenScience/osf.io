1ec30b6032f6b878e01b167ec90a2bd5ea46e92a
Henrique Moco <hmoco94@vt.edu>
2015-12-18 11:45:17 -0500

    clean up magic numbers and syntax

--- scripts/analytics/logs_analytics.py
+++ scripts/analytics/logs_analytics.py
@@ -16,20 +16,23 @@ color_map = {
     'files': 'magenta'
 }
 
-def last_week():
+NUMBER_OF_DAY_DATA_POINTS = 7
+NUMBER_OF_USERS_TO_SAMPLE = 3
+
+def recent_time_frame(days=NUMBER_OF_DAY_DATA_POINTS):
     now = dt.utcnow()
-    return [now - timedelta(days=i) for i in range(0,6)]
+    return [now - timedelta(days=i) for i in range(0, days)]
 
 def build_time_query(end):
     return Q('date', 'gt', end - timedelta(days=1)) & Q('date', 'lt', end)
 
-def order_users_get():
+def order_users_get(sample_size=NUMBER_OF_USERS_TO_SAMPLE):
     us = User.find()
     points = {u.get_activity_points: u for u in us}
     ordered = collections.OrderedDict(sorted(points.items()))
     ordered = ordered.values()
     l = len(ordered)
-    return ordered[0:3], ordered[int(l/3), int(l/3)+3], ordered[int(l/1.5), int(l/1.5) + 3]
+    return ordered[0: sample_size], ordered[int(l/3): int(l/3) + sample_size], ordered[int(l/1.5): int(l/1.5) + sample_size]
 
 def get_agg_for_user(user, date):
     return {
@@ -43,7 +46,7 @@ def get_agg_for_user(user, date):
 
 def main():
     top, mid, bot = order_users_get()
-    days_in_last_week = last_week()
+    days_in_last_week = recent_time_frame()
     top_agg = {str(day): {} for day in days_in_last_week}
     mid_agg = {str(day): {} for day in days_in_last_week}
     bot_agg = {str(day): {} for day in days_in_last_week}
@@ -57,16 +60,16 @@ def main():
                         str(day): {key: val + (agg[str(day)].get(key) or 0) for key, val in data.iteritems()}}
                 )
         agg.update(
-            {key: {k: v/3 for k, v in val.iteritems()} for key, val in agg.iteritems()}
+            {key: {k: v/len(sample) for k, v in val.iteritems()} for key, val in agg.iteritems()}
         )
-    days = [1, 2, 3, 4, 5, 6]
     days_map, junk = top_agg.iteritems()
+    days = range(1, len(days_in_last_week) + 1)
     plt.figure(1)
     plt.subplot(211)
     plt.ylabel('Number of events')
     plt.xlabel('Day of the week')
     plt.title('Top users (100%)')
-    plt.plot(days, [top_agg[day].get('commments') for day in days_map], color_map['comments'],
+    plt.plot(days, [top_agg[day].get('comments') for day in days_map], color_map['comments'],
              days, [top_agg[day].get('wiki') for day in days_map], color_map['wiki'],
              days, [top_agg[day].get('registrations') for day in days_map], color_map['registrations'],
              days, [top_agg[day].get('nodes') for day in days_map], color_map['nodes'],
@@ -76,7 +79,7 @@ def main():
     plt.ylabel('Number of events')
     plt.xlabel('Day of the week')
     plt.title('Middle users (66%)')
-    plt.plot(days, [mid_agg[day].get('commments') for day in days_map], color_map['comments'],
+    plt.plot(days, [mid_agg[day].get('comments') for day in days_map], color_map['comments'],
              days, [mid_agg[day].get('wiki') for day in days_map], color_map['wiki'],
              days, [mid_agg[day].get('registrations') for day in days_map], color_map['registrations'],
              days, [mid_agg[day].get('nodes') for day in days_map], color_map['nodes'],
@@ -86,7 +89,7 @@ def main():
     plt.ylabel('Number of events')
     plt.xlabel('Day of the week')
     plt.title('Bottom users (33%)')
-    plt.plot(days, [bot_agg[day].get('commments') for day in days_map], color_map['comments'],
+    plt.plot(days, [bot_agg[day].get('comments') for day in days_map], color_map['comments'],
              days, [bot_agg[day].get('wiki') for day in days_map], color_map['wiki'],
              days, [bot_agg[day].get('registrations') for day in days_map], color_map['registrations'],
              days, [bot_agg[day].get('nodes') for day in days_map], color_map['nodes'],
