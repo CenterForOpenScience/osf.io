e512ab41b09fda5a3435358bfeb9b54bc4fd54fe
Zak <zik3qe@virginia.edu>
2015-12-18 15:46:10 -0500

    Add template and some basic code for registration

--- admin/common_auth/urls.py
+++ admin/common_auth/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^login/?$', views.login, name='login'),
     url(r'^logout/$', views.logout, name='logout'),
+    url(r'^register/$', views.register, name='register'),
 ]
--- admin/common_auth/views.py
+++ admin/common_auth/views.py
@@ -2,7 +2,7 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, logout as logout_user, login as auth_login
 from django.shortcuts import render, redirect
 
-from forms import LoginForm
+from forms import LoginForm, CustomUserRegistrationForm
 
 def login(request):
     if request.user.is_authenticated():
@@ -24,3 +24,36 @@ def login(request):
 def logout(request):
     logout_user(request)
     return redirect('auth:login')
+
+def register(request):
+    # Creates User, takes selected permissions and assigns it to user, then saves resetform for email invitation
+    if request.user.is_authenticated():
+        return redirect('auth:login')
+    if request.method == 'POST':
+        form = CustomUserRegistrationForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            password = form.cleaned_data['password']
+            user = User.objects.create_user(username=email,
+                first_name=email, last_name=last_name, password=password)
+            user.save()
+            # Send email
+
+
+
+
+
+            user = authenticate(username=email, password=password)
+            auth_login(request, user)
+            return redirect('auth:login')
+        else:
+            context = {'form': form}
+            return render(request, 'register.html', context)
+    else:
+        ''' User not submitting form, show blank registrations form '''
+        form = CustomUserRegistrationForm()
+        context = {'form': form}
+        return render(request, 'register.html', context)
+
