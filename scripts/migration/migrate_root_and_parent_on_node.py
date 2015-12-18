bc751644021b1213eccd146d48dd95d3f9289e56
erinspace <erin.braswell@gmail.com>
2015-12-18 15:04:22 -0500

    Change variables and make migration resumable

--- scripts/migration/migrate_root_and_parent_on_node.py
+++ scripts/migration/migrate_root_and_parent_on_node.py
@@ -21,11 +21,11 @@ logger = logging.getLogger(__name__)
 def do_migration():
     init_app(routes=False)
     logger.warn('parent and root fields will be added to all nodes.')
-    all_undeleted_nodes = models.Node.find(Q('is_deleted', 'eq', False))
-    all_undeleted_nodes_count = all_undeleted_nodes.count()
+    all_nodes = models.Node.find(Q('is_deleted', 'eq', False) & Q('root', 'eq', None))
+    all_nodes_count = all_nodes.count()
     touched_counter = 0
-    logger.info('There are {} total nodes'.format(all_undeleted_nodes_count))
-    for node in all_undeleted_nodes:
+    logger.info('There are {} total nodes'.format(all_nodes_count))
+    for node in all_nodes:
         if not getattr(node, '_parent_node', None):
             touched_counter += 1
             node.save()
@@ -33,7 +33,7 @@ def do_migration():
             logger.info(
                 '{}/{}: touched node {} with children {}'.format(
                     touched_counter,
-                    all_undeleted_nodes_count,
+                    all_nodes_count,
                     node._id,
                     [child._id for child in children]
                 )
@@ -48,7 +48,7 @@ def do_migration():
                 logger.info(
                     '{}/{}: touched node {} with parent {}'.format(
                         touched_counter,
-                        all_undeleted_nodes_count,
+                        all_nodes_count,
                         child._id,
                         child.parent_id
                     )
@@ -60,7 +60,7 @@ def do_migration():
                 assert child.parent_node._id == child.parent_id
                 assert child.root._id == node._id
 
-    assert all_undeleted_nodes_count == touched_counter
+    assert all_nodes_count == touched_counter
 
 
 def main(dry=True):
