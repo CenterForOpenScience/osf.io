0c0d3ebc742ec39cf0aab55a0a22a48abdff9e5f
Sam Chrisinger <s.chrisinger@gmail.com>
2015-12-21 14:32:48 -0500

    Add get_prereg_schema and do proper comparison against registered_schema

--- website/project/model.py
+++ website/project/model.py
@@ -63,6 +63,7 @@ from website.project.licenses import (
     NodeLicenseRecord,
 )
 from website.project import signals as project_signals
+from website.prereg import uitls as prereg_utils
 
 logger = logging.getLogger(__name__)
 
@@ -3687,7 +3688,8 @@ class PreregCallbackMixin(object):
 
     def _notify_initiator(self):
         registration = self._get_registration()
-        if registration.registered_schema.name == 'Prereg Challenge':
+        prereg_schema = prereg_utils.get_prereg_schema()
+        if prereg_schema in registration.registered_schema:
             mails.send_mail(
                 self.initiator.username,
                 template=mails.PREREG_CHALLENGE_ACCEPTED,
@@ -4109,7 +4111,9 @@ class DraftRegistrationApproval(Sanction):
 
     def _send_rejection_email(self, user, draft):
         schema = draft.registration_schema
-        if schema.name == 'Prereg Challenge':
+        prereg_schema = prereg_utils.get_prereg_schema()
+
+        if schema._id == prereg_schema:
             mails.send_mail(
                 user.username,
                 mails.PREREG_CHALLENGE_REJECTED,
