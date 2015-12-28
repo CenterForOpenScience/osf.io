c57b4d070723d78e9df1a3b252c758c52bae1f4f
Henrique Moco <hmoco94@vt.edu>
2015-12-28 09:31:33 -0500

    Replace all instances of 'support@cos.io' with 'support@osf.io'

--- website/addons/wiki/static/dragNDrop.js
+++ website/addons/wiki/static/dragNDrop.js
@@ -232,7 +232,7 @@ var imageFolder = 'Wiki Image Uploads';
 var notUploaded = function(multiple) {
     var files = multiple ? 'File(s)' : 'File';
     $osf.growl('Error', files + ' not uploaded. Please refresh the page and try ' +
-        'again or contact <a href="mailto: support@cos.io">support@cos.io</a> ' +
+        'again or contact <a href="mailto: support@osf.io">support@osf.io</a> ' +
         'if the problem persists.', 'danger');
 };
 
