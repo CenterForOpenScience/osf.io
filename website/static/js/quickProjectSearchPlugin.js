94be43367a3e94f41479a766c718b74970699ee3
Dawn Pattison <pattison.dawn.r@gmail.com>
2015-12-29 21:53:23 -0500

    Have mithril view return node titles from API call users/me/nodes/

--- website/static/js/quickProjectSearchPlugin.js
+++ website/static/js/quickProjectSearchPlugin.js
@@ -9,15 +9,35 @@ var $osf = require('js/osfHelpers');
 // XHR config for apiserver connection
 var xhrconfig = function(xhr) {
     xhr.withCredentials = true;
-}
+};
+
 
 var quickSearchProject = {
-    controller: function(options){
+    controller: function() {
         var self = this;
-    },
-    view: function(ctrl, options){
+        self.nodes = [];
+        self.getMyNodes = function () {
+            var url = $osf.apiV2Url('users/me/nodes/', {});
+            var promise = m.request({method: 'GET', url : url, config : xhrconfig});
+            promise.then(function(result){
+                //console.log(result)
+                result.data.forEach(function(node){
+                    self.nodes.push(node)
+                })
+            });
+            return promise;
+        };
+        self.getMyNodes()
 
+    },
+    view : function(ctrl) {
+        console.log(ctrl.nodes);
+        return [
+            m("h1", 'My nodes'),
+            m('p', ctrl.nodes.map(function(n){
+               return m('li', n.attributes.title)}))]
     }
 };
 
 module.exports = quickSearchProject;
+
