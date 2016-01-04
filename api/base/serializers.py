7a3810e9050b167f56a512a7d1f6d4e44d45a8a2
Saman Ehsan <se8ea@virginia.edu>
2016-01-04 15:55:15 -0500

    Make unread comments count opt-in
    Require users to add ?related_counts=True
    in order to see unread comments.

--- api/base/serializers.py
+++ api/base/serializers.py
@@ -334,7 +334,7 @@ class RelationshipField(ser.HyperlinkedIdentityField):
         """
         meta = {}
         for key in meta_data or {}:
-            if key == 'count':
+            if key == 'count' or key == 'unread':
                 show_related_counts = self.context['request'].query_params.get('related_counts', False)
                 if utils.is_truthy(show_related_counts):
                     meta[key] = website_utils.rapply(meta_data[key], _url_val, obj=value, serializer=self.parent)
