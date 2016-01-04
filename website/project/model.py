c687ba75c4f24abf33e7f5eda32533671f01a9d2
Saman Ehsan <se8ea@virginia.edu>
2016-01-04 10:45:50 -0500

    Remove unused 'comment_target' backref

--- website/project/model.py
+++ website/project/model.py
@@ -181,7 +181,7 @@ class Comment(GuidStoredObject):
     # the direct 'parent' of the comment (e.g. the target of a comment reply is another comment)
     target = fields.AbstractForeignField(required=True)
     # The file or project overview page that the comment is for
-    root_target = fields.AbstractForeignField(backref='comment_target')
+    root_target = fields.AbstractForeignField()
 
     date_created = fields.DateTimeField(auto_now_add=datetime.datetime.utcnow)
     date_modified = fields.DateTimeField(auto_now=datetime.datetime.utcnow)
