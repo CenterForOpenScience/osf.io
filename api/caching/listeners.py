71f8ed10bc044566d0762c0be40f137c757bc537
Chris Wisecarver <cwisecarver@cos.io>
2016-01-04 12:08:29 -0500

    Remove failsafe for things without absolute_v2_url, now they fail.

--- api/caching/listeners.py
+++ api/caching/listeners.py
@@ -8,13 +8,6 @@ def log_object_saved(sender, instance, fields_changed, cached_data):
     abs_url = None
     if hasattr(instance, 'absolute_api_v2_url'):
         abs_url = instance.absolute_api_v2_url
-    else:
-        #  I don't think this should ever happen, but ... just in case.
-        guid_obj = Guid.load(instance._id)
-        if guid_obj is not None:
-            typedModel = guid_obj.referent
-            if hasattr(typedModel, 'absolute_api_v2_url'):
-                abs_url = typedModel.absolute_api_v2_url
 
     if abs_url is not None:
         enqueue_task(ban_url.s(abs_url))
