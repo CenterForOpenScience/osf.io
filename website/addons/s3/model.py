b76cf5072d189062175491263ca2bd3f3652091c
Matt Frazier <maf7sm@virginia.edu>
2016-01-15 16:40:10 -0500

    Update model with abstractproperties     -Remove dead code
    [#OSF-4936]

--- website/addons/s3/model.py
+++ website/addons/s3/model.py
@@ -29,6 +29,17 @@ class S3NodeSettings(StorageAddonBase, AddonOAuthNodeSettingsBase):
         return self.bucket
 
     @property
+    def folder_id(self):
+        return self.bucket
+
+    @property
+    def folder_path(self):
+        return self.bucket
+
+    def fetch_folder_name(self):
+        return self.folder_name
+
+    @property
     def display_name(self):
         return u'{0}: {1}'.format(self.config.full_name, self.bucket)
 
@@ -49,8 +60,13 @@ class S3NodeSettings(StorageAddonBase, AddonOAuthNodeSettingsBase):
         if save:
             self.save()
 
-    def deauthorize(self, auth=None, log=True, save=False):
-        self.bucket, self.user_settings = None, None
+    def clear_settings(self):
+        self.bucket = None
+
+    def deauthorize(self, auth=None, log=True):
+        """Remove user authorization from this node and log the event."""
+        self.clear_settings()
+        self.clear_auth()  # Also performs a save
 
         if log:
             self.owner.add_log(
@@ -61,12 +77,10 @@ class S3NodeSettings(StorageAddonBase, AddonOAuthNodeSettingsBase):
                 },
                 auth=auth,
             )
-        if save:
-            self.save()
 
     def delete(self, save=True):
         self.deauthorize(log=False, save=False)
-        super(AddonS3NodeSettings, self).delete(save=save)
+        super(S3NodeSettings, self).delete(save=save)
 
     def serialize_waterbutler_credentials(self):
         if not self.has_auth:
@@ -101,136 +115,6 @@ class S3NodeSettings(StorageAddonBase, AddonOAuthNodeSettingsBase):
                 }
             },
         )
-
-    def to_json(self, user):
-        ret = super(AddonS3NodeSettings, self).to_json(user)
-
-        user_settings = user.get_addon('s3')
-
-        ret.update({
-            'bucket': self.bucket or '',
-            'encrypt_uploads': self.encrypt_uploads,
-            'has_bucket': self.bucket is not None,
-            'user_is_owner': (
-                self.user_settings and self.user_settings.owner == user
-            ),
-            'user_has_auth': bool(user_settings) and user_settings.has_auth,
-            'node_has_auth': self.has_auth,
-            'owner': None,
-            'bucket_list': None,
-            'valid_credentials': user_settings and self.serializer(node_settings=self, user_settings=user_settings).credentials_are_valid,
-        })
-
-        if self.has_auth:
-            ret['owner'] = self.user_settings.owner.fullname
-            ret['owner_url'] = self.user_settings.owner.url
-            ret['node_has_auth'] = True
-
-        return ret
-
-    @property
-    def has_auth(self):
-        return bool(self.user_settings and self.user_settings.has_auth)
-        #TODO Update callbacks
-
-    def before_register(self, node, user):
-        """
-
-        :param Node node:
-        :param User user:
-        :return str: Alert message
-
-        """
-        category = node.project_or_component
-        if self.user_settings and self.user_settings.has_auth:
-            return (
-                u'The contents of S3 add-ons cannot be registered at this time; '
-                u'the S3 bucket linked to this {category} will not be included '
-                u'as part of this registration.'
-            ).format(**locals())
-
-    def after_fork(self, node, fork, user, save=True):
-        """
-
-        :param Node node: Original node
-        :param Node fork: Forked node
-        :param User user: User creating fork
-        :param bool save: Save settings after callback
-        :return tuple: Tuple of cloned settings and alert message
-
-        """
-        clone, _ = super(AddonS3NodeSettings, self).after_fork(
-            node, fork, user, save=False
-        )
-
-        # Copy authentication if authenticated by forking user
-        if self.user_settings and self.user_settings.owner == user:
-            clone.user_settings = self.user_settings
-            clone.bucket = self.bucket
-            message = (
-                'Amazon Simple Storage authorization copied to forked {cat}.'
-            ).format(
-                cat=fork.project_or_component,
-            )
-        else:
-            message = (
-                'Amazon Simple Storage authorization not copied to forked {cat}. You may '
-                'authorize this fork on the <u><a href={url}>Settings</a></u> '
-                'page.'
-            ).format(
-                cat=fork.project_or_component,
-                url=fork.url + 'settings/'
-            )
-
-        if save:
-            clone.save()
-
-        return clone, message
-
-    def before_remove_contributor(self, node, removed):
-        """
-
-        :param Node node:
-        :param User removed:
-        :return str: Alert message
-
-        """
-        if self.user_settings and self.user_settings.owner == removed:
-            return (
-                'The Amazon Simple Storage add-on for this {category} is authenticated '
-                'by {user}. Removing this user will also remove access '
-                'to {bucket} unless another contributor re-authenticates.'
-            ).format(
-                category=node.project_or_component,
-                user=removed.fullname,
-                bucket=self.bucket
-            )
-
-    def after_remove_contributor(self, node, removed, auth=None):
-        """
-
-        :param Node node:
-        :param User removed:
-        :return str: Alert message
-
-        """
-        if self.user_settings and self.user_settings.owner == removed:
-            self.user_settings = None
-            self.bucket = None
-            self.save()
-
-            message = (
-                u'Because the Amazon Simple Storage add-on for {category} "{title}" was '
-                u'authenticated by {user}, authentication information has been deleted.'
-            ).format(category=node.category_display, title=node.title, user=removed.fullname)
-
-            if not auth or auth.user != removed:
-                url = node.web_url_for('node_setting')
-                message += (
-                    u' You can re-authenticate on the <u><a href="{url}">Settings</a></u> page.'
-                ).format(url=url)
-            #
-            return message
 
     def after_delete(self, node, user):
         self.deauthorize(Auth(user=user), log=True, save=True)
