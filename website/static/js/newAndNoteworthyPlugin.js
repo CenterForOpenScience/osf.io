857e45e12febb646cdd8aa5056df5a655653ebba
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-01-15 12:27:25 -0500

    Return title, description, and contributors for first ten new projects.

--- website/static/js/newAndNoteworthyPlugin.js
+++ website/static/js/newAndNoteworthyPlugin.js
@@ -15,11 +15,54 @@ var xhrconfig = function(xhr) {
 
 var newAndNoteworthy = {
     controller: function() {
+        var self = this;
+        self.newNodes = m.prop([]);
+        self.noteworthyNodes = m.prop([]);
+
+        // Load new nodes
+        var url = $osf.apiV2Url('nodes/', { query : { 'embed': 'contributors'}});
+        var promise = m.request({method: 'GET', url : url, config: xhrconfig});
+        promise.then(function(result){
+            result.data.forEach(function(node){
+                self.newNodes().push(node);
+            });
+            return promise
+        });
+
+        // Gets contrib full name for display
+        self.getFullName = function(node) {
+            return node.embeds.contributors.data[0].embeds.users.data.attributes.full_name
+        };
+
+        // Formats contrib names for display
+        self.getContributors = function (node) {
+            var numContributors = node.embeds.contributors.links.meta.total;
+            if (numContributors === 1) {
+                return self.getFullName(node)
+            }
+            else {
+                return self.getFullName(node) + ' et al'
+            }
+
+        };
+
 
     },
     view : function(ctrl) {
+        function nodeDisplay(node) {
+            return m('div', {class: 'row'},
+                m('div', {class: 'col-sm-12'},
+                    m('h5', node.attributes.title),
+                    m('h5', node.attributes.description),
+                    m('h5', 'Contributors: ' + ctrl.getContributors(node))
+                )
+            )
+        }
+
         function newProjectsTemplate () {
-            return m('h1', 'Node')
+            return ctrl.newNodes().map(function(node){
+                return nodeDisplay(node)
+            })
 
         }
         function noteworthyProjectsTemplate() {
@@ -27,10 +70,10 @@ var newAndNoteworthy = {
 
         }
         return m('div', {class: 'container'}, [
-            m('div', {class: 'row'}, m('div', {class: 'col-sm-12'}, m('h4', 'Discover Public Projects'))),
+            m('div', {class: 'row'}, m('div', {class: 'col-sm-12'}, m('h3', 'Discover Public Projects'))),
             m('div', {class: 'row'},
-                m('div', {class: 'col-sm-6'}, [m('h5', 'New'), newProjectsTemplate() ]),
-                m('div', {class: 'col-sm-6'}, [m('h5', 'Noteworthy', noteworthyProjectsTemplate())])
+                m('div', {class: 'col-sm-6'}, [m('h4', 'New'), newProjectsTemplate() ]),
+                m('div', {class: 'col-sm-6'}, [m('h4', 'Noteworthy', noteworthyProjectsTemplate())])
             )
         ])
     }
