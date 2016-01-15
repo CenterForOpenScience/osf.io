a8c1375193d3d292147c4f021f7833917a534ff9
Matt Frazier <maf7sm@virginia.edu>
2016-01-15 16:41:53 -0500

    Update provider --  remove dead code

--- website/addons/s3/provider.py
+++ website/addons/s3/provider.py
@@ -1,10 +1,4 @@
-import httplib as http
-
-from framework.auth import Auth
-from framework.exceptions import HTTPError, PermissionsError
 from website.addons.s3.serializer import S3Serializer
-from website.oauth.models import ExternalAccount
-
 
 class S3Provider(object):
     """An alternative to `ExternalProvider` not tied to OAuth"""
@@ -14,42 +8,14 @@ class S3Provider(object):
     provider_name = 's3'
     serializer = S3Serializer
 
-    def __init__(self):
+    def __init__(self, account=None):
         super(S3Provider, self).__init__()
 
         # provide an unauthenticated session by default
-        self.account = None
+        self.account = account
 
     def __repr__(self):
         return '<{name}: {status}>'.format(
             name=self.__class__.__name__,
             status=self.account.provider_id if self.account else 'anonymous'
         )
-
-    def add_user_auth(self, node_addon, user, external_account_id):
-
-        external_account = ExternalAccount.load(external_account_id)
-
-        if external_account not in user.external_accounts:
-            raise HTTPError(http.FORBIDDEN)
-
-        try:
-            node_addon.set_auth(external_account, user)
-        except PermissionsError:
-            raise HTTPError(http.FORBIDDEN)
-
-        result = self.serializer(
-            node_settings=node_addon,
-            user_settings=user.get_addon('s3'),
-        ).serialized_node_settings
-        return {'result': result}
-
-    def remove_user_auth(self, node_addon, user):
-
-        node_addon.deauthorize(Auth(user=user))
-        node_addon.reload()
-        result = self.serializer(
-            node_settings=node_addon,
-            user_settings=user.get_addon(self.provider_name),
-        ).serialized_node_settings
-        return {'result': result}
