632096fc16dcb700e86bb253e2343818f6c75355
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-01-15 12:00:19 -0500

    Start new/noteworthy view template.

--- website/static/js/newAndNoteworthyPlugin.js
+++ website/static/js/newAndNoteworthyPlugin.js
@@ -15,9 +15,24 @@ var xhrconfig = function(xhr) {
 
 var newAndNoteworthy = {
     controller: function() {
+
     },
     view : function(ctrl) {
+        function newProjectsTemplate () {
+            return m('h1', 'Node')
 
+        }
+        function noteworthyProjectsTemplate() {
+            return m('h1', 'Nodes')
+
+        }
+        return m('div', {class: 'container'}, [
+            m('div', {class: 'row'}, m('div', {class: 'col-sm-12'}, m('h4', 'Discover Public Projects'))),
+            m('div', {class: 'row'},
+                m('div', {class: 'col-sm-6'}, [m('h5', 'New'), newProjectsTemplate() ]),
+                m('div', {class: 'col-sm-6'}, [m('h5', 'Noteworthy', noteworthyProjectsTemplate())])
+            )
+        ])
     }
 };
 
