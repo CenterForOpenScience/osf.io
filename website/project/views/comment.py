9492501278197adda5a71c78a8b316e4db5e7495
Saman Ehsan <se8ea@virginia.edu>
2016-01-19 17:03:05 -0500

    Move event_type check
    Since source and destination are
    not included in the payload of every
    waterbutler logged action, (but it is
    included when files are moved or renamed),
    move that check before getting the source
    and destination.

--- website/project/views/comment.py
+++ website/project/views/comment.py
@@ -20,12 +20,15 @@ from website import settings
 
 @file_updated.connect
 def update_comment_root_target_file(self, node, event_type, payload, user=None):
-    source = payload.get('source')
-    destination = payload.get('destination')
-    source_node = Node.load(source['node']['_id'])
-    destination_node = node
-    
-    if event_type == 'addon_file_moved' or (event_type == 'addon_file_renamed' and source.get('provider') != 'osfstorage'):
+    if event_type == 'addon_file_moved' or event_type == 'addon_file_renamed':
+        source = payload['source']
+        destination = payload['destination']
+        source_node = Node.load(source['node']['_id'])
+        destination_node = node
+
+        if event_type == 'addon_file_renamed' and source.get('provider') == 'osfstorage':
+            return
+
         if source.get('provider') == 'osfstorage':
             try:
                 old_file = TrashedFileNode.find_one(Q('provider', 'eq', source.get('provider')) &
