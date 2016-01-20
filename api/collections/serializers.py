4184d41206b65d1676400c02d46827a44989e1ea
Henrique Moco <hmoco94@vt.edu>
2016-01-20 10:41:34 -0500

    Add error details to the id NotFound exceptions raised

--- api/collections/serializers.py
+++ api/collections/serializers.py
@@ -90,7 +90,7 @@ class CollectionNodeLinkSerializer(NodeLinksSerializer):
     def get_absolute_url(self, obj):
         node_id = self.context['request'].parser_context['kwargs']['collection_id']
         return absolute_reverse(
-            'collections:node-pointer-detail',
+        ''    'collections:node-pointer-detail',
             kwargs={
                 'collection_id': node_id,
                 'node_link_id': obj._id
@@ -128,7 +128,7 @@ class CollectionLinkedNodesRelationshipSerializer(ser.Serializer):
         for node_id in diff['add']:
             node = Node.load(node_id)
             if not node:
-                raise exceptions.NotFound
+                raise exceptions.NotFound(detail='Node with id "{}" was not found'.format(node_id))
             nodes_to_add.append(node)
 
         return nodes_to_add, diff['remove'].values()
