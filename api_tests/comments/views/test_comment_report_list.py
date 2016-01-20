12be13825fd586dedf52d5be17817412986cf66f
Bryan Gorges <bgorges@cos.io>
2016-01-20 10:13:37 -0500

    Fix all api list comment tests

--- api_tests/comments/views/test_comment_report_list.py
+++ api_tests/comments/views/test_comment_report_list.py
@@ -27,10 +27,13 @@ class TestCommentReportsView(ApiTestCase):
         self.private_project = ProjectFactory.build(is_public=False, creator=self.user)
         self.private_project.add_contributor(contributor=self.contributor, save=True)
         self.comment = CommentFactory.build(node=self.private_project, target=self.private_project, user=self.contributor)
-        # self.comment.reports = self.comment.reports or {}
-        # self.comment.reports[self.user._id] = {'category': 'spam', 'text': 'This is spam'}
-        self.comment.report_spam(self.user, datetime.utcnow(),
-                                 category='spam', message='Really?')
+        self.comment.reports = self.comment.reports or {}
+        self.comment.reports[self.user._id] = {
+            'category': 'spam',
+            'text': 'This is spam',
+            'date': datetime.utcnow(),
+            'retracted': False,
+        }
         self.comment.save()
         self.private_url = '/{}comments/{}/reports/'.format(API_BASE, self.comment._id)
 
@@ -39,7 +42,12 @@ class TestCommentReportsView(ApiTestCase):
         self.public_project.add_contributor(contributor=self.contributor, save=True)
         self.public_comment = CommentFactory.build(node=self.public_project, target=self.public_project, user=self.contributor)
         self.public_comment.reports = self.public_comment.reports or {}
-        self.public_comment.reports[self.user._id] = {'category': 'spam', 'text': 'This is spam'}
+        self.public_comment.reports[self.user._id] = {
+            'category': 'spam',
+            'text': 'This is spam',
+            'date': datetime.utcnow(),
+            'retracted': False,
+        }
         self.public_comment.save()
         self.public_url = '/{}comments/{}/reports/'.format(API_BASE, self.public_comment._id)
 
@@ -104,7 +112,12 @@ class TestCommentReportsView(ApiTestCase):
         project = ProjectFactory(is_public=True, comment_level='public')
         comment = CommentFactory.build(node=project, user=project.creator)
         comment.reports = comment.reports or {}
-        comment.reports[self.non_contributor._id] = {'category': 'spam', 'text': 'This is spam.'}
+        comment.reports[self.non_contributor._id] = {
+            'category': 'spam',
+            'text': 'This is spam',
+            'date': datetime.utcnow(),
+            'retracted': False,
+        }
         comment.save()
         url = '/{}comments/{}/reports/'.format(API_BASE, comment._id)
         res = self.app.get(url, auth=self.non_contributor.auth)
