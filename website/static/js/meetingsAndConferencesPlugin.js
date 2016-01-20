2ff2a1803e0f2063ec23c9e38e83eccd65c2daf7
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-01-20 09:48:23 -0500

    Modify conferences and meetings plugin so on screen resizing, title doesn't run all the way to the end of the div.

--- website/static/js/meetingsAndConferencesPlugin.js
+++ website/static/js/meetingsAndConferencesPlugin.js
@@ -37,19 +37,21 @@ var meetingsAndConferences = {
         }
         return m('div', {class: 'container conferences-and-meetings m-v-md'}, [
             m('div', {class: 'row m-v-sm'},
-                m('div', {class: 'col-sm-6'},
+                m('div', {class: 'col-sm-5'},
                     m('div', {class: 'row'},
-                        m('div', {class: 'col-sm-offset-2'},
-                            m('h3', 'Hosting a Conference or Meeting?')
+                        m('div', {class: 'col-sm-offset-1 col-xs-offset-1'},
+                            m('div', {class: 'col-sm-11'}, m('h3', 'Hosting a Conference or Meeting?')),
+                            m('div', {class: 'col-sm-1'})
                         )
                     ),
                     m('div', {class: 'row'},
-                        m('div', {class: 'col-sm-offset-2'},
-                            m('h6', 'Use the OSF meetings service to provide a central location for collection submissions!'), m('a', {href: '/meetings/'}, 'Learn more')
+                        m('div', {class: 'col-sm-offset-1 col-xs-offset-1'},
+                            m('div', {class: 'col-sm-11'},  m('h6', 'Use the OSF meetings service to provide a central location for collection submissions!'), m('a', {href: '/meetings/'}, 'Learn more')),
+                            m('div', {class: 'col-sm-1'})
                         )
                     )
                 ),
-                m('div', {class: 'col-sm-6'},
+                m('div', {class: 'col-sm-7'},
                     m('div', {class: 'row'}),
                     m('div', {class: 'row'},
                         m('div', {class: 'col-sm-1'}),
