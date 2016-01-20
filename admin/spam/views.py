c6cd66e6645d4e276050feccd468e691037c9d8b
Bryan Gorges <bgorges@cos.io>
2016-01-20 10:26:38 -0500

    Fix flake8

--- admin/spam/views.py
+++ admin/spam/views.py
@@ -1,6 +1,4 @@
-import operator
-
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth.decorators import login_required
 
