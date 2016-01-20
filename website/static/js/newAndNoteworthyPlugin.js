9793cdef6f7c920175806d19ec35a0fd1480c11a
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-01-20 17:43:59 -0500

    Modify newAndNoteworthyNodes so now this list is generated the same way popular nodes are generated.  Both are node links of either the POPULAR_LINKS_NODE or the NEW_AND_NOTEWORTHY_LINKS_NODE defined in settings/defaults.

--- website/static/js/newAndNoteworthyPlugin.js
+++ website/static/js/newAndNoteworthyPlugin.js
@@ -18,18 +18,30 @@ var xhrconfig = function(xhr) {
 var newAndNoteworthy = {
     controller: function() {
         var self = this;
-        self.newNodes = m.prop([]);
+        self.newAndNoteworthyNodes = m.prop([]);
         self.popularNodes = m.prop([]);
-        self.popularContributors = {};
+        self.contributorsMapping = {};
+
+        //// Load new and noteworthy nodes
+        //var newUrl = $osf.apiV2Url('nodes/', { query : { 'embed': 'contributors', 'sort': '-date_created'}});
+        //var newPromise = m.request({method: 'GET', url : newUrl, config: xhrconfig});
+        //newPromise.then(function(result){
+        //    for (var i = 0; i <= 4; i++) {
+        //        self.newAndNoteworthyNodes().push(result.data[i])
+        //    }
+        //    return newPromise
+        //});
 
         // Load new and noteworthy nodes
-        var newUrl = $osf.apiV2Url('nodes/', { query : { 'embed': 'contributors', 'sort': '-date_created'}});
-        var newPromise = m.request({method: 'GET', url : newUrl, config: xhrconfig});
-        newPromise.then(function(result){
-            for (var i = 0; i <= 4; i++) {
-                self.newNodes().push(result.data[i])
-            }
-            return newPromise
+        var newAndNoteworthyUrl = $osf.apiV2Url('nodes/' + window.contextVars.newAndNoteworthy + '/node_links/', {});
+        var newAndNoteworthyPromise = m.request({method: 'GET', url: newAndNoteworthyUrl, config: xhrconfig});
+        newAndNoteworthyPromise.then(function(result){
+            var numPopular = Math.min(result.data.length - 1, 4);
+            for (var l=0; l <= numPopular; l++) {
+                self.newAndNoteworthyNodes().push(result.data[l]);
+                self.fetchContributors(result.data[l]);
+                  }
+            return newAndNoteworthyPromise
         });
 
         // Load popular nodes
@@ -37,16 +49,15 @@ var newAndNoteworthy = {
         var popularPromise = m.request({method: 'GET', url: popularUrl, config: xhrconfig});
         popularPromise.then(function(result){
             var numPopular = Math.min(result.data.length - 1, 4);
-            console.log(numPopular);
             for (var l=0; l <= numPopular; l++) {
                 self.popularNodes().push(result.data[l]);
-                self.fetchPopularContributors(result.data[l]);
+                self.fetchContributors(result.data[l]);
                   }
             return popularPromise
         });
 
         // Additional API call to fetch node link contributors
-        self.fetchPopularContributors = function(nodeLink) {
+        self.fetchContributors = function(nodeLink) {
             url = nodeLink.embeds.target_node.data.relationships.contributors.links.related.href;
             var promise = m.request({method: 'GET', url : url, config: xhrconfig});
             promise.then(function(result){
@@ -56,71 +67,49 @@ var newAndNoteworthy = {
                 });
                 var numContrib = result.links.meta.total;
                 var nodeId = nodeLink.id;
-                self.popularContributors[nodeId] = [contribNames, numContrib]
+                self.contributorsMapping[nodeId] = [contribNames, numContrib];
             })
         };
 
         // Gets contrib family name for display
-        self.getFamilyName = function(i, node, type) {
-            if (type === 'new') {
-                return node.embeds.contributors.data[i].embeds.users.data.attributes.family_name
-            }
-            else {
-                return self.popularContributors[node.id][0][i]
-            }
-
+        self.getFamilyName = function(i, node) {
+            return self.contributorsMapping[node.id][0][i]
         };
 
         // Returns name if one contrib, or adds et al if > 1
-        self.contribNameFormat = function(node, number, type) {
+        self.contribNameFormat = function(node, number) {
             if (number === 1) {
-                return self.getFamilyName(0, node, type)
+                return self.getFamilyName(0, node)
             }
             else if (number === 2) {
-                return self.getFamilyName(0, node, type) + ' and ' +
-                    self.getFamilyName(1, node, type)
+                return self.getFamilyName(0, node) + ' and ' +
+                    self.getFamilyName(1, node)
             }
             else if (number === 3) {
-                return self.getFamilyName(0, node, type) + ', ' +
-                    self.getFamilyName(1, node, type) + ', and ' +
-                    self.getFamilyName(2, node, type)
+                return self.getFamilyName(0, node) + ', ' +
+                    self.getFamilyName(1, node) + ', and ' +
+                    self.getFamilyName(2, node)
             }
             else {
-                return self.getFamilyName(0, node, type) + ', ' +
-                    self.getFamilyName(1, node, type) + ', ' +
-                    self.getFamilyName(2, node, type) + ' + ' + (number - 3)
+                return self.getFamilyName(0, node) + ', ' +
+                    self.getFamilyName(1, node) + ', ' +
+                    self.getFamilyName(2, node) + ' + ' + (number - 3)
             }
         };
 
         // Formats contrib names for display
-        self.getContributors = function (type, node) {
-            if (type === 'new') {
-                return self.contribNameFormat(node, node.embeds.contributors.links.meta.total, type)
-            }
-            else {
-                return self.contribNameFormat(node, self.popularContributors[node.id][1], type)
-            }
+        self.getContributors = function (node) {
+            return self.contribNameFormat(node, self.contributorsMapping[node.id][1]);
         };
 
         // Grabs title for display
-        self.getTitle = function (type, node){
-            if (type === 'new') {
-                return node.attributes.title
-            }
-            else {
-                return node.embeds.target_node.data.attributes.title
-
-            }
+        self.getTitle = function (node){
+            return node.embeds.target_node.data.attributes.title
         };
 
         // Grabs description for display
-        self.getDescription = function(type, node){
-            if (type === 'new'){
-                return node.attributes.description
-            }
-            else {
-                return node.embeds.target_node.data.attributes.description
-            }
+        self.getDescription = function(node){
+            return node.embeds.target_node.data.attributes.description
         };
 
          // Colors node div and changes cursor to pointer on hover
@@ -134,13 +123,8 @@ var newAndNoteworthy = {
         };
 
          // Onclick, directs user to project page
-        self.nodeDirect = function(type, node) {
-            if (type === 'new') {
-                location.href = '/'+ node.id
-            }
-            else {
-                location.href = '/' + node.embeds.target_node.data.id
-            }
+        self.nodeDirect = function(node) {
+            location.href = '/' + node.embeds.target_node.data.id
         };
 
         self.redirectToSearch = function() {
@@ -150,26 +134,26 @@ var newAndNoteworthy = {
 
     },
     view : function(ctrl) {
-        function nodeDisplay(type, node) {
-            return m('div', {class: 'row node-styling m-v-md m-r-sm', onmouseover: function(){ctrl.mouseOver(this)}, onmouseout: function(){ctrl.mouseOut(this)}, onclick: function(){{ctrl.nodeDirect(type, node)}}},
+        function nodeDisplay(node) {
+            return m('div', {class: 'row node-styling m-v-md m-r-sm', onmouseover: function(){ctrl.mouseOver(this)}, onmouseout: function(){ctrl.mouseOut(this)}, onclick: function(){{ctrl.nodeDirect(node)}}},
                 m('div', {class: 'col-sm-12'},
-                    m('h5', m('em', ctrl.getTitle(type, node))),
-                    m('h5', ctrl.getDescription(type, node)),
-                    m('div', m('h5', {class: 'contributors-bold f-w-xl'}, 'Contributors: '), m('h5', {class: 'contributors-bold'}, ctrl.getContributors(type, node))
+                    m('h5', m('em', ctrl.getTitle(node))),
+                    m('h5', ctrl.getDescription(node)),
+                    m('div', m('h5', {class: 'contributors-bold f-w-xl'}, 'Contributors: '), m('h5', {class: 'contributors-bold'}, ctrl.getContributors(node))
                     )
                 )
             )
         }
 
         function newAndNoteworthyProjectsTemplate () {
-            return ctrl.newNodes().map(function(node){
-                return nodeDisplay('new', node)
+            return ctrl.newAndNoteworthyNodes().map(function(node){
+                return nodeDisplay(node)
             })
 
         }
         function popularProjectsTemplate() {
             return ctrl.popularNodes().map(function(node){
-                return nodeDisplay('noteworthy', node)
+                return nodeDisplay(node)
             })
 
         }
