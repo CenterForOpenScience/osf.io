d65e1ef8f5669fb0535969cc4c2f1d5d736e8df8
Caner Uguz <caneruguz@gmail.com>
2016-01-21 11:14:49 -0500

    Adjust api calls to v2  [broken] This method of resolving will be abandoned, fixes coming to component-3

--- website/static/js/pages/prereg-landing-page.js
+++ website/static/js/pages/prereg-landing-page.js
@@ -7,6 +7,13 @@ require('js/qToggle');
 require('js/onboarder.js');
 require('js/components/autocomplete');
 
+var xhrconfig = function (xhr) {
+    xhr.withCredentials = true;
+    xhr.setRequestHeader('Content-Type', 'application/vnd.api+json');
+    xhr.setRequestHeader('Accept', 'application/vnd.api+json; ext=bulk');
+};
+
+
 $(function(){
     $('.prereg-button').qToggle();
     $('.prereg-button').click(function(){
@@ -29,29 +36,46 @@ $(function(){
         });
     });
 
-    // Activate "existing projects" typeahead.
-    var url = '/api/v1/dashboard/get_nodes/';
-    $.getJSON(url).done(function(response) {
-        var allNodes = response.nodes;
+    var allNodes = [];
+    var allRegistrations = [];
 
-        // If we need to change what nodes can be registered, filter here
-        var registrationSelection = ko.utils.arrayFilter(allNodes, function(node) {
-            return $.inArray(node.permissions, ['admin']) !== -1;
+    // Get all projects with multiple calls to get all pages
+    function collectProjects (url) {
+        var promise = $.ajax({
+            method: "GET",
+            url: url,
+            xhrFields: {
+                withCredentials: true
+            }
         });
+        promise.done(function(result){
+            // loop through items and check for admin permission first
+            allNodes.concat(result.data);
+            if(result.links.next){
+                collectProjects(result.links.next);
+            }
+            else {
+                $osf.applyBindings({
+                    nodes: allNodes,
+                    enableComponents: true
+                }, '#existingProject');
+                $osf.applyBindings({
+                    nodes: allNodes,
+                    enableComponents: true
+                }, '#existingProjectXS');
+            }
+        });
+        promise.fail(function(xhr, textStatus, error) {
+            Raven.captureMessage('Next page load failed for user nodes.', {
+                url: url, textStatus: textStatus, error: error
+            });
+        });
+    }
 
-        $osf.applyBindings({
-            nodes: registrationSelection,
-            enableComponents: true
-        }, '#existingProject');
-        $osf.applyBindings({
-            nodes: registrationSelection,
-            enableComponents: true
-        }, '#existingProjectXS');
-    }).fail(function(xhr, textStatus, error) {
-        Raven.captureMessage('Could not fetch dashboard nodes.', {
-            url: '/api/v1/dashboard/get_nodes/', textStatus: textStatus, error: error
-        });
-    });
+
+    // Get nodes and apply bindings
+    var nodeLink = $osf.apiV2Url('users/me/nodes/', { query : { 'page[size]' : 100}});
+    collectProjects(nodeLink);
 
     // Activate autocomplete for draft registrations
     $.getJSON('/api/v1/prereg/draft_registrations/').then(function(response){
