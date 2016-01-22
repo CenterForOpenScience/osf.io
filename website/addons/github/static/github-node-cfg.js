4002dcffc3301d1a672e6a69ca72526952bc867b
Matt Frazier <maf7sm@virginia.edu>
2016-01-21 19:05:32 -0500

    Fix account creation from node settings

--- website/addons/github/static/github-node-cfg.js
+++ website/addons/github/static/github-node-cfg.js
@@ -76,6 +76,59 @@ var GithubConfigHelper = (function() {
         });
     };
 
+    var askImport = function() {
+        $.get('/api/v1/settings/github/accounts/'
+        ).done(function(data){
+            var accounts = data.accounts.map(function(account) {
+                return {
+                    name: account.display_name,
+                    id: account.id
+                };
+            });
+            if (accounts.length > 1) {
+                bootbox.prompt({
+                    title: 'Choose GitHub Account to Import',
+                    inputType: 'select',
+                    inputOptions: ko.utils.arrayMap(
+                        accounts,
+                        function(item) {
+                            return {
+                                text: item.name,
+                                value: item.id
+                            };
+                        }
+                    ),
+                    value: accounts[0].id,
+                    callback: function(accountId) {
+                        connectExistingAccount(accountId);
+                    },
+                    buttons: {
+                        confirm:{
+                            label:'Import',
+                        }
+                    }
+                });
+            } else {
+                bootbox.confirm({
+                    title: 'Import GitHub Account?',
+                    message: 'Are you sure you want to link your GitHub account with this project?',
+                    callback: function(confirmed) {
+                        if (confirmed) {
+                            connectExistingAccount(accounts[0].id);
+                        }
+                    },
+                    buttons: {
+                        confirm: {
+                            label:'Import',
+                        }
+                    }
+                });
+            }
+        }).fail(function(xhr, textStatus, error) {
+            displayError('Could not GET GitHub accounts for user.');
+        });
+    };
+
     $(document).ready(function() {
         $('#githubSelectRepo').on('change', function() {
             var value = $(this).val();
@@ -89,60 +142,14 @@ var GithubConfigHelper = (function() {
         });
 
         $('#githubImportToken').on('click', function() {
-            $.get('/api/v1/settings/github/accounts/'
-            ).done(function(data){
-                var accounts = data.accounts.map(function(account) {
-                    return {
-                        name: account.display_name,
-                        id: account.id
-                    };
-                });
-                if (accounts.length > 1) {
-                    bootbox.prompt({
-                        title: 'Choose GitHub Account to Import',
-                        inputType: 'select',
-                        inputOptions: ko.utils.arrayMap(
-                            accounts,
-                            function(item) {
-                                return {
-                                    text: item.name,
-                                    value: item.id
-                                };
-                            }
-                        ),
-                        value: accounts[0].id,
-                        callback: function(accountId) {
-                            connectExistingAccount(accountId);
-                        },
-                        buttons: {
-                            confirm:{
-                                label:'Import',
-                            }
-                        }
-                    });
-                } else {
-                    bootbox.confirm({
-                        title: 'Import GitHub Account?',
-                        message: 'Are you sure you want to link your GitHub account with this project?',
-                        callback: function(confirmed) {
-                            if (confirmed) {
-                                connectExistingAccount(accounts[0].id);
-                            }
-                        },
-                        buttons: {
-                            confirm: {
-                                label:'Import',
-                            }
-                        }
-                    });
-                }
-            }).fail(function(xhr, textStatus, error) {
-                displayError('Could not GET GitHub accounts for user.');
-            });
+            askImport();
         });
 
         $('#githubCreateToken').on('click', function() {
-            window.location.href = nodeApiUrl + 'oauth/connect/github/';
+            window.oauthComplete = function(res) {
+                askImport();
+            };
+            window.open('/oauth/connect/github/');
         });
 
         $('#githubRemoveToken').on('click', function() {
