f5d3525920c84731c5e24b32bfee06bb4294c0b5
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-01-26 16:53:16 -0500

    Change query for getting logs associated with a particular node. Now each log has a "node" field and backers don't need to be used.

--- website/project/model.py
+++ website/project/model.py
@@ -1728,7 +1728,7 @@ class Node(GuidStoredObject, AddonModelMixin, IdentifierMixin):
         ids = [self._id] + [n._id
                             for n in self.get_descendants_recursive()
                             if n.can_view(auth)]
-        query = Q('__backrefs.logged.node.logs', 'in', ids) & Q('should_hide', 'ne', True)
+        query = Q('node', 'in', ids) & Q('should_hide', 'ne', True)
         return query
 
     def get_aggregate_logs_queryset(self, auth):
