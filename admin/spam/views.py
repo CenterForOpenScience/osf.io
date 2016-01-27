6449d457fd05cd9009edf4b4d63dd81bb9dafc53
Bryan Gorges <bgorges@cos.io>
2016-01-27 10:57:06 -0500

    Switch to CBV
    post in spam detail will hopefully mark the comment as
    spam

--- admin/spam/views.py
+++ admin/spam/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+from django.utils.decorators import method_decorator
 
 from modularodm import Q
 from website.project.model import Comment
@@ -17,23 +19,49 @@ def get_spam_list(mark=1):
     return Comment.find(query).sort('date_created')
 
 
-@login_required
-def spam_list(request):
-    paginator = Paginator(get_spam_list(), 10)
+class SpamList(TemplateView):
+    template_name = 'spam/spam.html'
 
-    page_number = request.GET.get('page', 1)
-    try:
-        page = paginator.page(page_number)
-    except PageNotAnInteger:
-        page = paginator.page(1)
-    except EmptyPage:
-        page = paginator.page(paginator.num_pages)
-    context = {
-        'spam': map(serialize_comment, page),
-        'page': page,
-        'page_number': page_number,
-    }
-    return render(request, 'spam/spam.html', context)
+    @method_decorator(login_required)  # TODO: 1.9 upgrade to class decorator
+    def get(self, request, *args, **kwargs):
+        spam_status = request.GET.get('status', 1)
+        paginator = Paginator(get_spam_list(mark=spam_status), 10)
+
+        page_number = request.GET.get('page', 1)
+        try:
+            page = paginator.page(page_number)
+        except PageNotAnInteger:
+            page = paginator.page(1)
+        except EmptyPage:
+            page = paginator.page(paginator.num_pages)
+        context = {
+            'spam': map(serialize_comment, page),
+            'page': page,
+            'status': spam_status,
+            'page_number': page_number,
+        }
+        return self.render_to_response(context)
+
+
+class SpamDetail(TemplateView):
+    template_name = 'spam/comment.html'
+    spam_id = None
+
+    @method_decorator(login_required)
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        context['page_number'] = request.GET.get('page', 1)
+        return self.render_to_response(context)
+
+    @method_decorator(login_required)
+    def post(self):
+        pass
+
+    def get_context_data(self, **kwargs):
+        kwargs = super(SpamDetail, self).get_context_data(**kwargs)
+        kwargs['comment'] = serialize_comment(Comment.load(kwargs['spam_id']))
+        return kwargs
+
 
 
 @login_required
