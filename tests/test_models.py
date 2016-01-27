c527467b17f9d1301edc9e02ba96e7fdb1e102df
Bryan Gorges <bgorges@cos.io>
2016-01-27 13:22:52 -0500

    Find spam tests
    Move them to their own mixin testing.

--- tests/test_models.py
+++ tests/test_models.py
@@ -58,7 +58,7 @@ from tests.factories import (
     UserFactory, ApiOAuth2ApplicationFactory, NodeFactory, PointerFactory,
     ProjectFactory, NodeLogFactory, WatchConfigFactory,
     NodeWikiFactory, RegistrationFactory, UnregUserFactory,
-    ProjectWithAddonFactory, UnconfirmedUserFactory, CommentFactory, PrivateLinkFactory,
+    ProjectWithAddonFactory, UnconfirmedUserFactory, PrivateLinkFactory,
     AuthUserFactory, DashboardFactory, FolderFactory,
     NodeLicenseRecordFactory
 )
@@ -4353,81 +4353,6 @@ class TestProjectWithAddons(OsfTestCase):
         p = ProjectWithAddonFactory(addon='s3')
         assert_true(p.get_addon('s3'))
         assert_true(p.creator.get_addon('s3'))
-
-
-class TestSpamMixin(OsfTestCase):
-
-    def setUp(self):
-        super(TestSpamMixin, self).setUp()
-        self.comment = CommentFactory()
-        self.auth = Auth(user=self.comment.user)
-
-    def test_report_abuse(self):
-        user = UserFactory()
-        time = datetime.datetime.utcnow()
-        assert_equal(self.comment.spam_status, 0)
-        self.comment.report_abuse(
-                user, date=time, category='spam', text='ads', save=True)
-        equivalent = dict(
-            date=time,
-            category='spam',
-            text='ads',
-            retracted=False
-        )
-        assert_in(user._id, self.comment.reports)
-        assert_equal(self.comment.reports[user._id], equivalent)
-        assert_equal(self.comment.spam_status, 1)
-
-    def test_report_spam_own_comment(self):
-        with assert_raises(ValueError):
-            self.comment.report_abuse(
-                self.comment.user,
-                category='spam', text='ads',
-                save=True
-            )
-        assert_equal(self.comment.spam_status, 0)
-
-    def test_retract_report(self):
-        user = UserFactory()
-        time = datetime.datetime.utcnow()
-        self.comment.report_abuse(
-                user, date=time, category='spam', text='ads', save=True
-        )
-        assert_equal(self.comment.spam_status, 1)
-        self.comment.retract_report(user, save=True)
-        equivalent = {
-            'date': time,
-            'category': 'spam',
-            'text': 'ads',
-            'retracted': True
-        }
-        assert_in(user._id, self.comment.reports)
-        assert_equal(self.comment.reports[user._id], equivalent)
-        assert_equal(self.comment.spam_status, 0)
-
-    def test_retract_report_not_reporter(self):
-        reporter = UserFactory()
-        non_reporter = UserFactory()
-        self.comment.report_abuse(
-                reporter, category='spam', text='ads', save=True
-        )
-        with assert_raises(ValueError):
-            self.comment.retract_report(non_reporter, save=True)
-
-    def test_validate_reports_bad_key(self):
-        self.comment.reports[None] = {'category': 'spam', 'text': 'ads'}
-        with assert_raises(ValidationValueError):
-            self.comment.save()
-
-    def test_validate_reports_bad_type(self):
-        self.comment.reports[self.comment.user._id] = 'not a dict'
-        with assert_raises(ValidationTypeError):
-            self.comment.save()
-
-    def test_validate_reports_bad_value(self):
-        self.comment.reports[self.comment.user._id] = {'foo': 'bar'}
-        with assert_raises(ValidationValueError):
-            self.comment.save()
 
 
 class TestPrivateLink(OsfTestCase):
