85b2d002e30f95c8cc5fa2d608ca2056db1c63bc
Matt Frazier <maf7sm@virginia.edu>
2016-01-28 14:57:36 -0500

    Remove unused code

--- website/addons/s3/static/user-cfg.js
+++ website/addons/s3/static/user-cfg.js
@@ -1,10 +1,6 @@
-var S3ViewModel = require('./s3UserConfig.js').S3ViewModel;
 var S3UserConfig = require('./s3UserConfig.js').S3UserConfig;
 
 // Endpoint for S3 user settings
 var url = '/api/v1/settings/s3/accounts/';
 
 var s3UserConfig = new S3UserConfig('#s3AddonScope', url);
-
-// Load initial S3 data
-s3UserConfig.viewModel.updateAccounts();
