ccda8b35663782da48c5ecda223c0615f594a202
Zak <zik3qe@virginia.edu>
2016-01-28 10:36:53 -0500

    flake8 fix and add comment for devops

--- admin/common_auth/forms.py
+++ admin/common_auth/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.core.exceptions import ValidationError
 from .models import MyUser
 
 class LoginForm(forms.Form):
