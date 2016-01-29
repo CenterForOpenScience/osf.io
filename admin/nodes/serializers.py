5dcd8d4d4c8d923c2af0f664ed70d69317c5c3a8
Bryan Gorges <bgorges@cos.io>
2016-01-29 10:57:55 -0500

    Add very basic node data
    - use guid class

---
+++ admin/nodes/serializers.py
@@ -0,0 +1,7 @@
+
+
+def serialize_node(node):
+    return {
+        'id': node._id,
+        'title': node.title,
+    }
--- admin/nodes/views.py
+++ admin/nodes/views.py
@@ -1,11 +1,20 @@
-from django.views.generic import FormView
+from django.core.urlresolvers import reverse
 
+from website.project.model import Node
+
+from admin.abstract.views import GuidFormView
+from .serializers import serialize_node
 from .forms import NodeForm
 
 
-class NodeFormView(FormView):
+class NodeFormView(GuidFormView):
     form_class = NodeForm
     template_name = 'nodes/node.html'
+    object_type = 'node'
 
-    def post(self, request, *args, **kwargs):
-        return super(NodeFormView, self).post(request, *args, **kwargs)
+    def get_guid_object(self):
+        return serialize_node(Node.load(self.guid))
+
+    @property
+    def success_url(self):
+        return reverse('nodes:node') + '?guid={}'.format(self.guid)
