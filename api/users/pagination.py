3a9a9baac7350050a3ee53e0d843202588229e6d
Henrique Moco <hmoco94@vt.edu>
2016-02-01 13:37:39 -0500

    Cleaned up user pagination to make use of super instead of rewriting code

--- api/users/pagination.py
+++ api/users/pagination.py
@@ -8,21 +8,11 @@ class UserLogPagination(JSONAPIPagination):
     node_log_aggregates = None
 
     def get_response_dict(self, data, url):
-        if self.request.query_params.get('aggregates'):
+        response_dict = super(UserLogPagination, self).get_response_dict(data, url)
+        if self.request.query_params.get('aggregates', False):
             comments, nodes, wiki, files = self.node_log_aggregates()
-            response_dict = OrderedDict([
-                ('data', data),
-                ('links', OrderedDict([
-                    ('first', self.get_first_real_link(url)),
-                    ('last', self.get_last_real_link(url)),
-                    ('prev', self.get_previous_real_link(url)),
-                    ('next', self.get_next_real_link(url)),
-                    ('meta', OrderedDict([
-                        ('total', self.page.paginator.count),
-                        ('per_page', self.page.paginator.per_page),
-                    ]))
-                ])),
-                ('meta', OrderedDict([
+            meta = {
+                'meta': OrderedDict([
                     ('aggregates', OrderedDict([
                         ('comments', comments),
                         ('wiki', wiki),
@@ -30,20 +20,7 @@ class UserLogPagination(JSONAPIPagination):
                         ('files', files),
                     ])),
                     ('last_log_date', list(self.page.paginator.object_list)[-1].date if self.page.paginator.object_list else 0)
-                ]))
-            ])
-        else:
-            response_dict = OrderedDict([
-                ('data', data),
-                ('links', OrderedDict([
-                    ('first', self.get_first_real_link(url)),
-                    ('last', self.get_last_real_link(url)),
-                    ('prev', self.get_previous_real_link(url)),
-                    ('next', self.get_next_real_link(url)),
-                    ('meta', OrderedDict([
-                        ('total', self.page.paginator.count),
-                        ('per_page', self.page.paginator.per_page)
-                    ]))
-                ])),
-            ])
+                ])
+            }
+            response_dict.update(meta)
         return response_dict
