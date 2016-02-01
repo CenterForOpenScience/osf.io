3b814147ff12e11001559372792198a1efbe794f
Caner Uguz <caneruguz@gmail.com>
2016-02-01 10:02:31 -0500

    Fix allLogText trailing white space and gitignore it [skip ci]

--- webpack.common.config.js
+++ webpack.common.config.js
@@ -114,7 +114,7 @@ function objToString (obj) {
     var str = '';
     for (var p in obj) {
         if (obj.hasOwnProperty(p)) {
-            str += '    \'' + p + '\' : ' + '\'' + obj[p] + '\', \n';
+            str += '    \'' + p + '\' : ' + '\'' + obj[p] + '\',\n';
         }
     }
     return str;
