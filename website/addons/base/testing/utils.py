2d6fae28311125366c178e871c66abcd6b1ec418
Matt Frazier <maf7sm@virginia.edu>
2016-02-02 12:16:35 -0500

    Add TestSuiteMixin for citation serializers     -add tests for citation addons' serializers

--- website/addons/base/testing/utils.py
+++ website/addons/base/testing/utils.py
@@ -27,3 +27,5 @@ class MockFolder(dict, object):
         self.name = 'Fake Folder'
         self.json = {'id': 'Fake Key', 'parent_id': 'cba321'}
         self['data'] = {'name': 'Fake Folder', 'key': 'Fake Key'}
+        self['name'] = 'Fake Folder'
+        self['id'] = 'Fake Key'
