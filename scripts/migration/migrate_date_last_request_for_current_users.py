1ddd67ea74e54f5eb013c7e48dbdd966d929dc2f
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-02-04 11:32:05 -0500

    Add a script to add date_last_request field to all users, which is set equal to date_last_login.

---
+++ scripts/migration/migrate_date_last_request_for_current_users.py
@@ -0,0 +1,34 @@
+"""
+Used to add users date_last_request, value set equal to date_last_login.
+"""
+
+import sys
+import logging
+from modularodm import Q
+from website.app import init_app
+from website import models
+from scripts import utils as script_utils
+from framework.transactions.context import TokuTransaction
+
+logger = logging.getLogger(__name__)
+
+
+def main(dry=True):
+    init_app(routes=False)
+    users = models.User.find(Q('date_last_request', 'eq', None))
+    logger.warn('All active users will have a date_last_request field added and set equal to date_last_login.')
+
+    for user in users:
+        with TokuTransaction():
+            if user.is_active:
+                user.date_last_request = user.date_last_login
+                if not dry:
+                    user.save()
+                    logger.info('User {0} "date_last_request" added'.format(user._id))
+
+if __name__ == '__main__':
+    dry_run = 'dry' in sys.argv
+    if not dry_run:
+        script_utils.add_file_logger(logger, __file__)
+    main(dry=dry_run)
+
