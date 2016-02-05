fca592be3befc379a426116cb894df2cc5288930
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-02-05 17:03:32 -0500

    NodeList should use default_node_list_query().

--- api/nodes/views.py
+++ api/nodes/views.py
@@ -234,17 +234,12 @@ class NodeList(JSONAPIBaseView, bulk_views.BulkUpdateJSONAPIView, bulk_views.Bul
 
     # overrides ODMFilterMixin
     def get_default_odm_query(self):
-        base_query = (
-            Q('is_deleted', 'ne', True) &
-            Q('is_collection', 'ne', True) &
-            Q('is_registration', 'ne', True)
-        )
         user = self.request.user
         permission_query = Q('is_public', 'eq', True)
         if not user.is_anonymous():
             permission_query = (permission_query | Q('contributors', 'eq', user._id))
 
-        query = base_query & permission_query
+        query = default_node_list_query() & permission_query
         return query
 
     # overrides ListBulkCreateJSONAPIView, BulkUpdateJSONAPIView
