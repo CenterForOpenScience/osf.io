5534cd5ebd295e724b8ce1987f5a5fd8a45bc2ab
Matt Frazier <maf7sm@virginia.edu>
2016-02-10 12:49:20 -0500

    Remove unused class

--- website/addons/base/testing/utils.py
+++ website/addons/base/testing/utils.py
@@ -1,26 +1,4 @@
 # -*- coding: utf-8 -*-
-
-class MockNode(object):
-
-    addon = None
-    addon_name = None
-
-    def __init__(self, name=None):
-        self.addon_name = name
-
-    @property
-    def is_deleted(self):
-        return False
-
-    @property
-    def is_public(self):
-        return True
-
-    def get_addon(self, name):
-        if name == self.addon_name:
-            return self.addon
-        return None
-
 class MockFolder(dict, object):
 
     def __init__(self):
