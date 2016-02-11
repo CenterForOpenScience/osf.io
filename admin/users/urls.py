ad418a4e9dad5f2a5ad72e7817f211857bae1778
Bryan Gorges <bgorges@cos.io>
2016-02-11 14:15:15 -0500

    Disable user
    Except for mailchimp of course.
    
    [skip ci]

--- admin/users/urls.py
+++ admin/users/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.UserFormView.as_view(), name='user'),
+    url(r'^(?P<guid>[a-z0-9]+)/disable/$', views.disable_user, name='disable'),
 ]
--- admin/users/views.py
+++ admin/users/views.py
@@ -1,8 +1,16 @@
+from django.shortcuts import redirect
+
 from website.project.model import User
 
 from admin.base.views import GuidFormView
 from admin.users.templatetags.user_extras import reverse_user
 from .serializers import serialize_user
+
+
+def disable_user(request, guid):
+    user = User.load(guid)
+    user.disable_account()
+    return redirect(reverse_user(guid))
 
 
 class UserFormView(GuidFormView):
