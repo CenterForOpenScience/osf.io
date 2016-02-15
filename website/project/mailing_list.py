3b0635bf6caabbea2216e53587a2a26a6d973c43
Matt Frazier <maf7sm@virginia.edu>
2016-02-15 17:47:39 -0500

    Add ML success template     -update sending process accordingly
    [skip ci]

--- website/project/mailing_list.py
+++ website/project/mailing_list.py
@@ -7,7 +7,7 @@ from framework.tasks.handlers import queued_task
 from framework.exceptions import HTTPError
 
 from website import settings
-
+from website.mails.mails import render_message
 
 def address(node_id):
     return node_id + '@' + settings.SHORT_DOMAIN
@@ -26,23 +26,34 @@ def send_messages(node, sender, message):
         if u not in node.mailing_unsubs
         and u != sender
         and u.is_active]
+    subject = message['subject']
+    context = {
+        'body': message['stripped-text'][0],
+        'cat': node.category,
+        'node_url': '{}{}'.format(settings.DOMAIN.rstrip('/'), node.url),
+        'node_title': node.title,
+        'url': '{}{}settings/#discussionsSettings'.format(settings.DOMAIN.rstrip('/'), node.url)
+    }
+    rendered_message = render_message('discussions_email_accepted.html.mako', **context)
     for recipient in recipients:
-        send_message(node._id, sender.fullname, recipient, message)
+        send_message(node._id, sender.fullname, recipient.username, subject, rendered_message)
 
 @queued_task
 @app.task
-def send_message(node_id, sender_fullname, recipient, message):
+def send_message(node_id, sender_fullname, recipient_email, subject, message):
     """ Sends a message from the node through the given mailing list
     :param node_id: The id of the node in question
-    :param user_fullname: The user sending the message
-    :param message: Dictionary with subject and text of the email to be sent
+    :param sender_fullname: The user sending the message
+    :param recipient_email: The user receiving the message
+    :param subject: email subject
+    :param message: Rendered email content from template
     """
     res = requests.post(
         'https://api.mailgun.net/v3/{}/messages'.format(settings.SHORT_DOMAIN),
         auth=('api', settings.MAILGUN_API_KEY),
         data={'from': '{0} <{1}>'.format(sender_fullname, address(node_id)),
-              'to': recipient.username,
-              'subject': message['subject'],
-              'html': '<html>{}</html>'.format(message['stripped-text'])})
+              'to': recipient_email,
+              'subject': subject,
+              'html': message})
     if res.status_code != 200:
         raise HTTPError(res.status_code)
