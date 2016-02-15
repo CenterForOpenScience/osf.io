4d240aa326e5826db7285641e3765e0544c6b25d
Caner Uguz <caneruguz@gmail.com>
2016-02-15 17:51:28 -0500

    Fix dashboard test

--- website/static/js/tests/dashboard.test.js
+++ website/static/js/tests/dashboard.test.js
@@ -42,16 +42,16 @@ describe('fileBrowser', function() {
 
         describe('#generateLinks', function () {
             it('should return correct collection link', function () {
-                assert.equal(collection.link, 'users/me/nodes/?related_counts=true');
+                assert.equal(collection.link, 'users/me/nodes/?related_counts=children');
             });
             it('should return correct tag link', function () {
-                assert.equal(tag.link, 'nodes/?filter%5Btags%5D=something&related_counts=true&embed=contributors');
+                assert.equal(tag.link, 'nodes/?filter%5Btags%5D=something&related_counts=children&embed=contributors');
             });
             it('should return correct name link', function () {
-                assert.equal(name.link, 'users/8q36f/nodes/?related_counts=true&embed=contributors');
+                assert.equal(name.link, 'users/8q36f/nodes/?related_counts=children&embed=contributors');
             });
             it('should return correct node link', function () {
-                assert.equal(node.link, 'nodes/qwerty/children/?related_counts=true&page%5Bsize%5D=60&embed=contributors');
+                assert.equal(node.link, 'nodes/qwerty/children/?related_counts=children&page%5Bsize%5D=60&embed=contributors');
             });
         });
     });
