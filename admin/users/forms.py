90c0e9d740817ace9fc37c709f3780eace3a0ecc
Bryan Gorges <bgorges@cos.io>
2016-02-16 16:12:27 -0500

    Connect to model

--- admin/users/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from .models import OSFUser
-
-
-class OSFUserForm(ModelForm):
-    class Meta:
-        model = OSFUser
-        fields = ['osf_id', 'notes']
--- admin/users/views.py
+++ admin/users/views.py
@@ -5,7 +5,7 @@ from website.project.model import User
 from admin.base.views import GuidFormView, GuidView
 from admin.users.templatetags.user_extras import reverse_user
 from .serializers import serialize_user
-from .forms import OSFUserForm
+from .models import OSFUserForm, OSFUser
 
 
 class UserFormView(GuidFormView):
@@ -23,18 +23,32 @@ class OSFUserFormView(FormView):
 
     def __init__(self):
         self.guid = None
+        self.model = None
         super(OSFUserFormView, self).__init__()
 
     def get_context_data(self, **kwargs):
         self.guid = self.kwargs.get('guid', None)
+        try:
+            self.model = OSFUser.objects.get(osf_id=self.guid)
+        except OSFUser.DoesNotExist:
+            self.model = OSFUser(osf_id=self.guid)
+            self.model.save()
         kwargs.setdefault('osf_id', self.guid)
         return super(OSFUserFormView, self).get_context_data(**kwargs)
-    #
-    # def get_form(self, form_class=None):
+
+    def get_form(self, form_class=None):
+        return self.form_class(instance=self.model, **self.get_form_kwargs())
 
     def form_valid(self, form):
         self.guid = form.cleaned_data.get('osf_id').strip()
-        print form.cleaned_data.get('notes').strip()
+        notes = form.cleaned_data.get('notes').strip()
+        try:
+            osf_user = OSFUser.objects.get(osf_id=self.guid)
+            f = OSFUserForm(self.request.POST, instance=osf_user)
+            f.save()
+        except:
+            new_user = OSFUser(osf_id=self.guid, notes=notes)
+            new_user.save()
         return super(OSFUserFormView, self).form_valid(form)
 
     @property
