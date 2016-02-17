5322f8258cab3dd1d01ce1c69d2d9fca815dedcf
Steven Loria <sloria1@gmail.com>
2016-02-17 09:23:03 -0500

    Remove maintenance alert

--- website/static/js/pages/base-page.js
+++ website/static/js/pages/base-page.js
@@ -110,26 +110,4 @@ $(function() {
         $osf.initializeResponsiveAffix();
     }
     new NavbarControl('.osf-nav-wrapper');
-
-    // Maintenance alert
-    var maintenancePersistKey = 'maintenance';
-    var $maintenance = $('#maintenance').on('closed.bs.alert', function() {
-        $.cookie(maintenancePersistKey, '0', { expires: 1, path: '/'});
-    })
-    var dismissed = $.cookie(maintenancePersistKey) === '0';
-    if (!dismissed) {
-        $maintenance.show();
-    }
-    // Localize mainteance period datetimes
-    var startMaintenanceUTC = moment.utc({year: 2016, month: 1, hour: 3, day: 17});
-    var endMaintenanceUTC = moment.utc({year: 2016, month: 1, hour: 11, day: 17});
-    var startMaintenance = moment(startMaintenanceUTC.toDate());
-    var endMaintenance = moment(endMaintenanceUTC.toDate());
-    $('#maintenanceTime').html(
-        '<strong>' +
-        startMaintenance.format('lll') +
-            '-' +
-                endMaintenance.format('lll') + '</strong>' +
-                    ' (' + startMaintenance.format('ZZ') + ' UTC)');
-    // END Maintenance alert
 });
