e1d1c4c952bd5d1210fda72408e70e3c96826ce5
Gage Gaskins <tgg8ec@virginia.edu>
2016-02-18 16:54:01 -0500

    Visit and pageview schemas complete. Keen session and id cookies complete

--- webpack.common.config.js
+++ webpack.common.config.js
@@ -133,7 +133,8 @@ var resolve = {
         'tests': staticPath('js/tests'),
         // GASP Items not defined as main in its package.json
         'TweenLite' : nodePath('gsap/src/minified/TweenLite.min.js'),
-        'EasePack' : nodePath('gsap/src/minified/easing/EasePack.min.js')
+        'EasePack' : nodePath('gsap/src/minified/easing/EasePack.min.js'),
+        'uuid': nodePath('uuid')
     }
 };
 
