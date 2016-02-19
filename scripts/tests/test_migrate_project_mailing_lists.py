eeb191704ff7090b5609e1ac5b73d16742b2e5d9
Matt Frazier <maf7sm@virginia.edu>
2016-02-18 23:09:38 -0500

    Ensure created models removed after testing

--- scripts/tests/test_migrate_project_mailing_lists.py
+++ scripts/tests/test_migrate_project_mailing_lists.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from nose.tools import *  # noqa
 
+from website.models import Node, NotificationSubscription
 from tests.base import OsfTestCase
 from tests.factories import DashboardFactory
 from tests.factories import NodeFactory
@@ -12,25 +13,34 @@ from scripts.migrate_project_mailing_lists import migrate
 
 class TestMigrateMailingLists(OsfTestCase):
 
+    def tearDown(self):
+        # Prevent errors
+        Node.remove()
+        NotificationSubscription.remove()
+
     def test_migrate_node(self):
         node = ProjectFactory()
+        NotificationSubscription.remove()
         migrate(dry_run=False)
         assert_true(node.mailing_enabled)
 
     def test_migrate_node_with_parent(self):
         node = ProjectFactory(parent=NodeFactory())
+        NotificationSubscription.remove()
         migrate(dry_run=False)
         assert_true(node.mailing_enabled)
         
 
     def test_migrate_registration(self):
         node = RegistrationFactory()
+        NotificationSubscription.remove()
         migrate(dry_run=False)
         assert_false(node.mailing_enabled)
 
 
     def test_migrate_dashboard(self):
         node = DashboardFactory()
+        NotificationSubscription.remove()
         migrate(dry_run=False)
         assert_false(node.mailing_enabled)
 
