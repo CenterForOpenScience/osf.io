72601bbf0c32f300379f6e170a42aea1e8c4d30d
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-02-19 16:21:20 -0500

    Run entire migration in TokuTransaction and raise error at end if dry.  Add more detailed logger info.

--- scripts/migration/migrate_backrefs_between_nodes_and_nodelogs.py
+++ scripts/migration/migrate_backrefs_between_nodes_and_nodelogs.py
@@ -30,31 +30,34 @@ def main(dry=True):
     errored_logs = []
     for log in node_logs:
         count += 1
-        with TokuTransaction():
-            original_node = lookup_node(log.params['node'])
-            log.original_node = original_node
-            log.node = lookup_node(log._backrefs['logged']['node']['logs'][0])
-            if not dry:
-                try:
-                    log.save()
-                    logger.info('{}/{} Log {} "original_node = {} and node = {}" added'.format(count, total_log_count, log._id, log.original_node._id, log.node._id))
-                except KeyError as error:
-                    logger.error('Could not migrate log due to error')
-                    logger.exception(error)
-                    errored_logs.append(log)
+        original_node = lookup_node(log.params['node'])
+        log.original_node = original_node
+        log.node = lookup_node(log._backrefs['logged']['node']['logs'][0])
+        try:
+            log.save()
+            logger.info('{}/{} Log {} "original_node = {} and node = {}" added'.format(count, total_log_count, log._id, log.original_node._id, log.node._id))
+        except KeyError as error:
+            logger.error('Could not migrate log due to error')
+            logger.exception(error)
+            errored_logs.append(log)
 
-            errored_clones = []
-            logger.warn('Cloning log {} for nodes in backrefs'.format(log._id))
-            for node in log._backrefs['logged']['node']['logs'][1:]:
-                clone = log.clone_node_log(node)
-                clone.original_node = original_node
-                try:
-                    clone.save()
-                    logger.info('Log {} cloned for node {}. New log is {}'.format(log._id, node, clone._id))
-                except KeyError as error:
-                    logger.error('Could not copy node log due to error')
-                    logger.exception(error)
-                    errored_clones.append([log, clone])
+        errored_clones = []
+        backref_total = len(log._backrefs['logged']['node']['logs']) - 1
+        backref_count = 0
+        for node in log._backrefs['logged']['node']['logs'][1:]:
+            backref_count += 1
+            clone = log.clone_node_log(node)
+            clone.original_node = original_node
+            try:
+                clone.save()
+                logger.info('{}/{} {}/{} Log {} cloned for node {}. New log is {}'.format(count, total_log_count, backref_count, backref_total, log._id, node, clone._id))
+            except KeyError as error:
+                logger.error('Could not copy node log due to error')
+                logger.exception(error)
+                errored_clones.append([log, clone])
+
+    if dry:
+        raise RuntimeError('Dry run -- transaction rolled back')
 
 
 if __name__ == '__main__':
@@ -62,4 +65,5 @@ if __name__ == '__main__':
     dry_run = 'dry' in sys.argv
     if not dry_run:
         script_utils.add_file_logger(logger, __file__)
-    main(dry=dry_run)
+    with TokuTransaction():
+        main(dry=dry_run)
