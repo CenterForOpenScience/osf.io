5ecdeef02752fde01866e63973300963d9fd0350
Bryan Gorges <bgorges@cos.io>
2016-02-19 14:05:40 -0500

    Add registrations

--- admin/nodes/serializers.py
+++ admin/nodes/serializers.py
@@ -6,12 +6,21 @@ from admin.users.serializers import serialize_simple_node
 
 def serialize_node(node):
     user_list = {key: reduce_permissions(value) for key, value in node.permissions.iteritems()}
+    embargo = node.embargo
+    if embargo is not None:
+        embargo = node.embargo.end_date
+
     return {
         'id': node._id,
         'title': node.title,
         'public': node.is_public,
         'parent': node.parent_id,
+        'root': node.root._id,
+        'is_registration': node.is_registration,
+        'date_created': node.date_created,
         'contributors': map(serialize_simple_user, user_list.iteritems()),
+        'retraction': node.retraction,
+        'embargo': embargo,
         'children': map(serialize_simple_node, node.nodes),
     }
 
