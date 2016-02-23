2ea0168c88758a6de8db9574d93e2e53078f209d
Henrique Moco <hmoco94@vt.edu>
2016-02-23 16:56:27 -0500

    move tag add/remove logic to the model

--- website/addons/osfstorage/views.py
+++ website/addons/osfstorage/views.py
@@ -249,15 +249,7 @@ def osfstorage_download(file_node, payload, node_addon, **kwargs):
 @decorators.autoload_filenode(must_be='file')
 def osfstorage_add_tag(file_node, **kwargs):
     data = request.get_json()
-    tag = data['tag']
-    if tag not in file_node.tags and not file_node.node.is_registration:
-        new_tag = Tag.load(tag)
-        if not new_tag:
-            new_tag = Tag(_id=tag)
-        new_tag.save()
-        file_node.tags.append(new_tag)
-        file_node.add_tag_log(NodeLog.FILE_TAG_ADDED, tag, kwargs['auth'])
-        file_node.save()
+    if file_node.add_tag(data['tag'], kwargs['auth']):
         return {'status': 'success'}, httplib.OK
     return {'status': 'failure'}, httplib.BAD_REQUEST
 
@@ -265,12 +257,6 @@ def osfstorage_add_tag(file_node, **kwargs):
 @decorators.autoload_filenode(must_be='file')
 def osfstorage_remove_tag(file_node, **kwargs):
     data = request.get_json()
-    tag = data['tag']
-    tag_string = tag
-    tag = Tag.load(tag)
-    if tag and tag in file_node.tags and not file_node.node.is_registration:
-        file_node.tags.remove(tag)
-        file_node.add_tag_log(NodeLog.FILE_TAG_REMOVED, tag_string, kwargs['auth'])
-        file_node.save()
+    if file_node.remove_tag(data['tag'], kwargs['auth']):
         return {'status': 'success'}, httplib.OK
     return {'status': 'failure'}, httplib.BAD_REQUEST
