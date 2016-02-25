e0639233efc4a32b7d7f23eaa5cf445ab6d86e13
Longze Chen <cs.longze.chen@gmail.com>
2016-02-25 15:39:58 -0500

    Fix matplotlib import order to use backends; add comments to requirements for metrics and release.

--- scripts/analytics/tasks.py
+++ scripts/analytics/tasks.py
@@ -4,16 +4,15 @@ from framework.tasks import app as celery_app
 
 from website.app import init_app
 
-from scripts.analytics import (
-    logs, addons, comments, folders, links, watch, email_invites,
-    permissions, profile, benchmarks
-)
-
 
 @celery_app.task(name='scripts.analytics.tasks')
 def analytics():
     matplotlib.use('Agg')
     init_app()
+    from scripts.analytics import (
+        logs, addons, comments, folders, links, watch, email_invites,
+        permissions, profile, benchmarks
+    )
     modules = (
         logs, addons, comments, folders, links, watch, email_invites,
         permissions, profile, benchmarks
