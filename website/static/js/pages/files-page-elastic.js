ade85a94cfc01929c02e1c108cd03457c0f1e652
Brandon Purvis <bcp7cc@virginia.edu>
2016-02-26 13:28:38 -0500

    Update the hight to displayed elements.

--- website/static/js/pages/files-page-elastic.js
+++ website/static/js/pages/files-page-elastic.js
@@ -60,6 +60,7 @@ $(document).ready(function(){
                             }
                         }
                     }
+                    tb.calculateHeight();
                     tb.refreshRange(0);
                 };
 
