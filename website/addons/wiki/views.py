c87ab7adeb76ced60c4f07285dc8cc240dae4ed2
erinspace <erin.braswell@gmail.com>
2016-02-26 15:02:13 -0500

    simplify more variable setting, rm BeautifulSoup
    the more variable was being set using len of wiki_html correctly.
    
    Beautiful soup was being used to render the html before the javascript
    re-renders everything anyhow, so just removed this superflous call to
    the library.

--- website/addons/wiki/views.py
+++ website/addons/wiki/views.py
@@ -140,16 +140,13 @@ def wiki_widget(**kwargs):
     wiki = node.get_addon('wiki')
     wiki_page = node.get_wiki_page('home')
 
-    more = True if len(node.wiki_pages_current.keys()) >= 2 or len(wiki_html) > 400 else False
+    more = True if len(node.wiki_pages_current.keys()) >= 2 else False
 
     use_python_render = False
     if wiki_page and wiki_page.html(node):
         wiki_html = wiki_page.html(node)
         if len(wiki_html) > 400:
-            wiki_html = BeautifulSoup(wiki_html[:400] + '...', 'html.parser')
-        else:
-            wiki_html = BeautifulSoup(wiki_html)
-
+            more = True
         use_python_render = wiki_page.rendered_before_update
     else:
         wiki_html = None
