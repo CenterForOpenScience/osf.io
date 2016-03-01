08aa975b100b1dbf8c9594a2e57368ad866f98a4
Chris Wisecarver <cwisecarver@cos.io>
2016-03-01 11:25:38 -0500

    Move from celery task to regular-old function

--- api/caching/tasks.py
+++ api/caching/tasks.py
@@ -1,24 +1,18 @@
 import urlparse
 
-import celery
 import requests
 from celery.utils.log import get_task_logger
+
 from api.base import settings
 
-from framework.tasks import app as celery_app
-
 logger = get_task_logger(__name__)
-
-class VarnishTask(celery.Task):
-    abstract = True
-    max_retries = 5
 
 def get_varnish_servers():
     #  TODO: this should get the varnish servers from HAProxy or a setting
     return settings.VARNISH_SERVERS
 
-@celery_app.task(base=VarnishTask, name='caching_tasks.ban_url')
 def ban_url(url):
+    timeout = 0.5  # 500ms timeout for bans
     if settings.ENABLE_VARNISH:
         parsed_url = urlparse.urlparse(url)
 
@@ -29,7 +23,7 @@ def ban_url(url):
                 netloc=varnish_parsed_url.netloc,
                 path=parsed_url.path
             )
-            response = requests.request('BAN', ban_url, headers=dict(
+            response = requests.request('BAN', ban_url, timeout=timeout, headers=dict(
                 Host=parsed_url.hostname
             ))
             if not response.ok:
