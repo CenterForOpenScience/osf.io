8350a11540c4811e1b8160a64265f89aba8b2f39
Bryan Gorges <bgorges@cos.io>
2016-03-01 10:46:58 -0500

    Add tests

--- admin_tests/users/test_views.py
+++ admin_tests/users/test_views.py
@@ -1,11 +1,12 @@
 from django.test import RequestFactory
 from nose.tools import *  # flake8: noqa
+import mock
 
 from tests.base import AdminTestCase
-from tests.factories import UserFactory
+from tests.factories import UserFactory, AuthUserFactory
 from admin_tests.utilities import setup_view
 
-from admin.users.views import UserView
+from admin.users.views import UserView, remove_2_factor
 
 
 class TestUserView(AdminTestCase):
@@ -36,3 +37,39 @@ class TestUserView(AdminTestCase):
         view.object = temp_object
         res = view.get_context_data()
         assert_equal(res[UserView.context_object_name], temp_object)
+
+    @mock.patch('admin.users.views.User.delete_addon')
+    def test_remove_two_factor_get(self, mock_delete_addon):
+        user = UserFactory()
+        guid = user._id
+        request = RequestFactory().get('/fake_path')
+        remove_2_factor(request, guid)
+        assert_equal(mock_delete_addon.call_args_list, [])
+
+    @mock.patch('admin.users.views.User.delete_addon')
+    def test_remove_two_factor_post(self, mock_delete_addon):
+        user = UserFactory()
+        guid = user._id
+        request = RequestFactory().post('/fake_path')
+        remove_2_factor(request, guid)
+        mock_delete_addon.assert_called_with('twofactor')
+
+    @mock.patch('admin.users.views.User.delete_addon')
+    def test_remove_two_factor_delete(self, mock_delete_addon):
+        user = UserFactory()
+        guid = user._id
+        request = RequestFactory().delete('/fake_path')
+        remove_2_factor(request, guid)
+        mock_delete_addon.assert_called_with('twofactor')
+
+    def test_integration_delete_two_factor(self):
+        user = AuthUserFactory()
+        guid = user._id
+        user_addon = user.get_or_add_addon('twofactor')
+        assert_not_equal(user_addon, None)
+        user_settings = user.get_addon('twofactor')
+        assert_not_equal(user_settings, None)
+        request = RequestFactory().post('/fake_path')
+        remove_2_factor(request, guid)
+        post_addon = user.get_addon('twofactor')
+        assert_equal(post_addon, None)
