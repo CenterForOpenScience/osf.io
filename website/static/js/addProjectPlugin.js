fc8d7ae26c77c1f89bd7f5d3ca949ec18bdc20bd
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-03-02 11:06:07 -0500

    Add event tracking to add project plugin.

--- website/static/js/addProjectPlugin.js
+++ website/static/js/addProjectPlugin.js
@@ -85,8 +85,11 @@ var AddProject = {
         var templates = {
             form : m('.modal-content', [
                 m('.modal-header', [
-                    m('button.close[data-dismiss="modal"][aria-label="Close"]',{ onclick : ctrl.reset}, [
-                        m('span[aria-hidden="true"]','×'),
+                    m('button.close[data-dismiss="modal"][aria-label="Close"]',{ onclick : function() {
+                        ctrl.reset();
+                        $osf.trackClick('dashboard', 'add-project', 'click-close-add-project-modal');
+                    }}, [
+                        m('span[aria-hidden="true"]','×')
                     ]),
                     m('h3.modal-title', 'Add new project')
                 ]),
@@ -102,11 +105,15 @@ var AddProject = {
                                     ctrl.newProjectName($(this).val());
                                     ctrl.checkValid();
                                 },
+                                onchange: function() {
+                                    $osf.trackClick('dashboard', 'add-project', 'type-project-name');
+                                },
                                 value : ctrl.newProjectName()
                             })
                         ]),
                         m('.text-muted.pointer', { onclick : function(){
                             ctrl.showMore(!ctrl.showMore());
+                            $osf.trackClick('dashboard', 'add-project', 'show-more-or-less');
                         }},[
                             ctrl.showMore() ? m('i.fa.fa-caret-down', { style: 'width: 10px;'}) : m('i.fa.fa-caret-right', { style: 'width: 10px;'}),
                             ' More (description, type)'
@@ -115,7 +122,10 @@ var AddProject = {
                             m('.form-group.m-v-sm', [
                                 m('label[for="projectDesc].f-w-lg.text-bigger', 'Project Description'),
                                 m('textarea.form-control.noresize', {
-                                    onchange: m.withAttr('value', ctrl.newProjectDesc),
+                                    onchange: function() {
+                                        m.withAttr('value', ctrl.newProjectDesc);
+                                        $osf.trackClick('dashboard', 'add-project', 'type-project-description');
+                                    },
                                     value : ctrl.newProjectDesc()
                                 })
                             ]),
@@ -127,7 +137,10 @@ var AddProject = {
                                         name: 'projectCategory',
                                         value: cat.value,
                                         checked: ctrl.newProjectCategory() === cat.value,
-                                        onchange : m.withAttr('value', ctrl.newProjectCategory)
+                                        onchange : function() {
+                                            m.withAttr('value', ctrl.newProjectCategory);
+                                            $osf.trackClick('dashboard', 'add-project', 'select-project-category');
+                                        }
                                     }), cat.display_name || m('i.text-muted', '(Empty category)') ]));
 
                                 })
@@ -136,16 +149,22 @@ var AddProject = {
                     ])
                 ]),
                 m('.modal-footer', [
-                    m('button[type="button"].btn.btn-default[data-dismiss="modal"]', { onclick : ctrl.reset},  'Cancel'),
-                    ctrl.isValid() ? m('button[type="button"].btn.btn-success', { onclick : ctrl.add },'Add') : m('button[type="button"].btn.btn-success[disabled]','Add')
+                    m('button[type="button"].btn.btn-default[data-dismiss="modal"]', { onclick : function() {
+                        ctrl.reset();
+                        $osf.trackClick('dashboard', 'add-project', 'click-cancel-button');
+                    }},  'Cancel'),
+                    ctrl.isValid() ? m('button[type="button"].btn.btn-success', { onclick : function() {
+                        ctrl.add();
+                        $osf.trackClick('dashboard', 'add-project', 'click-add-button');
+                    }},'Add') : m('button[type="button"].btn.btn-success[disabled]','Add')
                 ])
             ]),
             processing : m('.modal-content',
                 m('.modal-content',
                     m('.modal-header', [
                         m('button.close[data-dismiss="modal"][aria-label="Close"]',{ onclick : ctrl.reset}, [
-                            m('span[aria-hidden="true"]','×'),
-                        ]),
+                            m('span[aria-hidden="true"]','×')
+                        ])
                     ]),
                     m('.modal-body.text-left', [
                             m('.add-project-processing', 'Saving your project...')
@@ -156,8 +175,11 @@ var AddProject = {
             success : m('.modal-content', [
                 m('.modal-content',
                     m('.modal-body.text-left', [
-                            m('button.close[data-dismiss="modal"][aria-label="Close"]',{ onclick : ctrl.reset}, [
-                                m('span[aria-hidden="true"]','×'),
+                            m('button.close[data-dismiss="modal"][aria-label="Close"]',{ onclick : function() {
+                                ctrl.reset();
+                                $osf.trackClick('dashboard', 'add-project', 'click-close-success-modal');
+                            }}, [
+                                m('span[aria-hidden="true"]','×')
                             ]),
                             m('h4.add-project-success.text-success', 'Project created successfully!')
                         ]
@@ -167,24 +189,32 @@ var AddProject = {
                             onclick : function() {
                                 ctrl.reset();
                                 ctrl.options.stayCallback.call(ctrl); // results are at ctrl.saveResult
+                                $osf.trackClick('dashboard', 'add-project', 'keep-working-here');
                             }
                         },  'Keep working here'),
-                        m('a.btn.btn-success', { href : ctrl.goToProjectLink() },'Go to new project')
+                        m('a.btn.btn-success', { href : ctrl.goToProjectLink(), onclick: function() {
+                             $osf.trackClick('dashboard', 'add-project', 'go-to-new-project');
+                        }},'Go to new project')
                     ])
                 )
             ]),
             error : m('.modal-content', [
                 m('.modal-content',
                     m('.modal-body.text-left', [
-                            m('button.close[data-dismiss="modal"][aria-label="Close"]',{ onclick : ctrl.reset}, [
-                                m('span[aria-hidden="true"]','×'),
+                            m('button.close[data-dismiss="modal"][aria-label="Close"]',{ onclick : function() {
+                                ctrl.reset();
+                                $osf.trackClick('dashboard', 'add-project', 'close-couldn\'t-create-your-project');
+                                }}, [
+                                m('span[aria-hidden="true"]','×')
                             ]),
                             m('h4.add-project-error.text-danger', 'Couldn\'t create your project'),
                             m('p', ctrl.errorMessage[ctrl.errorMessageType()])
                         ]
                     ),
                     m('.modal-footer', [
-                        m('button[type="button"].btn.btn-default[data-dismiss="modal"]',  'OK')
+                        m('button[type="button"].btn.btn-default[data-dismiss="modal"]', {onclick: function() {
+                            $osf.trackClick('dashboard', 'add-project', 'click-OK-couldn\'t-create-your-project');
+                        }},  'OK')
                     ])
                 )
             ])
