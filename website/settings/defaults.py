af1def269cb809ad9676d9cf666ab5f3f828b837
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-03-04 16:15:44 -0500

    Restore POPULAR_LINKS_NODE and NEW_AND_NOTEWORTHY_LINKS_NODE to None.  Raise error if these values are not set.

--- website/settings/defaults.py
+++ website/settings/defaults.py
@@ -230,8 +230,8 @@ ALL_MY_PROJECTS_NAME = 'All my projects'
 ALL_MY_REGISTRATIONS_NAME = 'All my registrations'
 
 # Most Popular and New and Noteworthy Nodes
-POPULAR_LINKS_NODE = '6824t'  # TODO Override in local.py in production.
-NEW_AND_NOTEWORTHY_LINKS_NODE = '9gyxb'  # TODO Override in local.py in production.
+POPULAR_LINKS_NODE = None  # TODO Override in local.py in production.
+NEW_AND_NOTEWORTHY_LINKS_NODE = None  # TODO Override in local.py in production.
 
 QA_USER_IDS = []  # TODO Override in local.py in production.
 URL_BASE = None  # TODO Override with LOCAL, STAGING, STAGING2, or PRODUCTION in local.py.
