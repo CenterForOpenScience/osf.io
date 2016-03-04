10c6112dd343901b502c31655a001e612ed6e441
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-03-04 12:17:34 -0500

    Add case for when there are no node backrefs on logs.  Again, this whole method will change when eliminating backrefs from nodelogs is merged.

--- api/logs/permissions.py
+++ api/logs/permissions.py
@@ -12,8 +12,14 @@ class ContributorOrPublicForLogs(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         assert isinstance(obj, (NodeLog)), 'obj must be a NodeLog, got {}'.format(obj)
 
-        for node_id in obj._backrefs['logged']['node']['logs']:
-            node = get_object_or_error(Node, node_id, display_name='node')
-            if ContributorOrPublic().has_object_permission(request, view, node):
+        if obj._backrefs.get('logged'):
+            for node_id in obj._backrefs['logged']['node']['logs']:
+                node = get_object_or_error(Node, node_id, display_name='node')
+                if ContributorOrPublic().has_object_permission(request, view, node):
+                    return True
+
+        if getattr(obj, 'node'):
+            if ContributorOrPublic().has_object_permission(request, view, obj.node):
                 return True
+
         return False
