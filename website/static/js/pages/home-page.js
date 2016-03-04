3efb3a20e069a59c40c60f3bb2501e42761afcfd
Caner Uguz <caneruguz@gmail.com>
2016-03-04 15:22:39 -0500

    Fix alignment of my projects with add project button [skip ci]

--- website/static/js/pages/home-page.js
+++ website/static/js/pages/home-page.js
@@ -55,7 +55,7 @@ $(document).ready(function(){
                             m('.col-xs-6 col-md-5 col-md-offset-1 col-lg-4 col-lg-offset-2', m('h3', 'My Projects')),
                             m('.col-xs-6 col-md-5 col-lg-4', m('.pull-right',
                                 m.component(AddProject, {
-                                    buttonTemplate : m('button.btn.btn-success.m-t-lg[data-toggle="modal"][data-target="#addProjectFromHome"]', 'Create New Project'),
+                                    buttonTemplate : m('button.btn.btn-success.m-t-md[data-toggle="modal"][data-target="#addProjectFromHome"]', 'Create New Project'),
                                     modalID : 'addProjectFromHome',
                                     categoryList : ctrl.categoryList,
                                     stayCallback : function _stayCallback_inPanel() {
