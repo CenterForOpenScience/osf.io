a2534ef01af3e309b4b9386ae93ffc3d396469d2
icereval <icereval@gmail.com>
2016-03-06 14:02:36 -0500

    use framework for middleware in django and flask apps

--- api/base/middleware.py
+++ api/base/middleware.py
@@ -1,70 +1,63 @@
-from pymongo.errors import OperationFailure
 from raven.contrib.django.raven_compat.models import sentry_exception_handler
 
+from framework.mongo.handlers import (
+    connection_before_request,
+    connection_teardown_request
+)
 from framework.tasks.handlers import (
     celery_before_request,
     celery_teardown_request
 )
-from framework.transactions import commands, messages, utils
+from framework.transactions.handlers import (
+    transaction_before_request,
+    transaction_after_request,
+    transaction_teardown_request
+)
 
 from .api_globals import api_globals
 
-from api.base import settings
+
+class MongoConnectionMiddleware(object):
+    """MongoDB Connection middleware."""
+
+    def process_request(self, request):
+        connection_before_request()
+
+    def process_response(self, request, response):
+        connection_teardown_request()
+        return response
+
+class CeleryTaskMiddleware(object):
+    """Celery Task middleware."""
+
+    def process_request(self, request):
+        celery_before_request()
+
+    def process_response(self, request, response):
+        celery_teardown_request()
+        return response
 
 
-# TODO: Verify that a transaction is being created for every
-# individual request.
-class TokuTransactionsMiddleware(object):
-    """TokuMX transaction middleware."""
+class TokuTransactionMiddleware(object):
+    """TokuMX Transaction middleware."""
 
     def process_request(self, request):
         """Begin a transaction if one doesn't already exist."""
-        try:
-            commands.begin()
-        except OperationFailure as err:
-            message = utils.get_error_message(err)
-            if messages.TRANSACTION_EXISTS_ERROR not in message:
-                raise err
+        transaction_before_request()
 
     def process_exception(self, request, exception):
         """If an exception occurs, rollback the current transaction
         if it exists.
         """
         sentry_exception_handler(request=request)
-        try:
-            commands.rollback()
-        except OperationFailure as err:
-            message = utils.get_error_message(err)
-            if messages.NO_TRANSACTION_ERROR not in message:
-                raise
-        commands.disconnect()
+        transaction_teardown_request(error=True)
         return None
 
     def process_response(self, request, response):
         """Commit transaction if it exists, rolling back in an
         exception occurs.
         """
-
-        try:
-            if response.status_code >= 400:
-                commands.rollback()
-            else:
-                commands.commit()
-        except OperationFailure as err:
-            message = utils.get_error_message(err)
-            if messages.NO_TRANSACTION_TO_COMMIT_ERROR not in message:
-                if settings.DEBUG_TRANSACTIONS:
-                    pass
-                else:
-                    raise err
-        except Exception as err:
-            try:
-                commands.rollback()
-            except OperationFailure:
-                pass
-            else:
-                raise err
-        commands.disconnect()
+        transaction_after_request(response, base_status_code_error=400)
         return response
 
 
@@ -74,7 +67,6 @@ class DjangoGlobalMiddleware(object):
     """
     def process_request(self, request):
         api_globals.request = request
-        celery_before_request()
 
     def process_exception(self, request, exception):
         sentry_exception_handler(request=request)
@@ -82,6 +74,5 @@ class DjangoGlobalMiddleware(object):
         return None
 
     def process_response(self, request, response):
-        celery_teardown_request()
         api_globals.request = None
         return response
