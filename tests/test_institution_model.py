9b5026969c8c315f5853daa862e3a61d8ab9cb10
Henrique Moco <hmoco94@vt.edu>
2016-03-10 11:58:44 -0500

    fix tests

--- tests/test_institution_model.py
+++ tests/test_institution_model.py
@@ -4,6 +4,7 @@ from tests.factories import InstitutionFactory
 
 class TestInstitution(OsfTestCase):
     def setUp(self):
+        super(TestInstitution, self).setUp()
         self.institution = InstitutionFactory()
 
     def test_institution_save_only_changes_mapped_fields_on_node(self):
@@ -26,15 +27,16 @@ class TestInstitution(OsfTestCase):
         self.institution.piwik_site_id = new['piwik_site_id']
         self.institution.save()
 
+        node.reload()
         #assert changes
-        assert_equal(self.node.title, new['title'])
-        assert_equal(self.node.institution_logo_name, new['institution_logo_name'])
+        assert_equal(node.title, new['title'])
+        assert_equal(node.institution_logo_name, new['institution_logo_name'])
 
         #assert remained same
-        assert_equal(self.node.system_tags, old['system_tags'])
-        assert_not_equal(self.node.system_tags, new['system_tags'])
-        assert_equal(self.node.piwik_site_id, old['piwik_site_id'])
-        assert_not_equal(self.node.piwik_site_id, new['piwik_site_id'])
+        assert_equal(node.system_tags, old['system_tags'])
+        assert_not_equal(node.system_tags, new['system_tags'])
+        assert_equal(node.piwik_site_id, old['piwik_site_id'])
+        assert_not_equal(node.piwik_site_id, new['piwik_site_id'])
 
 
     def test_institution_mappings(self):
