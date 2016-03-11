fad5ac0e3da1d43985bfab1441a88ef506302f22
Zak <zik3qe@virginia.edu>
2016-03-11 14:44:50 -0500

    refactor tests for user reg

--- admin_tests/common_auth/test_views.py
+++ admin_tests/common_auth/test_views.py
@@ -1,48 +1,50 @@
 from django.test import Client, TestCase
 from django.test.client import RequestFactory
 from django.contrib.auth.models import User, Group
+from models import MyUser
 from django.core.urlresolvers import reverse
 from forms import CustomUserRegistrationForm
-from common_auth.views import register
+from views import register
 
 
 class UserRegFormTestCase(TestCase):
     def test_user_reg_form_success(self):
-        group_perms = Group.objects.filter(name="prereg_group")
+        group_perms = Group.objects.all()
         form_data = {
             'email': 'example@example.com',
             'first_name': 'Zak',
             'last_name': 'K',
             'password1': 'password',
-            'password2', 'password',
-            'group_perms': group_perms,
+            'password2': 'password',
         }
         form = CustomUserRegistrationForm(form_data)
         self.assertTrue(form.is_valid())
 
 
     def test_user_reg_form_failure(self):
-        # Every field is required, password length should be >= 5
+        # Every field is required, password length should be >= 5 (need Django 1.9 to use AUTH_PASSWORD_VALIDATORS)
         form_data = {
             'email': '',
             'first_name': '',
             'last_name': '',
             'password1': 'pass',
-            'password2', 'pass',
-            'group_perms': '',
+            'password2': 'pass',
         }
         form = CustomUserRegistrationForm(form_data)
         self.assertFalse(form.is_valid())
 
 class UserRegSaveTestCase(TestCase):
     def setUp(self):
-        self.factory = RequestFactory()
-        self.user = User.objects.create_user(username='Zak', email='zak@zak.com',
-            password='secret', is_staff=True)
+        self.client = Client()
+        user = MyUser.objects.create_user(email='zak@zak.com',
+            password='secret')
+        user.is_staff = True
+        user.save()
+
 
     def test_registration(self):
-        request.user = self.user
-        request = self.factory.post('/register')
+        # request.user = self.user
+        # request = self.factory.post('/register')
         group_perms = Group.objects.filter(name="prereg_group")
 
         post_data = {
@@ -50,11 +52,9 @@ class UserRegSaveTestCase(TestCase):
             'first_name': 'Zak',
             'last_name': 'K',
             'password1': 'password',
-            'password2', 'password',
-            'group_perms': group_perms,
+            'password2': 'password',
         }
 
-        response = self.client.post('/register', post_data)
-        self.assertEqual(response.status_code, 200)
-
-
+        self.client.login(email='zak@zak.com', password='secret')
+        response = self.client.post(reverse('auth:register'), post_data)
+        self.assertEqual(response.status_code, 302)
