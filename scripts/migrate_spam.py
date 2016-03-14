cbf1e6cb8615b1017c887ba504fe29a2cff634ff
Bryan Gorges <bgorges@cos.io>
2016-03-14 12:16:30 -0400

    Start to test migration for spam
    [skip ci]

--- scripts/migrate_spam.py
+++ scripts/migrate_spam.py
@@ -1,4 +1,13 @@
+import logging
+import sys
+
+from modularodm import Q
+from website.app import init_app
 from website.project.model import Comment
+from framework.transactions.context import TokuTransaction
+from scripts import utils as script_utils
+
+logger = logging.getLogger(__name__)
 
 
 def migrate_comment(comment):
@@ -23,10 +32,40 @@ def migrate_comment(comment):
     comment.save()
 
 
+def migrate_status(records, dry=True):
+    pass
+
+
+def migrate_latest(records, dry=True):
+    pass
+
+
+def get_no_status_targets():
+    return [c for c in Comment.find(Q('spam_status', 'eq', None))]
+
+
+def get_no_latest_targets():
+    query = (
+        Q('latest_report', 'eq', None) &
+        Q('spam_status', 'ne', Comment.UNKNOWN) &
+        Q('spam_status', 'ne', None)
+    )
+    return [c for c in Comment.find(query)]
+
+
 def main():
-    comments = Comment.find()
-    for comment in comments:
-        migrate_comment(comment)
+    dry_run = False
+    status = True
+    latest = True
+    if '--dry' in sys.argv:
+        dry_run = True
+    if not dry_run:
+        script_utils.add_file_logger(logger, __file__)
+    if '--no_status' in sys.argv:
+        status = False
+    if '--no_latest' in sys.argv:
+        latest = False
+    init_app(set_backends=True, routes=False)
 
 
 if __name__ == '__main__':
