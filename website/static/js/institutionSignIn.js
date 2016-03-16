42cb0e599ac05fb1d9c86b543c57301e723da26a
Henrique Moco <hmoco94@vt.edu>
2016-03-16 11:45:44 -0400

    filter out the items that don't have an auth_url

--- website/static/js/institutionSignIn.js
+++ website/static/js/institutionSignIn.js
@@ -18,12 +18,18 @@ var ViewModel = function() {
                 isCors: true
             }
         ).done(function (response) {
-            self.instNames(response.data.map(function(item){
-                var name = item.attributes.name;
-                self.insts[name] = item.attributes.auth_url + '&target=' + encodeURIComponent(window.contextVars.institution_redirect);
-                self.insts[item.id] = name;
-                return name;
-            }));
+            self.instNames(
+                response.data.map(function(item){
+                    var name = item.attributes.name;
+                    self.insts[name] = item.attributes.auth_url + '&target=' + encodeURIComponent(window.contextVars.institution_redirect);
+                    self.insts[item.id] = name;
+                    if (item.attributes.auth_url){
+                        return name;
+                    }
+                }).filter(function (item) {
+                   return typeof item !== 'undefined';
+                })
+            );
             var inst_redirect = decodeURIComponent(decodeURIComponent(window.contextVars.institution_redirect));
             if (inst_redirect){
                 var inst_id = inst_redirect.split('institutions')[1];
