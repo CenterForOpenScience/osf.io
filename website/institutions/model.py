b771e1e8c530ad1916e513fea3eb651c2b5e099d
Henrique Moco <hmoco94@vt.edu>
2016-03-17 11:23:52 -0400

    fix img paths

--- website/institutions/model.py
+++ website/institutions/model.py
@@ -148,13 +148,13 @@ class Institution(object):
     @property
     def logo_path(self):
         if self.logo_name:
-            return '/static/img/institutions/{}/'.format(self.logo_name)
+            return '/static/img/institutions/{}'.format(self.logo_name)
         else:
             return None
 
     @property
     def banner_path(self):
         if self.banner_name:
-            return '/static/img/institutions/{}/'.format(self.banner_name)
+            return '/static/img/institutions/{}'.format(self.banner_name)
         else:
             return None
