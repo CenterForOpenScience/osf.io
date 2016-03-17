a95bca5c13b20ff1e262883fa4150f323c4fe3c9
Brian J. Geiger <bgeiger@pobox.com>
2016-03-17 14:24:02 -0400

    Add test for missing embedded parent

--- api_tests/nodes/views/test_node_embeds.py
+++ api_tests/nodes/views/test_node_embeds.py
@@ -47,6 +47,13 @@ class TestNodeEmbeds(ApiTestCase):
         embeds = res.json['data']['embeds']
         assert_equal(embeds['parent']['data']['id'], self.root_node._id)
 
+    def test_embed_no_parent(self):
+        url = '/{0}nodes/{1}/?embed=parent'.format(API_BASE, self.root_node._id)
+
+        res = self.app.get(url, auth=self.user.auth)
+        data = res.json['data']
+        assert_not_in('embeds', data)
+
     def test_embed_contributors(self):
         url = '/{0}nodes/{1}/?embed=contributors'.format(API_BASE, self.child1._id)
 
