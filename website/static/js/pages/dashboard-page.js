726bdadd8361d4b4d6d62f106c7158c2a8c637fa
Caner Uguz <caneruguz@gmail.com>
2016-03-20 10:29:39 -0400

    Fix class name for error message after changed load indicator [skip ci]

--- website/static/js/pages/dashboard-page.js
+++ website/static/js/pages/dashboard-page.js
@@ -45,7 +45,7 @@ $(document).ready(function() {
 
     // Appears in 10 second if the spinner is still there.
     setTimeout(function(){
-        if($('#dashboard .spinner-loading-wrapper').length > 0) {
+        if($('#dashboard .ball-scale').length > 0) {
             $('#dashboard').append('<div class="text-danger text-center text-bigger">This is taking longer than normal. <br>  Try reloading the page. If the problem persist contact us at support@cos.io.</div>');
         }
     }, 10000);
