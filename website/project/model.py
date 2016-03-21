e8840482c3acb297d2c35baf6b10e6f406e721f5
Matt Frazier <maf7sm@virginia.edu>
2016-03-21 10:24:34 -0400

    Fix Q logic

--- website/project/model.py
+++ website/project/model.py
@@ -3399,13 +3399,13 @@ class Node(GuidStoredObject, AddonModelMixin, IdentifierMixin):
     @classmethod
     def find(cls, query=None, allow_institution=False, **kwargs):
         if not allow_institution:
-            query = (query & Q('institution_id', 'ne', None)) if query else Q('institution_id', 'ne', None)
+            query = (query & Q('institution_id', 'eq', None)) if query else Q('institution_id', 'eq', None)
         return super(Node, cls).find(query, **kwargs)
 
     @classmethod
     def find_one(cls, query=None, allow_institution=False, **kwargs):
         if not allow_institution:
-            query = (query & Q('institution_id', 'ne', None)) if query else Q('institution_id', 'ne', None)
+            query = (query & Q('institution_id', 'eq', None)) if query else Q('institution_id', 'eq', None)
         return super(Node, cls).find_one(query, **kwargs)
 
     @classmethod
