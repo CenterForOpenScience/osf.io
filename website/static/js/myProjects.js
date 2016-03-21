ca67c134dbd9adc70b036a9a8ea0cca2af2bd65f
Caner Uguz <caneruguz@gmail.com>
2016-03-21 13:59:13 -0400

    Add proper options to registrations api call - needs fix for the end point [skip ci]

--- website/static/js/myProjects.js
+++ website/static/js/myProjects.js
@@ -151,7 +151,7 @@ var MyProjects = {
         // Load 'All my Projects' and 'All my Registrations'
         self.systemCollections = options.systemCollections || [
             new LinkObject('collection', { path : 'users/me/nodes/', query : { 'related_counts' : 'children', 'embed' : 'contributors', 'filter[parent]' : 'null' }, systemCollection : 'nodes'}, 'All my projects'),
-            new LinkObject('collection', { path : 'users/me/registrations/', query : { 'related_counts' : 'children', 'embed' : 'contributors'}, systemCollection : 'registrations'}, 'All my registrations')
+            new LinkObject('collection', { path : 'users/me/registrations/', query : { 'related_counts' : 'children', 'embed' : 'contributors', 'filter[parent]' : 'null'}, systemCollection : 'registrations'}, 'All my registrations')
         ];
         // Initial Breadcrumb for All my projects
         var initialBreadcrumbs = options.initialBreadcrumbs || [new LinkObject('collection', { path : 'users/me/nodes/', query : { 'related_counts' : 'children', 'embed' : 'contributors', 'filter[parent]' : 'null' }, systemCollection : 'nodes'}, 'All my projects')];
