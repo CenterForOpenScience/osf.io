1f59026ae96eb0965aa12611d4a7df350ab0ece7
Zak <zik3qe@virginia.edu>
2016-03-23 15:49:53 -0400

    address custom email template confusion

--- admin/common_auth/views.py
+++ admin/common_auth/views.py
@@ -50,8 +50,8 @@ def register(request):
                 reset_form = PasswordResetForm({'email': new_user.email}, request.POST)
                 assert reset_form.is_valid()
                 reset_form.save(
-                    subject_template_name='common_auth/emails/account_creation_subject.txt',
-                    email_template_name='common_auth/emails/invitation_email.html',
+                    subject_template_name='emails/account_creation_subject.txt',
+                    email_template_name='emails/password_reset_email.html',
                     request=request
                 )
                 messages.success(request, 'Registration successful')  # add email reference here
