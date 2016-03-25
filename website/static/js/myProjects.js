8e2bc1820efdc0411a15e770940a0e73476fa9ba
Caner Uguz <caneruguz@gmail.com>
2016-03-25 10:47:00 -0400

    Add nodetype of collection to newly created collections

--- website/static/js/myProjects.js
+++ website/static/js/myProjects.js
@@ -884,7 +884,7 @@ var Collections = {
             promise.then(function(result){
                 var node = result.data;
                 var count = node.relationships.linked_nodes.links.related.meta.count || 0;
-                self.collections().push(new LinkObject('collection', { path : 'collections/' + node.id + '/linked_nodes/', query : { 'related_counts' : 'children' }, node : node, count : m.prop(count) }, node.attributes.title));
+                self.collections().push(new LinkObject('collection', { path : 'collections/' + node.id + '/linked_nodes/', query : { 'related_counts' : 'children' }, node : node, count : m.prop(count), nodeType : 'collection' }, node.attributes.title));
                 self.newCollectionName('');
                 self.calculateTotalPages();
                 self.currentPage(self.totalPages()); // Go to last page
