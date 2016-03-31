35363a68f6bd4f080bfdb14161535cba38ad9c2d
Matt Frazier <maf7sm@virginia.edu>
2016-03-31 13:17:48 -0400

    Add UVA DV to list of possible hosts
    [skip ci]

--- website/addons/dataverse/settings/defaults.py
+++ website/addons/dataverse/settings/defaults.py
@@ -1,5 +1,6 @@
 DEFAULT_HOSTS = [
     'dataverse.harvard.edu',            # Harvard PRODUCTION server
+    'dataverse.lib.virginia.edu',       # UVA PRODUCTION server
     'dataverse-demo.iq.harvard.edu',    # Harvard DEMO server
     'apitest.dataverse.org',            # Dataverse TEST server
 ]
