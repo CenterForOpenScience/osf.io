d1b73d953de6748360b90e64a43a95e2a9339cdb
Brian J. Geiger <bgeiger@pobox.com>
2016-04-04 15:18:11 -0400

    Add API tests for push_status_message to ensure it behaves as I believe it should

--- api_tests/base/test_utils.py
+++ api_tests/base/test_utils.py
@@ -2,10 +2,11 @@
 from nose.tools import *  # flake8: noqa
 
 from rest_framework import fields
-
+from rest_framework.exceptions import ValidationError
 from api.base import utils as api_utils
 
 from tests.base import ApiTestCase
+from framework.status import push_status_message
 
 
 class TruthyFalsyTestCase(ApiTestCase):
@@ -18,3 +19,27 @@ class TruthyFalsyTestCase(ApiTestCase):
 
     def test_falsy(self):
         assert_equal(api_utils.FALSY, fields.BooleanField.FALSE_VALUES)
+
+
+class FlaskDjangoIntegrationTestCase(ApiTestCase):
+    def test_push_status_message_no_response(self):
+        status_message = 'This is a message'
+        statuses = ['info', 'warning', 'warn', 'success', 'danger', 'default']
+        for status in statuses:
+            try:
+                push_status_message(status_message, kind=status)
+            except:
+                assert_true(False, 'Exception from push_status_message via API v2 with type "{}".'.format(status))
+
+    def test_push_status_message_error(self):
+        status_message = 'This is a message'
+        try:
+            push_status_message(status_message, kind='error')
+            assert_true(False, 'push_status_message() should have generated a ValidationError exception.')
+        except ValidationError as e:
+            assert_equal(e.detail[0], status_message,
+                         'push_status_message() should have passed along the message with the Exception.')
+        except RuntimeError:
+            assert_true(False, 'push_status_message() should have caught the runtime error and replaced it.')
+        except:
+            assert_true(False, 'Exception from push_status_message when called from the v2 API with type "error"')
