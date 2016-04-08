c9910fb25bfa39a1027eb6a7d1d71b1bea296911
Erin Braswell <erinspace@users.noreply.github.com>
2016-04-08 15:54:16 -0400

    Don't allow users to view Collections as Projects [OSF-5843] (#5391)
    * Add check for collection in must_be_valid_project decorator
    
    [#OSF-5843]
    
    * Add test for new decorator code collection guid bad request
    
    * Change ODM search test to return 400 when finding collections
    
    * Update test to be not found
    
    * Update collections view tests to look for 404
    
    * Change status to Not Found for vald project decorator
    
    * Get rid of collections specific message

--- tests/test_views.py
+++ tests/test_views.py
@@ -3875,9 +3875,8 @@ class TestODMTitleSearch(OsfTestCase):
                                'includePublic': 'yes',
                                'includeContributed': 'yes',
                                'isFolder': 'yes'
-                           }, auth=self.user.auth)
-        assert_equal(res.status_code, 200)
-        assert_equal(len(res.json), 1)
+                           }, auth=self.user.auth, expect_errors=True)
+        assert_equal(res.status_code, 404)
         res = self.app.get(self.url,
                            {
                                'term': self.folder.title,
@@ -3902,9 +3901,8 @@ class TestODMTitleSearch(OsfTestCase):
                                'includePublic': 'yes',
                                'includeContributed': 'yes',
                                'isFolder': 'yes'
-                           }, auth=self.user.auth)
-        assert_equal(res.status_code, 200)
-        assert_equal(len(res.json), 1)
+                           }, auth=self.user.auth, expect_errors=True)
+        assert_equal(res.status_code, 404)
 
 
 class TestReorderComponents(OsfTestCase):
