cab8098a5b989b42b27f471d9765563f7282c6c9
Bryan Gorges <bgorges@cos.io>
2016-04-11 12:24:05 -0400

    Finish most issues

--- admin/spam/views.py
+++ admin/spam/views.py
@@ -11,15 +11,28 @@ from website.project.model import Comment
 from website.settings import SUPPORT_EMAIL
 
 from admin.base.utils import OSFAdmin
-from admin.common_auth.logs import update_admin_log, CONFIRM_HAM, CONFIRM_SPAM
+from admin.common_auth.logs import (
+    update_admin_log,
+    CONFIRM_HAM,
+    CONFIRM_SPAM,
+    USER_EMAILED,
+)
 from admin.spam.serializers import serialize_comment
 from admin.spam.forms import EmailForm, ConfirmForm
 from admin.spam.templatetags.spam_extras import reverse_spam_detail
 
+STATUS = dict(
+    UNKNOWN=Comment.UNKNOWN,
+    SPAM=Comment.SPAM,
+    HAM=Comment.HAM,
+    FLAGGED=Comment.FLAGGED,
+)
+
 
 class EmailFormView(OSFAdmin, FormView):
-    """ Allow authorized admin user to Email spammers
+    """ Allow authorized admin user to Email spammers, use judiciously
 
+    Gets user data and supplies email template. Logs mail sent to user.
     """
     form_class = EmailForm
     template_name = "spam/email.html"
@@ -70,11 +83,22 @@ class EmailFormView(OSFAdmin, FormView):
         return super(EmailFormView, self).get_initial()
 
     def form_valid(self, form):
+        message = form.cleaned_data.get('message')
+        email = form.cleaned_data.get('email')
+        update_admin_log(
+            user_id=self.request.user.id,
+            object_id=self.kwargs.get('spam_id'),
+            object_repr='Comment',
+            message='User with email {} sent this message: {}'.format(
+                email, message
+            ),
+            action_flag=USER_EMAILED
+        )
         send_mail(
             subject=form.cleaned_data.get('subject').strip(),
-            message=form.cleaned_data.get('message'),
+            message=message,
             from_email=SUPPORT_EMAIL,
-            recipient_list=[form.cleaned_data.get('email')]
+            recipient_list=[email]
         )
         return super(EmailFormView, self).form_valid(form)
 
@@ -88,6 +112,10 @@ class EmailFormView(OSFAdmin, FormView):
 
 
 class SpamList(OSFAdmin, ListView):
+    """ Allow authorized admin user to see the things people have marked as spam
+
+    Interface with OSF database. No admin models.
+    """
     template_name = 'spam/spam_list.html'
     paginate_by = 10
     paginate_orphans = 1
@@ -114,7 +142,12 @@ class SpamList(OSFAdmin, ListView):
         return super(SpamList, self).get_context_data(**kwargs)
 
 
-class UserSpamList(OSFAdmin, SpamList):
+class UserSpamList(SpamList):
+    """ Allow authorized admin user to see the comments a user has had
+     marked spam
+
+    Interface with OSF database. No admin models.
+    """
     template_name = 'spam/user.html'
 
     def get_queryset(self):
@@ -127,19 +160,15 @@ class UserSpamList(OSFAdmin, SpamList):
         return Comment.find(query).sort(self.ordering)
 
     def get_context_data(self, **kwargs):
-        queryset = kwargs.pop('object_list', self.object_list)
-        page_size = self.get_paginate_by(queryset)
-        paginator, page, queryset, is_paginated = self.paginate_queryset(
-            queryset, page_size)
-        kwargs.setdefault('spam', map(serialize_comment, queryset))
-        kwargs.setdefault('page', page)
-        kwargs.setdefault('status', self.request.GET.get('status', '1'))
-        kwargs.setdefault('page_number', page.number)
         kwargs.setdefault('user_id', self.kwargs.get('user_id', None))
         return super(UserSpamList, self).get_context_data(**kwargs)
 
 
 class SpamDetail(OSFAdmin, FormView):
+    """ Allow authorized admin user to see details of reported spam.
+
+    Interface with OSF database. Logs action (confirming spam) on admin db.
+    """
     form_class = ConfirmForm
     template_name = 'spam/detail.html'
 
@@ -173,6 +202,7 @@ class SpamDetail(OSFAdmin, FormView):
         kwargs.setdefault('page_number', self.request.GET.get('page', 1))
         kwargs.setdefault('comment', serialize_comment(item))
         kwargs.setdefault('status', self.request.GET.get('status', u'1'))
+        kwargs.update(STATUS)  # Pass status in to check against
         return kwargs
 
     def form_valid(self, form):
