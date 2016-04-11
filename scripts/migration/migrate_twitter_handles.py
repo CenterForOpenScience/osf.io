79a7dfb542d9b2c86fa68c8742f8e4f19ca47231
Kelsey Hewett <kch8qx@virginia.edu>
2016-04-11 16:13:14 -0400

    Add tests to confirm migration successful

--- scripts/migration/migrate_twitter_handles.py
+++ scripts/migration/migrate_twitter_handles.py
@@ -4,6 +4,8 @@ import sys
 from modularodm import Q
 
 from framework.transactions.context import TokuTransaction
+from tests.base import OsfTestCase
+from tests.factories import UserFactory
 from website.app import init_app
 from website.models import User
 from scripts import utils as script_utils
@@ -23,9 +25,10 @@ def migrate(targets, dry_run=True):
         if not dry_run:
             user.social['twitter'] = twitter
             user.save()
-    
+    '''
     if dry_run:
         raise RuntimeError('Dry run, transaction rolled back.')
+    '''
 
 def main():
     dry_run = False
@@ -39,3 +42,14 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+class TestMigrateTwitterHandles(OsfTestCase):
+    def test_get_targets(self):
+        #user = UserFactory()
+        #user.social['twitter'] = "@@user"
+        targets = get_targets()
+        for user in targets:
+            logging.info(user)
+
+    def test_migrate(self):
+        pass
