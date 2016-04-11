406b2c882131aa90c936d8a157a95d54c46850ad
Chris Wisecarver <cwisecarver@cos.io>
2016-04-11 19:28:41 -0400

    Limit banning to once per url

--- api/caching/listeners.py
+++ api/caching/listeners.py
@@ -6,9 +6,5 @@ from modularodm import signals
 
 @signals.save.connect
 def ban_object_from_cache(sender, instance, fields_changed, cached_data):
-    abs_url = None
     if hasattr(instance, 'absolute_api_v2_url'):
-        abs_url = instance.absolute_api_v2_url
-
-    if abs_url is not None:
-        enqueue_postcommit_task(partial(ban_url, instance, fields_changed))
+        enqueue_postcommit_task((ban_url, (instance,)))
--- framework/postcommit_tasks/handlers.py
+++ framework/postcommit_tasks/handlers.py
@@ -1,6 +1,8 @@
 import logging
 import threading
 
+import gevent
+
 from website import settings
 
 _local = threading.local()
@@ -8,31 +10,33 @@ logger = logging.getLogger(__name__)
 
 def postcommit_queue():
     if not hasattr(_local, 'postcommit_queue'):
-        _local.postcommit_queue = []
+        _local.postcommit_queue = set()
     return _local.postcommit_queue
 
 def postcommit_before_request():
-    _local.postcommit_queue = []
+    _local.postcommit_queue = set()
 
 def postcommit_after_request(response, base_status_error_code=500):
     if response.status_code >= base_status_error_code:
-        _local.postcommmit_queue = []
+        _local.postcommmit_queue = set()
         return response
     try:
         if postcommit_queue():
-            for task in postcommit_queue():
-                task()
+            pcq = postcommit_queue()
+            threads = [gevent.spawn(func, *args) for func, args in postcommit_queue()]
+            gevent.joinall(threads)
     except AttributeError:
         if not settings.DEBUG_MODE:
             logger.error('Post commit task queue not initialized')
     return response
 
-def enqueue_postcommit_task(f):
+def enqueue_postcommit_task(function_and_args):
     try:
-        if f not in postcommit_queue():
-            postcommit_queue().append(f)
+        postcommit_queue().add(function_and_args)
     except RuntimeError:
-        f()
+        # if we can't add to the queue just log and run it
+        logger.error('Couldn\'t add {} to the postcommit queue, running syncronously'.format(function_and_args))
+        function_and_args[0](*function_and_args[1])
 
 
 handlers = {
