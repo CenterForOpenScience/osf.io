34f685eb7ade3527211c6a8d16bf25f17411cf5b
Sam Chrisinger <s.chrisinger@gmail.com>
2016-04-11 14:23:52 -0400

    Make sure embargo termination logs are nouser

--- website/project/model.py
+++ website/project/model.py
@@ -3398,7 +3398,7 @@ class Node(GuidStoredObject, AddonModelMixin, IdentifierMixin):
                 'project': self._id,
                 'node': self._id,
             },
-            auth=auth,
+            auth=None,
             save=True
         )
         self.embargo.mark_as_completed()
