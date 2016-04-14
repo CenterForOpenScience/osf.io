dcd80d103247b3555b93e659812d1ae6c5855a4a
adlius <adlius45@hotmail.com>
2016-04-13 20:36:49 -0400

    fix flake 8

--- website/project/views/node.py
+++ website/project/views/node.py
@@ -25,7 +25,6 @@ from website.project import new_node, new_private_link
 from website.project.decorators import (
     must_be_contributor_or_public_but_not_anonymized,
     must_be_contributor_or_public,
-    must_be_contributor,
     must_be_valid_project,
     must_have_permission,
     must_not_be_registration,
