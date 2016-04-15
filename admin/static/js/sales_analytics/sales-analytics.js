94378bf41221aa65ba259372563d462941214ef9
Longze Chen <cs.longze.chen@gmail.com>
2016-04-15 13:32:56 -0400

    Refactor script structure and visualization     - refactor sales-analytics.js to look more like javascript     - add history data for average user/mau sessions &  visualization

--- admin/static/js/sales_analytics/sales-analytics.js
+++ admin/static/js/sales_analytics/sales-analytics.js
@@ -3,158 +3,216 @@
 var keen = require('keen-js');
 var ss = require('simple-statistics');
 
-keen.ready(runSalesAnalytics());
+var SalesAnalytics = function() {
+    var self = this;
 
-function runSalesAnalytics() {
-
-    //  setup keen client
-    var keenClient = new keen({
+    self.keenClient = new keen({
         projectId: keenProjectId,
         readKey : keenReadKey
     });
 
-    //  keen query filters
-    var nullUserFilter = {
-        property_name: 'user.id',
-        operator: 'ne',
-        property_value: 'null'
-    };
-    var inactiveUserFilter = {
-        property_name: 'user.id',
-        operator: 'ne',
-        property_value: ''
-    };
-
-    //  query for average user session length
-    var userSessionQuery = new keen.Query('select_unique', {
-        event_collection: 'pageviews',
-        timeframe: 'previous_7_days',
-        target_property: 'keen.timestamp',
-        group_by: ['user.id', 'sessionId'],
-        filters: [nullUserFilter]
-    });
-
-    //  query for average MAU (monthly active user) session length
-    var activeUserSessionQuery = new keen.Query('select_unique', {
-        event_collection: 'pageviews',
-        timeframe: 'previous_7_days',
-        target_property: 'keen.timestamp',
-        group_by: ['user.id', 'sessionId'],
-        filters: [nullUserFilter, inactiveUserFilter]
-    });
-
-    //  make query, process the result and visualize it
-    var query = [userSessionQuery, activeUserSessionQuery];
-    var chartAverageSessionUser = new keen.Dataviz();
-    chartAverageSessionUser.el(document.getElementById('keen-chart-average-session-user')).prepare();
-    var chartAverageSessionMAU = new keen.Dataviz();
-    chartAverageSessionMAU.el(document.getElementById('keen-chart-average-session-mau')).prepare();
-
-    var req = keenClient.run(query, function(err, res) {
-        if (err) {
-            console.log('keen query error: ' + err);
+    self.keenFilters = {
+        nullUserFilter: {
+            property_name: 'user.id',
+            operator: 'ne',
+            property_value: 'null'
+        },
+        inactiveUserFilter: {
+            property_name: 'user.id',
+            operator: 'ne',
+            property_value: ''
         }
-        else {
-            var dataSetUser = extractDataSet(res[0]);
-            var dataSetMAU = extractDataSet(res[1]);
-            var result1 = ss.mean(dataSetUser);
-            var result2 = ss.mean(dataSetMAU);
-            console.log('average user session length is ' + result1 + " ms" );
-            console.log('average user session length (mau) is ' + result2 + " ms" );
+    };
 
-            chartAverageSessionUser.attributes({ title: 'Average User Session Length in Minutes', width: 600 });
-            chartAverageSessionUser.adapter({chartType: 'metric'});
-            chartAverageSessionMAU.attributes({ title: 'Average MAU Session Length in Minutes', width: 600 });
-            chartAverageSessionUser.parseRawData({result: result1/1000/60});
-            chartAverageSessionMAU.parseRawData({result: result2/1000/60});
-            chartAverageSessionUser.render();
-            chartAverageSessionMAU.render();
-        }
-    });
+    self.getAverageUserSessionLength = function () {
+        var query = new keen.Query('select_unique', {
+            event_collection: 'pageviews',
+            timeframe: 'previous_7_days',
+            target_property: 'keen.timestamp',
+            group_by: ['user.id', 'sessionId'],
+            filters: [self.keenFilters.nullUserFilter]
+        });
 
-    //  refresh every 10 minutes (testing)
-    setInterval(function() {
-        console.log('chart refresh');
-        req.refresh();
-    }, 1000 * 60 * 10);
+        var chart = new keen.Dataviz();
+        chart.el(document.getElementById('keen-chart-average-session-user')).prepare();
 
-    //  average user/mau session length each month for the past 12 months
-    //var date = new Date();
-    //var yearEnd = date.getFullYear();
-    //var monthEnd = date.getMonth();
-    //
-    //var count = 12;
-    //while (count-- > 0) {
-    //    var yearStart = yearEnd - 1 < 0 ? yearEnd - 1 : yearEnd;
-    //    var monthStart = (monthEnd - 1) % 12;
-    //
-    //    var dateStart = new Date(yearStart, monthStart, 1);
-    //    var dateEnd = new Date(yearEnd, monthEnd, 1);
-    //
-    //    console.log(dateStart.toISOString());
-    //    console.log(dateEnd.toISOString());
-    //
-    //    var userSessionQueryHistory = new keen.Query('select_unique', {
-    //        event_collection: 'pageviews',
-    //        timeframe: {
-    //            start: dateStart.toISOString(),
-    //            end: dateEnd.toISOString()
-    //            //start: '2016-03-01T05:00:00.000Z',
-    //            //end: '2016-04-01T05:00:00.000Z'
-    //        },
-    //        target_property: 'keen.timestamp',
-    //        group_by: ['user.id', 'sessionId'],
-    //        filters: [
-    //            {
-    //                property_name: 'user.id',
-    //                operator: 'ne',
-    //                property_value: 'null'
-    //            }
-    //        ]
-    //    });
-    //
-    //    keenClient.run(userSessionQueryHistory, function(err, res) {
-    //        if (err) {
-    //            console.log('keen query error: ' + err);
-    //        }
-    //        else {
-    //            var result = averageUserSessionLength(res);
-    //            console.log(result + ' ms');
-    //        }
-    //    });
-    //
-    //    yearEnd = yearStart;
-    //    monthEnd = monthStart;
-    //}
-}
-
-/**
- * Retrieve data set from keen query result for further statistical processing
- *
- * @param ts
- * @returns {number}
- */
-function extractDataSet(ts) {
-
-    if (!ts) {
-        return 0;
-    }
-
-    var beginTime;
-    var endTime;
-    var deltaSet = [];
-
-    for ( var i in ts.result) {
-        var session = ts.result[i];
-        if (session.hasOwnProperty('result')) {
-            if (session.result.length == 1) {
-                // TODO: take care of the situation where there is only one 'keen.timestamp'
-                continue;
+        var request = self.keenClient.run(query, function(error, response) {
+            if (error) {
+                document.getElementById('keen-chart-average-session-user').innerHTML = 'Keen Query Failure';
+                console.log(error);
             }
-            beginTime = Date.parse(session.result[0]);
-            endTime = Date.parse(session.result[session.result.length-1]);
-            deltaSet.push(endTime - beginTime);
+            else {
+                var dataSet = self.extractDataSet(response);
+                var result = ss.mean(dataSet);
+                console.log('average user session length is ' + result + " ms" );
+
+                chart.attributes({ title: 'Average User Session Length', width: 600 });
+                chart.adapter({chartType: 'metric'});
+                chart.parseRawData({result: result/1000/60});
+                chart.render();
+            }
+        });
+    };
+
+    self.getAverageMAUSessionLength = function() {
+        var query = new keen.Query('select_unique', {
+            event_collection: 'pageviews',
+            timeframe: 'previous_7_days',
+            target_property: 'keen.timestamp',
+            group_by: ['user.id', 'sessionId'],
+            filters: [self.keenFilters.nullUserFilter, self.keenFilters.inactiveUserFilter]
+        });
+
+        var chart = new keen.Dataviz();
+        chart.el(document.getElementById('keen-chart-average-session-mau')).prepare();
+
+        var request = self.keenClient.run(query, function(error, response) {
+            if (error) {
+                document.getElementById('keen-chart-average-session-mau').innerHTML = 'Keen Query Failure';
+                console.log(error);
+            }
+            else {
+                var dataSet = self.extractDataSet(response);
+                var result = ss.mean(dataSet);
+                console.log('average mau session length is ' + result + " ms" );
+
+                chart.attributes({ title: 'Average MAU Session Length', width: 600 });
+                chart.adapter({chartType: 'metric'});
+                chart.parseRawData({result: result/1000/60});
+                chart.render();
+            }
+        });
+    };
+
+    self.getAverageUserSessionHistory = function(init, numberOfWeeks, weekEnd, weekStart, weeklyResult) {
+        if (init == true) {
+            var date = new Date();
+            date.setHours(0, 0, 0, 0);
+            date.setDate(date.getDate() - date.getDay());
+            weekEnd = new Date(date);
+            weekStart = new Date(date.setDate(date.getDate() - 7));
+            weeklyResult = [];
+            self.getAverageUserSessionHistory(false, numberOfWeeks, weekEnd, weekStart, weeklyResult);
+            return;
         }
-    }
-    return deltaSet;
-}
+
+        if (numberOfWeeks == 0) {
+            var chart = new keen.Dataviz();
+            chart.el(document.getElementById('keen-chart-average-session-history')).prepare();
+            chart.attributes({title: 'Average User Session History of the Past 12 Weeks', width: 1000, height: 500});
+            chart.adapter({chartType: 'columnchart'});
+            chart.chartOptions({
+                hAxis: {
+                    title: "Week"
+                },
+                vAxis: {
+                    title: "Minutes"
+                }
+            });
+
+            chart.parseRawData({result: weeklyResult});
+            chart.render();
+            return;
+        }
+
+        console.log(numberOfWeeks + ': ' + weekEnd.toISOString());
+        document.getElementById('keen-chart-average-session-history').innerHTML = numberOfWeeks;
+
+        var queryUser = new keen.Query('select_unique', {
+            event_collection: 'pageviews',
+            timeframe: {
+                start: weekStart.toISOString(),
+                end: weekEnd.toISOString()
+            },
+            target_property: 'keen.timestamp',
+            group_by: ['user.id', 'sessionId'],
+            filters: [self.keenFilters.nullUserFilter]
+        });
+
+        var queryMAU = new keen.Query('select_unique', {
+            event_collection: 'pageviews',
+            timeframe: {
+                start: weekStart.toISOString(),
+                end: weekEnd.toISOString()
+            },
+            target_property: 'keen.timestamp',
+            group_by: ['user.id', 'sessionId'],
+            filters: [self.keenFilters.nullUserFilter, self.keenFilters.inactiveUserFilter]
+        });
+
+        var request = self.keenClient.run([queryUser, queryMAU], function (error, response) {
+            var resultUser;
+            var resultMAU;
+            if (error) {
+                document.getElementById('keen-chart-average-session-history').innerHTML = 'Keen Query Failure';
+                console.log(error);
+                //resultUser = resultMAU = -1;
+            }
+            else {
+                var dataSetUser = self.extractDataSet(response[0]);
+                var dataSetMAU = self.extractDataSet(response[1]);
+                resultUser = ss.mean(dataSetUser);
+                resultMAU = ss.mean(dataSetMAU);
+            }
+            var item = {
+                timeframe: {
+                    start: weekStart.toISOString(),
+                    end: weekEnd.toISOString()
+                },
+                value: [
+                    { category: 'User', result: resultUser/1000/60 },
+                    { category: 'MAU', result: resultMAU/1000/60 }
+                ]
+            };
+            weeklyResult.push(item);
+
+            weekEnd = new Date(weekStart);
+            weekStart.setDate(weekEnd.getDate() - 7);
+            numberOfWeeks--;
+            self.getAverageUserSessionHistory(false, numberOfWeeks, weekEnd, weekStart, weeklyResult);
+        });
+    };
+
+    self.init = function() {
+        console.log('init');
+        keen.ready(self.run());
+    };
+
+    self.run = function() {
+        console.log('run');
+        self.getAverageUserSessionLength();
+        self.getAverageMAUSessionLength();
+        self.getAverageUserSessionHistory(true, 12, null, null, null);
+    };
+
+    self.clean = function() {
+        console.log('clean');
+    };
+
+    self.extractDataSet = function(keenResult) {
+        if (!keenResult) {
+            return 0;
+        }
+
+        var beginTime;
+        var endTime;
+        var deltaSet = [];
+
+        for ( var i in keenResult.result) {
+            var session = keenResult.result[i];
+            if (session.hasOwnProperty('result')) {
+                if (session.result.length == 1) {
+                    // TODO: take care of the situation where there is only one 'keen.timestamp'
+                    continue;
+                }
+                beginTime = Date.parse(session.result[0]);
+                endTime = Date.parse(session.result[session.result.length-1]);
+                deltaSet.push(endTime - beginTime);
+            }
+        }
+        return deltaSet;
+    };
+};
+
+var salesAnalytics = new SalesAnalytics();
+salesAnalytics.init();
