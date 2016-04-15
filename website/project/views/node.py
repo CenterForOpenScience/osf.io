89ebb1cc005c91a9578352d90c5bbc3424574863
Yuhuai Liu <adlius45@hotmail.com>
2016-04-15 12:49:18 -0400

    OSF-5199 No forbidden error when accessing settings page (#5465)
    * osf-5199 Refactor the tests on decorators
    
    * osf-5199 Refactoring unit tests on decorator functions
    
    * Skip test that fails due to buggy decorator factory function
    
    * change permissions of the settings page
    
    * fix flake 8

--- website/project/views/node.py
+++ website/project/views/node.py
@@ -24,7 +24,6 @@ from website.project import new_node, new_private_link
 from website.project.decorators import (
     must_be_contributor_or_public_but_not_anonymized,
     must_be_contributor_or_public,
-    must_be_contributor,
     must_be_valid_project,
     must_have_permission,
     must_not_be_registration,
@@ -246,7 +245,7 @@ def node_forks(auth, node, **kwargs):
 
 @must_be_valid_project
 @must_be_logged_in
-@must_be_contributor
+@must_have_permission(READ)
 def node_setting(auth, node, **kwargs):
 
     #check institutions:
