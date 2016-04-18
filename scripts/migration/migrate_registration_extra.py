871c045bc7c6ad4c91332ff9b1c87b2e98239305
Eric Barbour <emb4gu@virginia.edu>
2016-04-18 09:54:44 -0400

    Migration for DraftRegistration question.extra
    Having multiple files in a DraftReg requires that the extra field be a
    list but looking at data on staging most are still dicts.
    
    {} -> []
    {                     [{
      'file': 'data' - >    'file': 'data'
    }                     }]

---
+++ scripts/migration/migrate_registration_extra.py
@@ -0,0 +1,53 @@
+"""
+Changes existing question.extra on a draft to a list
+required for multiple files attached to a question
+"""
+import sys
+import logging
+
+from website.app import init_app
+from scripts import utils as scripts_utils
+from website.models import DraftRegistration
+from framework.transactions.context import TokuTransaction
+
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+def migrate_drafts(dry):
+    draft_registrations = DraftRegistration.find()
+    count = 0
+    for r in draft_registrations:
+        data = r.registration_metadata
+        for q, ans in data.iteritems():
+            try:
+                files = ans['extra']
+            except (TypeError, KeyError) as e:
+                # Non-prereg Registrations
+                logger.exception(e)
+            else:
+                if type(files) is dict:
+                    if len(files.keys()) == 0:
+                        ans['extra'] = []
+                    else:
+                        ans['extra'] = [files]
+        count += 1
+        if not dry:
+            r.save()
+    logger.info('Done with {0} drafts migrated.'.format(count))
+
+
+def main(dry=True):
+    init_app(set_backends=True, routes=False)
+    scripts_utils.add_file_logger(logger, __file__)
+    migrate_drafts(dry)
+
+
+
+if __name__ == '__main__':
+    dry_run = 'dry' in sys.argv
+    with TokuTransaction():
+        main(dry=dry_run)
+        if dry_run:
+            raise RuntimeError('Dry run, rolling back transaction.')
