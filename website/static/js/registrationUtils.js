43c251b7a1dde7f2063b40e621d92a8182a2e181
Eric Barbour <emb4gu@virginia.edu>
2016-04-20 11:36:37 -0400

    Make prereg form preview  preserve paragraphs
    - Replace newlines with </br> in preview
    [OSF-5849]

--- website/static/js/registrationUtils.js
+++ website/static/js/registrationUtils.js
@@ -877,6 +877,10 @@ var RegistrationEditor = function(urls, editorId, preview) {
                     value = question.preview();
                 } else {
                     value = $osf.htmlEscape(question.value() || '');
+                    // preserve user's paragraph formatting
+                    while (value.indexOf('\n') > -1)
+                        value = value.replace('\n', '<br>');
+
                 }
 		$elem.append(
 		    $('<span class="col-md-12">').append(
