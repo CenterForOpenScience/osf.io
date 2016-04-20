de5803d994f6e48b48a85eb9f3b16e6b1f8f8a83
Matt Frazier <maf7sm@virginia.edu>
2016-04-20 09:41:59 -0400

    Specify which addons are configurable

--- api/base/settings/defaults.py
+++ api/base/settings/defaults.py
@@ -190,5 +190,6 @@ ENABLE_ESI = osf_settings.ENABLE_ESI
 VARNISH_SERVERS = osf_settings.VARNISH_SERVERS
 ESI_MEDIA_TYPES = osf_settings.ESI_MEDIA_TYPES
 
-ADDONS_OAUTH = ['box', 'dataverse', 'dropbox', 'figshare', 'github', 'mendeley', 'zotero', 's3', 'googledrive']
+ADDONS_FOLDER_CONFIGURABLE = ['box', 'dropbox', 's3', 'googledrive']
+ADDONS_OAUTH = ADDONS_FOLDER_CONFIGURABLE + ['dataverse', 'github', 'mendeley', 'zotero']
 ADDONS_MANAGEABLE = ADDONS_OAUTH + ['figshare']
