e106fb1862f3c21cc8ed98720389123403d46455
Lauren Barker <leb2dg@virginia.edu>
2016-04-20 10:34:44 -0400

    fix off by one, update placeholder, rename variable, clear error message on cancel

--- website/static/js/comment.js
+++ website/static/js/comment.js
@@ -42,18 +42,18 @@ var lastElementBr = function(){
 
 // ensure that return inserts a <br> in all browsers
 var onReturn = function (e) {
-    var doxExec = false;
+    var docExec = false;
     var range;
 
     // Gecko
     try {
-        doxExec = document.execCommand('insertBrOnReturn', false, true);
+        docExec = document.execCommand('insertBrOnReturn', false, true);
     }
     catch (error) {
         // IE throws an error if it does not recognize the command...
     }
 
-    if (doxExec) {
+    if (docExec) {
         return true;
     }
     // Standard
@@ -319,6 +319,7 @@ BaseComment.prototype.cancelReply = function() {
     this.replying(false);
     this.submittingReply(false);
     this.replyErrorMessage('');
+    this.errorMessage('');
 };
 
 BaseComment.prototype.setupToolTips = function(elm) {
@@ -643,6 +644,7 @@ CommentModel.prototype.cancelEdit = function() {
     this.editing(false);
     this.$root.editors -= 1;
     this.editErrorMessage('');
+    this.errorMessage('');
     this.content(this._content);
 };
 
