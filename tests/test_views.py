0a89f4ea664668f534caa005c9b19c9d375adb0a
Steven Loria <sloria1@gmail.com>
2016-04-27 15:28:52 -0400

    Update getting-started links to new Guides page
    h/t @saradbowman
    
    - Also redirect /getting-started/ and /howosfworks/ -> help.osf.io

--- tests/test_views.py
+++ tests/test_views.py
@@ -4296,7 +4296,8 @@ class TestStaticFileViews(OsfTestCase):
 
     def test_getting_started_page(self):
         res = self.app.get('/getting-started/')
-        assert_equal(res.status_code, 200)
+        assert_equal(res.status_code, 302)
+        assert_equal(res.request.location, 'http://help.osf.io/')
 
 
 class TestUserConfirmSignal(OsfTestCase):
