a985fd386bc612c05b4871211cdddbf10ec8c963
Daniel Brown <dsb9ef@virginia.edu>
2016-04-28 14:50:59 -0400

    Redirct /help/ to /faq/ (#4039)

--- tests/test_views.py
+++ tests/test_views.py
@@ -4257,6 +4257,10 @@ class TestStaticFileViews(OsfTestCase):
         res = self.app.get('/getting-started/')
         assert_equal(res.status_code, 200)
 
+    def test_help_page(self):
+        res = self.app.get('/help/')
+        assert_equal(res.status_code, 302)
+
 
 class TestUserConfirmSignal(OsfTestCase):
 
