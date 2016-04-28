9f7da531e60160714526105380fda0e24f5cce96
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-04-28 09:33:21 -0400

    Add API urls to DraftRegistration model.

--- website/project/model.py
+++ website/project/model.py
@@ -4607,6 +4607,16 @@ class DraftRegistration(StoredObject):
         return urlparse.urljoin(settings.DOMAIN, self.url)
 
     @property
+    def absolute_api_v2_url(self):
+        node = self.branched_from
+        path = '/nodes/{}/draft_registrations/{}/'.format(node._id, self._id)
+        return api_v2_url(path)
+
+    # used by django and DRF
+    def get_absolute_url(self):
+        return self.absolute_api_v2_url
+
+    @property
     def requires_approval(self):
         return self.registration_schema.requires_approval
 
