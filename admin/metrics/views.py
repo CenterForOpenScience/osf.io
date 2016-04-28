d3366f6b1a03fc62b462d7aea4d669b84b296cf9
jliu3230 <jeffreyliu1003@gmail.com>
2016-04-28 09:49:14 -0400

    add analytics dashboard

--- admin/metrics/views.py
+++ admin/metrics/views.py
@@ -7,8 +7,11 @@ from django.core.urlresolvers import reverse
 from admin.metrics.utils import get_osf_statistics
 from admin.metrics.models import OSFWebsiteStatistics
 from admin.base.settings import KEEN_PROJECT_ID, KEEN_READ_KEY, ENTRY_POINTS
+from bson.code import Code
 
+from datetime import datetime, timedelta
 from framework.mongo import database as db
+from website.util.metrics import get_entry_point
 
 def update_metrics(request):
     get_osf_statistics()
@@ -22,10 +25,57 @@ def download_csv(request):
 
 def get_user_count(db=db, entry_points=ENTRY_POINTS):
     counts = []
+    total = db.user.find({}).count()
     for i in entry_points:
-        counts.append({'Product': i, 'Count': db.user.find({'system_tags': i}).count()})
-    counts.append({'Product': 'osf', 'Count': db.user.find({'system_tags': {'$nin': entry_points}}).count()})
+        count = db.user.find({'system_tags': i}).count()
+        percent = round(float(count) / float(total), 2)
+        counts.append({'Product': i, 'Count': count, 'Percentage': percent})
+    counts.append({'Product': 'osf', 'Count': total - sum([i['Count'] for i in counts]),
+                  'Percentage': 1 - sum([i['Percentage'] for i in counts])})
     return {'items': counts}
+
+
+def get_multi_product_usage(db=db, timedelta=timedelta(days=365)):
+    """
+    Get the number of users using 2+ products within a period of time
+    """
+    start_date = datetime.now() - timedelta
+    map = Code('''function () {
+                    emit(this.user, this.params.node);
+                    };''')
+    reduce = Code('''function (key, values) {
+                        var node_id = [];
+                        for (var i = 0; i < values.length; i++) {
+                            node_id.push(i);
+                        }
+                        return node_id''')
+    user_node = db.nodelog.map_reduce(map, reduce, "user_node", query={'date': {'$gt': start_date}})
+    return user_node
+
+
+def get_multi_product_metrics(db=db, timedelta=timedelta(days=365)):
+    """
+    Get the number of users using 2+ products within a period of time
+    """
+    start_date = datetime.now() - timedelta
+    pipeline = [
+        {'$match': {'date': {'$gt': start_date}}},
+        {'$group': {'_id': '$user', 'node_id': {'$addToSet': '$params.node'}}}
+    ]
+    user_node = db.nodelog.aggregate(pipeline)['result']
+    multi_product_count = 0
+    for i in user_node:
+        user_id = i['_id']
+        node_id = i['node_id']
+        product = []
+        if user_id:
+            entry_point = get_entry_point(db.user.find({'_id': user_id}).next()['system_tags'])
+        nodes = db.node.find({'_id': {'$in': node_id}})
+        for node in nodes:
+            product.append(get_entry_point(node['system_tags']))
+        if len(set(product)) > 1:
+            multi_product_count += 1
+    return {'multi_product_count': multi_product_count}
 
 
 class OSFStatisticsListView(ListView):
@@ -43,10 +93,14 @@ def sales_analytics(request):
     # Compute metrics from OSF Database
     # User count for each product
     user_count = get_user_count(db, ENTRY_POINTS)
+    multi_product_metrics_yearly = get_multi_product_metrics()
+    multi_product_metrics_monthly = get_multi_product_metrics(timedelta=timedelta(days=30))
 
     context = {
         'keen_project_id': KEEN_PROJECT_ID,
         'keen_read_key': KEEN_READ_KEY,
         'user_count': user_count,
+        'multi_product_metrics_yearly': multi_product_metrics_yearly,
+        'multi_product_metrics_monthly': multi_product_metrics_monthly,
     }
     return render(request, 'metrics/sales_analytics.html', context)
