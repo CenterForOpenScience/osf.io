0ea9ac74401773733c7385ee4c18323530fe41db
erinspace <erin.braswell@gmail.com>
2016-04-29 15:49:00 -0400

    Update webpack config and add resetpassword page

--- webpack.common.config.js
+++ webpack.common.config.js
@@ -54,6 +54,7 @@ var entry = {
     'profile-settings-addons-page': staticPath('js/pages/profile-settings-addons-page.js'),
     'twofactor-page': staticPath('js/pages/twofactor-page.js'),
     'forgotpassword-page': staticPath('js/pages/forgotpassword-page.js'),
+    'resetpassword-page': staticPath('js/pages/resetpassword-page.js'),
     'login-page': staticPath('js/pages/login-page.js'),
     'notifications-config-page': staticPath('js/pages/notifications-config-page.js'),
     'faq-page' : staticPath('js/pages/faq-page.js'),
