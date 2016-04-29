6c0814bd559ec65cd05009ff9907479bd02c89f1
Sam Chrisinger <s.chrisinger@gmail.com>
2016-04-29 12:20:16 -0400

    Seperate migration scripts for drafts and regs

--- scripts/migration/migrate_registration_extra.py
+++ scripts/migration/migrate_registration_extra.py
@@ -1,6 +1,6 @@
 """
-Changes existing question.extra on a draft to a list
-required for multiple files attached to a question
+Changes existing question.extra on Prereg registrations
+to a list. Required for multiple files attached to a question
 """
 import sys
 import logging
@@ -8,7 +8,7 @@ import logging
 from modularodm import Q
 from website.app import init_app
 from scripts import utils as scripts_utils
-from website.models import DraftRegistration
+from website.models import Node
 from website.prereg.utils import get_prereg_schema
 from framework.transactions.context import TokuTransaction
 
@@ -16,40 +16,41 @@ from framework.transactions.context import TokuTransaction
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-def migrate_drafts(dry):
+def migrate_file_meta(question):
+    wfiles = question.get('extra')        
+    if files and isinstance(files, dict):
+        if len(files) == 0:
+            question['extra'] = []
+        else:
+            question['extra'] = [files]
 
+
+def migrate():
     PREREG_CHALLENGE_METASCHEMA = get_prereg_schema()
-    draft_registrations = DraftRegistration.find(
-        Q('registration_schema', 'eq', PREREG_CHALLENGE_METASCHEMA) &
-        Q('approval', 'eq', None) &
-        Q('registered_node', 'eq', None)
+    registrations = Node.find(
+        Q('is_registration', 'eq', True) &
+        Q('registered_schema', 'eq', PREREG_CHALLENGE_METASCHEMA) &
+        Q('is_deleted', 'eq', False)
     )
     count = 0
-    for r in draft_registrations:
-        data = r.registration_metadata
-        for q, ans in data.iteritems():
-            files = ans['extra']
-            if type(files) is dict:
-                if len(files.keys()) == 0:
-                    ans['extra'] = []
-                else:
-                    ans['extra'] = [files]
-                    count += 1
-        if not dry:
-            r.save()
+    for reg in registrations:
+        data = reg.registered_meta[PREREG_CHALLENGE_METASCHEMA._id]
+        for question in data.values():
+            if isinstance(question.get('value'), dict):
+                for value in question['value'].values():
+                    migrate_file_meta(value)
+            else:
+                migrate_file_meta(question)
+        reg.save()
+        count += 1        
     logger.info('Done with {0} drafts migrated.'.format(count))
-
-
-def main(dry=True):
-    init_app(set_backends=True, routes=False)
-    scripts_utils.add_file_logger(logger, __file__)
-    migrate_drafts(dry)
-
-
 
 if __name__ == '__main__':
     dry_run = 'dry' in sys.argv
+    if not dry_run:
+        scripts_utils.add_file_logger(logger, __file__)
     with TokuTransaction():
-        main(dry=dry_run)
+        init_app(set_backends=True, routes=False)
+        migrate()
         if dry_run:
             raise RuntimeError('Dry run, rolling back transaction.')
--- scripts/tests/test_migrate_registration_extra.py
+++ scripts/tests/test_migrate_registration_extra.py
@@ -1,87 +1,98 @@
 from nose.tools import *  # noqa
 
-from tests.base import OsfTestCase
-from tests.factories import UserFactory
-from tests.factories import DraftRegistrationFactory
+from tests.base import (
+    OsfTestCase,
+    fake
+)
+from tests.factories import (
+    UserFactory,
+    ProjectFactory
+)
+from tests import utils
 
 from website.models import MetaSchema
 from website.project.model import ensure_schemas
 from website.prereg.utils import get_prereg_schema
-from scripts.migration.migrate_registration_extra import main
 
-
+from scripts.migration.migrate_registration_extra import migrate
 
 class TestMigrateRegistrationExtra(OsfTestCase):
     def setUp(self):
         super(TestMigrateRegistrationExtra, self).setUp()
         self.user = UserFactory()
-        MetaSchema.remove()
+        self.node = ProjectFactory(creator=self.user)
         ensure_schemas()
-
-
-        self.file_ans = {
-            'file': {
-                'data':{
-                    'kind':'file',
-                    'extra':{
-                        'checkout': None,
-                        'hashes':{
-                            'sha256':'1fffe6116ecfa778f9938060d5caab923ba4b8db60bd2dd57f16a72e5ef06292'
+        self.prereg_schema = get_prereg_schema()
+        self.data = {
+            'uploader': {
+                'extra': {
+                    'hasSelectedFile': True,
+                    'nodeId': self.node._id,
+                    'selectedFileName': 'file.txt',
+                    'sha256': fake.sha256(),
+                    'viewUrl': '/project/{}/files/osfstorage/5723787136b74e1a953d9612/'.format(
+                        self.node._id
+                    )
+                },
+                'value': 'file.txt'
+            },
+            'other': {
+                'value': 'foo'
+            },
+            'nested': {
+                'value': {
+                    'uploader': {
+                        'extra': {
+                            'hasSelectedFile': True,
+                            'nodeId': self.node._id,
+                            'selectedFileName': 'file.txt',
+                            'sha256': fake.sha256(),
+                            'viewUrl': '/project/{}/files/osfstorage/5723787136b74e1a953d9612/'.format(
+                                self.node._id
+                            )
                         },
-                        'downloads':0,
-                        'version':1
+                        'value': 'file.txt'
                     },
-                    'modified':'2016-04-15T18:10:48',
-                    'name':'file.txt',
-                    'provider':'osfstorage',
+                    'other': {
+                        'value': 'foo'
+                    }
                 }
             }
         }
-        self.complex_metadata = {
-            'q1': {
-                'value': 'Answer 1',
-                'extra': []
-            },
-            'q2': {
-                'value': 'Answer 2',
-                'extra': {}
-            },
-            'q3': {
-                'value': 'Answer 3',
-                'extra': self.file_ans
-            }
-        }
-        self.simple_metadata = {
-            'Summary': 'Some airy'
-        }
-        self.schema = get_prereg_schema()
-        self.draft1 = DraftRegistrationFactory(
-            registration_metadata=self.complex_metadata,
-            registration_schema=self.schema,
-            approval=None,
-            registered_node=None
-
-        )
-        self.draft2 = DraftRegistrationFactory(
-            registration_metadata=self.simple_metadata
-        )
 
     def test_migrate_registration_extra(self):
-
-        assert_equal(type(self.draft1.registration_metadata['q1']['extra']), list)
-        assert_equal(type(self.draft1.registration_metadata['q2']['extra']), dict)
-        assert_equal(type(self.draft1.registration_metadata['q2']['extra']), dict)
-
-        assert_equal(self.draft2.registration_metadata, self.simple_metadata)
-
-        main(dry=False)
-        self.draft1.reload()
-        self.draft2.reload()
-
-        assert_equal(type(self.draft1.registration_metadata['q1']['extra']), list)
-        assert_equal(type(self.draft1.registration_metadata['q2']['extra']), list)
-        assert_equal(type(self.draft1.registration_metadata['q3']['extra']), list)
-
-        assert_equal(self.draft1.registration_metadata['q3']['extra'][0], self.file_ans)
-
-        assert_equal(self.draft2.registration_metadata, self.simple_metadata)
+        with utils.mock_archive(
+                self.node,
+                schema=self.prereg_schema,
+                data=self.data,
+                autocomplete=True,
+                autoapprove=True
+        ) as reg:
+            migrate()
+            reg.reload()
+            data = reg.registered_meta[self.prereg_schema._id]
+            assert_true(
+                isinstance(data['uploader']['extra'], list)
+            )
+            assert_true(
+                isinstance(
+                    data['nested']['value']['uploader']['extra'],
+                    list
+                )
+            )
+            assert_equal(
+                self.data['uploader']['extra'],
+                data['uploader']['extra'][0]
+            )
+            assert_equal(
+                self.data['nested']['value']['uploader']['extra'],
+                data['nested']['value']['uploader']['extra'][0]
+            )
+            assert_equal(
+                self.data['nested']['value']['other'],
+                data['nested']['value']['other']
+            )
+            assert_equal(
+                self.data['other'],
+                data['other']
+            )
