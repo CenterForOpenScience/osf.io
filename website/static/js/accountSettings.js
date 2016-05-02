391253dab88745ce3a8ffc6966c5913addb03a05
erinspace <erin.braswell@gmail.com>
2016-05-02 14:36:48 -0400

    Add new change passwd viewmodel for ko validation

--- website/static/js/accountSettings.js
+++ website/static/js/accountSettings.js
@@ -6,6 +6,7 @@ var bootbox = require('bootbox');
 var ko = require('knockout');
 var oop = require('js/oop');
 var Raven = require('raven-js');
+var zxcvbn = require('zxcvbn');
 var ChangeMessageMixin = require('js/changeMessage');
 
 
@@ -303,6 +304,117 @@ var UserProfileViewModel = oop.extend(ChangeMessageMixin, {
 });
 
 
+var ChangePasswordViewModel = function() {
+
+    var self = this;
+
+    ko.validation.rules.complexity = {
+        validator: function (val, minimumComplexity) {
+            return self.passwordComplexity() >= minimumComplexity;
+        },
+        message: 'Please enter a more complex password'
+    };
+    ko.validation.registerExtenders();
+
+    self.typedPassword = ko.observable('');
+
+    self.passwordFeedback = ko.observable('');
+
+    self.passwordComplexity = ko.pureComputed(function() {
+        var current = zxcvbn(self.typedPassword());
+        self.passwordFeedback(current.feedback.warning);
+        return current.score;
+    });
+
+    self.passwordComplexityBar = ko.computed(function() {
+        if (self.passwordComplexity() === 0) {
+            return {
+                class: 'progress-bar progress-bar-danger',
+                style: 'width: 0%'
+            };
+        }
+        if (self.passwordComplexity() === 1) {
+            return {
+                class: 'progress-bar progress-bar-danger',
+                style: 'width: 25%'
+            };
+        } else if (self.passwordComplexity() === 2) {
+            return {
+                class: 'progress-bar progress-bar-warning',
+                style: 'width: 50%'
+            };
+        } else if (self.passwordComplexity() === 3) {
+            return {
+                class: 'progress-bar progress-bar-warning',
+                style: 'width: 75%'
+            };
+        } else if (self.passwordComplexity() === 4) {
+            return {
+                class: 'progress-bar progress-bar-success',
+                style: 'width: 100%'
+            };
+        }
+    });
+
+    self.oldPassword = ko.observable('').extend({
+        required: true
+    });
+
+    self.password = ko.observable('').extend({
+        required: true,
+        minLength: 6,
+        maxLength: 256,
+        complexity: 2
+    });
+
+    self.passwordConfirmation = ko.observable('').extend({
+        required: true,
+        validation: {
+            validator: function(val, other) {
+                return String(val).toLowerCase() === String(other).toLowerCase();
+            },
+            'message': 'Passwords must match.',
+            params: self.password
+        }
+    });
+
+    // Preserve object of validated fields for use in `submit`
+    var validatedFields = {
+        oldPassword: self.oldPassword,
+        password: self.password,
+        passwordConfirmation: self.passwordConfirmation
+    };
+    // Collect validated fields
+    self.validatedFields = ko.validatedObservable($.extend({}, validatedFields));
+
+    self.trim = function(observable) {
+        observable($.trim(observable()));
+    };
+
+    self.isValid = ko.computed(function() {
+        return self.validatedFields.isValid();
+    });
+
+    self.submit = function() {
+        // Show errors if invalid
+        if (!self.isValid()) {
+            // Ensure validation errors are displayed
+            $.each(validatedFields, function(key, value) {
+                value.notifySubscribers();
+            });
+            return false;
+        }
+        // Else submit
+        $osf.postJSON(
+            '',
+            ko.toJS(self)
+        );
+    };
+
+    self.errors = ko.validation.group(self);
+
+};
+
 var DeactivateAccountViewModel = oop.defclass({
     constructor: function () {
         this.success = ko.observable(false);
@@ -351,7 +463,6 @@ var DeactivateAccountViewModel = oop.defclass({
         });
     }
 });
-
 
 var ExportAccountViewModel = oop.defclass({
     constructor: function () {
@@ -402,6 +513,7 @@ var ExportAccountViewModel = oop.defclass({
 
 module.exports = {
     UserProfileViewModel: UserProfileViewModel,
+    ChangePasswordViewModel: ChangePasswordViewModel,
     DeactivateAccountViewModel: DeactivateAccountViewModel,
     ExportAccountViewModel: ExportAccountViewModel
 };
