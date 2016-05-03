d31c096338171cb2f9b8837995523c1f1edf7af4
Henrique Moco <hmoco94@vt.edu>
2016-05-03 13:20:41 -0400

    fix script problems

--- scripts/migration/migrate_primary_institutions.py
+++ scripts/migration/migrate_primary_institutions.py
@@ -6,7 +6,7 @@ from modularodm import Q
 from website.app import init_app
 from website.models import Node, NodeLog
 
-logger = logging.getLogget(__name__)
+logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.WARN)
 
 PRIMARY_INSTITUTION_CHANGED = 'primary_institution_changed'
@@ -15,13 +15,13 @@ PRIMARY_INSTITUTION_REMOVED = 'primary_institution_removed'
 def migrate_logs(dry_run):
     added_logs = NodeLog.find(Q('action', 'eq', PRIMARY_INSTITUTION_CHANGED))
     for log in added_logs:
-        logger.warn('Log with id <{}> being updated for affiliation added').format(log._id)
+        logger.warn('Log with id <{}> being updated for affiliation added'.format(log._id))
         if not dry_run:
             log.action = NodeLog.AFFILIATED_INSTITUTION_ADDED
             log.save()
     removed_logs = NodeLog.find(Q('action', 'eq', PRIMARY_INSTITUTION_REMOVED))
     for log in removed_logs:
-        logger.warn('Log with id <{}> being updated for affiliation removed').format(log._id)
+        logger.warn('Log with id <{}> being updated for affiliation removed'.format(log._id))
         if not dry_run:
             log.action = NodeLog.AFFILIATED_INSTITUTION_REMOVED
             log.save()
@@ -29,7 +29,7 @@ def migrate_logs(dry_run):
 def migrate_nodes(dry_run):
     nodes = Node.find(Q('primary_institution', 'ne', None))
     for node in nodes:
-        logger.warn('Node with id <{}> and title <{}> being updated').format(node._id, node.title)
+        logger.warn('Node with id <{}> and title <{}> being updated'.format(node._id, node.title))
         if not dry_run:
             inst = node.primary_institution
             if inst not in node.affiliated_institutions:
