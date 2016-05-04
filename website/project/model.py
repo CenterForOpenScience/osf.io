878c595d80ce51d03443cda6d93ff79cc1570c68
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-05-04 17:08:44 -0400

    Add get_absolute_url to Metaschema model.

--- website/project/model.py
+++ website/project/model.py
@@ -111,6 +111,15 @@ class MetaSchema(StoredObject):
     def requires_consent(self):
         return self._config.get('requiresConsent', False)
 
+    @property
+    def absolute_api_v2_url(self):
+        path = '/metaschemas/{}/'.format(self._id)
+        return api_v2_url(path)
+
+    # used by django and DRF
+    def get_absolute_url(self):
+        return self.absolute_api_v2_url
+
 def ensure_schema(schema, name, version=1):
     schema_obj = None
     try:
