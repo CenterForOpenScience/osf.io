fa358a5ede7346d05729e1e259331c3ef82e9457
jliu3230 <jeffreyliu1003@gmail.com>
2016-05-05 11:09:54 -0400

    update get_entry_point

--- website/util/metrics.py
+++ website/util/metrics.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
 
-def get_entry_point(system_tags):
+def get_entry_point(user):
     """
     Given the user system_tags, return the user entry point (osf, osf4m, prereg, institution)
     In case of multiple entry_points existing in the system_tags, return only the first one.
     """
     entry_points = ['osf4m', 'prereg_challenge_campaign', 'institution_campaign']
-    for i in system_tags:
+    for i in user.system_tags:
         if i in entry_points:
             return i
     else:
