00bb631437fdf45c7a067da43aa042f8b1f6ef8e
Chris Wisecarver <cwisecarver@cos.io>
2016-05-06 11:36:20 -0400

    Add unique together on _id and system

--- osf_models/models/tag.py
+++ osf_models/models/tag.py
@@ -7,3 +7,6 @@ class Tag(BaseModel):
     _id = models.CharField(max_length=1024)
     lower = models.CharField(max_length=1024)
     system = models.BooleanField(default=False)
+
+    class Meta:
+        unique_together = ('_id', 'system')
