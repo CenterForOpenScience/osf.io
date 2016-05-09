b8135c5075520ec220374a28ecaa484520f77426
Henrique Moco <hmoco94@vt.edu>
2016-05-09 10:27:29 -0400

    Rewrite migration

--- scripts/migration/migrate_primary_institutions.py
+++ scripts/migration/migrate_primary_institutions.py
@@ -3,6 +3,9 @@ import logging
 
 from modularodm import Q
 
+from framework.transactions.context import TokuTransaction
+
+from scripts import utils
 from website.app import init_app
 from website.models import Node, NodeLog
 
@@ -12,38 +15,39 @@ logging.basicConfig(level=logging.WARN)
 PRIMARY_INSTITUTION_CHANGED = 'primary_institution_changed'
 PRIMARY_INSTITUTION_REMOVED = 'primary_institution_removed'
 
-def migrate_logs(dry_run):
+def migrate(dry_run=True):
     added_logs = NodeLog.find(Q('action', 'eq', PRIMARY_INSTITUTION_CHANGED))
     for log in added_logs:
         logger.warn('Log with id <{}> being updated for affiliation added'.format(log._id))
-        if not dry_run:
-            log.action = NodeLog.AFFILIATED_INSTITUTION_ADDED
-            log.save()
+        log.action = NodeLog.AFFILIATED_INSTITUTION_ADDED
+        log.save()
+
     removed_logs = NodeLog.find(Q('action', 'eq', PRIMARY_INSTITUTION_REMOVED))
     for log in removed_logs:
         logger.warn('Log with id <{}> being updated for affiliation removed'.format(log._id))
-        if not dry_run:
-            log.action = NodeLog.AFFILIATED_INSTITUTION_REMOVED
-            log.save()
+        log.action = NodeLog.AFFILIATED_INSTITUTION_REMOVED
+        log.save()
 
-def migrate_nodes(dry_run):
     nodes = Node.find(Q('primary_institution', 'ne', None))
     for node in nodes:
         logger.warn('Node with id <{}> and title <{}> being updated'.format(node._id, node.title))
-        if not dry_run:
-            inst = node.primary_institution
-            if inst not in node.affiliated_institutions:
-                node.affiliated_institutions.append(inst)
-            node.primary_institution = None
-            node.save()
+        inst = node.primary_institution
+        if inst not in node.affiliated_institutions:
+            node.affiliated_institutions.append(inst)
+        node.primary_institution = None
+        node.save()
+    if dry_run:
+        raise RuntimeError('Dry run, transaction rolled back.')
 
-def main(dry_run=True):
+def main():
+    dry_run = 'dry' in sys.argv
     if dry_run:
         logger.warn('DRY RUN mode')
-    migrate_logs(dry_run)
-    migrate_nodes(dry_run)
+    else:
+        utils.add_file_logger(logger, __file__)
+    init_app()
+    with TokuTransaction():
+        migrate(dry_run)
 
 if __name__ == '__main__':
-    dry_run = 'dry' in sys.argv
-    app = init_app()
-    main(dry_run=dry_run)
+    main()
