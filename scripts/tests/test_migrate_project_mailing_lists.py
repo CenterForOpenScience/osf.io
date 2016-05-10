e9098080f16c12945112af7b0707fcb92724fd44
Matt Frazier <maf7sm@virginia.edu>
2016-05-10 14:02:33 -0400

    Update script tests

--- scripts/tests/test_migrate_project_mailing_lists.py
+++ scripts/tests/test_migrate_project_mailing_lists.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import mock
 from nose.tools import *  # noqa
 
 from website.models import Node, NotificationSubscription
@@ -13,36 +14,40 @@ from scripts.migrate_project_mailing_lists import migrate
 
 class TestMigrateMailingLists(OsfTestCase):
 
-    # TODO: mock ml_utils
-
     def tearDown(self):
         # Prevent errors
         Node.remove()
         NotificationSubscription.remove()
 
-    def test_migrate_node(self):
+    @mock.patch('scripts.migrate_project_mailing_lists.create_list')
+    def test_migrate_node(self, mock_create):
         node = ProjectFactory()
         NotificationSubscription.remove()
         migrate(dry_run=False)
         assert_true(node.mailing_enabled)
+        assert node._id in str(mock_create.call_args_list)
 
-    def test_migrate_node_with_parent(self):
+    @mock.patch('scripts.migrate_project_mailing_lists.create_list')
+    def test_migrate_node_with_parent(self, mock_create):
         node = ProjectFactory(parent=NodeFactory())
         NotificationSubscription.remove()
         migrate(dry_run=False)
         assert_true(node.mailing_enabled)
-        
+        assert node._id in str(mock_create.call_args_list)
+        assert node.parent._id in str(mock_create.call_args_list)
 
-    def test_migrate_registration(self):
+    @mock.patch('scripts.migrate_project_mailing_lists.create_list')
+    def test_migrate_registration(self, mock_create):
         node = RegistrationFactory()
         NotificationSubscription.remove()
         migrate(dry_run=False)
         assert_false(node.mailing_enabled)
+        assert node._id not in str(mock_create.call_args_list)
 
-
-    def test_migrate_dashboard(self):
+    @mock.patch('scripts.migrate_project_mailing_lists.create_list')
+    def test_migrate_dashboard(self, mock_create):
         node = CollectionFactory()
         NotificationSubscription.remove()
         migrate(dry_run=False)
         assert_false(node.mailing_enabled)
-
+        assert node._id not in str(mock_create.call_args_list)
