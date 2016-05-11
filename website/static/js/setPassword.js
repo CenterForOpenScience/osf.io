f19d9c1565c519ed1e7e7cbea065bbca0cc9fa5c
erinspace <erin.braswell@gmail.com>
2016-05-11 16:40:11 -0400

    Re get-rid of signUp js add eric's changes

--- website/static/js/setPassword.js
+++ website/static/js/setPassword.js
@@ -168,6 +168,10 @@ var ViewModel = function(passwordViewType, submitUrl, campaign) {
     };
 
     self.submit = function() {
+        if (self.submitted()) {
+            self.changeMessage(self.flashMessage, self.flashMessageClass, 'You have already submitted. You cannot sign up more than once.', 'text-danger p-xs');
+            return false;
+        }
         // Show errors if invalid
         if (!self.isValid()) {
             // Ensure validation errors are displayed
