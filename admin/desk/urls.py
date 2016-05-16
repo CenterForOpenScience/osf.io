97b609ae79ee3f7c1370a968150a702cf9b00a87
Bryan Gorges <bgorges@cos.io>
2016-05-16 15:12:52 -0400

    Remove visible password

--- admin/desk/urls.py
+++ admin/desk/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.DeskCaseList.as_view(), name='cases'),
-    url(r'^customer/id-(?P<user_id>[a-z0-9]+)/$', views.DeskCustomer.as_view(),
+    url(r'^customer/(?P<user_id>[a-z0-9]+)/$', views.DeskCustomer.as_view(),
         name='customer'),
-    url(r'^cases/id-(?P<user_id>[a-z0-9]+)/$', views.DeskCaseList.as_view(),
+    url(r'^cases/(?P<user_id>[a-z0-9]+)/$', views.DeskCaseList.as_view(),
         name='user_cases'),
 ]
