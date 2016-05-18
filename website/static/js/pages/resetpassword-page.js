b820702791e89257a33dfb8859292930fb34f844
erinspace <erin.braswell@gmail.com>
2016-05-18 09:43:15 -0400

    add new redirect url to resetpassword page for login

--- website/static/js/pages/resetpassword-page.js
+++ website/static/js/pages/resetpassword-page.js
@@ -5,8 +5,11 @@
 var $ = require('jquery');
 
 var SetPassword = require('js/setPassword');
+var verificationKey = window.contextVars.verification_key;
+var resetUrl = '/api/v1/resetpassword/' + verificationKey + '/';
+var redirectrUrl = '/login/';
 
 
 $(document).ready(function() {
-    new SetPassword('#resetPasswordForm', 'reset');
+    new SetPassword('#resetPasswordForm', 'reset', resetUrl, '', redirectrUrl);
 });
