ed72be91ec3d091f8da52924a2b87c458d40e1d7
erinspace <erin.braswell@gmail.com>
2016-05-19 15:40:05 -0400

    Some more tests for the detail and list routes

--- api_tests/identifiers/views/test_identifier_detail.py
+++ api_tests/identifiers/views/test_identifier_detail.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
+from nose.tools import *
+import urlparse
 from api.base.settings.defaults import API_BASE
+from website.identifiers.model import Identifier
 
 from tests.base import ApiTestCase
 from tests.factories import (
-    NodeFactory,
-    ProjectFactory,
     RegistrationFactory,
     AuthUserFactory,
     IdentifierFactory
@@ -14,9 +15,28 @@ class TestIdentifierDetail(ApiTestCase):
     def setUp(self):
         super(TestIdentifierDetail, self).setUp()
         self.user = AuthUserFactory()
-        self.user_two = AuthUserFactory()
 
-        self.public_registration = RegistrationFactory(creator=self.user, is_public=True)
-        self.private_registration = RegistrationFactory(creator=self.user)
+        self.registration = RegistrationFactory(creator=self.user, is_public=True)
+        self.identifier = IdentifierFactory(referent=self.registration)
+        self.url = '/{}identifiers/{}/'.format(API_BASE, self.identifier.value)
 
-        self.identifier = IdentifierFactory(referent=self.public_registration)
+        self.res = self.app.get(self.url)
+        self.data = self.res.json['data']
+
+    def tearDown(self):
+        super(TestIdentifierDetail, self).tearDown()
+        Identifier.remove()
+
+    def test_identifier_detail_success(self):
+        assert_equal(self.res.status_code, 200)
+        assert_equal(self.res.content_type, 'application/vnd.api+json')
+
+    def test_identifier_detail_returns_correct_referent(self):
+        path = urlparse.urlparse(self.data['relationships']['referent']['links']['related']['href']).path
+        assert_equal('/{}registrations/{}/'.format(API_BASE, self.registration._id), path)
+
+    def test_identifier_detail_returns_correct_category(self):
+        assert_equal(self.data['attributes']['category'], self.identifier.category)
+
+    def test_identifier_detail_returns_correct_value(self):
+        assert_equal(self.data['attributes']['identifier']['self'], self.identifier.value)
--- api_tests/identifiers/views/test_identifier_list.py
+++ api_tests/identifiers/views/test_identifier_list.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
+from nose.tools import *  # flake8: noqa
+import urlparse
+
 from api.base.settings.defaults import API_BASE
+from website.identifiers.model import Identifier
 
 from tests.base import ApiTestCase
 from tests.factories import (
@@ -15,16 +19,43 @@ class TestIdentifierList(ApiTestCase):
         self.user = AuthUserFactory()
         self.user_two = AuthUserFactory()
 
-        self.public_registration = RegistrationFactory(creator=self.user, is_public=True)
-        self.private_registration = RegistrationFactory(creator=self.user)
-
-        self.identifier = IdentifierFactory(referent=self.public_registration)
-        self.public_url = '/{}nodes/{}/identifiers/'.format(API_BASE, self.public_registration._id)
-        # self.private_url = '/{}nodes/{}/identifiers/'.format(API_BASE, self.private_registration._id)
+        self.registration = RegistrationFactory(creator=self.user, is_public=True)
+        self.identifier = IdentifierFactory(referent=self.registration)
+        self.url = '/{}nodes/{}/identifiers/'.format(API_BASE, self.registration._id)
 
         self.res = self.app.get(self.url)
         self.data = self.res.json['data']
 
+        self.all_identifiers = Identifier.find()
+
+    def tearDown(self):
+        super(TestIdentifierList, self).tearDown()
+        Identifier.remove()
+
     def test_identifier_list_success(self):
         assert_equal(self.res.status_code, 200)
         assert_equal(self.res.content_type, 'application/vnd.api+json')
+
+    def test_identifier_list_returns_correct_number(self):
+        total = self.res.json['links']['meta']['total']
+        assert_equal(total, self.all_identifiers.count())
+
+    def test_identifier_list_returns_correct_referent(self):
+        paths = [
+            urlparse.urlparse(
+                item['relationships']['referent']['links']['related']['href']
+            ).path for item in self.data
+        ]
+        assert_in('/{}registrations/{}/'.format(API_BASE, self.registration._id), paths)
+
+    def test_identifier_list_returns_correct_categories(self):
+        categories = [identifier.category for identifier in self.all_identifiers]
+        categories_in_response = [identifier['attributes']['category'] for identifier in self.data]
+
+        assert_items_equal(categories_in_response, categories)
+
+    def test_identifier_list_returns_correct_values(self):
+        categories = [identifier.value for identifier in self.all_identifiers]
+        categories_in_response = [identifier['attributes']['identifier']['self'] for identifier in self.data]
+
+        assert_items_equal(categories_in_response, categories)
