249557a6ec7ad4a6868d153de1ceaaaa13325d93
Mark Patton <mark.patton@gmail.com>
2016-05-20 11:28:12 -0400

    Correct host arguments to default to 127.0.0.1, which is what the default behavior is.

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -75,10 +75,10 @@ def git_logs(count=100, pretty='format:"%s - %b"', grep='"Merge pull request"'):
 
 
 @task
-def apiserver(port=8000, wait=True):
+def apiserver(port=8000, wait=True, host='127.0.0.1'):
     """Run the API server."""
     env = os.environ.copy()
-    cmd = 'DJANGO_SETTINGS_MODULE=api.base.settings {} manage.py runserver {} --nothreading'.format(sys.executable, port)
+    cmd = 'DJANGO_SETTINGS_MODULE=api.base.settings {} manage.py runserver {}:{} --nothreading'.format(sys.executable, host, port)
     if wait:
         return run(cmd, echo=True, pty=True)
     from subprocess import Popen
@@ -87,10 +87,10 @@ def apiserver(port=8000, wait=True):
 
 
 @task
-def adminserver(port=8001):
+def adminserver(port=8001, host='127.0.0.1'):
     """Run the Admin server."""
     env = 'DJANGO_SETTINGS_MODULE="admin.base.settings"'
-    cmd = '{} python manage.py runserver {} --nothreading'.format(env, port)
+    cmd = '{} python manage.py runserver {}:{} --nothreading'.format(env, host, port)
     run(cmd, echo=True, pty=True)
 
 
@@ -904,10 +904,7 @@ def webpack(clean=False, watch=False, dev=False):
         clean_assets()
     webpack_bin = os.path.join(HERE, 'node_modules', 'webpack', 'bin', 'webpack.js')
     args = [webpack_bin]
-    if settings.DEBUG_MODE and dev:
-        args += ['--colors']
-    else:
-        args += ['--progress']
+    args += ['--progress']
     if watch:
         args += ['--watch']
     config_file = 'webpack.dev.config.js' if dev else 'webpack.prod.config.js'
