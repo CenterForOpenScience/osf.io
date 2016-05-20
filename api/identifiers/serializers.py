c77632360b99eccbb9d1c05c12f6e6fb00671f8f
erinspace <erin.braswell@gmail.com>
2016-05-20 14:33:59 -0400

    add id field and fix values field

--- api/identifiers/serializers.py
+++ api/identifiers/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers as ser
-from api.base.serializers import JSONAPISerializer, LinksField, RelationshipField
+from api.base.serializers import JSONAPISerializer, LinksField, RelationshipField, IDField
 
 
 class IdentifierSerializer(JSONAPISerializer):
@@ -8,20 +8,20 @@ class IdentifierSerializer(JSONAPISerializer):
 
     filterable_fields = frozenset(['category'])
 
-    identifier = LinksField({
-        'self': 'get_identifiers'
-    })
+    value = ser.CharField(read_only=True)
 
     referent = RelationshipField(
         related_view='registrations:registration-detail',
         related_view_kwargs={'node_id': '<referent._id>'},
     )
 
+    id = IDField(source='_id', read_only=True)
+
     class Meta:
         type_ = 'identifiers'
 
-    def get_identifiers(self, obj):
-        return obj.value
-
     def get_absolute_url(self, obj):
         return obj.absolute_api_v2_url
+
+    def get_id(self, obj):
+        return obj._id
