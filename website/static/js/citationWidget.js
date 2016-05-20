31212cf066f146a56b80732a1bff9d2e4c527c87
RomanZWang <rwang2006@yahoo.com>
2016-05-19 20:57:08 -0400

    [OSF-3622] CSL Citation widget fix

--- website/static/js/citationWidget.js
+++ website/static/js/citationWidget.js
@@ -30,6 +30,24 @@ function CitationWidget(inputSelector, displaySelector) {
     this.init();
 }
 
+function recurPrint(jsonObj){
+  return [ "<span class=\"csl-entry\">" +  recurPrintHelp(jsonObj, "") + "</span>"];
+
+}
+function recurPrintHelp(jsonObj, citeholder) {
+    for (var key in jsonObj) {
+      if (typeof(jsonObj[key]) == "object" && jsonObj[key] != null) {
+        citeholder = recurPrintHelp(jsonObj[key], citeholder);
+
+      } else {
+        if(jsonObj[key]!= undefined){
+            citeholder += " " + jsonObj[key];
+          }
+        }
+    }
+    return citeholder;
+  }
+
 CitationWidget.prototype.init = function() {
     var self = this;
 
@@ -61,7 +79,12 @@ CitationWidget.prototype.init = function() {
         var citationRequest = $.get(ctx.node.urls.api + 'citation/');
         $.when(styleRequest, citationRequest).done(function(style, data) {
             var citeproc = citations.makeCiteproc(style[0], data[0], 'html');
-            var items = citeproc.makeBibliography()[1];
+            if (citeproc.bibliography.tokens.length>0) {
+                            var items = citeproc.makeBibliography()[1];
+                        }
+            else {
+                var items= recurPrint(data[0]);
+            }
             /* the safety of this was discussed with JIRA ticket OSF-4889 */
             self.$citationElement.html(items[0]).slideDown();
         }).fail(function(jqxhr, status, error) {
