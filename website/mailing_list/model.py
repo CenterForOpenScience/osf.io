0cee519ef6f7b4aef398e75159853b147b4633bd
Matt Frazier <maf7sm@virginia.edu>
2016-05-23 23:09:30 -0400

    Remove unnecessary indices
    [skip ci]

--- website/mailing_list/model.py
+++ website/mailing_list/model.py
@@ -4,14 +4,12 @@ import datetime
 from modularodm import fields, StoredObject
 
 from framework.mongo import ObjectId
-from framework.mongo.utils import unique_on
 
-@unique_on(['destination_node', '_id'])
 class MailingListEventLog(StoredObject):
     _id = fields.StringField(primary=True, default=lambda: str(ObjectId()))
 
-    date_created = fields.DateTimeField(default=datetime.datetime.utcnow, index=True)
+    date_created = fields.DateTimeField(default=datetime.datetime.utcnow)
 
     email_content = fields.StringField()
-    sending_user = fields.ForeignField('user', index=True)
-    destination_node = fields.ForeignField('node', index=True)
+    sending_user = fields.ForeignField('user')
+    destination_node = fields.ForeignField('node')
