b96c18d6537222565375e685819764b88cd5ae18
erinspace <erin.braswell@gmail.com>
2016-05-24 11:51:47 -0400

    actually fix the js test for real

--- website/static/js/tests/osfHelpers.test.js
+++ website/static/js/tests/osfHelpers.test.js
@@ -180,8 +180,8 @@ describe('osfHelpers', () => {
 
     describe('valueProgressBar', () => {
         it('Returns a css class for a progress bar with a width specified on a scale of 1 to 5', () => {
-            for (var i = 1; i < 6; i++) {
-                assert.equal($osf.valueProgressBar[i].style, 'width: ' + (i+1)*20 + '%');
+            for (var i = 0; i < 6; i++) {
+                assert.equal($osf.valueProgressBar[i].style, 'width: ' + i*20 + '%');
             }
         });
     });
