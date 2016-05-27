879204f410850338ca200d733eca7e6761dfb51a
Steven Loria <sloria1@gmail.com>
2016-05-27 13:21:13 -0400

    Add institution logo links to Dashboard page
    implements part of OSF-6311
    
    - Adds InstitutionImg
    - Adds allInstitutions and currentUser.institutions to contextVars
    - Adds InstitutionsPanel component to dashboard

--- website/static/js/pages/home-page.js
+++ website/static/js/pages/home-page.js
@@ -6,16 +6,27 @@
 
 var $ = require('jquery');
 var m = require('mithril');
+var lodashGet = require('lodash.get');
 
 var QuickSearchProject = require('js/home-page/quickProjectSearchPlugin');
 var NewAndNoteworthy = require('js/home-page/newAndNoteworthyPlugin');
 var MeetingsAndConferences = require('js/home-page/meetingsAndConferencesPlugin');
+var InstitutionsPanel = require('js/home-page/institutionsPanelPlugin');
 
 var columnSizeClass = '.col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2';
 
 $(document).ready(function(){
     var osfHome = {
         view : function(ctrl, args) {
+            // Camel-case institutions keys
+            var _affiliatedInstitutions = lodashGet(window, 'contextVars.currentUser.institutions') || [];
+            var affiliatedInstitutions = _affiliatedInstitutions.map(function(inst) {
+                return {logoPath: inst.logo_path, id: inst.id, name: inst.name};
+            });
+            var _allInstitutions = lodashGet(window, 'contextVars.allInstitutions') || [];
+            var allInstitutions = _allInstitutions.map(function(inst) {
+                return {logoPath: inst.logo_path, id: inst.id, name: inst.name};
+            });
             return [
                 m('.quickSearch', m('.container.p-t-lg',
                     [
@@ -24,6 +35,16 @@ $(document).ready(function(){
                         ])
                     ]
                 )),
+                allInstitutions.length ? m('.institutions-panel', m('.container',
+                    [
+                        m('.row', [
+                            m(columnSizeClass,  m.component(InstitutionsPanel, {
+                                affiliatedInstitutions: affiliatedInstitutions,
+                                allInstitutions: allInstitutions
+                            }))
+                        ])
+                    ]
+                )) : '',
                 m('.newAndNoteworthy', m('.container',
                     [
                         m('.row', [
