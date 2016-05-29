4a417cc1490052fd019918b5f6e5a809e36104e1
erinspace <erin.braswell@gmail.com>
2016-05-29 17:36:56 -0400

    Remove DB writes from this script, only email

--- scripts/cleanup_failed_registrations.py
+++ scripts/cleanup_failed_registrations.py
@@ -10,6 +10,12 @@ from website.archiver import (
     ARCHIVER_FAILURE,
     ARCHIVER_INITIATED
 )
+
+from website import (
+    mails,
+    settings
+)
+
 from website.settings import ARCHIVE_TIMEOUT_TIMEDELTA
 from website.archiver.utils import handle_archive_fail
 from website.archiver.model import ArchiveJob
@@ -21,6 +27,7 @@ from scripts import utils as script_utils
 
 logger = logging.getLogger(__name__)
 
+
 def find_failed_registrations():
     expired_if_before = datetime.utcnow() - ARCHIVE_TIMEOUT_TIMEDELTA
     jobs = ArchiveJob.find(
@@ -30,7 +37,8 @@ def find_failed_registrations():
     )
     return {node.root for node in [job.dst_node for job in jobs] if node}
 
-def remove_failed_registrations(dry_run=True):
+
+def notify_desk_about_failed_registrations(dry_run=True):
     init_app(set_backends=True, routes=False)
     count = 0
     failed = find_failed_registrations()
@@ -40,26 +48,27 @@ def remove_failed_registrations(dry_run=True):
             if not f.registered_from:
                 logging.info('Node {0} had registered_from == None'.format(f._id))
                 continue
-            if not f.archive_job:  # Be extra sure not to delete legacy registrations
+            if not f.archive_job:  # Be extra sure not to deal with legacy registrations
                 continue
-            f.archive_job.status = ARCHIVER_FAILURE
-            f.archive_job.sent = True
-            f.archive_job.save()
-            handle_archive_fail(
-                ARCHIVER_UNCAUGHT_ERROR,
-                f.registered_from,
-                f,
-                f.creator,
-                f.archive_job.target_info()
+
+            # Send an email to the OSF Help Desk
+            mails.send_mail(
+                to_addr=settings.SUPPORT_EMAIL,
+                mail=mails.ARCHIVE_UNCAUGHT_ERROR_DESK,
+                user=f.creator,
+                src=f.registered_from,
+                results=f.archive_job.target_info(),
             )
+
             count += 1
-    logging.info('Cleaned {} registrations'.format(count))
+    logging.info('Found {} registrations, notified the OSF Help Desk'.format(count))
+
 
 def main():
     dry = 'dry' in sys.argv
     if not dry:
         script_utils.add_file_logger(logger, __file__)
-    remove_failed_registrations(dry_run=dry)
+    notify_desk_about_failed_registrations(dry_run=dry)
 
 if __name__ == '__main__':
     main()
