2cb91ba37363cc5f988755b91faa1fa16bca3c79
Shaquille Johnson <sjohns25@nd.edu>
2016-06-01 14:34:58 -0400

    fixed syntax issues

--- website/static/js/pages/base-page.js
+++ website/static/js/pages/base-page.js
@@ -128,7 +128,7 @@ $(function() {
     var adBlockPersistKey = 'adBlock';
     var $adBlock = $('#adBlock').on('closed.bs.alert', function() {
         $.cookie(adBlockPersistKey, '0', { expires: 1, path: '/'});
-    })
+    });
     var dismissed = $.cookie(adBlockPersistKey) === '0';
     if (!dismissed) {
         $adBlock.show();
