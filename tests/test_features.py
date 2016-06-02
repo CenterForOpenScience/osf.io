2e537d00acd2c986aaba7b649314cc1d19278087
Fitz Elliott <fitz@cos.io>
2016-06-02 00:09:28 -0400

    Purge all remnants of piwik from OSF

--- tests/test_features.py
+++ tests/test_features.py
@@ -7,10 +7,6 @@ requires_search = unittest.skipIf(
     not settings.SEARCH_ENGINE,
     'search disabled'
 )
-requires_piwik = unittest.skipIf(
-    settings.PIWIK_HOST is None,
-    'no PIWIK_HOST specified in settings'
-)
 requires_gnupg = unittest.skipIf(
     not settings.USE_GNUPG,
     'gnupg disabled'
