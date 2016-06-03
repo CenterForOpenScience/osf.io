044e116ec94721f5e32d7d3c4b4e970b7d8c18fe
Billy Hunt <billy@billyhunt.com>
2016-06-03 12:10:43 -0400

    change redirect url to cas

--- framework/auth/views.py
+++ framework/auth/views.py
@@ -199,10 +199,10 @@ def auth_logout(redirect_url=None):
     return resp
 
 
-def auth_email_logout(token, user):
+def auth_email_logout(token, user, redirect_url=None):
     """When a user is adding an email or merging an account, add the email to the user and log them out.
     """
-    redirect_url = web_url_for('auth_login')
+    redirect_url = cas.get_login_url(redirect_url)
     try:
         unconfirmed_email = user.get_unconfirmed_email_for_token(token)
     except InvalidTokenError:
