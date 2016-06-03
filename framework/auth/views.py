7715cef86c42841095543d5260b2e233b3afc847
Billy Hunt <billy@billyhunt.com>
2016-06-03 15:47:24 -0400

    update redirect for brevity and clarity

--- framework/auth/views.py
+++ framework/auth/views.py
@@ -202,8 +202,7 @@ def auth_logout(redirect_url=None):
 def auth_email_logout(token, user):
     """When a user is adding an email or merging an account, add the email to the user and log them out.
     """
-    no_redirect = None
-    redirect_url = cas.get_login_url(no_redirect)
+    redirect_url = cas.get_login_url(service_url='')
     try:
         unconfirmed_email = user.get_unconfirmed_email_for_token(token)
     except InvalidTokenError:
