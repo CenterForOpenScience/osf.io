5d9b8d53eaa6a8872fa17f40632d4555c88c346f
Longze Chen <cs.longze.chen@gmail.com>
2016-06-08 13:52:23 -0400

    Remove unused helper method. [skip ci]

--- website/landing_pages.py
+++ website/landing_pages.py
@@ -5,16 +5,11 @@ from framework.auth import decorators
 from website.util import web_url_for
 
 
-def connected_tools():
-    return _landing_page(title='Connected Tools',
-                         redirect_to=web_url_for('user_addons', _absolute=True))
-
-
-def enriched_profile():
-    return _landing_page(title='Enriched Profile',
-                         redirect_to=web_url_for('profile_view', _absolute=True))
+@decorators.must_be_logged_in
+def connected_tools(auth):
+    return redirect(web_url_for('user_addons'))
 
 
 @decorators.must_be_logged_in
-def _landing_page(auth, title, redirect_to, **kwargs):
-        return redirect(redirect_to)
+def enriched_profile(auth):
+    return redirect(web_url_for('profile_view'))
