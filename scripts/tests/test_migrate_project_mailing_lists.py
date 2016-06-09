077bef72d64ea3cd8fb15bd486d36c49dacb4900
Matt Frazier <maf7sm@virginia.edu>
2016-06-09 17:05:33 -0400

    Update script tests
    [skip ci]

--- scripts/tests/test_migrate_project_mailing_lists.py
+++ scripts/tests/test_migrate_project_mailing_lists.py
@@ -21,33 +21,37 @@ class TestMigrateMailingLists(OsfTestCase):
 
     @mock.patch('scripts.mailing_lists.migrate_project_mailing_lists.real_create_list')
     def test_migrate_node(self, mock_create):
-        node = ProjectFactory()
+        node = ProjectFactory(mailing_enabled=None)
         NotificationSubscription.remove()
         migrate(dry_run=False)
+        node.reload()
         assert_true(node.mailing_enabled)
         assert node._id in str(mock_create.call_args_list)
 
     @mock.patch('scripts.mailing_lists.migrate_project_mailing_lists.real_create_list')
     def test_migrate_node_with_parent(self, mock_create):
-        node = ProjectFactory(parent=NodeFactory())
+        node = ProjectFactory(parent=NodeFactory(mailing_enabled=None), mailing_enabled=None)
         NotificationSubscription.remove()
         migrate(dry_run=False)
+        node.reload()
         assert_true(node.mailing_enabled)
         assert node._id in str(mock_create.call_args_list)
         assert node.parent._id in str(mock_create.call_args_list)
 
     @mock.patch('scripts.mailing_lists.migrate_project_mailing_lists.real_create_list')
     def test_migrate_registration(self, mock_create):
-        node = RegistrationFactory()
+        node = RegistrationFactory(mailing_enabled=None)
         NotificationSubscription.remove()
         migrate(dry_run=False)
+        node.reload()
         assert_false(node.mailing_enabled)
         assert node._id not in str(mock_create.call_args_list)
 
     @mock.patch('scripts.mailing_lists.migrate_project_mailing_lists.real_create_list')
     def test_migrate_dashboard(self, mock_create):
-        node = CollectionFactory()
+        node = CollectionFactory(mailing_enabled=None)
         NotificationSubscription.remove()
         migrate(dry_run=False)
+        node.reload()
         assert_false(node.mailing_enabled)
         assert node._id not in str(mock_create.call_args_list)
