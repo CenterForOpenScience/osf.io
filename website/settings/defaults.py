8467681323673eca03ac99d60ed57da163700d00
Nan Chen <nc6r@virginia.edu>
2016-06-11 17:04:49 -0400

    code clean up

--- website/settings/defaults.py
+++ website/settings/defaults.py
@@ -314,6 +314,7 @@ LOW_PRI_MODULES = {
     'scripts.osfstorage.files_audit',
     'scripts.osfstorage.glacier_audit',
     'scripts.populate_new_and_noteworthy_projects',
+    'scripts.meeting_visit_count',
     'website.search.elastic_search',
 }
 
@@ -377,6 +378,7 @@ CELERY_IMPORTS = (
     'scripts.approve_embargo_terminations',
     'scripts.triggered_mails',
     'scripts.send_queued_mails',
+    'scripts.meeting_visit_count',
 )
 
 # Modules that need metrics and release requirements
@@ -446,6 +448,11 @@ else:
             'task': 'scripts.populate_new_and_noteworthy_projects',
             'schedule': crontab(minute=0, hour=2, day_of_week=6),  # Saturday 2:00 a.m.
             'kwargs': {'dry_run': False}
+        },
+        'meeting_visit_count': {
+            'task': 'scripts.meeting_visit_count',
+            'schedule': crontab(minute=0, hour=0),  # Daily 12 a.m
+            'kwargs': {'dry_run': False},
         },
     }
 
@@ -520,4 +527,3 @@ ESI_MEDIA_TYPES = {'application/vnd.api+json', 'application/json'}
 
 # Used for gathering meta information about the current build
 GITHUB_API_TOKEN = None
-
