c2660cb12807ebcc3d9278bfee993ecd2575c67d
Nan Chen <nc6r@virginia.edu>
2016-06-11 17:00:24 -0400

    modify to use keen and celery task

--- scripts/meeting_visit_count.py
+++ scripts/meeting_visit_count.py
@@ -7,9 +7,13 @@ import sys
 import requests
 from modularodm import Q
 
+from framework.celery_tasks import app as celery_app
+
 from website import settings
 from website.app import init_app
+from framework.transactions.context import TokuTransaction
 from website.models import Tag, Conference
+from website.files.models import StoredFileNode
 from scripts import utils as scripts_utils
 
 logger = logging.getLogger(__name__)
@@ -17,30 +21,38 @@ logging.basicConfig(level=logging.INFO)
 
 
 def main(dry_run=True):
+    init_app(routes=False)
     for conf in Conference.find():
         count = 0
         for tag in Tag.find(Q('lower', 'eq', conf.endpoint.lower())):
             for node in tag.node__tagged.find(Q('is_public', 'eq', True) & Q('is_deleted', 'eq', False)):
+                record = next(
+                    x for x in
+                    StoredFileNode.find(
+                        Q('node', 'eq', node) &
+                        Q('is_file', 'eq', True)
+                    ).limit(1)
+                ).wrapped()
+
                 if not dry_run:
-                    start_time = node.date_created.strftime('%Y-%m-%d')
-                    today = datetime.datetime.utcnow().strftime('%Y-%m-%d')
-                    url = 'https://osf.io/piwik/index.php?token_auth={}&module=API' \
-                          '&method=VisitsSummary.getVisits&format=json&period=range' \
-                          '&date={},{}&idSite={}'.format(
-                            settings.PIWIK_ADMIN_TOKEN,
-                            start_time, today,
-                            node.piwik_site_id)
+                    url = 'https://api.keen.io/3.0/projects/{}/events/meeting_view' \
+                          '?api_key={}&event_collection=pageviews&timezone=UTC&timeframe=this_14_days' \
+                          '&filters=%5B%7B%22property_name%22%3A%22page.info.path%22%2C%22' \
+                          'operator%22%3A%22eq%22%2C%22property_value%22%3A%22{}%22%7D%5D'.format(
+                            settings.KEEN_PROJECT_ID,
+                            settings.KEEN_READ_KEY,
+                            record._id)
+
                     resp = requests.get(url)
-                    node.visit = resp.json()['value']
+                    node.visit = resp.json()['result']
                     node.save()
                     count += 1
 
         logger.info('Get visit counts for {} projects in Conference {}'.format(count, conf.name))
     
 
-if __name__ == '__main__':
-    dry_run = 'dry' in sys.argv
-    init_app(routes=False)
-    if not dry_run:
-        scripts_utils.add_file_logger(logger, __file__)
+@celery_app.task(name='scripts.meeting_visit_count')
+def run_main(dry_run=True):
+    scripts_utils.add_file_logger(logger, __file__)
+    with TokuTransaction():
         main(dry_run=dry_run)
