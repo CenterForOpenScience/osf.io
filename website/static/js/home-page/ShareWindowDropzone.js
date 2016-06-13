883cda1c024dac1d6695065316509e3597dc4001
Maxwell Luo <maxwellluo@Guests-MacBook-Air.local>
2016-06-13 16:18:38 -0400

    Add tooltip, css changes

--- website/static/js/home-page/ShareWindowDropzone.js
+++ website/static/js/home-page/ShareWindowDropzone.js
@@ -14,6 +14,7 @@ window.ondrop = function(e) { e.preventDefault(); };
 
 var ShareWindowDropzone = {
   controller: function() {
+      var dangerCount = 0;
     Dropzone.options.shareWindowDropzone = {
         // Dropzone is setup to upload multiple files in one request this configuration forces it to do upload file-by-
         //file, one request at a time.
@@ -22,35 +23,43 @@ var ShareWindowDropzone = {
         autoProcessQueue: false,
         withCredentials: true,
         method:'put',
+        // in MB; lower to test errors. Default is 256 MB.
         maxFilesize: 1,
-
         accept: function(file, done) {
             if (this.files.length <= 10) {
                 this.options.url = waterbutler.buildUploadUrl(false,'osfstorage',window.contextVars['shareWindowId'], file,{});
                 this.processFile(file);
             }
-            else if (this.files.length > 10) {
-                $osf.growl("Error", "You can only upload 10 files at once.");
+            else  {
+                dangerCount = document.getElementsByClassName('alert').length;
+                if (dangerCount === 0)
+                    $osf.growl("Error", "You can only upload a maximum of 10 files at once.", "danger", 5000);
+
                 return this.emit("error", file);
-            }else{}
-        },
+            }},
+
         sending: function(file, xhr) {
             //Hack to remove webkitheaders
             var _send = xhr.send;
             xhr.send = function() {
                _send.call(xhr, file);
            };
-            $('.drop-zone-format').css({'padding-bottom': '10px'});
+           $('.drop-zone-format').css({'padding-bottom': '10px'});
         },
+
         success: function(file, xhr) {
             this.processQueue();
             file.previewElement.classList.add("dz-success");
             file.previewElement.classList.add("dz-preview-background-success");
         },
 
+
         error: function(file, message) {
             file.previewElement.classList.add("dz-error");
             file.previewElement.classList.add("dz-preview-background-error");
+            // Need the padding change twice because the padding doesn't resize when there is an error
+            $('.drop-zone-format').css({'padding-bottom': '10px'});
+
         },
     };
 
@@ -65,14 +74,13 @@ var ShareWindowDropzone = {
 
 
 
-      $('#ShareButton').click(function(e) {
+      $('#ShareButton').click(function() {
         document.getElementById("ShareButton").style.cursor = "pointer";
           $('#shareWindowDropzone').stop().slideToggle();
           $('#glyph').toggleClass('glyphicon glyphicon-chevron-down');
           $('#glyph').toggleClass('glyphicon glyphicon-chevron-up');
 
       });
-
 
   },
 
@@ -94,8 +102,16 @@ var ShareWindowDropzone = {
                 templatesFetcher: ctrl.templateNodes
             })))];
         }
+                // Activate Public Files tooltip info
+                  $('[data-toggle="tooltip"]').tooltip();
+
           return m('.row-bottom-xs', m('.col-xs-12.m-b-sm', headerTemplate()),
-              m('h4.text-center #LinkToShareFiles', 'View your ', m('a', {href: '/share_window/', onclick: function() {}}, 'Public Files Project')),
+              m('h4.text-center #LinkToShareFiles', 'View your ', m('a', {href: '/share_window/', onclick: function() {}}, 'Public Files Project '),
+                  m('i.fa.fa-question-circle.text-muted', {
+                    'data-toggle':  'tooltip',
+                    'title':  'The Public Files Project allows you to easily collaborate and share your files with anybody.',
+                    'data-placement' : 'bottom'
+                }, '')),
               m('div.p-v-xs.drop-zone-format.drop-zone-invis .pointer .panel #shareWindowDropzone',
               m('button.close[aria-label="Close"]',{ onclick : function() {
                   $('#shareWindowDropzone').hide();
@@ -103,7 +119,6 @@ var ShareWindowDropzone = {
                   $('#glyph').toggleClass('glyphicon glyphicon-chevron-up');
                   $('#glyph').toggleClass('glyphicon glyphicon-chevron-down');
                   $('.drop-zone-format').css({'padding-bottom': '180px'});
-
               }}, m('.drop-zone-close', '×')),
               m('.dz-p.text-center.top#shareWindowDropzone', m('h1.drop-zone-bold',  'Drop files to upload'),
                   'Click the box to upload files. Files are automatically uploaded to your ',
