80f5a63f68d55b91af969e1adbe0b8b6145bcfc6
Steven Loria <sloria1@gmail.com>
2016-06-14 11:15:16 -0400

    Add more logging to migration

--- scripts/migration/migrate_deleted_wikis.py
+++ scripts/migration/migrate_deleted_wikis.py
@@ -1,4 +1,6 @@
+# -*- coding: utf-8 -*-
 import logging
+from copy import deepcopy
 import sys
 
 from modularodm import Q
@@ -15,16 +17,24 @@ def get_targets():
 
 def migrate(targets, dry_run=True):
     # iterate over targets
+    count = 0
     for log in targets:
         node = log.node
+        logger.debug('Checking node {}'.format(node._id))
         versions = node.wiki_pages_versions
+        original_versions = deepcopy(versions)
         current = node.wiki_pages_current
         updated_versions = {}
         for wiki in versions:
             if wiki in current:
                 updated_versions[wiki] = versions[wiki]
-        node.wiki_pages_versions = updated_versions
-        node.save()
+
+        if original_versions != updated_versions:
+            logger.info('Updating wiki_pages_versions from {} to {}'.format(original_versions, updated_versions))
+            node.wiki_pages_versions = updated_versions
+            node.save()
+            count += 1
+    logger.info('Migrated {} nodes'.format(count))
 
 
 def main():
@@ -39,5 +49,5 @@ def main():
         if dry_run:
             raise RuntimeError('Dry run, transaction rolled back.')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
