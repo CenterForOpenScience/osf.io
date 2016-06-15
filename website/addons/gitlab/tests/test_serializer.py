63e9cdf0be1cef65a78524f884038879b2e1a155
Daniel Neis Araujo <danielneis@gmail.com>
2016-06-15 10:36:06 -0300

    copying github plugin as is

---
+++ website/addons/gitlab/tests/test_serializer.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""Serializer tests for the GitHub addon."""
+import mock
+from nose.tools import *  # noqa (PEP8 asserts)
+
+from website.addons.base.testing.serializers import StorageAddonSerializerTestSuiteMixin
+from website.addons.github.api import GitHubClient
+from website.addons.github.tests.factories import GitHubAccountFactory
+from website.addons.github.serializer import GitHubSerializer
+
+from tests.base import OsfTestCase
+
+class TestGitHubSerializer(StorageAddonSerializerTestSuiteMixin, OsfTestCase):
+
+    addon_short_name = 'github'
+
+    Serializer = GitHubSerializer
+    ExternalAccountFactory = GitHubAccountFactory
+    client = GitHubClient()
+
+    def set_provider_id(self, pid):
+        self.node_settings.repo = pid
+    
+    ## Overrides ##
+
+    def setUp(self):
+        super(TestGitHubSerializer, self).setUp()
+        self.mock_api_user = mock.patch("website.addons.github.api.GitHubClient.user")
+        self.mock_api_user.return_value = mock.Mock()
+        self.mock_api_user.start()
+
+    def tearDown(self):
+        self.mock_api_user.stop()
+        super(TestGitHubSerializer, self).tearDown()
