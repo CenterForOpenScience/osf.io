595ac554202e350074f97536c4156437b2cff54d
Longze Chen <cs.longze.chen@gmail.com>
2016-06-16 17:01:22 -0400

    Update settings.

--- website/settings/defaults.py
+++ website/settings/defaults.py
@@ -91,7 +91,7 @@ OSF_SESSION_TIMEOUT = 30 * 24 * 60 * 60  # 30 days in seconds
 # TODO: Override SECRET_KEY in local.py in production
 SECRET_KEY = 'CHANGEME'
 SESSION_COOKIE_SECURE = SECURE_MODE
-# TODO: Change to SECURE_MODE after ticket #OSF-6339 has been resolved
+# TODO: Change to True after ticket #OSF-6339 has been resolved
 SESSION_COOKIE_HTTPONLY = False
 
 # local path to private key and cert for local development using https, overwrite in local.py
