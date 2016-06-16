c317a4dfdbfe45bfb4b9510ce918e2d6c25ccdac
Lauren Barker <lauren@cos.io>
2016-06-16 17:10:37 -0400

    disconnect only notify_added_contributors for NotificationTestCase

--- website/notifications/listeners.py
+++ website/notifications/listeners.py
@@ -1,12 +1,10 @@
 from website.notifications.utils import subscribe_user_to_notifications
 from website.project.signals import contributor_added, project_created
-from website.project.views.contributor import notify_added_contributor
 
 @project_created.connect
 def subscribe_creator(node):
     subscribe_user_to_notifications(node, node.creator)
 
 @contributor_added.connect
-def subscribe_contributor(node, contributor, auth=None, *args, **kwargs):
+def subscribe_contributor(node, contributor, auth=None):
     subscribe_user_to_notifications(node, contributor)
-    notify_added_contributor(node, contributor, auth)
