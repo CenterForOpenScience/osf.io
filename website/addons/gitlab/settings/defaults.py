5277328d1652fea0ff40e91c6e159c2f8380890c
Luis Henrique Mulinari <luis.mulinari@gmail.com>
2016-06-20 15:08:42 +0000

    Add defaul GitLab URL

--- website/addons/gitlab/settings/defaults.py
+++ website/addons/gitlab/settings/defaults.py
@@ -15,6 +15,8 @@ HOOK_EVENTS = ['push']  # Only log commits
 OAUTH_AUTHORIZE_URL = 'https://gitlab.com/oauth/authorize'
 OAUTH_ACCESS_TOKEN_URL = 'https://gitlab.com/oauth/token'
 
+GITLAB_BASE_URL = 'https://gitlab.com'
+
 # Max render size in bytes; no max if None
 MAX_RENDER_SIZE = None
 
