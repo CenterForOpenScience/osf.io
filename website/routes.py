7bf3275dbbc5f4690fb2f9e3fbdb90648b49cffd
Steven Loria <sloria1@gmail.com>
2016-06-21 14:30:55 -0400

    Ensure maintenance collection is created before trying to read from it
    When doing implicitly collection creation, e.g. with
    `db.maintenance.find_one(...)`, the collection will not get
    created if no data are inserted into it. Therefore, every
    request would try to create the collection, causing lock not
    granted errors.

--- website/routes.py
+++ website/routes.py
@@ -30,6 +30,7 @@ from website import language
 from website.util import metrics
 from website.util import paths
 from website.util import sanitize
+from website import maintenance
 from website.models import Institution
 from website import landing_pages as landing_page_views
 from website import views as website_views
@@ -52,9 +53,6 @@ def get_globals():
     user = _get_current_user()
     user_institutions = [{'id': inst._id, 'name': inst.name, 'logo_path': inst.logo_path} for inst in user.affiliated_institutions] if user else []
     all_institutions = [{'id': inst._id, 'name': inst.name, 'logo_path': inst.logo_path} for inst in Institution.find().sort('name')]
-    maintenance = database.maintenance.find_one({'maintenance': True})
-    if maintenance:
-        del maintenance['_id']
     if request.host_url != settings.DOMAIN:
         try:
             inst_id = (Institution.find_one(Q('domains', 'eq', request.host.lower())))._id
@@ -106,7 +104,7 @@ def get_globals():
         'enable_institutions': settings.ENABLE_INSTITUTIONS,
         'keen_project_id': settings.KEEN_PROJECT_ID,
         'keen_write_key': settings.KEEN_WRITE_KEY,
-        'maintenance': maintenance,
+        'maintenance': maintenance.get_maintenance(),
     }
 
 def is_private_link_anonymous_view():
