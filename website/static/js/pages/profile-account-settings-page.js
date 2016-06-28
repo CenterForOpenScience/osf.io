c097d931a1977677691bc705f9911914e3919f35
erinspace <erin.braswell@gmail.com>
2016-06-28 15:21:19 -0400

    Use proper name and args for setPassword

--- website/static/js/pages/profile-account-settings-page.js
+++ website/static/js/pages/profile-account-settings-page.js
@@ -3,14 +3,15 @@
 var $ = require('jquery');
 var $osf = require('js/osfHelpers.js');
 var accountSettings = require('js/accountSettings.js');
-var changePassword = require('js/setPassword');
+var SetPassword = require('js/setPassword');
+
 
 $(function() {
     var viewModel = new accountSettings.UserProfileViewModel();
     $osf.applyBindings(viewModel, '#connectedEmails');
     viewModel.init();
 
-    new changePassword('#changePassword', 'change');
+    new SetPassword('#changePassword', 'change');
 
     $osf.applyBindings(
         new accountSettings.DeactivateAccountViewModel(),
