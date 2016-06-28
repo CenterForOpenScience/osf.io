00d99649cece418afb74ce2d418da8073fbe7ee9
Maxwell Luo <mustmax123@yahoo.com>
2016-06-28 10:47:01 -0400

    Add CSS changes

--- website/static/js/home-page/dropzonePreviewTemplate.js
+++ website/static/js/home-page/dropzonePreviewTemplate.js
@@ -15,7 +15,7 @@ function shareButton(link) {
                 m('span', 'Copy Share Link')
             )
         ),
-        m('input[value="' + link + '"][readonly="readonly"][style="height: 30px;color:#333333;min-width:175px;"]')
+        m('input[value="' + link + '"][readonly="readonly"][onclick="this.select();"][style="height: 30px;color:#333333;min-width:175px;"]')
     );
 }
 
@@ -36,19 +36,19 @@ function dropzonePreviewTemplate() {
             ),
 
 
-            m('div.col-sm-6.p-xs', m('span.p-md', 'Generating Share Link'),
+            m('div.col-sm-6.p-xs.text-center', m('span.p-md', 'Generating Share Link'),
                 m('div.dz-progress',
                     m('span[data-dz-uploadprogress].dz-upload')
                 )
             ),
 
             m('.dz-logo-spin',
-                m('span.logo-spin.m-r-md.logo-sm')
+                m('span.logo-spin.m-r-sm.logo-sm')
             ),
             m('.dz-success-mark',
                 m('span.fa.fa-check-circle-o.p-xs')
             ),
-            m('span.button.close.pull-right.p-r-md[data-dz-remove]', '×'),
+            m('span.button.close.p-r-sm[data-dz-remove]', '×'),
 
 
             m('.dz-error-mark.col-sm-1',
