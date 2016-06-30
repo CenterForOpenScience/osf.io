c0b7f1adf0de1d43243acdf16fc697a3b46cd4c8
Steven Loria <sloria1@gmail.com>
2016-06-30 17:06:16 -0400

    Paginate query to avoid cursor tieout

--- scripts/consistency/ensure_piwik_nodes.py
+++ scripts/consistency/ensure_piwik_nodes.py
@@ -7,6 +7,7 @@ import sys
 from modularodm import Q
 
 from framework.analytics import piwik
+from framework.mongo.utils import paginated
 from scripts import utils as scripts_utils
 from website.app import init_app
 from website.models import Node
@@ -20,14 +21,15 @@ def main():
     if not dry:
         scripts_utils.add_file_logger(logger, __file__)
 
-    nodes = Node.find(Q('piwik_site_id', 'eq', None))
-    count = nodes.count()
+    nodes = paginated(Node, Q('piwik_site_id', 'eq', None), increment=15)
+    count = 0
     for node in nodes:
         logger.info('Provisioning Piwik node for Node {}'.format(node._id))
         if not dry:
             piwik._provision_node(node._id)
             # Throttle to reduce load on Piwik
             time.sleep(1)
+        count += 1
     logger.info('Provisioned {} nodes'.format(count))
 
 
