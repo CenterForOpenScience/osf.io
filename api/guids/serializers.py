5bf176938d0a1280c027abcd146c7e8f4de4e5aa
Sam Chrisinger <s.chrisinger@gmail.com>
2016-06-30 15:49:56 -0400

    Add self and html links to guid serialization

--- api/guids/serializers.py
+++ api/guids/serializers.py
@@ -1,14 +1,19 @@
-from website.models import Node, User, Guid
-from website.files.models import FileNode
+import urlparse
 
-from api.base.serializers import (JSONAPISerializer, IDField, TypeField, RelationshipField)
+from website.models import Node, User, Guid
+from website.files.models.base import StoredFileNode
+from website import settings as website_settings
+
+from api.base.utils import absolute_reverse
+
+from api.base.serializers import (JSONAPISerializer, IDField, TypeField, RelationshipField, LinksField)
 
 def get_type(record):
     if isinstance(record, Node):
         return 'nodes'
     elif isinstance(record, User):
         return 'users'
-    elif isinstance(record, FileNode):
+    elif isinstance(record, StoredFileNode):
         return 'files'
     elif isinstance(record, Guid):
         return get_type(record.referent)
@@ -44,9 +49,18 @@ class GuidSerializer(JSONAPISerializer):
             'type': 'get_type'
         }
     )
+    links = LinksField({
+        'self': 'get_absolute_url',
+        'html': 'get_absolute_html_url'
+    })
 
     def get_type(self, guid):
         return get_type(guid.referent)
 
     def get_absolute_url(self, obj):
-        return obj.referent.get_absolute_url()
+        return absolute_reverse('guids:guid-detail', kwargs={'guids': obj._id})
+
+    def get_absolute_html_url(self, obj):
+        if not isinstance(obj.referent, StoredFileNode):
+            return obj.referent.absolute_url
+        return urlparse.urljoin(website_settings.DOMAIN, '/{}/'.format(obj._id))
