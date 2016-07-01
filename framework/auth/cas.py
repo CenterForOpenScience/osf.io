94e5f878b8ede404608257695f84e0f3f03cff02
Longze Chen <cs.longze.chen@gmail.com>
2016-07-01 09:18:49 -0400

    [Feature] Add HttpOnly flag for OSF, API and ADMIN cookie [#OSF-6339, #SEC-16] (#5853)
    * Test XHR-CORS and withCrentials=false.
    
    * Still need CORS for OSF-WB and OSF-MFR.
    
    * Set HttpOnly flag for OSF, ADMIN and API cookie. [skip ci]
        - always True, indepedent of SECURE_MODE
        - add MSIE 9 compatibility exception
    
    * temporary commit, please rebse [skip ci]
    
    * Remove IE9 httponly compatibility support.
    
    * Clean up code in framework/auth/cas.py and framework/sessions/__init__.py.
        - fix imports order
        - remove unused params
        - unify comments style
        - rename shadowing variable names
    
    * Remove accidentally added _log_tempates.mako from merge.

--- framework/auth/cas.py
+++ framework/auth/cas.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-import json
-import urllib
-import httplib as http
 
 import furl
+import httplib as http
+import json
+import urllib
+
 from lxml import etree
 import requests
 
@@ -16,11 +17,13 @@ from website import settings
 
 class CasError(HTTPError):
     """General CAS-related error."""
+
     pass
 
 
 class CasHTTPError(CasError):
     """Error raised when an unexpected error is returned from the CAS server."""
+
     def __init__(self, code, message, headers, content):
         super(CasHTTPError, self).__init__(code, message)
         self.headers = headers
@@ -35,6 +38,7 @@ class CasHTTPError(CasError):
 
 class CasTokenError(CasError):
     """Raised if an invalid token is passed by the client."""
+
     def __init__(self, message):
         super(CasTokenError, self).__init__(http.BAD_REQUEST, message)
 
@@ -81,14 +85,15 @@ class CasClient(object):
         return url.url
 
     def service_validate(self, ticket, service_url):
-        """Send request to validate ticket.
+        """
+        Send request to CAS to validate ticket.
 
-        :param str ticket: CAS service ticket.
-        :param str service_url: Service URL from which the authentication request
-            originates.
+        :param str ticket: CAS service ticket
+        :param str service_url: Service URL from which the authentication request originates
         :rtype: CasResponse
         :raises: CasError if an unexpected response is returned
         """
+
         url = furl.furl(self.BASE_URL)
         url.path.segments.extend(('p3', 'serviceValidate',))
         url.args['ticket'] = ticket
@@ -101,12 +106,14 @@ class CasClient(object):
             self._handle_error(resp)
 
     def profile(self, access_token):
-        """Send request to get profile information, given an access token.
+        """
+        Send request to get profile information, given an access token.
 
         :param str access_token: CAS access_token.
         :rtype: CasResponse
         :raises: CasError if an unexpected response is returned.
         """
+
         url = self.get_profile_url()
         headers = {
             'Authorization': 'Bearer {}'.format(access_token),
@@ -168,9 +175,14 @@ class CasClient(object):
 
 
 def parse_auth_header(header):
-    """Given a Authorization header string, e.g. 'Bearer abc123xyz', return a token
-    or raise an error if the header is invalid.
     """
+    Given an Authorization header string, e.g. 'Bearer abc123xyz',
+    return a token or raise an error if the header is invalid.
+
+    :param header:
+    :return:
+    """
+
     parts = header.split()
     if parts[0].lower() != 'bearer':
         raise CasTokenError('Unsupported authorization type')
@@ -186,11 +198,13 @@ def get_client():
 
 
 def get_login_url(*args, **kwargs):
-    """Convenience function for getting a login URL for a service.
+    """
+    Convenience function for getting a login URL for a service.
 
     :param args: Same args that `CasClient.get_login_url` receives
     :param kwargs: Same kwargs that `CasClient.get_login_url` receives
     """
+
     return get_client().get_login_url(*args, **kwargs)
 
 
@@ -199,24 +213,31 @@ def get_institution_target(redirect_url):
 
 
 def get_logout_url(*args, **kwargs):
-    """Convenience function for getting a logout URL for a service.
+    """
+    Convenience function for getting a logout URL for a service.
 
     :param args: Same args that `CasClient.get_logout_url` receives
     :param kwargs: Same kwargs that `CasClient.get_logout_url` receives
     """
+
     return get_client().get_logout_url(*args, **kwargs)
 
 
 def get_profile_url():
-    """Convenience function for getting a profile URL for a user.
-    """
+    """Convenience function for getting a profile URL for a user."""
+
     return get_client().get_profile_url()
 
 
 def make_response_from_ticket(ticket, service_url):
-    """Given a CAS ticket and service URL, attempt to the user and return a proper
-    redirect response.
     """
+    Given a CAS ticket and service URL, attempt to validate the user and return a proper redirect response.
+
+    :param str ticket: CAS service ticket
+    :param str service_url: Service URL from which the authentication request originates
+    :return:
+    """
+
     service_furl = furl.furl(service_url)
     if 'ticket' in service_furl.args:
         service_furl.args.pop('ticket')
