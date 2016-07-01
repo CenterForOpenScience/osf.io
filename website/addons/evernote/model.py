198a1ac85d85025826311df2c54a1d689e209971
Raymond Yee <raymond.yee@gmail.com>
2016-07-01 11:15:51 -0700

    simplifying evernote/model.py based on feedback

--- website/addons/evernote/model.py
+++ website/addons/evernote/model.py
@@ -20,7 +20,6 @@ class Evernote(ExternalProvider):
 
     client_id = settings.EVERNOTE_CLIENT_ID
     client_secret = settings.EVERNOTE_CLIENT_SECRET
-    #_sandbox = settings.EVERNOTE_SANDBOX
 
     auth_url_base = '{}/OAuth.action'.format(settings.BASE_URL)
     request_token_url = '{}/oauth'.format(settings.BASE_URL)
@@ -45,7 +44,6 @@ class EvernoteUserSettings(AddonOAuthUserSettingsBase):
     oauth_provider = Evernote
     serializer = EvernoteSerializer
 
-
 class EvernoteNodeSettings(StorageAddonBase, AddonOAuthNodeSettingsBase):
     oauth_provider = Evernote
     serializer = EvernoteSerializer
@@ -54,20 +52,18 @@ class EvernoteNodeSettings(StorageAddonBase, AddonOAuthNodeSettingsBase):
     folder_name = fields.StringField()
     folder_path = fields.StringField()
 
-    # to hold data about current Notebook
-    _folder_data = None
-
     def set_user_auth(self, user_settings):
-        """Import a user's Evernote authentication and create a NodeLog.
-        :param EvernoteUserSettings user_settings: The user settings to link.
-        """
+
         self.user_settings = user_settings
-        #nodelogger = BoxNodeLogger(node=self.owner, auth=Auth(user_settings.owner))
-        #nodelogger.log(action="node_authorized", save=True)
+        self.nodelogger.log(action='node_authorized', save=True)
 
     def set_folder(self, folder_id, auth):
+
         self.folder_id = str(folder_id)
-        self._update_folder_data()
+        client = utils.get_evernote_client(self.external_account.oauth_key)
+        _folder_data = utils.get_notebook(client, self.folder_id)
+        self.folder_name = _folder_data['name']
+        self.folder_path = _folder_data['name']
         self.save()
 
         if not self.complete:
@@ -78,42 +74,10 @@ class EvernoteNodeSettings(StorageAddonBase, AddonOAuthNodeSettingsBase):
             )
             self.user_settings.save()
 
-        # Add log to node
-        #nodelogger = BoxNodeLogger(node=self.owner, auth=auth)
-        #nodelogger.log(action="folder_selected", save=True)
-
-    def _update_folder_data(self):
-        """
-        given the folder_id, compute folder_name, folder_path
-        """
-        if self.folder_id is None:
-            return None
-
-        if not self._folder_data:
-            try:
-                # don't know whether we'll need to do something like this
-                #refresh_oauth_key(self.external_account)
-                client = utils.get_evernote_client(self.external_account.oauth_key)
-                self._folder_data = utils.get_notebook(client, self.folder_id)
-            except Exception:
-                return
-
-            self.folder_name = self._folder_data['name']
-            self.folder_path = self._folder_data['name']
-
-            self.save()
-
-    # Sam C says following can be removed https://github.com/CenterForOpenScience/osf.io/pull/4670/files#r49327525
-    # boilerplate from https://github.com/CenterForOpenScience/osf.io/blob/e4e1bd951b6e79d3bb2335d326bb44d9939bffb8/website/addons/box/model.py#L94-L99
-    # @property
-    # def complete(self):
-    #     return bool(self.has_auth and self.user_settings.verify_oauth_access(
-    #         node=self.owner,
-    #         external_account=self.external_account,
-    #     ))
+        self.nodelogger.log(action='folder_selected', save=True)
 
     # based on https://github.com/CenterForOpenScience/osf.io/blob/4a5d4e5a887c944174694300c42b399638184722/website/addons/box/model.py#L105-L107
     def fetch_full_folder_path(self):
         # don't know why this would be needed for Evernote
-        #self._update_folder_data()
+
         return self.folder_path
--- website/addons/evernote/settings/__init__.py
+++ website/addons/evernote/settings/__init__.py
@@ -9,4 +9,4 @@ except ImportError as error:
     logger.warn('No local.py settings file found')
 
 BASE_URL = 'https://www.evernote.com' if not EVERNOTE_SANDBOX  \
-       else 'https://sandbox.evernote.com'
+    else 'https://sandbox.evernote.com'
