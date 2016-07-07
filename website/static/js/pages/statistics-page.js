2ebbb2bf5d313373e2209ba45f563b5f83f88c3a
Fitz Elliott <fitz@cos.io>
2016-07-07 10:41:02 -0400

    Factor out date range picker into its own module

--- website/static/js/pages/statistics-page.js
+++ website/static/js/pages/statistics-page.js
@@ -1,11 +1,9 @@
 'use strict';
 
-require('pikaday-css');
-
 var $  = require('jquery');
-var pikaday = require('pikaday');
 var Cookie = require('js-cookie');
 var keenAnalysis = require('keen-analysis');
+var DateRangePicker = require('js/dateRangePicker');
 var ProjectUsageStatistics = require('js/statistics');
 
 $(function(){
@@ -20,79 +18,46 @@ $(function(){
     }
 });
 
+
+
+
 keenAnalysis.ready(function(){
     function drawAllCharts(statsStartDate, statsEndDate) {
-        projectUsageStats.visitsByDay('#visits', startDate, endDate);
-        projectUsageStats.topReferrers('#topReferrers', startDate, endDate);
-        projectUsageStats.popularPages('#popularPages', startDate, endDate, window.contextVars.node.title);
-        projectUsageStats.visitsServerTime('#serverTimeVisits', startDate, endDate);
-    }
-
-    function updateStartDate() {
-        startPicker.setStartRange(startDate);
-        endPicker.setStartRange(startDate);
-        endPicker.setMinDate(startDate);
-    }
-
-    function updateEndDate() {
-        startPicker.setEndRange(endDate);
-        startPicker.setMaxDate(endDate);
-        endPicker.setEndRange(endDate);
+        projectUsageStats.visitsByDay('#visits', statsStartDate, statsEndDate);
+        projectUsageStats.topReferrers('#topReferrers', statsStartDate, statsEndDate);
+        projectUsageStats.visitsServerTime('#serverTimeVisits', statsStartDate, statsEndDate);
+        projectUsageStats.popularPages(
+            '#popularPages', statsStartDate, statsEndDate,
+            window.contextVars.node.title
+        );
     }
 
     var oneDayInMs = 24 * 60 * 60 * 1000;
     var thirtyDaysAgoInMs = 30 * oneDayInMs;
+
     var statsMinDate = new Date(2013, 5, 1);
     var statsMaxDate = new Date();
 
-    var startDate = new Date(Date.now() - thirtyDaysAgoInMs);
     var startPickerElem = document.getElementById('startDatePicker');
-    var startPicker = new pikaday(
-        {
-            bound: true,
-            field: startPickerElem,
-            defaultDate: startDate,
-            setDefaultDate: true,
-            minDate: statsMinDate,
-            maxDate: statsMaxDate,
-            onSelect: function() {
-                startDate = this.getDate();
-                updateStartDate();
-                startPickerElem.value = this.toString();
-            }
-        }
-    );
+    var startDate = new Date(Date.now() - thirtyDaysAgoInMs);
 
     var endPickerElem = document.getElementById('endDatePicker');
     var endDate = new Date(Date.now() - oneDayInMs);
-    var endPicker = new pikaday(
-        {
-            bound: true,
-            field: endPickerElem,
-            defaultDate: endDate,
-            setDefaultDate: true,
-            minDate: statsMinDate,
-            maxDate: statsMaxDate,
-            onSelect: function() {
-                endDate = this.getDate();
-                updateEndDate();
-                endPickerElem.value = this.toString();
-            }
-        }
+
+    var dateRangePicker = new DateRangePicker(
+        startPickerElem, startDate,
+        endPickerElem, endDate,
+        statsMinDate, statsMaxDate
     );
-
-
-    updateStartDate();
-    updateEndDate();
 
     var projectUsageStats = new ProjectUsageStatistics(
         window.contextVars.keen.public.projectId,
         window.contextVars.keen.public.readKey
     );
-    drawAllCharts(startDate, endDate);
+    drawAllCharts(dateRangePicker.startDate, dateRangePicker.endDate);
 
     $('#updateStatsDates').on('submit', function() {
-        drawAllCharts(startDate, endDate);
+        drawAllCharts(dateRangePicker.startDate, dateRangePicker.endDate);
         return false;
     });
 
