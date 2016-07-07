38ed096a7eb6b0b0dd6088205afb41dd36edee63
John Tordoff <johnetordoff@gmail.com>
2016-07-07 10:50:37 -0400

    fix dumb branch error

--- website/static/img/api/metaschemas/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from api.metaschemas import views
-
-urlpatterns = [
-    url(r'^$', views.MetaSchemasList.as_view(), name=views.MetaSchemasList.view_name),
-    url(r'^(?P<metaschema_id>\w+)/$', views.MetaSchemaDetail.as_view(), name=views.MetaSchemaDetail.view_name)
-]
--- website/static/img/api/tokens/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from api.tokens import views
-
-urlpatterns = [
-    url(r'^$', views.TokenList.as_view(), name='token-list'),
-    url(r'^(?P<_id>\w+)/$', views.TokenDetail.as_view(), name='token-detail')
-]
