b7aeb00f0f7e9e1864b106eb1da3913837aae6d9
John Tordoff <johnetordoff@gmail.com>
2016-07-20 18:17:52 -0400

    make migration script legit

--- scripts/migrate_public_files_nodes.py
+++ scripts/migrate_public_files_nodes.py
@@ -2,20 +2,17 @@
 # encoding: utf-8
 """Migrate Share Windows to existing users
 
-Run dry run: python -m scripts.migrate_public_files dry
-Run migration: python -m scripts.migrate_public_files
+Run dry run: python -m scripts.migrate_public_files_nodes --dry
+Run migration: python -m scripts.migrate_public_files_nodes
 
 """
 import sys
 import logging
 
-from modularodm import Q
-
 from website.app import init_app
 from framework.auth import core
-from website.project.model import Node
-from modularodm.exceptions import NoResultsFound
-from website.project import new_public_files_collection
+from website.public_files import give_user_public_files_node
+from framework.transactions.context import TokuTransaction
 
 from scripts import utils as script_utils
 
@@ -23,22 +20,20 @@ from scripts import utils as script_utils
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-
-def get_targets():
-    return core.User.find()
-
-
 def main(dry_run):
-    users = get_targets()
-    for user in users:
-        try:
-            Node.find_one(Q('is_public_files_collection', 'eq', True) & Q('contributors', 'eq', user._id))
-        except NoResultsFound:
-            new_public_files_collection(user)
+    users = core.User.find()
+
+    with TokuTransaction():
+        for user in users:
+            if user.is_registered and user.public_files_node is None:
+                give_user_public_files_node(user)
+
+        if dry_run:
+            raise BaseException
 
 if __name__ == '__main__':
     init_app(set_backends=True, routes=False)
-    dry_run = 'dry' in sys.argv
+    dry_run = '--dry' in sys.argv
 
     # Log to file
     if not dry_run:
