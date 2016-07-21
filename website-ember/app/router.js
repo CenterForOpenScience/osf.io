c6ef5ce0e2f973bd4a8852c3b8994132169abd6e
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-07-21 15:20:05 -0400

    Copy some of the template logic from project/contributors/mako.

--- website-ember/app/router.js
+++ website-ember/app/router.js
@@ -6,8 +6,8 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('ember-sample');
-  this.route('node-contributors', { path: '/:node_id/contributors' });
+    this.route('ember-sample');
+    this.route('node-contributors', { path: '/:node_id/contributors' });
 });
 
 export default Router;
