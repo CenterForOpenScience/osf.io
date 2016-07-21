c0d211ab714c37c270e0dbe0cbda7d762e345f7d
Nan Chen <nc6r@virginia.edu>
2016-07-20 21:31:09 -0400

    api tests all fixed

--- api_tests/registrations/views/test_withdrawn_registrations.py
+++ api_tests/registrations/views/test_withdrawn_registrations.py
@@ -71,7 +71,7 @@ class TestWithdrawnRegistrations(NodeCRUDTestCase):
     def test_cannot_access_withdrawn_node_links_detail(self):
         url = '/{}registrations/{}/node_links/{}/'.format(API_BASE, self.registration._id, self.public_pointer._id)
         res = self.app.get(url, auth=self.user.auth, expect_errors=True)
-        assert_equal(res.status_code, 403)
+        assert_equal(res.status_code, 404)
 
     def test_cannot_access_withdrawn_node_links_list(self):
         url = '/{}registrations/{}/node_links/'.format(API_BASE, self.registration._id)
