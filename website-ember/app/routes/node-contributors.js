117983410f3aa79e3f55c3fb874676e8255553ac
Andy Boughton <abought@gmail.com>
2016-07-22 13:55:04 -0400

    Add project navbar to page, and some styling to work with that.
    [ci skip]
    [#EOSF-116]

--- website-ember/app/routes/node-contributors.js
+++ website-ember/app/routes/node-contributors.js
@@ -1,7 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    currentUser: Ember.inject.service(),
+
     model(params) {
         return this.store.findRecord('node', params.node_id);
+    },
+
+    setupController(controller) {
+        this.get('currentUser').load()
+            .then((user) => controller.set('user', user))
+            .catch(() => controller.set('user', null));
+
+        return this._super(...arguments);
     }
 });
