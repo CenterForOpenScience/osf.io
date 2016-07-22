a5461ca24b6f982dae9da175f1db0aea3280a9f9
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-07-22 15:43:17 -0400

    Only show Discard Changes button if contributor attributes have proposed changes.

--- website-ember/app/components/contributor-manager/component.js
+++ website-ember/app/components/contributor-manager/component.js
@@ -3,12 +3,16 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     permissionChanges: {},
     bibliographicChanges: {},
+    changed: false,
+    canSubmit: false,
     actions: {
-        permissionChange(contributor, permission) {
+        permissionChange(contributor, contributors, permission) {
             this.set(`permissionChanges.${contributor.id}`, permission);
+            this.set('changed', this.attributesChanged(contributors));
         },
-        bibliographicChange(contributor, isBibliographic) {
+        bibliographicChange(contributor, contributors, isBibliographic) {
             this.set(`bibliographicChanges.${contributor.id}`, isBibliographic);
+            this.set('changed', this.attributesChanged(contributors));
         },
         updateContributors() {
             this.sendAction(
@@ -18,5 +22,20 @@ export default Ember.Component.extend({
                 this.get('bibliographicChanges')
             );
         }
-    }
+    },
+    attributesChanged: function(contributors) {
+        var _this = this;
+        var changed = false;
+        contributors.content.canonicalState.forEach(function(contrib) {
+            var changedAttribute = _this.get('permissionChanges')[contrib.id];
+            if (changedAttribute && contrib._data.permission !== changedAttribute) {
+                changed = true;
+            }
+            var changedAttribute = _this.get('bibliographicChanges')[contrib.id];
+            if (changedAttribute !== undefined && contrib._data.bibliographic !== changedAttribute) {
+                changed = true;
+            }
+        });
+        return changed;
+    },
 });
