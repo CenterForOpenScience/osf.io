b875e1429e179c7b6b7b9439293deaae7cf5495f
Andy Boughton <abought@gmail.com>
2016-07-25 10:20:41 -0400

    Start switching over to bootstrap-modals
    [ci skip]
    [#EOSF-116]

--- website-ember/app/controllers/node-contributors.js
+++ website-ember/app/controllers/node-contributors.js
@@ -4,10 +4,8 @@ export default Ember.Controller.extend({
     canEdit: true, // TODO: Implement based on comments PR logic
 
     showModalAddContributors: false,
-    dummyVal: 'bob!',
     actions: {
         toggleAddContributorModal() {
-            console.log('hit handler');
             this.toggleProperty('showModalAddContributors');
         }
     }
--- website-ember/ember-cli-build.js
+++ website-ember/ember-cli-build.js
@@ -6,6 +6,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
     var app = new EmberApp(defaults, {
         // Add options here
+        'ember-bootstrap': {
+            'importBootstrapTheme': true
+        }
     });
 
     // Use `app.import` to add additional libraries to the generated
