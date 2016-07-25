5aeb847abe00ee84cc965dae8357be546a129f4c
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-07-24 20:57:57 -0400

    Only display Save Changes button if 1) there are proposed changes 2) at least one admin 3) at least one bibliographic contrib.

--- website-ember/app/components/contributor-manager/component.js
+++ website-ember/app/components/contributor-manager/component.js
@@ -4,15 +4,19 @@ export default Ember.Component.extend({
     permissionChanges: {},
     bibliographicChanges: {},
     changed: false,
-    canSubmit: false,
+    hasMinAdmins: false,
+    hasMinBibliographic: false,
+    canSubmit: Ember.computed('hasMinAdmins', 'hasMinBibliographic', 'changed', function() {
+        return this.get('hasMinAdmins') && this.get('hasMinBibliographic') && this.get('changed');
+    }),
     actions: {
         permissionChange(contributor, contributors, permission) {
             this.set(`permissionChanges.${contributor.id}`, permission);
-            this.set('changed', this.attributesChanged(contributors));
+            this.updateAttributes(contributors);
         },
         bibliographicChange(contributor, contributors, isBibliographic) {
             this.set(`bibliographicChanges.${contributor.id}`, isBibliographic);
-            this.set('changed', this.attributesChanged(contributors));
+            this.updateAttributes(contributors);
         },
         updateContributors() {
             this.sendAction(
@@ -23,19 +27,50 @@ export default Ember.Component.extend({
             );
         }
     },
-    attributesChanged: function(contributors) {
+    updateAttributes: function(contributors) {
+        var proposedChanges = this.checkProposedChanges(contributors);
+        this.set('changed', proposedChanges.changed);
+        proposedChanges.numAdmins > 0 ? this.set('hasMinAdmins', true) : this.set('hasMinAdmins', false);
+        proposedChanges.numBibliographic > 0 ? this.set('hasMinBibliographic', true) : this.set('hasMinBibliographic', false);
+    },
+    checkProposedChanges: function(contributors) {
         var _this = this;
         var changed = false;
-        contributors.content.canonicalState.forEach(function(contrib) {
-            var changedAttribute = _this.get('permissionChanges')[contrib.id];
-            if (changedAttribute && contrib._data.permission !== changedAttribute) {
+        var numAdmins = 0;
+        var numBibliographic = 0;
+
+        contributors.content.canonicalState.slice(0).forEach(function(contrib, index) {
+            var changedPermission = _this.get('permissionChanges')[contrib.id];
+            var originalPermission = contrib._data.permission;
+            if (changedPermission && (originalPermission !== changedPermission)) {
                 changed = true;
+                if (changedPermission === 'admin') {
+                    numAdmins++;
+                }
+            } else {
+                if (originalPermission === 'admin') {
+                    numAdmins++;
+                }
             }
-            var changedAttribute = _this.get('bibliographicChanges')[contrib.id];
-            if (changedAttribute !== undefined && contrib._data.bibliographic !== changedAttribute) {
+
+            var changedBibliographic = _this.get('bibliographicChanges')[contrib.id];
+            var originalBibliographic = contrib._data.bibliographic;
+            if ((changedBibliographic !== undefined) && (originalBibliographic !== changedBibliographic)) {
                 changed = true;
+                if (changedBibliographic === true) {
+                    numBibliographic++;
+                }
+            } else {
+                if (originalBibliographic === true) {
+                    numBibliographic++;
+                }
             }
         });
-        return changed;
-    },
+
+        return {
+            changed: changed,
+            numAdmins: numAdmins,
+            numBibliographic: numBibliographic
+        };
+    }
 });
