1a6c914acad409fa3708fbbfb457bcf47dd0bbe5
Andy Boughton <abought@gmail.com>
2016-07-25 14:07:19 -0400

    Convert some buttons and results rendering- WIP
    [ci skip]
    [#EOSF-116]

--- website-ember/app/components/modal-add-contribs.js
+++ website-ember/app/components/modal-add-contribs.js
@@ -11,8 +11,16 @@ export default Ember.Component.extend({
      */
     page: 'whom',  // TODO: Replace this with components; temp feature
 
+    //////////////////////////////////
+    //  OPTIONS FOR THE WHOM PAGE   //
+    //////////////////////////////////
     // The username to search for
     _searchText: null,
+    // Store results array from search. TODO: Make this store a promise so we can check fulfilled status
+    searchResults: null,
+
+    // People to add as contributors
+    contribsToAdd: Ember.A(),
 
     // Some computed properties
     canViewParent: Ember.computed.alias('model.parent'), // TODO : verify first whether user can view parent. May need a new model field.
@@ -22,6 +30,13 @@ export default Ember.Component.extend({
     showAddAll: true, // TODO: Implement
 
     actions: {
+        selectPage(pageName) {
+            this.set('page', pageName);
+        }
+
+        //////////////////////////////////
+        //  Actions for the whom page   //
+        //////////////////////////////////
         searchPeople() {
             // TODO: implement
             console.log('Performed a search');
@@ -34,10 +49,21 @@ export default Ember.Component.extend({
             //TODO: Import contributors from parent
             console.log('Imported contributors!');
         },
-        
+
         addAllContributors() {
             // TODO: Implement, was addAll in contribAdder.js
             console.log('Added all available results as contributors');
+        },
+        addOneContributor() {
+            // TODO: Implement. Was $root.add
+        },
+        submitContributors() {
+            // TODO: Implement. Send contribs list to server.
+
+        },
+        submitInvite() {
+            // TODO: Not currently implemented pending required APIv2 capabilities
+            console.log('User invited!');
         }
     }
 });
