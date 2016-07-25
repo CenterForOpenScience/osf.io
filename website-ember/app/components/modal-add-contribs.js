dddb8d299c62e332e733877e285a279c097bd1d0
Andy Boughton <abought@gmail.com>
2016-07-25 12:13:41 -0400

    Work on implementing template conditionals
    [ci skip]
    [#EOSF-116]

--- website-ember/app/components/modal-add-contribs.js
+++ website-ember/app/components/modal-add-contribs.js
@@ -9,5 +9,35 @@ export default Ember.Component.extend({
      * @property {String} page Which page of the modal to display
      * @default whom|which|invite
      */
-    page: 'whom'  // TODO: Replace this with components; temp feature
+    page: 'whom',  // TODO: Replace this with components; temp feature
+
+    // The username to search for
+    _searchText: null,
+
+    // Some computed properties
+    canViewParent: Ember.computed.alias('model.parent'), // TODO : verify first whether user can view parent. May need a new model field.
+
+    // TODO: This link should be hidden if the only search results returned are for people who are already contributors on the node
+    // Was: addAllVisible, contribAdder.js
+    showAddAll: true, // TODO: Implement
+
+    actions: {
+        searchPeople() {
+            // TODO: implement
+            console.log('Performed a search');
+            let text = this.get('_searchText');
+            if (!text) {
+                return;
+            }
+        },
+        importContribsFromParent() {
+            //TODO: Import contributors from parent
+            console.log('Imported contributors!');
+        },
+        
+        addAllContributors() {
+            // TODO: Implement, was addAll in contribAdder.js
+            console.log('Added all available results as contributors');
+        }
+    }
 });
