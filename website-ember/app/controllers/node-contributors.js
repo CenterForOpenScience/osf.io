0580f25afd8ac53a3c7364c8d6d19392c8e5a3ba
Andy Boughton <abought@gmail.com>
2016-07-26 10:19:35 -0400

    Depaginate node contributors relationship to support search result filtering
    ("is this person a contributor on the project")
    
    To avoid two requests to the same endpoint interfering, we pass the `isContributor` check in to override the one that the navbar usually does for itself.
    
    [ci skip]
    [#EOSF-116]

--- website-ember/app/controllers/node-contributors.js
+++ website-ember/app/controllers/node-contributors.js
@@ -1,6 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+    contributors: Ember.A(),
+
+    isContributor: Ember.computed('user', 'contributors', function() {
+        // Is a contributor if userId in list of known contributors
+        return !!this.get('contributors').findBy('userId', this.get('user.id'));
+    }),
+
     canEdit: true, // TODO: Implement based on comments PR logic
 
     showModalAddContributors: false,
