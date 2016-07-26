00269eefc043d75aa9b1c3d714f897c3a324e1d3
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-07-26 19:56:03 -0400

    Close modal when hit cancel.

--- website-ember/app/components/modal-remove-contribs.js
+++ website-ember/app/components/modal-remove-contribs.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    isOpen: false,
     removeSelf: Ember.computed('contributorToRemove', 'currentUser', function() {
         if (this.get('contributorToRemove')) {
             return this.get('contributorToRemove').id.split('-')[1] === this.get('currentUser').id;
@@ -12,5 +13,8 @@ export default Ember.Component.extend({
         removeContributor(contrib) {
             this.sendAction('removeContributor', contrib);
         },
+        close() {
+            this.set('isOpen', false);
+        }
     }
 });
