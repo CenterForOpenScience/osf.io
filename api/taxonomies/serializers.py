cb498fd6c528eeb0a3e19f5113dd3e9aebf7271a
Terron Ishihara <tishihara@email.arizona.edu>
2016-07-26 12:06:30 -0400

    Convert data string into JSON object, add "Plos" prefix, add doc strings for each taxonomy

--- api/taxonomies/serializers.py
+++ api/taxonomies/serializers.py
@@ -1,12 +1,4 @@
 from rest_framework import serializers as ser
 
-from api.base.serializers import (
-    JSONAPISerializer, LinksField, IDField, TypeField
-)
-
-class TaxonomySerializer(JSONAPISerializer):
-    id = IDField(source='_id', read_only=True)
-    data = ser.CharField()
-
-    class Meta:
-        type_ = 'taxonomies'
+class TaxonomySerializer(ser.Serializer):
+    data = ser.JSONField()
--- api/taxonomies/views.py
+++ api/taxonomies/views.py
@@ -3,36 +3,63 @@ from rest_framework import generics
 from api.taxonomies.serializers import TaxonomySerializer
 from api.base.views import JSONAPIBaseView
 
-from modularodm import fields
-from framework.mongo import (
-    ObjectId,
-    StoredObject,
-)
-
 import json
 
-class TaxonomyMixin(JSONAPIBaseView, generics.RetrieveAPIView):
+class PlosTaxonomyMixin(JSONAPIBaseView, generics.RetrieveAPIView):
     permission_classes = (
     )
     serializer_class = TaxonomySerializer
-    view_category = 'taxonomies'
-    view_name = 'taxonomy'
-    data_file = ''
+    view_category = 'plos-taxonomies'
 
+    # overrides RetrieveAPIView
     def get_object(self):
         with open(self.data_file, 'r') as json_file:
             data = json.load(json_file)
         return data
 
-class TaxonomyObject(StoredObject):
-    _id = fields.StringField(primary=True, default=lambda: str(ObjectId()))
-    id = fields.StringField(required=True, unique=True, editable=False)
-    data = fields.StringField(list=True)
+# TODO:  reverse flag
+class PlosTaxonomyFlat(PlosTaxonomyMixin):
+    '''PLOS taxonomy of subjects in flattened form. *Read-only*
 
-class TaxonomyFlat(TaxonomyMixin):
+    ##Taxonomy Attributes
+
+        name           type                   description
+        ----------------------------------------------------------------------------
+        data           array of strings       List of subjects
+
+    Raw list of subjects. Each subject is formatted as an underscore-separated string,
+    where a string with no underscores indicates the subject is at the highest level
+    of the taxonomy and all subsequent underscores indicate subjects further down
+    the taxonomy. For example,
+
+        "Biology and life sciences_Agriculture_Agricultural biotechnology"
+
+    indicates that "Biology and life sciences" is the parent subject of "Agriculture"
+    which is the parent subject of "Agricultural biotechnology".
+
+    Including `reverse=True` will reverse this ordering.
+
+    Only the top three levels of the PLOS taxonomy are included.
+    '''
+    view_name = 'plos-taxonomy-flat'
     data_file = 'api/static/json/top_3_levels_flat.json'
-    id = 'flat'
 
-class TaxonomyTreeview(TaxonomyMixin):
+class PlosTaxonomyTreeview(PlosTaxonomyMixin):
+    '''PLOS taxonomy of subjects in the format expected by
+    [bootstrap-treeview](https://github.com/jonmiles/bootstrap-treeview).
+    *Read-only*
+
+    ##Taxonomy Attributes
+
+        name           type                      description
+        ---------------------------------------------------------------------------------------------------
+        data           array of JSON objects     List of subjects as objects with 'text' and 'nodes' fields
+
+    Bootstrap-treeview is a tool for displaying tree structures in a clean, Bootstrap-styled list. The PLOS
+    taxonomy has been parsed to fit the format bootstrap-treeview expects, which entails each subject being
+    represented by an object with two key fields: `text` and `nodes`. In this case, `text` is a single-element
+    array that contains the subject itself as a string. `nodes` is an array of child objects each with their own
+    `text` and `nodes` fields, with the exception that leaf nodes do not contain a `nodes` field.
+    '''
+    view_name = 'plos-taxonomy-treeview'
     data_file = 'api/static/json/top_3_levels_treeview.json'
-    id = 'treeview'
