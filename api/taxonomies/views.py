8caa35f493d35b816a21cd1d1b6ac21a970b31db
Terron Ishihara <tishihara@email.arizona.edu>
2016-07-27 14:05:03 -0400

    Add reversed flat taxonomy, process query_param for setting reverse=true

--- api/taxonomies/views.py
+++ api/taxonomies/views.py
@@ -17,9 +17,8 @@ class PlosTaxonomyMixin(JSONAPIBaseView, generics.RetrieveAPIView):
             data = json.load(json_file)
         return data
 
-# TODO:  reverse flag
 class PlosTaxonomyFlat(PlosTaxonomyMixin):
-    '''PLOS taxonomy of subjects in flattened form. *Read-only*
+    '''[PLOS taxonomy of subjects](http://journals.plos.org/plosone/browse/) in flattened form. *Read-only*
 
     ##Taxonomy Attributes
 
@@ -27,7 +26,7 @@ class PlosTaxonomyFlat(PlosTaxonomyMixin):
         ----------------------------------------------------------------------------
         data           array of strings       List of subjects
 
-    Raw list of subjects. Each subject is formatted as an underscore-separated string,
+    Each subject is formatted as an underscore-separated string,
     where a string with no underscores indicates the subject is at the highest level
     of the taxonomy and all subsequent underscores indicate subjects further down
     the taxonomy. For example,
@@ -37,15 +36,25 @@ class PlosTaxonomyFlat(PlosTaxonomyMixin):
     indicates that "Biology and life sciences" is the parent subject of "Agriculture"
     which is the parent subject of "Agricultural biotechnology".
 
-    Including `reverse=True` will reverse this ordering.
+    Including `reverse=true` as a query parameter (`v2/taxonomies/plos/flat?reverse=true`) will return the same list
+    but with each subject underscore-separated in reverse order (lowest subject first).
 
     Only the top three levels of the PLOS taxonomy are included.
     '''
     view_name = 'plos-taxonomy-flat'
-    data_file = 'api/static/json/top_3_levels_flat.json'
+
+    @property
+    def data_file(self):
+        # Handle reverse=true query parameter
+        reverse_flag = self.request.query_params.get('reverse')
+        if (reverse_flag and reverse_flag.lower() == 'true' or reverse_flag == '1'):
+            return 'api/static/json/top_3_levels_flat_reverse.json'
+        return 'api/static/json/top_3_levels_flat.json'
+
+
 
 class PlosTaxonomyTreeview(PlosTaxonomyMixin):
-    '''PLOS taxonomy of subjects in the format expected by
+    '''[PLOS taxonomy of subjects](http://journals.plos.org/plosone/browse/) in the format expected by
     [bootstrap-treeview](https://github.com/jonmiles/bootstrap-treeview).
     *Read-only*
 
