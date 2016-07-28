b88f9e293054abdc93f8be1be96a360ba66c1db0
Acshi Haggenmiller <acshikh@gmail.com>
2016-07-28 13:40:10 -0400

    moved site customizations into the relevant plugins; added extra discourse settings needed

--- framework/discourse/configure.py
+++ framework/discourse/configure.py
@@ -17,23 +17,6 @@ def configure_embeddable_host():
     data['embeddable_host[host]'] = settings.DOMAIN
     return request('post', '/admin/embeddable_hosts', data)
 
-def _get_customizations():
-    result = request('get', '/admin/customize/css_html.json')
-    return result['site_customizations']
-
-def configure_customizations():
-    old_ids = [c['id'] for c in _get_customizations()]
-    for old_id in old_ids:
-        request('delete', '/admin/site_customizations/' + str(old_id))
-
-    for customization in settings.DISCOURSE_SERVER_CUSTOMIZATIONS:
-        data = {}
-        for key, val in customization.items():
-            data['site_customization[' + key + ']'] = val
-        request('post', '/admin/site_customizations', data)
-
-        time.sleep(0.1)
-
 def configure_server_settings():
     for key, val in settings.DISCOURSE_SERVER_SETTINGS.items():
         data = {}
@@ -44,5 +27,4 @@ def configure_server_settings():
 
 if __name__ == '__main__':
     configure_server_settings()
-    configure_customizations()
     configure_embeddable_host()
