61dd76a23e4d6da12cff8ece19896171adb04c23
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-07-28 17:02:18 -0400

    Remove reload of window on save.

--- website-ember/app/helpers/can-remove-contributor.js
+++ website-ember/app/helpers/can-remove-contributor.js
@@ -4,8 +4,13 @@ export function canRemoveContributor(params/*, hash*/) {
     var contributor = params[0];
     var currentUser = params[1];
     var registration = params[2];
-    var currentUserId = currentUser.get('currentUserId') || currentUser.get('id');
-    return contributor.id.split('-')[1] === currentUserId && !registration;
+    if (currentUser) {
+        var currentUserId = currentUser.get('currentUserId') || currentUser.get('id');
+        return contributor.id.split('-')[1] === currentUserId && !registration;
+    } else {
+        return params;
+    }
+
 }
 
 export default Ember.Helper.helper(canRemoveContributor);
