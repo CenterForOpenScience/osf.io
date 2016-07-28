7623310e0d82aba08b6f9651e03436aedcc053cc
Steven Loria <sloria1@gmail.com>
2016-07-28 15:24:01 -0400

    Remove deleted fields from factory

--- tests/django_factories.py
+++ tests/django_factories.py
@@ -10,7 +10,5 @@ class NodeFactory(DjangoModelFactory):
     title = factory.Faker('catch_phrase')
     description = factory.Faker('sentence')
     date_created = factory.LazyFunction(dt.datetime.now)
-    institution_domains = factory.LazyFunction(list)
-    institution_email_domains = factory.LazyFunction(list)
     class Meta:
         model = Node
