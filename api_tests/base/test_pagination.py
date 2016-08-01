c767ee0b4392c519335a6055f64bbbb5a500e997
Matt Frazier <maf7sm@virginia.edu>
2016-08-01 16:47:31 -0400

    Add error message for future breakers-of-tests

--- api_tests/base/test_pagination.py
+++ api_tests/base/test_pagination.py
@@ -7,4 +7,5 @@ from api.base.pagination import MaxSizePagination
 
 class TestMaxPagination(ApiTestCase):
     def test_no_query_param_alters_page_size(self):
-        assert_is_none(MaxSizePagination.page_size_query_param)
+        assert MaxSizePagination.page_size_query_param is None, 'Adding variable page sizes to the paginator ' +\
+            'requires tests to ensure that you can\'t request more than the class\'s maximum number of values.'
