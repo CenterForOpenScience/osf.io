da5bdfd24bc56f87a64a250cf9ed65314fff9323
Steven Loria <sloria1@gmail.com>
2016-08-02 11:08:25 -0400

    Add invoke tasks for setting up and running tests (#61)

--- README.rst
+++ README.rst
@@ -6,3 +6,20 @@ osf-models
     :target: https://travis-ci.org/CenterForOpenScience/osf-models
 
 **In progress** Django models for OSF.
+
+
+Running tests
+=============
+
+First setup the tests. This only needs to be run once: ::
+
+    inv setup_tests
+
+
+Run the tests: ::
+
+    inv tests
+
+To update the osf.io repo used for testing: ::
+
+    inv setup_tests --update
--- tasks.py
+++ tasks.py
@@ -1,16 +1,69 @@
+from shutil import copyfile
+import os
+import sys
 import webbrowser
 
-from invoke import task, run
+from invoke import task
 
-# TODO: test task
+OSF_GIT_URL = 'https://github.com/sloria/osf.io.git'
+POSTGRES_BRANCH = 'feature/postgres'
+
+@task
+def setup_tests(ctx, update=False, requirements=True):
+    first_run = False
+    if not os.path.exists('osf.io'):
+        first_run = True
+        ctx.run('git clone --branch={} {}'.format(POSTGRES_BRANCH, OSF_GIT_URL), echo=True)
+
+    os.chdir('osf.io')
+    if update and not first_run:
+        print('Updating osf.io ({})'.format(POSTGRES_BRANCH))
+        ctx.run('git pull origin {}'.format(POSTGRES_BRANCH), echo=True)
+    # Copy necessary local.py files
+    try:
+        copyfile(
+            os.path.join('website', 'settings', 'local-travis.py'),
+            os.path.join('website', 'settings', 'local.py')
+        )
+    except IOError:
+        pass
+    try:
+        copyfile(
+            os.path.join('api', 'settings', 'local-dist.py'),
+            os.path.join('api', 'settings', 'local.py')
+        )
+    except IOError:
+        pass
+    if requirements:
+        # Install osf requirements
+        ctx.run('pip install -r {}'.format(os.path.join('requirements.txt')), echo=True)
+        # Install osf-models test requirements
+        ctx.run('pip install -r {}'.format(os.path.join('osf_models_tests', 'requirements.txt')), echo=True)
+    os.chdir('..')
+    print('Finished test setup.')
+
+
+@task
+def test(ctx, setup=False):
+    import pytest
+    if setup:
+        setup_tests(ctx)
+    if not os.path.exists('osf.io'):
+        print('Must run "inv setup_tests" before running tests.')
+        sys.exit(1)
+    os.chdir('osf.io')
+    retcode = pytest.main(['osf_models_tests'])
+    os.chdir('..')
+    sys.exit(retcode)
+
 
 @task
 def flake(ctx):
     """Run flake8 on codebase."""
-    run('flake8 .', echo=True)
+    ctx.run('flake8 .', echo=True)
 
 @task
 def readme(ctx, browse=False):
-    run("rst2html.py README.rst > README.html")
+    ctx.run("rst2html.py README.rst > README.html")
     if browse:
         webbrowser.open_new_tab('README.html')
