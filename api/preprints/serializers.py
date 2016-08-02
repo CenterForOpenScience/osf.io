856a3f041299959834382cebf8331ed7c93bb68f
erinspace <erin.braswell@gmail.com>
2016-08-02 14:41:04 -0400

    updates to serializer to more closely match what we want from preprints

--- api/preprints/serializers.py
+++ api/preprints/serializers.py
@@ -11,22 +11,16 @@ class PreprintSerializer(JSONAPISerializer):
     filterable_fields = frozenset([
         'id',
         'title',
-        'description',
-        'public',
         'tags',
         'date_created',
         'date_modified',
-        'preprint_created',
-        'root',
-        'parent',
-        'contributors',
+        'authors',
         'preprint_subjects'
     ])
 
     title = ser.CharField(required=True)
     preprint_subjects = JSONAPIListField()
-    date_created = ser.DateTimeField(read_only=True)
-    preprint_created = ser.DateTimeField(read_only=True)
+    date_created = ser.DateTimeField(read_only=True, source='preprint_created')
     date_modified = ser.DateTimeField(read_only=True)
     id = IDField(source='_id', required=False)
     abstract = ser.CharField(source='description', required=False)
@@ -37,23 +31,17 @@ class PreprintSerializer(JSONAPISerializer):
         related_view_kwargs={'file_id': '<_id>'},
     )
 
+    files = RelationshipField(
+        related_view='nodes:node-providers',
+        related_view_kwargs={'node_id': '<pk>'}
+    )
+
     links = LinksField({'self': 'get_preprint_url', 'html': 'get_absolute_html_url'})
 
     authors = RelationshipField(
         related_view='preprints:preprint-authors',
         related_view_kwargs={'node_id': '<pk>'},
         related_meta={'count': 'get_contrib_count'},
-    )
-
-    parent = RelationshipField(
-        related_view='nodes:node-detail',
-        related_view_kwargs={'node_id': '<parent_node._id>'},
-        filter_key='parent_node'
-    )
-
-    root = RelationshipField(
-        related_view='nodes:node-detail',
-        related_view_kwargs={'node_id': '<root._id>'}
     )
 
     class Meta:
