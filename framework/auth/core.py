9386a2b1da23523d254ccadb29261c37f1666cb8
TomBaxter <tkb608@gmail.com>
2016-08-04 16:14:02 -0400

    Change minimum password length from 6 to 8 [#OSF-6780]

--- framework/auth/core.py
+++ framework/auth/core.py
@@ -730,8 +730,8 @@ class User(GuidStoredObject, AddonModelMixin):
             issues.append('Password cannot be the same as your email address')
         if not raw_old_password or not raw_new_password or not raw_confirm_password:
             issues.append('Passwords cannot be blank')
-        elif len(raw_new_password) < 6:
-            issues.append('Password should be at least six characters')
+        elif len(raw_new_password) < 8:
+            issues.append('Password should be at least eight characters')
         elif len(raw_new_password) > 256:
             issues.append('Password should not be longer than 256 characters')
 
