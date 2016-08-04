430f61026130493d791ee29707d8b9a67b03b7d9
erinspace <erin.braswell@gmail.com>
2016-08-04 11:32:01 -0400

    Update preprint tests to pass files instead of their ids

--- tests/test_preprints.py
+++ tests/test_preprints.py
@@ -80,41 +80,43 @@ class TestSetPreprintFile(OsfTestCase):
     @assert_logs(NodeLog.MADE_PUBLIC, 'project')
     @assert_logs(NodeLog.PREPRINT_INITIATED, 'project', -2)
     def test_is_preprint_property_new_file(self):
-        self.project.set_preprint_file(self.file._id, auth=self.auth, save=True)
+        self.project.set_preprint_file(self.file, auth=self.auth, save=True)
+        self.project.reload()
         assert_true(self.project.is_preprint)
 
     def test_project_made_public(self):
         assert_false(self.project.is_public)
-        self.project.set_preprint_file(self.file._id, auth=self.auth, save=True)
+        self.project.set_preprint_file(self.file, auth=self.auth, save=True)
         assert_true(self.project.is_public)
 
     def test_add_primary_file(self):
-        self.project.set_preprint_file(self.file._id, auth=self.auth, save=True)
-        assert_equal(self.project.preprint_file._id, self.file._id)
+        self.project.set_preprint_file(self.file, auth=self.auth, save=True)
+        assert_equal(self.project.preprint_file, self.file)
+        assert_equal(type(self.project.preprint_file), type(self.file.stored_object))
 
     @assert_logs(NodeLog.PREPRINT_FILE_UPDATED, 'project')
     def test_change_primary_file(self):
-        self.project.set_preprint_file(self.file._id, auth=self.auth, save=True)
-        assert_equal(self.project.preprint_file._id, self.file._id)
+        self.project.set_preprint_file(self.file, auth=self.auth, save=True)
+        assert_equal(self.project.preprint_file, self.file)
 
-        self.project.set_preprint_file(self.file_two._id, auth=self.auth, save=True)
+        self.project.set_preprint_file(self.file_two, auth=self.auth, save=True)
         assert_equal(self.project.preprint_file._id, self.file_two._id)
 
     def test_add_invalid_file(self):
-        with assert_raises(NodeStateError):
+        with assert_raises(AttributeError):
             self.project.set_preprint_file('inatlanta', auth=self.auth, save=True)
 
     def test_preprint_created_date(self):
-        self.project.set_preprint_file(self.file._id, auth=self.auth, save=True)
+        self.project.set_preprint_file(self.file, auth=self.auth, save=True)
         assert_equal(self.project.preprint_file._id, self.file._id)
 
         assert(self.project.preprint_created)
         assert_not_equal(self.project.date_created, self.project.preprint_created)
 
     def test_non_admin_update_file(self):
-        self.project.set_preprint_file(self.file._id, auth=self.auth, save=True)
+        self.project.set_preprint_file(self.file, auth=self.auth, save=True)
         assert_equal(self.project.preprint_file._id, self.file._id)
 
         with assert_raises(PermissionsError):
-            self.project.set_preprint_file(self.file_two._id, auth=self.read_write_user_auth, save=True)
+            self.project.set_preprint_file(self.file_two, auth=self.read_write_user_auth, save=True)
         assert_equal(self.project.preprint_file._id, self.file._id)
