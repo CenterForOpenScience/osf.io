8abab2b6391cc024f3a275713b67ce1b784d4523
Chris Wisecarver <cwisecarver@cos.io>
2016-08-08 20:52:08 -0400

    Adding python type awareness to jsonfield

--- osf_models/tests.py
+++ osf_models/tests.py
@@ -1,3 +1,69 @@
-from django.test import TestCase
+import datetime as dt
+import json
+from decimal import Decimal
+from pprint import pprint
 
-# Create your tests here.
+from django.test import TestCase
+from osf_models.utils.datetime_aware_jsonfield import DateTimeAwareJSONEncoder, decode_datetime_objects
+
+
+class DateTimeAwareJSONFieldTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.json_dict_data = dict(
+            sample_date=dt.date.today(),
+            nested_data=dict(
+                sample_date=dt.date.today(),
+                sample_datetime=dt.datetime.utcnow(),
+                sample_decimal=Decimal("10.259")
+            ),
+            sample_datetime=dt.datetime.utcnow(),
+            sample_decimal=Decimal("10.259"),
+            sample_text='wut wut',
+            list_of_things=[
+                dict(
+                    sample_date=dt.date.today(),
+                    sample_datetime=dt.datetime.utcnow(),
+                    sample_decimal=Decimal("10.259")
+                ),
+                dict(
+                    sample_date=dt.date.today(),
+                    sample_datetime=dt.datetime.utcnow(),
+                    sample_decimal=Decimal("10.259")
+                ),
+                [
+                    dict(
+                        sample_date=dt.date.today(),
+                        sample_datetime=dt.datetime.utcnow(),
+                        sample_decimal=Decimal("10.259")
+                    ),
+                    dict(
+                        sample_date=dt.date.today(),
+                        sample_datetime=dt.datetime.utcnow(),
+                        sample_decimal=Decimal("10.259")
+                    ),
+                ]
+            ]
+        )
+        cls.json_list_data = [
+            dict(
+                sample_date=dt.date.today(),
+                sample_datetime=dt.datetime.utcnow(),
+                sample_decimal=Decimal("10.259")
+            ),
+            dict(
+                sample_date=dt.date.today(),
+                sample_datetime=dt.datetime.utcnow(),
+                sample_decimal=Decimal("10.259")
+            ),
+        ]
+
+    def test_dict(self):
+        json_string = json.dumps(self.json_dict_data, cls=DateTimeAwareJSONEncoder)
+        json_data = decode_datetime_objects(json.loads(json_string))
+        assert json_data == self.json_dict_data, 'Nope'
+
+    def test_list(self):
+        json_string = json.dumps(self.json_list_data, cls=DateTimeAwareJSONEncoder)
+        json_data = decode_datetime_objects(json.loads(json_string))
+        assert json_data == self.json_list_data, 'Nope'
--- osf_models/utils/datetime_aware_jsonfield.py
+++ osf_models/utils/datetime_aware_jsonfield.py
@@ -1,29 +1,106 @@
-import ujson
+import datetime as dt
+import json
+from decimal import Decimal
+from functools import partial
+
+from dateutil import parser
 from django.contrib.postgres import lookups
 from django.contrib.postgres.fields.jsonb import JSONField
-from django.core import exceptions
+from django.core.serializers.json import DjangoJSONEncoder
+from osf_models.exceptions import ValidationError
 from psycopg2.extras import Json
 
 
-class DatetimeAwareJSONField(JSONField):
+class DateTimeAwareJSONEncoder(DjangoJSONEncoder):
+    def default(self, o):
+        if isinstance(o, dt.datetime):
+            return dict(type='encoded_datetime', value=o.isoformat())
+        elif isinstance(o, dt.date):
+            return dict(type='encoded_date', value=o.isoformat())
+        elif isinstance(o, dt.time):
+            return dict(type='encoded_time', value=o.isoformat())
+        elif isinstance(o, Decimal):
+            return dict(type='encoded_decimal', value=str(o))
+        return super(DateTimeAwareJSONEncoder, self).default(o)
+
+
+def decode_datetime_objects(nested_value):
+
+    if isinstance(nested_value, list):
+        new_list = list()
+        for item in nested_value:
+            new_list.append(decode_datetime_objects(item))
+        return new_list
+    elif isinstance(nested_value, dict):
+        new_value = nested_value
+        for key, value in nested_value.iteritems():
+            if isinstance(value, dict) and 'type' in value.keys():
+                if value['type'] == 'encoded_datetime':
+                    new_value[key] = parser.parse(value['value'])
+                if value['type'] == 'encoded_date':
+                    new_value[key] = parser.parse(value['value']).date()
+                if value['type'] == 'encoded_time':
+                    new_value[key] = parser.parse(value['value']).time()
+                if value['type'] == 'encoded_decimal':
+                    new_value[key] = Decimal(value['value'])
+            elif isinstance(value, dict):
+                new_value[key] = decode_datetime_objects(value)
+            elif isinstance(value, list):
+                new_value[key] = decode_datetime_objects(value)
+        return new_value
+    return nested_value
+
+
+
+# def decode_datetime_objects(nested_value):
+#     if isinstance(nested_value, dict):
+#         new_value = dict()
+#         for key, value in nested_value.iteritems():
+#             if key == 'type' and value == 'encoded_datetime':
+#                 new_value[key] = parser.parse(nested_value['value'])
+#             elif key == 'type' and value == 'encoded_date':
+#                 new_value[key] = parser.parse(nested_value['value'])
+#             elif key == 'type' and value == 'encoded_time':
+#                 new_value[key] = parser.parse(nested_value['value'])
+#             elif isinstance(value, collections.Mapping):
+#                 # recurse
+#                 new_value[key] = decode_datetime_objects(value)
+#         return new_value
+#     elif isinstance(nested_value, list):
+#         new_list = list()
+#         for item in nested_value:
+#             # recurse
+#             new_value = decode_datetime_objects(item)
+#             new_list.append(new_value)
+#         return new_list
+#     return nested_value
+
+
+class DateTimeAwareJSONField(JSONField):
     def get_prep_value(self, value):
         if value is not None:
-            return Json(value, dumps=ujson.dumps)
+            return Json(value, dumps=partial(json.dumps, cls=DateTimeAwareJSONEncoder))
         return value
+
+    def to_python(self, value):
+        if value is None:
+            return None
+        return super(DateTimeAwareJSONField, self).to_python(decode_datetime_objects(value))
+
 
     def get_prep_lookup(self, lookup_type, value):
         if lookup_type in ('has_key', 'has_keys', 'has_any_keys'):
             return value
         if isinstance(value, (dict, list)):
-            return Json(value, dumps=ujson.dumps)
+            return Json(value, dumps=partial(json.dumps, cls=DateTimeAwareJSONEncoder))
         return super(JSONField, self).get_prep_lookup(lookup_type, value)
 
     def validate(self, value, model_instance):
         super(JSONField, self).validate(value, model_instance)
         try:
-            ujson.dumps(value)
+            json.dumps(value, cls=DateTimeAwareJSONEncoder)
         except TypeError:
-            raise exceptions.ValidationError(
+            raise ValidationError(
                 self.error_messages['invalid'],
                 code='invalid',
                 params={'value': value},
