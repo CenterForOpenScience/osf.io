f5747c93065c8fc94c6426ab6ae10e5ae1c46518
Matthew Keitelman <zamattiac@gmail.com>
2016-08-09 10:11:58 -0400

    Add toku block

--- scripts/migration/migrate_retraction_dates.py
+++ scripts/migration/migrate_retraction_dates.py
@@ -7,9 +7,9 @@ import logging
 
 from modularodm import Q
 from website.models import Node, NodeLog
-
 from website.app import init_app
 from scripts import utils as script_utils
+from framework.transactions.context import TokuTransaction
 
 logger = logging.getLogger(__name__)
 
@@ -17,37 +17,32 @@ logger = logging.getLogger(__name__)
 def do_migration(logs):
     # ... perform the migration using a list of logs ...
     for log in logs:
-        when = log.date
         registration = Node.load(log.params['registration'])
 
-        registration.update_fields(date_modified=when)
+        registration.date_modified = log.date
         registration.save()
 
-        logger.warning("{} date updated to {}".format(registration, when))
+        logger.info('{} date updated to {}'.format(registration, log.date))
 
 
 def get_targets():
     # ... return the list of logs whose registrations we want to migrate ...
-    query = Q('action', 'eq', "retraction_approved")
-    targets = NodeLog.find(query)
+    targets = NodeLog.find(Q('action', 'eq', 'retraction_approved'))
 
-    logger.info("Retractions found: {}".format(len(targets)))
+    logger.info('Retractions found: {}'.format(len(targets)))
     return targets
 
-
-def main(dry=True):
-    init_app(set_backends=True, routes=False)  # Sets the storage backends on all models
-
-    targets = get_targets()
-
-    if dry:
-        for target in targets:
-            logger.info("{} {}: {}".format(target.date, target.params['registration'], target.action))
-    else:
-        do_migration(targets)
 
 if __name__ == '__main__':
     dry = '--dry' in sys.argv
     if not dry:
         script_utils.add_file_logger(logger, __file__)
-    main(dry=dry)
+
+    with TokuTransaction():
+        init_app(set_backends=True, routes=False)  # Sets the storage backends on all models
+        targets = get_targets()
+        for target in targets:
+            logger.info('{} {}: {}'.format(target.date, target.params['registration'], target.action))
+        do_migration(targets)
+        if dry:
+            raise RuntimeError('Dry run, rolling back transaction.')
