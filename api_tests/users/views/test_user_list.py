043962af771966d23d10b1d91d3820dd9dd060e1
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-08-10 14:08:16 -0400

    Change permission class RequiresScopedRequestOrReadOnly so cookies/properly scoped tokens are allowed but basic auth is not.

--- api_tests/users/views/test_user_list.py
+++ api_tests/users/views/test_user_list.py
@@ -113,7 +113,7 @@ class TestUsersCreate(ApiTestCase):
         User.remove()
 
     @mock.patch('framework.auth.views.mails.send_mail')
-    def test_logged_in_user_can_create_other_user_or_send_mail(self, mock_mail):
+    def test_logged_in_user_with_basic_auth_cannot_create_other_user_or_send_mail(self, mock_mail):
         assert_equal(User.find(Q('username', 'eq', self.unconfirmed_email)).count(), 0)
         res = self.app.post_json_api(
             '{}?send_email=true'.format(self.base_url),
@@ -122,9 +122,9 @@ class TestUsersCreate(ApiTestCase):
             expect_errors=True
         )
 
-        assert_equal(res.status_code, 201)
-        assert_equal(User.find(Q('username', 'eq', self.unconfirmed_email)).count(), 1)
-        assert_equal(mock_mail.call_count, 1)
+        assert_equal(res.status_code, 403)
+        assert_equal(User.find(Q('username', 'eq', self.unconfirmed_email)).count(), 0)
+        assert_equal(mock_mail.call_count, 0)
 
     @mock.patch('framework.auth.views.mails.send_mail')
     def test_logged_out_user_cannot_create_other_user_or_send_mail(self, mock_mail):
