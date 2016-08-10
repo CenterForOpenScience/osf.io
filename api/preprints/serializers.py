f277b759609ecd7620b5fc159c232ccee808d6af
Henrique Moco <hmoco94@vt.edu>
2016-08-10 09:52:53 -0400

    change taxonomy

--- api/preprints/serializers.py
+++ api/preprints/serializers.py
@@ -39,7 +39,7 @@ class PreprintSerializer(JSONAPISerializer):
     ])
 
     title = ser.CharField(required=False)
-    subjects = JSONAPIListField(required=False, source='preprint_subjects')
+    subjects = JSONAPIListField(required=False, source='get_preprint_subjects')
     provider = ser.CharField(source='preprint_provider', required=False)
     date_created = ser.DateTimeField(read_only=True, source='preprint_created')
     date_modified = ser.DateTimeField(read_only=True)
