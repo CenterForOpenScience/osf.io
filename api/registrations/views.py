a7fcacfbb55958f574f4a4997798a41eb4e53487
John Tordoff <johnetordoff@gmail.com>
2016-08-11 14:51:45 -0400

    add Swagger docs to registrations

--- api/registrations/views.py
+++ api/registrations/views.py
@@ -284,7 +284,88 @@ class RegistrationDetail(JSONAPIBaseView, generics.RetrieveUpdateAPIView, Regist
 
 
 class RegistrationContributorsList(BaseContributorList, RegistrationMixin, UserMixin):
-    """List of contributors for a registration."""
+    """Contributors (users) for a registration.
+
+    Contributors are users who can make changes to the node or, in the case of private nodes,
+    have read access to the node. Contributors are divided between 'bibliographic' and 'non-bibliographic'
+    contributors. From a permissions standpoint, both are the same, but bibliographic contributors
+    are included in citations, while non-bibliographic contributors are not included in citations.
+
+    Note that if an anonymous view_only key is being used, the user relationship will not be exposed and the id for
+    the contributor will be an empty string.
+
+    ##Node Contributor Attributes
+
+    <!--- Copied Attributes from NodeContributorDetail -->
+
+    `type` is "contributors"
+
+        name                        type     description
+        ======================================================================================================
+        bibliographic               boolean  Whether the user will be included in citations for this node. Default is true.
+        permission                  string   User permission level. Must be "read", "write", or "admin". Default is "write".
+        unregistered_contributor    string   Contributor's assigned name if contributor hasn't yet claimed account
+
+    ##Links
+
+    See the [JSON-API spec regarding pagination](http://jsonapi.org/format/1.0/#fetching-pagination).
+
+    ##Relationships
+
+    ###Users
+
+    This endpoint shows the contributor user detail and is automatically embedded.
+
+    ##Actions
+
+    ###Adding Contributors
+
+        Method:        POST
+        URL:           /links/self
+        Query Params:  <none>
+        Body (JSON): {
+                      "data": {
+                        "type": "contributors",                   # required
+                        "attributes": {
+                          "bibliographic": true|false,            # optional
+                          "permission": "read"|"write"|"admin"    # optional
+                        },
+                        "relationships": {
+                          "users": {
+                            "data": {
+                              "type": "users",                    # required
+                              "id":   "{user_id}"                 # required
+                            }
+                        }
+                    }
+                }
+            }
+        Success:       201 CREATED + node contributor representation
+
+    Add a contributor to a node by issuing a POST request to this endpoint.  This effectively creates a relationship
+    between the node and the user.  Besides the top-level type, there are optional "attributes" which describe the
+    relationship between the node and the user. `bibliographic` is a boolean and defaults to `true`.  `permission` must
+    be a [valid OSF permission key](/v2/#osf-node-permission-keys) and defaults to `"write"`.  A relationship object
+    with a "data" member, containing the user `type` and user `id` must be included.  The id must be a valid user id.
+    All other fields not listed above will be ignored.  If the request is successful the API will return
+    a 201 response with the representation of the new node contributor in the body.  For the new node contributor's
+    canonical URL, see the `/links/self` field of the response.
+
+    ##Query Params
+
+    + `page=<Int>` -- page number of results to view, default 1
+
+    + `filter[<fieldname>]=<Str>` -- fields and values to filter the search results on.
+
+    NodeContributors may be filtered by `bibliographic`, or `permission` attributes.  `bibliographic` is a boolean, and
+    can be filtered using truthy values, such as `true`, `false`, `0`, or `1`.  Note that quoting `true` or `false` in
+    the query will cause the match to fail regardless.
+
+    + `profile_image_size=<Int>` -- Modifies `/links/profile_image_url` of the user entities so that it points to
+    the user's profile image scaled to the given size in pixels.  If left blank, the size depends on the image provider.
+
+    #This Request/Response
+    """
     view_category = 'registrations'
     view_name = 'registration-contributors'
     serializer_class = RegistrationContributorsSerializer
@@ -315,7 +396,47 @@ class RegistrationContributorsList(BaseContributorList, RegistrationMixin, UserM
 
 
 class RegistrationContributorDetail(BaseContributorDetail, RegistrationMixin, UserMixin):
-    """Detail of a contributor for a registration."""
+    """Detail of a contributor for a registration.
+
+    Contributors are users who can make changes to the node or, in the case of private nodes,
+    have read access to the node. Contributors are divided between 'bibliographic' and 'non-bibliographic'
+    contributors. From a permissions standpoint, both are the same, but bibliographic contributors
+    are included in citations, while non-bibliographic contributors are not included in citations.
+
+    Note that if an anonymous view_only key is being used, the user relationship will not be exposed and the id for
+    the contributor will be an empty string.
+
+    Contributors can be viewed, removed, and have their permissions and bibliographic status changed via this
+    endpoint.
+
+    ##Attributes
+
+    `type` is "contributors"
+
+        name                        type     description
+        ======================================================================================================
+        bibliographic               boolean  Whether the user will be included in citations for this node. Default is true.
+        permission                  string   User permission level. Must be "read", "write", or "admin". Default is "write".
+        unregistered_contributor    string   Contributor's assigned name if contributor hasn't yet claimed account
+
+    ###Users
+
+    This endpoint shows the contributor user detail.
+
+    ##Links
+
+        self:           the canonical api endpoint of this contributor
+        html:           the contributing user's page on the OSF website
+        profile_image:  a url to the contributing user's profile image
+
+    ##Query Params
+
+    + `profile_image_size=<Int>` -- Modifies `/links/profile_image_url` so that it points the image scaled to the given
+    size in pixels.  If left blank, the size depends on the image provider.
+
+    #This Request/Response
+
+    """
     view_category = 'registrations'
     view_name = 'registration-contributor-detail'
     serializer_class = RegistrationContributorsSerializer
@@ -331,7 +452,53 @@ class RegistrationContributorDetail(BaseContributorDetail, RegistrationMixin, Us
     )
 
 class RegistrationChildrenList(JSONAPIBaseView, generics.ListAPIView, ODMFilterMixin, RegistrationMixin):
-    """Lists the children of a registration."""
+    """Children of the current registration.
+
+    This will get the next level of child nodes for the selected node if the current user has read access for those
+    nodes. Creating a node via this endpoint will behave the same as the [node list endpoint](/v2/nodes/), but the new
+    node will have the selected node set as its parent.
+
+    ##Node Attributes
+
+    <!--- Copied Attributes from NodeDetail -->
+
+    OSF Node entities have the "nodes" `type`.
+
+        name                            type                description
+        =================================================================================
+        title                           string              title of project or component
+        description                     string              description of the node
+        category                        string              node category, must be one of the allowed values
+        date_created                    iso8601 timestamp   timestamp that the node was created
+        date_modified                   iso8601 timestamp   timestamp when the node was last updated
+        tags                            array of strings    list of tags that describe the node
+        current_user_can_comment        boolean            Whether the current user is allowed to post comments
+        current_user_permissions        array of strings    list of strings representing the permissions for the current user on this node
+        registration                    boolean             is this a registration? (always false - may be deprecated in future versions)
+        fork                            boolean             is this node a fork of another node?
+        public                          boolean             has this node been made publicly-visible?
+        collection                      boolean             is this a collection? (always false - may be deprecated in future versions)
+
+    ##Links
+
+    See the [JSON-API spec regarding pagination](http://jsonapi.org/format/1.0/#fetching-pagination).
+
+    ##Query Params
+
+    + `page=<Int>` -- page number of results to view, default 1
+
+    + `filter[<fieldname>]=<Str>` -- fields and values to filter the search results on.
+
+    <!--- Copied Query Params from NodeList -->
+
+    Nodes may be filtered by their `id`, `title`, `category`, `description`, `public`, `tags`, `date_created`, `date_modified`,
+    `root`, `parent`, and `contributors`.  Most are string fields and will be filtered using simple substring matching.  `public`
+    is a boolean, and can be filtered using truthy values, such as `true`, `false`, `0`, or `1`.  Note that quoting `true`
+    or `false` in the query will cause the match to fail regardless.  `tags` is an array of simple strings.
+
+    #This Request/Response
+
+    """
     view_category = 'registrations'
     view_name = 'registration-children'
     serializer_class = RegistrationSerializer
@@ -373,7 +540,75 @@ class RegistrationChildrenList(JSONAPIBaseView, generics.ListAPIView, ODMFilterM
         return sorted([each for each in nodes if each.can_view(auth)], key=lambda n: n.date_modified, reverse=True)
 
 class RegistrationForksList(NodeForksList, RegistrationMixin):
-    """List of forks for a registration."""
+    """Forks of the current registration. *Writeable*.
+
+    Paginated list of the current node's forks ordered by their `forked_date`. Forks are copies of projects that you can
+    change without affecting the original project.  When creating a fork, your fork will will only contain public components or those
+    for which you are a contributor.  Private components that you do not have access to will not be forked.
+
+    ##Node Fork Attributes
+
+    <!--- Copied Attributes from NodeDetail with exception of forked_date-->
+
+    OSF Node Fork entities have the "nodes" `type`.
+
+        name                        type               description
+        ===============================================================================================================================
+        title                       string             title of project or component
+        description                 string             description of the node
+        category                    string             node category, must be one of the allowed values
+        date_created                iso8601 timestamp  timestamp that the node was created
+        date_modified               iso8601 timestamp  timestamp when the node was last updated
+        tags                        array of strings   list of tags that describe the node
+        registration                boolean            has this project been registered? (always False)
+        collection                  boolean            is this node a collection (always False)
+        fork                        boolean            is this node a fork of another node? (always True)
+        public                      boolean            has this node been made publicly-visible?
+        forked_date                 iso8601 timestamp  timestamp when the node was forked
+        current_user_can_comment    boolean            Whether the current user is allowed to post comments
+        current_user_permissions    array of strings   List of strings representing the permissions for the current user on this node
+
+    ##Links
+
+    See the [JSON-API spec regarding pagination](http://jsonapi.org/format/1.0/#fetching-pagination).
+
+    ##Actions
+
+    ###Create Node Fork
+
+        Method:        POST
+        URL:           /links/self
+        Query Params:  <none>
+        Body (JSON): {
+                         "data": {
+                           "type": "nodes", # required
+                           "attributes": {
+                             "title": {title} # optional
+                           }
+                         }
+                    }
+        Success: 201 CREATED + node representation
+
+    To create a fork of the current node, issue a POST request to this endpoint.  The `title` field is optional, with the
+    default title being 'Fork of ' + the current node's title. If the fork's creation is successful the API will return a
+    201 response with the representation of the forked node in the body. For the new fork's canonical URL, see the `/links/self`
+    field of the response.
+
+    ##Query Params
+
+    + `page=<Int>` -- page number of results to view, default 1
+
+    + `filter[<fieldname>]=<Str>` -- fields and values to filter the search results on.
+
+    <!--- Copied Query Params from NodeList -->
+
+    Nodes may be filtered by their `title`, `category`, `description`, `public`, `registration`, `tags`, `date_created`,
+    `date_modified`, `root`, `parent`, and `contributors`. Most are string fields and will be filtered using simple
+    substring matching.  Others are booleans, and can be filtered using truthy values, such as `true`, `false`, `0`, or `1`.
+    Note that quoting `true` or `false` in the query will cause the match to fail regardless. `tags` is an array of simple strings.
+
+    #This Request/Response
+    """
     view_category = 'registrations'
     view_name = 'registration-forks'
 
@@ -405,7 +640,59 @@ class RegistrationProvidersList(NodeProvidersList, RegistrationMixin):
 
 
 class RegistrationNodeLinksList(BaseNodeLinksList, RegistrationMixin):
-    """List of node links for a registration."""
+    """Node Links to other nodes. *Writeable*.
+
+    Node Links act as pointers to other nodes. Unlike Forks, they are not copies of nodes;
+    Node Links are a direct reference to the node that they point to.
+
+    ##Node Link Attributes
+    `type` is "node_links"
+
+        None
+
+    ##Links
+
+    See the [JSON-API spec regarding pagination](http://jsonapi.org/format/1.0/#fetching-pagination).
+
+    ##Relationships
+
+    ### Target Node
+
+    This endpoint shows the target node detail and is automatically embedded.
+
+    ##Actions
+
+    ###Adding Node Links
+        Method:        POST
+        URL:           /links/self
+        Query Params:  <none>
+        Body (JSON): {
+                       "data": {
+                          "type": "node_links",                  # required
+                          "relationships": {
+                            "nodes": {
+                              "data": {
+                                "type": "nodes",                 # required
+                                "id": "{target_node_id}",        # required
+                              }
+                            }
+                          }
+                       }
+                    }
+        Success:       201 CREATED + node link representation
+
+    To add a node link (a pointer to another node), issue a POST request to this endpoint.  This effectively creates a
+    relationship between the node and the target node.  The target node must be described as a relationship object with
+    a "data" member, containing the nodes `type` and the target node `id`.
+
+    ##Query Params
+
+    + `page=<Int>` -- page number of results to view, default 1
+
+    + `filter[<fieldname>]=<Str>` -- fields and values to filter the search results on.
+
+    #This Request/Response
+    """
     view_category = 'registrations'
     view_name = 'registration-pointers'
     serializer_class = RegistrationNodeLinksSerializer
@@ -417,14 +704,51 @@ class RegistrationNodeLinksList(BaseNodeLinksList, RegistrationMixin):
         ExcludeWithdrawals
     )
 
-    required_read_scopes = [CoreScopes.NODE_LINKS_READ]
-    required_write_scopes = [CoreScopes.NODE_LINKS_WRITE]
+    required_read_scopes = [CoreScopes.NODE_REGISTRATIONS_READ]
+    required_write_scopes = [CoreScopes.NULL]
 
     model_class = Pointer
 
 
 class RegistrationNodeLinksDetail(BaseNodeLinksDetail, RegistrationMixin):
-    """Detail of a node link for a registration."""
+    """Node Link details. *Writeable*.
+
+    Node Links act as pointers to other nodes. Unlike Forks, they are not copies of nodes;
+    Node Links are a direct reference to the node that they point to.
+
+    ##Attributes
+    `type` is "node_links"
+
+        None
+
+    ##Links
+
+    *None*
+
+    ##Relationships
+
+    ###Target node
+
+    This endpoint shows the target node detail and is automatically embedded.
+
+    ##Actions
+
+    ###Remove Node Link
+
+        Method:        DELETE
+        URL:           /links/self
+        Query Params:  <none>
+        Success:       204 No Content
+
+    To remove a node link from a node, issue a DELETE request to the `self` link.  This request will remove the
+    relationship between the node and the target node, not the nodes themselves.
+
+    ##Query Params
+
+    *None*.
+
+    #This Request/Response
+    """
     view_category = 'registrations'
     view_name = 'registration-pointer-detail'
     serializer_class = RegistrationNodeLinksSerializer
@@ -434,8 +758,8 @@ class RegistrationNodeLinksDetail(BaseNodeLinksDetail, RegistrationMixin):
         base_permissions.TokenHasScope,
         ExcludeWithdrawals
     )
-    required_read_scopes = [CoreScopes.NODE_LINKS_READ]
-    required_write_scopes = [CoreScopes.NODE_LINKS_WRITE]
+    required_read_scopes = [CoreScopes.NODE_REGISTRATIONS_READ]
+    required_write_scopes = [CoreScopes.NULL]
 
     model_class = Pointer
 
