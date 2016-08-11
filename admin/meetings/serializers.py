cb548d3fc368eb0b5da76becce5e4e63800e02b9
Nan Chen <nc6r@virginia.edu>
2016-08-11 13:08:29 -0400

    [#OSF-6475] only have those comfirmed admins

--- admin/meetings/serializers.py
+++ admin/meetings/serializers.py
@@ -10,7 +10,7 @@ def serialize_meeting(meeting):
         'info_url': meeting.info_url,
         'logo_url': meeting.logo_url,
         'active': meeting.active,
-        'admins': ', '.join([u.emails[0] for u in meeting.admins]),
+        'admins': ', '.join([u.emails[0] for u in meeting.admins if u.is_confirmed]),
         'public_projects': meeting.public_projects,
         'poster': meeting.poster,
         'talk': meeting.talk,
