616b468303bfe44797727aeefb1abefab118133f
Matt Frazier <maf7sm@virginia.edu>
2016-08-15 19:48:45 -0400

    Update node settings frontend

--- website/addons/figshare/static/node-cfg.js
+++ website/addons/figshare/static/node-cfg.js
@@ -1,9 +1,9 @@
 'use strict';
 
-var AddonNodeConfig = require('js/addonNodeConfig').AddonNodeConfig;
+var OauthAddonNodeConfig = require('js/oauthAddonNodeConfig').OauthAddonNodeConfig;
 
-var url = window.contextVars.node.urls.api + 'figshare/config/';
-new AddonNodeConfig('figshare', '#figshareScope', url, '#figshareGrid', {
+var url = window.contextVars.node.urls.api + 'figshare/settings/';
+new OauthAddonNodeConfig('figshare', '#figshareScope', url, '#figshareGrid', {
     onPickFolder: function (evt, item) {
         evt.preventDefault();
         this.selected({name: item.data.name, type: item.data.type, id: item.data.id});
