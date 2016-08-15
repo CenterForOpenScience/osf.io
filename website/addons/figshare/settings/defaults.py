322340c9a39a6d335194a88a8727679748e46878
Matt Frazier <maf7sm@virginia.edu>
2016-08-15 19:47:05 -0400

    Update figshare settings

--- website/addons/figshare/settings/defaults.py
+++ website/addons/figshare/settings/defaults.py
@@ -1,7 +1,21 @@
 CLIENT_ID = None
 CLIENT_SECRET = None
 
-API_URL = 'http://api.figshare.com/v1/'
-API_OAUTH_URL = API_URL + 'my_data/'
+API_BASE_URL = 'https://api.figshare.com/v2/'
 
 MAX_RENDER_SIZE = 1000
+
+FIGSHARE_OAUTH_TOKEN_ENDPOINT = '{}{}'.format(API_BASE_URL, 'token')
+FIGSHARE_OAUTH_AUTH_ENDPOINT = 'https://figshare.com/account/applications/authorize'
+
+FIGSHARE_DEFINED_TYPE_NUM_MAP = {
+    'figure': 1,
+    'media': 2,
+    'dataset': 3,
+    'fileset': 4,
+    'poster': 5,
+    'paper': 6,
+    'presentation': 7,
+    'thesis': 8,
+    'code': 9,
+}
