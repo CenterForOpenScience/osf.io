90040f62e8a02446331124c942aa2959c27b49dd
Matt Frazier <maf7sm@virginia.edu>
2016-08-16 15:44:37 -0400

    Update API User tests for figshare

--- api_tests/users/views/test_user_addons.py
+++ api_tests/users/views/test_user_addons.py
@@ -12,6 +12,7 @@ from tests.utils import mock_auth
 from website.addons.box.tests.factories import BoxAccountFactory
 from website.addons.dataverse.tests.factories import DataverseAccountFactory
 from website.addons.dropbox.tests.factories import DropboxAccountFactory
+from website.addons.figshare.tests.factories import FigshareAccountFactory
 from website.addons.github.tests.factories import GitHubAccountFactory
 from website.addons.googledrive.tests.factories import GoogleDriveAccountFactory
 from website.addons.mendeley.tests.factories import MendeleyAccountFactory
@@ -371,10 +372,6 @@ class TestUserWikiAddon(UserUnmanageableAddonTestSuiteMixin, ApiAddonTestCase):
     short_name = 'wiki'
 
 
-class TestUserFigshareAddon(UserUnmanageableAddonTestSuiteMixin, ApiAddonTestCase):
-    short_name = 'figshare'
-
-
 # OAUTH
 
 
@@ -417,6 +414,10 @@ class TestUserZoteroAddon(UserOAuthAddonTestSuiteMixin, ApiAddonTestCase):
     short_name = 'zotero'
     AccountFactory = ZoteroAccountFactory
 
+
+class TestUserFigshareAddon(UserOAuthAddonTestSuiteMixin, ApiAddonTestCase):
+    short_name = 'figshare'
+    AccountFactory = FigshareAccountFactory
 
 class TestUserInvalidAddon(UserAddonTestSuiteMixin, ApiAddonTestCase):
     addon_type = 'INVALID'
