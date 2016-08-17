d589e81f7629e5f67fd170e0ab3277a914dc86a1
erinspace <erin.braswell@gmail.com>
2016-08-17 12:44:34 -0400

    Add url for getting all of a provider's preprints

--- api/preprint_providers/urls.py
+++ api/preprint_providers/urls.py
@@ -6,4 +6,5 @@ from api.preprint_providers import views
 urlpatterns = [
     url(r'^$', views.PreprintProviderList.as_view(), name=views.PreprintProviderList.view_name),
     url(r'^(?P<provider_id>\w+)/$', views.PreprintProviderDetail.as_view(), name=views.PreprintProviderDetail.view_name),
+    url(r'^(?P<provider_id>\w+)/preprints/$', views.PreprintProviderPreprintList.as_view(), name=views.PreprintProviderPreprintList.view_name),
 ]
