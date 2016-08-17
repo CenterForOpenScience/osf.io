3e614ba8df2097ef9c12a079c2505ebb7e39b36b
Matt Frazier <maf7sm@virginia.edu>
2016-08-17 12:03:29 -0400

    Add migration script for figshare logs     -update log template

---
+++ scripts/figshare/migrate_folder_logs.py
@@ -0,0 +1,71 @@
+import sys
+import logging
+
+from framework.mongo import database
+from framework.transactions.context import TokuTransaction
+from scripts import utils as script_utils
+from website.app import init_app
+
+logger = logging.getLogger(__name__)
+
+
+def validate_migration(expected_count):
+    logger.info('Validating migration...')
+    
+    updated_logs = database['nodelog'].find({'action': 'figshare_folder_selected'})
+    actual_count = updated_logs.count()
+    for log in updated_logs:
+        assert log['params']['folder_name'] == log['params']['figshare']['title']
+        assert log['params']['folder_path'] == log['params']['figshare']['type']
+        assert log['params']['folder_id'] == log['params']['figshare']['id']
+
+    assert actual_count == expected_count
+    logger.info('Done.')
+
+
+def do_migration(records, dry=False):
+    count = records.count()
+
+    for log in records:
+        logger.info(
+            'Migrating log - {}, '.format(log['_id'])
+        )
+
+        params = log['params']
+        params.update({
+            'folder_name': log['params']['figshare']['title'],
+            'folder_path': log['params']['figshare']['type'],
+            'folder_id': log['params']['figshare']['id']
+        })
+        database['nodelog'].find_and_modify(
+            {'_id': log['_id']},
+            {
+                '$set': {
+                    'params': params,
+                    'action': 'figshare_folder_selected'
+                }
+            }
+        )
+
+    validate_migration(count)
+
+    logger.info('{}Migrated {} logs'.format('[dry]'if dry else '', count))
+
+
+def get_targets():
+    return database['nodelog'].find({'action': 'figshare_content_linked'})
+
+
+def main():
+    init_app(routes=False)  # Sets the storage backends on all models
+    dry = '--dry' in sys.argv
+    if not dry:
+        script_utils.add_file_logger(logger, __file__)
+    with TokuTransaction():
+        do_migration(get_targets(), dry)
+        if dry:
+            raise RuntimeError('Dry run, transaction rolled back')
+
+
+if __name__ == '__main__':
+    main()
