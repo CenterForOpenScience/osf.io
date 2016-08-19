75606e2b13a29a5d68894eda86dbede8292fb0c8
erinspace <erin.braswell@gmail.com>
2016-08-19 16:13:13 -0400

    Add unique index on the subject model for @chrisseto

--- website/project/taxonomies/__init__.py
+++ website/project/taxonomies/__init__.py
@@ -1,3 +1,4 @@
+import pymongo
 from modularodm import fields
 
 from framework.mongo import (
@@ -10,6 +11,16 @@ from website.util import api_v2_url
 
 @mongo_utils.unique_on(['text'])
 class Subject(StoredObject):
+
+    __indices__ = [
+        {
+            'unique': True,
+            'key_or_list': [
+                ('text', pymongo.DESCENDING),
+            ]
+        }
+    ]
+
     _id = fields.StringField(primary=True, default=lambda: str(ObjectId()))
     text = fields.StringField(required=True)
     parents = fields.ForeignField('subject', list=True)
