0c5d1f78e5d6ae48a48c7d279bfea0338efd02b9
erinspace <erin.braswell@gmail.com>
2016-08-19 17:04:27 -0400

    Add preprint_providers list to preprints urls

--- api/preprints/urls.py
+++ api/preprints/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^$', views.PreprintList.as_view(), name=views.PreprintList.view_name),
     url(r'^(?P<node_id>\w+)/$', views.PreprintDetail.as_view(), name=views.PreprintDetail.view_name),
     url(r'^(?P<node_id>\w+)/contributors/$', views.PreprintContributorsList.as_view(), name=views.PreprintContributorsList.view_name),
-    url(r'^(?P<node_id>\w+)/relationships/preprint_provider/$', views.PreprintToPreprintProviderRelationship.as_view(), name=views.PreprintToPreprintProviderRelationship.view_name),
+    url(r'^(?P<node_id>\w+)/relationships/preprint_providers/$', views.PreprintToPreprintProviderRelationship.as_view(), name=views.PreprintToPreprintProviderRelationship.view_name),
+    url(r'^(?P<node_id>\w+)/preprint_providers/$', views.PreprintPreprintProvidersList.as_view(), name=views.PreprintPreprintProvidersList.view_name),
 ]
