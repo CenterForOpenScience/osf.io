f712a03fea451b846e6f8b3e33a685dc5794f923
Steven Loria <sloria1@gmail.com>
2016-08-22 15:33:19 -0400

    Handle when database is None

--- framework/transactions/commands.py
+++ framework/transactions/commands.py
@@ -1,35 +1,41 @@
 # -*- coding: utf-8 -*-
+import contextlib
+
 import logging
 from framework.mongo import database as proxy_database
 from website import settings as osfsettings
 
 logger = logging.getLogger(__name__)
 
-
-def begin(database=None):
-    database = database or proxy_database
-    database.command('beginTransaction')
-
-
-def rollback(database=None):
-    database = database or proxy_database
-    database.command('rollbackTransaction')
-
-
-def commit(database=None):
-    database = database or proxy_database
-    database.command('commitTransaction')
-
-
-def show_live(database=None):
-    database = database or proxy_database
-    return database.command('showLiveTransactions')
-
-
-def disconnect(database=None):
-    database = database or proxy_database
+@contextlib.contextmanager
+def handle_missing_client():
     try:
-        database.connection.close()
+        yield
     except AttributeError:
         if not osfsettings.DEBUG_MODE:
             logger.error('MongoDB client not attached to request.')
+
+def begin(database=None):
+    database = database or proxy_database
+    with handle_missing_client():
+        database.command('beginTransaction')
+
+def rollback(database=None):
+    database = database or proxy_database
+    with handle_missing_client():
+        database.command('rollbackTransaction')
+
+def commit(database=None):
+    database = database or proxy_database
+    with handle_missing_client():
+        database.command('commitTransaction')
+
+def show_live(database=None):
+    database = database or proxy_database
+    with handle_missing_client():
+        return database.command('showLiveTransactions')
+
+def disconnect(database=None):
+    database = database or proxy_database
+    with handle_missing_client():
+        database.connection.close()
