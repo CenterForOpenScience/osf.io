f38210694d8f29df181a3209c37debe29ad5446c
Chris Wisecarver <cwisecarver@cos.io>
2016-08-23 11:20:50 -0400

    Adding a couple of guid tests

--- osf_models_tests/test_guid.py
+++ osf_models_tests/test_guid.py
@@ -16,17 +16,57 @@ class TestReferent:
         guid = Guid.objects.get(guid=obj._id)
         assert guid.referent == obj
 
-    def test_referent_can_be_set(self):
+    @pytest.mark.parametrize('Factory',
+    [
+        UserFactory,
+        NodeFactory
+    ])
+    def test_referent_can_be_set(self, Factory):
+        obj = Factory()
+        obj1 = Factory()
+
+        guid = Guid.load(obj._id)
+        assert guid.referent == obj  # sanity check
+
+        guid.referent = obj1
+        assert guid.referent == obj1
+
+    def test_swapping_guids(self):
         user = UserFactory()
         node = NodeFactory()
 
-        guid = Guid.load(user._id)
-        assert guid.referent == user  # sanity check
+        user_guid = user._guid
+        node_guid = node._guid
 
-        node._guid = None
-        node.save()
-        guid.referent = node
+        user._guid = node_guid
+        node._guid = user_guid
 
-        # Guid points to both node and user
-        assert node._id == guid.guid
-        assert user._id == guid.guid
+        assert node_guid == user._guid
+        assert user_guid == node._guid
+
+    def test_id_matches(self):
+        user = UserFactory()
+
+        assert user._id == user._guid.guid
+
+    @pytest.mark.parametrize('Factory',
+     [
+         UserFactory,
+         NodeFactory
+     ])
+    def test_nulling_out_guid(self, Factory):
+        obj = Factory()
+
+        guid = Guid.load(obj._id)
+
+        obj._guid = None
+
+        obj.save()
+        obj.refresh_from_db()
+
+        # queryset cache returns the old version
+        guid.refresh_from_db()
+
+        assert obj._guid != guid
+
+        assert guid.guid != obj._guid.guid
