2d8c1d078e904018da98b73b50239a817c884f44
Steven Loria <sloria1@gmail.com>
2016-08-24 10:25:39 -0400

    Add/fix GUID tests

--- osf_models_tests/test_guid.py
+++ osf_models_tests/test_guid.py
@@ -1,7 +1,35 @@
 import pytest
 
-from osf_models.models import Guid
-from .factories import UserFactory, NodeFactory
+from osf_models.models import Guid, NodeLicenseRecord, OSFUser
+from osf_models.modm_compat import Q
+from .factories import UserFactory, NodeFactory, NodeLicenseRecordFactory, RegistrationFactory
+
+@pytest.mark.django_db
+class TestGuid:
+
+    def test_long_id_gets_generated_on_creation(self):
+        obj = NodeLicenseRecordFactory()
+        assert obj._id
+        assert len(obj._id) > 5
+
+    def test_loading_by_object_id(self):
+        obj = NodeLicenseRecordFactory()
+        assert NodeLicenseRecord.load(obj._id) == obj
+
+    def test_loading_by_short_guid(self):
+        obj = UserFactory()
+        assert OSFUser.load(obj._id) == obj
+
+    @pytest.mark.parametrize('Factory',
+    [
+        UserFactory,
+        NodeFactory,
+        RegistrationFactory,
+    ])
+    def test_short_guid_gets_generated_on_creation(self, Factory):
+        obj = Factory()
+        assert obj._id
+        assert len(obj._id) == 5
 
 @pytest.mark.django_db
 class TestReferent:
@@ -15,6 +43,12 @@ class TestReferent:
         obj = Factory()
         guid = Guid.objects.get(guid=obj._id)
         assert guid.referent == obj
+
+    def test_querying_on_referent(self):
+        user = UserFactory()
+
+        guids = Guid.find(Q('referent', 'eq', user))
+        assert user.guid in guids
 
     @pytest.mark.parametrize('Factory',
     [
@@ -35,19 +69,19 @@ class TestReferent:
         user = UserFactory()
         node = NodeFactory()
 
-        user_guid = user._guid
-        node_guid = node._guid
+        user_guid = user.guid
+        node_guid = node.guid
 
-        user._guid = node_guid
-        node._guid = user_guid
+        user.guid = node_guid
+        node.guid = user_guid
 
-        assert node_guid == user._guid
-        assert user_guid == node._guid
+        assert node_guid == user.guid
+        assert user_guid == node.guid
 
     def test_id_matches(self):
         user = UserFactory()
 
-        assert user._id == user._guid.guid
+        assert user._id == user.guid.guid
 
     @pytest.mark.parametrize('Factory',
      [
@@ -59,7 +93,7 @@ class TestReferent:
 
         guid = Guid.load(obj._id)
 
-        obj._guid = None
+        obj.guid = None
 
         obj.save()
         obj.refresh_from_db()
@@ -67,6 +101,6 @@ class TestReferent:
         # queryset cache returns the old version
         guid.refresh_from_db()
 
-        assert obj._guid != guid
+        assert obj.guid != guid
 
-        assert guid.guid != obj._guid.guid
+        assert guid.guid != obj.guid.guid
