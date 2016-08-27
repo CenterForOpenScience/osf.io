1009f2d97d5afd45e720783470412c10af5a1f76
Matt Frazier <maf7sm@virginia.edu>
2016-08-27 19:37:51 -0400

    Add TODO

--- website/static/js/fangorn.js
+++ website/static/js/fangorn.js
@@ -2275,6 +2275,8 @@ function _dropLogic(event, items, folder) {
 
     $.each(items, function(index, item) {
         // Check all the ways that the primary preprint file could be moved out of its current node
+        // TODO: this will break when preprints can be created from existing projects -- it relies on
+        //       the fact that the current node is the preprint, not and node in the component tree.
         if (
             window.contextVars.node.preprintFileId === item.data.path.replace('/', '') &&
             item.data.nodeId === window.contextVars.node.id &&
