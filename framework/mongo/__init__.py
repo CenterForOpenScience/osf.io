3b5449806c5145fe9f78ae6085439779caf17721
Sam Chrisinger <s.chrisinger@gmail.com>
2016-09-01 14:36:53 -0400

    Use a more real fake ip address
    [skip ci]

--- framework/mongo/__init__.py
+++ framework/mongo/__init__.py
@@ -12,7 +12,7 @@ from api.base.api_globals import api_globals
 
 
 class DummyRequest(object):
-    remote_addr = 'https://dinosaurs.sexy'
+    remote_addr = '146.9.219.56'
     headers = {
         'User-Agent': 'Mozilla/5.0 (X11; U; SunOS sun4u; en-US; rv:0.9.4.1) Gecko/20020518 Netscape6/6.2.3'
     }
