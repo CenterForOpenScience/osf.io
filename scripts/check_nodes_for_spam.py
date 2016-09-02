61538c3fc273a111656c7d0a8d5d7d6e9560887f
Sam Chrisinger <s.chrisinger@gmail.com>
2016-09-02 13:16:05 -0400

    Update spam-checking script

--- scripts/check_nodes_for_spam.py
+++ scripts/check_nodes_for_spam.py
@@ -10,6 +10,8 @@ from website.search.elastic_search import serialize_node, get_doctype_from_node
 parser = argparse.ArgumentParser()
 parser.add_argument('-n', '--number', type=int, default=200,
                     help='Number of nodes to look at. Uses N most recent nodes.')
+parser.add_argument('-f', '--flag', type=bool, default=False,
+                    help='Actually flag nodes as spam?')
 
 def generate_ip():
     ip_ranges = zip(
@@ -25,18 +27,18 @@ def check_nodes(num_nodes, flag=False):
     for node in nodes:
         serialized = serialize_node(node, get_doctype_from_node(node))
         spam.check_node_for_spam(
-            serialized,
-            node.creator,
+            node,
             {
                 'Remote-Addr': generate_ip(),
                 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'
-            }          
+            },
+            flag=False
         )
 
 def main():
     args = parser.parse_args()
-    init_app(routes=False)    
-    check_nodes(args.number)
+    init_app(routes=False)
+    check_nodes(args.number, flag=args.flag)
 
 
 main() if __name__ == '__main__' else None
