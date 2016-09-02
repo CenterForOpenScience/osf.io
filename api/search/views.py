c39e799b8fe846319ec812f125218eb4a5b5b44b
Casey Rollins <cmr3rm@virginia.edu>
2016-09-02 13:51:52 -0400

    Add base search views.
    - Add views for projects, components, registrations, users.

--- api/search/views.py
+++ api/search/views.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from modularodm import Q
+
 from rest_framework import generics, permissions as drf_permissions
 
 from api.base import permissions as base_permissions
@@ -7,10 +11,18 @@ from api.files.serializers import FileSerializer
 from api.institutions.serializers import InstitutionSerializer
 from api.nodes.serializers import NodeSerializer
 from api.registrations.serializers import RegistrationSerializer
-from api.search.serializers import SearchSerializer
+from api.search.serializers import (
+    SearchSerializer,
+)
 from api.users.serializers import UserSerializer
 
 from framework.auth.oauth_scopes import CoreScopes, ComposedScopes
+from framework.auth.core import User
+
+from website.models import Node
+
+from website.search import search
+from website.search.util import build_query
 
 
 class Search(JSONAPIBaseView, generics.ListAPIView):
@@ -46,7 +58,16 @@ class SearchComponents(JSONAPIBaseView, generics.ListAPIView):
     view_name = 'search-components'
 
     def get_queryset(self):
-        pass
+        query = self.request.query_params.get('q', '*')
+        res = search.search(build_query(query), doc_type='component')
+        results = res.get('results')
+        # what if results is None?
+        nodes = []
+        for item in results:
+            node = Node.load(item.get('id'))
+            if node and not node.is_deleted:
+                nodes.append(node)
+        return nodes
 
 
 class SearchFiles(JSONAPIBaseView, generics.ListAPIView):
@@ -95,7 +116,16 @@ class SearchProjects(JSONAPIBaseView, generics.ListAPIView):
     view_name = 'search-projects'
 
     def get_queryset(self):
-        pass
+        query = self.request.query_params.get('q', '*')
+        res = search.search(build_query(query), doc_type='project')
+        results = res.get('results')
+        # what if results is None?
+        nodes = []
+        for item in results:
+            node = Node.load(item.get('id'))
+            if node and not node.is_deleted:
+                nodes.append(node)
+        return nodes
 
 
 class SearchRegistrations(JSONAPIBaseView, generics.ListAPIView):
@@ -112,7 +142,16 @@ class SearchRegistrations(JSONAPIBaseView, generics.ListAPIView):
     view_name = 'search-registrations'
 
     def get_queryset(self):
-        pass
+        query = self.request.query_params.get('q', '*')
+        res = search.search(build_query(query), doc_type='registration')
+        results = res.get('results')
+        # what if results is None?
+        nodes = []
+        for item in results:
+            node = Node.load(item.get('id'))
+            if node and not node.is_deleted:
+                nodes.append(node)
+        return nodes
 
 
 class SearchUsers(JSONAPIBaseView, generics.ListAPIView):
@@ -129,4 +168,14 @@ class SearchUsers(JSONAPIBaseView, generics.ListAPIView):
     view_name = 'search-users'
 
     def get_queryset(self):
-        pass
+        query = self.request.query_params.get('q', '*')
+        res = search.search(build_query(query), doc_type='user')
+        results = res.get('results')
+        # what if results is None?
+        users = []
+        for item in results:
+            user = User.load(item.get('id'))
+            # same checks for /v2/users/
+            if user and user.is_registered and not user.is_merged and not user.date_disabled:
+                users.append(user)
+        return users
