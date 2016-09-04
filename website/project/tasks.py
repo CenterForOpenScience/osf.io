a9b4f573fe99c3609d75a07922106cea2917eafb
icereval <icereval@gmail.com>
2016-09-04 19:17:29 -0400

    avoid search and spam updates for collection and archiving nodes

--- website/project/tasks.py
+++ website/project/tasks.py
@@ -6,16 +6,15 @@ def on_node_updated(node_id, saved_fields, request_headers=None):
     from website.models import Node
     node = Node.load(node_id)
 
-    if request_headers:
-        if node.check_spam(saved_fields, request_headers):
+    if node.is_collection or node.archiving:
+        if request_headers and node.check_spam(saved_fields, request_headers):
             node.save()
 
-    need_update = bool(node.SEARCH_UPDATE_FIELDS.intersection(saved_fields))
-    # due to async nature of call this can issue a search delete for a new record (acceptable trade-off)
-    if not node.is_public and 'is_public' not in saved_fields:
-        need_update = False
-    # short circuit unneeded celery task for search update
-    if node.is_collection or node.archiving:
-        need_update = False
-    if need_update:
-        node.update_search()
+        need_update = bool(node.SEARCH_UPDATE_FIELDS.intersection(saved_fields))
+        # due to async nature of call this can issue a search delete for a new record (acceptable trade-off)
+        if 'spam_status' in saved_fields:
+            need_update = True
+        elif not node.is_public and 'is_public' not in saved_fields:
+            need_update = False
+        if need_update:
+            node.update_search()
