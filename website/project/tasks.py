f3fe75c4daebb42a07f001a90b3adab0013984fb
icereval <icereval@gmail.com>
2016-09-04 20:02:22 -0400

    typo

--- website/project/tasks.py
+++ website/project/tasks.py
@@ -6,15 +6,17 @@ def on_node_updated(node_id, saved_fields, request_headers=None):
     from website.models import Node
     node = Node.load(node_id)
 
-    if not node.is_collection and not node.archiving:
-        if request_headers and node.check_spam(saved_fields, request_headers):
-            node.save()
+    if node.is_collection or node.archiving:
+        return
 
-        need_update = bool(node.SEARCH_UPDATE_FIELDS.intersection(saved_fields))
-        # due to async nature of call this can issue a search delete for a new record (acceptable trade-off)
-        if 'spam_status' in saved_fields:
-            need_update = True
-        elif not node.is_public and 'is_public' not in saved_fields:
-            need_update = False
-        if need_update:
-            node.update_search()
+    if request_headers and node.check_spam(saved_fields, request_headers):
+        node.save()
+
+    need_update = bool(node.SEARCH_UPDATE_FIELDS.intersection(saved_fields))
+    # due to async nature of call this can issue a search delete for a new record (acceptable trade-off)
+    if 'spam_status' in saved_fields:
+        need_update = True
+    elif not node.is_public and 'is_public' not in saved_fields:
+        need_update = False
+    if need_update:
+        node.update_search()
