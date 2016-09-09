d7dbd2659aa80d86bf5b4ac7f47a80e16b107788
Matt Frazier <maf7sm@virginia.edu>
2016-09-09 00:26:34 -0400

    Add helper to get request and user

--- framework/mongo/__init__.py
+++ framework/mongo/__init__.py
@@ -31,6 +31,31 @@ def get_cache_key():
             return dummy_request
 
 
+def get_request_and_user_id():
+    """
+    Fetch a request key and user id from either a Django or Flask request. Fall back on a process-global dummy object
+    if we are not in either type of request
+    """
+    # TODO: This is ugly use of exceptions; is there a better way to track whether in a given type of request?
+    from framework.sessions import get_session
+
+    try:
+        req = request._get_current_object()
+        session = get_session()
+        user_id = session.data.get('auth_user_id')
+        return req, user_id
+    except RuntimeError:  # Not in a flask request context
+        if getattr(api_globals, 'request', None) is not None:
+            req = api_globals.request
+            try:
+                user_id = req.user._id
+            except AttributeError:  # Request comes from Admin Module
+                return dummy_request, None
+            return req, user_id
+        else:  # Not in a Django request
+            return dummy_request, None
+
+
 @with_proxies(proxied_members, get_cache_key)
 class StoredObject(GenericStoredObject):
     pass
