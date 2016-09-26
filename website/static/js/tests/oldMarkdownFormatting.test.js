0590edfbd363f4d076888a4f2726c13cb257495d
erinspace <erin.braswell@gmail.com>
2016-09-26 17:50:43 -0400

    Add test to check ordered list formatting

---
+++ website/static/js/tests/oldMarkdownFormatting.test.js
@@ -0,0 +1,24 @@
+/*global describe, it, expect, example, before, after, beforeEach, afterEach, mocha, sinon*/
+'use strict';
+var assert = require('chai').assert;
+var mdOld = require('js/markdown').old;
+
+
+// Add sinon asserts to chai.assert, so we can do assert.calledWith instead of sinon.assert.calledWith
+sinon.assert.expose(assert, {prefix: ''});
+
+describe('oldMarkdown', () => {
+
+    describe('orderedList', () => {
+
+        it('returns a numbered list', () => {
+            var text = '1. hello \n 2. there';
+
+            var rendered = mdOld.render(text);
+
+            var html = '<ol>\n<li>hello</li><li>there</li></ol>';
+            assert.equal(rendered, html);
+        });
+    });
+
+});
