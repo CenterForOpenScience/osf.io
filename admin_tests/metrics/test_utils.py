94c08aa7770bdc74b365fbb025e21095c710ae0a
Nan Chen <nc6r@virginia.edu>
2016-09-27 02:12:24 -0400

    recompile test

--- admin_tests/metrics/test_utils.py
+++ admin_tests/metrics/test_utils.py
@@ -210,6 +210,7 @@ class TestRenderToCSVResponse(AdminTestCase):
         queryset = OSFWebsiteStatistics.objects.all().order_by('-date')
         response = render_to_csv_response(queryset)
         self.assertEqual(response['Content-Type'], 'text/csv')
-        self.assertEqual(response.content.split('\n'), self.FULL_PERSON_CSV_NO_VERBOSE)
+        self.assertEqual(response.content.split('\n'),
+                         self.FULL_PERSON_CSV_NO_VERBOSE)
         self.assertRegexpMatches(response['Content-Disposition'],
                                  r'attachment; filename=osfwebsitestatistics_export.csv;')
