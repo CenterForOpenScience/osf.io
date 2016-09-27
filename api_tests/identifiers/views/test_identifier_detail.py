6b0b1dc51f99dfefa79afa0b3933de490652152b
Casey Rollins <cmr3rm@virginia.edu>
2016-09-27 11:26:35 -0400

    Add identifier tests for nodes.

--- api_tests/identifiers/views/test_identifier_detail.py
+++ api_tests/identifiers/views/test_identifier_detail.py
@@ -8,12 +8,15 @@ from tests.base import ApiTestCase
 from tests.factories import (
     RegistrationFactory,
     AuthUserFactory,
-    IdentifierFactory
+    IdentifierFactory,
+    NodeFactory,
 )
 
-class TestIdentifierDetail(ApiTestCase):
+
+class TestRegistrationIdentifierDetail(ApiTestCase):
+
     def setUp(self):
-        super(TestIdentifierDetail, self).setUp()
+        super(TestRegistrationIdentifierDetail, self).setUp()
         self.user = AuthUserFactory()
 
         self.registration = RegistrationFactory(creator=self.user, is_public=True)
@@ -24,7 +27,7 @@ class TestIdentifierDetail(ApiTestCase):
         self.data = self.res.json['data']
 
     def tearDown(self):
-        super(TestIdentifierDetail, self).tearDown()
+        super(TestRegistrationIdentifierDetail, self).tearDown()
         Identifier.remove()
 
     def test_identifier_detail_success(self):
@@ -34,6 +37,38 @@ class TestIdentifierDetail(ApiTestCase):
     def test_identifier_detail_returns_correct_referent(self):
         path = urlparse.urlparse(self.data['relationships']['referent']['links']['related']['href']).path
         assert_equal('/{}registrations/{}/'.format(API_BASE, self.registration._id), path)
+
+    def test_identifier_detail_returns_correct_category(self):
+        assert_equal(self.data['attributes']['category'], self.identifier.category)
+
+    def test_identifier_detail_returns_correct_value(self):
+        assert_equal(self.data['attributes']['value'], self.identifier.value)
+
+
+class TestNodeIdentifierDetail(ApiTestCase):
+
+    def setUp(self):
+        super(TestNodeIdentifierDetail, self).setUp()
+        self.user = AuthUserFactory()
+
+        self.node = NodeFactory(creator=self.user, is_public=True)
+        self.identifier = IdentifierFactory(referent=self.node)
+        self.url = '/{}identifiers/{}/'.format(API_BASE, self.identifier._id)
+
+        self.res = self.app.get(self.url)
+        self.data = self.res.json['data']
+
+    def tearDown(self):
+        super(TestNodeIdentifierDetail, self).tearDown()
+        Identifier.remove()
+
+    def test_identifier_detail_success(self):
+        assert_equal(self.res.status_code, 200)
+        assert_equal(self.res.content_type, 'application/vnd.api+json')
+
+    def test_identifier_detail_returns_correct_referent(self):
+        path = urlparse.urlparse(self.data['relationships']['referent']['links']['related']['href']).path
+        assert_equal('/{}nodes/{}/'.format(API_BASE, self.node._id), path)
 
     def test_identifier_detail_returns_correct_category(self):
         assert_equal(self.data['attributes']['category'], self.identifier.category)
