4a4e96f77116a582497951c37b05cda5c9cee5cc
Bryan Gorges <bgorges@cos.io>
2016-09-27 09:45:25 -0400

    flake

--- admin_tests/users/test_views.py
+++ admin_tests/users/test_views.py
@@ -4,7 +4,7 @@ from nose import tools as nt
 import mock
 import csv
 import os
-from datetime import datetime, timedelta
+from datetime import datetime
 
 from tests.base import AdminTestCase
 from website import settings
