a09c8a4c6ebf896adf51ef57a51904220ff504e0
Casey Rollins <cmr3rm@virginia.edu>
2016-09-28 13:26:52 -0400

    Add test for getting dashboard institutions.

--- tests/framework_tests/test_routing.py
+++ tests/framework_tests/test_routing.py
@@ -8,6 +8,12 @@ from webtest_plus import TestApp
 from framework.exceptions import HTTPError
 from framework.routing import json_renderer, process_rules, Rule
 
+from tests import factories
+from tests.base import OsfTestCase
+
+from website import routes
+from website import settings
+
 def error_view():
     raise HTTPError(400)
 
@@ -40,3 +46,34 @@ class TestJSONRenderer(unittest.TestCase):
         data = res.json
         assert_equal(data['message_short'], 'Invalid')
         assert_equal(data['message_long'], 'Invalid request')
+
+
+class TestGetGlobals(OsfTestCase):
+
+    def setUp(self):
+        super(TestGetGlobals, self).setUp()
+
+        self.inst_one = factories.InstitutionFactory()
+        self.inst_two = factories.InstitutionFactory()
+
+        self.user = factories.AuthUserFactory()
+        self.user.affiliated_institutions.append(self.inst_one)
+        self.user.affiliated_institutions.append(self.inst_two)
+        self.user.save()
+
+        for i in range(settings.INSTITUTION_DISPLAY_NODE_THRESHOLD):
+            node = factories.NodeFactory(creator=self.user, is_public=True)
+            node.affiliated_institutions.append(self.inst_one)
+            node.save()
+
+        for i in range(settings.INSTITUTION_DISPLAY_NODE_THRESHOLD - 1):
+            node = factories.NodeFactory(creator=self.user, is_public=True)
+            node.affiliated_institutions.append(self.inst_two)
+            node.save()
+
+    def test_get_globals_dashboard_institutions(self):
+        globals = routes.get_globals()
+        dashboard_institutions = globals['dashboard_institutions']
+        assert_equal(len(dashboard_institutions), 1)
+        assert_equal(dashboard_institutions[0]['id'], self.inst_one._id)
+        assert_not_equal(dashboard_institutions[0]['id'], self.inst_two._id)
