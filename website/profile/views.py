7889d1dede7527545367e11d3da90c4362eb7776
Steven Loria <sloria1@gmail.com>
2016-09-30 12:01:22 -0400

    Fix query for ApiOAuth2Application

--- website/profile/views.py
+++ website/profile/views.py
@@ -418,7 +418,7 @@ def oauth_application_detail(auth, **kwargs):
     # The client ID must be an active and existing record, and the logged-in user must have permission to view it.
     try:
         #
-        record = ApiOAuth2Application.find_one(Q('client_id', 'eq', uuid.UUID(client_id)))
+        record = ApiOAuth2Application.find_one(Q('client_id', 'eq', client_id))
     except NoResultsFound:
         raise HTTPError(http.NOT_FOUND)
     except ValueError:  # Invalid client ID -- ApiOAuth2Application will not exist
