250188b41f593a85bae2dd05fe2a7f9615082a3c
erinspace <erin.braswell@gmail.com>
2016-10-06 10:13:02 -0400

    Move activity and hits and node data methods to utils

--- website/project/utils.py
+++ website/project/utils.py
@@ -2,7 +2,11 @@
 """Various node-related utilities."""
 from modularodm import Q
 
-from website.project.model import Node
+from website import settings
+from website.project import Node
+
+from keen import KeenClient
+
 
 # Alias the project serializer
 from website.project.views.node import _view_project
@@ -45,3 +49,98 @@ def recent_public_registrations(n=10):
             continue
         n -= 1
         yield reg
+
+
+def get_node_data():
+    max_popular_projects = 20
+
+    if settings.KEEN['public']['read_key']:
+        client = KeenClient(
+            project_id=settings.KEEN['public']['project_id'],
+            read_key=settings.KEEN['public']['read_key'],
+        )
+
+        node_pageviews = client.count(
+            event_collection='pageviews',
+            timeframe='this_7_days',
+            group_by='node.id',
+            filters=[
+                {
+                    'property_name': 'node.id',
+                    'operator': 'exists',
+                    'property_value': True
+                }
+            ]
+        )
+
+        node_visits = client.count_unique(
+            event_collection='pageviews',
+            target_property='anon.id',
+            timeframe='this_7_days',
+            group_by='node.id',
+            filters=[
+                {
+                    'property_name': 'node.id',
+                    'operator': 'exists',
+                    'property_value': True
+                }
+            ]
+        )
+
+        node_data = [{'node': x['node.id'], 'views': x['result']} for x in node_pageviews[0:max_popular_projects]]
+
+        for node_visit in node_visits[0:max_popular_projects]:
+            for node_result in node_data:
+                if node_visit['node.id'] == node_result['node']:
+                    node_result.update({'visits': node_visit['result']})
+
+        node_data.sort(key=lambda datum: datum['views'], reverse=True)
+
+        return node_data
+
+
+def get_popular_projects_and_registrations(node_data):
+    popular_public_projects = []
+    popular_public_registrations = []
+
+    for nid in node_data:
+        node = Node.load(nid['node'])
+        if node is None:
+            continue
+        if node.is_public and not node.is_registration and not node.is_deleted:
+            if len(popular_public_projects) < 10:
+                popular_public_projects.append(node)
+        elif node.is_public and node.is_registration and not node.is_deleted and not node.is_retracted:
+            if len(popular_public_registrations) < 10:
+                popular_public_registrations.append(node)
+        if len(popular_public_projects) >= 10 and len(popular_public_registrations) >= 10:
+            break
+
+    return popular_public_projects, popular_public_registrations
+
+
+def hits(node_data):
+    return {
+        datum['node']: {
+            'hits': datum['views'],
+            'visits': datum['visits']
+        } for datum in node_data
+    }
+
+
+def activity():
+    node_data = get_node_data()
+    popular_public_projects, popular_public_registrations = get_popular_projects_and_registrations(node_data)
+    project_hits = hits(node_data)
+
+    # Projects
+    new_and_noteworthy_pointers = Node.find_one(Q('_id', 'eq', settings.NEW_AND_NOTEWORTHY_LINKS_NODE)).nodes_pointer
+    new_and_noteworthy_projects = [pointer.node for pointer in new_and_noteworthy_pointers]
+
+    return {
+        'new_and_noteworthy_projects': new_and_noteworthy_projects,
+        'recent_public_registrations': recent_public_registrations(),
+        'popular_public_projects': popular_public_projects,
+        'popular_public_registrations': popular_public_registrations,
+        'hits': project_hits,
+    }
