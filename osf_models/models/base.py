8f99f76bb3c2f5b38dd6ecd98cef9f24db93c512
Steven Loria <sloria1@gmail.com>
2016-10-06 09:54:46 -0400

    Fix .load() so that it doesn't load other model instances

--- osf_models/models/base.py
+++ osf_models/models/base.py
@@ -418,8 +418,9 @@ class GuidMixin(BaseIDMixin):
     @classmethod
     def load(cls, q):
         try:
+            content_type = ContentType.objects.get_for_model(cls)
             # if referent doesn't exist it will return None
-            return Guid.objects.get(_id=q).referent
+            return Guid.objects.get(_id=q, content_type=content_type).referent
         except Guid.DoesNotExist:
             # modm doesn't throw exceptions when loading things that don't exist
             return None
