5d805d3cc3f0a9de8e82a5f56b401c1662929e2b
erinspace <erin.braswell@gmail.com>
2016-10-11 12:17:51 -0400

    Break out Keen call and update formatting for activity call

--- website/project/utils.py
+++ website/project/utils.py
@@ -51,105 +51,81 @@ def recent_public_registrations(n=10):
         yield reg
 
 
-def get_popular_nodes():
-    max_popular_projects = 20
+def get_keen_activity():
+    client = KeenClient(
+        project_id=settings.KEEN['public']['project_id'],
+        read_key=settings.KEEN['public']['read_key'],
+    )
 
-    if settings.KEEN['public']['read_key']:
-        client = KeenClient(
-            project_id=settings.KEEN['public']['project_id'],
-            read_key=settings.KEEN['public']['read_key'],
-        )
+    node_pageviews = client.count(
+        event_collection='pageviews',
+        timeframe='this_7_days',
+        group_by='node.id',
+        filters=[
+            {
+                'property_name': 'node.id',
+                'operator': 'exists',
+                'property_value': True
+            }
+        ]
+    )
 
-        node_pageviews = client.count(
-            event_collection='pageviews',
-            timeframe='this_7_days',
-            group_by='node.id',
-            filters=[
-                {
-                    'property_name': 'node.id',
-                    'operator': 'exists',
-                    'property_value': True
-                }
-            ]
-        )
+    node_visits = client.count_unique(
+        event_collection='pageviews',
+        target_property='anon.id',
+        timeframe='this_7_days',
+        group_by='node.id',
+        filters=[
+            {
+                'property_name': 'node.id',
+                'operator': 'exists',
+                'property_value': True
+            }
+        ]
+    )
 
-        node_visits = client.count_unique(
-            event_collection='pageviews',
-            target_property='anon.id',
-            timeframe='this_7_days',
-            group_by='node.id',
-            filters=[
-                {
-                    'property_name': 'node.id',
-                    'operator': 'exists',
-                    'property_value': True
-                }
-            ]
-        )
-
-        return node_pageviews, node_visits
-
-    else:
-        # just return the previous public projects and registrations
-        popular_project_pointers = Node.find_one(Q('_id', 'eq', settings.POPULAR_LINKS_NODE)).nodes_pointer
-        popular_projects = [pointer.node for pointer in popular_project_pointers]
-
-        popular_registration_pointers = Node.find_one(Q('_id', 'eq', settings.POPULAR_LINKS_NODE_REGISTRATIONS)).nodes_pointer
-        popular_registrations = [pointer.node for pointer in popular_registration_pointers]
-
-        return popular_projects, popular_registrations
-
-
-def format_node_data(nodes, views, node_pageviews):
-    max_popular_projects = 20
-
-    node_data = [{'node': x['node.id'], 'views': x['result']} for x in node_pageviews[0:max_popular_projects]]
-
-    for node_visit in node_visits[0:max_popular_projects]:
-        for node_result in node_data:
-            if node_visit['node.id'] == node_result['node']:
-                node_result.update({'visits': node_visit['result']})
-
-    node_data.sort(key=lambda datum: datum['views'], reverse=True)
-
-    return node_data
-
-
-def get_popular_projects_and_registrations(node_data):
-    popular_public_projects = []
-    popular_public_registrations = []
-
-    for nid in node_data:
-        node = Node.load(nid['node'])
-        if node is None:
-            continue
-        if node.is_public and not node.is_registration and not node.is_deleted:
-            if len(popular_public_projects) < 10:
-                popular_public_projects.append(node)
-        elif node.is_public and node.is_registration and not node.is_deleted and not node.is_retracted:
-            if len(popular_public_registrations) < 10:
-                popular_public_registrations.append(node)
-        if len(popular_public_projects) >= 10 and len(popular_public_registrations) >= 10:
-            break
-
-    return popular_public_projects, popular_public_registrations
-
-
-def hits(node_data):
-    return {
-        datum['node']: {
-            'hits': datum['views'],
-            'visits': datum['visits']
-        } for datum in node_data
-    }
+    return {'node_pageviews': node_pageviews, 'node_visits': node_visits}
 
 
 def activity():
-    node_data = get_node_data()
-    popular_public_projects, popular_public_registrations = get_popular_projects_and_registrations(node_data)
-    project_hits = hits(node_data)
+    """Generate analytics for most popular public projects and registrations.
+    Called by `scripts/update_populate_projects_and_registrations`
+    """
+    popular_public_projects = []
+    popular_public_registrations = []
+    max_popular_projects = 20
 
-    # Projects
+    if settings.KEEN['public']['read_key']:
+        keen_activity = get_keen_activity()
+        node_pageviews = keen_activity['node_pageviews']
+        node_visits = keen_activity['node_visits']
+
+        node_data = [{'node': x['node.id'], 'views': x['result']} for x in node_pageviews[0:max_popular_projects]]
+
+        # Even though view counts won't be used, still gather this data for sorting
+        for node_visit in node_visits[0:max_popular_projects]:
+            for node_result in node_data:
+                if node_visit['node.id'] == node_result['node']:
+                    node_result.update({'visits': node_visit['result']})
+
+        node_data.sort(key=lambda datum: datum['views'], reverse=True)
+
+        node_data = [node_dict['node'] for node_dict in node_data]
+
+        for nid in node_data:
+            node = Node.load(nid)
+            if node is None:
+                continue
+            if node.is_public and not node.is_registration and not node.is_deleted:
+                if len(popular_public_projects) < 10:
+                    popular_public_projects.append(node)
+            elif node.is_public and node.is_registration and not node.is_deleted and not node.is_retracted:
+                if len(popular_public_registrations) < 10:
+                    popular_public_registrations.append(node)
+            if len(popular_public_projects) >= 10 and len(popular_public_registrations) >= 10:
+                break
+
+    # New and Noteworthy projects are updated manually
     new_and_noteworthy_pointers = Node.find_one(Q('_id', 'eq', settings.NEW_AND_NOTEWORTHY_LINKS_NODE)).nodes_pointer
     new_and_noteworthy_projects = [pointer.node for pointer in new_and_noteworthy_pointers]
 
@@ -157,6 +133,5 @@ def activity():
         'new_and_noteworthy_projects': new_and_noteworthy_projects,
         'recent_public_registrations': recent_public_registrations(),
         'popular_public_projects': popular_public_projects,
-        'popular_public_registrations': popular_public_registrations,
-        'hits': project_hits,
+        'popular_public_registrations': popular_public_registrations
     }
