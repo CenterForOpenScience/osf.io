5e2300bc473cf640070e8007e035edc8206b5a73
erinspace <erin.braswell@gmail.com>
2016-10-11 12:18:40 -0400

    Simplify script a bit y just passing nodes

--- scripts/populate_popular_projects_and_registrations.py
+++ scripts/populate_popular_projects_and_registrations.py
@@ -15,19 +15,8 @@ from website.settings import POPULAR_LINKS_NODE, POPULAR_LINKS_NODE_REGISTRATION
 
 logger = logging.getLogger(__name__)
 
-def popular_activity_json():
-    """ Return popular_public_projects node_ids """
 
-    activity_json = activity()
-    popular_projects = activity_json['popular_public_projects']
-    popular_registrations = activity_json['popular_public_registrations']
-
-    return {
-        'popular_public_projects': [proj._id for proj in popular_projects],
-        'popular_public_registrations': [reg._id for reg in popular_registrations]
-    }
-
-def update_node_links(designated_node, target_node_ids, description):
+def update_node_links(designated_node, target_nodes, description):
     """ Takes designated node, removes current node links and replaces them with node links to target nodes """
     logger.info('Repopulating {} with latest {} nodes.'.format(designated_node._id, description))
     user = designated_node.creator
@@ -36,24 +25,22 @@ def update_node_links(designated_node, target_node_ids, description):
     for pointer in designated_node.nodes_pointer:
         designated_node.rm_pointer(pointer, auth)
 
-    for n_id in target_node_ids:
-        n = models.Node.load(n_id)
-        if is_eligible_node(n):
-            designated_node.add_pointer(n, auth, save=True)
-            logger.info('Added node link {} to {}'.format(n, designated_node))
+    for node in target_nodes:
+        designated_node.add_pointer(node, auth, save=True)
+        logger.info('Added node link {} to {}'.format(node, designated_node))
 
 def main(dry_run=True):
     init_app(routes=False)
-    popular_activity = popular_activity_json()
 
-    popular_node_ids = popular_activity['popular_node_ids']
+    popular_activity = activity()
+
+    popular_nodes = popular_activity['popular_public_projects']
     popular_links_node = models.Node.find_one(Q('_id', 'eq', POPULAR_LINKS_NODE))
-    popular_registration_ids = popular_activity_json()['popular_registration_ids']
+    popular_registrations = popular_activity['popular_public_registrations']
     popular_links_node_registrations = models.Node.find_one(Q('_id', 'eq', POPULAR_LINKS_NODE_REGISTRATIONS))
 
-    update_node_links(popular_links_node, popular_node_ids, 'popular')
-    update_node_links(popular_links_node_registrations, popular_registration_ids, 'popular registrations')
-
+    update_node_links(popular_links_node, popular_nodes, 'popular')
+    update_node_links(popular_links_node_registrations, popular_registrations, 'popular registrations')
     try:
         popular_links_node.save()
         logger.info('Node links on {} updated.'.format(popular_links_node._id))
