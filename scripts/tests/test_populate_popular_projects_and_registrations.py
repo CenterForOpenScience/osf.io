096a0ee6707b551d438b288e0d04599e51094ccf
erinspace <erin.braswell@gmail.com>
2016-10-11 12:19:19 -0400

    Correctly mock keen call and be more specific with node tests for ids

--- scripts/tests/test_populate_popular_projects_and_registrations.py
+++ scripts/tests/test_populate_popular_projects_and_registrations.py
@@ -1,12 +1,12 @@
 from nose.tools import *  # noqa
 
-import json
+import mock
 
 from tests.base import OsfTestCase
-from tests.factories import ProjectFactory, UserFactory, RegistrationFactory
+from tests.factories import ProjectFactory, RegistrationFactory
 
 from website.project.model import Node
-from website.settings import POPULAR_LINKS_NODE, POPULAR_LINKS_NODE_REGISTRATIONS
+from website.settings import POPULAR_LINKS_NODE, POPULAR_LINKS_NODE_REGISTRATIONS, NEW_AND_NOTEWORTHY_LINKS_NODE
 
 from scripts import populate_popular_projects_and_registrations as script
 
@@ -15,66 +15,93 @@ class TestPopulateNewAndNoteworthy(OsfTestCase):
 
     def setUp(self):
         super(TestPopulateNewAndNoteworthy, self).setUp()
-        self.pop1 = ProjectFactory()
-        self.pop2 = ProjectFactory()
-        self.pop3 = ProjectFactory()
-        self.pop4 = ProjectFactory()
-        self.pop5 = ProjectFactory()
+        self.pop1 = ProjectFactory(is_public=True)
+        self.pop2 = ProjectFactory(is_public=True)
 
-        self.popreg1 = RegistrationFactory()
-        self.popreg2 = RegistrationFactory()
-        self.popreg3 = RegistrationFactory()
-        self.popreg4 = RegistrationFactory()
-        self.popreg5 = RegistrationFactory()
-
-        self.user = UserFactory()
-
-        popular_json = {"popular_node_ids": [self.pop1._id, self.pop2._id, self.pop3._id, self.pop4._id, self.pop5._id]}
-        self.popular_json_body = json.dumps(popular_json)
+        self.popreg1 = RegistrationFactory(is_public=True)
+        self.popreg2 = RegistrationFactory(is_public=True)
 
     def tearDown(self):
         super(TestPopulateNewAndNoteworthy, self).tearDown()
         Node.remove()
 
-    def test_populate_popular_nodes_and_registrations(self):
-        self.popular_links_node = ProjectFactory(creator=self.user)
+    @mock.patch('website.project.utils.get_keen_activity')
+    def test_populate_popular_nodes_and_registrations(self, mock_client):
+
+        # only for setup, not used
+        self.new_noteworthy_node = ProjectFactory()
+        self.new_noteworthy_node._id = NEW_AND_NOTEWORTHY_LINKS_NODE
+        self.new_noteworthy_node.save()
+
+        self.popular_links_node = ProjectFactory()
         self.popular_links_node._id = POPULAR_LINKS_NODE
         self.popular_links_node.save()
 
-        self.popular_links_node_registrations = ProjectFactory(creator=self.user)
+        self.popular_links_node_registrations = ProjectFactory()
         self.popular_links_node_registrations._id = POPULAR_LINKS_NODE_REGISTRATIONS
         self.popular_links_node_registrations.save()
 
-        popular_nodes = [self.pop1, self.pop2, self.pop3, self.pop4, self.pop5]
-        popular_node_registrations = [self.popreg1, self.popreg2, self.popreg3, self.popreg4, self.popreg5]
+        popular_nodes = [self.pop1, self.pop2]
+        popular_node_registrations = [self.popreg1, self.popreg2]
 
-        # TODO - make nodes and registrations popular!
+        # mock_client.count.return_value = {
 
-        # for node in popular_nodes:
-        #     self.popular_links_node.add_pointer(node, auth=Auth(self.user), save=True)
-        #
-        # for reg in popular_node_registrations:
-        #     self.popular_links_node_registrations.add_pointer(reg, auth=Auth(self.user), save=True)
+        node_pageviews = [
+            {
+                'result': 5,
+                'node.id': self.pop1._id
+            },
+            {
+                'result': 5,
+                'node.id': self.pop2._id
+            },
+            {
+                'result': 5,
+                'node.id': self.popreg1._id
+            },
+            {
+                'result': 5,
+                'node.id': self.popreg2._id
+            }
+        ]
+
+        node_visits = [
+            {
+                'result': 2,
+                'node.id': self.pop1._id
+            },
+            {
+                'result': 2,
+                'node.id': self.pop2._id
+            },
+            {
+                'result': 2,
+                'node.id': self.popreg1._id
+            },
+            {
+                'result': 2,
+                'node.id': self.popreg2._id
+            }
+        ]
+
+        mock_client.return_value = {'node_pageviews': node_pageviews, 'node_visits': node_visits}
 
         assert_equal(len(self.popular_links_node.nodes), 0)
         assert_equal(len(self.popular_links_node_registrations.nodes), 0)
 
         script.main(dry_run=False)
-        self.popular_links_node.reload()
-        self.popular_links_node_registrations.reload()
-
-        assert_equal(len(self.popular_links_node.nodes), 5)  # verifies remove pointer is working
-        assert_equal(len(self.popular_links_node_registrations.nodes), 5)
 
         self.popular_links_node.reload()
         self.popular_links_node_registrations.reload()
-        self.new_and_noteworthy_links_node.reload()
 
-        popular_node_links = [pointer.node._id for pointer in self.popular_links_node.nodes]
-        popular_node_registration_links = [pointer.node._id for pointer in self.popular_links_node_registrations.nodes]
-        assert_equal(popular_node_links, [])
-        assert_equal(popular_node_registration_links, [])
+        assert_equal(len(self.popular_links_node.nodes), 2)
+        assert_equal(len(self.popular_links_node_registrations.nodes), 2)
 
-        new_and_noteworthy_node_links = {pointer.node._id for pointer in self.new_and_noteworthy_links_node.nodes}
-
-        assert_equal(set(new_and_noteworthy_node_links), {self.nn1._id, self.nn2._id, self.nn3._id, self.nn4._id, self.nn5._id})
+        assert_items_equal(
+            popular_nodes,
+            [pointer.node for pointer in self.popular_links_node.nodes]
+        )
+        assert_items_equal(
+            popular_node_registrations,
+            [pointer.node for pointer in self.popular_links_node_registrations.nodes]
+        )
