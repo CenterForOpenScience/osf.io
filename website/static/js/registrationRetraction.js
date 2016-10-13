bbea7a10ed57e4f539f1b42d90b188083f08983e
Alex Schiller <alexschiller@gmail.com>
2016-10-13 17:16:56 -0400

    Use $osf confirmationString instead of truncatedTitle

--- website/static/js/registrationRetraction.js
+++ website/static/js/registrationRetraction.js
@@ -18,7 +18,7 @@ var Raven = require('raven-js');
 var RegistrationRetractionViewModel = oop.extend(
     ChangeMessageMixin,
     {
-        constructor: function(submitUrl, registrationTitle) {
+        constructor: function(submitUrl) {
             this.super.constructor.call(this);
 
             ko.validation.registerExtenders();
@@ -26,24 +26,14 @@ var RegistrationRetractionViewModel = oop.extend(
             var self = this;
 
             self.submitUrl = submitUrl;
-            // Strips smart quotes and replaces them with typeable quotes
-            self.registrationTitle = $osf.htmlDecode(registrationTitle).replace(/[\u2018\u2019]/g, '\'').replace(/[\u201C\u201D]/g, '"');
 
-            // Truncate title to around 50 chars
-            var parts = self.registrationTitle.slice(0, 50).split(' ');
-            if (parts.length > 1) {
-                self.truncatedTitle = parts.slice(0, -1).join(' ');
-            }
-            else {
-                self.truncatedTitle = parts[0];
-            }
-
+            self.confirmationString = $osf.getConfirmationString();
             self.justification = ko.observable('').extend({
                 maxLength: 2048
             });
             self.confirmationText = ko.observable().extend({
                 required: true,
-                mustEqual: self.truncatedTitle
+                mustEqual: self.confirmationString
             });
             self.disableSave = ko.observable(false);
             self.valid = ko.computed(function(){
@@ -90,8 +80,8 @@ var RegistrationRetractionViewModel = oop.extend(
         }
 });
 
-var RegistrationRetraction = function(selector, submitUrl, registrationTitle) {
-    this.viewModel = new RegistrationRetractionViewModel(submitUrl, registrationTitle);
+var RegistrationRetraction = function(selector, submitUrl) {
+    this.viewModel = new RegistrationRetractionViewModel(submitUrl);
     $osf.applyBindings(this.viewModel, selector);
 };
 
