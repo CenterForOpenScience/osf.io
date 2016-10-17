70a6261bf166b9b0188f4ef70e08bf12a343d5b6
erinspace <erin.braswell@gmail.com>
2016-10-17 14:36:04 -0400

    Woo only one Django query and switch the other ODM query too

--- website/prereg/utils.py
+++ website/prereg/utils.py
@@ -9,14 +9,13 @@ PREREG_CAMPAIGNS = {
 def drafts_for_user(user, campaign):
     from website import models  # noqa
 
-    all_user_projects = models.Node.objects.filter(is_deleted=False, _contributors=user)
-    user_projects = [node for node in all_user_projects if node.has_permission(user, 'admin')]
+    user_projects = models.Node.objects.filter(is_deleted=False, contributor__admin=True, contributor__user=user)
     PREREG_CHALLENGE_METASCHEMA = get_prereg_schema(campaign)
-    return models.DraftRegistration.find(
-        Q('registration_schema', 'eq', PREREG_CHALLENGE_METASCHEMA) &
-        Q('approval', 'eq', None) &
-        Q('registered_node', 'eq', None) &
-        Q('branched_from', 'in', [p._id for p in user_projects])
+    return models.DraftRegistration.objects.filter(
+        registration_schema=PREREG_CHALLENGE_METASCHEMA,
+        approval=None,
+        registered_node=None,
+        branched_from__in=[p.id for p in user_projects]
     )
 
 def get_prereg_schema(campaign='prereg'):
