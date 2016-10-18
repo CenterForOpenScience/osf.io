e9f7362e47eeeb7433f905404da28639d5163384
Henrique Moco <hmoco94@vt.edu>
2016-10-18 13:45:59 -0400

    fix filter logic

--- api/preprint_providers/views.py
+++ api/preprint_providers/views.py
@@ -200,7 +200,7 @@ class PreprintProviderSubjectList(JSONAPIBaseView, generics.ListAPIView):
     serializer_class = TaxonomySerializer
 
     def is_valid_subject(self, allows_children, allowed_parents, sub):
-        if sub in allowed_parents:
+        if sub._id in allowed_parents:
             return True
         for parent in sub.parents:
             if parent._id in allows_children:
@@ -211,14 +211,14 @@ class PreprintProviderSubjectList(JSONAPIBaseView, generics.ListAPIView):
         return False
 
     def get_queryset(self):
-        parent = self.request.query_params.get('filter[parent]', None)
+        parent = self.request.query_params.get('filter[parents]', None)
         provider = PreprintProvider.load(self.kwargs['provider_id'])
         if parent:
-            if parent[0] == 'null':
+            if parent == 'null':
                 return provider.top_level_subjects
             #  Calculate this here to only have to do it once.
-            allowed_parents = [id_ for sublist in provider.acceptable_subjects for id_ in sublist[0]]
-            allows_children = [subs[0][-1] for subs in provider.acceptable_subjects if subs[1]]
-            return (sub for sub in Subject.find('parents', 'eq', parent[0]) if self.is_valid_subject(allows_children=allows_children, allowed_parents=allowed_parents, sub=sub))
+            allowed_parents = [id_ for sublist in provider.subjects_acceptable for id_ in sublist[0]]
+            allows_children = [subs[0][-1] for subs in provider.subjects_acceptable if subs[1]]
+            return [sub for sub in Subject.find(Q('parents', 'eq', parent)) if provider.subjects_acceptable == [] or self.is_valid_subject(allows_children=allows_children, allowed_parents=allowed_parents, sub=sub)]
         return provider.all_subjects
 
