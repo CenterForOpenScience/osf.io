7643635278fc1c92289e8fdd456614ce85a2c2f3
Steven Loria <sloria1@gmail.com>
2016-10-20 16:26:20 -0400

    Add required overrides to osfstorage.NodeSettings

--- addons/osfstorage/models.py
+++ addons/osfstorage/models.py
@@ -5,4 +5,6 @@ from addons.base.models import BaseNodeSettings, BaseStorageAddon
 logger = logging.getLogger(__name__)
 
 class NodeSettings(BaseStorageAddon, BaseNodeSettings):
-    pass
+    # Required overrides
+    complete = True
+    has_auth = True
