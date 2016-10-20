c87f75334aa7253ad209bdd4d88c2429723e51d1
erinspace <erin.braswell@gmail.com>
2016-10-20 13:01:05 -0400

    Remove unused bare desk URL

--- admin/desk/urls.py
+++ admin/desk/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import url
 from admin.desk import views
 
 urlpatterns = [
-    url(r'^$', views.DeskCaseList.as_view(), name='cases'),
     url(r'^customer/(?P<user_id>[a-z0-9]+)/$', views.DeskCustomer.as_view(),
         name='customer'),
     url(r'^cases/(?P<user_id>[a-z0-9]+)/$', views.DeskCaseList.as_view(),
