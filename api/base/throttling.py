f868a80fea911da392c9278619e1894e179fc46a
Casey Rollins <cmr3rm@virginia.edu>
2016-10-25 14:33:31 -0400

    Add ability to avoid throttle via request header token.

--- api/base/throttling.py
+++ api/base/throttling.py
@@ -1,4 +1,7 @@
+from rest_framework.settings import api_settings as drf_settings
 from rest_framework.throttling import UserRateThrottle, AnonRateThrottle, SimpleRateThrottle
+
+from api.base import settings
 
 
 class BaseThrottle(SimpleRateThrottle):
@@ -6,10 +9,32 @@ class BaseThrottle(SimpleRateThrottle):
     def failure(self, request):
         return False
 
+    def get_ident(self, request):
+        xff = request.META.get('HTTP_X_FORWARDED_FOR')
+        remote_addr = request.META.get('REMOTE_ADDR')
+        throttle_token = request.META.get('HTTP_X_THROTTLE_TOKEN')
+        num_proxies = drf_settings.NUM_PROXIES
+
+        if throttle_token:
+            return throttle_token
+
+        if num_proxies is not None:
+            if num_proxies == 0 or xff is None:
+                return remote_addr
+            addrs = xff.split(',')
+            client_addr = addrs[-min(num_proxies, len(addrs))]
+            return client_addr.strip()
+
+        return ''.join(xff.split()) if xff else remote_addr
+
     def allow_request(self, request, view):
         """
         Implement the check to see if the request should be throttled.
         """
+        throttle_token = settings.BYPASS_THROTTLE_TOKEN
+        if throttle_token and self.get_ident(request) == throttle_token:
+            return True
+
         if self.rate is None:
             return True
 
@@ -46,6 +71,7 @@ class AddContributorThrottle(BaseThrottle, UserRateThrottle):
             return False
         return True
 
+
 class CreateGuidThrottle(BaseThrottle, UserRateThrottle):
 
     scope = 'create-guid'
@@ -61,11 +87,11 @@ class RootAnonThrottle(AnonRateThrottle):
     scope = 'root-anon-throttle'
 
 
-class TestUserRateThrottle(UserRateThrottle):
+class TestUserRateThrottle(BaseThrottle, UserRateThrottle):
 
     scope = 'test-user'
 
 
-class TestAnonRateThrottle(AnonRateThrottle):
+class TestAnonRateThrottle(BaseThrottle, AnonRateThrottle):
 
     scope = 'test-anon'
