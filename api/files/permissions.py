28c95f409dd94db0a9f6b2517397a05757b6b1fa
Henrique Moco <hmoco94@vt.edu>
2016-10-25 17:00:26 -0400

    add preprintfile check for changes to file

--- api/files/permissions.py
+++ api/files/permissions.py
@@ -1,7 +1,9 @@
+from modularodm import Q
 from rest_framework import permissions
 
 from api.base.utils import get_user_auth
 from website.files.models import FileNode
+from website.models import Node
 
 class CheckedOutOrAdmin(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
@@ -17,3 +19,16 @@ class CheckedOutOrAdmin(permissions.BasePermission):
         return obj.checkout is None \
             or obj.checkout == auth.user \
             or obj.node.has_permission(auth.user, 'admin')
+
+
+class IsPreprintFile(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        assert isinstance(obj, FileNode), 'obj must be a FileNode, got {}'.format(obj)
+
+        if request.method not in permissions.SAFE_METHODS and len(Node.find(Q('preprint_file', 'eq', obj))):
+            return False
+
+        return True
+
+        
+
