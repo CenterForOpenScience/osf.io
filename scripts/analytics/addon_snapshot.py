6c9bfea403bd4dcca0f68603fd1e566cafc19cdd
erinspace <erin.braswell@gmail.com>
2016-10-26 11:25:39 -0400

    Add deleted and disconnected account info to counts

--- scripts/analytics/addon_snapshot.py
+++ scripts/analytics/addon_snapshot.py
@@ -1,4 +1,5 @@
 import logging
+from modularodm import Q
 
 from website.settings import ADDONS_AVAILABLE
 from website.app import init_app
@@ -15,10 +16,22 @@ def count():
     for short_name, addon in addons_available.iteritems():
         user_count = addon.settings_models['user'].find().count() if addon.settings_models.get('user') else 0
         node_count = addon.settings_models['node'].find().count() if addon.settings_models.get('node') else 0
+        deleted_count = addon.settings_models['node'].find(Q('deleted', 'eq', True)).count() if addon.settings_models.get('node') else 0
+        disconnected_count = addon.settings_models['node'].find(Q('external_account', 'eq', None) & Q('deleted', 'ne', True)).count() if addon.settings_models.get('node') else 0
+
         counts.append({
-            'provider': short_name,
-            'user_count': user_count,
-            'node_count': node_count
+            'provider': {
+                'name': short_name
+            },
+            'users': {
+                'total': user_count
+            },
+            'nodes': {
+                'total': node_count,
+                'connected': node_count,
+                'deleted': deleted_count,
+                'disconnected': disconnected_count
+            }
         })
 
         logger.info('{} counted. Users: {}, Nodes: {}'.format(addon.short_name, user_count, node_count))
