8f9caa175b6747542901671f85008527053e8f62
erinspace <erin.braswell@gmail.com>
2016-10-27 09:35:55 -0400

    Add basis to also pass list of script names to run

--- scripts/analytics/run_keen_summaries.py
+++ scripts/analytics/run_keen_summaries.py
@@ -8,24 +8,38 @@ from keen.client import KeenClient
 
 from website.app import init_app
 from website.settings import KEEN as keen_settings
-from scripts.analytics.user_count import count as user_count
-from scripts.analytics.node_count import count as node_count
-from scripts.analytics.node_log_count import get_node_log_events as node_log_count
+from scripts.analytics.user_summary import get_events as user_events
+from scripts.analytics.node_summary import get_events as node_events
+from scripts.analytics.node_log_count import get_events as node_log_events
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 
-def get_events_for_day(day):
+def get_events_for_day(day, scripts=None):
     """Get an event summary for a certain 24 hour period.
      00:00UTC - 23:59:59UTC on the day provided.
+
+     Default to all known analytic summary scripts, but optionally
+     pass in the names of specific scripts to run.
      """
     logger.info('<---- Keen Counts for {} ---->'.format(day.isoformat()))
 
     keen_events = {}
-    keen_events.update({'user_count_analytics': user_count(day)})
-    keen_events.update({'node_analytics': [node_count(day)]})
-    keen_events.update({'node_log_analytics': [node_log_count(day)]})
+
+    if scripts:
+        # try to imprt the scripts given the string name
+
+        # raise a helpful error if the script named can't be found
+
+        # assume that script has a "get_events" method and use that to add to keen_events
+
+        # use the name of the script file as the assumed collection name
+        pass
+    else:
+        keen_events.update({'user_summary': user_events(day)})
+        keen_events.update({'node_summary': [node_events(day)]})
+        keen_events.update({'node_log_events': [node_log_events(day)]})
 
     return keen_events
 
@@ -38,7 +52,7 @@ def yield_chunked_events(events):
         yield events[i:i + 5000]
 
 
-def main(start_date, end_date):
+def main(start_date, end_date, scripts=None):
     keen_project = keen_settings['private']['project_id']
     write_key = keen_settings['private']['write_key']
     if keen_project and write_key:
@@ -50,7 +64,7 @@ def main(start_date, end_date):
 
     day = start_date
     while day < end_date:
-        events_for_day = get_events_for_day(day)
+        events_for_day = get_events_for_day(day, scripts)
         for key, value in events_for_day.iteritems():
             for chunk in yield_chunked_events(value):
                 client.add_events({key: chunk})
@@ -61,6 +75,7 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Populate keen counts!')
     parser.add_argument('-e', '--end', dest='end_date', required=False)
     parser.add_argument('-s', '--start', dest='start_date', required=False)
+    parser.add_argument('-as', '--analytics_scripts', nargs='+', dest='analytics_scripts', required=False)
     return parser.parse_args()
 
 if __name__ == '__main__':
@@ -70,5 +85,6 @@ if __name__ == '__main__':
     args = parse_args()
     end_date = parse(args.end_date) if args.end_date else now
     start_date = parse(args.start_date) if args.end_date else now - datetime.timedelta(1)
+    scripts = args.analytics_scripts
 
-    main(start_date, end_date)
+    main(start_date, end_date, scripts)
