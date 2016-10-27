6f3c39c59e3078f4e3f80ea9acea6950ed7e4072
Chris Wisecarver <cwisecarver@cos.io>
2016-10-27 17:40:35 -0400

    Add sql and test for node root

--- osf_tests/test_node.py
+++ osf_tests/test_node.py
@@ -67,18 +67,49 @@ def test_component_has_parent_node():
     assert node.parent_node == project
 
 def test_components_have_root():
+    import ipdb; ipdb.set_trace()
     root = ProjectFactory()
     child = NodeFactory(parent=root)
+    child1 = NodeFactory(parent=root)
+    child2 = NodeFactory(parent=root)
     grandchild = NodeFactory(parent=child)
+    grandchild1 = NodeFactory(parent=child)
+    grandchild2 = NodeFactory(parent=child)
+    grandchild3 = NodeFactory(parent=child)
+    grandchild_1 = NodeFactory(parent=child1)
+    grandchild1_1 = NodeFactory(parent=child1)
+    grandchild2_1 = NodeFactory(parent=child1)
+    grandchild3_1 = NodeFactory(parent=child1)
+    grandchild_2 = NodeFactory(parent=child2)
+    grandchild1_2 = NodeFactory(parent=child2)
+    grandchild2_2 = NodeFactory(parent=child2)
+    grandchild3_2 = NodeFactory(parent=child2)
     greatgrandchild = NodeFactory(parent=grandchild)
-    child.reload()
-    grandchild.reload()
-    assert child.root == root
-    assert grandchild.root == root
+    greatgrandchild1 = NodeFactory(parent=grandchild1)
+    greatgrandchild2 = NodeFactory(parent=grandchild2)
+    greatgrandchild3 = NodeFactory(parent=grandchild3)
+    greatgrandchild_1 = NodeFactory(parent=grandchild_1)
 
-    # assert child.get_root() == root
-    # assert grandchild.get_root() == rooo
-    assert greatgrandchild.get_root() == root
+    assert child.root == root
+    assert child1.root == root
+    assert child2.root == root
+    assert grandchild.root == root
+    assert grandchild1.root == root
+    assert grandchild2.root == root
+    assert grandchild3.root == root
+    assert grandchild_1.root == root
+    assert grandchild1_1.root == root
+    assert grandchild2_1.root == root
+    assert grandchild3_1.root == root
+    assert grandchild_2.root == root
+    assert grandchild1_2.root == root
+    assert grandchild2_2.root == root
+    assert grandchild3_2.root == root
+    assert greatgrandchild.root == root
+    assert greatgrandchild1.root == root
+    assert greatgrandchild2.root == root
+    assert greatgrandchild3.root == root
+    assert greatgrandchild_1.root == root
 
 def test_license_searches_parent_nodes():
     license_record = NodeLicenseRecordFactory()
