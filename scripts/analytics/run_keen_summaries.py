b39648cf8e0d9ec95c57a7e1612c9f3085aff751
erinspace <erin.braswell@gmail.com>
2016-10-28 11:32:11 -0400

    add ability to pass scripts to run via argument

--- scripts/analytics/run_keen_summaries.py
+++ scripts/analytics/run_keen_summaries.py
@@ -1,6 +1,7 @@
 import time
 import logging
 import argparse
+import importlib
 from dateutil.parser import parse
 from datetime import datetime, timedelta
 
@@ -20,26 +21,24 @@ def get_events_for_day(day, scripts=None):
     """Get an event summary for a certain 24 hour period.
      00:00UTC - 23:59:59UTC on the day provided.
 
-     Default to all known analytic summary scripts, but optionally
-     pass in the names of specific scripts to run.
+     Default to explicitly imported and run analytic summary scripts,
+     but optionally pass in the names of specific scripts to run.
      """
     logger.info('<---- Keen Counts for {} ---->'.format(day.isoformat()))
 
     keen_events = {}
-
     if scripts:
-        # try to imprt the scripts given the string name
-
-        # raise a helpful error if the script named can't be found
-
-        # assume that script has a "get_events" method and use that to add to keen_events
-
-        # use the name of the script file as the assumed collection name
-        pass
+        for script in scripts:
+            try:
+                script_events = importlib.import_module('scripts.analytics.{}'.format(script))
+                keen_events.update({script: script_events.get_events(day)})
+            except ImportError as e:
+                logger.error(e)
+                logger.error('Error importing script - make sure the script specified is inside of scripts/analytics')
     else:
         keen_events.update({'user_summary': user_events(day)})
-        keen_events.update({'node_summary': [node_events(day)]})
-        keen_events.update({'node_log_events': [node_log_events(day)]})
+        keen_events.update({'node_summary': node_events(day)})
+        keen_events.update({'node_log_events': node_log_events(day)})
 
     return keen_events
 
@@ -75,7 +74,10 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Populate keen counts!')
     parser.add_argument('-e', '--end', dest='end_date', required=False)
     parser.add_argument('-s', '--start', dest='start_date', required=False)
-    parser.add_argument('-as', '--analytics_scripts', nargs='+', dest='analytics_scripts', required=False)
+    parser.add_argument(
+        '-as', '--analytics_scripts', nargs='+', dest='analytics_scripts', required=False,
+        help='Enter the names of scripts inside scripts/analytics you would like to run separated by spaces (ex: -as user_summary node_summary)'
+    )
     return parser.parse_args()
 
 if __name__ == '__main__':
@@ -84,7 +86,7 @@ if __name__ == '__main__':
     init_app()
     args = parse_args()
     end_date = parse(args.end_date) if args.end_date else now
-    start_date = parse(args.start_date) if args.end_date else now - datetime.timedelta(1)
+    start_date = parse(args.start_date) if args.end_date else now - timedelta(1)
     scripts = args.analytics_scripts
 
     main(start_date, end_date, scripts)
