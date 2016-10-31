b0d46f91b74422e05998a4b1beb012edbfb094bb
Steven Loria <sloria1@gmail.com>
2016-10-31 15:56:33 -0400

    Remove expensive node query from index view
    To eliminate possible locking issue

--- website/views.py
+++ website/views.py
@@ -21,7 +21,6 @@ from website.institutions.views import view_institution
 from website.models import Guid
 from website.models import Node, Institution
 from website.project import new_bookmark_collection
-from website.settings import INSTITUTION_DISPLAY_NODE_THRESHOLD
 from website.util import permissions
 
 logger = logging.getLogger(__name__)
@@ -87,19 +86,10 @@ def index():
     except NoResultsFound:
         pass
 
-    all_institutions = [inst for inst in Institution.find().sort('name')]
+    all_institutions = Institution.find().sort('name')
     dashboard_institutions = [
         {'id': inst._id, 'name': inst.name, 'logo_path': inst.logo_path_rounded_corners}
         for inst in all_institutions
-        if len(
-            Node.find_by_institutions(inst, query=(
-                Q('is_public', 'eq', True) &
-                Q('is_folder', 'ne', True) &
-                Q('is_deleted', 'ne', True) &
-                Q('parent_node', 'eq', None) &
-                Q('is_registration', 'eq', False)
-            ))
-        ) >= INSTITUTION_DISPLAY_NODE_THRESHOLD
     ]
 
     return {
