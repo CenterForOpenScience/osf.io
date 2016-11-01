b6485fc726e5fa46406493d278b70dd794351d6c
Casey Rollins <cmr3rm@virginia.edu>
2016-11-01 10:29:28 -0400

    Add script to set institution affiliated nodes count.

---
+++ scripts/institution_num_nodes.py
@@ -0,0 +1,63 @@
+import sys
+import logging
+
+from modularodm import Q
+
+from framework.transactions.context import TokuTransaction
+from scripts import utils as script_utils
+from website.app import init_app
+from website.institutions.model import Institution
+from website.project.model import Node
+
+
+logger = logging.getLogger(__name__)
+
+
+def set_institution_affiliated_nodes_count():
+    all_institutions = Institution.find()
+    for inst in all_institutions:
+        logger.info(
+            'Finding affiliated nodes for Institution with id <{}> and title <{}>'.format(inst._id, inst.name)
+        )
+        affiliated_nodes = Node.find_by_institutions(
+            inst,
+            query=(
+                Q('is_public', 'eq', True) &
+                Q('is_folder', 'ne', True) &
+                Q('is_deleted', 'ne', True) &
+                Q('parent_node', 'eq', None) &
+                Q('is_registration', 'eq', False)
+            )
+        )
+        logger.info(
+            'Found {} affiliated nodes for Institution with id <{}> and title <{}>'.format(
+                len(affiliated_nodes), inst._id, inst.name)
+        )
+        inst.node.institution_num_nodes = len(affiliated_nodes)
+        inst.node.save()
+
+
+def main(dry=True):
+    init_app(set_backends=True, routes=False)
+
+    with TokuTransaction():
+        set_institution_affiliated_nodes_count()
+        if dry:
+            raise Exception('Abort Transaction - Dry Run')
+
+
+if __name__ == '__main__':
+    dry = '--dry' in sys.argv
+    if not dry:
+        script_utils.add_file_logger(logger, __file__)
+
+    if 'debug' in sys.argv:
+        logger.setLevel(logging.DEBUG)
+    elif 'warning' in sys.argv:
+        logger.setLevel(logging.WARNING)
+    elif 'info' in sys.argv:
+        logger.setLevel(logging.INFO)
+    elif 'error' in sys.argv:
+        logger.setLevel(logging.ERROR)
+
+    main(dry=dry)
