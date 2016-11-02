723cc5dc1d108f3c9fa988df41e18745a6e19bbf
erinspace <erin.braswell@gmail.com>
2016-11-02 09:57:17 -0400

    ADDONS after init app, sane total, correct add events call

--- scripts/analytics/addon_snapshot.py
+++ scripts/analytics/addon_snapshot.py
@@ -1,7 +1,6 @@
 import logging
 from modularodm import Q
 
-from website.settings import ADDONS_AVAILABLE
 from website.app import init_app
 from website.settings import KEEN as keen_settings
 from keen.client import KeenClient
@@ -11,14 +10,15 @@ logging.basicConfig(level=logging.INFO)
 
 
 def get_events():
+    from website.settings import ADDONS_AVAILABLE
     counts = []
     addons_available = {k: v for k, v in [(addon.short_name, addon) for addon in ADDONS_AVAILABLE]}
     for short_name, addon in addons_available.iteritems():
         user_count = addon.settings_models['user'].find().count() if addon.settings_models.get('user') else 0
-        node_count = addon.settings_models['node'].find().count() if addon.settings_models.get('node') else 0
+        connected_count = addon.settings_models['node'].find().count() if addon.settings_models.get('node') else 0
         deleted_count = addon.settings_models['node'].find(Q('deleted', 'eq', True)).count() if addon.settings_models.get('node') else 0
         disconnected_count = addon.settings_models['node'].find(Q('external_account', 'eq', None) & Q('deleted', 'ne', True)).count() if addon.settings_models.get('node') else 0
-
+        total = connected_count + deleted_count + disconnected_count
         counts.append({
             'provider': {
                 'name': short_name
@@ -27,14 +27,14 @@ def get_events():
                 'total': user_count
             },
             'nodes': {
-                'total': node_count,
-                'connected': node_count,
+                'total': total,
+                'connected': connected_count,
                 'deleted': deleted_count,
                 'disconnected': disconnected_count
             }
         })
 
-        logger.info('{} counted. Users: {}, Nodes: {}'.format(addon.short_name, user_count, node_count))
+        logger.info('{} counted. Users: {}, Total Nodes: {}'.format(addon.short_name, user_count, total))
 
     return counts
 
@@ -47,7 +47,7 @@ def main():
             project_id=keen_project,
             write_key=write_key,
         )
-        client.add_event('addon_count_analytics', addon_count)
+        client.add_events({'addon_count_analytics': addon_count})
     else:
         print(addon_count)
 
