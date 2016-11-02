4b2d6f4b145727728cf35f8df0d61f85f47eb463
erinspace <erin.braswell@gmail.com>
2016-11-02 14:40:09 -0400

    Add new EventAnalytics and also analytic_type property

--- scripts/analytics/base.py
+++ scripts/analytics/base.py
@@ -1,5 +1,6 @@
-import argparse
+import time
 import logging
+import argparse
 from datetime import datetime
 
 from website.app import init_app
@@ -17,13 +18,13 @@ class BaseAnalytics(object):
 
     @property
     def collection_name(self):
-        raise NotImplementedError
+        raise NotImplementedError('Must specify a Keen event collection name')
+
+    @property
+    def analytic_type(self):
+        raise NotImplementedError('Must specify the analytic type for logging purposes')
 
     def get_events(self, date):
-        """
-        :param date: the date to run the script, defaults to None. Cannot be day of or in the future
-        :return: list of events to be added to keen
-        """
         pass
 
     def send_events(self, events):
@@ -43,12 +44,22 @@ class BaseAnalytics(object):
 
 class SnapshotAnalytics(BaseAnalytics):
 
+    @property
+    def analytic_type(self):
+        return 'snapshot'
+
     def get_events(self, date=None):
         if date:
             raise AttributeError('Snapshot analytics may not be called with a date.')
 
+        logger.info('Gathering {} analytics for the {} collection'.format(self.analytic_type, self.collection_name))
+
 
 class SummaryAnalytics(BaseAnalytics):
+
+    @property
+    def analytic_type(self):
+        return 'summary'
 
     def get_events(self, date):
         # Date must be specified, must be a date (not a datetime), and must not be today or in the future
@@ -60,11 +71,55 @@ class SummaryAnalytics(BaseAnalytics):
         if type(date) != type(today):
             raise AttributeError('Please call the script using a date object, not a datetime object')
 
-        logger.info('Gathering Summary Analytics for the {} collection up until {}'.format(self.collection_name, date.isoformat()))
+        logger.info('Gathering {} analytics for the {} collection up until {}'.format(
+            self.analytic_type,
+            self.collection_name,
+            date.isoformat()
+        ))
 
     def parse_args(self):
         parser = argparse.ArgumentParser(
-            description='Enter the date to gather summary analytics for the {} collection'.format(self.collection_name))
+            description='Enter the date to gather {} analytics for the {} collection'.format(
+                self.analytic_type,
+                self.collection_name
+            )
+        )
         parser.add_argument('-d', '--date', dest='date', required=False)
 
         return parser.parse_args()
+
+
+class EventAnalytics(SummaryAnalytics):
+
+    @property
+    def analytic_type(self):
+        return 'event'
+
+    def yield_chunked_events(self, events):
+        """ The keen API likes events in chunks no bigger than 5000 -
+        Only yield that many at a time.
+        """
+        for i in range(0, len(events), 5000):
+            yield events[i:i + 5000]
+
+    def send_events(self, events):
+        keen_project = keen_settings['private']['project_id']
+        write_key = keen_settings['private']['write_key']
+        if keen_project and write_key:
+            client = KeenClient(
+                project_id=keen_project,
+                write_key=write_key,
+            )
+            logger.info('Adding {} events to the {} collection'.format(len(events), self.collection_name))
+
+            for chunk in self.yield_chunked_events(events):
+                client.add_events({self.collection_name: chunk})
+                time.sleep(1)
+
+        else:
+            logger.info(
+                'Keen not enabled - would otherwise be adding the following {} events to the {} collection'.format(
+                    len(events), self.collection_name
+                )
+            )
+            print(events)
