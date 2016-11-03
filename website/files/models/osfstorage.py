d7968f6c6d5121c8b1279279b6f43355c1a00e50
Henrique Harman <hmoco@users.noreply.github.com>
2016-11-03 13:38:31 -0400

    Update osfstorage.py

--- website/files/models/osfstorage.py
+++ website/files/models/osfstorage.py
@@ -127,7 +127,6 @@ class OsfStorageFileNode(FileNode):
 
     def move_under(self, destination_parent, name=None):
         if self.node.preprint_file == self and not self.node._has_abandoned_preprint:
-            self.copy_under(destination_parent, name)
             raise exceptions.FileNodeIsPrimaryFile()
         if self.is_checked_out:
             raise exceptions.FileNodeCheckedOutError()
