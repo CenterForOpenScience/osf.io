58f67418b1fa6ab96f27c6edf18604bb33e35046
erinspace <erin.braswell@gmail.com>
2016-11-03 14:16:37 -0400

    Update test node log events script to get right date

--- scripts/tests/test_node_log_events.py
+++ scripts/tests/test_node_log_events.py
@@ -1,11 +1,13 @@
+from framework.mongo import database
+
 import datetime
 from framework.auth.core import User
 from tests.base import OsfTestCase
 from tests.factories import UserFactory, NodeLogFactory
 from nose.tools import *  # PEP8 asserts
-from website.project.model import Node
+from website.project.model import Node, NodeLog
 
-from scripts.analytics.node_log_events import get_events
+from scripts.analytics.node_log_events import NodeLogEvents
 
 
 class TestNodeLogAnalytics(OsfTestCase):
@@ -19,18 +21,25 @@ class TestNodeLogAnalytics(OsfTestCase):
         # Two node logs for user one
         self.node_log_node_created = NodeLogFactory(action='node_created', user=self.user_one)
         self.node_log_file_added = NodeLogFactory(action='file_added', user=self.user_one)
-        self.node_log_node_created.reload()
-        self.node_log_file_added.reload()
 
         # Two node logs for user two
         self.node_log_wiki_updated = NodeLogFactory(action='wiki_updated', user=self.user_two)
         self.node_log_project_created = NodeLogFactory(action='project_created', user=self.user_two)
+
+        self.end_date = datetime.datetime.utcnow() - datetime.timedelta(1)
+
+        for node_log in NodeLog.find():
+            node_log.date = self.end_date - datetime.timedelta(0.1)
+            node_log.save()
+            node_log.reload()
+
+        self.results = NodeLogEvents().get_events(self.end_date.date())
+
+        self.node_log_node_created.reload()
+        self.node_log_file_added.reload()
         self.node_log_wiki_updated.reload()
         self.node_log_project_created.reload()
 
-        self.end_date = datetime.datetime.utcnow()
-
-        self.results = get_events(self.end_date)
 
     def tearDown(self):
         super(TestNodeLogAnalytics, self).tearDown()
@@ -40,25 +49,25 @@ class TestNodeLogAnalytics(OsfTestCase):
     def test_results_structure(self):
         expected = [
             {
-                'keen': {'timestamp': self.end_date.isoformat()},
+                'keen': {'timestamp': self.node_log_node_created.date.isoformat()},
                 'date': self.node_log_node_created.date.isoformat(),
                 'action': 'node_created',
                 'user_id': self.user_one._id
             },
             {
-                'keen': {'timestamp': self.end_date.isoformat()},
+                'keen': {'timestamp': self.node_log_file_added.date.isoformat()},
                 'date': self.node_log_file_added.date.isoformat(),
                 'action': 'file_added',
                 'user_id': self.user_one._id
             },
             {
-                'keen': {'timestamp': self.end_date.isoformat()},
+                'keen': {'timestamp': self.node_log_wiki_updated.date.isoformat()},
                 'date': self.node_log_wiki_updated.date.isoformat(),
                 'action': 'wiki_updated',
                 'user_id': self.user_two._id
             },
             {
-                'keen': {'timestamp': self.end_date.isoformat()},
+                'keen': {'timestamp': self.node_log_project_created.date.isoformat()},
                 'date': self.node_log_project_created.date.isoformat(),
                 'action': 'project_created',
                 'user_id': self.user_two._id
