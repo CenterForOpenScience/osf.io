5fb03068113ccdaebb2496f127146617f8931c02
erinspace <erin.braswell@gmail.com>
2016-11-15 15:12:41 -0500

    Add depth users to user summary

--- scripts/analytics/user_summary.py
+++ scripts/analytics/user_summary.py
@@ -6,11 +6,35 @@ from datetime import datetime, timedelta
 from modularodm import Q
 
 from website.app import init_app
-from website.models import User
+from website.models import User, NodeLog
 from scripts.analytics.base import SummaryAnalytics
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
+
+LOG_THRESHOLD = 11
+
+
+# Modified from scripts/analytics/depth_users.py
+def count_user_logs(user):
+    logs = NodeLog.find(Q('user', 'eq', user._id))
+    length = logs.count()
+    if length > 0:
+        item = logs[0]
+        if item.action == 'project_created' and item.node.is_bookmark_collection:
+            length -= 1
+    return length
+
+
+# Modified from scripts/analytics/depth_users.py
+def get_number_of_depth_users(active_users):
+    depth_users = 0
+    for user in active_users:
+        log_count = count_user_logs(user)
+        if log_count >= LOG_THRESHOLD:
+            depth_users += 1
+
+    return depth_users
 
 
 class UserSummary(SummaryAnalytics):
@@ -26,19 +50,24 @@ class UserSummary(SummaryAnalytics):
         timestamp_datetime = datetime(date.year, date.month, date.day).replace(tzinfo=pytz.UTC)
         query_datetime = timestamp_datetime + timedelta(1)
 
-        counts = {
-            'keen': {
-                'timestamp': timestamp_datetime.isoformat()
-            },
-            'status': {
-                'active': User.find(
+        active_users = User.find(
                     Q('is_registered', 'eq', True) &
                     Q('password', 'ne', None) &
                     Q('merged_by', 'eq', None) &
                     Q('date_disabled', 'eq', None) &
                     Q('date_confirmed', 'ne', None) &
                     Q('date_confirmed', 'lt', query_datetime)
-                ).count(),
+                )
+
+        depth_users = get_number_of_depth_users(active_users)
+
+        counts = {
+            'keen': {
+                'timestamp': timestamp_datetime.isoformat()
+            },
+            'status': {
+                'active': active_users.count(),
+                'depth': depth_users,
                 'unconfirmed': User.find(
                     Q('date_registered', 'lt', query_datetime) &
                     Q('date_confirmed', 'eq', None)
@@ -50,8 +79,9 @@ class UserSummary(SummaryAnalytics):
             }
         }
         logger.info(
-            'Users counted. Active: {}, Unconfirmed: {}, Deactivated: {}'.format(
+            'Users counted. Active: {}, Depth: {}, Unconfirmed: {}, Deactivated: {}'.format(
                 counts['status']['active'],
+                counts['status']['depth'],
                 counts['status']['unconfirmed'],
                 counts['status']['deactivated']
             )
