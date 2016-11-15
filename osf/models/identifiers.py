5cb18c100c733d6d5302c5e1c868bbd8cf47110a
Steven Loria <sloria1@gmail.com>
2016-11-15 15:24:07 -0500

    Fix set_identifier_value and port identifier tests

--- osf/models/identifiers.py
+++ osf/models/identifiers.py
@@ -39,7 +39,8 @@ class IdentifierMixin(models.Model):
         return identifier.value if identifier else None
 
     def set_identifier_value(self, category, value):
-        identifier, created = Identifier.objects.get_or_create(referent=self,
+        identifier, created = Identifier.objects.get_or_create(object_id=self.pk,
+                                                               content_type=ContentType.objects.get_for_model(self),
                                                                category=category,
                                                                defaults=dict(value=value))
         if not created:
