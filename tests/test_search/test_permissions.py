8bca5a998f7ebda633014f6e80c9d5456420374d
Chad Whitacre <chad@zetaweb.com>
2016-11-21 07:16:55 -0500

    Update number of tests

--- tests/test_search/test_permissions.py
+++ tests/test_search/test_permissions.py
@@ -402,7 +402,7 @@ class TestGenerateCases(unittest.TestCase):
     # gc - generate_cases
 
     def test_gc_generates_cases(self):
-        assert_equal(len(list(generate_cases())), 108)
+        assert_equal(len(list(generate_cases())), 144)
 
     def test_gc_doesnt_create_any_nodes(self):
         list(generate_cases())
