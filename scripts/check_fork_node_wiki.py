a23bf20b7ae3ace3cea80d9a1b62fd5521ecf0f4
Nan Chen <nc6r@virginia.edu>
2016-12-01 11:59:08 -0500

    improve query and display

--- scripts/check_fork_node_wiki.py
+++ scripts/check_fork_node_wiki.py
@@ -1,39 +1,37 @@
 import logging
 import sys
 
-from modularodm import Q
 from framework.transactions.context import TokuTransaction
 
 from website.app import init_app
-from website.project.model import Node
-from website.addons.wiki.model import NodeWikiPage
+from framework.mongo import database
 from scripts import utils as script_utils
 
 logger = logging.getLogger(__name__)
 
 
 def get_fork_projects():
-    return Node.find(
-        Q('is_fork', 'exists', True) &
-        Q('is_deleted', 'eq', False)
-    )
+    return database.node.find({'is_fork': True, 'is_deleted': False}, {'wiki_page_versions': True})
 
 
 def main():
     counts = 0
     for node in get_fork_projects():
-        if node.wiki_pages_versions:
-            for key in node.wiki_pages_versions:
-                for wiki_id in node.wiki_pages_versions[key]:
-                    wiki = NodeWikiPage.load(wiki_id)
-                    if wiki.node._id != node._id:
-                        logger.info(
-                            'NodeWikiPage {} points to wrong node {} instead of correct node {}'.format(
-                                wiki_id, wiki.node._id, node._id
-                            ))
-                        counts += 1
-    if counts == 0:
-        logger.info('All wiki of fork nodes point to the correct nodes')
+        logger.info('Going through the wikis of node {}'.format(node._id))
+        for key in node.wiki_pages_versions:
+            for wiki_id in node.wiki_pages_versions[key]:
+                wiki = database.nodewikipage.find_one({'_id': wiki_id}, {'node': True})
+                if wiki['node'] != node._id:
+                    logger.error(
+                        'NodeWikiPage {} points to wrong node {} instead of correct node {}'.format(
+                            wiki_id, wiki.node._id, node._id
+                        ))
+                    counts += 1
+    if counts != 0:
+        logger.info('There are {} wikis points to the wrong node.'.format(counts))
+    else:
+        logger.info('All wiki of fork nodes point to the correct node.')
+
 
 
 if __name__ == '__main__':
