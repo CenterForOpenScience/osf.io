6670e8e2884426bed577441b87aa826cf9f84c65
erinspace <erin.braswell@gmail.com>
2016-12-02 09:56:47 -0500

    Be more consistent stats > metrics

--- admin/metrics/urls.py
+++ admin/metrics/urls.py
@@ -4,5 +4,5 @@ from django.contrib.auth.decorators import login_required as login
 from . import views
 
 urlpatterns = [
-    url(r'^$', login(views.MetricsView.as_view()), name='stats_list'),
+    url(r'^$', login(views.MetricsView.as_view()), name='metrics'),
 ]
