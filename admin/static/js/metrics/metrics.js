37b8e560a98c3c31e18f34846b2668f79e961b5e
erinspace <erin.braswell@gmail.com>
2016-12-05 10:38:52 -0500

    Add linked addon and users by status

--- admin/static/js/metrics/metrics.js
+++ admin/static/js/metrics/metrics.js
@@ -82,12 +82,117 @@ Keen.ready(function () {
             "domain"
         ],
         interval: "daily",
-        timeframe: "previous_7_days",
+        timeframe: "previous_1_week",
         timezone: "UTC"
     });
 
     client.draw(email_domains, document.getElementById("user-registration-by-email-domain"), {
         chartType: "linechart",
+        height: "auto",
+        width: "auto",
+        title: ' '
+    });
+
+    // Yesterday's Node Logs by User
+
+    // Previous 7 Days of Users by Status
+    var previous_week_active_users = new Keen.Query("sum",  {
+        eventCollection: "user_summary",
+        interval: "daily",
+        targetProperty: "status.active",
+        timeframe: "previous_1_week",
+        timezone: "UTC"
+    });
+
+    var previous_week_unconfirmed_users = new Keen.Query("sum",  {
+        eventCollection: "user_summary",
+        interval: "daily",
+        targetProperty: "status.unconfirmed",
+        timeframe: "previous_1_week",
+        timezone: "UTC"
+    });
+
+    var previous_week_merged_users = new Keen.Query("sum",  {
+        eventCollection: "user_summary",
+        interval: "daily",
+        targetProperty: "status.merged",
+        timeframe: "previous_1_week",
+        timezone: "UTC"
+    });
+
+    var previous_week_depth_users = new Keen.Query("sum",  {
+        eventCollection: "user_summary",
+        interval: "daily",
+        targetProperty: "status.depth",
+        timeframe: "previous_1_week",
+        timezone: "UTC"
+    });
+
+    var previous_week_deactivated_users = new Keen.Query("sum",  {
+        eventCollection: "user_summary",
+        interval: "daily",
+        targetProperty: "status.deactivated",
+        timeframe: "previous_1_week",
+        timezone: "UTC"
+    });
+
+    var chart = new Keen.Dataviz()
+        .el(document.getElementById("previous-7-days-of-users-by-status"))
+        .chartType("linechart")
+        .chartOptions({
+            legend: {position: "top"}
+        }).prepare();
+
+    client.run([
+        previous_week_active_users,
+        previous_week_unconfirmed_users,
+        previous_week_merged_users,
+        previous_week_depth_users,
+        previous_week_deactivated_users
+    ], function(err, res) {
+        var active_result = res[0].result;
+        var unconfirmed_result = res[1].result;
+        var merged_result = res[2].result;
+        var depth_result = res[3].result;
+        var deactivated_result = res[4].result;
+        var data = [];
+        var i=0;
+
+        while (i < active_result.length) {
+            data[i] = {
+                timeframe: active_result[i]["timeframe"],
+                value: [
+                    {category: "Active", result: active_result[i].value},
+                    {category: "Unconfirmed", result: unconfirmed_result[i].value},
+                    {category: "Merged", result: merged_result[i].value},
+                    {category: "Depth", result: depth_result[i].value},
+                    {category: "Deactivated", result: deactivated_result[i].value}
+                ]
+            };
+            if (i === active_result.length - 1) {
+                chart.parseRawData({result: data}).render();
+            }
+            i++;
+        }
+    });
+
+    // Previous 7 days of linked addon by addon name
+    var linked_addon = new Keen.Query("sum", {
+        eventCollection: "addon_snapshot",
+        targetProperty: "users.linked",
+        groupBy: [
+            "provider.name"
+        ],
+        interval: "daily",
+        timeframe: "previous_1_week",
+        timezone: "UTC"
+    });
+
+    client.draw(linked_addon, document.getElementById("previous-7-days-of-linked-addon-by-addon-name"), {
+        chartType: "linechart",
+        chartOptions: {
+            legend: {position: "top"}
+        },
         height: "auto",
         width: "auto",
         title: ' '
