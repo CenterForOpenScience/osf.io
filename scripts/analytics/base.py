488099ac84b7975d73e0f615f77b108ef5741437
erinspace <erin.braswell@gmail.com>
2016-12-07 09:31:25 -0500

    Change calling of entered scripts from args

--- scripts/analytics/base.py
+++ scripts/analytics/base.py
@@ -162,9 +162,8 @@ class BaseAnalyticsHarness(object):
         analytics_classes = self.analytics_classes
         if command_line:
             args = self.parse_args()
-            entered_scripts = args.analytics_scripts
-            if args.entered_scripts:
-                analytics_classes = self.try_to_import_from_args(entered_scripts)
+            if args.analytics_scripts:
+                analytics_classes = self.try_to_import_from_args(args.analytics_scripts)
 
         for analytics_class in analytics_classes:
             class_instance = analytics_class()
@@ -199,7 +198,7 @@ class DateAnalyticsHarness(BaseAnalyticsHarness):
                 except AttributeError:
                     raise AttributeError('You must either specify a date or use the yesterday argument to gather analytics for yesterday.')
             if args.analytics_scripts:
-                analytics_classes = self.try_to_import_from_args(entered_scripts)
+                analytics_classes = self.try_to_import_from_args(args.analytics_scripts)
 
         for analytics_class in analytics_classes:
             class_instance = analytics_class()
