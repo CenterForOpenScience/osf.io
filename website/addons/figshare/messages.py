d27bbed4afdabd7f7f48c538a1b9747dc9e3270c
Matt Frazier <maf7sm@virginia.edu>
2016-12-08 14:27:42 -0500

    Ensure .???_is_public hits the correct endpoint     -make perms messages reflect container type

--- website/addons/figshare/messages.py
+++ website/addons/figshare/messages.py
@@ -3,9 +3,9 @@ BEFORE_PAGE_LOAD_PRIVATE_NODE_MIXED_FS = 'Warning: This OSF {category} is privat
 
 BEFORE_PAGE_LOAD_PUBLIC_NODE_MIXED_FS = 'Warning: This OSF {category} is public but figshare project {project_id} may contain some private files or filesets.'
 
-BEFORE_PAGE_LOAD_PERM_MISMATCH = 'Warning: This OSF {category} is {node_perm}, but the figshare article {figshare_id} is {figshare_perm}. '
+BEFORE_PAGE_LOAD_PERM_MISMATCH = 'Warning: This OSF {category} is {node_perm}, but the figshare {folder_type} {figshare_id} is {figshare_perm}. '
 
-BEFORE_PAGE_LOAD_PUBLIC_NODE_PRIVATE_FS = 'Users can view the contents of this private figshare article. '
+BEFORE_PAGE_LOAD_PUBLIC_NODE_PRIVATE_FS = 'Users can view the contents of this private figshare {folder_type}. '
 # END MODEL MESSAGES
 
 # MFR MESSAGES :views/crud.py
