b9d7fd7ba576a5d1aae24e941829e683748ede48
erinspace <erin.braswell@gmail.com>
2016-12-27 14:55:40 -0500

    Add a metrics page to handle the loading of each tab for the charts

---
+++ admin/static/js/pages/metrics-page.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var $  = require('jquery');
+var keenAnalysis = require('keen-analysis');
+var Metrics = require('js/metrics/metrics');
+
+
+
+keenAnalysis.ready(function() {
+
+    Metrics.UserGainMetrics();
+
+    $('#reload-node-logs')[0].onclick = function() {
+        Metrics.NodeLogsPerUser();
+    };
+
+    $('#institution-tab')[0].onclick = function() {
+        Metrics.InstitutionMetrics();
+    };
+
+    $('#active-user-tab')[0].onclick = function() {
+        Metrics.ActiveUserMetrics();
+    };
+
+    $('#healthy-user-tab')[0].onclick = function() {
+        Metrics.HealthyUserMetrics();
+    };
+
+    $('#raw-numbers-tab')[0].onclick = function() {
+        Metrics.RawNumberMetrics();
+    };
+
+});
