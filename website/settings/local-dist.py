2927f958d3a5d4ba8753f5af5e02ac6d4b2a513a
Henrique Moco <hmoco94@vt.edu>
2017-01-09 16:55:04 -0500

    Add registries link and ember config info

--- website/settings/local-dist.py
+++ website/settings/local-dist.py
@@ -26,6 +26,11 @@ EXTERNAL_EMBER_APPS = {
     #     'url': '/meetings/',
     #     'server': 'http://localhost:4201',
     #     'path': '../osf-meetings/dist/'
+    # },
+    # 'registries': {
+    #     'url': '/registries/',
+    #     'server': 'http://localhost:4200',
+    #     'path': '../ember-osf2/ember-osf-registries/dist/'
     # }
 }
 
