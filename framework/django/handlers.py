8ba3c436696bc6b723e0ae299fdb6d13eaff354c
Chris Wisecarver <cwisecarver@cos.io>
2017-01-17 20:14:41 -0500

    Add missing files.

---
+++ framework/django/handlers.py
@@ -0,0 +1,17 @@
+import logging
+
+from django.db import close_old_connections, reset_queries
+
+logger = logging.getLogger(__name__)
+
+
+def before_request(*args, **kwargs):
+    reset_queries()
+
+def teardown_request(*args, **kwargs):
+    close_old_connections()
+
+handlers = {
+    'before_request': before_request,
+    'teardown_request': teardown_request,
+}
