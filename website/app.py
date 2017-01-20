49614db84697ad491099d35bf8602732b2c6a387
Chris Wisecarver <cwisecarver@cos.io>
2017-01-20 12:00:08 -0500

    Only run init_app once ... no matter what

--- website/app.py
+++ website/app.py
@@ -104,6 +104,10 @@ def init_app(settings_module='website.settings', set_backends=True, routes=True,
     :param routes: Whether to set the url map.
 
     """
+    # Ensure app initialization only takes place once
+    if app.config.get('IS_INITIALIZED', False) is True:
+        return app
+
     logger.info('Initializing the application from process {}, thread {}.'.format(
         os.getpid(), thread.get_ident()
     ))
@@ -149,6 +153,7 @@ def init_app(settings_module='website.settings', set_backends=True, routes=True,
         ensure_licenses()
     apply_middlewares(app, settings)
 
+    app.config['IS_INITIALIZED'] = True
     return app
 
 
