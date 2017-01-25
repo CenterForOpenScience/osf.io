9f0d1577416e32ef6ed3ea979f4ca8e6c514b3e8
Alex Schiller <alexschiller@gmail.com>
2017-01-25 16:25:58 -0500

    Swap to ensure bytes, add isinstance checks to starting values in tests

--- framework/encryption/__init__.py
+++ framework/encryption/__init__.py
@@ -6,22 +6,21 @@ from website import settings
 
 SENSITIVE_DATA_KEY = jwe.kdf(settings.SENSITIVE_DATA_SECRET.encode('utf-8'), settings.SENSITIVE_DATA_SALT.encode('utf-8'))
 
-def ensure_str(value):
+def ensure_bytes(value):
     """Helper function to ensure all inputs are encoded to the proper value utf-8 value regardless of input type"""
-    try:
-        return value.encode('utf-8')
-    except UnicodeDecodeError:
-        return value.decode('utf-8').encode('utf-8')
+    if isinstance(value, bytes):
+        return value
+    return value.encode('utf-8')
 
 def encrypt(value):
     if value:
-        value = ensure_str(value)
+        value = ensure_bytes(value)
         return jwe.encrypt(bytes(value), SENSITIVE_DATA_KEY)
     return None
 
 def decrypt(value):
     if value:
-        value = ensure_str(value)
+        value = ensure_bytes(value)
         return jwe.decrypt(bytes(value), SENSITIVE_DATA_KEY)
     return None
 
