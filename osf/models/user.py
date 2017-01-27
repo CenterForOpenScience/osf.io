58c9651ac5b0333edd01a8f7a419d6240cdb6b48
erinspace <erin.braswell@gmail.com>
2017-01-27 16:10:19 -0500

    Port old disable_account method to new user model

--- osf/models/user.py
+++ osf/models/user.py
@@ -5,6 +5,7 @@ import urllib
 import urlparse
 import uuid
 from copy import deepcopy
+from flask import Request as FlaskRequest
 from framework import analytics
 
 # OSF imports
@@ -27,6 +28,7 @@ from framework.auth.exceptions import (ChangePasswordError, ExpiredTokenError,
 from framework.exceptions import PermissionsError
 from framework.sentry import log_exception
 from framework.sessions.utils import remove_sessions_for_user
+from framework.mongo import get_cache_key
 from modularodm.exceptions import NoResultsFound
 from osf.exceptions import reraise_django_validation_errors
 from osf.models.base import BaseModel, GuidMixin
@@ -676,6 +678,40 @@ class OSFUser(DirtyFieldsMixin, GuidMixin, BaseModel,
         user.merged_by = self
 
         user.save()
+
+    def disable_account(self):
+        """
+        Disables user account, making is_disabled true, while also unsubscribing user
+        from mailchimp emails, remove any existing sessions.
+
+        Ported from framework/auth/core.py
+        """
+        from website import mailchimp_utils
+        from framework.auth import logout
+
+        try:
+            mailchimp_utils.unsubscribe_mailchimp(
+                list_name=website_settings.MAILCHIMP_GENERAL_LIST,
+                user_id=self._id,
+                username=self.username
+            )
+        except mailchimp_utils.mailchimp.ListNotSubscribedError:
+            pass
+        except mailchimp_utils.mailchimp.InvalidApiKeyError:
+            if not website_settings.ENABLE_EMAIL_SUBSCRIPTIONS:
+                pass
+            else:
+                raise
+        except mailchimp_utils.mailchimp.EmailNotExistsError:
+            pass
+        self.is_disabled = True
+
+        # we must call both methods to ensure the current session is cleared and all existing
+        # sessions are revoked.
+        req = get_cache_key()
+        if isinstance(req, FlaskRequest):
+            logout()
+        remove_sessions_for_user(self)
 
     def update_is_active(self):
         """Update ``is_active`` to be consistent with the fields that
