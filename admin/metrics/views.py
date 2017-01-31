3885e8fd36f419976d0b002c391dc246588929c7
erinspace <erin.braswell@gmail.com>
2017-01-31 14:18:40 -0500

    Add view metrics permission to metrics view

--- admin/metrics/views.py
+++ admin/metrics/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import PermissionRequiredMixin
 
 from admin.base.settings import KEEN_CREDENTIALS
 
@@ -6,8 +7,9 @@ from admin.base.settings import KEEN_CREDENTIALS
 from admin.base.utils import OSFAdmin
 
 
-class MetricsView(OSFAdmin, TemplateView):
+class MetricsView(OSFAdmin, TemplateView, PermissionRequiredMixin):
     template_name = 'metrics/osf_metrics.html'
+    permission_required = 'admin.view_metrics'
 
     def get_context_data(self, **kwargs):
 
