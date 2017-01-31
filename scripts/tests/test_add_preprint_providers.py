54218e42f9effdb7d30e3bbe9c85115a7f94b0da
Henrique Moco <hmoco94@vt.edu>
2017-01-31 10:01:24 -0500

    Add bitss preprint provider

--- scripts/tests/test_add_preprint_providers.py
+++ scripts/tests/test_add_preprint_providers.py
@@ -6,7 +6,7 @@ from website.models import PreprintProvider
 from website.project.licenses import ensure_licenses
 from scripts.update_taxonomies import main as taxonomy_main
 from scripts.populate_preprint_providers import main as populate_main
-
+from scripts.populate_preprint_providers import STAGING_PREPRINT_PROVIDERS, PROD_PREPRINT_PROVIDERS
 
 class TestAddPreprintProviders(OsfTestCase):
     def setUp(self):
@@ -18,12 +18,30 @@ class TestAddPreprintProviders(OsfTestCase):
 
     def test_add_prod_providers(self):
         populate_main('prod')
-        assert_equal(PreprintProvider.find().count(), 4)
+        providers = PreprintProvider.find()
+        assert_equal(providers.count(), len(PROD_PREPRINT_PROVIDERS))
+        ids = [provider._id for provider in providers]
+        for id in PROD_PREPRINT_PROVIDERS:
+            assert_in(id, ids)
+        for id in set(STAGING_PREPRINT_PROVIDERS) - set(PROD_PREPRINT_PROVIDERS):
+            assert_not_in(id, ids)
+
 
     def test_add_default_providers(self):
         populate_main(None)
-        assert_equal(PreprintProvider.find().count(), 4)
+        providers = PreprintProvider.find()
+        assert_equal(providers.count(), len(PROD_PREPRINT_PROVIDERS))
+        ids = [provider._id for provider in providers]
+        for id in PROD_PREPRINT_PROVIDERS:
+            assert_in(id, ids)
+        for id in set(STAGING_PREPRINT_PROVIDERS) - set(PROD_PREPRINT_PROVIDERS):
+            assert_not_in(id, ids)
+
 
     def test_add_staging_providers(self):
         populate_main('stage')
-        assert_equal(PreprintProvider.find().count(), 6)
+        providers = PreprintProvider.find()
+        assert_equal(PreprintProvider.find().count(), len(STAGING_PREPRINT_PROVIDERS))
+        ids = [provider._id for provider in providers]
+        for id in STAGING_PREPRINT_PROVIDERS:
+            assert_in(id, ids)
