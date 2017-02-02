ef15650715fa11bcf5e93f43d75d8c5cc2084124
longze chen <cs.longze.chen@gmail.com>
2017-02-02 11:29:30 -0500

    Add permission classes and required scopes for api cas endpoints.

--- api/cas/views.py
+++ api/cas/views.py
@@ -3,14 +3,26 @@ from django.contrib.auth.models import AnonymousUser
 from rest_framework import generics
 from rest_framework import status
 from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from api.base.views import JSONAPIBaseView
+from api.base.permissions import TokenHasScope
 from api.base.serializers import JSONAPISerializer
+from api.base.views import JSONAPIBaseView
 from api.cas.auth import CasAuthentication
+
+from framework.auth.oauth_scopes import CoreScopes
 
 
 class CasLogin(JSONAPIBaseView, generics.CreateAPIView):
+
+    permission_classes = (
+        IsAuthenticated,
+        TokenHasScope,
+    )
+
+    required_read_scopes = [CoreScopes.NULL]
+    required_write_scopes = [CoreScopes.NULL]
 
     view_category = 'cas'
     view_name = 'cas-login'
@@ -48,35 +60,15 @@ class CasLogin(JSONAPIBaseView, generics.CreateAPIView):
         return Response(content)
 
 
-class CasTwoFactor(JSONAPIBaseView, generics.CreateAPIView):
-
-    view_category = 'cas'
-    view_name = 'cas-two-factor'
-
-    serializer_class = JSONAPISerializer
-
-    authentication_classes = (CasAuthentication,)
-
-    def post(self, request, *args, **kwargs):
-
-        user = request.user
-        if not user or isinstance(user, AnonymousUser):
-            raise AuthenticationFailed
-        else:
-            content = {
-                'status': 'AUTHENTICATION_SUCCESS',
-                'userId': user._id,
-                'attributes': {
-                    'username': user.username,
-                    'givenName': user.given_name,
-                    'familyName': user.family_name,
-                }
-            }
-
-        return Response(content)
-
-
 class CasRegister(JSONAPIBaseView, generics.CreateAPIView):
+
+    permission_classes = (
+        IsAuthenticated,
+        TokenHasScope,
+    )
+
+    required_read_scopes = [CoreScopes.NULL]
+    required_write_scopes = [CoreScopes.NULL]
 
     view_category = 'cas'
     view_name = 'cas-login'
@@ -102,6 +94,14 @@ class CasRegister(JSONAPIBaseView, generics.CreateAPIView):
 
 
 class CasInstitutionAuthenticate(JSONAPIBaseView, generics.CreateAPIView):
+
+    permission_classes = (
+        IsAuthenticated,
+        TokenHasScope,
+    )
+
+    required_read_scopes = [CoreScopes.NULL]
+    required_write_scopes = [CoreScopes.NULL]
 
     view_category = 'cas'
     view_name = 'cas-institution-authenticate'
