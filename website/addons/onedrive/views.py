1b178ae9bfc822a24461d9c1c46f76abdb01e8da
Fitz Elliott <fitz@cos.io>
2017-02-03 13:40:40 -0500

    convert OneDrive views to use generic views
     * remove unused share emails view
    
     * repoint routes to new views

--- website/addons/onedrive/views.py
+++ website/addons/onedrive/views.py
@@ -7,129 +7,54 @@ import logging
 from flask import request
 from website.addons.onedrive.client import OneDriveClient
 
-from framework.exceptions import HTTPError, PermissionsError
-from framework.auth.decorators import must_be_logged_in
+from framework.exceptions import HTTPError
 
-from website.oauth.models import ExternalAccount
-
-from website.util import permissions
 from website.project.decorators import (
     must_have_addon, must_be_addon_authorizer,
-    must_have_permission, must_not_be_registration,
 )
 
+from website.addons.base import generic_views
 from website.addons.onedrive.serializer import OneDriveSerializer
 
 logger = logging.getLogger(__name__)
 
 logging.getLogger('onedrive1').setLevel(logging.WARNING)
 
-@must_be_logged_in
-def onedrive_get_user_settings(auth):
-    """ Returns the list of all of the current user's authorized OneDrive accounts """
-    serializer = OneDriveSerializer(user_settings=auth.user.get_addon('onedrive'))
-    return serializer.serialized_user_settings
+SHORT_NAME = 'onedrive'
+FULL_NAME = 'Microsoft OneDrive'
 
+onedrive_account_list = generic_views.account_list(
+    SHORT_NAME,
+    OneDriveSerializer
+)
 
-@must_have_addon('onedrive', 'node')
-@must_have_permission(permissions.WRITE)
-def onedrive_get_config(node_addon, auth, **kwargs):
-    """API that returns the serialized node settings."""
-    return {
-        'result': OneDriveSerializer().serialize_settings(node_addon, auth.user),
-    }
+onedrive_get_config = generic_views.get_config(
+    SHORT_NAME,
+    OneDriveSerializer
+)
 
-
-@must_not_be_registration
-@must_have_addon('onedrive', 'user')
-@must_have_addon('onedrive', 'node')
-@must_be_addon_authorizer('onedrive')
-@must_have_permission(permissions.WRITE)
-def onedrive_set_config(node_addon, user_addon, auth, **kwargs):
-    """View for changing a node's linked onedrive folder."""
-    folder = request.json.get('selected')
-    serializer = OneDriveSerializer(node_settings=node_addon)
-
-    logger.debug('folder::' + repr(folder))
-    logger.debug('serializer::' + repr(serializer))
-
-    name = folder['name']
-
+def _set_folder(node_addon, folder, auth):
     node_addon.set_folder(folder, auth=auth)
-
-    return {
-        'result': {
-            'folder': {
-                'name': name,
-                'path': name,
-            },
-            'urls': serializer.addon_serialized_urls,
-        },
-        'message': 'Successfully updated settings.',
-    }
-
-
-@must_have_addon('onedrive', 'user')
-@must_have_addon('onedrive', 'node')
-@must_have_permission(permissions.WRITE)
-def onedrive_add_user_auth(auth, node_addon, user_addon, **kwargs):
-    """Import onedrive credentials from the currently logged-in user to a node.
-    """
-    external_account = ExternalAccount.load(
-        request.json['external_account_id']
-    )
-
-    if external_account not in user_addon.external_accounts:
-        raise HTTPError(http.FORBIDDEN)
-
-    try:
-        node_addon.set_auth(external_account, user_addon.owner)
-    except PermissionsError:
-        raise HTTPError(http.FORBIDDEN)
-
     node_addon.save()
 
-    return {
-        'result': OneDriveSerializer().serialize_settings(node_addon, auth.user),
-        'message': 'Successfully imported access token from profile.',
-    }
+onedrive_set_config = generic_views.set_config(
+    SHORT_NAME,
+    FULL_NAME,
+    OneDriveSerializer,
+    _set_folder
+)
 
+onedrive_import_auth = generic_views.import_auth(
+    SHORT_NAME,
+    OneDriveSerializer
+)
 
-@must_not_be_registration
-@must_have_addon('onedrive', 'node')
-@must_have_permission(permissions.WRITE)
-def onedrive_remove_user_auth(auth, node_addon, **kwargs):
-    node_addon.deauthorize(auth=auth)
-    node_addon.save()
+onedrive_deauthorize_node = generic_views.deauthorize_node(
+    SHORT_NAME
+)
 
-
-@must_have_addon('onedrive', 'user')
-@must_have_addon('onedrive', 'node')
-@must_have_permission(permissions.WRITE)
-def onedrive_get_share_emails(auth, user_addon, node_addon, **kwargs):
-    """Return a list of emails of the contributors on a project.
-
-    The current user MUST be the user who authenticated OneDrive for the node.
-    """
-    if not node_addon.user_settings:
-        raise HTTPError(http.BAD_REQUEST)
-    # Current user must be the user who authorized the addon
-    if node_addon.user_settings.owner != auth.user:
-        raise HTTPError(http.FORBIDDEN)
-
-    return {
-        'result': {
-            'emails': [
-                contrib.username
-                for contrib in node_addon.owner.contributors
-                if contrib != auth.user
-            ],
-        }
-    }
-
-
-@must_have_addon('onedrive', 'node')
-@must_be_addon_authorizer('onedrive')
+@must_have_addon(SHORT_NAME, 'node')
+@must_be_addon_authorizer(SHORT_NAME)
 def onedrive_folder_list(node_addon, **kwargs):
     """Returns a list of folders in OneDrive"""
     if not node_addon.has_auth:
