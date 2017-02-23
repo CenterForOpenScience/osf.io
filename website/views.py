4bc266a817283edd297cf8c3549449486efdbd5e
Chris Wisecarver <cwisecarver@cos.io>
2017-02-23 17:13:58 -0500

    Change find_bookmark_collection to be more django-y

--- website/views.py
+++ website/views.py
@@ -117,9 +117,9 @@ def index():
 def find_bookmark_collection(user):
     Collection = apps.get_model('osf.Collection')
     bookmark_collection = Collection.find(Q('is_bookmark_collection', 'eq', True) & Q('creator', 'eq', user))
-    if bookmark_collection.count() == 0:
-        new_bookmark_collection(user)
-    return bookmark_collection[0]
+    if not bookmark_collection.exists():
+        return new_bookmark_collection(user)
+    return bookmark_collection.get()
 
 
 @must_be_logged_in
