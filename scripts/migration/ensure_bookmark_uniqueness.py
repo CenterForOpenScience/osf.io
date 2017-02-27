62dddc78ac8fb980d796e92ad7add2ec7ae30d28
Matt Frazier <maf7sm@virginia.edu>
2017-02-27 16:14:40 -0500

    Port uniqueness script to django
    [#OSF-7551]

--- scripts/migration/ensure_bookmark_uniqueness.py
+++ scripts/migration/ensure_bookmark_uniqueness.py
@@ -1,20 +1,22 @@
 import argparse
 import logging
 
-from modularodm import Q
+import django
+from django.db import transaction
 
-from framework.auth.core import User
-from framework.transactions.context import TokuTransaction
+django.setup()
+
+from osf.models import Collection, OSFUser
+from osf.modm_compat import Q
 from scripts import utils as script_utils
 from website.app import init_app
-from website.project import Node
 
 
 logger = logging.getLogger(__name__)
 
 def get_targets():
     logger.info('Acquiring targets...')
-    targets = [u for u in User.find() if Node.find(Q('is_bookmark_collection', 'eq', True) & Q('is_deleted', 'eq', False) & Q('contributors', 'eq', u._id)).count() > 1]
+    targets = [u for u in OSFUser.find() if Collection.find(Q('is_bookmark_collection', 'eq', True) & Q('is_deleted', 'eq', False) & Q('creator', 'eq', u)).count() > 1]
     logger.info('Found {} target users.'.format(len(targets)))
     return targets
 
@@ -23,15 +25,16 @@ def migrate():
     total = len(targets)
     for i, user in enumerate(targets):
         logger.info('({}/{}) Preparing to migrate User {}'.format(i + 1, total, user._id))
-        bookmarks = Node.find(Q('is_bookmark_collection', 'eq', True) & Q('contributors', 'eq', user._id))
+        bookmarks = Collection.find(Q('is_bookmark_collection', 'eq', True) & Q('creator', 'eq', user))
         if sum([bool(n.nodes) for n in bookmarks]) > 1:
-            raise Exception('Expected no users to have more than one bookmark with .nodes, {} violated'.format(user._id))
+            logger.warn('Expected no users to have more than one bookmark with .nodes, {} violated. Selecting first one'.format(user._id))
         bookmark_to_keep = None
         for n in bookmarks:
             if n.nodes:
                 bookmark_to_keep = n
+                break
         bookmark_to_keep = bookmark_to_keep or bookmarks[0]
-        logger.info('Marking Node {} as primary Bookmark Collection for User {}, preparing to delete others'.format(bookmark_to_keep._id, user._id))
+        logger.info('Marking Collection {} as primary Bookmark Collection for User {}, preparing to delete others'.format(bookmark_to_keep._id, user._id))
         for n in bookmarks:
             if n._id != bookmark_to_keep._id:
                 n.is_deleted = True
@@ -53,7 +56,7 @@ def main():
     if not pargs.dry_run:
         script_utils.add_file_logger(logger, __file__)
     init_app(set_backends=True, routes=False)
-    with TokuTransaction():
+    with transaction.atomic():
         migrate()
         if pargs.dry_run:
             raise Exception('Dry Run -- Transaction aborted.')
