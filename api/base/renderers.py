4baea8ffeca9221a7c1cfdcef9017f585a462f68
Chris Wisecarver <cwisecarver@cos.io>
2017-03-02 09:58:35 -0500

    Use ujson in renderer

--- api/base/renderers.py
+++ api/base/renderers.py
@@ -1,6 +1,8 @@
 import re
-from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer, StaticHTMLRenderer
 
+from django.utils import six
+from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer, StaticHTMLRenderer
+import ujson
 
 class JSONRendererWithESISupport(JSONRenderer):
     format = 'json'
@@ -17,6 +19,42 @@ class JSONRendererWithESISupport(JSONRenderer):
 class JSONAPIRenderer(JSONRendererWithESISupport):
     format = 'jsonapi'
     media_type = 'application/vnd.api+json'
+
+    def render(self, data, accepted_media_type=None, renderer_context=None):
+        """
+        Render `data` into JSON, returning a bytestring.
+        """
+        if data is None:
+            return bytes()
+
+        renderer_context = renderer_context or {}
+        indent = self.get_indent(accepted_media_type, renderer_context) or 4
+
+        # if indent is None:
+        #     separators = SHORT_SEPARATORS if self.compact else LONG_SEPARATORS
+        # else:
+        #     separators = INDENT_SEPARATORS
+
+        ret = ujson.dumps(  # UJSON is faster
+            data,
+            # , cls=self.encoder_class,
+            escape_forward_slashes=False,
+            indent=indent, ensure_ascii=self.ensure_ascii,
+            # separators=separators
+        )
+
+        # On python 2.x json.dumps() returns bytestrings if ensure_ascii=True,
+        # but if ensure_ascii=False, the return type is underspecified,
+        # and may (or may not) be unicode.
+        # On python 3.x json.dumps() returns unicode strings.
+        if isinstance(ret, six.text_type):
+            # We always fully escape \u2028 and \u2029 to ensure we output JSON
+            # that is a strict javascript subset. If bytes were returned
+            # by json.dumps() then we don't have these characters in any case.
+            # See: http://timelessrepo.com/json-isnt-a-javascript-subset
+            ret = ret.replace('\u2028', '\\u2028').replace('\u2029', '\\u2029')
+            return bytes(ret.encode('utf-8'))
+        return ret
 
 
 class BrowsableAPIRendererNoForms(BrowsableAPIRenderer):
