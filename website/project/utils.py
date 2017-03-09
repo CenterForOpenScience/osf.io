eaa7e25d07ca2544e433ae9c0c98aef20e5f42b3
Chris Wisecarver <cwisecarver@cos.io>
2017-03-09 14:20:51 -0500

    flake

--- website/project/utils.py
+++ website/project/utils.py
@@ -29,12 +29,12 @@ def recent_public_registrations(n=10):
     Registration = apps.get_model('osf.Registration')
 
     return Registration.objects.filter(
-            is_public=True,
-            is_deleted=False,
-        ).filter(
-            DQ(DQ(embargo__isnull=True) | ~DQ(embargo__state='unapproved')) &
-            DQ(DQ(retraction__isnull=True) | ~DQ(retraction__state='approved'))
-        ).get_roots().order_by('-registered_date').limit(n)
+        is_public=True,
+        is_deleted=False,
+    ).filter(
+        DQ(DQ(embargo__isnull=True) | ~DQ(embargo__state='unapproved')) &
+        DQ(DQ(retraction__isnull=True) | ~DQ(retraction__state='approved'))
+    ).get_roots().order_by('-registered_date').limit(n)
 
 
 def get_keen_activity():
