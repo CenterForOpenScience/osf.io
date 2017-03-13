c5797979eab9028924085ca7ebb388fb924e337a
longze chen <cs.longze.chen@gmail.com>
2017-03-13 15:46:47 -0400

    Update recent changes from https://github.com/CenterForOpenScience/osf.io/pull/6827.

--- tests/test_views.py
+++ tests/test_views.py
@@ -3895,28 +3895,27 @@ class TestAuthLogout(OsfTestCase):
         self.valid_next_url = web_url_for('dashboard', _absolute=True)
         self.invalid_next_url = 'http://localhost:1234/abcde'
         self.auth_user = AuthUserFactory()
-        self.user = UserFactory()
 
     def test_logout_with_valid_next_url_logged_in(self):
-        logout_url = web_url_for('auth_logout', _absolute=True, next_url=self.valid_next_url)
+        logout_url = web_url_for('auth_logout', _absolute=True, next=self.valid_next_url)
         resp = self.app.get(logout_url, auth=self.auth_user.auth)
         assert_equal(resp.status_code, http.FOUND)
         assert_equal(cas.get_logout_url(logout_url), resp.headers['Location'])
 
     def test_logout_with_valid_next_url_logged_out(self):
-        logout_url = web_url_for('auth_logout', _absolute=True, next_url=self.valid_next_url)
+        logout_url = web_url_for('auth_logout', _absolute=True, next=self.valid_next_url)
         resp = self.app.get(logout_url, auth=None)
         assert_equal(resp.status_code, http.FOUND)
         assert_equal(self.valid_next_url, resp.headers['Location'])
 
     def test_logout_with_invalid_next_url_logged_in(self):
-        logout_url = web_url_for('auth_logout', _absolute=True, next_url=self.invalid_next_url)
+        logout_url = web_url_for('auth_logout', _absolute=True, next=self.invalid_next_url)
         resp = self.app.get(logout_url, auth=self.auth_user.auth)
         assert_equal(resp.status_code, http.FOUND)
         assert_equal(cas.get_logout_url(self.goodbye_url), resp.headers['Location'])
 
     def test_logout_with_invalid_next_url_logged_out(self):
-        logout_url = web_url_for('auth_logout', _absolute=True, next_url=self.invalid_next_url)
+        logout_url = web_url_for('auth_logout', _absolute=True, next=self.invalid_next_url)
         resp = self.app.get(logout_url, auth=None)
         assert_equal(resp.status_code, http.FOUND)
         assert_equal(cas.get_logout_url(self.goodbye_url), resp.headers['Location'])
