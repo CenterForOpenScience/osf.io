f4caec178a21f71828dc52626da03f467aa6d6c3
Chris Wisecarver <cwisecarver@cos.io>
2017-03-23 09:35:22 -0400

    Revert "Remove generic index and add partial"
    This reverts commit 7031c2f119d238c627f6228e54cf883510d48d9e.

--- osf/models/files.py
+++ osf/models/files.py
@@ -103,6 +103,9 @@ class BaseFileNode(TypedModel, CommentableMixin, OptionalGuidMixin, Taggable, Ob
             ('node', 'type', 'provider', '_path'),
             ('node', 'type', 'provider'),
         )
+        unique_together = (
+            ('node', 'name', 'parent', 'type', '_path'),
+        )
 
     @property
     def history(self):
