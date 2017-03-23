78dbbf8d0e9b3cd7531caf442aa85d96189609ee
Casey Rollins <case.rollins@gmail.com>
2017-03-23 15:55:59 -0400

    Add metaschema filtering tests.

--- api_tests/metaschemas/views/test_metaschemas_list.py
+++ api_tests/metaschemas/views/test_metaschemas_list.py
@@ -6,9 +6,11 @@ from website.project.model import ensure_schemas
 from api.base.settings.defaults import API_BASE
 
 from tests.base import ApiTestCase
+from osf.models import MetaSchema
 from osf_tests.factories import (
     AuthUserFactory
 )
+from website.project.metadata.schemas import ACTIVE_META_SCHEMAS, LATEST_SCHEMA_VERSION
 
 
 class TestMetaSchemaList(ApiTestCase):
@@ -40,3 +42,36 @@ class TestMetaSchemaList(ApiTestCase):
     def test_pass_unauthenticated_user_can_view_schemas(self):
         res = self.app.get(self.url)
         assert_equal(res.status_code, 200)
+
+class TestMetaSchemaListFiltering(ApiTestCase):
+
+    def setUp(self):
+        super(TestMetaSchemaListFiltering, self).setUp()
+        ensure_schemas()
+        self.url = '/{}metaschemas/?version=2.1&filter[{}]={}'
+        self.registration_schemas_count = MetaSchema.objects.filter(
+            category='registration',
+            schema_version=LATEST_SCHEMA_VERSION,
+            name__in=ACTIVE_META_SCHEMAS
+        ).count()
+        self.preprintprovider_schemas_count = MetaSchema.objects.filter(category='preprintprovider').count()
+
+    def test_filter_by_category_registration(self):
+        res = self.app.get(self.url.format(API_BASE, 'category', 'registration'))
+        assert_equal(res.status_code, 200)
+        assert_equal(res.json['meta']['total'], self.registration_schemas_count)
+
+    def test_filter_by_category_preprintprovider(self):
+        res = self.app.get(self.url.format(API_BASE, 'category', 'preprintprovider'))
+        assert_equal(res.status_code, 200)
+        assert_equal(res.json['meta']['total'], self.preprintprovider_schemas_count)
+
+    def test_filter_by_category_null(self):
+        res = self.app.get(self.url.format(API_BASE, 'category', 'null'))
+        assert_equal(res.status_code, 200)
+        assert_equal(res.json['meta']['total'], 0)
+
+    def test_invalid_filter(self):
+        res = self.app.get(self.url.format(API_BASE, 'fake', 'fake'), expect_errors=True)
+        assert_equal(res.status_code, 400)
+        assert_equal(res.json['errors'][0]['source']['parameter'], 'filter')
