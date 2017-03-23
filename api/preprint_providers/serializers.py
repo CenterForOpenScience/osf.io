2540077255652d5cfb41d2b611f87c8ba4347e25
Casey Rollins <case.rollins@gmail.com>
2017-03-23 13:50:03 -0400

    Add `extra` field to PreprintProviderSerializer.
    [skip ci]

--- api/preprint_providers/serializers.py
+++ api/preprint_providers/serializers.py
@@ -26,6 +26,7 @@ class PreprintProviderSerializer(JSONAPISerializer):
     subjects_acceptable = ser.JSONField(required=False, allow_null=True)
     logo_path = ser.CharField(read_only=True)
     banner_path = ser.CharField(read_only=True)
+    extra = ser.JSONField(read_only=True)
 
     preprints = RelationshipField(
         related_view='preprint_providers:preprints-list',
