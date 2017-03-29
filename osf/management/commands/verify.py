3bf97de436de460b30e070004a3aee3886c6aa3b
Chris Wisecarver <cwisecarver@cos.io>
2017-03-28 20:35:15 -0400

    More verification logic

--- osf/management/commands/verify.py
+++ osf/management/commands/verify.py
@@ -7,6 +7,7 @@ from datetime import datetime
 import pytz
 from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import ObjectDoesNotExist
 from django.core.management import BaseCommand
 
 from api.base.celery import app
@@ -18,6 +19,7 @@ from osf.models import OSFUser
 from osf.models import Tag
 from osf.utils.order_apps import get_ordered_models
 from website.app import init_app
+from website.files.models import StoredFileNode
 from .migratedata import set_backend
 logger = logging.getLogger('migrations')
 
@@ -83,7 +85,7 @@ def validate_m2m_field(field_name, django_obj, modm_obj):
             primary_identifier_name = field.through._meta.model.primary_identifier_name
         except AttributeError:
             primary_identifier_name = '_id'
-    django_guids = manager.all().values(primary_identifier_name)
+    django_guids = manager.all().values_list(primary_identifier_name, flat=True)
     # if there's a field alias, let's use that.
     if getattr(django_obj, 'FIELD_ALIASES', None) is None:
         modm_field_name = field_name
@@ -102,6 +104,10 @@ def validate_fk_relation(field_name, django_obj, modm_obj):
     if field_name in ['content_type', 'content_type_pk', 'content_type_id']:
         # modm doesn't have gfk
         return
+
+    if isinstance(modm_obj, StoredFileNode) and field_name == 'deleted_by':
+        return
+
     if django_obj._meta.model is EmbargoTerminationApproval and field_name == 'initiated_by':
         # EmbargoTerminationApproval didn't have an initiated_by in modm even though it was supposed to.
         return
@@ -127,8 +133,8 @@ def validate_fk_relation(field_name, django_obj, modm_obj):
 
 
 def validate_basic_field(field_name, django_obj, modm_obj):
-    if field_name in ['id', 'pk', 'object_id', 'guid_string', 'last_login']:
-        # modm doesn't have ids or guids or last_login
+    if field_name in ['id', 'pk', 'object_id', 'guid_string', 'last_login', 'is_superuser']:
+        # modm doesn't have these
         return
     # if there's a field alias, let's use that.
     if getattr(django_obj, 'FIELD_ALIASES', None) is None:
@@ -159,7 +165,7 @@ def validate_basic_field(field_name, django_obj, modm_obj):
             return
 
         if modm_value is None:
-            logger.error('modm value was None but django value was {}'.format(django_value))
+            logger.error('modm value was None but django value was {} for {} on {}.{}'.format(django_value, field_name, django_obj._meta.model.__module__, django_obj._meta.model.__class__))
             return
 
         # sometimes strings used to be integers, in some documents... LicenseRecord.year
@@ -189,51 +195,62 @@ def get_pk(modm_object, django_model, modm_to_django):
             format_lookup_key(modm_object._id, ContentType.objects.get_for_model(django_model).pk), ex))
 
 
-@app.task()
-def validate_page_of_model_data(django_model, basic_fields, fk_relations, m2m_relations, offset, limit):
-    set_backend()
-    init_app(routes=False, attach_request_handlers=False, fixtures=False)
-    register_nonexistent_models_with_modm()
+@app.task(bind=True, max_retries=None)  # retry forever because of deadlocks
+def validate_page_of_model_data(self, django_model, basic_fields, fk_relations, m2m_relations, offset, limit):
+    try:
+        set_backend()
+        init_app(routes=False, attach_request_handlers=False, fixtures=False)
+        register_nonexistent_models_with_modm()
 
-    modm_model = get_modm_model(django_model)
-    modm_to_django = build_toku_django_lookup_table_cache()
+        modm_model = get_modm_model(django_model)
+        modm_to_django = build_toku_django_lookup_table_cache()
 
-    if isinstance(django_model.modm_query, dict):
-        modm_queryset = modm_model.find(**django_model.modm_query)
-    else:
-        modm_queryset = modm_model.find(django_model.modm_query)
-    count = 0
-    page_of_modm_objects = modm_queryset.sort('-_id')[offset:limit]
-    django_ids = [get_pk(modm_obj, django_model, modm_to_django) for modm_obj in page_of_modm_objects]
-    django_objects = django_model.objects.filter(id__in=django_ids)
+        if isinstance(django_model.modm_query, dict):
+            modm_queryset = modm_model.find(**django_model.modm_query)
+        else:
+            modm_queryset = modm_model.find(django_model.modm_query)
+        count = 0
+        page_of_modm_objects = modm_queryset.sort('-_id')[offset:limit]
+        django_ids = [get_pk(modm_obj, django_model, modm_to_django) for modm_obj in page_of_modm_objects]
+        django_objects = django_model.objects.filter(id__in=django_ids)
 
-    # TODO users aren't going to match
-    if django_model is not OSFUser:
-        try:
-            assert len(django_ids) == len(django_objects) == len(page_of_modm_objects), 'Lost some keys along the way for {}.{}\n' \
-                                                                                    'Django_id count: {}\n' \
-                                                                                    'Django count: {}\n' \
-                                                                                    'Modm count: {}'.format(
-                django_model._meta.model.__module__,
-                django_model._meta.model.__name__,
-                len(django_ids),
-                len(django_objects),
-                len(page_of_modm_objects),
-            )
-        except AssertionError as ex:
-            logger.error(ex)
+        # TODO users aren't going to match
+        if django_model is not OSFUser:
+            try:
+                assert len(django_ids) == len(django_objects) == len(page_of_modm_objects), 'Lost some keys along the way for {}.{}\n' \
+                                                                                        'Django_id count: {}\n' \
+                                                                                        'Django count: {}\n' \
+                                                                                        'Modm count: {}'.format(
+                    django_model._meta.model.__module__,
+                    django_model._meta.model.__name__,
+                    len(django_ids),
+                    len(django_objects),
+                    len(page_of_modm_objects),
+                )
+            except AssertionError as ex:
+                logger.error(ex)
 
-    for modm_obj in page_of_modm_objects:
-        django_obj = django_objects.get(pk=get_pk(modm_obj, django_model, modm_to_django))
-        for m2m_field in m2m_relations:
-            validate_m2m_field(m2m_field.name, django_obj, modm_obj)
-        for fk_field in fk_relations:
-            validate_fk_relation(fk_field.name, django_obj, modm_obj)
-        for basic_field in basic_fields:
-            validate_basic_field(basic_field.name, django_obj, modm_obj)
+        for modm_obj in page_of_modm_objects:
+            try:
+                django_obj = django_objects.get(pk=get_pk(modm_obj, django_model, modm_to_django))
+            except ObjectDoesNotExist:
+                logger.error('Django object {} with id {} does not exist'.format(django_model, modm_obj._id))
+                continue
+            for m2m_field in m2m_relations:
+                validate_m2m_field(m2m_field.name, django_obj, modm_obj)
+            for fk_field in fk_relations:
+                validate_fk_relation(fk_field.name, django_obj, modm_obj)
+            for basic_field in basic_fields:
+                validate_basic_field(basic_field.name, django_obj, modm_obj)
 
-        count += 1
-    logger.info('Through {} {}.{}s...'.format(count, django_model._meta.model.__module__, django_model._meta.model.__name__))
+            count += 1
+        logger.info('Through {} {}.{}s...'.format(count, django_model._meta.model.__module__, django_model._meta.model.__name__))
+    except Exception as ex:
+        logger.error(
+            'Retrying: Failed to validate page model: {} offset:{} limit:{} of foreign keys with exception {}'.format(
+                django_model, offset, limit, ex))
+        logger.error('{} {}'.format(ex.__dict__, ex.__class__))
+        self.retry(countdown=20)  # retry in 20s
 
 
 def validate_model_data(django_model, page_size=20000):
