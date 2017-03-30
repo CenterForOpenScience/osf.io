4b730b0dc2816a7947fb1f537f24d47e69d1e334
erinspace <erin.braswell@gmail.com>
2017-03-30 15:30:12 -0400

    Add debug toolbar if in DEBUG mode

--- admin/base/settings/defaults.py
+++ admin/base/settings/defaults.py
@@ -233,3 +233,10 @@ DESK_KEY = ''
 DESK_KEY_SECRET = ''
 
 OSF_URL = 'http://osf.io'
+
+if DEBUG:
+    INSTALLED_APPS += ('debug_toolbar', )
+    MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware', )
+    DEBUG_TOOLBAR_CONFIG = {
+        'SHOW_TOOLBAR_CALLBACK': lambda(_): True
+    }
