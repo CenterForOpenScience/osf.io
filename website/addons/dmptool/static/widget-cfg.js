4190b9598b02729a034fd415a3680cce8b8af019
Raymond Yee <raymond.yee@gmail.com>
2017-04-01 18:25:39 -0700

    get rid of legacy js widget code

--- website/addons/dmptool/static/widget-cfg.js
+++
@@ -1,7 +0,0 @@
-var DmptoolWidget = require('./dmptoolWidget.js');
-
-var url = window.contextVars.node.urls.api + 'dmptool/widget/contents/';
-// #dmptoolScope will only be in the DOM if the addon is properly configured
-if ($('#dmptoolScope')[0]) {
-    new DmptoolWidget('#dmptoolScope', url);
-}
