9b2c5d6f5a786912ad1e5d081a4c342a48446480
Matt Frazier <maf7sm@virginia.edu>
2017-04-19 14:29:43 -0400

    Add reverse migrations

--- osf/migrations/0015_migrate_subject_parents_to_parent.py
+++ osf/migrations/0015_migrate_subject_parents_to_parent.py
@@ -18,6 +18,14 @@ def add_custom_mapping_constraint(state, schema):
         with connection.cursor() as cursor:
             cursor.execute(sql, [osf_id])
 
+def remove_custom_mapping_constraint(*args):
+    sql = """
+        ALTER TABLE osf_subject
+        DROP CONSTRAINT IF EXISTS customs_must_be_mapped RESTRICT;
+    """
+    with connection.cursor() as cursor:
+        cursor.execute(sql)
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -41,22 +49,29 @@ class Migration(migrations.Migration):
             field=models.ForeignKey(blank=True, null=True, on_delete=models.deletion.CASCADE, to='osf.Subject', related_name='aliases')
         ),
         migrations.RunSQL(
-            """
+            ["""
             UPDATE osf_subject
             SET provider_id = (SELECT id FROM osf_preprintprovider WHERE _id = 'osf');
-            """
+            """], ["""
+            UPDATE osf_subject
+            SET provider_id = NULL;
+            """]
         ),
         migrations.RunSQL(
-            """
+            ["""
             UPDATE osf_subject
             SET parent_id=subquery.to_subject_id
             FROM (SELECT from_subject_id, to_subject_id
                   FROM  osf_subject_parents) AS subquery
             WHERE osf_subject.id=subquery.from_subject_id;
-            """
+            """], ["""
+            INSERT INTO osf_subject_parents (from_subject_id, to_subject_id)
+            SELECT id, parent_id FROM osf_subject
+            WHERE parent_id IS NOT NULL;
+            """]
         ),
         migrations.RunPython(
-            add_custom_mapping_constraint
+            add_custom_mapping_constraint, remove_custom_mapping_constraint
         ),
         migrations.RemoveField(
             model_name='subject',
