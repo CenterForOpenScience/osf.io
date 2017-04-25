b8291dd5bee21ea2dfb77f0fc5b6756773eb5798
erinspace <erin.braswell@gmail.com>
2017-04-25 14:21:14 -0400

    Add more logging and asserts to script

--- scripts/add_identifiers_to_existing_preprints.py
+++ scripts/add_identifiers_to_existing_preprints.py
@@ -2,7 +2,7 @@ import logging
 import time
 
 from website.app import init_app
-from website.identifiers.utils import get_or_create_identifiers
+from website.identifiers.utils import get_or_create_identifiers, get_subdomain
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -14,12 +14,18 @@ def add_identifiers_to_preprints():
     preprints_without_identifiers = PreprintService.objects.filter(identifiers__isnull=True)
     logger.info('About to add identifiers to {} preprints.'.format(preprints_without_identifiers.count()))
 
-
     for preprint in preprints_without_identifiers:
         new_identifiers = get_or_create_identifiers(preprint)
-        logger.info('Saving identifier for preprint {}'.format(preprint.node.title))
+        logger.info('Saving identifier for preprint {} from source {}'.format(preprint.node.title, preprint.provider.name))
         preprint.set_preprint_identifiers(new_identifiers)
         preprint.save()
+
+        doi = preprint.get_identifier('doi')
+        subdomain = get_subdomain(preprint)
+        assert subdomain.upper() in doi.value
+        assert preprint._id.upper() in doi.value
+
+        logger.info('Created DOI {} for Preprint from service {}'.format(doi.value, preprint.provider.name))
         time.sleep(1)
 
     logger.info('Finished Adding identifiers to {} preprints.'.format(preprints_without_identifiers.count()))
