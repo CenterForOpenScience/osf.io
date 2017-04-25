721aedc1c6473d832fcf58ce8bf60752ebb98357
Barrett K. Harber <barrett.harber@gmail.com>
2017-04-25 19:14:58 -0400

    Update origins whitelist for preprint providers (CORS)

--- api/base/wsgi.py
+++ api/base/wsgi.py
@@ -58,6 +58,6 @@ Request.__getattribute__ = object.__getattribute__
 ############# /monkeys ####################
 
 init_app(set_backends=True, routes=False, attach_request_handlers=False)
-api_settings.load_institutions()
+api_settings.load_origins_whitelist()
 
 application = get_wsgi_application()
