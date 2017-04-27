d8b6b115c1491aa4da9aadee72abab378f86e279
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-04-27 09:56:35 -0400

    Add lawarxiv to list of prod preprint providers.

--- scripts/populate_preprint_providers.py
+++ scripts/populate_preprint_providers.py
@@ -20,7 +20,7 @@ logging.basicConfig(level=logging.INFO)
 ENVS = ['prod', 'stage']
 SUBJECTS_CACHE = {}
 STAGING_PREPRINT_PROVIDERS = ['osf', 'psyarxiv', 'engrxiv', 'socarxiv', 'scielo', 'agrixiv', 'bitss', 'lawarxiv']
-PROD_PREPRINT_PROVIDERS = ['osf', 'psyarxiv', 'engrxiv', 'socarxiv', 'agrixiv', 'bitss']
+PROD_PREPRINT_PROVIDERS = ['osf', 'psyarxiv', 'engrxiv', 'socarxiv', 'agrixiv', 'bitss', 'lawarxiv']
 
 
 def get_subject_id(name):
