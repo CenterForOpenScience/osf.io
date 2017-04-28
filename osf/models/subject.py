7ca07136ae2818ea645075dff402373763a53975
Matt Frazier <maf7sm@virginia.edu>
2017-04-28 15:36:15 -0400

    Update uniqueness constraint to be provider-specific

--- osf/models/subject.py
+++ osf/models/subject.py
@@ -18,12 +18,15 @@ class SubjectQuerySet(MODMCompatibilityQuerySet, IncludeQuerySet):
 class Subject(ObjectIDMixin, BaseModel):
     """A subject discipline that may be attached to a preprint."""
 
-    text = models.CharField(null=False, max_length=256, unique=True)  # max length on prod: 73
+    text = models.CharField(null=False, max_length=256)  # max length on prod: 73
     parent = models.ForeignKey('self', related_name='children', null=True, blank=True, on_delete=models.SET_NULL, validators=[validate_subject_hierarchy_length])
     bepress_subject = models.ForeignKey('self', related_name='aliases', null=True, blank=True, on_delete=models.deletion.CASCADE)
     provider = models.ForeignKey(PreprintProvider, related_name='subjects', on_delete=models.deletion.CASCADE)
 
     objects = SubjectQuerySet.as_manager()
+
+    class Meta:
+        unique_together = ('text', 'provider')
 
     def __unicode__(self):
         return '{} with id {}'.format(self.text, self.id)
