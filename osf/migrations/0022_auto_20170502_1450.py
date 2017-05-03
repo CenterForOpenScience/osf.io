314a26ffa2604cd308c1f8a2d4559983e3074e84
Henrique Harman <henriqueharman@gmail.com>
2017-05-03 13:57:39 -0400

    Rename migration to be 22

--- osf/migrations/0022_auto_20170502_1450.py
+++ osf/migrations/0022_auto_20170502_1450.py
@@ -9,7 +9,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('osf', '0022_backfill_date_retracted'),
+        ('osf', '0021_retraction_date_retracted'),
     ]
 
     operations = [
