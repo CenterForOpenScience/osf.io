f7466715ce7531b29d4bc2eb0f7f467b59d439d0
yacchin1205 <yazawa@yzwlab.net>
2017-05-15 16:26:16 +0900

    Fix conflicts of migrations

--- osf/migrations/0030_add_swift_addon.py
+++ osf/migrations/0030_add_swift_addon.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('osf', '0023_merge_20170503_1947'),
+        ('osf', '0029_auto_20170511_1553'),
     ]
 
     operations = [
