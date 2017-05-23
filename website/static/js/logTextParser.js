c1eeec3cfc16f6100319948c5d8c75d397048f71
Nan Chen <nanchen@Admins-MacBook-Pro.local>
2017-05-23 11:55:05 -0400

    improve the coding

--- website/static/js/logTextParser.js
+++ website/static/js/logTextParser.js
@@ -446,10 +446,8 @@ var LogPieces = {
                     'github_file_added', 'github_file_updated', 'box_file_added', 'box_file_updated', 'dropbox_file_added', 'dropbox_file_updated',
                     's3_file_added', 's3_file_updated', 'figshare_file_added', 'checked_in', 'checked_out'];
                     if (acceptableLinkedItems.indexOf(action) !== -1 && logObject.attributes.params.urls) {
-                        if (lodashGet(logObject,'attributes.params.params_file', null)){
-                            return logObject.attributes.params.params_file;
-                        }
-                        return logObject.attributes.params.urls.view;
+                       var viewUrl = lodashGet(logObject, 'attributes.params.urls.view', null);
+                       return lodashGet(logObject, 'attributes.params.params_file', viewURL)
                     }
                 }
                 return null;
