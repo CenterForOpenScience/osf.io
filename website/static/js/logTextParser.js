07568938d346bd93d5cb95987bff3995d81d08cc
Nan Chen <nanchen@Admins-MacBook-Pro.local>
2017-05-23 12:21:11 -0400

    fix flake8

--- website/static/js/logTextParser.js
+++ website/static/js/logTextParser.js
@@ -447,7 +447,7 @@ var LogPieces = {
                     's3_file_added', 's3_file_updated', 'figshare_file_added', 'checked_in', 'checked_out'];
                     if (acceptableLinkedItems.indexOf(action) !== -1 && logObject.attributes.params.urls) {
                        var viewUrl = lodashGet(logObject, 'attributes.params.urls.view', null);
-                       return lodashGet(logObject, 'attributes.params.params_file', viewURL)
+                       return lodashGet(logObject, 'attributes.params.params_file', viewURL);
                     }
                 }
                 return null;
