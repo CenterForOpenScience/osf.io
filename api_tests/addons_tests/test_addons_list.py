fdd095004928763877138df1199568399d9a9f7c
Fabrice Mizero <fabmizz@gmail.com>
2017-07-17 19:50:33 -0400

    Convert api_tests addons list to pytest

--- api_tests/addons_tests/test_addons_list.py
+++ api_tests/addons_tests/test_addons_list.py
@@ -1,24 +1,19 @@
-from nose.tools import *  # flake8: noqa
-
+import pytest
 from api.base.settings.defaults import API_BASE
 
-from tests.base import ApiTestCase
+@pytest.mark.django_db
+class TestAddonsList:
 
-class TestAddonsList(ApiTestCase):
+    def test_filter_by_category(self, app):
+        url = '/{}addons/'.format(API_BASE)
+        storage_url = '{}?filter[categories]=storage'.format(url)
+        citations_url = '{}?filter[categories]=citations'.format(url)
 
-    def setUp(self):
-        super(TestAddonsList, self).setUp()
-        self.url = '/{}addons/'.format(API_BASE)
-
-    def test_filter_by_category(self):
-        storage_url = '{}?filter[categories]=storage'.format(self.url)
-        citations_url = '{}?filter[categories]=citations'.format(self.url)
-
-        storage_data = self.app.get(storage_url).json['data']
-        citations_data = self.app.get(citations_url).json['data']
+        storage_data = app.get(storage_url).json['data']
+        citations_data = app.get(citations_url).json['data']
 
         for addon in storage_data:
-            assert_in('storage', addon['attributes']['categories'])
+            assert 'storage' in addon['attributes']['categories']
 
         for addon in citations_data:
-            assert_in('citations', addon['attributes']['categories'])
+            assert 'citations' in addon['attributes']['categories']
