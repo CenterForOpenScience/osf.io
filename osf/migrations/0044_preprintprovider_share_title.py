9264da5084b88325410803fcc1695a2810334b07
Henrique Harman <henriqueharman@gmail.com>
2017-07-21 13:34:42 -0400

    Resolve migration conflicts again

--- osf/migrations/0044_preprintprovider_share_title.py
+++ osf/migrations/0044_preprintprovider_share_title.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('osf', '0041_auto_20170706_1024'),
+        ('osf', '0043_auto_20170626_0912'),
     ]
 
     operations = [
