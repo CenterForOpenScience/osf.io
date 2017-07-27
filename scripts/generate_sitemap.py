7bc03eef972ac52b7e2a7d7557578d62021e1eaf
Yuhuai Liu <yuhuailiu@Admins-MacBook-Pro.local>
2017-07-27 08:57:04 -0400

    remove trailing slashes for user links

--- scripts/generate_sitemap.py
+++ scripts/generate_sitemap.py
@@ -161,7 +161,7 @@ class Sitemap(object):
         for obj in objs:
             try:
                 config = settings.SITEMAP_USER_CONFIG
-                config['loc'] = urlparse.urljoin(settings.DOMAIN, '/{}/'.format(obj))
+                config['loc'] = urlparse.urljoin(settings.DOMAIN, '/{}'.format(obj))
                 self.add_url(config)
             except Exception as e:
                 self.log_errors('USER', obj, e)
