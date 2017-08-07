277aa3e1ff9e046e31280f4d2bd830b430263a00
Nan Chen <nanchen@Admins-MacBook-Pro-6.local>
2017-08-07 14:37:49 -0400

    fix flake 8

--- website/mailchimp_utils.py
+++ website/mailchimp_utils.py
@@ -76,7 +76,6 @@ def unsubscribe_mailchimp(list_name, user_id, username=None, send_goodbye=True):
     user = OSFUser.load(user_id)
     m = get_mailchimp_api()
     list_id = get_list_id_from_name(list_name=list_name)
-    
     try:
         m.lists.unsubscribe(
             id=list_id, email={'email': username or user.username},
