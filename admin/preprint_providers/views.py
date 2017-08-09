a501ed4d658a8bd8574f2cec1a571934839740cf
Lauren Revere <laurenrevere@cos.io>
2017-08-09 14:40:38 -0400

    Remove import/export of all subjects

--- admin/preprint_providers/views.py
+++ admin/preprint_providers/views.py
@@ -18,7 +18,8 @@ from osf.models.preprint_provider import rules_to_subjects
 
 # When preprint_providers exclusively use Subject relations for creation, set this to False
 SHOW_TAXONOMIES_IN_PREPRINT_PROVIDER_CREATE = True
-FIELDS_TO_NOT_IMPORT_EXPORT = ['access_token', 'share_source', 'subjects_acceptable']
+#TODO: Add subjects back in when custom taxonomies are fully integrated
+FIELDS_TO_NOT_IMPORT_EXPORT = ['access_token', 'share_source', 'subjects_acceptable', 'subjects']
 
 
 class PreprintProviderList(PermissionRequiredMixin, ListView):
