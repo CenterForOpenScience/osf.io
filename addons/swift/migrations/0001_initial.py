266f859ef49636dd5b638bedda59c483775bceae
satoshi-yazawa-nii <satoshi-yazawa@nii.ac.jp>
2017-09-12 10:51:58 +0900

    Fix dependencies of migrations

--- addons/swift/migrations/0001_initial.py
+++ addons/swift/migrations/0001_initial.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('osf', '0030_add_swift_addon'),
+        ('osf', '0044_add_swift_addon'),
     ]
 
     operations = [
