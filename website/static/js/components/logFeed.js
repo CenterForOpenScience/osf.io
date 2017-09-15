17c561d144a1eee5806cbb49a28709fc0fd2e4e6
Matt Frazier <maf7sm@virginia.edu>
2017-09-15 13:25:59 -0400

    Update logFeed.js for cursor pagination

--- website/static/js/components/logFeed.js
+++ website/static/js/components/logFeed.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var $ = require('jquery');
 require('css/log-feed.css');
 var m = require('mithril'); // exposes mithril methods, useful for redraw etc.
 var $osf = require('js/osfHelpers');
@@ -14,10 +15,9 @@ var LOG_PAGE_SIZE = 6;
 var PROFILE_IMAGE_SIZE = 16;
 
 var _buildLogUrl = function(node, page, limitLogs) {
-    var logPage = page || 1;
     var urlPrefix = (node.isRegistration || node.is_registration) ? 'registrations' : 'nodes';
     var size = limitLogs ? LOG_PAGE_SIZE_LIMITED : LOG_PAGE_SIZE;
-    var query = { 'page[size]': size, 'page': logPage, 'embed': ['original_node', 'user', 'linked_node', 'linked_registration', 'template_node'], 'profile_image_size': PROFILE_IMAGE_SIZE};
+    var query = { 'page[size]': size, 'embed': ['original_node', 'user', 'linked_node', 'linked_registration', 'template_node'], 'profile_image_size': PROFILE_IMAGE_SIZE};
     var viewOnly = $osf.urlParams().view_only;
     if (viewOnly) {
         query.view_only = viewOnly;
@@ -37,9 +37,8 @@ var LogFeed = {
         self.paginators = m.prop([]);
         self.nextPage = m.prop('');
         self.prevPage = m.prop('');
-        self.totalPages = m.prop(0);
-        self.currentPage = m.prop(0);
-        self.pageToGet = m.prop(0);
+        self.firstPage = m.prop('');
+        self.lastPage = m.prop('');
 
         self.getLogs = function _getLogs (url) {
             self.activityLogs([]); // Empty logs from other projects while load is happening;
@@ -51,11 +50,11 @@ var LogFeed = {
                 self.activityLogs(result.data);  // Set activity log data
                 self.nextPage(result.links.next);
                 self.prevPage(result.links.prev);
+                self.firstPage(result.links.first);
+                self.lastPage(result.links.last);
 
                 var params = $osf.urlParams(url);
-                var page = params.page || 1;
-                self.currentPage(parseInt(page));
-                self.totalPages(Math.ceil(result.links.meta.total / result.links.meta.per_page));
+                m.redraw();
             }
             self.logRequestPending(true);
             var promise = m.request({method : 'GET', url : url, config: mHelpers.apiV2Config({withCredentials: window.contextVars.isOnRootDomain})});
@@ -63,10 +62,12 @@ var LogFeed = {
                 function(result) {
                     _processResults(result);
                     self.logRequestPending(false);
+                    $('#logFeed').removeClass('db-loading-list');
                     return promise;
                 }, function(xhr, textStatus, error) {
                     self.failed = true;
                     self.logRequestPending(false);
+                    $('#logFeed').removeClass('db-loading-list');
                     Raven.captureMessage('Error retrieving logs', {extra: {url: url, textStatus: textStatus, error: error}});
                 }
             );
@@ -85,112 +86,27 @@ var LogFeed = {
 
         var i;
         ctrl.paginators([]);
-        if (ctrl.totalPages() > 1 && !ctrl.limitLogs) {
+        if (!ctrl.limitLogs) {
+            // first page
+            ctrl.paginators().push({
+                url: function() { return ctrl.firstPage(); },
+                text: '|<<'
+            });
             // previous page
             ctrl.paginators().push({
                 url: function() { return ctrl.prevPage(); },
                 text: '<'
             });
-            // first page
-            ctrl.paginators().push({
-                text: 1,
-                url: function() {
-                    ctrl.pageToGet(1);
-                    if(ctrl.pageToGet() !== ctrl.currentPage()) {
-                        return _buildLogUrl(ctrl.node, ctrl.pageToGet());
-                    }
-                }
-            });
-            // no ellipses
-            if (ctrl.totalPages() <= MAX_PAGES_ON_PAGINATOR) {
-                for (i = 2; i < ctrl.totalPages(); i++) {
-                    ctrl.paginators().push({
-                        text: i,
-                        url: function() {
-                            ctrl.pageToGet(parseInt(this.text));
-                            if (ctrl.pageToGet() !== ctrl.currentPage()) {
-                                return _buildLogUrl(ctrl.node, ctrl.pageToGet());
-                            }
-                        }
-                    });/* jshint ignore:line */
-                    // function defined inside loop
-                }
-            }
-            // one ellipse at the end
-            else if (ctrl.currentPage() < MAX_PAGES_ON_PAGINATOR_SIDE - 1) {
-                for (i = 2; i < MAX_PAGES_ON_PAGINATOR_SIDE; i++) {
-                    ctrl.paginators().push({
-                        text: i,
-                        url: function() {
-                            ctrl.pageToGet(parseInt(this.text));
-                            if (ctrl.pageToGet() !== ctrl.currentPage()) {
-                                return _buildLogUrl(ctrl.node, ctrl.pageToGet());
-                            }
-                        }
-                    });/* jshint ignore:line */
-                    // function defined inside loop
-                }
-                ctrl.paginators().push({
-                    text: '...',
-                    url: function() { }
-                });
-            }
-            // one ellipse at the beginning
-            else if (ctrl.currentPage() > ctrl.totalPages() - MAX_PAGES_ON_PAGINATOR_SIDE + 2) {
-                ctrl.paginators().push({
-                    text: '...',
-                    url: function() { }
-                });
-                for (i = ctrl.totalPages() - MAX_PAGES_ON_PAGINATOR_SIDE + 2; i <= ctrl.totalPages() - 1; i++) {
-                    ctrl.paginators().push({
-                        text: i,
-                        url: function() {
-                            ctrl.pageToGet(parseInt(this.text));
-                            if (ctrl.pageToGet() !== ctrl.currentPage()) {
-                                return _buildLogUrl(ctrl.node, ctrl.pageToGet());
-                            }
-                        }
-                    });/* jshint ignore:line */
-                    // function defined inside loop
-                }
-            }
-            // two ellipses
-            else {
-                ctrl.paginators().push({
-                    text: '...',
-                    url: function() { }
-                });
-                for (i = parseInt(ctrl.currentPage()) - 1; i <= parseInt(ctrl.currentPage()) + 1; i++) {
-                    ctrl.paginators().push({
-                        text: i,
-                        url: function() {
-                            ctrl.pageToGet(parseInt(this.text));
-                            if (ctrl.pageToGet() !== ctrl.currentPage()) {
-                                return _buildLogUrl(ctrl.node, ctrl.pageToGet());
-                            }
-                        }
-                    });/* jshint ignore:line */
-                    // function defined inside loop
-                }
-                ctrl.paginators().push({
-                    text: '...',
-                    url: function() { }
-                });
-            }
-            // last page
-            ctrl.paginators().push({
-                text: ctrl.totalPages(),
-                url: function() {
-                    ctrl.pageToGet(ctrl.totalPages());
-                    if (ctrl.pageToGet() !== ctrl.currentPage()) {
-                        return _buildLogUrl(ctrl.node, ctrl.pageToGet());
-                    }
-                }
-            });
+            // TODO DatePicker
             // next page
             ctrl.paginators().push({
                 url: function() { return ctrl.nextPage(); },
                 text: '>'
+            });
+            // last page
+            ctrl.paginators().push({
+                url: function() { return ctrl.lastPage(); },
+                text: '>>|'
             });
         }
 
@@ -224,6 +140,10 @@ var LogFeed = {
             m('.db-activity-nav.text-center', [
                 ctrl.paginators() && !ctrl.limitLogs ? ctrl.paginators().map(function(page) {
                     return page.url() ? m('.btn.btn-sm.btn-link', { onclick : function() {
+                        ctrl.logRequestPending(true);
+                        $('#logFeed').addClass('db-loading-list');
+                        $('#logFeed .btn').addClass('disabled');
+                        $('#logFeed a').addClass('disabled');
                         ctrl.getLogs(page.url());
                     }}, page.text) : m('.btn.btn-sm.btn-link.disabled', {style: 'color: black'}, page.text);
                 }) : ''
