4166b0659aa661717f433db40d50cd348a68bafe
John Tordoff <john@cos.io>
2017-09-15 11:08:52 -0400

    Add unit tests to confirm HAM

--- admin_tests/nodes/test_views.py
+++ admin_tests/nodes/test_views.py
@@ -10,6 +10,7 @@ from admin.nodes.views import (
     NodeFlaggedSpamList,
     NodeKnownSpamList,
     NodeKnownHamList,
+    NodeConfirmHamView
 )
 from admin_tests.utilities import setup_log_view, setup_view
 
@@ -314,3 +315,31 @@ class TestNodeReindex(AdminTestCase):
         nt.assert_true(mock_update_search.called)
         nt.assert_true(mock_bulk_update_nodes.called)
         nt.assert_equal(AdminLogEntry.objects.count(), count + 1)
+
+class TestNodeConfirmHamView(AdminTestCase):
+    def setUp(self):
+        super(TestNodeConfirmHamView, self).setUp()
+
+        self.request = RequestFactory().post('/fake_path')
+        self.user = AuthUserFactory()
+
+        self.node = ProjectFactory(creator=self.user)
+        self.registration = RegistrationFactory(creator=self.user)
+
+    def test_confirm_node_as_ham(self):
+        view = NodeConfirmHamView()
+        view = setup_log_view(view, self.request, guid=self.node._id)
+        view.delete(self.request)
+
+        updated_node = view.get_object()
+        nt.assert_equal(updated_node, self.node)
+        nt.assert_true(updated_node.spam_status == 4)
+
+    def test_confirm_registration_as_ham(self):
+        view = NodeConfirmHamView()
+        view = setup_log_view(view, self.request, guid=self.registration._id)
+        view.delete(self.request)
+
+        updated_registration = view.get_object()
+        nt.assert_equal(updated_registration, self.registration)
+        nt.assert_true(updated_registration.spam_status == 4)
