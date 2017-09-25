a78099919dce628655d4beef8246f50b5ab55441
Yuhuai Liu <yuhuai@cos.io>
2017-09-25 17:37:00 -0400

    Remove line

--- website/static/js/tests/tests.webpack.js
+++ website/static/js/tests/tests.webpack.js
@@ -12,4 +12,3 @@ context.keys().forEach(context);
 // Include all files in the addons directory that end with .test.js
 var addonContext = require.context('../../../../addons/', true, /.*test\.js$/); //make sure you have your directory and regex test set correctly!
 addonContext.keys().forEach(addonContext);
-
