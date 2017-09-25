c33d55967dce73454ac5c0998455d8fb06c2bd89
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-09-25 17:15:26 -0500

    Remove default flag/switch values from serializers.

--- api/waffle_flags/serializers.py
+++ api/waffle_flags/serializers.py
@@ -9,19 +9,15 @@ class WaffleFlagSerializer(JSONAPISerializer):
     id = ser.CharField(source='name', required=True, help_text='The id of the flag, which is the same as the name')
     type = TypeField()
     name = ser.CharField(required=True, help_text='The name of the Flag')
-    flag_default = ser.SerializerMethodField(help_text='Flag defaults for all flags')
     active = ser.SerializerMethodField()
     note = ser.CharField(required=False, allow_blank=True, help_text='Describe where the Flag is used.')
 
     def get_active(self, obj):
         """
-        A flag is active if any of the criteria are true for the current user or request 
+        A flag is active if A Flag will be active if any of the criteria are true for the current user or request
         """
         request = self.context.get("request")
         return flag_is_active(request, obj)
-
-    def get_flag_default(self, obj):
-        return get_setting('FLAG_DEFAULT')
 
     class Meta:
         type_ = 'waffle-flag',
--- api/waffle_switches/serializers.py
+++ api/waffle_switches/serializers.py
@@ -10,10 +10,6 @@ class WaffleSwitchSerializer(JSONAPISerializer):
     name = ser.CharField(required=True, help_text='The name of the Switch')
     note = ser.CharField(required=False, allow_blank=True, help_text='Describe where the Switch is used.')
     active = ser.BooleanField(read_only=True, help_text='Is the Switch active or inactive.')
-    switch_default = ser.SerializerMethodField(help_text='Flag defaults for all flags')
-
-    def get_switch_default(self, obj):
-        return get_setting('SWITCH_DEFAULT')
 
     class Meta:
         type_ = 'waffle-switch',
