591f79e555c641d595b5be8f2dc716dfd9e43bb4
Henrique Harman <henriqueharman@gmail.com>
2017-09-26 11:17:20 -0400

    Remove unused quickfiles_dir references

--- website/quickfiles/views.py
+++ website/quickfiles/views.py
@@ -5,8 +5,5 @@ from website.settings import EXTERNAL_EMBER_APPS
 
 quickfiles_dir = os.path.abspath(os.path.join(os.getcwd(), EXTERNAL_EMBER_APPS['quickfiles']['path']))
 
-def quickfiles_landing_page(**kwargs):
-    return {}
-
 def use_ember_app(**kwargs):
     return send_from_directory(quickfiles_dir, 'index.html')
