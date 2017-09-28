1164ed9e8b676d632b9e7ae141ec0a48673bb0a0
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-09-28 18:44:28 -0500

    Remove unneeded double underscores, and use id from auth_user.

--- api/preprint_providers/views.py
+++ api/preprint_providers/views.py
@@ -206,7 +206,7 @@ class PreprintProviderPreprintList(JSONAPIBaseView, generics.ListAPIView, Prepri
 
         if auth_user:
             contrib_user_query = Q(is_published=True, node__contributor__user_id=auth_user.id, node__contributor__read=True)
-            sub_qs = Contributor.objects.filter(node=OuterRef('pk'), user__id=auth.user.id, admin=True)
+            sub_qs = Contributor.objects.filter(node=OuterRef('pk'), user_id=auth_user.id, admin=True)
             return default_qs.annotate(admin_user=Exists(sub_qs)).filter(Q(admin_user=True) | contrib_user_query | no_user_query )
         return default_qs.filter(no_user_query)
 
