08ebf297f2e15610cea21784763065441bc6e190
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-10-04 15:45:17 -0500

    Add tests for copying files to other nodes.

--- api_tests/nodes/views/test_node_provider_file_metadata_create.py
+++ api_tests/nodes/views/test_node_provider_file_metadata_create.py
@@ -10,6 +10,7 @@ from osf_tests.factories import (
     ProjectFactory,
     UserFactory,
 )
+from website.util import permissions
 
 
 @pytest.fixture()
@@ -25,12 +26,20 @@ class TestNodeProviderFileMetadataCreateView:
         return ProjectFactory(creator=user)
 
     @pytest.fixture()
+    def node2(self, user):
+        return ProjectFactory(creator=user)
+
+    @pytest.fixture()
     def file(self, user, node):
         return api_utils.create_test_file(node, user, create_guid=False)
 
     @pytest.fixture()
     def root(self, node):
         return node.get_addon('osfstorage').get_root()
+
+    @pytest.fixture()
+    def node2_root(self, node2):
+        return node2.get_addon('osfstorage').get_root()
 
     @pytest.fixture()
     def folder(self, root):
@@ -78,76 +87,114 @@ class TestNodeProviderFileMetadataCreateView:
         res = app.post_json_api(create_metadata_url, move_payload, auth=non_contributor.auth, expect_errors=True)
         assert res.status_code == 403
 
-    def test_move_file_from_root_to_subfolder(self, app, user, create_metadata_url, move_payload, file, folder, root):
+    def test_move_file_from_root_to_subfolder_and_back(self, app, user, create_metadata_url, move_payload, file, folder, root):
         assert len(folder.children) == 0
         assert len(root.children) == 2
-        assert file.parent._id == root._id
+
         res = app.post_json_api(create_metadata_url, move_payload, auth=user.auth)
         assert len(folder.children) == 1
         assert len(root.children) == 1
-        assert res.status_code == 201
+        assert res.status_code == 200
         assert res.json['data']['id'] == file._id
         assert res.json['data']['attributes']['name'] == file.name
+
+        move_payload['data']['attributes']['destination_parent'] = None
+        res = app.post_json_api(create_metadata_url, move_payload, auth=user.auth)
+        assert res.status_code == 200
+        assert len(folder.children) == 0
+        assert len(root.children) == 2
 
     def test_copy_file_from_root_to_subfolder(self, app, user, create_metadata_url, copy_payload, file, folder, root):
         assert len(folder.children) == 0
         assert len(root.children) == 2
         res = app.post_json_api(create_metadata_url, copy_payload, auth=user.auth)
+        assert res.status_code == 201
         assert len(folder.children) == 1
         assert len(root.children) == 2
         assert res.json['data']['id'] == folder.children[0]._id
         assert res.json['data']['attributes']['name'] == file.name
 
-    def test_move_file_from_subfolder_to_root(self, app, user, create_metadata_url, copy_payload, file, folder, root):
-        pass
+    def test_copy_file_from_subfolder_to_root(self, app, user, create_metadata_url, copy_payload, folder, root):
+        file2 = folder.append_file("Second file")
+        assert len(folder.children) == 1
+        assert len(root.children) == 2
 
-    def test_copy_file_from_subfolder_to_root(self, app, user, create_metadata_url, copy_payload, file, folder, root):
-        pass
+        copy_payload['data']['attributes']['destination_parent'] = None
+        copy_payload['data']['attributes']['source'] = file2._id
+        res = app.post_json_api(create_metadata_url, copy_payload, auth=user.auth)
+        assert res.status_code == 201
 
-    def test_copy_file_to_another_node(self, app, user, create_metadata_url, copy_payload, file, folder, root):
-        pass
+        assert len(folder.children) == 1
+        assert len(root.children) == 3
+        assert res.json['data']['id'] != file2._id
+        assert res.json['data']['attributes']['name'] == file2.name
 
-    def test_move_file_to_another_node(self, app, user, create_metadata_url, copy_payload, file, folder, root):
-        pass
+    def test_copy_file_to_another_node(self, app, user, create_metadata_url, copy_payload, root, node2_root, node2):
+        assert len(node2_root.children) == 0
+        assert len(root.children) == 2
+        copy_payload['data']['attributes']['destination_node'] = node2._id
+        copy_payload['data']['attributes']['destination_parent'] = None
 
-    def test_copy_file_and_change_name(self, app, user, create_metadata_url, copy_payload, file, folder, root):
-        pass
+        res = app.post_json_api(create_metadata_url, copy_payload, auth=user.auth)
+        assert res.status_code == 201
+        assert len(node2_root.children) == 1
+        assert len(root.children) == 2
 
-    def test_move_file_and_change_name(self, app, user, create_metadata_url, copy_payload, file, folder, root):
-        pass
+    def test_copy_file_to_another_node_without_permissions(self, app, user, create_metadata_url, copy_payload, root, node2_root, node, node2):
+        user2 = AuthUserFactory()
+        node.add_contributor(user2, permissions=permissions.DEFAULT_CONTRIBUTOR_PERMISSIONS, save=True)
 
-    def test_move_file_in_quickfiles_node(self, app, user, create_metadata_url, copy_payload, file, folder, root):
-        pass
+        assert len(node2_root.children) == 0
+        assert len(root.children) == 2
+        copy_payload['data']['attributes']['destination_node'] = node2._id
+        copy_payload['data']['attributes']['destination_parent'] = None
 
-    def test_copy_file_in_quickfiles_node(self, app, user, create_metadata_url, copy_payload, file, folder, root):
-        pass
+        res = app.post_json_api(create_metadata_url, copy_payload, auth=user2.auth, expect_errors=True)
+        assert res.status_code == 403
+        assert len(node2_root.children) == 0
+        assert len(root.children) == 2
 
-    def test_move_file_into_location_where_it_already_exists(self, app, user, create_metadata_url, copy_payload, file, folder, root):
-        pass
-
-    def test_copy_file_into_location_where_it_already_exists(self, app, user, create_metadata_url, copy_payload, file, folder, root):
-        pass
-
-    def test_move_primary_preprint_file(self, app, user, create_metadata_url, copy_payload, file, folder, root):
-        pass
-
-    def test_move_checked_out_file(self, app, user, create_metadata_url, copy_payload, file, folder, root):
-        pass
-
-    def test_invalid_action(self, app, user, create_metadata_url, copy_payload, file, folder, root):
-        pass
-
-    def test_invalid_node(self, app, user, create_metadata_url, copy_payload, file, folder, root):
-        pass
-
-    def test_invalid_source(self, app, user, create_metadata_url, copy_payload, file, folder, root):
-        pass
-
-    def test_invalid_destination_node(self, app, user, create_metadata_url, copy_payload, file, folder, root):
-        pass
-
-    def test_move_file_out_of_registration(self, app, user, create_metadata_url, copy_payload, file, folder, root):
-        pass
-
-    def test_copy_file_out_of_registration(self, app, user, create_metadata_url, copy_payload, file, folder, root):
-        pass
+    # def test_move_file_to_another_node(self, app, user, create_metadata_url, move_payload, file, folder, root):
+    #     pass
+    #
+    # def test_copy_file_and_change_name(self, app, user, create_metadata_url, copy_payload, file, folder, root):
+    #     pass
+    #
+    # def test_move_file_and_change_name(self, app, user, create_metadata_url, copy_payload, file, folder, root):
+    #     pass
+    #
+    # def test_move_file_in_quickfiles_node(self, app, user, create_metadata_url, copy_payload, file, folder, root):
+    #     pass
+    #
+    # def test_copy_file_in_quickfiles_node(self, app, user, create_metadata_url, copy_payload, file, folder, root):
+    #     pass
+    #
+    # def test_move_file_into_location_where_it_already_exists(self, app, user, create_metadata_url, copy_payload, file, folder, root):
+    #     pass
+    #
+    # def test_copy_file_into_location_where_it_already_exists(self, app, user, create_metadata_url, copy_payload, file, folder, root):
+    #     pass
+    #
+    # def test_move_primary_preprint_file(self, app, user, create_metadata_url, copy_payload, file, folder, root):
+    #     pass
+    #
+    # def test_move_checked_out_file(self, app, user, create_metadata_url, copy_payload, file, folder, root):
+    #     pass
+    #
+    # def test_invalid_action(self, app, user, create_metadata_url, copy_payload, file, folder, root):
+    #     pass
+    #
+    # def test_invalid_node(self, app, user, create_metadata_url, copy_payload, file, folder, root):
+    #     pass
+    #
+    # def test_invalid_source(self, app, user, create_metadata_url, copy_payload, file, folder, root):
+    #     pass
+    #
+    # def test_invalid_destination_node(self, app, user, create_metadata_url, copy_payload, file, folder, root):
+    #     pass
+    #
+    # def test_move_file_out_of_registration(self, app, user, create_metadata_url, copy_payload, file, folder, root):
+    #     pass
+    #
+    # def test_copy_file_out_of_registration(self, app, user, create_metadata_url, copy_payload, file, folder, root):
+    #     pass
