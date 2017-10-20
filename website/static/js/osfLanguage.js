547246e1825435c9ec0ae35fcc824382a96110f8
chennan47 <nc6r@virginia.edu>
2017-10-20 13:31:56 -0400

    fix js test

--- website/static/js/osfLanguage.js
+++ website/static/js/osfLanguage.js
@@ -1,4 +1,6 @@
-var OSF_SUPPORT_EMAIL = window.contextVars.osfSupportEmail;
+var $osf = require('js/osfHelpers');
+
+var OSF_SUPPORT_EMAIL = $osf.osfSupportEmail();
 var SUPPORT_LINK = '<a href="mailto:' + OSF_SUPPORT_EMAIL + '">' + OSF_SUPPORT_EMAIL +'</a>';
 
 var REFRESH_OR_SUPPORT = 'Please refresh the page and try again or contact ' + SUPPORT_LINK + ' if the problem persists.';
