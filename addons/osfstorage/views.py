2dd5b35607463178d558922676d018d7feaa4fa9
erinspace <erin.braswell@gmail.com>
2017-10-25 15:38:14 -0400

    Revert "Add checks, exceptions, and tests to prevent moving/copying quickfiles"
    [#OSF-8841]
    
    This reverts commit 45faea194b1c39243ecb17508f1cfc4a77e3879f.

--- addons/osfstorage/views.py
+++ addons/osfstorage/views.py
@@ -85,12 +85,7 @@ def osfstorage_get_revisions(file_node, node_addon, payload, **kwargs):
 
 @decorators.waterbutler_opt_hook
 def osfstorage_copy_hook(source, destination, name=None, **kwargs):
-    try:
-        return source.copy_under(destination, name=name).serialize(), httplib.CREATED
-    except exceptions.FileNodeIsQuickFilesNode:
-        raise HTTPError(httplib.BAD_REQUEST, data={
-            'message_long': 'Cannot copy file as it is in a quickfiles node'
-        })
+    return source.copy_under(destination, name=name).serialize(), httplib.CREATED
 
 
 @decorators.waterbutler_opt_hook
@@ -104,10 +99,6 @@ def osfstorage_move_hook(source, destination, name=None, **kwargs):
     except exceptions.FileNodeIsPrimaryFile:
         raise HTTPError(httplib.FORBIDDEN, data={
             'message_long': 'Cannot move file as it is the primary file of preprint.'
-        })
-    except exceptions.FileNodeIsQuickFilesNode:
-        raise HTTPError(httplib.BAD_REQUEST, data={
-            'message_long': 'Cannot move file as it is in a quickfiles node'
         })
 
 
