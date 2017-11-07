b733aebfeb7336e1dea62f4f4fc72e2e2df09e3d
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-11-07 14:59:38 -0600

    Concat LoaderOptionsPlugin to other webpack.common.config plugins instead of overwriting.

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,10 +2,16 @@ var webpack = require('webpack');
 var common = require('./webpack.common.config.js');
 var assign = require('object-assign');
 
-module.exports = assign(common, {
-    plugins: [
-        new webpack.LoaderOptionsPlugin({
-            debug: true
-        })
-    ]
+
+// Adding LoaderOptionsPlugin to plugins
+var plugins = common.plugins.concat([
+    new webpack.LoaderOptionsPlugin({
+        debug: true,
+    })
+]);
+
+var config = assign(common, {
+    plugins: plugins
 });
+
+module.exports = config;
