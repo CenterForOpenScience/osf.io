d743a1da471a96c5762771b7044d818b092c7f33
Casey Rollins <case.rollins@gmail.com>
2017-11-09 16:30:26 -0500

    Specify app_name in included modules.
    - Addresses RemovedInDjango20Warnings

--- admin/users/urls.py
+++ admin/users/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'admin'
+
 urlpatterns = [
     url(r'^$', views.UserFormView.as_view(), name='search'),
     url(r'^flagged_spam$', views.UserFlaggedSpamList.as_view(), name='flagged-spam'),
