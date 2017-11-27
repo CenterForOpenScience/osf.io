c8cbd204bdf1cddce523d6aa08fb7f36cfe4dc3f
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-11-27 12:59:18 -0600

    Update field name documentation on preprints views to replace obsolete field.

--- api/preprints/views.py
+++ api/preprints/views.py
@@ -78,7 +78,7 @@ class PreprintList(JSONAPIBaseView, generics.ListCreateAPIView, PreprintFilterMi
         is_preprint_orphan              boolean                             whether or not this preprint is orphaned
         subjects                        list of lists of dictionaries       ids of Subject in the BePress taxonomy. Dictionary, containing the subject text and subject ID
         doi                             string                              bare DOI for the manuscript, as entered by the user
-        preprint_doi_on_datacite        boolean                             whether or not the preprint doi has been added on datacite
+        preprint_doi_created            iso8601 timestamp                   timestamp that the preprint doi was created
 
     ##Relationships
 
@@ -202,7 +202,7 @@ class PreprintDetail(JSONAPIBaseView, generics.RetrieveUpdateDestroyAPIView, Pre
         is_preprint_orphan              boolean                             whether or not this preprint is orphaned
         subjects                        array of tuples of dictionaries     ids of Subject in the BePress taxonomy. Dictionary, containing the subject text and subject ID
         doi                             string                              bare DOI for the manuscript, as entered by the user
-        preprint_doi_on_datacite        boolean                             whether or not the preprint doi has been added on datacite
+        preprint_doi_created            iso8601 timestamp                   timestamp that the preprint doi was created
 
 
     ##Relationships
