03c685cd5d5f756c202a4d12eb25da50426b3b5e
Alex Schiller <alexschiller@gmail.com>
2017-12-06 12:04:06 -0500

    Fix success syntax

--- website/static/js/profile.js
+++ website/static/js/profile.js
@@ -446,11 +446,12 @@ var NameViewModel = function(urls, modes, preventUnsaved, fetchCallback) {
                 name: self.full()
             },
             dataType: 'json',
-        }).success(function (response) {
-            self.imputedGiven(response.given);
-            self.imputedMiddle(response.middle);
-            self.imputedFamily(response.family);
-            self.imputedSuffix(response.suffix);
+            success: function(response) {
+                self.imputedGiven(response.given);
+                self.imputedMiddle(response.middle);
+                self.imputedFamily(response.family);
+                self.imputedSuffix(response.suffix);
+            }
         });
     };
 
