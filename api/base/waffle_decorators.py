3d5de4b69be9d99fec4a8ffb46338f0684ffac26
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-12-07 17:10:28 -0600

    Add switch and sample decorators.

--- api/base/waffle_decorators.py
+++ api/base/waffle_decorators.py
@@ -3,7 +3,7 @@ from rest_framework.exceptions import NotFound
 
 def require_flag(flag_name):
     """
-    Decorator to check whether flag is active.
+    Decorator to check whether waffle flag is active.
 
     If inactive, raise NotFound.
     """
@@ -14,4 +14,34 @@ def require_flag(flag_name):
             else:
                 raise NotFound('Endpoint is disabled.')
         return check_flag
+    return wrapper
+
+def require_switch(switch_name):
+    """
+    Decorator to check whether waffle switch is active.
+
+    If inactive, raise NotFound.
+    """
+    def wrapper(fn):
+        def check_switch(*args,**kwargs):
+            if waffle.switch_is_active(switch_name):
+                return fn(*args,**kwargs)
+            else:
+                raise NotFound('Endpoint is disabled.')
+        return check_switch
+    return wrapper
+
+def require_sample(sample_name):
+    """
+    Decorator to check whether waffle sample is active.
+
+    If inactive, raise NotFound.
+    """
+    def wrapper(fn):
+        def check_sample(*args,**kwargs):
+            if waffle.sample_is_active(sample_name):
+                return fn(*args,**kwargs)
+            else:
+                raise NotFound('Endpoint is disabled.')
+        return check_sample
     return wrapper
