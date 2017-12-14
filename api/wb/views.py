bdd0591699a2d8a3a3cf1af2a57d5b4ccdfbe870
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-12-14 17:52:58 -0600

    Remove unused nodes from view.

--- api/wb/views.py
+++ api/wb/views.py
@@ -1,7 +1,7 @@
-from rest_framework import generics, permissions as drf_permissions
+from rest_framework import generics, status, permissions as drf_permissions
 
+from osf.models import AbstractNode
 from api.base import permissions as base_permissions
-
 from api.base.utils import get_object_or_error
 from api.base.views import JSONAPIBaseView
 from api.base.views import (
@@ -41,30 +41,10 @@ class MoveFile(JSONAPIBaseView, generics.CreateAPIView, NodeMixin, WaterButlerMi
     view_category = 'nodes'
     view_name = 'node-provider-file-metadata'
 
-    # Overrides NodeMixin for passing in specific node_id, or fetching quickfiles node.
-    def get_node(self, check_object_permissions=True, specific_node_id=None):
-        node_kwarg = self.kwargs[self.node_lookup_url_kwarg]
-        if (specific_node_id and len(specific_node_id) == 5) or len(node_kwarg) == 5:
-            node = super(NodeProviderFileMetadataCreate, self).get_node(check_object_permissions, specific_node_id)
-        else:
-            node = self.get_quickfiles_node(node_kwarg, specific_node_id)
-
-        if not(node.get_addon('osfstorage')):
-            raise ValidationError('Node must have OSFStorage Addon.')
-        return node
-
-    # Regular nodes or quickfiles nodes both work at this endpoint.
-    def get_quickfiles_node(self, node_kwarg, specific_node_id):
-        node = get_object_or_error(AbstractNode, specific_node_id if specific_node_id else node_kwarg, self.request, display_name='node')
-        self.check_object_permissions(self.request, node)
-        return node
-
-    # This endpoint only works for the OsfStorage provider
-    def get_provider_id(self):
-        provider = self.kwargs.get('provider')
-        if provider != 'osfstorage':
-            raise ValidationError('This endpoint only valid for the OSFStorage Addon.')
-        return provider
+    # overrides CreateApiView
+    def perform_create(self, serializer):
+        source = AbstractNode.load(self.kwargs.get('node_id'))
+        return serializer.save(action='move', source_node=source)
 
     def create(self, request, *args, **kwargs):
         response = super(MoveFile, self).create(request, *args, **kwargs)
