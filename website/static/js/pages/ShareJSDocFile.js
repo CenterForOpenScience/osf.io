fb1bd53d466179b329555e5b4359d2f327786224
Alex Schiller <alexschiller@gmail.com>
2017-12-15 10:23:40 -0500

    Fix jshint

--- website/static/js/pages/ShareJSDocFile.js
+++ website/static/js/pages/ShareJSDocFile.js
@@ -83,7 +83,7 @@ var ShareJSDoc = function(shareWSUrl, metadata, editor, observables) {
     function register() {
         // our shareJS explicitly wants 'userGravatar' and not our more general 'userProfileImage'
         // see https://github.com/CenterForOpenScience/sharejs/search?utf8=%E2%9C%93&q=gravatar&type=
-        metadata['userGravatar'] = metadata['userProfileImage'];
+        metadata.userGravatar = metadata.userProfileImage;
         socket.send(JSON.stringify(metadata));
     }
 
