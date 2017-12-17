aadd83f9665ebe6602f4b060927efa347a6d8bcc
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-12-17 17:36:43 -0600

    Pull common code for move/copy file metadata hooks into mixin.

--- api/wb/urls.py
+++ api/wb/urls.py
@@ -4,6 +4,6 @@ from api.wb import views
 app_name = 'osf'
 
 urlpatterns = [
-    url(r'^hooks/(?P<node_id>\w+)/move/', views.MoveFileMetadata.as_view(), name=views.MoveFileMetadata.view_name),
-    url(r'^hooks/(?P<node_id>\w+)/copy/', views.CopyFileMetadata.as_view(), name=views.CopyFileMetadata.view_name),
+    url(r'^hooks/(?P<node_id>\w+)/move/', views.MoveFileMetadataView.as_view(), name=views.MoveFileMetadataView.view_name),
+    url(r'^hooks/(?P<node_id>\w+)/copy/', views.CopyFileMetadataView.as_view(), name=views.CopyFileMetadataView.view_name),
 ]
--- api/wb/views.py
+++ api/wb/views.py
@@ -1,37 +1,21 @@
-from rest_framework import generics, status, permissions as drf_permissions
+from rest_framework import generics, status
 from rest_framework.exceptions import NotFound
 
 from osf.models import AbstractNode
 from addons.osfstorage.models import OsfStorageFileNode, OsfStorageFolder
-from api.base import permissions as base_permissions
 from api.base.utils import get_object_or_error
-from api.base.views import JSONAPIBaseView
-from api.base.views import (
-    WaterButlerMixin
-)
-from api.nodes.views import NodeMixin
-from api.nodes.permissions import (
-    ContributorOrPublic,
-    ExcludeWithdrawals,
-)
+from api.base.parsers import HMACSignedParser
 from api.wb.serializers import (
     WaterbutlerMetadataSerializer
-
 )
-from api.base.parsers import HMACSignedParser
-from framework.auth.oauth_scopes import CoreScopes
 
-
-class MoveFileMetadata(JSONAPIBaseView, generics.CreateAPIView, WaterButlerMixin):
+class FileMetadataMixin(generics.CreateAPIView):
     """
-    View for creating metadata for file move/copy in osfstorage.  Only WaterButler should talk to this endpoint.
-    To move/copy a file, send a request to WB, and WB will call this view.
+    Mixin with common code for WB move/copy hooks
     """
     parser_classes = (HMACSignedParser,)
-
     serializer_class = WaterbutlerMetadataSerializer
     view_category = 'wb'
-    view_name = 'metadata-move'
     node_lookup_url_kwarg = 'node_id'
 
     # Overrides CreateAPIView
@@ -49,13 +33,11 @@ class MoveFileMetadata(JSONAPIBaseView, generics.CreateAPIView, WaterButlerMixin
             raise NotFound
         return node
 
-    # overrides CreateApiView
-    def perform_create(self, serializer):
+    def perform_create(self, serializer, action):
         source = serializer.validated_data.pop('source')
         destination = serializer.validated_data.pop('destination')
         name = destination.get('name')
-
-        dest_node = self.get_node(node_id = destination.get('node'))
+        dest_node = self.get_node(node_id=destination.get('node'))
 
         try:
             source = OsfStorageFileNode.get(source, self.get_object())
@@ -63,12 +45,45 @@ class MoveFileMetadata(JSONAPIBaseView, generics.CreateAPIView, WaterButlerMixin
             raise NotFound
 
         try:
-             dest_parent = OsfStorageFolder.get(destination.get('parent'), dest_node)
+            dest_parent = OsfStorageFolder.get(destination.get('parent'), dest_node)
         except OsfStorageFolder.DoesNotExist:
             raise NotFound
-        return serializer.save(action='move', source=source, destination=dest_parent, name=name)
+        return serializer.save(action=action, source=source, destination=dest_parent, name=name)
 
+
+class MoveFileMetadataView(FileMetadataMixin):
+    """
+    View for creating metadata for file move/copy in osfstorage.  Only WaterButler should talk to this endpoint.
+    To move/copy a file, send a request to WB, and WB will call this view.
+    """
+
+    view_name = 'metadata-move'
+
+    # overrides CreateApiView
+    def perform_create(self, serializer):
+        return super(MoveFileMetadataView, self).perform_create(serializer, 'move')
+
+    # overrides CreateApiView
     def create(self, request, *args, **kwargs):
-        response = super(MoveFileMetadata, self).create(request, *args, **kwargs)
+        response = super(MoveFileMetadataView, self).create(request, *args, **kwargs)
         response.status_code = status.HTTP_200_OK
+        return response
+
+
+class CopyFileMetadataView(FileMetadataMixin):
+    """
+    View for creating metadata for file move/copy in osfstorage.  Only WaterButler should talk to this endpoint.
+    To move/copy a file, send a request to WB, and WB will call this view.
+    """
+
+    view_name = 'metadata-copy'
+
+    # overrides CreateApiView
+    def perform_create(self, serializer):
+        return super(CopyFileMetadataView, self).perform_create(serializer, 'copy')
+
+    # overrides CreateApiView
+    def create(self, request, *args, **kwargs):
+        response = super(CopyFileMetadataView, self).create(request, *args, **kwargs)
+        response.status_code = status.HTTP_201_CREATED
         return response
