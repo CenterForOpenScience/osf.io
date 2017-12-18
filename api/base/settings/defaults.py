6f628e02ed3ef2c7b4d0b26749999071d679f5a2
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-12-18 13:39:56 -0600

    Remove unneeded waffle template settings in API app.

--- api/base/settings/defaults.py
+++ api/base/settings/defaults.py
@@ -219,13 +219,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
+        'APP_DIRS': True
     }]
 
 
