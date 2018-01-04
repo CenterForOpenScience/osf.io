16a17928f4a067349d45fc7c05038b680e25c7a1
Lauren Revere <laurenrevere@cos.io>
2018-01-04 17:59:22 -0500

    Update to include project urls

--- scripts/get_users_with_ARKs.py
+++ scripts/get_users_with_ARKs.py
@@ -9,43 +9,57 @@ setup_django()
 from website import mails
 from website import settings
 
-from osf.models import AbstractNode
+from osf.models import OSFUser
 
 logger = logging.getLogger(__name__)
 
 
-def find_users_with_arks():
-    return list(
-        AbstractNode.objects.filter(
-        is_deleted=False,
-        is_public=True,
-        identifiers__category='ark',
-        ).values_list(
-            'contributor__user__username', flat=True
-        ).distinct()
-    )
+def find_ark_dict():
+    users = OSFUser.objects.filter(nodes__identifiers__category='ark')
+
+    users_and_guids = {}
+
+    for user in users:
+        if user.is_registered:
+            users_and_guids[user.username] = list(
+                user.nodes.filter(
+                    is_deleted=False,
+                    is_public=True,
+                    identifiers__category='ark',
+                )
+            )
+
+    return users_and_guids
+
+def get_node_urls(node_list):
+    urls = ''
+    for node in node_list:
+        urls += node.absolute_url + ', '
+    return urls[0:-2]
+
 
 def main():
-    users_with_arks = find_users_with_arks()
-    if users_with_arks:
-        filename = 'users_with_arks.csv'
+    ark_dict = find_ark_dict()
+    if ark_dict:
+        filename = 'users_ark_nodes.csv'
 
         output = io.BytesIO()
         writer = csv.writer(output)
-        for user in users_with_arks:
-            writer.writerow([user])
+        for user in ark_dict:
+            urls = get_node_urls(ark_dict[user])
+            writer.writerow([user, urls])
 
         mails.send_mail(
             mail=mails.EMPTY,
-            subject='Users with fake ARKs',
+            subject='Users/Projects with ARKs',
             to_addr=settings.OSF_SUPPORT_EMAIL,
-            body='List of users with ARKs attached.',
+            body='CSV with users/nodes with ARKs attached.',
             attachment_name=filename,
             attachment_content=output.getvalue(),
         )
 
-    logger.info('{n} users with ARKs found.'.format(n=len(users_with_arks)))
-    logger.info('Users with ARKs email sent.')
+    logger.info('{n} users with ARKs found.'.format(n=len(ark_dict)))
+    logger.info('ARK csv email sent.')
 
 if __name__ == '__main__':
     main()
