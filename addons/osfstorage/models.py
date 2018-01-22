9f14f44302aad8dd68d88b05ebbc7061f7954b6a
erinspace <erin.braswell@gmail.com>
2018-01-22 16:21:07 -0500

    Change the default for NullBooleanField to none

--- addons/osfstorage/models.py
+++ addons/osfstorage/models.py
@@ -378,7 +378,7 @@ class OsfStorageFile(OsfStorageFileNode, File):
 
 class OsfStorageFolder(OsfStorageFileNode, Folder):
 
-    is_root = models.NullBooleanField(default=False)
+    is_root = models.NullBooleanField(default=None)
 
     objects = OsfStorageFolderManager()
 
