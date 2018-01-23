a4d480ed1cbc53aff14fc075ab5bff3a2f67adbe
chennan47 <nc6r@virginia.edu>
2018-01-22 21:24:15 -0500

    change keyerror bypass to if check

--- osf_tests/factories.py
+++ osf_tests/factories.py
@@ -548,9 +548,10 @@ def sync_set_identifiers(preprint):
         'already_exists': False
     }
     id_dict = parse_identifiers(ezid_return_value)
-    try:
-        preprint.set_identifier_values(doi=id_dict['doi'], ark=id_dict['ark'])
-    except KeyError:
+    ark = id_dict.get('ark', None)
+    if ark:
+        preprint.set_identifier_values(doi=id_dict['doi'], ark=ark)
+    else:
         preprint.set_identifier_values(doi=id_dict['doi'])
 
 
