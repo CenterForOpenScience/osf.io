e12f5fb83065a0a9c12e74f928be792c706ff7d8
Fabrice Mizero <fabmizz@gmail.com>
2018-01-31 16:32:18 -0500

    Clean dry up nodesDelete; fix back binding

--- website/static/js/nodesDelete.js
+++ website/static/js/nodesDelete.js
@@ -11,6 +11,257 @@ var m = require('mithril');
 var bootbox = require('bootbox');
 var NodesDeleteTreebeard = require('js/nodesDeleteTreebeard');
 
+
+var BULK_DELETE_LIMIT = 100;
+
+/**
+ * view model which corresponds to nodes_delete.mako (#nodesDelete)
+ * Used for quickly deleting nodes with no children.
+ * @type {QuickDeleteViewModel}
+ */
+
+var QuickDeleteViewModel = function(nodeType, isPreprint, nodeApiUrl) {
+    var self = this;
+
+    self.confirmationString = $osf.getConfirmationString();
+    self.QUICKDELETE = 'quickDelete';
+    self.SELECT = 'select';
+    self.CONFIRM = 'confirm';
+    self.page = ko.observable(self.QUICKDELETE);
+    self.confirmInput = ko.observable('');
+    self.canDelete = ko.observable(false);
+    self.nodeType = nodeType;
+    self.nodeApiUrl = nodeApiUrl;
+    self.isPreprint = isPreprint;
+    self.preprintMessage = '<br><br>This ' + self.nodeType +
+      ' also contains a <strong>preprint</strong>. Deleting this ' + self.nodeType +
+      ' will also delete your <strong>preprint</strong>. This action is irreversible.';
+
+    self.message = ko.computed(function() {
+        return 'It will no longer be available to other contributors on the project.' +
+          (self.isPreprint ? self.preprintMessage : '');
+    });
+    self.atMaxLength = ko.observable(false);
+    self.nodesDeleted = ko.observable(true);
+    self.pageTitle = ko.computed(function() {
+        return 'Are you sure you want to delete this ' + self.nodeType + '?';
+    });
+};
+
+QuickDeleteViewModel.prototype.clear = function() {
+    var self = this;
+    self.nodesDeleted = ko.observable(false);
+};
+
+QuickDeleteViewModel.prototype.handleEditableUpdate = function(element) {
+    var self = this;
+    var $element = $(element);
+    var inputText = $element[0].innerText;
+    var inputTextLength = inputText.length;
+
+    self.atMaxLength(inputTextLength >= self.confirmationString.length);
+    self.canDelete(inputText === self.confirmationString);
+};
+
+QuickDeleteViewModel.prototype.confirmChanges = function() {
+    var self = this;
+    var request = $.ajax({
+        type: 'DELETE',
+        dataType: 'json',
+        url: self.nodeApiUrl
+    });
+    request.done(function(response) {
+        // Redirect to either the parent project or the dashboard
+        window.location.href = response.url;
+    });
+    request.fail($osf.handleJSONError);
+};
+
+/**
+ * view model which corresponds to nodes_delete.mako (#nodesDelete)
+ * Used for deleting nodes with children.
+ * @type {NodesDeleteViewModel}
+ */
+
+var NodesDeleteViewModel = function(nodeType, isPreprint, nodeApiUrl) {
+    var self = this;
+    QuickDeleteViewModel.call(self, nodeType, isPreprint, nodeApiUrl);
+
+    self.confirmationString = '';
+    self.tbCache = null;
+    self.treebeardUrl = nodeApiUrl + 'tree/';
+    self.nodesOriginal = {};
+    self.nodesDeleted = ko.observable();
+    self.nodesChanged = ko.observableArray([]);
+    //state of current nodes
+    self.nodesState = ko.observableArray();
+    self.nodesState.subscribe(function(newValue) {
+        var nodesDeleted = 0;
+        for (var key in newValue) {
+            if (newValue[key].changed !== self.nodesOriginal[key].changed) {
+                newValue[key].changed = true;
+                nodesDeleted++;
+            }
+            else {
+                newValue[key].changed = false;
+            }
+        }
+        self.nodesDeleted(nodesDeleted > 0 && nodesDeleted === Object.keys(self.nodesOriginal).length);
+        m.redraw(true);
+    });
+    //original node state on page load
+    self.hasChildren = ko.observable(false);
+    $('#nodesDelete').on('hidden.bs.modal', function () {
+        self.clear();
+    });
+
+    self.page = ko.observable(self.SELECT);
+
+    self.pageTitle = ko.computed(function() {
+        if (self.nodeType === 'project') {
+            return {
+                select: 'Delete Project',
+                confirm: 'Delete Project and Components'
+            }[self.page()];
+        }
+
+        if (self.nodeType === 'component') {
+            return {
+                select: 'Delete Component',
+                confirm: 'Delete Components'
+            }[self.page()];
+        }
+
+    });
+
+    self.message = ko.computed(function() {
+        var message = 'It looks like your ' + self.nodeType + ' has components within it. To delete this ' +
+          self.nodeType + ', you must also delete all child components.';
+
+        var confirm_message = 'The following ' + (self.nodeType === 'project' ? 'project and ' : '') + 'components will be deleted.';
+
+        if (self.page() === self.CONFIRM) {
+            return confirm_message;
+        }
+
+        return {
+            select: message + (self.isPreprint ? self.preprintMessage : ''),
+            confirm: confirm_message
+        }[self.page()];
+    });
+
+    self.warning = ko.computed(function() {
+        return 'Please note that deleting your ' + self.nodeType + ' will erase all your ' +
+          self.nodeType + ' data and this process is IRREVERSIBLE.';
+    });
+};
+
+NodesDeleteViewModel.prototype = Object.create(QuickDeleteViewModel.prototype);
+
+NodesDeleteViewModel.prototype.fetchNodeTree = function() {
+    var self = this;
+
+    return $.ajax({
+        url: self.treebeardUrl,
+        type: 'GET',
+        dataType: 'json'
+    }).done(function(response) {
+        self.nodesOriginal = getNodesOriginal(response[0], self.nodesOriginal);
+        var size = 0;
+        $.each(Object.keys(self.nodesOriginal), function(_, key) {
+            if (self.nodesOriginal.hasOwnProperty(key)) {
+                size++;
+            }
+        });
+        self.hasChildren(size > 1);
+        var nodesState = $.extend(true, {}, self.nodesOriginal);
+        var nodeParent = response[0].node.id;
+        self.nodesState(nodesState);
+    }).fail(function(xhr, status, error) {
+        $osf.growl('Error', 'Unable to retrieve project settings');
+        Raven.captureMessage('Could not GET project settings.', {
+            extra: {
+                url: self.treebeardUrl, status: status, error: error
+            }
+        });
+    });
+};
+
+NodesDeleteViewModel.prototype.selectProjects = function() {
+    var self = this;
+    self.page(this.SELECT);
+};
+
+NodesDeleteViewModel.prototype.confirmWarning =  function() {
+    var self = this;
+    var nodesState = ko.toJS(self.nodesState);
+    for (var node in nodesState) {
+        if (nodesState[node].changed) {
+            self.nodesChanged().push(nodesState[node].title);
+        }
+    }
+    self.confirmationString = $osf.getConfirmationString();
+    self.page(this.CONFIRM);
+};
+
+NodesDeleteViewModel.prototype.selectAll = function() {
+    var self = this;
+
+    var nodesState = ko.toJS(self.nodesState());
+    for (var node in nodesState) {
+        if (nodesState[node].isAdmin) {
+            nodesState[node].changed = true;
+        }
+    }
+    self.nodesState(nodesState);
+    m.redraw(true);
+};
+
+NodesDeleteViewModel.prototype.confirmChanges =  function() {
+    var self = this;
+
+    var nodesState = ko.toJS(this.nodesState());
+    nodesState = Object.keys(nodesState).map(function(key) {
+        return nodesState[key];
+    });
+    var nodesChanged = nodesState.filter(function(node) {
+        return node.changed;
+    });
+
+    if (nodesChanged.length <= BULK_DELETE_LIMIT) {
+        $osf.block('Deleting ' + self.nodeType);
+        batchNodesDelete(nodesChanged.reverse(), self.nodeType).then(function () {
+            self.page(self.WARNING);
+        }).fail(function (xhr) {
+            $osf.unblock();
+            var errorMessage = 'Unable to delete ' + self.nodeType;
+            if (xhr.responseJSON && xhr.responseJSON.errors) {
+                errorMessage = xhr.responseJSON.errors[0].detail;
+            }
+            $osf.growl('Problem deleting ' + self.nodeType, errorMessage);
+            Raven.captureMessage('Could not batch delete project and its components.');
+            self.clear();
+            $('#nodesDelete').modal('hide');
+        }).always(function() {
+            $osf.unblock();
+        });
+    }
+};
+
+NodesDeleteViewModel.prototype.clear = function() {
+    var self = this;
+    self.nodesChanged([]);
+    self.page(self.SELECT);
+    self.tbCache = null;
+};
+
+NodesDeleteViewModel.prototype.back = function() {
+    var self = this;
+    self.nodesChanged([]);
+    self.page(self.SELECT);
+    return new NodesDeleteTreebeard('nodesDeleteTreebeard', self.tbCache, self.nodesState, self.nodesOriginal);
+};
+
 function _flattenNodeTree(nodeTree) {
     var ret = [];
     var stack = [nodeTree];
@@ -65,7 +316,7 @@ function batchNodesDelete(nodes, nodeType) {
         data: JSON.stringify({
             data: nodesBatch
         }),
-        success: function(response){
+        success: function(response) {
             if (nodeType === 'project')
                 bootbox.alert({
                     message: 'Project has been successfully deleted.',
@@ -85,265 +336,6 @@ function batchNodesDelete(nodes, nodeType) {
     });
 }
 
-/**
- * view model which corresponds to nodes_delete.mako (#nodesDelete)
- *
- * @type {NodesDeleteViewModel}
- */
-var NodesDeleteViewModel = function(nodeType, isPreprint, nodeApiUrl) {
-    var self = this;
-
-    self.SELECT = 'select';
-    self.CONFIRM = 'confirm';
-    self.QUICKDELETE = 'quickDelete';
-    self.nodeType = nodeType;
-    self.confirmationString = '';
-    self.treebeardUrl = nodeApiUrl + 'tree/';
-    self.isPreprint = isPreprint;
-    self.nodesOriginal = {};
-    self.nodesDeleted = ko.observable();
-    self.nodesChanged = ko.observableArray([]);
-    //state of current nodes
-    self.nodesState = ko.observableArray();
-    self.nodesState.subscribe(function(newValue) {
-        var nodesDeleted = 0;
-        for (var key in newValue) {
-            if (newValue[key].changed !== self.nodesOriginal[key].changed){
-                newValue[key].changed = true;
-                nodesDeleted++;
-            }
-            else {
-                newValue[key].changed = false;
-            }
-        }
-        self.nodesDeleted(nodesDeleted > 0 && nodesDeleted === Object.keys(self.nodesOriginal).length);
-        m.redraw(true);
-    });
-    //original node state on page load
-    self.hasChildren = ko.observable(false);
-    $('#nodesDelete').on('hidden.bs.modal', function () {
-        self.clear();
-    });
-
-    self.page = ko.observable(self.SELECT);
-
-    self.pageTitle = ko.computed(function() {
-        if (self.nodeType === 'project'){
-            return {
-                select: 'Delete Project',
-                confirm: 'Delete Project and Components'
-            }[self.page()];
-        }
-
-        if (self.nodeType === 'component'){
-            return {
-                select: 'Delete Component',
-                confirm: 'Delete Components'
-            }[self.page()];
-        }
-
-    });
-
-    self.message = ko.computed(function() {
-        var message = 'It looks like your ' + self.nodeType + ' has components within it. To delete this ' +
-          self.nodeType + ', you must also delete all child components.';
-
-        var preprintMessage = '<br><br>This ' + self.nodeType + ' also contains a <strong>preprint</strong>. Deleting this ' +
-          self.nodeType + ' will also delete your <strong>preprint</strong>. This action is irreversible.';
-
-        var confirm_message = 'The following ' + (self.nodeType === 'project' ? 'project and ' : '') + 'components will be deleted.';
-
-        if (self.page() === self.CONFIRM) {
-            return confirm_message;
-        }
-
-        return {
-            select: message + (self.isPreprint ? preprintMessage : ''),
-            confirm: confirm_message
-        }[self.page()];
-    });
-
-    self.warning = ko.computed(function() {
-        return 'Please note that deleting your ' + self.nodeType + ' will erase all your ' +
-          self.nodeType + ' data and this process is IRREVERSIBLE.';
-    });
-    self.atMaxLength = ko.observable(false);
-    self.confirmInput = ko.observable('');
-    self.canDelete = ko.observable(false);
-};
-
-/**
- * get node tree for treebeard from API V1
- */
-NodesDeleteViewModel.prototype.fetchNodeTree = function() {
-    var self = this;
-
-    return $.ajax({
-        url: self.treebeardUrl,
-        type: 'GET',
-        dataType: 'json'
-    }).done(function(response) {
-        self.nodesOriginal = getNodesOriginal(response[0], self.nodesOriginal);
-        var size = 0;
-        $.each(Object.keys(self.nodesOriginal), function(_, key) {
-            if (self.nodesOriginal.hasOwnProperty(key)) {
-                size++;
-            }
-        });
-        self.hasChildren(size > 1);
-        var nodesState = $.extend(true, {}, self.nodesOriginal);
-        var nodeParent = response[0].node.id;
-        self.nodesState(nodesState);
-    }).fail(function(xhr, status, error) {
-        $osf.growl('Error', 'Unable to retrieve project settings');
-        Raven.captureMessage('Could not GET project settings.', {
-            extra: {
-                url: self.treebeardUrl, status: status, error: error
-            }
-        });
-    });
-};
-
-NodesDeleteViewModel.prototype.selectProjects = function() {
-    this.page(this.SELECT);
-};
-
-NodesDeleteViewModel.prototype.handleEditableUpdate = function(element) {
-    var self = this;
-    var $element = $(element);
-    var inputText = $element[0].innerText;
-    var inputTextLength = inputText.length;
-
-    self.atMaxLength(inputTextLength >= self.confirmationString.length);
-    self.canDelete(inputText === self.confirmationString);
-};
-
-NodesDeleteViewModel.prototype.confirmWarning =  function() {
-    var nodesState = ko.toJS(this.nodesState);
-    for (var node in nodesState) {
-        if (nodesState[node].changed) {
-            this.nodesChanged().push(nodesState[node].title);
-        }
-    }
-    this.confirmationString = $osf.getConfirmationString();
-    this.page(this.CONFIRM);
-};
-
-NodesDeleteViewModel.prototype.selectAll = function() {
-    var nodesState = ko.toJS(this.nodesState());
-    for (var node in nodesState) {
-        if (nodesState[node].isAdmin) {
-            nodesState[node].changed = true;
-        }
-    }
-    this.nodesState(nodesState);
-    m.redraw(true);
-};
-
-
-NodesDeleteViewModel.prototype.confirmChanges =  function() {
-    var self = this;
-
-    var nodesState = ko.toJS(this.nodesState());
-    nodesState = Object.keys(nodesState).map(function(key) {
-        return nodesState[key];
-    });
-    var nodesChanged = nodesState.filter(function(node) {
-        return node.changed;
-    });
-
-    if (nodesChanged.length <= 100) {
-        $osf.block('Deleting Project');
-        batchNodesDelete(nodesChanged.reverse(), self.nodeType).then(function () {
-            self.page(self.WARNING);
-        }).fail(function (xhr) {
-            $osf.unblock();
-            var errorMessage = 'Unable to delete project';
-            if (xhr.responseJSON && xhr.responseJSON.errors) {
-                errorMessage = xhr.responseJSON.errors[0].detail;
-            }
-            $osf.growl('Problem deleting project', errorMessage);
-            Raven.captureMessage('Could not batch delete projects.');
-            self.clear();
-            $('#nodesDelete').modal('hide');
-        }).always(function() {
-            $osf.unblock();
-        });
-    }
-};
-
-NodesDeleteViewModel.prototype.clear = function() {
-    this.nodesChanged([]);
-    this.page(this.SELECT);
-};
-
-NodesDeleteViewModel.prototype.back = function() {
-    this.nodesChanged([]);
-    this.page(this.SELECT);
-};
-
-/**
- * view model which corresponds to nodes_delete.mako (#nodesDelete)
- * Used for quickly deleting nodes with no children.
- * @type {QuickDeleteViewModel}
- */
-
-var QuickDeleteViewModel = function(nodeType, isPreprint, nodeApiUrl){
-    var self = this;
-    self.confirmationString = $osf.getConfirmationString();
-    self.QUICKDELETE = 'quickDelete';
-    self.SELECT = 'select';
-    self.CONFIRM = 'confirm';
-    self.page = ko.observable(self.QUICKDELETE);
-    self.confirmInput = ko.observable('');
-    self.canDelete = ko.observable(false);
-    self.nodeType = nodeType;
-    self.nodeApiUrl = nodeApiUrl;
-    self.isPreprint = isPreprint;
-    self.message = ko.computed(function(){
-        var preprintMessage = '<br><br>This ' + self.nodeType + ' contains a <strong>preprint</strong>. Deleting this ' +
-          self.nodeType + ' will also delete your <strong>preprint</strong>. This action is irreversible.';
-        var message = 'It will no longer be available to other contributors on the project.' +
-        (self.isPreprint ? preprintMessage : '');
-
-        return message;
-    });
-    self.atMaxLength = ko.observable(false);
-    self.nodesDeleted = ko.observable(true);
-    self.pageTitle = ko.computed(function() {
-        return 'Are you sure you want to delete this ' + self.nodeType + '?';
-    });
-};
-
-QuickDeleteViewModel.prototype.clear = function(){
-    var self = this;
-    self.nodesDeleted = ko.observable(false);
-};
-
-QuickDeleteViewModel.prototype.handleEditableUpdate = function(element) {
-    var self = this;
-    var $element = $(element);
-    var inputText = $element[0].innerText;
-    var inputTextLength = inputText.length;
-
-    self.atMaxLength(inputTextLength >= self.confirmationString.length);
-    self.canDelete(inputText === self.confirmationString);
-};
-
-QuickDeleteViewModel.prototype.confirmChanges = function(){
-    var self = this;
-    var request = $.ajax({
-        type: 'DELETE',
-        dataType: 'json',
-        url: self.nodeApiUrl
-    });
-    request.done(function(response) {
-        // Redirect to either the parent project or the dashboard
-        window.location.href = response.url;
-    });
-    request.fail($osf.handleJSONError);
-};
-
 function NodesDelete(childExists, nodeType, isPreprint, nodeApiUrl) {
     var self = this;
 
@@ -353,6 +345,7 @@ function NodesDelete(childExists, nodeType, isPreprint, nodeApiUrl) {
     if (childExists) {
         self.viewModel = new NodesDeleteViewModel(nodeType, isPreprint, nodeApiUrl);
         self.viewModel.fetchNodeTree().done(function(response) {
+            self.viewModel.tbCache = response;
             new NodesDeleteTreebeard('nodesDeleteTreebeard', response, self.viewModel.nodesState, self.viewModel.nodesOriginal);
         });
     } else {
@@ -362,7 +355,7 @@ function NodesDelete(childExists, nodeType, isPreprint, nodeApiUrl) {
     return self.viewModel;
 }
 
-var NodesDeleteManager = function(){
+var NodesDeleteManager = function() {
     var self = this;
 
     self.modal = ko.observable();
@@ -371,7 +364,7 @@ var NodesDeleteManager = function(){
     };
 };
 
-var DeleteManager = function(selector){
+var DeleteManager = function(selector) {
     var self = this;
 
     self.selector = selector;
@@ -381,6 +374,5 @@ var DeleteManager = function(selector){
 };
 
 module.exports = {
-    _NodesDeleteViewModel: NodesDeleteViewModel,
-    DeleteManager: DeleteManager
+    DeleteManager: DeleteManager,
 };
