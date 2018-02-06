85621c9a878e405f0e4c722384653844c40ae9ca
Dawn Pattison <pattison.dawn.r@gmail.com>
2018-02-06 12:49:43 -0600

    Instead of first(), use [:1].get() so queryset isn't ordered to save time.

--- osf/models/base.py
+++ osf/models/base.py
@@ -304,8 +304,8 @@ class GuidMixin(BaseIDMixin):
         try:
             # guids___id__isnull=False forces an INNER JOIN
             if select_for_update:
-                return cls.objects.filter(guids___id__isnull=False, guids___id=q).select_for_update().first()
-            return cls.objects.filter(guids___id__isnull=False, guids___id=q).first()
+                return cls.objects.filter(guids___id__isnull=False, guids___id=q).select_for_update()[:1].get()
+            return cls.objects.filter(guids___id__isnull=False, guids___id=q)[:1].get()
         except cls.DoesNotExist:
             return None
 
