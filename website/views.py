4759d7d36a361bdd0fa13b832c79a7b1a90d2ba3
Henrique Harman <henriqueharman@gmail.com>
2018-02-09 13:29:52 -0500

    Use waffle to use ember-osf-web for dashboard when switch is active

--- website/views.py
+++ website/views.py
@@ -10,6 +10,7 @@ import urllib
 from django.apps import apps
 from django.db.models import Count
 from flask import request, send_from_directory, Response, stream_with_context
+from waffle.models import Switch
 
 from framework import sentry
 from framework.auth import Auth
@@ -142,26 +143,29 @@ def index():
 
     user_id = get_current_user_id()
     if user_id:  # Logged in: return either landing page or user home page
-        all_institutions = (
-            Institution.objects.filter(
-                is_deleted=False,
-                nodes__is_public=True,
-                nodes__is_deleted=False,
-                nodes__type='osf.node'
+        if Switch.objects.get(name='ember_dashboard').is_active():
+            return send_from_directory(ember_osf_web_dir, 'index.html')
+        else:
+            all_institutions = (
+                Institution.objects.filter(
+                    is_deleted=False,
+                    nodes__is_public=True,
+                    nodes__is_deleted=False,
+                    nodes__type='osf.node'
+                )
+                .annotate(Count('nodes'))
+                .filter(nodes__count__gte=INSTITUTION_DISPLAY_NODE_THRESHOLD)
+                .order_by('name').only('_id', 'name', 'logo_name')
             )
-            .annotate(Count('nodes'))
-            .filter(nodes__count__gte=INSTITUTION_DISPLAY_NODE_THRESHOLD)
-            .order_by('name').only('_id', 'name', 'logo_name')
-        )
-        dashboard_institutions = [
-            {'id': inst._id, 'name': inst.name, 'logo_path': inst.logo_path_rounded_corners}
-            for inst in all_institutions
-        ]
+            dashboard_institutions = [
+                {'id': inst._id, 'name': inst.name, 'logo_path': inst.logo_path_rounded_corners}
+                for inst in all_institutions
+            ]
 
-        return {
-            'home': True,
-            'dashboard_institutions': dashboard_institutions,
-        }
+            return {
+                'home': True,
+                'dashboard_institutions': dashboard_institutions,
+            }
     else:  # Logged out: return landing page
         return {
             'home': True,
