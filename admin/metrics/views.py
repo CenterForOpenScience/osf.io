c66e8151ebb2a84171587deb19bfdf3da0236994
chennan47 <nc6r@virginia.edu>
2018-02-19 15:45:48 -0500

    small fix

--- admin/metrics/views.py
+++ admin/metrics/views.py
@@ -38,6 +38,6 @@ class FileDownloadCounts(TemplateView):
     def update(cls, number_downloads_total, number_downloads_unique, update_date):
         if update_date > update_date:
             raise ValueError('The download counts has been recently updated on {}.'.format(self.update_date))
-        cls.number_downloads_total = number_downloads_total
-        cls.number_downloads_unique = number_downloads_unique
+        cls.number_downloads_total += number_downloads_total
+        cls.number_downloads_unique += number_downloads_unique
         cls.update_date = update_date
