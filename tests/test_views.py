a5f1fde931f58f7445d39e0ac8d7d2bd668ea06e
erinspace <erin.braswell@gmail.com>
2018-02-27 10:50:05 -0500

    Remove this commit after PLAT-338 is merged

--- tests/test_views.py
+++ tests/test_views.py
@@ -1869,6 +1869,7 @@ class TestAddingContributorViews(OsfTestCase):
             mails.CONTRIBUTOR_ADDED_DEFAULT,
             user=contributor,
             node=project,
+            mimetype='plain',
             referrer_name=self.auth.user.fullname,
             all_global_subscriptions_none=False,
             branded_service=None,
