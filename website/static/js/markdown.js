806d385b409b7fad648400e2cc66784e26947106
John Tordoff <john@cos.io>
2018-03-29 12:28:03 -0400

    add compatibility for markdown-it-mfr

--- website/static/js/markdown.js
+++ website/static/js/markdown.js
@@ -3,6 +3,7 @@ var hljs = require('highlight.js');
 require('highlight-css');
 var MarkdownIt = require('markdown-it');
 
+var $ = require('jquery');
 var insDel = require('markdown-it-ins-del');
 var pymarkdownList = require('js/markdown-it-pymarkdown-lists');
 
@@ -30,12 +31,18 @@ var bootstrapTable = function(md) {
 var oldMarkdownList = function(md) {
     md.block.ruler.after('hr', 'pyMarkdownList', pymarkdownList);
 };
+var mfrURL = window.contextVars.mfrURL;
+var osfURL = window.contextVars.osfURL;
 
 // Full markdown renderer for views / wiki pages / pauses between typing
 var markdown = new MarkdownIt('commonmark', {
     highlight: highlighter,
     linkify: true
-})
+    }).use(require('markdown-it-mfr'), {
+        type: 'osf',
+        pattern: /^http(?:s?):\/\/(?:www\.)?[a-zA-Z0-9 .:]{1,}\/render\?url=http(?:s?):\/\/[a-zA-Z0-9 .:]{1,}\/([a-zA-Z0-9]{5})\/\?action=download|(^[a-zA-Z0-9]{5}$)/,
+        formatUrl: function(assetID) { return mfrURL + 'render?url='+ osfURL + assetID + '/?action=download%26mode=render'; },
+    })
     .use(require('markdown-it-video'))
     .use(require('@centerforopenscience/markdown-it-toc'))
     .use(require('markdown-it-sanitizer'))
@@ -75,3 +82,35 @@ module.exports = {
     quick: markdownQuick,
     old: markdownOld
 };
+
+// Disable backspace sending you back a page in firefox. This is just a usability fix because users
+// tend to click out of the text box while it loads MFR embeds, then press backspace, believing the
+// cursor is still active.
+// https://stackoverflow.com/questions/1495219/how-can-i-prevent-the-backspace-key-from-navigating-back
+$(document).unbind('keydown').bind('keydown', function (event) {
+    if (event.keyCode === 8) {
+        var doPrevent = true;
+        var types = ['text', 'password', 'file', 'search', 'email', 'number', 'date', 'color', 'datetime', 'datetime-local', 'month', 'range', 'search', 'tel', 'time', 'url', 'week'];
+        var d = $(event.srcElement || event.target);
+        var disabled = d.prop('readonly') || d.prop('disabled');
+        if (!disabled) {
+            if (d[0].isContentEditable) {
+                doPrevent = false;
+            } else if (d.is('input')) {
+                var type = d.attr('type');
+                if (type) {
+                    type = type.toLowerCase();
+                }
+                if (types.indexOf(type) > -1) {
+                    doPrevent = false;
+                }
+            } else if (d.is('textarea')) {
+                doPrevent = false;
+            }
+        }
+        if (doPrevent) {
+            event.preventDefault();
+            return false;
+        }
+    }
+});
