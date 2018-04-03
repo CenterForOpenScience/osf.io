621b04a289cb798f741f93f783458e7fad0e4f4b
John Tordoff <john@cos.io>
2018-04-03 14:15:09 -0400

    "mock" unauthenticated users for Waffle

--- website/ember_osf_web/decorators.py
+++ website/ember_osf_web/decorators.py
@@ -15,7 +15,10 @@ def ember_flag_is_active(flag_name):
     def decorator(func):
         @functools.wraps(func)
         def wrapped(*args, **kwargs):
-            request.user = _get_current_user()
+            # Waffle does not enjoy NoneTypes as user values.
+            mock_user = type('Mock User', (object,), {'is_authenticated': False})()
+            request.user = _get_current_user() or mock_user
+
             if waffle.flag_is_active(request, flag_name):
                 return use_ember_app()
             else:
