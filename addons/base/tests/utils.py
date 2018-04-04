1ae0fed5176e6121cd555cd9f54e8e73ae509eae
Dawn Pattison <pattison.dawn.r@gmail.com>
2018-04-04 17:03:10 -0500

    Replace zotero model tests of obsolete method with replacement tests of get_folders method.

--- addons/base/tests/utils.py
+++ addons/base/tests/utils.py
@@ -3,7 +3,8 @@ class MockFolder(dict, object):
     def __init__(self):
         self.name = 'Fake Folder'
         self.json = {'id': 'Fake Key', 'parent_id': 'cba321'}
-        self['data'] = {'name': 'Fake Folder', 'key': 'Fake Key'}
+        self['data'] = {'name': 'Fake Folder', 'key': 'Fake Key', 'parentCollection': False}
+        self['library'] = {'type': 'personal', 'id': '34241'}
         self['name'] = 'Fake Folder'
         self['id'] = 'Fake Key'
 
@@ -13,6 +14,6 @@ class MockLibrary(dict, object):
     def __init__(self):
         self.name = 'Fake Library'
         self.json = {'id': 'Fake Library Key', 'parent_id': 'cba321'}
-        self['data'] = {'name': 'Fake Library', 'key': 'Fake Key'}
+        self['data'] = {'name': 'Fake Library', 'key': 'Fake Key', 'id': '12345' }
         self['name'] = 'Fake Library'
         self['id'] = 'Fake Library Key'
