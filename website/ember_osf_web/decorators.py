1a4253e4f2ead14ede89d6cf16b409954bc1bb0c
John Tordoff <john@cos.io>
2018-04-06 11:20:45 -0400

    add tests for waffle

--- website/ember_osf_web/decorators.py
+++ website/ember_osf_web/decorators.py
@@ -7,6 +7,10 @@ from framework.auth.core import _get_current_user
 from website.ember_osf_web.views import use_ember_app
 
 
+class MockUser(object):
+    is_authenticated = False
+
+
 def ember_flag_is_active(flag_name):
     """
     Decorator for checking whether ember flag is active.  If so, proxy to ember
@@ -16,8 +20,7 @@ def ember_flag_is_active(flag_name):
         @functools.wraps(func)
         def wrapped(*args, **kwargs):
             # Waffle does not enjoy NoneTypes as user values.
-            mock_user = type('Mock User', (object,), {'is_authenticated': False})()
-            request.user = _get_current_user() or mock_user
+            request.user = _get_current_user() or MockUser()
 
             if waffle.flag_is_active(request, flag_name):
                 return use_ember_app()
