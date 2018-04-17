42a38bf54fed348cdf527d5a2fa90df2f8676891
Fabrice Mizero <fabmizz@gmail.com>
2018-04-17 13:50:33 -0400

    Add custom link for banners

--- website/static/js/home-page/donateBannerPlugin.js
+++ website/static/js/home-page/donateBannerPlugin.js
@@ -48,15 +48,19 @@ var BannerDisplay = {
         var defaultAltText = args.banner.attributes.default_alt_text;
         var mobileAltText = args.banner.attributes.mobile_alt_text;
         var color = args.banner.attributes.color;
+        var link = args.banner.attributes.link;
+        var name = args.banner.attributes.name;
 
         $('.donate-banner-background')[0].style.backgroundColor = color;
 
         return m('.row',
             [
                 m('a', {
-                        href: 'https://www.crowdrise.com/centerforopenscience', onclick: function () {
-                            $osf.trackClick('link', 'click', 'DonateBanner - Donate now');
-                        }
+                        href: link,
+                        onclick: function () {
+                            $osf.trackClick('link', 'click', 'Banner - ' + name);
+                          },
+                        target : '_blank'
                     },
                     m('.col-sm-md-lg-12.hidden-xs',
                         m('img.donate-banner.img-responsive.banner-image', {'src': defaultPhoto, 'alt': defaultAltText})
