6bc60e888e7a674c10180b63133fd78682e0882f
t-imamura <takashi.imamura.wh@hitachi-solutions.com>
2018-04-23 21:44:29 +0900

    new file:   Dockerfile_admin 	modified:   addons/base/views.py 	new file:   admin/base/settings/local-dist.py 	modified:   api/timestamp/timestamptoken_verify.py 	new file:   osf/migrations/0080_auto_20180423_0634.py

---
+++ admin/base/settings/local-dist.py
@@ -0,0 +1,6 @@
+# session:
+# TODO: Uncomment SESSION_COOKIE_DOMAIN if OSFAdmin and OSF domains are different.then set up a shared domain.
+#   ex. OSFAdmin: admin.shared.domain , OSF: web.shared.domain
+#         website/settings/local.py    OSF_COOKIE_DOMAIN = '.shared.domain'
+#         admin/base/settings/local.py SESSION_COOKIE_DOMAIN = '.shared.domain'
+#SESSION_COOKIE_DOMAIN = '.shared.domain'
