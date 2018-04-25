32dc1ae100d0ff85f42d3aa55d94deede48ef972
Henrique Harman <henriqueharman@gmail.com>
2018-04-25 10:55:47 -0400

    Add HideWiki only to latest version of API, bump up latest to 2.8

--- api/base/settings/defaults.py
+++ api/base/settings/defaults.py
@@ -152,6 +152,7 @@ REST_FRAMEWORK = {
         '2.5',
         '2.6',
         '2.7',
+        '2.8',
     ),
     'DEFAULT_FILTER_BACKENDS': ('api.base.filters.OSFOrderingFilter',),
     'DEFAULT_PAGINATION_CLASS': 'api.base.pagination.JSONAPIPagination',
