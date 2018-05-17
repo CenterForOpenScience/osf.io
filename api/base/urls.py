8afda32d77423ced1c6167d0940fa9711cb6c21d
Brian J. Geiger <bgeiger@pobox.com>
2018-05-17 14:28:14 -0400

    Create scopes endpoint
    This is a read-only endpoint for grabbing the list of scopes available to the public on this server. The main use is to allow people making Personal Access Tokens to know what their options are for limiting what the token can do on a global basis.

--- api/base/urls.py
+++ api/base/urls.py
@@ -43,6 +43,7 @@ urlpatterns = [
                 url(r'^preprint_providers/', include('api.preprint_providers.urls', namespace='preprint_providers')),
                 url(r'^registrations/', include('api.registrations.urls', namespace='registrations')),
                 url(r'^requests/', include('api.requests.urls', namespace='requests')),
+                url(r'^scopes/', include('api.scopes.urls', namespace='scopes')),
                 url(r'^search/', include('api.search.urls', namespace='search')),
                 url(r'^subscriptions/', include('api.subscriptions.urls', namespace='subscriptions')),
                 url(r'^taxonomies/', include('api.taxonomies.urls', namespace='taxonomies')),
