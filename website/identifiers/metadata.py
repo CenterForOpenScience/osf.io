43c6ab59b289fdd373b125866f527a28c3a33764
erinspace <erin.braswell@gmail.com>
2018-05-22 16:58:07 -0400

    Move all new metadata code out of metadata, make sure it only lives in clients

--- website/identifiers/metadata.py
+++ website/identifiers/metadata.py
@@ -1,38 +1,21 @@
 # -*- coding: utf-8 -*-
-import time
-
 import unicodedata
 import lxml.etree
 import lxml.builder
 
 from website import settings
 
-
-# datacite metadata
-DATACITE_NAMESPACE = 'http://datacite.org/schema/kernel-4'
+NAMESPACE = 'http://datacite.org/schema/kernel-4'
 XSI = 'http://www.w3.org/2001/XMLSchema-instance'
-DATACITE_SCHEMA_LOCATION = 'http://datacite.org/schema/kernel-4 http://schema.datacite.org/meta/kernel-4/metadata.xsd'
-DATACITE_SUBJECT_SCHEME = 'bepress Digital Commons Three-Tiered Taxonomy'
-
-# crossref metadata
-CROSSREF_DEPOSIT_URL = None
-CROSSREF_DEPOSITOR_EMAIL = 'None'  # This email will receive confirmation/error messages from CrossRef on submission
-CROSSREF_NAMESPACE = 'http://www.crossref.org/schema/4.4.1'
-CROSSREF_SCHEMA_LOCATION = 'http://www.crossref.org/schema/4.4.1 http://www.crossref.org/schemas/crossref4.4.1.xsd'
-CROSSREF_ACCESS_INDICATORS = 'http://www.crossref.org/AccessIndicators.xsd'
-CROSSREF_RELATIONS = 'http://www.crossref.org/relations.xsd'
-CROSSREF_SCHEMA_VERSION = '4.4.1'
-JATS_NAMESPACE = 'http://www.ncbi.nlm.nih.gov/JATS1'
-CROSSREF_DEPOSITOR_NAME = 'Open Science Framework'
-
-
+SCHEMA_LOCATION = 'http://datacite.org/schema/kernel-4 http://schema.datacite.org/meta/kernel-4/metadata.xsd'
 E = lxml.builder.ElementMaker(nsmap={
-    None: DATACITE_NAMESPACE,
+    None: NAMESPACE,
     'xsi': XSI},
 )
 
 CREATOR = E.creator
 CREATOR_NAME = E.creatorName
+SUBJECT_SCHEME = 'bepress Digital Commons Three-Tiered Taxonomy'
 
 # From https://stackoverflow.com/a/19016117
 # lxml does not accept strings with control characters
@@ -60,11 +43,11 @@ def datacite_metadata(doi, title, creators, publisher, publication_year, pretty_
         E.publicationYear(str(publication_year)),
     )
     # set xsi:schemaLocation
-    root.attrib['{%s}schemaLocation' % XSI] = DATACITE_SCHEMA_LOCATION
+    root.attrib['{%s}schemaLocation' % XSI] = SCHEMA_LOCATION
     return lxml.etree.tostring(root, pretty_print=pretty_print)
 
 
-def datacite_format_contributor(contributor):
+def format_contributor(contributor):
     return remove_control_characters(u'{}, {}'.format(contributor.family_name, contributor.given_name))
 
 
@@ -75,7 +58,7 @@ def datacite_metadata_for_node(node, doi, pretty_print=False):
     :param Node node
     :param str doi
     """
-    creators = [datacite_format_contributor(each) for each in node.visible_contributors]
+    creators = [format_contributor(each) for each in node.visible_contributors]
     return datacite_metadata(
         doi=doi,
         title=node.title,
@@ -86,10 +69,10 @@ def datacite_metadata_for_node(node, doi, pretty_print=False):
     )
 
 
-def datacite_format_creators(preprint):
+def format_creators(preprint):
     creators = []
     for contributor in preprint.node.visible_contributors:
-        creator = CREATOR(E.creatorName(datacite_format_contributor(contributor)))
+        creator = CREATOR(E.creatorName(format_contributor(contributor)))
         creator.append(E.givenName(remove_control_characters(contributor.given_name)))
         creator.append(E.familyName(remove_control_characters(contributor.family_name)))
         creator.append(E.nameIdentifier(contributor.absolute_url, nameIdentifierScheme='OSF', schemeURI=settings.DOMAIN))
@@ -105,112 +88,8 @@ def datacite_format_creators(preprint):
     return creators
 
 
-def datacite_format_subjects(preprint):
-    return [E.subject(subject, subjectScheme=DATACITE_SUBJECT_SCHEME) for subject in preprint.subjects.values_list('text', flat=True)]
-
-
-def crossref_format_contributors(element, preprint):
-    contributors = []
-    for index, contributor in enumerate(preprint.node.visible_contributors):
-        if index == 0:
-            sequence = 'first'
-        else:
-            sequence = 'additional'
-
-        person = element.person_name(sequence=sequence, contributor_role='author')
-        contributor_given_plus_middle = remove_control_characters(
-            ' '.join([contributor.given_name, contributor.middle_names]).strip()
-        )
-        person.append(element.given_name(contributor_given_plus_middle))
-        person.append(element.surname(remove_control_characters(contributor.family_name)))
-        if contributor.suffix:
-            person.append(element.suffix(remove_control_characters(contributor.suffix)))
-
-        contributors.append(person)
-
-    return contributors
-
-
-def format_date_crossref(element, date):
-    elements = [
-        element.month(date.strftime('%m')),
-        element.day(date.strftime('%d')),
-        element.year(date.strftime('%Y'))
-    ]
-    return elements
-
-
-def crossref_metadata_for_preprint(preprint, doi, pretty_print=False, **kwargs):
-    """Return the crossref metadata XML document for a given preprint as a string for DOI minting purposes
-
-    :param preprint -- the preprint
-    """
-    if kwargs.get('status', '') == 'unavailable':
-        return ''
-
-    element = lxml.builder.ElementMaker(nsmap={
-        None: CROSSREF_NAMESPACE,
-        'xsi': XSI},
-    )
-
-    head = element.head(
-        element.doi_batch_id(preprint._id),
-        element.timestamp(str(int(time.time()))),
-        element.depositor(
-            element.depositor_name(CROSSREF_DEPOSITOR_NAME),
-            element.email_address(CROSSREF_DEPOSITOR_EMAIL)
-        ),
-        element.registrant('Center for Open Science')
-    )
-
-    posted_content = element.posted_content(
-        element.group_title(preprint.provider.name),
-        element.contributors(*crossref_format_contributors(element, preprint)),
-        element.titles(element.title(preprint.node.title)),
-        element.posted_date(*format_date_crossref(element, preprint.date_published)),
-        element.item_number('osf.io/{}'.format(preprint._id)),
-        type='preprint'
-    )
-
-    if preprint.node.description:
-        posted_content.append(element.abstract(element.p(preprint.node.description), xmlns=JATS_NAMESPACE))
-
-    if preprint.license and preprint.license.node_license.url:
-        posted_content.append(
-            element.program(
-                element.license_ref(preprint.license.node_license.url, start_date=preprint.date_published.strftime('%Y-%m-%d')),
-                xmlns=CROSSREF_ACCESS_INDICATORS
-            )
-        )
-
-    if preprint.node.preprint_article_doi:
-        posted_content.append(
-            element.program(
-                element.related_item(
-                    element.intra_work_relation(
-                        preprint.node.preprint_article_doi,
-                        **{'relationship-type': 'isPreprintOf', 'identifier-type': 'doi'}
-                    ),
-                    xmlns=CROSSREF_RELATIONS
-                )
-            )
-        )
-
-    doi_data = [
-        element.doi(doi),
-        element.resource(settings.DOMAIN + preprint._id)
-    ]
-    posted_content.append(element.doi_data(*doi_data))
-
-    root = element.doi_batch(
-        head,
-        element.body(posted_content),
-        version=CROSSREF_SCHEMA_VERSION
-    )
-
-    # set xsi:schemaLocation
-    root.attrib['{%s}schemaLocation' % XSI] = CROSSREF_SCHEMA_LOCATION
-    return lxml.etree.tostring(root, pretty_print=pretty_print)
+def format_subjects(preprint):
+    return [E.subject(subject, subjectScheme=SUBJECT_SCHEME) for subject in preprint.subjects.values_list('text', flat=True)]
 
 
 # This function is OSF specific.
@@ -228,8 +107,8 @@ def datacite_metadata_for_preprint(preprint, doi, pretty_print=False):
     root = E.resource(
         E.resourceType('Preprint', resourceTypeGeneral='Text'),
         E.identifier(doi, identifierType='DOI'),
-        E.subjects(*datacite_format_subjects(preprint)),
-        E.creators(*datacite_format_creators(preprint)),
+        E.subjects(*format_subjects(preprint)),
+        E.creators(*format_creators(preprint)),
         E.titles(E.title(remove_control_characters(preprint.node.title))),
         E.publisher(preprint.provider.name),
         E.publicationYear(str(getattr(preprint.date_published, 'year'))),
@@ -244,5 +123,5 @@ def datacite_metadata_for_preprint(preprint, doi, pretty_print=False):
     if preprint.article_doi:
         root.append(E.relatedIdentifiers(E.relatedIdentifier(settings.DOI_URL_PREFIX + preprint.article_doi, relatedIdentifierType='URL', relationType='IsPreviousVersionOf'))),
     # set xsi:schemaLocation
-    root.attrib['{%s}schemaLocation' % XSI] = DATACITE_SCHEMA_LOCATION
+    root.attrib['{%s}schemaLocation' % XSI] = SCHEMA_LOCATION
     return lxml.etree.tostring(root, pretty_print=pretty_print)
