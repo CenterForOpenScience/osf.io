61cda86efdbb0e2238e62b9917def3173a416a13
John Tordoff <john@cos.io>
2018-05-29 16:25:21 -0400

    make storageRegion a prop

--- website/static/js/addProjectPlugin.js
+++ website/static/js/addProjectPlugin.js
@@ -39,6 +39,7 @@ var AddProject = {
         self.newProjectName = m.prop('');
         self.newProjectDesc = m.prop('');
         self.newProjectStorageLocation = m.prop(window.contextVars.storageRegions[0]); // first storage region is default
+        self.storageRegions = m.prop(window.contextVars.storageRegions);
         self.newProjectCategory = m.prop(self.defaultCat);
         self.newProjectTemplate = m.prop('');
         self.newProjectInheritContribs = m.prop(false);
@@ -232,7 +233,10 @@ var AddProject = {
                         m('.form-group.m-v-sm', [
                             m('row',
                                 m('f-w-lg.text-bigger', 'Storage location'),
-                                m.component(SelectStorageLocation, {value: ctrl.newProjectStorageLocation})
+                                m.component(SelectStorageLocation, {
+                                    value: ctrl.newProjectStorageLocation,
+                                    locations: ctrl.storageRegions
+                                })
                             )
                             ]
                         ),
@@ -486,7 +490,7 @@ var SelectStorageLocation = {
     view: function(ctrl, options) {
         return m('select.p-t-sm', {config: SelectStorageLocation.config(options)},
             [
-            window.contextVars.storageRegions.map(function(region) {
+            options.locations().map(function(region) {
                 var args = {value: region._id};
                 return m('option', args, region.name);
             })
@@ -500,7 +504,7 @@ var SelectStorageLocation = {
                     var id = $el.select2('val');
                     m.startComputation();
                     //Set the value to the selected option
-                    window.contextVars.storageRegions.map(function (location) {
+                    ctrl.locations().map(function (location) {
                         if (location._id === id) {
                             ctrl.value(location);
                         }
