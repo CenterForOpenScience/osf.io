62f299d2214c4b1bf4130a166ca57d0b5e639758
John Tordoff <john@cos.io>
2018-05-29 14:37:36 -0400

    move disable back button

--- website/static/js/markdown.js
+++ website/static/js/markdown.js
@@ -35,7 +35,7 @@ var mfrURL = window.contextVars.mfrURL;
 var osfURL = window.contextVars.osfURL;
 
 var getMfrUrl = function (guid) {
-    return mfrURL + 'render?url='+ osfURL + guid + '/?action=download%26mode=render'
+    return mfrURL + 'render?url='+ osfURL + guid + '/?action=download%26mode=render';
 };
 
 // Full markdown renderer for views / wiki pages / pauses between typing
@@ -90,35 +90,3 @@ module.exports = {
     quick: markdownQuick,
     old: markdownOld
 };
-
-// Disable backspace sending you back a page in firefox. This is just a usability fix because users
-// tend to click out of the text box while it loads MFR embeds, then press backspace, believing the
-// cursor is still active.
-// https://stackoverflow.com/questions/1495219/how-can-i-prevent-the-backspace-key-from-navigating-back
-$(document).unbind('keydown').bind('keydown', function (event) {
-    if (event.keyCode === 8) {
-        var doPrevent = true;
-        var types = ['text', 'password', 'file', 'search', 'email', 'number', 'date', 'color', 'datetime', 'datetime-local', 'month', 'range', 'search', 'tel', 'time', 'url', 'week'];
-        var d = $(event.srcElement || event.target);
-        var disabled = d.prop('readonly') || d.prop('disabled');
-        if (!disabled) {
-            if (d[0].isContentEditable) {
-                doPrevent = false;
-            } else if (d.is('input')) {
-                var type = d.attr('type');
-                if (type) {
-                    type = type.toLowerCase();
-                }
-                if (types.indexOf(type) > -1) {
-                    doPrevent = false;
-                }
-            } else if (d.is('textarea')) {
-                doPrevent = false;
-            }
-        }
-        if (doPrevent) {
-            event.preventDefault();
-            return false;
-        }
-    }
-});
