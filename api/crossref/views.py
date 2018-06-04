c7e2b733b76fe3e7a534b9b5b8e1c8de95fdba5a
erinspace <erin.braswell@gmail.com>
2018-06-04 17:42:10 -0400

    Add logging for success, updates, and errors

--- api/crossref/views.py
+++ api/crossref/views.py
@@ -1,5 +1,6 @@
-import lxml.etree
+import logging
 
+import lxml.etree
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.views import APIView
@@ -8,6 +9,8 @@ from api.crossref.permissions import RequestComesFromMailgun
 from framework.auth.views import mails
 from osf.models import PreprintService
 from website import settings
+
+logger = logging.getLogger(__name__)
 
 
 class ParseCrossRefConfirmation(APIView):
@@ -33,15 +36,22 @@ class ParseCrossRefConfirmation(APIView):
         preprint_guid = crossref_email_content.find('batch_id').text
         preprint = PreprintService.load(preprint_guid)
         registered_doi = crossref_email_content.find('record_diagnostic/doi').text
+        message = crossref_email_content.find('record_diagnostic/msg').text.lower()
 
         success = record_status == 'completed' and batch_status == 'success'
+        updated = success and message == 'successfully updated'
 
         # Set the DOI on the preprint if this is not just a metadata update
-        if success:
+        if success and not updated:
+            logger.info('Success email recieved from CrossRef for preprint {}'.format())
             if not preprint.get_identifier_value('doi'):
                 preprint.set_identifier_value(category='doi', value=registered_doi)
 
+        elif updated:
+            logger.info('Metadata for preprint {} successfully updated with CrossRef'.format(preprint_guid))
+
         else:
+            logger.error('Error submitting metdata for preprint {} with CrossRef, email sent to help desk'.format(preprint_guid))
             mails.send_mail(
                 to_addr=settings.OSF_SUPPORT_EMAIL,
                 mail=mails.CROSSREF_ERROR,
