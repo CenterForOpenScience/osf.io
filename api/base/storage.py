11e22c7182e71ff6e4b1714c5ced237e7647aed7
Yuhuai Liu <yuhuailiu@Admins-MacBook-Pro-4.local>
2018-06-07 15:58:17 -0400

    add setup for dev

--- api/base/storage.py
+++ api/base/storage.py
@@ -9,7 +9,7 @@ class DevFileSystemStorage(FileSystemStorage):
 
     def url(self, name):
         if self.base_url is None:
-            raise ValueError("This file is not accessible via a URL.")
+            raise ValueError('This file is not accessible via a URL.')
         url = filepath_to_uri(name)
         if url is not None:
             url = url.lstrip('/')
