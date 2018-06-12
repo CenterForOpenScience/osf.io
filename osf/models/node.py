217e00c7f40be7fe41474b55b55de83b2bde5b35
Steven Loria <sloria1@gmail.com>
2018-06-12 17:35:18 -0400

    Fix adding a user by email in preprints when email is already registered...
    ...as a secondary email.
    
    Steps:
    
    * Register email A
    * Add email B as a confirmed, secondary email
    * Create a preprint, inviting email B as an unregistered contributor
    
    Expected:
    
    The registered user (with username A) is added as a contributor to the
    preprint/node.
    
    Actual:
    
    There is an unregistered contributor with email B as the username, even
    though email B is a secondary email for a registered user. This
    prevents them from adding new contributors to the project.
    
    https://www.flowdock.com/app/cos/archiver/threads/6Az9BWQBQPlk_HELqquwhMwEJ9b

--- osf/models/node.py
+++ osf/models/node.py
@@ -1332,19 +1332,18 @@ class AbstractNode(DirtyFieldsMixin, TypedModel, AddonModelMixin, IdentifierMixi
             contributor, _ = self.add_contributor(contributor=contributor, auth=auth, visible=bibliographic,
                                  permissions=permissions, send_email=send_email, save=True)
         else:
-
-            try:
+            contributor = get_user(email=email)
+            if contributor:
+                if self.contributor_set.filter(user=contributor).exists():
+                    raise ValidationValueError('{} is already a contributor.'.format(contributor.fullname))
+                self.add_contributor(contributor=contributor, auth=auth, visible=bibliographic,
+                                    send_email=send_email, permissions=permissions, save=True)
+            else:
                 contributor = self.add_unregistered_contributor(
                     fullname=full_name, email=email, auth=auth,
                     send_email=send_email, permissions=permissions,
                     visible=bibliographic, save=True
                 )
-            except ValidationError:
-                contributor = get_user(email=email)
-                if self.contributor_set.filter(user=contributor).exists():
-                    raise ValidationValueError('{} is already a contributor.'.format(contributor.fullname))
-                self.add_contributor(contributor=contributor, auth=auth, visible=bibliographic,
-                                     send_email=send_email, permissions=permissions, save=True)
 
         auth.user.email_last_sent = timezone.now()
         auth.user.save()
