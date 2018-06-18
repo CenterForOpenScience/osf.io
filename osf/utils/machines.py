33ec2aa27a935e95e51cdee85a241f1fd004fd71
Matt Frazier <maf7sm@virginia.edu>
2018-06-18 16:37:15 -0400

    Add migration -refactor some refactoring/groundwork
    [IN-280]

--- osf/utils/machines.py
+++ osf/utils/machines.py
@@ -243,28 +243,27 @@ class PreprintRequestMachine(BaseMachine):
         """
         if ev.event.name == DefaultTriggers.EDIT_COMMENT.value and self.action is not None:
             self.machineable.comment = self.action.comment
+        elif ev.event_name == DefaultTriggers.SUBMIT.value:
+            # If the provider is pre-moderated and target has not been through moderation, auto approve withdrawal
+            if self.auto_approval_allowed():
+                self.target.run_withdraw(user=self.machineable.creator, comment=self.action.comment)
+        elif ev.event.name == DefaultTriggers.ACCEPT.value:
+            # If moderator accepts the withdrawal request
+            self.target.run_withdraw(user=self.machineable.creator, comment=self.action.comment)
         self.machineable.save()
 
-        # If the provider is pre-moderated and target has not been through moderation, auto approve withdrawal
-        if self.auto_approval_allowed(ev):
-            self.target.run_withdraw(user=self.machineable.creator, comment='Auto approval.')
-        # If moderator accepts the withdrawal request
-        elif ev.event.name == DefaultTriggers.ACCEPT.value:
-            self.target.run_withdraw(user=self.machineable.creator, comment=self.action.comment)
-
-    def auto_approval_allowed(self, ev):
+    def auto_approval_allowed(self):
         # Returns True if the provider is pre-moderated and the preprint is never public.
-        return ev.event_name == DefaultTriggers.SUBMIT.value \
-               and self.machineable.target.provider.reviews_workflow == Workflows.PRE_MODERATION.value \
+        return self.machineable.target.provider.reviews_workflow == Workflows.PRE_MODERATION.value \
                and not self.machineable.target.ever_public
 
     def notify_submit(self, ev):
         # TODO: [IN-284]
-        raise NotImplementedError
+        pass
 
     def notify_accept_reject(self, ev):
         # TODO: [IN-331]
-        raise NotImplementedError
+        pass
 
     def get_context(self):
         return {
