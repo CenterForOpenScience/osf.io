8874ac73c2fd781045f84990b4961ad01dfe8cc0
Matt Frazier <maf7sm@virginia.edu>
2018-06-18 16:37:15 -0400

    Add migration -refactor some refactoring/groundwork
    [IN-280]

--- osf/models/action.py
+++ osf/models/action.py
@@ -32,7 +32,7 @@ class BaseAction(ObjectIDMixin, BaseModel):
 
 
 class ReviewAction(BaseAction):
-    target = models.ForeignKey('PreprintService', related_name='review_actions', on_delete=models.CASCADE)
+    target = models.ForeignKey('PreprintService', related_name='actions', on_delete=models.CASCADE)
 
     trigger = models.CharField(max_length=31, choices=ReviewTriggers.choices())
     from_state = models.CharField(max_length=31, choices=ReviewStates.choices())
@@ -40,7 +40,7 @@ class ReviewAction(BaseAction):
 
 
 class NodeRequestAction(BaseAction):
-    target = models.ForeignKey('NodeRequest', related_name='node_request_actions', on_delete=models.CASCADE)
+    target = models.ForeignKey('NodeRequest', related_name='actions', on_delete=models.CASCADE)
     permissions = models.CharField(
         max_length=5,
         choices=[(permission, permission.title()) for permission in permissions.PERMISSIONS],
@@ -50,9 +50,4 @@ class NodeRequestAction(BaseAction):
 
 
 class PreprintRequestAction(BaseAction):
-    target = models.ForeignKey('PreprintRequest', related_name='preprint_request_actions', on_delete=models.CASCADE)
-    permissions = models.CharField(
-        max_length=5,
-        choices=[(permission, permission.title()) for permission in permissions.PERMISSIONS],
-        default=permissions.READ
-    )
+    target = models.ForeignKey('PreprintRequest', related_name='actions', on_delete=models.CASCADE)
