b8ccccd8c3027ea468e6206638c5e1c37ea5e52b
Yuhuai Liu <yuhuailiu@Admins-MacBook-Pro-4.local>
2018-06-28 12:15:56 -0400

    add contributor emails, modify save_changes() for PreprintRequestMachine

--- website/mails/mails.py
+++ website/mails/mails.py
@@ -421,3 +421,18 @@ CROSSREF_ERROR = Mail(
     'crossref_doi_error',
     subject='There was an error creating a DOI for preprint(s). batch_id: ${batch_id}'
 )
+
+CROSSREF_CSV = Mail(
+    'crossref_csv',
+    subject='[auto] Here is a CSV of DOIs related to crossref: ${csv_type}'
+)
+
+PREPRINT_DOI_CHANGE = Mail(
+    'preprint_doi_change',
+    subject='Improvements to OSF Preprints DOIs'
+)
+
+PREPRINT_WITHDRAWAL_REQUEST_GRANTED = Mail(
+    'preprint_withdrawal_request_granted',
+    subject='Your ${preprint.provider.preprint_word} has been withdrawn',
+)
