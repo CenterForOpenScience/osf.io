8241bf98896acd68193f8c3c0ba12335d0a0f0de
erinspace <erin.braswell@gmail.com>
2018-07-05 13:37:21 -0400

    Iterate over files instead of all quickfiles nodes

--- scripts/remove_after_use/reindex_quickfiles.py
+++ scripts/remove_after_use/reindex_quickfiles.py
@@ -7,12 +7,12 @@ from website.app import setup_django
 setup_django()
 
 from website.search.search import update_file
-from osf.models import QuickFilesNode
+from addons.osfstorage.models import OsfStorageFile
 
-PAGE_SIZE = 50
+PAGE_SIZE = 100
 
 def reindex_quickfiles(dry):
-    qs = QuickFilesNode.objects.all().order_by('id')
+    qs = OsfStorageFile.objects.filter(node__type='osf.quickfilesnode').order_by('id')
     count = qs.count()
     paginator = Paginator(qs, PAGE_SIZE)
 
@@ -20,10 +20,9 @@ def reindex_quickfiles(dry):
     n_processed = 0
     for page_num in paginator.page_range:
         page = paginator.page(page_num)
-        for quickfiles in page.object_list:
-            for file_ in quickfiles.files.all():
-                if not dry:
-                    update_file(file_)
+        for quickfile in page.object_list:
+            if not dry:
+                update_file(quickfile)
         n_processed += len(page.object_list)
         progress_bar.update(n_processed)
 
