e92dfef426dbc385625b91dafe8568da15596206
John Tordoff <john@cos.io>
2018-07-09 12:07:14 -0400

    fix typo

--- osf/models/node.py
+++ osf/models/node.py
@@ -1190,7 +1190,7 @@ class AbstractNode(DirtyFieldsMixin, TypedModel, AddonModelMixin, IdentifierMixi
             raise ValidationValueError('Deactivated users cannot be added as contributors.')
 
         if not contrib_to_add.is_registered and not contrib_to_add.unclaimed_records:
-            raise ValidationValueError('Unregistered users cannot be added as contributors without unclaimed records')
+            raise ValidationValueError('Unregistered users cannot be added as contributors without unclaimed records.')
 
         if not self.is_contributor(contrib_to_add):
 
