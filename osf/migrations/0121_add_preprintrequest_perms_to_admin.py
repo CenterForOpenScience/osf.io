7b3078cbc346dbad93f850987a3e0a13dc09c848
Matt Frazier <maf7sm@virginia.edu>
2018-07-23 10:47:03 -0400

    Fix migration

--- osf/migrations/0121_add_preprintrequest_perms_to_admin.py
+++ osf/migrations/0121_add_preprintrequest_perms_to_admin.py
@@ -5,12 +5,15 @@ from __future__ import unicode_literals
 from django.db import migrations
 from django.contrib.auth.models import Group
 from django.contrib.auth.models import Permission
+from django.core.management.sql import emit_post_migrate_signal
 
 
 def get_preprint_requests_perms():
     return Permission.objects.filter(codename__endswith='_preprintrequest').exclude(codename='add_preprintrequest')
 
 def add_to_osf_admin_group_permissions(*args):
+    # this is to make sure that the permissions created in an earlier migration exist!
+    emit_post_migrate_signal(2, False, 'default')
 
     # Add preprintrequests permissions to OSF Admin group
     admin_group = Group.objects.get(name='osf_admin')
@@ -28,7 +31,7 @@ def remove_from_osf_admin_group_permissions(*args):
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('osf', '0119_add_registrationprovider_perms_to_admin'),
+        ('osf', '0120_merge_20180716_1457'),
     ]
 
     operations = [
