6347669b9cc527e4135e1508d643e2e8d692bdf0
Dawn Pattison <pattison.dawn.r@gmail.com>
2018-07-24 13:08:01 -0500

    Modify tests now that legacy dashboard/index views are not served.

--- tests/test_webtests.py
+++ tests/test_webtests.py
@@ -103,8 +103,7 @@ class TestAUser(OsfTestCase):
     def test_is_redirected_to_dashboard_if_already_logged_in_at_login_page(self):
         res = self.app.get('/login/', auth=self.user.auth)
         assert_equal(res.status_code, 302)
-        res = res.follow(auth=self.user.auth)
-        assert_equal(res.request.path, '/dashboard/')
+        assert 'dashboard' in res.headers.get('Location')
 
     def test_register_page(self):
         res = self.app.get('/register/')
@@ -113,8 +112,7 @@ class TestAUser(OsfTestCase):
     def test_is_redirected_to_dashboard_if_already_logged_in_at_register_page(self):
         res = self.app.get('/register/', auth=self.user.auth)
         assert_equal(res.status_code, 302)
-        res = res.follow(auth=self.user.auth)
-        assert_equal(res.request.path, '/dashboard/')
+        assert 'dashboard' in res.headers.get('Location')
 
     def test_sees_projects_in_her_dashboard(self):
         # the user already has a project
@@ -127,11 +125,11 @@ class TestAUser(OsfTestCase):
     def test_logged_in_index_route_renders_home_template(self):
         res = self.app.get('/', auth=self.user.auth)
         assert_equal(res.status_code, 200)
-        assert_in('My Projects', res)  # Will change once home page populated
+        assert_in('ember', res)  # Will change once home page populated
 
     def test_logged_out_index_route_renders_landing_page(self):
         res = self.app.get('/')
-        assert_in('Simplified Scholarly Collaboration', res)
+        assert_in('ember', res)
 
     def test_does_not_see_osffiles_in_user_addon_settings(self):
         res = self.app.get('/settings/addons/', auth=self.auth, auto_follow=True)
@@ -146,12 +144,11 @@ class TestAUser(OsfTestCase):
         res = self.app.get('/{0}/addons/'.format(project._primary_key), auth=self.auth, auto_follow=True)
         assert_in('OSF Storage', res)
 
-    def test_sees_correct_title_home_page(self):
+    def test_sees_correct_title_home_page_ember(self):
         # User goes to homepage
         res = self.app.get('/', auto_follow=True)
         title = res.html.title.string
-        # page title is correct
-        assert_equal('OSF | Home', title)
+        assert_in('ember', res)
 
     def test_sees_correct_title_on_dashboard(self):
         # User goes to dashboard
