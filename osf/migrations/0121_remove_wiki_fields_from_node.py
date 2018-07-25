36e5723ad576ee2db483733edbb41b09fa8246c6
Dawn Pattison <pattison.dawn.r@gmail.com>
2018-07-25 08:30:46 -0500

    Move and rename osf node wiki fields migration.

--- osf/migrations/0121_remove_wiki_fields_from_node.py
+++ osf/migrations/0121_remove_wiki_fields_from_node.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('osf', '0098_merge_20180416_1807'),
+        ('osf', '0120_merge_20180716_1457'),
     ]
 
     operations = [
