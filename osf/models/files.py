0d210b35acb504a461bdd6d2eff3d19e2e538389
Steven Loria <sloria1@gmail.com>
2018-08-01 17:37:09 -0400

    Add index for BaseFileNode.target

--- osf/models/files.py
+++ osf/models/files.py
@@ -117,6 +117,9 @@ class BaseFileNode(TypedModel, CommentableMixin, OptionalGuidMixin, Taggable, Ob
 
     class Meta:
         base_manager_name = 'objects'
+        index_together = (
+            ('target_content_type', 'target_object_id', )
+        )
 
     @property
     def history(self):
