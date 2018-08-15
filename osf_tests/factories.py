578bf8864ff1ebd709e979f95be47525310d5933
John Tordoff <john@cos.io>
2018-08-15 15:49:29 -0400

    update factory for py3 bytes

--- osf_tests/factories.py
+++ osf_tests/factories.py
@@ -772,14 +772,14 @@ class NodeRelationFactory(DjangoModelFactory):
 class ExternalAccountFactory(DjangoModelFactory):
     class Meta:
         model = models.ExternalAccount
-    oauth_key = 'some-silly-key'
-    oauth_secret = 'some-super-secret'
+    oauth_key = b'some-silly-key'
+    oauth_secret = b'some-super-secret'
     provider = 'mock2'
     provider_id = factory.Sequence(lambda n: 'user-{0}'.format(n))
     provider_name = 'Fake Provider'
-    display_name = factory.Sequence(lambda n: 'user-{0}'.format(n))
-    profile_url = 'http://wutwut.com/'
-    refresh_token = 'some-sillier-key'
+    display_name = factory.Sequence(lambda n: 'user-{0}'.format(n).encode('utf-8'))
+    profile_url = b'http://wutwut.com/'
+    refresh_token = b'some-sillier-key'
 
 
 class MockOAuth2Provider(models.ExternalProvider):
