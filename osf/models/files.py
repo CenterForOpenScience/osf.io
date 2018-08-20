770c3e04bc40c974ea7dff47da799a324f19eabc
erinspace <erin.braswell@gmail.com>
2018-08-20 11:55:00 -0400

    Add region fk to FileVersion, add migration
    [#OSF-1053]

--- osf/models/files.py
+++ osf/models/files.py
@@ -735,6 +735,7 @@ class FileVersion(ObjectIDMixin, BaseModel):
     metadata = DateTimeAwareJSONField(blank=True, default=dict)
     location = DateTimeAwareJSONField(default=None, blank=True, null=True, validators=[validate_location])
     seen_by = models.ManyToManyField('OSFUser', through=FileVersionUserMetadata, related_name='versions_seen')
+    region = models.ForeignKey('addons_osfstorage.Region', null=True, blank=True, on_delete=models.CASCADE)
 
     includable_objects = IncludeManager()
 
