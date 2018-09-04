6a41da9593942d4658ed8bb95127d00729522a2a
John Tordoff <john@cos.io>
2018-09-04 09:50:05 -0400

    add backfill script for all 2018 download counts

--- scripts/tests/test_downloads_summary.py
+++ scripts/tests/test_downloads_summary.py
@@ -16,7 +16,6 @@ from scripts.analytics.download_count_summary import DownloadCountSummary
 @pytest.mark.django_db
 class TestDownloadCount(StorageTestCase):
 
-
     def test_download_count(self):
         # Keen does not allow same day requests so we have to do some time traveling to my birthday
         with mock.patch('django.utils.timezone.now', return_value=datetime.datetime(1991, 9, 25).replace(tzinfo=pytz.utc)):
