1b5d4348e7d02d8d3e58e1249080fd46ed98d084
John Tordoff <john@cos.io>
2018-09-04 16:26:41 -0400

    fix unicode and permissions problem

--- scripts/remove_after_use/fix_unclaimed_records.py
+++ scripts/remove_after_use/fix_unclaimed_records.py
@@ -1,5 +1,7 @@
 import sys
 import logging
+
+from framework.auth.core import generate_verification_key
 from website.app import setup_django
 
 setup_django()
@@ -13,10 +15,19 @@ def add_missing_unclaimed_record(user, node, dry_run):
         if user._id in log.params['contributors']:
             if log.user_id:
                 referrer = OSFUser.objects.get(id=log.user_id)
-                user.add_unclaimed_record(node, referrer, user.fullname)
+                verification_key = generate_verification_key(verification_type='confirm')
+                record = {
+                    'name': user.given_name,
+                    'referrer_id': referrer._id,
+                    'token': verification_key['token'],
+                    'expires': verification_key['expires'],
+                    'email': None,
+                }
+                user.unclaimed_records[node._id] = record
+
                 if not dry_run:
                     user.save()
-                logger.info('User {} has been given an unclaimed record with name {} for node {}'.format(user._id, user.fullname, node._id))
+                logger.info(u'User {} has been given an unclaimed record with name {} for node {}'.format(user._id, user.fullname, node._id))
             else:
                 logger.info('User {} has could not be given a record, because their referer was anonymous'.format(user._id))
 
