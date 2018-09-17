9328c1b7ad0bf499e26b52b3dba0904528a954e9
erinspace <erin.braswell@gmail.com>
2018-09-17 10:30:32 -0400

    Add back format creator and subject utils from @caseyrollins

---
+++ osf/metadata/utils.py
@@ -0,0 +1,86 @@
+from website import settings
+
+
+SUBJECT_SCHEME = 'bepress Digital Commons Three-Tiered Taxonomy'
+
+RESOURCE_TYPE_MAP = {
+    'Audio/Video': 'Audiovisual',
+    'Dataset': 'Dataset',
+    'Image': 'Image',
+    'Model': 'Model',
+    'Software': 'Software',
+    'Book': 'Text',
+    'Funding Submission': 'Text',
+    'Journal Article': 'Text',
+    'Lesson': 'Text',
+    'Poster': 'Text',
+    'Preprint': 'Text',
+    'Presentation': 'Text',
+    'Research Tool': 'Text',
+    'Thesis': 'Text',
+    'Other': 'Text',
+    '(unas)': 'Other'
+}
+
+
+def datacite_format_contributors(contributors):
+    """
+    contributors_list: list of OSFUsers to format
+
+    returns: formatted json for datacite
+    """
+    creators = []
+    for contributor in contributors:
+        name_identifiers = [
+            {
+                'nameIdentifier': contributor.absolute_url,
+                'nameIdentifierScheme': 'OSF',
+                'schemeURI': settings.DOMAIN
+            }
+        ]
+
+        if contributor.external_identity.get('ORCID'):
+            verified = contributor.external_identity['ORCID'].values()[0] == 'VERIFIED'
+            if verified:
+                name_identifiers.append({
+                    'nameIdentifier': contributor.external_identity['ORCID'].keys()[0],
+                    'nameIdentifierScheme': 'ORCID',
+                    'schemeURI': 'http://orcid.org/'
+                })
+
+        creators.append({
+            'creatorName': {
+                'creatorName': contributor.fullname,
+                'familyName': contributor.family_name,
+                'givenName': contributor.given_name
+            },
+            'nameIdentifiers': name_identifiers
+        })
+
+    return creators
+
+
+def datacite_format_subjects(subjects):
+    return [
+        {
+            'subject': subject,
+            'subjectScheme': SUBJECT_SCHEME
+        }
+        for subject in subjects
+    ]
+
+
+def datacite_format_identifier(target):
+    identifier = target.get_identifier('doi')
+    if identifier:
+        return {
+            'identifier': identifier.value,
+            'identifierType': 'DOI'
+        }
+
+
+def datacite_format_rights(license):
+    return {
+        'rights': license.name,
+        'rightsURI': license.url
+    }
