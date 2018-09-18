571a676af31a5d7b4ffb8059f4579c0ee5e03b37
Yuhuai Liu <yuhuai@cos.io>
2018-09-18 11:29:55 -0400

    Fix Chronos filtering, url
    Closes #8699
    
    Squashed commit of the following:
    
    commit aee74ad3e98aef4ee6410edb92729476953de609
    Author: Yuhuai Liu <yuhuai@cos.io>
    Date:   Tue Sep 18 10:26:53 2018 -0400
    
        add filters test
    
    commit b4bb1da772f6dd9f14e1969495d43848656dd74c
    Author: Yuhuai Liu <yuhuai@cos.io>
    Date:   Mon Sep 17 17:21:13 2018 -0400
    
        change regex, add filters
    
    commit 236b2537357b6e4ed356e6689da1f1b88c118fd2
    Author: Yuhuai Liu <yuhuai@cos.io>
    Date:   Mon Sep 17 16:08:39 2018 -0400
    
        fix url, change OsfStorageFile query

--- api/chronos/urls.py
+++ api/chronos/urls.py
@@ -6,7 +6,7 @@ app_name = 'osf'
 
 urlpatterns = [
     url(r'^journals/$', views.ChronosJournalList.as_view(), name=views.ChronosJournalList.view_name),
-    url(r'^journals/(?P<journal_id>\w+)/$', views.ChronosJournalDetail.as_view(), name=views.ChronosJournalDetail.view_name),
+    url(r'^journals/(?P<journal_id>[-0-9A-Za-z]+)/$', views.ChronosJournalDetail.as_view(), name=views.ChronosJournalDetail.view_name),
     url(r'^(?P<preprint_id>\w+)/submissions/$', views.ChronosSubmissionList.as_view(), name=views.ChronosSubmissionList.view_name),
     url(r'^(?P<preprint_id>\w+)/submissions/(?P<submission_id>\w+)/$', views.ChronosSubmissionDetail.as_view(), name=views.ChronosSubmissionDetail.view_name),
 ]
--- api_tests/chronos/views/test_chronos_journal_list.py
+++ api_tests/chronos/views/test_chronos_journal_list.py
@@ -29,3 +29,32 @@ class TestChronosJournalList:
     def test_journal_list(self, res, data, journal_ids):
         assert res.status_code == 200
         assert set(journal_ids) == set([datum['id'] for datum in data])
+
+
+@pytest.mark.django_db
+class TestChronosJournalListFilter:
+
+    @pytest.fixture()
+    def journal_one(self):
+        return ChronosJournalFactory()
+
+    @pytest.fixture()
+    def journal_two(self):
+        return ChronosJournalFactory()
+
+    @pytest.fixture()
+    def journal_one_filter_name_url(self, journal_one):
+        return '/_/chronos/journals/?filter[name]={}'.format(journal_one.name)
+
+    @pytest.fixture()
+    def journal_one_filter_title_url(self, journal_one):
+        return '/_/chronos/journals/?filter[title]={}'.format(journal_one.title)
+
+    def test_journal_list_filter(self, app, journal_one, journal_two, journal_one_filter_name_url):
+        res = app.get(journal_one_filter_name_url)
+        assert len(res.json['data']) == 1
+        assert res.json['data'][0]['attributes']['name'] == journal_one.name
+
+        res = app.get(journal_one_filter_title_url)
+        assert len(res.json['data']) == 1
+        assert res.json['data'][0]['attributes']['title'] == journal_one.title
