f17ac2fde38ca712a54886d1a8931f7c29870931
John Tordoff <john@cos.io>
2018-10-02 14:27:15 -0400

    add file logger

--- scripts/remove_after_use/set_meetings_users_fullnames_to_guids.py
+++ scripts/remove_after_use/set_meetings_users_fullnames_to_guids.py
@@ -2,10 +2,10 @@ import sys
 import logging
 
 import django
-from django.db import transaction
 django.setup()
 
 from osf.models import OSFUser
+from scripts import utils as script_utils
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -13,6 +13,8 @@ logging.basicConfig(level=logging.INFO)
 
 def main():
     dry_run = '--dry' in sys.argv
+    if not dry_run:
+        script_utils.add_file_logger(logger, __file__)
     users = OSFUser.objects.filter(fullname__regex=r'^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$', tags__name='osf4m')
     logger.info('{} users found added by OSF 4 Meetings with emails for fullnames'.format(users.count()))
     for user in users:
