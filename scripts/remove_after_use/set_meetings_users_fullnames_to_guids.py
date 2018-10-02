19c4bb5992efd2bd6c32560c352307e7cb9023b8
John Tordoff <john@cos.io>
2018-10-02 14:26:40 -0400

    remove atomic tranaction

--- scripts/remove_after_use/set_meetings_users_fullnames_to_guids.py
+++ scripts/remove_after_use/set_meetings_users_fullnames_to_guids.py
@@ -10,15 +10,16 @@ from osf.models import OSFUser
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
+
 def main():
     dry_run = '--dry' in sys.argv
-    with transaction.atomic():
-        users = OSFUser.objects.filter(fullname__regex=r'^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$', tags__name='osf4m')
-        logger.info('{} users found added by OSF 4 Meetings with emails for fullnames'.format(users.count()))
-        for user in users:
-            user.fullname = user._id
-            if not dry_run:
-                user.save()
+    users = OSFUser.objects.filter(fullname__regex=r'^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$', tags__name='osf4m')
+    logger.info('{} users found added by OSF 4 Meetings with emails for fullnames'.format(users.count()))
+    for user in users:
+        user.fullname = user._id
+        if not dry_run:
+            user.save()
+
 
 if __name__ == '__main__':
     main()
