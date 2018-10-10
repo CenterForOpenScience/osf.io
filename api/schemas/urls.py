cd14704144d064fe27f466b59a2db19a5bd0768d
Fabrice Mizero <fabrice@cos.io>
2018-10-10 12:39:28 -0400

    Add FormBlock API v2 endpoints

--- api/schemas/urls.py
+++ api/schemas/urls.py
@@ -7,4 +7,6 @@ app_name = 'osf'
 urlpatterns = [
     url(r'^registrations/$', views.RegistrationSchemaList.as_view(), name=views.RegistrationSchemaList.view_name),
     url(r'^registrations/(?P<schema_id>\w+)/$', views.RegistrationSchemaDetail.as_view(), name=views.RegistrationSchemaDetail.view_name),
+    url(r'^registrations/(?P<schema_id>\w+)/form_blocks/$', views.RegistrationSchemaFormBlocks.as_view(), name=views.RegistrationSchemaFormBlocks.view_name),
+    url(r'^registrations/(?P<schema_id>\w+)/form_blocks/(?P<form_block_id>\w+)/$', views.RegistrationSchemaFormBlockDetail.as_view(), name=views.RegistrationSchemaFormBlockDetail.view_name),
 ]
--- api/schemas/views.py
+++ api/schemas/views.py
@@ -1,5 +1,5 @@
 import waffle
-from rest_framework import generics, permissions as drf_permissions
+from rest_framework import exceptions, generics, permissions as drf_permissions
 from framework.auth.oauth_scopes import CoreScopes
 
 from website.project.metadata.schemas import LATEST_SCHEMA_VERSION
@@ -8,9 +8,12 @@ from api.base.views import JSONAPIBaseView
 from api.base.utils import get_object_or_error
 from api.base.filters import ListFilterMixin
 
-from osf.models import RegistrationSchema
 from osf.features import ENABLE_INACTIVE_SCHEMAS
-from api.schemas.serializers import RegistrationSchemaSerializer
+from osf.models import RegistrationFormBlock, RegistrationSchema
+from api.schemas.serializers import (
+    RegistrationSchemaSerializer,
+    RegistrationSchemaFormBlockSerializer,
+)
 
 
 class RegistrationSchemaList(JSONAPIBaseView, generics.ListAPIView, ListFilterMixin):
@@ -26,7 +29,7 @@ class RegistrationSchemaList(JSONAPIBaseView, generics.ListAPIView, ListFilterMi
     required_write_scopes = [CoreScopes.NODE_DRAFT_REGISTRATIONS_WRITE]
 
     serializer_class = RegistrationSchemaSerializer
-    view_category = 'registration-schemas'
+    view_category = 'schemas'
     view_name = 'registration-schema-list'
 
     ordering = ('-id',)
@@ -54,10 +57,49 @@ class RegistrationSchemaDetail(JSONAPIBaseView, generics.RetrieveAPIView):
     required_write_scopes = [CoreScopes.NULL]
 
     serializer_class = RegistrationSchemaSerializer
-    view_category = 'registration-schemas'
+    view_category = 'schemas'
     view_name = 'registration-schema-detail'
 
     # overrides RetrieveAPIView
     def get_object(self):
         schema_id = self.kwargs['schema_id']
         return get_object_or_error(RegistrationSchema, schema_id, self.request)
+
+class RegistrationSchemaFormBlocks(JSONAPIBaseView, generics.ListAPIView):
+
+    permission_classes = (
+        drf_permissions.IsAuthenticatedOrReadOnly,
+        base_permissions.TokenHasScope,
+    )
+
+    required_read_scopes = [CoreScopes.SCHEMA_FORM_BLOCKS_READ]
+    required_write_scopes = [CoreScopes.NULL]
+
+    serializer_class = RegistrationSchemaFormBlockSerializer
+    view_category = 'schemas'
+    view_name = 'registration-schema-form-blocks'
+    ordering = ('_order',)
+
+    def get_queryset(self):
+        schema_id = self.kwargs.get('schema_id')
+        schema = get_object_or_error(RegistrationSchema, schema_id, self.request)
+        if schema.schema_version != LATEST_SCHEMA_VERSION or not schema.active:
+            raise exceptions.ValidationError('Registration schema must be active.')
+        return schema.form_blocks.all()
+
+class RegistrationSchemaFormBlockDetail(JSONAPIBaseView, generics.RetrieveAPIView):
+
+    permission_classes = (
+        drf_permissions.IsAuthenticatedOrReadOnly,
+        base_permissions.TokenHasScope,
+    )
+
+    required_read_scopes = [CoreScopes.SCHEMA_FORM_BLOCKS_READ]
+    required_write_scopes = [CoreScopes.NULL]
+
+    serializer_class = RegistrationSchemaFormBlockSerializer
+    view_category = 'schemas'
+    view_name = 'registration-schema-form-block-detail'
+
+    def get_object(self):
+        return get_object_or_error(RegistrationFormBlock, self.kwargs.get('form_block_id'), self.request)
