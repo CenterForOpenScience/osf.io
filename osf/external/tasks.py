07ba901502467c3292654fb6fbb89444acd27ddf
Yuhuai Liu <yuhuai@cos.io>
2018-10-15 16:33:39 -0400

    add conditional in task

--- osf/external/tasks.py
+++ osf/external/tasks.py
@@ -1,5 +1,8 @@
 from framework.celery_tasks import app as celery_app
 from django.apps import apps
+from django.utils import timezone
+
+from website import settings
 from osf.external.chronos import ChronosClient
 import logging
 
@@ -12,4 +15,5 @@ def update_submissions_status_async(ids):
     ChronosSubmission = apps.get_model('osf.ChronosSubmission')
     for submission_id in ids:
         submission = ChronosSubmission.load(submission_id)
-        client.sync_manuscript(submission)
+        if submission.modified < timezone.now() - settings.CHRONOS_SUBMISSION_UPDATE_TIME:
+            client.sync_manuscript(submission)
