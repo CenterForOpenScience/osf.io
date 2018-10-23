b2e1090a0c7d3c8df524c14c4792328ea0b7c89c
Steven Loria <sloria1@gmail.com>
2018-10-23 16:07:36 -0400

    Only check_metrics if waffle switch is enabled

--- osf/management/commands/check_deploy_ready.py
+++ osf/management/commands/check_deploy_ready.py
@@ -6,15 +6,18 @@ any of them error.
 
 from django.core.management.base import BaseCommand
 from django.core.management import call_command
+import waffle
+from osf import features
 
-
-CHECKS = [
-    ['checkmigrations'],
-    ['check_metrics'],
-]
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
+        CHECKS = [
+            ['checkmigrations'],
+        ]
+
+        if waffle.switch_is_active(features.ELASTICSEARCH_METRICS):
+            CHECKS.append(['check_metrics'])
         for check in CHECKS:
             call_command(*check)
