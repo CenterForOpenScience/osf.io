6d2796287be7e0b2e1b396aad4da00c129be616e
rdm-dev08 <rdm-dev08@ml.itg.hitachi.co.jp>
2018-11-12 13:41:06 +0900

    Separate checkbox and File Path columns

--- website/static/js/pages/timestamp-page.js
+++ website/static/js/pages/timestamp-page.js
@@ -11,27 +11,26 @@ $(document).ready(function(){
     var index = 0;
     for (var i = 0; i < project_file_list.length; i++) {
         var file_list = project_file_list[i].error_list;
-        //console.log(file_list);
         var provider_tr = '<tr><td colspan="4">' + project_file_list[i].provider + '</td></tr>';
         $(provider_tr).appendTo($('#tree_timestamp_error_data'));
-        for (var j = 0; j < file_list.length; j++) { 
-                     var error_tr = '<tr>' +
-                                    '<td class="addTimestamp">' +
-                                    '<input type="checkBox" id="addTimestampCheck" value="' + index + '"/>&nbsp;' + 
-                                    file_list[j].file_path + 
-                                    '<input type="hidden" id="provider" value="' + project_file_list[i].provider + '" />' +
-                                    '<input type="hidden" id="file_id" value="' + file_list[j].file_id + '" />' +
-                                    '<input type="hidden" id="file_path" value="' + file_list[j].file_path + '" />' +
-                                    '<input type="hidden" id="version" value="' + file_list[j].version + '" />' +
-                                    '<input type="hidden" id="file_name" value="' + file_list[j].file_name + '" />' +
-                                    '</td>' +
-                                    '<td>' + file_list[j].operator_user + '</td>' +
-                                    '<td>' + file_list[j].operator_date + '</td>' +
-                                    '<td>' + file_list[j].verify_result_title + '</td>' +
-                                    '</tr>';
-                     //console.log(error_tr);
-                     $(error_tr).appendTo($('#tree_timestamp_error_data'));
-             index++;
+        for (var j = 0; j < file_list.length; j++) {
+            var error_tr =
+                '<tr>' +
+                '<td class="addTimestamp">' +
+                '<input type="checkBox" id="addTimestampCheck" style="width: 13px; height: 13px;" value="' + index + '"/>&nbsp;' +
+                '<td>' + file_list[j].file_path + '</td>' +
+                '<input type="hidden" id="provider" value="' + project_file_list[i].provider + '" />' +
+                '<input type="hidden" id="file_id" value="' + file_list[j].file_id + '" />' +
+                '<input type="hidden" id="file_path" value="' + file_list[j].file_path + '" />' +
+                '<input type="hidden" id="version" value="' + file_list[j].version + '" />' +
+                '<input type="hidden" id="file_name" value="' + file_list[j].file_name + '" />' +
+                '</td>' +
+                '<td>' + file_list[j].operator_user + '</td>' +
+                '<td>' + file_list[j].operator_date + '</td>' +
+                '<td>' + file_list[j].verify_result_title + '</td>' +
+                '</tr>';
+            $(error_tr).appendTo($('#tree_timestamp_error_data'));
+            index++;
         }
      }
 });
