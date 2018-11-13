6a826566b9c686f9b267f179bda73baccf6b6b85
Steven Loria <sloria1@gmail.com>
2018-11-13 13:17:32 -0500

    Fixes to backfill script
    * Temporarily turn off replication and set refresh_interval to -1
      to improve indexing speed
    * Throttle bulk requests

--- scripts/remove_after_use/backfill_elasticsearch_metrics.py
+++ scripts/remove_after_use/backfill_elasticsearch_metrics.py
@@ -1,12 +1,20 @@
+"""Script to backfill PageCounter data into elasticsearch.
+
+* This MUST be run before enabling the elasticsearch_metrics switch.
+* This MUST run to completion without errors. If an error occurs, delete
+  the existing osf_preprint* indices, fix the error, and re-run the script.
+"""
 import logging
 import sys
 import argparse
 import datetime
+import progressbar
+from time import sleep
 
-import django
-django.setup()
+from website.app import setup_django
+setup_django()
 
-from elasticsearch2.helpers import bulk
+from elasticsearch.helpers import bulk
 from elasticsearch_dsl.connections import connections
 
 from osf.models import PageCounter, PreprintService
@@ -16,17 +24,28 @@ from website.search.elastic_search import client
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
+CHUNK_SIZE = 100
+MAX_BATCH_SIZE = 1000
+THROTTLE_PERIOD = 3  # seconds
+REQUEST_TIMEOUT = 30  # seconds
+
 
 def main():
 
     es = connections.get_connection()
 
     dry = '--dry' in sys.argv
+    if not dry:
+        utils.add_file_logger(logger, __file__)
     preprints = PreprintService.objects.all().values_list('node__guids___id', 'node__preprint_file___id', 'guids___id',
                                               'provider___id')
 
+    logger.info('Collecting data on {} preprints...'.format(preprints.count()))
+
+    progress_bar = progressbar.ProgressBar(maxval=preprints.count()).start()
     batch_to_update = []
-    for preprint in preprints:
+    for i, preprint in enumerate(preprints, 1):
+        progress_bar.update(i)
         node__id, file_id, preprint__id, provider__id = preprint
         page_counters = PageCounter.objects.filter(_id__startswith='download:{node__id}:{file_id}:'.format(node__id=node__id,
                                                                                                       file_id=file_id)).values_list('_id', 'date')
@@ -34,24 +53,36 @@ def main():
             page_counter__id, date = page_counter
             version_num = page_counter__id.split(':')[-1]
             for date, totals in date.items():
-                for _ in range(0, totals['total']):
-                    batch_to_update.append({
-                        '_index': 'osf_preprintdownload-{}'.format(date.replace('/', '.')),
-                        '_source': {
-                            'path': '/{}'.format(file_id),
-                            'preprint_id': preprint__id,
-                            'provider_id': provider__id,
-                            'timestamp': datetime.datetime.strptime(date, '%Y/%m/%d'),
-                            'user_id': None,  # Pagecounter never tracked this
-                            'version': int(version_num) + 1
-                        },
-                        '_type': 'doc'
-                    })
+                batch_to_update.append({
+                    '_index': 'osf_preprintdownload-{}'.format(date.replace('/', '.')),
+                    '_source': {
+                        'count': totals['total'],
+                        'path': '/{}'.format(file_id),
+                        'preprint_id': preprint__id,
+                        'provider_id': provider__id,
+                        'timestamp': datetime.datetime.strptime(date, '%Y/%m/%d'),
+                        'user_id': None,  # Pagecounter never tracked this
+                        'version': int(version_num) + 1
+                    },
+                    '_type': 'doc'
+                })
 
+                if len(batch_to_update) >= MAX_BATCH_SIZE:
+                    logger.info('Bulk-indexing data from {} PageCounter records'.format(len(batch_to_update)))
+                    if not dry:
+                        bulk(es, batch_to_update, max_retries=3, chunk_size=CHUNK_SIZE, request_timeout=REQUEST_TIMEOUT)
+                    batch_to_update = []
+                    # Allow elasticsearch to catch up
+                    sleep(THROTTLE_PERIOD)
+
+    # Index final batch
+    if len(batch_to_update):
+        logger.info('Bulk-indexing data from {} PageCounter records'.format(len(batch_to_update)))
+        if not dry:
+            bulk(es, batch_to_update, max_retries=3, chunk_size=CHUNK_SIZE, request_timeout=REQUEST_TIMEOUT)
+
+    progress_bar.finish()
     logger.info('This will migrate {} Pagecounter entries to Elasticsearch'.format(len(batch_to_update)))
-    if not dry:
-        utils.add_file_logger(logger, __file__)
-        bulk(es, batch_to_update)
 
 
 if __name__ == '__main__':
