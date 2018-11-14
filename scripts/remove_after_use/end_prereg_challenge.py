c66c47937c60647de67362f57b2418a06b8eb3ef
John Tordoff <john@cos.io>
2018-11-14 12:25:03 -0500

    make sure osf preregistration is activated by waffle switch

--- scripts/remove_after_use/end_prereg_challenge.py
+++ scripts/remove_after_use/end_prereg_challenge.py
@@ -1,13 +1,16 @@
 import sys
 import logging
 
-from waffle.models import Switch
-
-from framework.celery_tasks import app as celery_app
 from website.app import setup_django
 setup_django()
 
+from waffle.models import Switch
+
+from framework.celery_tasks import app as celery_app
+
+
 from scripts.utils import add_file_logger
+from osf.models import RegistrationSchema
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -17,10 +20,13 @@ def main(dry_run=True):
 
     switch = Switch.objects.get(name='osf_preregistration')
     switch.active = True
+    prereg_schema = RegistrationSchema.objects.get(name='OSF Preregistration')
+    prereg_schema.active = True
     if dry_run:
         logger.warn('This is a dry run')
     else:
         switch.save()
+        prereg_schema.save()
 
 
 @celery_app.task(name='scripts.remove_after_use.end_prereg_challenge')
