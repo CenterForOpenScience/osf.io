2949d7d93e5e5ad6ae196b5fd74276bd813df4b4
John Tordoff <john@cos.io>
2018-11-14 15:19:46 -0500

    add prereg schema with migration

--- osf/migrations/0143_add_prereg_schema_switch.py
+++ osf/migrations/0143_add_prereg_schema_switch.py
@@ -3,14 +3,19 @@ from __future__ import unicode_literals
 from django.db import migrations
 from waffle.models import Switch
 
+from osf.utils.migrations import ensure_schemas
+from osf.features import OSF_PREREGISTRATION
+
 
 def add_prereg_switch(*args, **kwargs):
-    switch = Switch(name='osf_preregistration', active=False)
+    switch = Switch(name=OSF_PREREGISTRATION, active=False)
     switch.save()
+    ensure_schemas()
 
 
 def remove_prereg_switch(*args, **kwargs):
-    Switch.objects.get(name='osf_preregistration').delete()
+    Switch.objects.get(name=OSF_PREREGISTRATION).delete()
+    ensure_schemas()
 
 
 class Migration(migrations.Migration):
