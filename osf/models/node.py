ac1c2c4a733e20f1c4f0af0e26867d1000a2df90
John Tordoff <john@cos.io>
2018-11-16 13:49:47 -0500

    make sure we don't follow node links that register their own parents, when recursively registering registration children.

--- osf/models/node.py
+++ osf/models/node.py
@@ -1826,14 +1826,22 @@ class AbstractNode(DirtyFieldsMixin, TypedModel, AddonModelMixin, IdentifierMixi
 
         for node_relation in original.node_relations.filter(child__is_deleted=False):
             node_contained = node_relation.child
-            if child_ids and node_contained._id not in child_ids:
-                if node_contained.node_relations.filter(child__is_deleted=False, child__guids___id__in=child_ids).exists():
-                    # We can't skip a node with children that we have to register.
-                    raise NodeStateError('The parents of all child nodes being registered must be registered.')
-                continue
 
-            # Register child nodes
-            if not node_relation.is_node_link:
+            if node_relation.is_node_link:
+                NodeRelation.objects.get_or_create(
+                    is_node_link=True,
+                    parent=registered,
+                    child=node_contained
+                )
+                continue
+            else:
+                if child_ids and node_contained._id not in child_ids:
+                    if node_contained.node_relations.filter(child__is_deleted=False, child__guids___id__in=child_ids, is_node_link=False).exists():
+                        # We can't skip a node with children that we have to register.
+                        raise NodeStateError('The parents of all child nodes being registered must be registered.')
+                    continue
+
+                # Register child nodes
                 node_contained.register_node(
                     schema=schema,
                     auth=auth,
@@ -1841,13 +1849,6 @@ class AbstractNode(DirtyFieldsMixin, TypedModel, AddonModelMixin, IdentifierMixi
                     provider=provider,
                     parent=registered,
                     child_ids=child_ids,
-                )
-            else:
-                # Copy linked nodes
-                NodeRelation.objects.get_or_create(
-                    is_node_link=True,
-                    parent=registered,
-                    child=node_contained
                 )
 
         registered.root = None  # Recompute root on save
