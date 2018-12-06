affb78b573f32773fa0bcdb2dd20fa7b59ba4570
erinspace <erin.braswell@gmail.com>
2018-12-06 14:32:33 -0500

    Change language on loading error messages  > persist to persists

--- website/static/js/pages/dashboard-page.js
+++ website/static/js/pages/dashboard-page.js
@@ -47,7 +47,7 @@ $(document).ready(function() {
     // Appears in 10 second if the spinner is still there.
     setTimeout(function(){
         if($('#dashboard>.ball-scale').length > 0) {
-            $('#dashboard').append('<div class="text-danger text-center text-bigger">This is taking longer than normal. <br>  Try reloading the page. If the problem persist, please contact us at ' + OSF_SUPPORT_EMAIL + '.</div>');
+            $('#dashboard').append('<div class="text-danger text-center text-bigger">This is taking longer than normal. <br>  Try reloading the page. If the problem persists, please contact us at ' + OSF_SUPPORT_EMAIL + '.</div>');
         }
     }, 10000);
 });
