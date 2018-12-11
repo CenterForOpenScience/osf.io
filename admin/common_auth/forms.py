48315cb92f1120d897e0456ed1307502adc9b296
John Tordoff <john@cos.io>
2018-12-11 13:32:45 -0500

    exclude new osf groups cluttering up admin app

--- admin/common_auth/forms.py
+++ admin/common_auth/forms.py
@@ -24,7 +24,7 @@ class UserRegistrationForm(forms.Form):
 
     # TODO: Moving to guardian, find a better way to distinguish "admin-like" groups from object permission groups
     group_perms = forms.ModelMultipleChoiceField(
-        queryset=Group.objects.exclude(Q(name__startswith='collections_') | Q(name__startswith='preprint_')),
+        queryset=Group.objects.exclude(Q(name__startswith='collections_') | Q(name__startswith='preprint_') | Q(name__startswith='node_')),
         required=False,
         widget=forms.CheckboxSelectMultiple
     )
