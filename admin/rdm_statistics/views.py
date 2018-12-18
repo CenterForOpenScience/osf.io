851b7d438f4e4e2f46c8f86bfa7b768fb797e97e
rdm-dev03 <rdm-dev03@ml.itg.hitachi.co.jp>
2018-12-18 21:07:27 +0900

    pass approximate_size when filesize over YB(yotta-byte 10^24 bytes)

--- admin/rdm_statistics/views.py
+++ admin/rdm_statistics/views.py
@@ -764,7 +764,7 @@ def approximate_size(size, a_kilobyte_is_1024_bytes=True):
         if size < multiple:
             return '{0:.1f} {1}'.format(size, suffix)
 
-    raise ValueError('number too large')
+    return '{0:.1f} {1}'.format(size, suffix)
 
 
 ############################################
