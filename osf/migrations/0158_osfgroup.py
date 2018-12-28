4c140528a3ef7e3f370d38f635a6357ebc3e697a
Dawn Pattison <pattison.dawn.r@gmail.com>
2018-12-28 11:37:55 -0500

    Consolidate group related migrations into one file.

--- osf/migrations/0158_osfgroup.py
+++ osf/migrations/0158_osfgroup.py
@@ -62,4 +62,45 @@ class Migration(migrations.Migration):
             name='osfgroupuserobjectpermission',
             unique_together=set([('user', 'permission', 'content_object')]),
         ),
+        migrations.CreateModel(
+            name='OSFGroupLog',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('created', django_extensions.db.fields.CreationDateTimeField(auto_now_add=True, verbose_name='created')),
+                ('modified', django_extensions.db.fields.ModificationDateTimeField(auto_now=True, verbose_name='modified')),
+                ('_id', models.CharField(db_index=True, default=osf.models.base.generate_object_id, max_length=24, unique=True)),
+                ('action', models.CharField(db_index=True, max_length=255)),
+                ('params', osf.utils.datetime_aware_jsonfield.DateTimeAwareJSONField(default=dict, encoder=osf.utils.datetime_aware_jsonfield.DateTimeAwareJSONEncoder)),
+                ('should_hide', models.BooleanField(default=False)),
+            ],
+            options={
+                'ordering': ['-created'],
+                'get_latest_by': 'created',
+            },
+        ),
+        migrations.AddField(
+            model_name='osfgroup',
+            name='last_logged',
+            field=osf.utils.fields.NonNaiveDateTimeField(blank=True, db_index=True, default=django.utils.timezone.now, null=True),
+        ),
+        migrations.AddField(
+            model_name='osfgrouplog',
+            name='group',
+            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='logs', to='osf.OSFGroup'),
+        ),
+        migrations.AddField(
+            model_name='osfgrouplog',
+            name='user',
+            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='group_logs', to=settings.AUTH_USER_MODEL),
+        ),
+        migrations.AddField(
+            model_name='osfuser',
+            name='group_connected_email_records',
+            field=osf.utils.datetime_aware_jsonfield.DateTimeAwareJSONField(blank=True, default=dict, encoder=osf.utils.datetime_aware_jsonfield.DateTimeAwareJSONEncoder),
+        ),
+        migrations.AddField(
+            model_name='osfuser',
+            name='member_added_email_records',
+            field=osf.utils.datetime_aware_jsonfield.DateTimeAwareJSONField(blank=True, default=dict, encoder=osf.utils.datetime_aware_jsonfield.DateTimeAwareJSONEncoder),
+        ),
     ]
