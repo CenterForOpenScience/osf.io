186fd44953fcd0c7323e001367e7b82195843ad7
Dawn Pattison <pattison.dawn.r@gmail.com>
2019-01-02 17:21:31 -0600

    Prevent write contributors from deleting projects in APIv2.

--- api/nodes/permissions.py
+++ api/nodes/permissions.py
@@ -61,6 +61,26 @@ class IsAdmin(permissions.BasePermission):
         return obj.has_permission(auth.user, osf_permissions.ADMIN)
 
 
+class NodeDetailPermissions(permissions.BasePermission):
+    """Permissions for contributor detail page."""
+
+    acceptable_models = (AbstractNode,)
+
+    def has_object_permission(self, request, view, obj):
+        """
+        Write members can edit nodes, but you must be admins to delete
+        """
+        assert_resource_type(obj, self.acceptable_models)
+        auth = get_user_auth(request)
+
+        if request.method in permissions.SAFE_METHODS:
+            return obj.is_public or obj.can_view(auth)
+        elif request.method == 'DELETE':
+            return obj.has_permission(auth.user, osf_permissions.ADMIN)
+        else:
+            return obj.can_edit(auth)
+
+
 class IsContributor(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         assert isinstance(obj, AbstractNode), 'obj must be an Node, got {}'.format(obj)
