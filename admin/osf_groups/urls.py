4dd92543e2576c05db37b9a3bd21c2f0bfca5bcf
Dawn Pattison <pattison.dawn.r@gmail.com>
2019-01-02 11:55:47 -0600

    Reorder admin app OSF Group urls.

--- admin/osf_groups/urls.py
+++ admin/osf_groups/urls.py
@@ -4,7 +4,7 @@ from admin.osf_groups import views
 app_name = 'admin'
 
 urlpatterns = [
+    url(r'^$', views.OSFGroupsListView.as_view(), name='osf_groups_list'),
     url(r'^search/$', views.OSFGroupsFormView.as_view(), name='search'),
     url(r'^(?P<id>[a-z0-9]+)/$', views.OSFGroupsView.as_view(), name='osf_group'),
-    url(r'^$', views.OSFGroupsListView.as_view(), name='osf_groups_list')
 ]
