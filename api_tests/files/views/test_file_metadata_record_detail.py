e3856d884733d3ed13a222dd216d9f7a7ccd3f15
erinspace <erin.braswell@gmail.com>
2019-01-15 15:37:19 -0500

    Add tests for file metadata records on preprints

--- api_tests/files/views/test_file_metadata_record_detail.py
+++ api_tests/files/views/test_file_metadata_record_detail.py
@@ -1,16 +1,27 @@
 import pytest
 
 from api.base.settings.defaults import API_BASE
+from osf.models import PreprintLog, NodeLog
 from api_tests import utils
 from osf_tests.factories import (
     AuthUserFactory,
     ProjectFactory,
     RegistrationFactory,
+    PreprintFactory,
 )
 
 @pytest.fixture()
 def user():
     return AuthUserFactory()
+
+@pytest.fixture()
+def preprint(user):
+    return PreprintFactory(creator=user)
+
+@pytest.fixture()
+def preprint_record(user, preprint):
+    primary_file = preprint.primary_file
+    return primary_file.records.get(schema___id='datacite')
 
 
 @pytest.mark.django_db
@@ -27,6 +38,11 @@ class TestFileMetadataRecordDetail:
         public_node = ProjectFactory(creator=user, is_public=True)
         public_file = utils.create_test_file(public_node, user, filename='public_file')
         return public_file.records.get(schema___id='datacite')
+
+    @pytest.fixture()
+    def unpublished_preprint_record(self, user):
+        unpublished_preprint = PreprintFactory(is_published=False, creator=user, is_public=False)
+        return unpublished_preprint.primary_file.records.get(schema___id='datacite')
 
     def get_url(self, record):
         return '/{}files/{}/metadata_records/{}/'.format(API_BASE, record.file._id, record._id)
@@ -61,6 +77,26 @@ class TestFileMetadataRecordDetail:
         res = app.delete_json_api(self.get_url(public_record), auth=user.auth, expect_errors=True)
         assert res.status_code == 405
 
+    def test_preprint_file_metadata_record(self, app, user, preprint_record, unpublished_preprint_record):
+
+        # unauthenticated view public preprint file metadata record
+        res = app.get(self.get_url(preprint_record))
+        assert res.status_code == 200
+        assert res.json['data']['id'] == preprint_record._id
+
+        # authenticated view public preprint file metadata record
+        res = app.get(self.get_url(preprint_record), auth=user.auth)
+        assert res.status_code == 200
+        assert res.json['data']['id'] == preprint_record._id
+
+        # unauthenticated cannot view unpublished preprint file metadata record
+        res = app.get(self.get_url(unpublished_preprint_record), expect_errors=True)
+        assert res.status_code == 401
+
+        # authenticated contributor can view unpublished preprint file metadata record
+        res = app.get(self.get_url(unpublished_preprint_record), auth=user.auth)
+        assert res.status_code == 200
+        assert res.json['data']['id'] == unpublished_preprint_record._id
 
 @pytest.mark.django_db
 class TestFileMetadataRecordUpdate:
@@ -80,8 +116,11 @@ class TestFileMetadataRecordUpdate:
         return registration_file.records.get(schema___id='datacite')
 
     @pytest.fixture()
-    def public_record(self, user, user_write, user_read):
-        node = ProjectFactory(creator=user, is_public=True)
+    def node(self, user):
+        return ProjectFactory(creator=user, is_public=True)
+
+    @pytest.fixture()
+    def public_record(self, user, node, user_write, user_read):
         node.add_contributor(user_write)
         node.add_contributor(user_read, permissions=['read'])
         public_file = utils.create_test_file(node, user, filename='public_file')
@@ -119,12 +158,13 @@ class TestFileMetadataRecordUpdate:
             return payload_data
         return payload
 
-    def test_admin_can_update(self, app, user, public_record, make_payload, metadata_record_json):
+    def test_admin_can_update(self, app, user, node, public_record, make_payload, metadata_record_json):
         res = app.patch_json_api(self.get_url(public_record), make_payload(public_record), auth=user.auth)
         public_record.reload()
         assert res.status_code == 200
         assert res.json['data']['attributes']['metadata'] == metadata_record_json
         assert public_record.metadata == metadata_record_json
+        assert node.logs.first().action == NodeLog.FILE_METADATA_UPDATED
 
     def test_write_can_update(self, app, user_write, public_record, make_payload, metadata_record_json):
         res = app.patch_json_api(self.get_url(public_record), make_payload(public_record), auth=user_write.auth)
@@ -159,3 +199,11 @@ class TestFileMetadataRecordUpdate:
         url = '/{}files/{}/metadata_records/{}/'.format(API_BASE, registration_record.file._id, registration_record._id)
         res = app.patch_json_api(url, make_payload(registration_record), auth=user.auth, expect_errors=True)
         assert res.status_code == 403
+
+    def test_update_file_metadata_for_preprint_file(self, app, user, metadata_record_json, make_payload, preprint_record, preprint):
+        res = app.patch_json_api(self.get_url(preprint_record), make_payload(preprint_record), auth=user.auth)
+        preprint_record.reload()
+        assert res.status_code == 200
+        assert res.json['data']['attributes']['metadata'] == metadata_record_json
+        assert preprint_record.metadata == metadata_record_json
+        assert preprint.logs.first().action == PreprintLog.FILE_METADATA_UPDATED
