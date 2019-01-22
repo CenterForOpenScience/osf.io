c2e8c0c3ab42ed053143ae0e1b095f8ef8f58be0
erinspace <erin.braswell@gmail.com>
2019-01-22 14:08:58 -0500

    Allow OAuth application client secret reset by PATCHing client_secret to null
    [#PLAT-1163]
    
    - Deprecate old ApplicationReset view
    - allow patching client_secret as null in ApiOAuth2ApplicationDetailSerializer
    - Tests for deprecated view and new route

--- api/applications/serializers.py
+++ api/applications/serializers.py
@@ -1,3 +1,5 @@
+from distutils.version import StrictVersion
+
 from django.core.validators import URLValidator
 from rest_framework import serializers as ser
 
@@ -35,12 +37,13 @@ class ApiOAuthApplicationBaseSerializer(JSONAPISerializer):
         return obj.get_absolute_url()
 
     def reset_url(self, obj):
-        return absolute_reverse(
-            'applications:application-reset', kwargs={
-                'client_id': obj.client_id,
-                'version': self.context['request'].parser_context['kwargs']['version'],
-            },
-        )
+        if StrictVersion(self.context['request'].version) < StrictVersion('2.13'):
+            return absolute_reverse(
+                'applications:application-reset', kwargs={
+                    'client_id': obj.client_id,
+                    'version': self.context['request'].parser_context['kwargs']['version'],
+                },
+            )
 
     class Meta:
         type_ = 'applications'
@@ -93,20 +96,30 @@ class ApiOAuth2ApplicationSerializer(ApiOAuthApplicationBaseSerializer):
         instance.save()
         return instance
 
+
+class ApiOAuth2ApplicationDetailSerializer(ApiOAuth2ApplicationSerializer):
+    """
+    Overrides ApiOAuth2ApplicationSerializer to make id required
+    and client_secret writable to reset the secret via API patch request
+    """
+
+    id = IDField(source='client_id', required=True, help_text='The client ID for this application (automatically generated)')
+
+    client_secret = ser.CharField(
+        help_text='The client secret for this application (automatically generated)',
+        allow_null=True,
+        required=False,
+    )
+
     def update(self, instance, validated_data):
         assert isinstance(instance, ApiOAuth2Application), 'instance must be an ApiOAuth2Application'
+        client_secret = validated_data.pop('client_secret', None)
+        if client_secret == '':
+            instance.reset_secret(save=True)
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
         instance.save()
         return instance
-
-
-class ApiOAuth2ApplicationDetailSerializer(ApiOAuth2ApplicationSerializer):
-    """
-    Overrides ApiOAuth2ApplicationSerializer to make id required.
-    """
-
-    id = IDField(source='client_id', required=True, help_text='The client ID for this application (automatically generated)')
 
 
 class ApiOAuth2ApplicationResetSerializer(ApiOAuth2ApplicationDetailSerializer):
--- api/base/settings/defaults.py
+++ api/base/settings/defaults.py
@@ -163,6 +163,7 @@ REST_FRAMEWORK = {
         '2.10',
         '2.11',
         '2.12',
+        '2.13',
     ),
     'DEFAULT_FILTER_BACKENDS': ('api.base.filters.OSFOrderingFilter',),
     'DEFAULT_PAGINATION_CLASS': 'api.base.pagination.JSONAPIPagination',
