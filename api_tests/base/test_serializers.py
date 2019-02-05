3e9991496fc81be46fd2b41c97029afe88296095
Dawn Pattison <pattison.dawn.r@gmail.com>
2019-02-04 20:55:21 -0600

    Update test now the node_links are properly serialized under relationships prior to v2.0.
    - Bug fix - node_links were being incorrectly serialized under attributes in old versions for registrations.

--- api_tests/base/test_serializers.py
+++ api_tests/base/test_serializers.py
@@ -539,7 +539,7 @@ class TestShowIfVersion(ApiTestCase):
             self.registration,
             context={'request': req}
         ).data['data']
-        assert_in('node_links', data['attributes'])
+        assert_in('node_links', data['relationships'])
 
     def test_node_links_bad_version_registration_serializer(self):
         req = make_drf_request_with_version(version='2.1')
@@ -547,7 +547,25 @@ class TestShowIfVersion(ApiTestCase):
             self.registration,
             context={'request': req}
         ).data['data']
-        assert_not_in('node_links', data['attributes'])
+        assert_not_in('node_links', data['relationships'])
+
+    def test_node_links_withdrawn_registration(self):
+        factories.WithdrawnRegistrationFactory(
+            registration=self.registration)
+
+        req = make_drf_request_with_version(version='2.0')
+        data = RegistrationSerializer(
+            self.registration,
+            context={'request': req}
+        ).data['data']
+        assert_not_in('node_links', data['relationships'])
+
+        req = make_drf_request_with_version(version='2.1')
+        data = RegistrationSerializer(
+            self.registration,
+            context={'request': req}
+        ).data['data']
+        assert_not_in('node_links', data['relationships'])
 
 
 class VersionedDateTimeField(DbTestCase):
