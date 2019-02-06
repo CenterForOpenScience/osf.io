yacchin1205/RDM2-osf.io

* add replace-support-email.sh * replace support@osf.io to nii-rdmp@meatmail.jp.

website/settings/defaults.py
@@ -121,7 +121,7 @@ USE_CDN_FOR_CLIENT_LIBS = True
 
 USE_EMAIL = True
 FROM_EMAIL = 'openscienceframework-noreply@osf.io'
-SUPPORT_EMAIL = 'support@osf.io'
+SUPPORT_EMAIL = 'nii-rdmp@meatmail.jp'
 
 # SMTP Settings
 MAIL_SERVER = 'smtp.sendgrid.net'

CenterForOpenScience/osf.io

website/settings/defaults.py
@@ -141,7 +141,7 @@ USE_EMAIL = True
 FROM_EMAIL = 'openscienceframework-noreply@osf.io'
 
 # support email
-OSF_SUPPORT_EMAIL = 'support@osf.io'
+OSF_SUPPORT_EMAIL = 'nii-rdmp@meatmail.jp'
 # contact email
 OSF_CONTACT_EMAIL = 'contact@osf.io'
 