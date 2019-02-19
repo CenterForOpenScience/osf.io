9b9277848cf9a51e38c2f581aee2b6b5106b4db6
fujimoto <fujimoto@dhcp-200-009.soum.co.jp>
2019-02-19 16:43:51 +0900

    Fix flake8 W391

--- osf/migrations/0082_auto_20180313_0039.py
+++ osf/migrations/0082_auto_20180313_0039.py
@@ -42,4 +42,3 @@ class Migration(migrations.Migration):
             field=models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', related_query_name='user', to='auth.Group', verbose_name='groups'),
         ),
     ]
-
--- osf/migrations/0083_merge_20180528_2128.py
+++ osf/migrations/0083_merge_20180528_2128.py
@@ -14,4 +14,3 @@ class Migration(migrations.Migration):
 
     operations = [
     ]
-
