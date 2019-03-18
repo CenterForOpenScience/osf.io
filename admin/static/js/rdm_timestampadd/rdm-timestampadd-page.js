92eadf704bd69f0760bc72ab316f7b4e8c0a15c5
rdm-dev08 <rdm-dev08@ml.itg.hitachi.co.jp>
2019-03-18 16:59:18 +0900

    Use js-cookie

--- admin/static/js/rdm_timestampadd/rdm-timestampadd-page.js
+++ admin/static/js/rdm_timestampadd/rdm-timestampadd-page.js
@@ -1,29 +1,14 @@
 'use strict';
 
 var $ = require('jquery');
-var jQuery = $;
+var Cookie = require('js-cookie');
 var urls = window.timestampaddUrls;
 var timestampCommon = require('js/pages/timestamp-common.js');  // website/static/js/pages
 timestampCommon.setWebOrAdmin('admin');
 
 
 $(function () {
-    function getCookie (name) {
-        var cookieValue = null;
-        if (document.cookie && document.cookie !== '') {
-            var cookies = document.cookie.split(';');
-            for (var i = 0; i < cookies.length; i++) {
-                var cookie = jQuery.trim(cookies[i]);
-                // Does this cookie string begin with the name we want?
-                if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                    break;
-                }
-            }
-        }
-        return cookieValue;
-    }
-    var csrftoken = getCookie('admin-csrf');
+    var csrftoken = Cookie.get('admin-csrf');
     function csrfSafeMethod (method) {
         // these HTTP methods do not require CSRF protection
         return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
