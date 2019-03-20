80d39744008ce89850703365248f7111f0c8736d
rdm-dev08 <rdm-dev08@ml.itg.hitachi.co.jp>
2019-03-20 11:40:01 +0900

    Get quota information from project creator instead of logged user

--- website/project/views/node.py
+++ website/project/views/node.py
@@ -775,10 +775,10 @@ def _view_project(node, auth, primary=False,
                 status.push_status_message(message, kind='info', dismissible=False, trust=True)
     NodeRelation = apps.get_model('osf.NodeRelation')
     try:
-        max_quota = user.userquota.max_quota
+        max_quota = node.creator.userquota.max_quota
     except ObjectDoesNotExist:
         max_quota = api_settings.DEFAULT_MAX_QUOTA
-    used_quota = quota.used_quota(user._id)
+    used_quota = quota.used_quota(node.creator._id)
     threshhold = api_settings.WARNING_THRESHOLD
     is_registration = node.is_registration
     timestamp_pattern = get_timestamp_pattern_division(auth, node)
