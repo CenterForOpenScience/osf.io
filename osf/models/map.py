4a9a59215576d05964a85a5046b13f4c397e7f0b
Hiroharu Naghara <hiro@nspl.co.jp>
2019-03-25 12:33:49 +0900

    o Remove unused imports.

--- osf/models/map.py
+++ osf/models/map.py
@@ -1,58 +1,12 @@
 import datetime as dt
 import logging
-#import re
-#import urllib
-#import urlparse
-#import uuid
-#from copy import deepcopy
-#from os.path import splitext
 
-#from flask import Request as FlaskRequest
-#from framework import analytics
-#from guardian.shortcuts import get_perms
-
-# OSF imports
-#import itsdangerous
-#import pytz
-#from dirtyfields import DirtyFieldsMixin
-
-#from django.conf import settings
-#from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
-#from django.contrib.auth.hashers import check_password
-#from django.contrib.auth.models import PermissionsMixin
-#from django.dispatch import receiver
 from django.db import models
-#from django.db.models import Count
-#from django.db.models.signals import post_save
 from django.utils import timezone
 
-#from framework.auth import Auth, signals, utils
-#from framework.auth.core import generate_verification_key
-'''
-from framework.auth.exceptions import (ChangePasswordError, ExpiredTokenError,
-                                       InvalidTokenError,
-                                       MergeConfirmedRequiredError,
-                                       MergeConflictError)
-'''
-#from framework.exceptions import PermissionsError
-#from framework.sessions.utils import remove_sessions_for_user
-#from osf.utils.requests import get_current_request
-#from osf.exceptions import reraise_django_validation_errors, MaxRetriesError, UserStateError
 from osf.models.base import BaseModel, GuidMixin, GuidMixinQuerySet
-#from osf.models.contributor import Contributor, RecentlyAddedContributor
-#from osf.models.institution import Institution
-#from osf.models.mixins import AddonModelMixin
-#from osf.models.session import Session
-#from osf.models.tag import Tag
-#from osf.models.validators import validate_email, validate_social, validate_history_item
 from osf.models.user import OSFUser
-#from osf.utils.datetime_aware_jsonfield import DateTimeAwareJSONField
 from osf.utils.fields import NonNaiveDateTimeField, LowercaseEmailField
-#from osf.utils.names import impute_names
-#from osf.utils.requests import check_select_for_update
-#from website import settings as website_settings
-#from website import filters, mails
-#from website.project import new_bookmark_collection
 
 logger = logging.getLogger(__name__)
 
