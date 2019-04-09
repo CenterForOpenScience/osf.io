a449ba766507517b7bd0840e15eb384762d26b07
rdm-dev08 <rdm-dev08@ml.itg.hitachi.co.jp>
2019-04-09 17:33:49 +0900

    Implement creator_quota on web's API

--- website/project/views/quota.py
+++ website/project/views/quota.py
@@ -1,6 +1,19 @@
+from django.core.exceptions import ObjectDoesNotExist
+
+from api.base import settings as api_settings
 from website.project.decorators import must_be_contributor_or_public
+from website.util import quota
 
 
 @must_be_contributor_or_public
 def creator_quota(auth, node, **kwargs):
-    return {'status': 'ok'}
+    try:
+        max_quota = node.creator.userquota.max_quota
+        used_quota = node.creator.userquota.used
+    except ObjectDoesNotExist:
+        max_quota = api_settings.DEFAULT_MAX_QUOTA
+        used_quota = quota.used_quota(node.creator._id)
+    return {
+        'max': max_quota * 1024 ** 3,
+        'used': used_quota
+    }
