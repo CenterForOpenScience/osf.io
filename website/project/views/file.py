822e1d2411dad648ccc9d416d497cc780f8878f9
rdm-dev12 <rdm-dev12@ml.itg.hitachi.co.jp>
2019-04-22 17:51:18 +0900

    Custom storage location icon added

--- website/project/views/file.py
+++ website/project/views/file.py
@@ -27,7 +27,15 @@ def grid_data(auth, node, **kwargs):
     """
     data = request.args.to_dict()
     ret = rubeus.to_hgrid(node, auth, **data)
-    if NodeSettings.objects.get(owner_id=node.id).region_id != 1:
-        ret[0]['children'][0]['iconUrl'] = '/static/addons/github/comicon.png'
-        ret[0]['children'][0]['addonFullname'] = ret[0]['children'][0]['nodeRegion']
+    import logging
+    logging.critical("#######################################")
+    for x in NodeSettings.objects.filter(owner_id=node.id):
+        logging.critical(vars(x))
+    logging.critical("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+    try:
+        if NodeSettings.objects.get(owner_id=node.id).region_id != 1:
+            ret[0]['children'][0]['iconUrl'] = '/static/addons/osfstorage/comicon_custom_storage.png'
+            ret[0]['children'][0]['addonFullname'] = ret[0]['children'][0]['nodeRegion']
+    except Exception as error: 
+        logging.critical(error)
     return {'data': ret}
