2b97cf539924f4cfb9de3dde40f624f3512884ed
Yusuke Komiyama <komiyama@nii.ac.jp>
2019-04-25 20:06:26 +0900

    Mofified .html.mako files about text to NII specific information.

--- website/settings/defaults.py
+++ website/settings/defaults.py
@@ -144,7 +144,7 @@ FROM_EMAIL = 'openscienceframework-noreply@osf.io'
 # support email
 OSF_SUPPORT_EMAIL = 'nii-rdmp@meatmail.jp'
 # contact email
-OSF_CONTACT_EMAIL = 'contact@osf.io'
+OSF_CONTACT_EMAIL = 'rcos-office@nii.ac.jp'
 
 # prereg email
 PREREG_EMAIL = 'prereg@cos.io'
