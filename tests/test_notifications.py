e8e9f00b4dd9bfd31342080a4bbfbc3ac7f8e2d1
John Tordoff <john@cos.io>
2019-05-02 15:56:54 -0400

    change dictionary order so we can assert as list correctly for py3

--- tests/test_notifications.py
+++ tests/test_notifications.py
@@ -979,8 +979,8 @@ class TestNotificationUtils(OsfTestCase):
                 'children': []
             }, {
                 'event': {
-                    'title': 'global_mentions',
-                    'description': constants.USER_SUBSCRIPTIONS_AVAILABLE['global_mentions'],
+                    'title': 'global_comments',
+                    'description': constants.USER_SUBSCRIPTIONS_AVAILABLE['global_comments'],
                     'notificationType': 'email_transactional',
                     'parent_notification_type': None
                 },
@@ -988,8 +988,8 @@ class TestNotificationUtils(OsfTestCase):
                 'children': []
             }, {
                 'event': {
-                    'title': 'global_comments',
-                    'description': constants.USER_SUBSCRIPTIONS_AVAILABLE['global_comments'],
+                    'title': 'global_mentions',
+                    'description': constants.USER_SUBSCRIPTIONS_AVAILABLE['global_mentions'],
                     'notificationType': 'email_transactional',
                     'parent_notification_type': None
                 },
@@ -1006,7 +1006,7 @@ class TestNotificationUtils(OsfTestCase):
                 'children': []
             }
         ]
-        assert_items_equal(data, expected)
+        assert_list_equal(data, expected)
 
     def test_get_global_notification_type(self):
         notification_type = utils.get_global_notification_type(self.user_subscription[1] ,self.user)
