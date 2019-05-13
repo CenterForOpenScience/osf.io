be0ccc14458c4c2179df7271432c1d46d9616f2a
rdm-dev12 <rdm-dev12@ml.itg.hitachi.co.j>
2019-05-13 13:26:59 +0900

    added missing comma

--- api/base/settings/defaults.py
+++ api/base/settings/defaults.py
@@ -221,7 +221,7 @@ MIDDLEWARE = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'waffle.middleware.WaffleMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware'
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 TEMPLATES = [
