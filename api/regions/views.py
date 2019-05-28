cc76ea2a68c8aa932d02c43a10d9246b7f86934c
rdm-dev08 <rdm-dev08@ml.itg.hitachi.co.jp>
2019-05-28 10:53:35 +0900

    Add missing test for region list

--- api/regions/views.py
+++ api/regions/views.py
@@ -66,9 +66,8 @@ class RegionList(JSONAPIBaseView, generics.ListAPIView, ListFilterMixin):
         institution_id = self.request.user.affiliated_institutions.first()
         if institution_id is not None:
             region_queryset = Region.objects.filter(_id=institution_id._id)
-            if region_queryset.count() > 0:
+            if region_queryset.exists():
                 return region_queryset
-            return available_regions
         return available_regions
 
     # overrides ListAPIView
