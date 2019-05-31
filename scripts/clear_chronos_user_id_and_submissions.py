cc4aaeb2632f63c1f89d61fa6425248e07254c7d
Yuhuai Liu <yuhuai@cos.io>
2019-05-31 11:45:29 -0400

    minor fix

--- scripts/clear_chronos_user_id_and_submissions.py
+++ scripts/clear_chronos_user_id_and_submissions.py
@@ -7,15 +7,24 @@ import django
 django.setup()
 
 from osf import models
+from website.app import init_app
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO)
-
 
 def main(dry_run=True):
-    models.OSFUser.objects.all().update(chronos_user_id=None)
-    models.ChronosSubmission.objects.all().delete()
+    if dry_run:
+        for user in models.OSFUser.objects.filter(chronos_user_id__isnull=False):
+            logger.info('Reset chronos_user_id for user with guid {}'.format(user._id))
+        for submission in models.ChronosSubmission.objects.all():
+            logger.info('Deleting chronos submission with id {}'.format(submission.publication_id))
+    else:
+        models.OSFUser.objects.all().update(chronos_user_id=None)
+        models.ChronosSubmission.objects.all().delete()
+        logger.info('Finished resetting all OSFUser chronos_user_id')
+        logger.info('Finished deleting all ChronosSubmission instances')
 
 
 if __name__ == '__main__':
-    main(dry_run='--dry' in sys.argv)
+    init_app(routes=False)
+    dry = '--dry' in sys.argv
+    main(dry_run=dry)
