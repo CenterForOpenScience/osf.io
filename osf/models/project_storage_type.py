4d525432bed19ca003d5873ee63f689df14c6cde
rdm-dev08 <rdm-dev08@ml.itg.hitachi.co.jp>
2019-06-19 11:04:51 +0900

    Create base class for storage type

--- osf/models/project_storage_type.py
+++ osf/models/project_storage_type.py
@@ -1,17 +1,11 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 
-from osf.models.base import BaseModel
+from osf.models.storage import StorageType
 
 
-class ProjectStorageType(BaseModel):
-    NII_STORAGE = 1
-    CUSTOM_STORAGE = 2
-
-    STORAGE_TYPE_CHOICES = (
-        (NII_STORAGE, 'NII Storage'),
-        (CUSTOM_STORAGE, 'Custom Storage'),
-    )
-
+class ProjectStorageType(StorageType):
     node = models.OneToOneField('AbstractNode', on_delete=models.CASCADE)
-    storage_type = models.IntegerField(choices=STORAGE_TYPE_CHOICES, default=NII_STORAGE)
+    storage_type = models.IntegerField(
+        choices=StorageType.STORAGE_TYPE_CHOICES,
+        default=StorageType.NII_STORAGE)
--- osf/models/user_quota.py
+++ osf/models/user_quota.py
@@ -1,19 +1,13 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 
-from osf.models.base import BaseModel
+from osf.models.storage import StorageType
 
 
-class UserQuota(BaseModel):
-    NII_STORAGE = 1
-    CUSTOM_STORAGE = 2
-
-    STORAGE_TYPE_CHOICES = (
-        (NII_STORAGE, 'NII Storage'),
-        (CUSTOM_STORAGE, 'Custom Storage'),
-    )
-
+class UserQuota(StorageType):
     user = models.ForeignKey('OSFUser', on_delete=models.CASCADE)
-    storage_type = models.IntegerField(choices=STORAGE_TYPE_CHOICES, default=NII_STORAGE)
+    storage_type = models.IntegerField(
+        choices=StorageType.STORAGE_TYPE_CHOICES,
+        default=StorageType.NII_STORAGE)
     max_quota = models.IntegerField(default=100)
     used = models.BigIntegerField(default=0)
