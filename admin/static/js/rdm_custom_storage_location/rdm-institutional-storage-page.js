29561b6d597575376ac83eee7309a71614f7a58f
rdm-dev08 <rdm-dev08@ml.itg.hitachi.co.jp>
2019-06-27 10:54:15 +0900

    Open sample modal when clicking Next button

--- admin/static/js/rdm_custom_storage_location/rdm-institutional-storage-page.js
+++ admin/static/js/rdm_custom_storage_location/rdm-institutional-storage-page.js
@@ -1,1 +1,12 @@
 'use strict';
+
+var $ = require('jquery');
+
+$('.next-btn').click(function () {
+    var selectedProvider = $('input[name=\'options\']:checked').val();
+    switch (selectedProvider) {
+        default:
+            $('#sample_modal').modal('show');
+            break;
+    }
+});
