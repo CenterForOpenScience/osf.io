093d1edeae2a73bb1a6bf8df52e20687752fdffa
rdm-dev12 <rdm-dev12@ml.itg.hitachi.co.j>
2019-06-27 15:57:02 +0900

    Create a modal swift

--- admin/static/js/rdm_custom_storage_location/rdm-institutional-storage-page.js
+++ admin/static/js/rdm_custom_storage_location/rdm-institutional-storage-page.js
@@ -15,3 +15,14 @@ $('#s3_modal input').keyup(function () {
     $('#s3_connect').toggleClass('disabled', !allFilled);
 });
 
+$("#swift_keytone_versionSelect").change(function() {
+    var swift_keystone_version_val = $(this).val();
+    if (swift_keystone_version_val == "v2") {
+        $('#swift_project_domain_name').attr('disabled', true);
+        $('#swift_user_domain_name').attr('disabled', true);
+    } else {
+              $('#swift_project_domain_name').attr('disabled', false);
+        $('#swift_user_domain_name').attr('disabled', false);
+    }
+  });
+
