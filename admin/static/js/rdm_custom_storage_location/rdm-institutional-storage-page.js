a4e8114f9ad77eb5f23077f469bd878cc464ff8d
rdm-dev12 <rdm-dev12@ml.itg.hitachi.co.j>
2019-06-27 15:33:42 +0900

    Disable the Connect button without input

--- admin/static/js/rdm_custom_storage_location/rdm-institutional-storage-page.js
+++ admin/static/js/rdm_custom_storage_location/rdm-institutional-storage-page.js
@@ -6,3 +6,12 @@ $('.next-btn').click(function () {
     var selectedProvider = $('input[name=\'options\']:checked').val();
     $('#' + selectedProvider + '_modal').modal('show');
 });
+
+$('#s3_modal input').keyup(function () {
+    // Check if all the inputs are filled, so we can enable the connect button
+    var allFilled = $('#s3_modal [required]').toArray().reduce(function (accumulator, current) {
+        return accumulator && current.value.length > 0;
+    }, true);
+    $('#s3_connect').toggleClass('disabled', !allFilled);
+});
+
