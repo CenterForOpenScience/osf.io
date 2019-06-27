fcb94465ff234b194559f0bfe61f657d4b600bb1
Brian J. Geiger <bgeiger@pobox.com>
2019-06-27 12:02:15 -0400

    Disable schedule until manually verified on Prod

--- website/settings/defaults.py
+++ website/settings/defaults.py
@@ -580,10 +580,10 @@ class CeleryConfig:
                 'schedule': crontab(minute=0, hour=9),  # Daily 4:00 a.m.
                 'kwargs': {'yesterday': True}
             },
-            'data_storage_usage': {
-              'task': 'management.commands.data_storage_usage',
-              'schedule': crontab(day_of_month=1, minute=30, hour=4),  # Last of the month at 11:30 p.m.
-            },
+            # 'data_storage_usage': {
+            #   'task': 'management.commands.data_storage_usage',
+            #   'schedule': crontab(day_of_month=1, minute=30, hour=4),  # Last of the month at 11:30 p.m.
+            # },
             'generate_sitemap': {
                 'task': 'scripts.generate_sitemap',
                 'schedule': crontab(minute=0, hour=5),  # Daily 12:00 a.m.
