672b822fac6d862413d93aca21ceb58ef15a07c3
rdm-dev12 <rdm-dev12@ml.itg.hitachi.co.j>
2019-06-27 09:35:27 +0900

    Test code for Get Method of admin's custom storage location

---
+++ admin_tests/rdm_custom_storage_location/test_views.py
@@ -0,0 +1,58 @@
+from nose import tools as nt
+from django.test import RequestFactory
+from tests.base import AdminTestCase
+from osf_tests.factories import (
+    AuthUserFactory,
+    RegionFactory,
+    InstitutionFactory,
+)
+from admin_tests.utilities import setup_user_view
+from admin.rdm_custom_storage_location import views
+from admin.rdm_custom_storage_location import csl_utils
+from addons.osfstorage.models import Region
+
+class TestInstitutionDefaultStorage(AdminTestCase):
+    def setUp(self):
+        super(TestInstitutionDefaultStorage, self).setUp()
+        self.institution1 = InstitutionFactory()
+        self.institution2 = InstitutionFactory()
+        self.user = AuthUserFactory()
+        self.default_region = Region.objects.first()
+
+        self.user = AuthUserFactory()
+        self.user.affiliated_institutions.add(self.institution1)
+        self.user.save()
+        self.request = RequestFactory().get('/fake_path')
+        self.view = views.InstitutionalStorage()
+        self.view = setup_user_view(self.view, self.request, user=self.user)
+        self.addon_type_dict = [
+            'BoxAddonAppConfig',
+            'OSFStorageAddonAppConfig',
+            'OwnCloudAddonAppConfig',
+            'S3AddonAppConfig',
+            'GoogleDriveAddonConfig',
+            'SwiftAddonAppConfig',
+            'S3CompatAddonAppConfig',
+            'NextcloudAddonAppConfig',
+        ]
+
+    def test_get(self, *args, **kwargs):
+        res = self.view.get(self.request, *args, **kwargs)
+        nt.assert_equal(res.status_code, 200)
+
+    def test_get_without_custom_storage(self, *args, **kwargs):
+        res = self.view.get(self.request, *args, **kwargs)
+        for addon in res.context_data['addons']:
+            nt.assert_true(type(addon).__name__ in self.addon_type_dict)
+        nt.assert_equal(res.context_data['region'], self.default_region)
+        nt.assert_equal(res.context_data['provider'], csl_utils.get_provider_short_name(res.context_data['region'].waterbutler_settings))
+
+    def test_get_custom_storage(self, *args, **kwargs):
+        self.us = RegionFactory()
+        self.us._id = self.institution1._id
+        self.us.save()
+        res = self.view.get(self.request, *args, **kwargs)
+        for addon in res.context_data['addons']:
+            nt.assert_true(type(addon).__name__ in self.addon_type_dict)
+        nt.assert_equal(res.context_data['region'], self.us)
+        nt.assert_equal(res.context_data['provider'], csl_utils.get_provider_short_name(res.context_data['region'].waterbutler_settings))
