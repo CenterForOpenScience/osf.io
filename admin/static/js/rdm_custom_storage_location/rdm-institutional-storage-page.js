f329cbc268337ee1978d6d3690a099d1287a871d
rdm-dev12 <rdm-dev12@ml.itg.hitachi.co.j>
2019-06-28 11:21:30 +0900

    Disable the connect button without all required input fields

--- admin/static/js/rdm_custom_storage_location/rdm-institutional-storage-page.js
+++ admin/static/js/rdm_custom_storage_location/rdm-institutional-storage-page.js
@@ -8,21 +8,33 @@ $('.next-btn').click(function () {
 });
 
 $('#s3_modal input').keyup(function () {
-    // Check if all the inputs are filled, so we can enable the connect button
-    var allFilled = $('#s3_modal [required]').toArray().reduce(function (accumulator, current) {
-        return accumulator && current.value.length > 0;
-    }, true);
-    $("#s3_connect").attr("disabled", !allFilled);
+    validateRequiredFields('s3')
 });
 
-$("#swift_keytone_versionSelect").change(function() {
+$('#swift_modal input').keyup(function () {
+    validateRequiredFields('swift')
+});
+
+function validateRequiredFields(providerShortName) {
+    // Check if all the inputs are filled, so we can enable the connect button
+    var allFilled = $('#' + providerShortName + '_modal [required]').toArray().reduce(function (accumulator, current) {
+        return accumulator && current.value.length > 0;
+    }, true);
+    $('#' + providerShortName + '_connect').attr('disabled', !allFilled);
+}
+
+$("#swift_keytone_versionSelect").change(function () {
     var swift_keystone_version_val = $(this).val();
     if (swift_keystone_version_val == "v2") {
         $('#swift_project_domain_name').attr('disabled', true);
         $('#swift_user_domain_name').attr('disabled', true);
+        $('#swift_project_domain_name').attr('required', false);
+        $('#swift_user_domain_name').attr('required', false);
     } else {
-              $('#swift_project_domain_name').attr('disabled', false);
+        $('#swift_project_domain_name').attr('disabled', false);
         $('#swift_user_domain_name').attr('disabled', false);
+        $('#swift_project_domain_name').attr('required', true);
+        $('#swift_user_domain_name').attr('required', true);
     }
-  });
-
+    validateRequiredFields('swift')
+});
