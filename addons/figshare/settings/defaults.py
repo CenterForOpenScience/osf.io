8e56d951208471f13917525bd6411a46b2a208c9
longze chen <cs.longze.chen@gmail.com>
2019-06-28 02:33:27 -0400

    Updated figshare settings for article types
    - Uses a bi-directional dict for type and name mapping
    - Added new types and renamed existing ones
    - Added a set for folder-like types
    - Added comments and TODO for future full fix

--- addons/figshare/settings/defaults.py
+++ addons/figshare/settings/defaults.py
@@ -1,3 +1,5 @@
+from bidict import bidict
+
 CLIENT_ID = None
 CLIENT_SECRET = None
 
@@ -11,14 +13,36 @@ MAX_UPLOAD_SIZE = 50
 FIGSHARE_OAUTH_TOKEN_ENDPOINT = '{}{}'.format(API_BASE_URL, 'token')
 FIGSHARE_OAUTH_AUTH_ENDPOINT = 'https://figshare.com/account/applications/authorize'
 
-FIGSHARE_DEFINED_TYPE_NUM_MAP = {
+# Each figshare article has a `defined_type` integer and a matching `defined_type_name` string. A
+# bi-directional dictionary (https://bidict.readthedocs.io/en/master/) is used here since both the
+# type and name are used in the figshare client.  In addition, check with figshare occasionally
+# for changes such as type deprecation, type name update as well as new types.
+#
+# TODO: refactor the client to use the `defined_type` only since figshare does change the names :(
+#
+FIGSHARE_DEFINED_TYPE_MAP = bidict({
     'figure': 1,
     'media': 2,
     'dataset': 3,
-    'fileset': 4,
+    'fileset': 4,  # Unofficially deprecated and replaced by type 3 dataset
     'poster': 5,
-    'paper': 6,
+    'journal contribution': 6,
     'presentation': 7,
     'thesis': 8,
-    'code': 9,
-}
+    'software': 9,
+    # For unknown reasons, 10 does not exist
+    'online resource': 11,
+    'preprints': 12,
+    'book': 13,
+    'conference': 14,
+})
+
+# figshare used to only let type 4 fileset to behave like folders, i.e. it can contain multiple
+# files.  However, it suddenly changed without any announcement that all article types are now
+# folder-like and that type 4 fileset is unofficially deprecated and converted to type 3 dataset
+# internally.  In order not to break the old behavior of OSF and WB, we decide for now to only
+# treat dataset and fileset as folders.
+#
+# TODO: refactor the figshare client to treat all article types as folders
+#
+FIGSHARE_FOLDER_TYPES = {3, 4, }
