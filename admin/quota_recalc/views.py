e45160687ab37987552441bafea1901c342b5d66
rdm-dev08 <rdm-dev08@ml.itg.hitachi.co.jp>
2019-07-09 16:17:52 +0900

    Quota recalculation for custom storage too

--- admin/quota_recalc/views.py
+++ admin/quota_recalc/views.py
@@ -1,26 +1,36 @@
 from django.http import JsonResponse
 
+from addons.osfstorage.models import Region
 from api.base import settings as api_settings
 from osf.models import OSFUser, UserQuota
 from website.util.quota import used_quota
 
 
 def calculate_quota(user):
-    used = used_quota(user._id, UserQuota.NII_STORAGE)
-    try:
-        user_quota = UserQuota.objects.get(
-            user=user,
-            storage_type=UserQuota.NII_STORAGE,
-        )
-        user_quota.used = used
-        user_quota.save()
-    except UserQuota.DoesNotExist:
-        UserQuota.objects.create(
-            user=user,
-            storage_type=UserQuota.NII_STORAGE,
-            max_quota=api_settings.DEFAULT_MAX_QUOTA,
-            used=used,
-        )
+    storage_type_list = [UserQuota.NII_STORAGE]
+
+    institution = user.affiliated_institutions.first()
+    if institution is not None:
+        region = Region.objects.filter(_id=institution._id).first()
+        if region is not None:
+            storage_type_list.append(UserQuota.CUSTOM_STORAGE)
+
+    for storage_type in storage_type_list:
+        used = used_quota(user._id, storage_type)
+        try:
+            user_quota = UserQuota.objects.get(
+                user=user,
+                storage_type=storage_type,
+            )
+            user_quota.used = used
+            user_quota.save()
+        except UserQuota.DoesNotExist:
+            UserQuota.objects.create(
+                user=user,
+                storage_type=storage_type,
+                max_quota=api_settings.DEFAULT_MAX_QUOTA,
+                used=used,
+            )
 
 def all_users(request, **kwargs):
     c = 0
