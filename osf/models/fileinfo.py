f8c8ce80252baf29e9933b9e0f98edacd6a0b76d
rdm-dev08 <rdm-dev08@ml.itg.hitachi.co.jp>
2019-07-10 14:09:31 +0900

    Increase FileInfo's size from int to bigint

--- osf/models/fileinfo.py
+++ osf/models/fileinfo.py
@@ -5,4 +5,4 @@ from osf.models.base import BaseModel, ObjectIDMixin
 class FileInfo(ObjectIDMixin, BaseModel):
     """Saves extra information about a file."""
     file = models.OneToOneField('osf.BaseFileNode', on_delete=models.CASCADE)
-    file_size = models.IntegerField()
+    file_size = models.BigIntegerField()
