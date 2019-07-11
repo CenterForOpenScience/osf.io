ab586dfa477cfe066bd88a777b64d0ac4f285020
rdm-dev12 <rdm-dev12@ml.itg.hitachi.co.j>
2019-07-11 17:57:02 +0900

    Box connect test testcode

--- admin_tests/rdm_custom_storage_location/test_views.py
+++ admin_tests/rdm_custom_storage_location/test_views.py
@@ -746,9 +746,55 @@ class TestGoogleDriveConnectionTest(AdminTestCase):
         nt.assert_equals(response_temp_account['provider_name'], temp_account.provider_name)
         nt.assert_equals(response_temp_account['fullname'], self.user.fullname)
 
-    @mock.patch('admin.rdm_custom_storage_location.views.utils.get_external_temporary_account', return_value=False)
-    def test_fail_old_entry(self, mock_timp):
-        ExternalAccountTemporary.objects.create(
+class TestBoxConnectionTest(AdminTestCase):
+    def setUp(self):
+        super(TestBoxConnectionTest, self).setUp()
+        self.institution = InstitutionFactory()
+        self.user = AuthUserFactory()
+        self.user.affiliated_institutions.add(self.institution)
+        self.user.is_staff = True
+        self.user.save()
+        self.seed_data = {
+            'provider_name': 'box',
+            'oauth_key': 'pzN7NJr1EDzXDHsoZRqJT6jHVkt7ryhQbOzQjiduLmPw8CHs8lzrUBrBiztMQvxK5KLplhpKuGxeP91W',
+            'oauth_secret': 'qgKnksgBkx76yCl9CqtTP4DOzPYiHLN9LSHFoVsgLgCc6ZqXngWMww5ydxrqY6OzyjUAcP5wL8c58D1Z',
+            'expires_at': timezone.now(),
+            'refresh_token': 'e97DkIMV6B0j6NjD1CYIiAm4',
+            'date_last_refreshed': timezone.now(),
+            'display_name': 'box display name is here',
+            'profile_url': 'example.com',
+            '_id': self.user.affiliated_institutions.first().id,
+            'provider_id': '88080800880',
+        }
+
+    def view_post(self, params):
+        request = RequestFactory().post(
+            'fake_path',
+            json.dumps(params),
+            content_type='application/json'
+        )
+        request.is_ajax()
+        request.user = self.user
+        return views.fetch_temporary_token(request)
+
+    def test_provider_missing(self):
+        response = self.view_post({
+            'no_pro': 'box',
+        })
+
+        nt.assert_equals(response.status_code, httplib.BAD_REQUEST)
+        nt.assert_in('Provider is missing.', response.content)
+
+    def test_fail_Oauth_procedure_canceled(self):
+        response = self.view_post({
+            'provider_short_name': 'box',
+        })
+
+        nt.assert_equals(response.status_code, httplib.BAD_REQUEST)
+        nt.assert_in('Oauth permission procedure was canceled', response.content)
+
+    def test_success(self):
+        temp_account = ExternalAccountTemporary.objects.create(
             provider=self.seed_data['provider_name'],
             provider_name=self.seed_data['provider_name'],
             oauth_key=self.seed_data['oauth_key'],
@@ -762,6 +808,14 @@ class TestGoogleDriveConnectionTest(AdminTestCase):
             provider_id=self.seed_data['provider_id'],
         )
         response = self.view_post({
-            'provider_short_name': 'googledrive',
+            'provider_short_name': 'box',
         })
-        nt.assert_equals(response.status_code, httplib.BAD_REQUEST)
+        nt.assert_equals(response.status_code, httplib.OK)
+        data = json.loads(response.content)
+        response_temp_account = data['response_data']
+        nt.assert_equals(response_temp_account['display_name'], temp_account.display_name)
+        nt.assert_equals(response_temp_account['oauth_key'], temp_account.oauth_key)
+        nt.assert_equals(response_temp_account['provider'], temp_account.provider)
+        nt.assert_equals(response_temp_account['provider_id'], temp_account.provider_id)
+        nt.assert_equals(response_temp_account['provider_name'], temp_account.provider_name)
+        nt.assert_equals(response_temp_account['fullname'], self.user.fullname)
