4009744ba659b7389e72911357b11dd250674c64
rdm-dev12 <rdm-dev12@ml.itg.hitachi.co.j>
2019-07-19 15:30:57 +0900

    NextCloud credentials save test code

--- admin_tests/rdm_custom_storage_location/test_views.py
+++ admin_tests/rdm_custom_storage_location/test_views.py
@@ -1030,6 +1030,71 @@ class TestOwnCloudStorageSaveCredentials(AdminTestCase):
         nt.assert_equals(wb_settings['storage']['provider'], 'owncloud')
         nt.assert_equals(wb_settings['storage']['folder'], '/reserved_for_osf/')
 
+class TestNextCloudStorageSaveCredentials(AdminTestCase):
+    def setUp(self):
+        super(TestNextCloudStorageSaveCredentials, self).setUp()
+        self.institution = InstitutionFactory()
+        self.user = AuthUserFactory()
+        self.user.affiliated_institutions.add(self.institution)
+        self.user.is_staff = True
+        self.user.save()
+
+    def view_post(self, params):
+        request = RequestFactory().post(
+            'fake_path',
+            json.dumps(params),
+            content_type='application/json'
+        )
+        request.is_ajax()
+        request.user = self.user
+        return views.save_credentials(request)
+
+    @mock.patch('admin.rdm_custom_storage_location.utils.test_owncloud_connection')
+    def test_connection_fail(self, mock_testconnection):
+        mock_testconnection.return_value = {'message': 'NG'}, httplib.BAD_REQUEST
+
+        response = self.view_post({
+            'storage_name': 'My storage',
+            'nextcloud_host': 'drop database;',
+            'nextcloud_username': 'invalid-user',
+            'nextcloud_password': 'invalid-password',
+            'nextcloud_folder': 'Hello World',
+            'provider_short_name': 'nextcloud',
+        })
+
+        nt.assert_equals(response.status_code, httplib.BAD_REQUEST)
+        nt.assert_in('NG', response.content)
+        nt.assert_false(Region.objects.filter(_id=self.institution._id).exists())
+
+    @mock.patch('admin.rdm_custom_storage_location.utils.test_owncloud_connection')
+    def test_success(self, mock_testconnection):
+        mock_testconnection.return_value = {'message': 'Nice'}, httplib.OK
+
+        response = self.view_post({
+            'storage_name': 'My storage',
+            'nextcloud_host': 'valid.nextcloud.net',
+            'nextcloud_username': 'admin',
+            'nextcloud_password': '1234',
+            'nextcloud_folder': 'reserved_for_osf',
+            'provider_short_name': 'nextcloud',
+        })
+
+        nt.assert_equals(response.status_code, httplib.OK)
+        nt.assert_in('Saved credentials successfully!!', response.content)
+
+        institution_storage = Region.objects.filter(_id=self.institution._id).first()
+        nt.assert_is_not_none(institution_storage)
+        nt.assert_equals(institution_storage.name, 'My storage')
+
+        wb_credentials = institution_storage.waterbutler_credentials
+        nt.assert_equals(wb_credentials['storage']['host'], 'https://valid.nextcloud.net')
+        nt.assert_equals(wb_credentials['storage']['username'], 'admin')
+        nt.assert_equals(wb_credentials['storage']['password'], '1234')
+
+        wb_settings = institution_storage.waterbutler_settings
+        nt.assert_equals(wb_settings['storage']['provider'], 'nextcloud')
+        nt.assert_equals(wb_settings['storage']['folder'], '/reserved_for_osf/')
+
 class TestGoogleDriveStorageSaveCredentials(AdminTestCase):
     def setUp(self):
         super(TestGoogleDriveStorageSaveCredentials, self).setUp()
