99b8e18c3d63290999ac6741eb95a2e5505f1e06
John Tordoff <john@cos.io>
2019-07-24 12:50:12 -0400

    seperate v1 and v2 tests

--- api/addons/forward/test_views.py
+++ api/addons/forward/test_views.py
@@ -1,8 +1,6 @@
 import mock
 import pytest
 
-from nose.tools import assert_equal
-
 from addons.forward.tests.utils import ForwardAddonTestCase
 from tests.base import OsfTestCase
 from website import settings
@@ -18,40 +16,16 @@ class TestForward(ForwardAddonTestCase, OsfTestCase):
         super(TestForward, self).setUp()
         self.app.authenticate(*self.user.auth)
 
-    def test_change_url_log_added(self):
-        log_count = self.project.logs.count()
-        self.app.put_json(
-            self.project.api_url_for('forward_config_put'),
-            dict(
-                url='http://how.to.bas/ic',
-            ),
-        )
-        self.project.reload()
-        assert_equal(
-            self.project.logs.count(),
-            log_count + 1
-        )
-
-    def test_change_timeout_log_not_added(self):
-        log_count = self.project.logs.count()
-        self.app.put_json(
-            self.project.api_url_for('forward_config_put'),
-            dict(
-                url=self.node_settings.url,
-            ),
-        )
-        self.project.reload()
-        assert_equal(
-            self.project.logs.count(),
-            log_count
-        )
-
     @mock.patch.object(settings, 'SPAM_CHECK_ENABLED', True)
     @mock.patch('osf.models.node.Node.do_check_spam')
     def test_change_url_check_spam(self, mock_check_spam):
         self.project.is_public = True
         self.project.save()
-        self.app.put_json(self.project.api_url_for('forward_config_put'), {'url': 'http://possiblyspam.com'})
+        self.django_app.put_json_api(
+            '/v2/nodes/{}/addons/forward/'.format(self.project._id),
+            {'data': {'attributes': {'url': 'http://possiblyspam.com'}}},
+            auth=self.user.auth,
+        )
 
         assert mock_check_spam.called
         data, _ = mock_check_spam.call_args
@@ -60,4 +34,3 @@ class TestForward(ForwardAddonTestCase, OsfTestCase):
         assert author == self.user.fullname
         assert author_email == self.user.username
         assert content == 'http://possiblyspam.com'
-
