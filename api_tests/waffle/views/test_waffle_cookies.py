e085acc3a7a74aa109b75c7e1d3ae3589286452c
John Tordoff <john@cos.io>
2019-07-25 16:30:28 -0400

    make osftestcase into pytest for waffle cookie

--- api_tests/waffle/views/test_waffle_cookies.py
+++ api_tests/waffle/views/test_waffle_cookies.py
@@ -4,22 +4,19 @@ from osf_tests.factories import (
     FlagFactory,
 )
 
-from tests.json_api_test_app import JSONAPITestApp
-from tests.base import OsfTestCase
-
-
 @pytest.mark.django_db
-class TestWaffleCookies(OsfTestCase):
+class TestWaffleCookies:
 
-    def setUp(self):
-        super(TestWaffleCookies, self).setUp()
-        self.flag = FlagFactory(name='test_flag')
-        self.flag.percent = 50
-        self.flag.everyone = None
-        self.flag.save()
+    @pytest.fixture()
+    def flag(self):
+        flag = FlagFactory(name='test_flag')
+        flag.percent = 50
+        flag.everyone = None
+        flag.save()
+        return flag
 
     @pytest.mark.enable_bookmark_creation
-    def test_waffle_v2_root_leaves_cookie(self):
+    def test_waffle_v2_root_leaves_cookie(self, app, flag):
         """
         Tests that django-waffle cookies work in the with our Flask requests. Don't need to inject here because the /v2/
         root view already checks if all flags in the DB are active and this the only route  the ember front-end uses
@@ -29,7 +26,6 @@ class TestWaffleCookies(OsfTestCase):
         `dwf_test_flag=True; expires=Fri, 09-Aug-2019 16:33:52 GMT; Max-Age=2592000; Path=/; secure`
 
         """
-        app = JSONAPITestApp()
         resp = app.get('/v2/')
         waffle_cookie = [value for key, value in list(resp.headers.items()) if 'dwf_test_flag' in value][0]
 
