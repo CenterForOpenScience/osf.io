90bb1421e5fff879ebce776b92bf259e6486cef3
rdm-dev12 <rdm-dev12@ml.itg.hitachi.co.j>
2019-08-06 14:41:18 +0900

    Swift layout fix

--- admin/static/js/rdm_custom_storage_location/rdm-institutional-storage-page.js
+++ admin/static/js/rdm_custom_storage_location/rdm-institutional-storage-page.js
@@ -8,11 +8,17 @@ $('[name=options]').change(function () {
     $('#storage_name').attr('disabled', this.value === 'osfstorage');
 });
 
+$('.modal').on('hidden.bs.modal', function (e) {
+    $('body').css("overflow", "auto");
+  })
+
 $('#institutional_storage_form').submit(function (e) {
     if ($('#institutional_storage_form')[0].checkValidity()) {
         var selectedProvider = $('input[name=\'options\']:checked').val();
         var showModal = function () {
             $('#' + selectedProvider + '_modal').modal('show');
+            $('body').css("overflow", "hidden");
+            $('.modal').css("overflow", "auto");
             validateRequiredFields(selectedProvider);
         };
         if (selectedProvider === 'osfstorage') {
