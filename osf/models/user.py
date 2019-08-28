3fc879251953d8724809ff5ce57b813f017dff5d
Takuya Ishibashi <takuya@soum.co.jp>
2019-08-28 06:50:07 +0000

    make "python manage.py gdpr_delete_user" workable.

--- osf/models/user.py
+++ osf/models/user.py
@@ -1780,7 +1780,7 @@ class OSFUser(DirtyFieldsMixin, GuidMixin, BaseModel, AbstractBaseUser, Permissi
                         node._id))
 
             for addon in node.get_addons():
-                if addon.short_name not in ('osfstorage', 'wiki') and addon.user_settings and addon.user_settings.owner.id == self.id:
+                if addon.short_name not in ('osfstorage', 'wiki') and hasattr(addon, 'user_settings') and addon.user_settings and addon.user_settings.owner.id == self.id:
                     raise UserStateError('You cannot delete this user because they '
                                          'have an external account for {} attached to Node {}, '
                                          'which has other contributors.'.format(addon.short_name, node._id))
