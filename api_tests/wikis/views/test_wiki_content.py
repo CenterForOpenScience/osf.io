2b273906f8734939603208b385056660967ca159
John Tordoff <john@cos.io>
2019-09-20 13:43:02 -0400

    fix api_tests/wikis/views/test_wiki_content.py for py3

--- api_tests/wikis/views/test_wiki_content.py
+++ api_tests/wikis/views/test_wiki_content.py
@@ -26,7 +26,7 @@ class TestWikiContentView(ApiWikiTestCase):
         self._set_up_public_project_with_wiki_page()
         self.public_registration = RegistrationFactory(
             project=self.public_project, user=self.user, is_public=True)
-        self.public_registration_wiki_id = WikiPage.objects.get_for_node(self.public_registration, 'home')
+        self.public_registration_wiki_id = WikiPage.objects.get_for_node(self.public_registration, 'home')._id
         self.public_registration.save()
         self.public_registration_url = '/{}wikis/{}/content/'.format(
             API_BASE, self.public_registration_wiki_id)
