c299025488e1a4dcee28c0de90c01d7f56c4cd82
John Tordoff <john@cos.io>
2019-09-20 08:54:04 -0400

    fix TestCSRFValidation for py3

--- api_tests/base/test_auth.py
+++ api_tests/base/test_auth.py
@@ -493,7 +493,7 @@ class TestCSRFValidation:
     @pytest.fixture(autouse=True)
     def set_session_cookie(self, user, app):
         session_cookie = user.get_or_create_cookie()
-        app.set_cookie(COOKIE_NAME, str(session_cookie))
+        app.set_cookie(COOKIE_NAME, session_cookie.decode())
 
     def test_waffle_switch_inactive_does_not_enforce_csrf(self, app, url, payload):
         with override_switch('enforce_csrf', active=False):
