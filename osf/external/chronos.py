896704d20634e5c5ba71b503d8c303e845194c50
John Tordoff <john@cos.io>
2019-10-08 15:59:16 -0400

    fix issue where fullname is given as one word, thanks Cher, Seal, Moby etc al

--- osf/external/chronos.py
+++ osf/external/chronos.py
@@ -83,10 +83,20 @@ class ChronosSerializer(object):
 
     @classmethod
     def serialize_user(cls, user):
+        if not bool(user.given_name) and not bool(user.family_name):
+            raise ValueError(
+                'Cannot submit because user {} with id {} requires a given and family name'.format(
+                    user.given_name if user.given_name and user.family_name else user.fullname,
+                    user._id
+                )
+            )
+
         return {
             'CHRONOS_USER_ID': user.chronos_user_id,
             'EMAIL': user.username,
             'GIVEN_NAME': user.given_name if str(user.given_name) and str(user.family_name) else user.fullname,
+            'FULLNAME': user.fullname,
+            'MIDDLE_NAME': user.middle_names,
             'ORCID_ID': user.social.get('orcid', None),
             'PARTNER_USER_ID': user._id,
             'SURNAME': user.family_name if str(user.given_name) and str(user.family_name) else None,
