e408d5d267ff85ddeb4a8faa5376a66da128e2b1
John Tordoff <john@cos.io>
2019-10-14 10:50:18 -0400

    fix merge conflicts again

--- api/registrations/serializers.py
+++ api/registrations/serializers.py
@@ -388,7 +388,7 @@ class RegistrationSerializer(NodeSerializer):
         matching ANONYMIZED_TITLES.  If present, deletes that question's response
         from meta_values.
         """
-        meta_values = strip_registered_meta_comments(obj.registered_meta.values()[0])
+        meta_values = strip_registered_meta_comments(list(obj.registered_meta.values())[0])
 
         if is_anonymized(self.context['request']):
             registration_schema = RegistrationSchema.objects.get(_id=obj.registered_schema_id)
