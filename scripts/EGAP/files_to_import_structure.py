de6aba76984e3e2b16ddf0b33e6679394561e680
John Tordoff <john@cos.io>
2019-10-23 14:42:08 -0400

    parse those args! for EGAP

--- scripts/EGAP/files_to_import_structure.py
+++ scripts/EGAP/files_to_import_structure.py
@@ -1,43 +1,55 @@
 import os
 import shutil
+import argparse
 from distutils.dir_util import copy_tree
-BASE_PATH = 'EGAP_REGISTRY_staging/3 Registrations/'
-NEW_PATH = 'EGAP_data_10-22-2019'
+
 get_item_id = lambda _path: _path.split(os.sep)[-1].split(' ')[0]
 
-# Copy whole tree, then pick out anonymous
-for item in os.listdir(BASE_PATH):
-    item_id = get_item_id(item)
-    source = os.path.join(BASE_PATH, item)
-    destination = os.path.join(NEW_PATH, item_id, 'data', 'nonanonymous')
-    copy_tree(source, destination)
-
-for root, dir, files in os.walk(NEW_PATH):
-    for item in files:
-        source = os.path.join(root, item)
-        if 'PAP_anon' in item or 'Anonymous.' in item:
-            destination_parent = os.path.join('/'.join(root.split('/')[:-1]), 'anonymous')
-            os.mkdir(destination_parent)
-            destination = os.path.join(destination_parent, item)
-            shutil.move(source, destination)
+parser = argparse.ArgumentParser()
+parser.add_argument('-source', '--source', help='')
+parser.add_argument('-destination', '--destination', help='')
 
 
-# Check All anon files in /anonymous/ direct
-for root, dir, files in os.walk(NEW_PATH):
-    for item in files:
-        if 'PAP_anon' in item or 'Anonymous.' in item:
-            assert root.endswith('/anonymous')
+def main(origin_path, new_path):
+    # Copy whole tree, then pick out anonymous
+    for item in os.listdir(origin_path):
+        item_id = get_item_id(item)
+        source = os.path.join(origin_path, item)
+        destination = os.path.join(origin_path, item_id, 'data', 'nonanonymous')
+        copy_tree(source, destination)
 
-original_files = []
-moved = []
+    for root, dir, files in os.walk(new_path):
+        for item in files:
+            source = os.path.join(root, item)
+            if 'PAP_anon' in item or 'Anonymous.' in item:
+                destination_parent = os.path.join('/'.join(root.split('/')[:-1]), 'anonymous')
+                os.mkdir(destination_parent)
+                destination = os.path.join(destination_parent, item)
+                shutil.move(source, destination)
 
-# Check for stragglers
-for root, dir, files in os.walk(NEW_PATH):
-    for item in files:
-        original_files.append(item)
+    # Check All anon files in /anonymous/ direct
+    for root, dir, files in os.walk(new_path):
+        for item in files:
+            if 'PAP_anon' in item or 'Anonymous.' in item:
+                assert root.endswith('/anonymous')
 
-for root, dir, files in os.walk(NEW_PATH):
-    for item in files:
-        moved.append(item)
+    original_files = []
+    moved = []
 
-assert sorted(original_files) == sorted(moved)
+    # Check for stragglers
+    for root, dir, files in os.walk(new_path):
+        for item in files:
+            original_files.append(item)
+
+    for root, dir, files in os.walk(new_path):
+        for item in files:
+            moved.append(item)
+
+    assert sorted(original_files) == sorted(moved)
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    source = args.source
+    destination = args.destination
+    main(source, destination)
