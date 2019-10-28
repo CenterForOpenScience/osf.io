12399a7d9479f696168365e6659c7d717101f6ab
longze chen <cs.longze.chen@gmail.com>
2019-10-28 13:54:18 -0400

    Improve (refactor) institution auth tests
    - Removed deprecated flask code
    - Renamed fake user names and emails professionally
    - Renamed test method names for better descriptiveness
    - Add extra / missing assertions for both old and new tests
    - Fix style / make it consistent

--- api_tests/institutions/views/test_institution_auth.py
+++ api_tests/institutions/views/test_institution_auth.py
@@ -3,27 +3,24 @@ import json
 import jwe
 import jwt
 import pytest
-from flask import Flask
 
 from api.base import settings
 from api.base.settings.defaults import API_BASE
+
 from framework.auth import signals, Auth
+
 from osf.models import OSFUser
 from osf_tests.factories import InstitutionFactory, ProjectFactory, UserFactory
 
 from tests.base import capture_signals
-
-decoratorapp = Flask('decorators')
 
 
 def make_user(username, fullname):
     return UserFactory(username=username, fullname=fullname)
 
 
-def make_payload(
-        institution, username, fullname='Fake User',
-        given_name='', family_name=''
-):
+def make_payload(institution, username, fullname='Fake User', given_name='', family_name=''):
+
     data = {
         'provider': {
             'id': institution._id,
@@ -37,10 +34,18 @@ def make_payload(
             }
         }
     }
-    return jwe.encrypt(jwt.encode({
-        'sub': username,
-        'data': json.dumps(data)
-    }, settings.JWT_SECRET, algorithm='HS256'), settings.JWE_SECRET)
+
+    return jwe.encrypt(
+        jwt.encode(
+            {
+                'sub': username,
+                'data': json.dumps(data)
+            },
+            settings.JWT_SECRET,
+            algorithm='HS256'
+        ),
+        settings.JWE_SECRET
+    )
 
 
 @pytest.mark.django_db
@@ -50,149 +55,126 @@ class TestInstitutionAuth:
     def institution(self):
         return InstitutionFactory()
 
-    @pytest.yield_fixture(autouse=True)
-    def flask_request_context(self):
-        """
-        required for waffle cookies
-        """
-        with decoratorapp.test_request_context():
-            yield
-
     @pytest.fixture()
     def url_auth_institution(self):
         return '/{0}institutions/auth/'.format(API_BASE)
 
-    def test_creates_user(self, app, url_auth_institution, institution):
-        username = 'hmoco@circle.edu'
+    def test_invalid_payload(self, app, url_auth_institution):
+        res = app.post(url_auth_institution, 'INVALID_PAYLOAD', expect_errors=True)
+        assert res.status_code == 403
+
+    def test_new_user_created(self, app, url_auth_institution, institution):
+
+        username = 'user_created@osf.edu'
         assert OSFUser.objects.filter(username=username).count() == 0
 
         with capture_signals() as mock_signals:
-            res = app.post(
-                url_auth_institution,
-                make_payload(institution, username)
-            )
-
+            res = app.post(url_auth_institution, make_payload(institution, username))
         assert res.status_code == 204
         assert mock_signals.signals_sent() == set([signals.user_confirmed])
 
         user = OSFUser.objects.filter(username=username).first()
-
         assert user
+        assert user.fullname == 'Fake User'
+        assert user.accepted_terms_of_service is not None
         assert institution in user.affiliated_institutions.all()
 
-    def test_adds_institution(self, app, institution, url_auth_institution):
-        username = 'hmoco@circle.edu'
+    def test_existing_user_found_but_not_affiliated(self, app, institution, url_auth_institution):
 
-        user = make_user(username, 'Mr Moco')
+        username = 'user_not_affiliated@osf.edu'
+        user = make_user(username, 'Foo Bar')
+        user.save()
+
+        with capture_signals() as mock_signals:
+            res = app.post(url_auth_institution, make_payload(institution, username))
+        assert res.status_code == 204
+        assert not mock_signals.signals_sent()
+
+        user.reload()
+        assert user.fullname == 'Foo Bar'
+        assert institution in user.affiliated_institutions.all()
+
+    def test_user_found_and_affiliated(self, app, institution, url_auth_institution):
+
+        username = 'user_affiliated@osf.edu'
+        user = make_user(username, 'Foo Bar')
+        user.affiliated_institutions.add(institution)
+        user.save()
+
+        with capture_signals() as mock_signals:
+            res = app.post(url_auth_institution, make_payload(institution, username))
+        assert res.status_code == 204
+        assert not mock_signals.signals_sent()
+
+        user.reload()
+        assert user.fullname == 'Foo Bar'
+        assert user.affiliated_institutions.count() == 1
+
+    def test_new_user_names_guessed_if_not_provided(self, app, institution, url_auth_institution):
+
+        username = 'user_created_with_fullname_only@osf.edu'
+        res = app.post(url_auth_institution, make_payload(institution, username))
+        assert res.status_code == 204
+
+        user = OSFUser.objects.filter(username=username).first()
+        assert user
+        assert user.fullname == 'Fake User'
+        # Given name and family name are guessed from full name
+        assert user.given_name == 'Fake'
+        assert user.family_name == 'User'
+
+    def test_new_user_names_used_when_provided(self, app, institution, url_auth_institution):
+
+        username = 'user_created_with_names@osf.edu'
+        res = app.post(
+            url_auth_institution,
+            make_payload(institution, username, given_name='Foo', family_name='Bar')
+        )
+        assert res.status_code == 204
+
+        user = OSFUser.objects.filter(username=username).first()
+        assert user
+        assert user.fullname == 'Fake User'
+        # Given name and family name are set instead of guessed
+        assert user.given_name == 'Foo'
+        assert user.family_name == 'Bar'
+
+    def test_user_active(self, app, institution, url_auth_institution):
+
+        username, fullname, password = 'user_active@user.edu', 'Foo Bar', 'FuAsKeEr'
+        user = make_user(username, fullname)
+        user.set_password(password)
         user.save()
 
         with capture_signals() as mock_signals:
             res = app.post(
                 url_auth_institution,
-                make_payload(institution, username)
+                make_payload(
+                    institution,
+                    username,
+                    family_name='User',
+                    given_name='Fake',
+                    fullname='Fake User'
+                )
             )
-
         assert res.status_code == 204
-        assert mock_signals.signals_sent() == set()
-
-        user.reload()
-        assert institution in user.affiliated_institutions.all()
-
-    def test_finds_user(self, app, institution, url_auth_institution):
-        username = 'hmoco@circle.edu'
-
-        user = make_user(username, 'Mr Moco')
-        user.affiliated_institutions.add(institution)
-        user.save()
-
-        res = app.post(
-            url_auth_institution,
-            make_payload(institution, username)
-        )
-        assert res.status_code == 204
-
-        user.reload()
-        assert user.affiliated_institutions.count() == 1
-
-    def test_bad_token(self, app, url_auth_institution):
-        res = app.post(
-            url_auth_institution,
-            'al;kjasdfljadf',
-            expect_errors=True
-        )
-        assert res.status_code == 403
-
-    def test_user_names_guessed_if_not_provided(
-            self, app, institution, url_auth_institution):
-        # Regression for https://openscience.atlassian.net/browse/OSF-7212
-        username = 'fake@user.edu'
-        res = app.post(
-            url_auth_institution,
-            make_payload(institution, username)
-        )
-
-        assert res.status_code == 204
-        user = OSFUser.objects.filter(username=username).first()
-
-        assert user
-        assert user.fullname == 'Fake User'
-        assert user.given_name == 'Fake'
-        assert user.family_name == 'User'
-
-    def test_user_names_used_when_provided(
-            self, app, institution, url_auth_institution):
-        # Regression for https://openscience.atlassian.net/browse/OSF-7212
-        username = 'fake@user.edu'
-        res = app.post(
-            url_auth_institution,
-            make_payload(
-                institution,
-                username,
-                family_name='West',
-                given_name='Kanye'
-            )
-        )
-
-        assert res.status_code == 204
-        user = OSFUser.objects.filter(username=username).first()
-
-        assert user
-        assert user.fullname == 'Fake User'
-        assert user.given_name == 'Kanye'
-        assert user.family_name == 'West'
-
-    def test_user_active(self, app, institution, url_auth_institution):
-
-        username, fullname, password = 'fake_active@user.edu', 'Active User', 'FuAsKeEr'
-        user = make_user(username, fullname)
-        user.set_password(password)
-        user.save()
-
-        res = app.post(
-            url_auth_institution,
-            make_payload(
-                institution,
-                username,
-                family_name='Family',
-                given_name='Given',
-                fullname='Full'
-            )
-        )
-        assert res.status_code == 204
+        assert not mock_signals.signals_sent()
 
         user = OSFUser.objects.filter(username=username).first()
         assert user
         # User names remains untouched
         assert user.fullname == fullname
-        assert user.family_name == 'User'
-        assert user.given_name == 'Active'
+        assert user.family_name == 'Bar'
+        assert user.given_name == 'Foo'
         # Existing active user keeps their password
         assert user.has_usable_password()
         assert user.check_password(password)
+        # Confirm affiliation
+        assert institution in user.affiliated_institutions.all()
 
     def test_user_unclaimed(self, app, institution, url_auth_institution):
 
-        username, fullname = 'fake_unclaimed@user.edu', 'Unclaimed User'
+        username, fullname = 'user_nclaimed@user.edu', 'Foo Bar'
         project = ProjectFactory()
         user = project.add_unregistered_contributor(
             fullname=fullname,
@@ -203,68 +185,76 @@ class TestInstitutionAuth:
         # Unclaimed user is given an unusable password when being added as a contributor
         assert not user.has_usable_password()
 
-        res = app.post(
-            url_auth_institution,
-            make_payload(
-                institution,
-                username,
-                family_name='Family',
-                given_name='Given',
-                fullname='Full'
+        with capture_signals() as mock_signals:
+            res = app.post(
+                url_auth_institution,
+                make_payload(
+                    institution,
+                    username,
+                    family_name='User',
+                    given_name='Fake',
+                    fullname='Fake User'
+                )
             )
-        )
         assert res.status_code == 204
+        assert mock_signals.signals_sent() == set([signals.user_confirmed])
 
         user = OSFUser.objects.filter(username=username).first()
         assert user
-        # User becomes active and names (except the full name) are updated
+        # User becomes active and all names are updated
         assert user.is_active
-        assert user.fullname == 'Full'
-        assert user.family_name == 'Family'
-        assert user.given_name == 'Given'
+        assert user.fullname == 'Fake User'
+        assert user.family_name == 'User'
+        assert user.given_name == 'Fake'
         # Unclaimed records must have been cleared
         assert not user.unclaimed_records
         # Previously unclaimed user must be assigned a usable password during institution auth
         assert user.has_usable_password()
         # User remains to be a contributor of the project
         assert project.is_contributor(user)
+        # Confirm affiliation
+        assert institution in user.affiliated_institutions.all()
 
     def test_user_unconfirmed(self, app, institution, url_auth_institution):
 
-        username, fullname, password = 'fake_unconfirmed@user.edu', 'Unconfirmed User', 'FuAsKeEr'
+        username, fullname, password = 'user_unconfirmed@osf.edu', 'Foo Bar', 'FuAsKeEr'
         user = OSFUser.create_unconfirmed(username, password, fullname)
         user.save()
         # Unconfirmed user has a usable password created during sign-up
         assert user.has_usable_password()
 
-        res = app.post(
-            url_auth_institution,
-            make_payload(
-                institution,
-                username,
-                family_name='Family',
-                given_name='Given',
-                fullname='Full'
+        with capture_signals() as mock_signals:
+            res = app.post(
+                url_auth_institution,
+                make_payload(
+                    institution,
+                    username,
+                    family_name='User',
+                    given_name='Fake',
+                    fullname='Fake User'
+                )
             )
-        )
         assert res.status_code == 204
+        assert mock_signals.signals_sent() == set([signals.user_confirmed])
 
         user = OSFUser.objects.filter(username=username).first()
         assert user
-        # User becomes active and names (except the full name) are updated
+        # User becomes active and all names are updated
         assert user.is_active
-        assert user.fullname == 'Full'
-        assert user.family_name == 'Family'
-        assert user.given_name == 'Given'
+        assert user.fullname == 'Fake User'
+        assert user.family_name == 'User'
+        assert user.given_name == 'Fake'
         # Pending email verifications must be cleared
         assert not user.email_verifications
         # Previously unconfirmed user must be given a new password during institution auth
         assert user.has_usable_password()
         assert not user.check_password(password)
+        # Confirm affiliation
+        assert institution in user.affiliated_institutions.all()
 
     def test_user_inactive(self, app, institution, url_auth_institution):
 
-        username, fullname, password = 'fake_inactive@user.edu', 'Inactive User', 'FuAsKeEr'
+        username, fullname, password = 'user_inactive@osf.edu', 'Foo Bar', 'FuAsKeEr'
         user = make_user(username, fullname)
         user.set_password(password)
         # User must be saved before deactivation
@@ -275,23 +265,26 @@ class TestInstitutionAuth:
         assert user.has_usable_password()
         assert user.check_password(password)
 
-        res = app.post(
-            url_auth_institution,
-            make_payload(
-                institution,
-                username,
-                family_name='Family',
-                given_name='Given',
-                fullname='Full'
-            ),
-            expect_errors=True
-        )
+        with capture_signals() as mock_signals:
+            res = app.post(
+                url_auth_institution,
+                make_payload(
+                    institution,
+                    username,
+                    family_name='User',
+                    given_name='Fake',
+                    fullname='Fake User'
+                ),
+                expect_errors=True
+            )
         assert res.status_code == 403
+        assert not mock_signals.signals_sent()
 
         user = OSFUser.objects.filter(username=username).first()
         assert user
-        # Inactive user remains untouched
+        # Inactive user remains untouched, including affiliation
         assert user.is_disabled
         assert user.fullname == fullname
-        assert user.family_name == 'User'
-        assert user.given_name == 'Inactive'
+        assert user.given_name == 'Foo'
+        assert user.family_name == 'Bar'
+        assert institution not in user.affiliated_institutions.all()
