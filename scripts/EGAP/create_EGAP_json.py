b4f1f1067e4dbe77a80da43a868bcb6458206f92
Brian J. Geiger <bgeiger@pobox.com>
2019-10-29 08:41:11 -0400

    Update script to work with Jupyter Notebook and Python 3
    This is significantly less DRY, though a refactor of utils would allow us to DRY it. Also add a Jupyter Notebook that will run script functions.

--- scripts/EGAP/create_EGAP_json.py
+++ scripts/EGAP/create_EGAP_json.py
@@ -4,15 +4,10 @@ import datetime
 import json
 import os
 import shutil
-import re
 import jsonschema
 import argparse
 
-from django.core.management.base import BaseCommand
 from jsonschema.exceptions import ValidationError
-
-from website.project.metadata.utils import create_jsonschema_from_metaschema
-from website.project.metadata.schemas import ensure_schema_structure, from_json
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -66,6 +61,22 @@ other_mapping = {
     'q30': 'q31'
 }
 
+
+here = os.path.split(os.path.abspath(__file__))[0]
+
+
+def from_json(fname):
+    with open(os.path.join(here, fname)) as f:
+        return json.load(f)
+
+
+def ensure_schema_structure(schema):
+    schema['pages'] = schema.get('pages', [])
+    schema['title'] = schema['name']
+    schema['version'] = schema.get('version', 1)
+    return schema
+
+
 def create_file_tree_and_json(author_source, registry_source, target):
     # Things this function needs to do:
     # For each row in the registry function, create a directory.
@@ -75,14 +86,17 @@ def create_file_tree_and_json(author_source, registry_source, target):
     logger.info('Creating EGAP directory at {}'.format(top_dir))
     os.mkdir(top_dir)
     author_list = create_author_dict(author_source)
-    with open(registry_source) as csv_registry_file:
+    with open(registry_source, 'rt', encoding='utf-8-sig') as csv_registry_file:
         csv_reader = csv.reader(csv_registry_file, delimiter=',')
         header_row = next(csv_reader)
-        normalized_header_row = [col_header.decode('ascii', 'ignore') for col_header in header_row]
+        normalized_header_row = [col_header.strip() for col_header in header_row]
+        logger.info('Debug data')
+        logger.info('Header row: {}'.format(header_row))
+        logger.info('Normalized header row: {}'.format(normalized_header_row))
 
         id_index = normalized_header_row.index('ID')
         for line in csv_reader:
-            row = [cell.decode('ascii', 'ignore') for cell in line]
+            row = [cell for cell in line]
             project_id = row[id_index]
             logger.info('Adding project ID: {}'.format(project_id))
             root_directory = os.path.join(top_dir, project_id)
@@ -95,18 +109,21 @@ def create_file_tree_and_json(author_source, registry_source, target):
             registration_dict = make_registration_dict(row, normalized_header_row)
             make_json_file(root_directory, registration_dict, 'registration')
 
+
 def create_author_dict(source):
     # Reads in author CSV and returns a list of dicts with names and emails of EGAP Authors
     authors = []
-    with open(source) as csv_file:
+    with open(source, 'rt', encoding='utf-8-sig') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         header_row = next(csv_reader)
-        normalized_header_row = [col_header.decode('ascii', 'ignore').strip() for col_header in header_row]
-
+        normalized_header_row = [col_header.strip() for col_header in header_row]
+        logger.info('Debug data')
+        logger.info('Header row: {}'.format(header_row))
+        logger.info('Normalized header row: {}'.format(normalized_header_row))
         name_index = normalized_header_row.index('Name')
         email_index = normalized_header_row.index('Email')
         for line in csv_reader:
-            row = [cell.decode('ascii', 'ignore') for cell in line]
+            row = [cell for cell in line]
             logger.info('Adding user: ' + row[name_index])
             if row[email_index] != '':
                 author_dict = {'name': row[name_index].strip(), 'email': row[email_index]}
@@ -114,6 +131,7 @@ def create_author_dict(source):
                 author_dict = {'name': row[name_index].strip()}
             authors.append(author_dict)
     return authors
+
 
 def make_project_dict(row, author_list, normalized_header_row):
     project = {}
@@ -141,12 +159,13 @@ def make_project_dict(row, author_list, normalized_header_row):
                 project['contributors'].append(author_list[author_list_index])
     return project
 
+
 def make_registration_dict(row, normalized_header_row):
     registration = {}
 
     for question in schema_to_spreadsheet_mapping:
-        qid = question.keys()[0]
-        column_name = question.values()[0]
+        qid = list(question.keys())[0]
+        column_name = list(question.values())[0]
         value = build_question_response(normalized_header_row, row, qid, column_name)
         validated_qid, other_response = validate_response(qid, value)
         registration[validated_qid] = value
@@ -158,6 +177,7 @@ def make_registration_dict(row, normalized_header_row):
     registration['q36'] = build_nested_response('Agree')
     return registration
 
+
 def make_json_file(filepath, data, json_type):
     if json_type == 'project':
         filepath = filepath + '/project.json'
@@ -165,6 +185,7 @@ def make_json_file(filepath, data, json_type):
         filepath = filepath + '/registration-schema.json'
     with open(filepath, 'w') as outfile:
         json.dump(data, outfile)
+
 
 def build_question_response(header_row, row, question_key, column_title):
     """Format the question's response to go in the registration_metadata
@@ -180,6 +201,7 @@ def build_question_response(header_row, row, question_key, column_title):
         value = value.split(', ')
     return build_nested_response(value)
 
+
 def clean_value(value):
     """Clean spreadsheet values of issues that will affect validation """
     if value == 'n/a':
@@ -194,6 +216,257 @@ def build_nested_response(value):
         'extra': [],
         'value': value
     }
+
+
+def base_metaschema(metaschema):
+    json_schema = {
+        'type': 'object',
+        'description': metaschema['description'],
+        'title': metaschema['title'],
+        'additionalProperties': False,
+        'properties': {
+        }
+    }
+    return json_schema
+
+
+def get_required(question):
+    """
+    Returns True if metaschema question is required.
+    """
+    required = question.get('required', False)
+    if not required:
+        properties = question.get('properties', False)
+        if properties and isinstance(properties, list):
+            for item, property in enumerate(properties):
+                if isinstance(property, dict) and property.get('required', False):
+                    required = True
+                    break
+    return required
+
+
+COMMENTS_SCHEMA = {
+    'type': 'array',
+    'items': {
+        'type': 'object',
+        'additionalProperties': False,
+        'properties': {
+            'seenBy': {
+                'type': 'array',
+            },
+            'canDelete': {'type': 'boolean'},
+            'created': {'type': 'string'},
+            'lastModified': {'type': 'string'},
+            'author': {'type': 'string'},
+            'value': {'type': 'string'},
+            'isOwner': {'type': 'boolean'},
+            'getAuthor': {'type': 'string'},
+            'user': {
+                'type': 'object',
+                'additionalProperties': True,
+                'properties': {
+                    'fullname': {'type': 'string'},
+                    'id': {'type': 'integer'}
+                }
+            },
+            'saved': {'type': 'boolean'},
+            'canEdit': {'type': 'boolean'},
+            'isDeleted': {'type': 'boolean'}
+        }
+    }
+}
+
+
+def get_options_jsonschema(options, required):
+    """
+    Returns multiple choice options for schema questions
+    """
+    for item, option in enumerate(options):
+        if isinstance(option, dict) and option.get('text'):
+            options[item] = option.get('text')
+    value = {'enum': options}
+
+    if not required and '' not in value['enum']:  # Non-required fields need to accept empty strings as a value.
+        value['enum'].append('')
+
+    return value
+
+
+def get_object_jsonschema(question, required_fields, is_reviewer, is_required):
+    """
+    Returns jsonschema for nested objects within schema
+    """
+    object_jsonschema = {
+        'type': 'object',
+        'additionalProperties': False,
+        'properties': {
+
+        }
+    }
+    required = []
+    properties = question.get('properties')
+    if properties:
+        for property in properties:
+            if property.get('required', False) and required_fields:
+                required.append(property['id'])
+            values = extract_question_values(property, required_fields, is_reviewer, is_required)
+            object_jsonschema['properties'][property['id']] = {
+                'type': 'object',
+                'additionalProperties': False,
+                'properties': values
+            }
+            if required_fields:
+                object_jsonschema['properties'][property['id']]['required'] = ['value']
+    if required_fields and is_required:
+        object_jsonschema['required'] = required
+
+    return object_jsonschema
+
+
+OSF_UPLOAD_EXTRA_SCHEMA = {
+    'type': 'array',
+    'items': {
+        'type': 'object',
+        'additionalProperties': False,
+        'properties': {
+            'data': {
+                'type': 'object',
+                'additionalProperties': False,
+                'properties': {
+                    'kind': {'type': 'string'},
+                    'contentType': {'type': 'string'},
+                    'name': {'type': 'string'},
+                    'extra': {
+                        'type': 'object',
+                        'additionalProperties': False,
+                        'properties': {
+                            'downloads': {'type': 'integer'},
+                            'version': {'type': 'integer'},
+                            'latestVersionSeen': {'type': 'string'},
+                            'guid': {'type': 'string'},
+                            'checkout': {'type': 'string'},
+                            'hashes': {
+                                'type': 'object',
+                                'additionalProperties': False,
+                                'properties': {
+                                    'sha256': {'type': 'string'},
+                                    'md5': {'type': 'string'}
+                                }
+                            }
+                        }
+                    },
+                    'materialized': {'type': 'string'},
+                    'modified': {'type': 'string'},
+                    'nodeId': {'type': 'string'},
+                    'etag': {'type': 'string'},
+                    'provider': {'type': 'string'},
+                    'path': {'type': 'string'},
+                    'nodeUrl': {'type': 'string'},
+                    'waterbutlerURL': {'type': 'string'},
+                    'resource': {'type': 'string'},
+                    'nodeApiUrl': {'type': 'string'},
+                    'type': {'type': 'string'},
+                    'accept': {
+                        'type': 'object',
+                        'additionalProperties': False,
+                        'properties': {
+                            'acceptedFiles': {'type': 'boolean'},
+                            'maxSize': {'type': 'integer'},
+                        }
+                    },
+                    'links': {
+                        'type': 'object',
+                        'additionalProperties': False,
+                        'properties': {
+                            'download': {'type': 'string'},
+                            'move': {'type': 'string'},
+                            'upload': {'type': 'string'},
+                            'delete': {'type': 'string'}
+                        }
+                    },
+                    'permissions': {
+                        'type': 'object',
+                        'additionalProperties': False,
+                        'properties': {
+                            'edit': {'type': 'boolean'},
+                            'view': {'type': 'boolean'}
+                        }
+                    },
+                    'created_utc': {'type': 'string'},
+                    'id': {'type': 'string'},
+                    'modified_utc': {'type': 'string'},
+                    'size': {'type': 'integer'},
+                    'sizeInt': {'type': 'integer'},
+                }
+            },
+            'fileId': {'type': ['string', 'object']},
+            'descriptionValue': {'type': 'string'},
+            'sha256': {'type': 'string'},
+            'selectedFileName': {'type': 'string'},
+            'nodeId': {'type': 'string'},
+            'viewUrl': {'type': 'string'}
+        }
+    }
+}
+
+
+def extract_question_values(question, required_fields, is_reviewer, is_required):
+    """
+    Pulls structure for 'value', 'comments', and 'extra' items
+    """
+    response = {
+        'value': {'type': 'string'},
+        'comments': COMMENTS_SCHEMA,
+        'extra': {'type': 'array'}
+    }
+    if question.get('type') == 'object':
+        response['value'] = get_object_jsonschema(question, required_fields, is_reviewer, is_required)
+    elif question.get('type') == 'choose':
+        options = question.get('options')
+        if options:
+            enum_options = get_options_jsonschema(options, is_required)
+            if question.get('format') == 'singleselect':
+                response['value'] = enum_options
+            elif question.get('format') == 'multiselect':
+                response['value'] = {'type': 'array', 'items': enum_options}
+    elif question.get('type') == 'osf-upload':
+        response['extra'] = OSF_UPLOAD_EXTRA_SCHEMA
+
+    if is_reviewer:
+        del response['extra']
+        if not question.get('type') == 'object':
+            del response['value']
+
+    return response
+
+
+def create_jsonschema_from_metaschema(metaschema, required_fields=False, is_reviewer=False):
+    """
+    Creates jsonschema from registration metaschema for validation.
+
+    Reviewer schemas only allow comment fields.
+    """
+    json_schema = base_metaschema(metaschema)
+    required = []
+
+    for page in metaschema['pages']:
+        for question in page['questions']:
+            is_required = get_required(question)
+            if is_required and required_fields:
+                required.append(question['qid'])
+            json_schema['properties'][question['qid']] = {
+                'type': 'object',
+                'additionalProperties': False,
+                'properties': extract_question_values(question, required_fields, is_reviewer, is_required)
+            }
+            if required_fields:
+                json_schema['properties'][question['qid']]['required'] = ['value']
+
+        if required and required_fields:
+            json_schema['required'] = required
+
+    return json_schema
+
 
 def validate_response(qid, value):
     """Validate question response
@@ -227,6 +500,7 @@ def validate_response(qid, value):
             raise Exception(exc)
     return qid, None
 
+
 def main(default_args=False):
     if default_args:
         args = parser.parse_args(['--source', 'default', '--target', 'default'])
@@ -252,6 +526,7 @@ def main(default_args=False):
     if dry_run:
         shutil.rmtree(target_directory)
         raise RuntimeError('Dry run, file tree being deleted.')
+
 
 if __name__ == '__main__':
 
