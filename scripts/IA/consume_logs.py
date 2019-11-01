1594d8fc5c59e96c47f953ac6303d34f7e1be876
corbinSanders <corbin@cos.io>
2019-11-01 15:06:52 -0400

    Creating script to consume logs

---
+++ scripts/IA/consume_logs.py
@@ -0,0 +1,65 @@
+import requests
+import os
+import argparse
+import json
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+	'-g',
+	'--guid',
+	help='This is the GUID of the target node on the OSF'
+)
+parser.add_argument(
+	'-d',
+	'--directory',
+	help='This is the target Directory for the project and its files'
+)
+
+LOGS_URL = 'https://api.osf.io/v2/registrations/{}/logs/?page[size]=100'
+
+def json_with_pagination(path, guid, page, url):
+	response = requests.get(url)
+	if response.status_code == 404:
+		raise ValueError('Project not found. Check GUID and try again.')
+
+	json_filename = guid + '-' + str(page) + '.json'
+	file_location = os.path.join(path, json_filename)
+	with open(file_location, 'wb') as file:
+		file.write(response.content)
+	return json.loads(response.content)
+
+def main(default_args=True):
+	if (default_args):
+		args = parser.parse_args(['--guid', 'default', '--directory', 'default'])
+	else:
+		args = parser.parse_args()
+	args = parser.parse_args()
+	guid = args.guid
+	directory = args.directory
+
+	if not guid:
+		raise ValueError('Project GUID must be specified! Use -g')
+	if not directory:
+		# Setting default to current directory
+		directory = '.'
+
+	path = os.path.join(directory,guid)
+	if not os.path.exists(path):
+		os.mkdir(path)
+	path = os.path.join(path,'logs')
+	os.mkdir(path)
+
+	response = json_with_pagination(path, guid, 1, LOGS_URL.format(guid))
+	page_num = 2
+	while response['links']['next']:
+		next_link = response['links']['next']
+		response = json_with_pagination(path, guid, page_num, next_link)
+		page_num = page_num + 1
+
+	print('Log data successfully transferred!')
+
+
+
+if __name__ == '__main__':
+
+    main(default_args=False)
