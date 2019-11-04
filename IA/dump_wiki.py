1a58b5ce65fcdb9304929f9011ca209e247f5e97
John Tordoff <john@cos.io>
2019-11-04 09:55:34 -0500

    add test for wikis with pagination for IA

--- IA/dump_wiki.py
+++ IA/dump_wiki.py
@@ -15,7 +15,7 @@ async def get_wiki_pages(guid, page, result={}):
 
 
 async def write_wiki_content(page):
-    with open(os.path.join(HERE, f'{page["attributes"]["name"]}.md'), 'wb') as fp:
+    with open(os.path.join(HERE, f'/{page["attributes"]["name"]}.md'), 'wb') as fp:
         fp.write(requests.get(page['links']['download']).content)
 
 
@@ -31,16 +31,17 @@ async def main(guid):
     :return:
     """
 
-    url = f'{API_URL}/v2/registrations/{guid}/wikis/'
-
-    data = requests.get(url).json()
-    pages = math.ceil(int(data['links']['meta']['total']) / int(data['links']['meta']['per_page']))
-    result = {1: data['data']}
+    url = f'{API_URL}/v2/registrations/{guid}/wikis/?page=1'
     tasks = []
 
-    for i in range(1, pages):
-        task = get_wiki_pages(guid, i + 1, result)
-        tasks.append(task)
+    data = requests.get(url).json()
+    result = {1: data['data']}
+
+    if data['links']['next'] is not None:
+        pages = math.ceil(int(data['meta']['total']) / int(data['meta']['per_page']))
+        for i in range(1, pages):
+            task = get_wiki_pages(guid, i + 1, result)
+            tasks.append(task)
 
     await asyncio.gather(*tasks)
 
@@ -48,8 +49,6 @@ async def main(guid):
     # through the magic of async all our pages have loaded.
     for page in list(result.values()):
         pages_as_list += page
-
-    print(len(pages_as_list))
 
     write_tasks = []
     for page in pages_as_list:
---
+++ IA/tests/test_dump_wiki.py
@@ -0,0 +1,118 @@
+import os
+import asyncio
+import json
+import mock
+from mock import call, Mock
+import unittest
+import responses
+HERE = os.path.dirname(os.path.abspath(__file__))
+from nose.tools import assert_equal
+
+from IA.dump_wiki import main
+
+
+def wiki_metadata():
+    with open(os.path.join(HERE, 'fixtures/wiki-metadata-response.json'), 'r') as fp:
+        return json.loads(fp.read())
+
+
+def wiki_metadata_two_pages():
+    with open(os.path.join(HERE, 'fixtures/wiki-metadata-response-page-1.json'), 'r') as fp:
+        page1 = json.loads(fp.read())
+
+    with open(os.path.join(HERE, 'fixtures/wiki-metadata-response-page-2.json'), 'r') as fp:
+        page2 = json.loads(fp.read())
+
+    return page1, page2
+
+
+class TestWikiDumper(unittest.TestCase):
+
+    @responses.activate
+    def test_wiki_dump(self):
+        responses.add(
+            responses.Response(
+                responses.GET,
+                'http://localhost:8000/v2/registrations/fxehm/wikis/?page=1',
+                json=wiki_metadata(),
+            )
+        )
+        responses.add(
+            responses.Response(
+                responses.GET,
+                'http://localhost:8000/v2/wikis/dtns3/content/',
+                body=b'dtns3 data',
+            ),
+        )
+        responses.add(
+            responses.Response(
+                responses.GET,
+                'http://localhost:8000/v2/wikis/md549/content/',
+                body=b'md549 data',
+            ),
+        )
+        responses.add(
+            responses.Response(
+                responses.GET,
+                'http://localhost:8000/v2/wikis/p8kxa/content/',
+                body=b'p8kxa data',
+            ),
+        )
+
+        with mock.patch('builtins.open', mock.mock_open()) as m:
+            asyncio.run(main('fxehm'))
+            assert m.call_args_list == [
+                call('/home.md', 'wb'),
+                call('/test1Ω≈ç√∫˜µ≤≥≥÷åß∂ƒ©˙∆∆˚¬…æ.md', 'wb'),
+                call('/test2.md', 'wb')
+            ]
+            handle = m()
+
+            assert handle.write.call_args_list == [
+                call(b'dtns3 data'),
+                call(b'md549 data'),
+                call(b'p8kxa data')
+            ]
+
+    @responses.activate
+    def test_wiki_dump_multiple_pages(self):
+        page1, page2 = wiki_metadata_two_pages()
+
+        responses.add(
+            responses.Response(
+                responses.GET,
+                'http://localhost:8000/v2/registrations/fxehm/wikis/?page=1',
+                json=page1,
+            )
+        )
+        responses.add(
+            responses.Response(
+                responses.GET,
+                'http://localhost:8000/v2/registrations/fxehm/wikis/?page=2',
+                json=page2,
+            )
+        )
+
+        data = page1['data'] + page2['data']
+        for wiki in data:
+            responses.add(
+                responses.Response(
+                    responses.GET,
+                    f'http://localhost:8000/v2/wikis{wiki["attributes"]["path"]}/content/',
+                    body=f'{wiki["attributes"]["path"]} data',
+                ),
+            )
+
+        with mock.patch('builtins.open', mock.mock_open()) as m:
+            asyncio.run(main('fxehm'))
+            assert_equal(
+                m.call_args_list,
+                [call(f'/{wiki["attributes"]["name"]}.md', 'wb') for wiki in data]
+            )
+
+            handle = m()
+
+            assert_equal(
+                handle.write.call_args_list,
+                [call(f'{wiki["attributes"]["path"]} data'.encode()) for wiki in data]
+            )
