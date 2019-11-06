6665611281a5816a42293ed678617ce63c5fd8d9
corbinSanders <corbin@cos.io>
2019-11-06 10:52:21 -0500

    Fixing various script issues. Writing tests

--- scripts/IA/consume_files.py
+++ scripts/IA/consume_files.py
@@ -14,31 +14,40 @@ parser.add_argument(
 	'--directory',
 	help='This is the target Directory for the project and its files'
 )
+parser.add_argument(
+	'-t',
+	'--token',
+	help='This is the bearer token for auth. This is required'
+)
 
-def main(default_args=True):
-	if (default_args):
-		args = parser.parse_args(['--guid', 'default', '--directory', 'default'])
-	else:
-		args = parser.parse_args()
-	args = parser.parse_args()
-	guid = args.guid
-	directory = args.directory
+def consume_files(guid, token, directory):
 
-	if not guid:
-		raise ValueError('Project GUID must be specified! Use -g')
-	if not directory:
-		# Setting default to current directory
-		directory = '.'
-
-	zip_url = 'https://files.osf.io/v1/resources/xxxxx/providers/osfstorage/?zip='
+	zip_url = 'https://files.osf.io/v1/resources/{}/providers/osfstorage/?zip='
 	path = os.path.join(directory,guid)
 	os.mkdir(path)
 	path = os.path.join(path,'files')
 	os.mkdir(path)
+	auth_header = {'Authorization': 'Bearer '+token}
 
-	response = requests.get(zip_url.format(guid))
-	if response.status_code == 404:
-		raise ValueError('Project not found. Check GUID and try again.')
+	try:
+		response = requests.get(zip_url.format(guid), headers=auth_header)
+		if response.status_code == 429:
+			keep_trying = retry
+			response_headers = response.headers
+			wait_time = response_headers['Retry-After']
+			if keep_trying:
+				logging.log(logging.INFO, 'Throttled: retrying in {wait_time}s')
+				time.sleep(int(wait_time))
+			else:
+				logging.log(logging.ERROR, 'Throttled. Please retry after {wait_time}s')
+		elif response.status_code >= 400:
+			status_code = response.status_code
+			content = getattr(response, 'content', None)
+			raise requests.exceptions.HTTPError(
+				'Status code {}. {}'.format(status_code, content))
+	except requests.exceptions.RequestException as e:
+		logging.log(logging.ERROR,'HTTP Request failed: {}'.format(e))
+		raise
 
 	zipfile_location = os.path.join(path, (guid+'.zip'))
 	with open(zipfile_location, 'wb') as file:
@@ -50,8 +59,28 @@ def main(default_args=True):
 	os.remove(zipfile_location)
 	print('File data successfully transferred!')
 
+def main(default_args=True):
+	if (default_args):
+		args = parser.parse_args(['--guid', 'default', '--directory', 'default'])
+	else:
+		args = parser.parse_args()
+	args = parser.parse_args()
+	guid = args.guid
+	directory = args.directory
+	token = args.token
+
+	if not guid:
+		raise ValueError('Project GUID must be specified! Use -g')
+	if not token:
+		raise ValueError('Token must be provided! Use -t')
+	if not directory:
+		# Setting default to current directory
+		directory = '.'
+
+	consume_files(guid, token, directory)
+
 
 
 if __name__ == '__main__':
 
-    main(default_args=False)
+	main(default_args=False)
---
+++ scripts/IA/consume_files_test.py
@@ -0,0 +1,62 @@
+import os
+import shutil
+import pytest
+from mock import call, Mock
+import unittest
+import responses
+import json
+HERE = os.path.dirname(os.path.abspath(__file__))
+from nose.tools import assert_equal
+from zipfile import ZipFile
+
+from consume_files import consume_files
+
+
+
+class TestIAFiles(unittest.TestCase):
+
+	def tearDown(self):
+		if os.path.isdir(os.path.join(HERE, 'sgg32')):
+			shutil.rmtree(os.path.join(HERE, 'sgg32'))
+		if os.path.isdir(os.path.join(HERE, 'jj81a')):
+			shutil.rmtree(os.path.join(HERE, 'jj81a'))
+
+
+	@responses.activate
+	def test_file_dump(self):
+		with open('tests/fixtures/sgg32.zip') as zipfile:
+			responses.add(
+				responses.Response(
+					responses.GET,
+					'https://files.osf.io/v1/resources/sgg32/providers/osfstorage/?zip=',
+					body = zipfile.read(), status=200,
+					stream=True
+				)
+			)
+
+
+		consume_files('sgg32', 'asdfasdfasdgfasg', '.')
+
+		assert os.path.isdir(os.path.join(HERE,'sgg32/files'))
+		assert os.path.isfile(os.path.join(HERE, 'sgg32/files/test.txt'))
+
+
+	@responses.activate
+	def test_file_dump_multiple_levels(self):
+		with open('tests/fixtures/jj81a.zip') as zipfile:
+			responses.add(
+				responses.Response(
+					responses.GET,
+					'https://files.osf.io/v1/resources/jj81a/providers/osfstorage/?zip=',
+					body = zipfile.read(), status=200,
+					stream=True
+				)
+			)
+
+
+		consume_files('jj81a', 'asdfasdfasdgfasg', '.')
+
+		assert os.path.isdir(os.path.join(HERE,'jj81a/files/Folder 1'))
+		assert os.path.isfile(os.path.join(HERE, 'jj81a/files/Folder 1/test.txt'))
+		assert os.path.isfile(os.path.join(HERE, 'jj81a/files/Folder 1/Folder two/test3.txt'))
+
