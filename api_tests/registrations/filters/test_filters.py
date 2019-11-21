c93dfe06cf2a0374b745c8123914bea92aa34818
corbinSanders <corbin@cos.io>
2019-11-21 11:15:15 -0500

    Fixing tests again

--- api_tests/registrations/filters/test_filters.py
+++ api_tests/registrations/filters/test_filters.py
@@ -32,7 +32,8 @@ class RegistrationListFilteringMixin(object):
 
         self.node_A = RegistrationFactory(project=self.A, creator=self.user)
         self.node_B2 = RegistrationFactory(project=self.B2, creator=self.user)
-        self.node_B3 = RegistrationFactory(project=self.B2, creator=self.user, parent=self.node_B2)
+        self.node_C = RegistrationFactory(project=self.B2, creator=self.user)
+        self.node_C.parent_node = self.B2
 
         self.parent_url = '{}filter[parent]='.format(self.url)
         self.parent_url_ne = '{}filter[parent][ne]='.format(self.url)
@@ -50,7 +51,7 @@ class RegistrationListFilteringMixin(object):
         assert_equal(set(expected), set(actual))
 
     def test_parent_filter_ne_null(self):
-        expected = [self.node_B3._id]
+        expected = []
         res = self.app.get(
             '{}null'.format(
                 self.parent_url_ne),
