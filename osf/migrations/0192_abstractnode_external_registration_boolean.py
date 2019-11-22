7304c46fbd85035b78b9c742a8cfd0d9b21efb86
UdayVarkhedkar <uvarkhedkar@gmail.com>
2019-11-22 14:08:57 -0500

    Modifies migration names to match migrations already run on staging

--- osf/migrations/0192_abstractnode_external_registration_boolean.py
+++ osf/migrations/0192_abstractnode_external_registration_boolean.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('osf', '0190_abstractnode_external_registered_date'),
+        ('osf', '0191_abstractnode_external_registered_date'),
     ]
 
     operations = [
