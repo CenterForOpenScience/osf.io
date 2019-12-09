286070d8d28ecc2f74719b68d9725f70d47f5ebf
John Tordoff <john@cos.io>
2019-12-09 15:55:06 -0500

    fix addon snapshot for py3

--- scripts/analytics/addon_snapshot.py
+++ scripts/analytics/addon_snapshot.py
@@ -89,7 +89,7 @@ class AddonSnapshot(SnapshotAnalytics):
                 for node_settings in paginated(node_settings_model):
                     if node_settings.owner and not node_settings.owner.all_tags.filter(name='old_node_collection', system=True).exists():
                         connected_count += 1
-                deleted_count = addon.models['nodesettings'].objects.filter(deleted=True).count() if addon.models.get('nodesettings') else 0
+                deleted_count = addon.models['nodesettings'].objects.filter(deleted__isnull=False).count() if addon.models.get('nodesettings') else 0
                 if has_external_account:
                     disconnected_count = addon.models['nodesettings'].objects.filter(external_account__isnull=True, is_deleted=False).count() if addon.models.get('nodesettings') else 0
                 else:
