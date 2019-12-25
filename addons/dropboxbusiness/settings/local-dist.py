eb574d9104b28f4d095a51e7b77283122edad6ba
Takuya Ishibashi <takuya@soum.co.jp>
2019-12-25 14:57:22 +0000

    dropboxbusiness: update local-dist.py

--- addons/dropboxbusiness/settings/local-dist.py
+++ addons/dropboxbusiness/settings/local-dist.py
@@ -3,7 +3,7 @@
   local.py and change these settings.
 """
 # Get an app key and secret at https://www.dropbox.com/developers/apps
-DROPBOX_BUSINESS_FILE_KEY = 'changeme'
-DROPBOX_BUSINESS_FILE_SECRET = 'changeme'
-DROPBOX_BUSINESS_MANAGE_KEY = 'changeme'
-DROPBOX_BUSINESS_MANAGE_SECRET = 'changeme'
+DROPBOX_BUSINESS_FILEACCESS_KEY = 'changeme'
+DROPBOX_BUSINESS_FILEACCESS_SECRET = 'changeme'
+DROPBOX_BUSINESS_MANAGEMENT_KEY = 'changeme'
+DROPBOX_BUSINESS_MANAGEMENT_SECRET = 'changeme'
