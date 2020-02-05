a97852fc8ea2ec385b5f9db7885248faedbde970
corbinSanders <corbin@cos.io>
2020-02-05 13:29:32 -0500

    Fixing more mistakes. Getting there

--- api_tests/nodes/views/test_node_draft_registration_list.py
+++ api_tests/nodes/views/test_node_draft_registration_list.py
@@ -541,7 +541,7 @@ class TestDraftRegistrationCreate(DraftRegistrationTestCase):
             expect_errors=True)
         errors = res.json['errors'][0]
         assert res.status_code == 400
-        assert errors['detail'] == 'For your registration your response to the \'Has data collection begun for this project?\' field' \
+        assert errors['detail'] == 'For your registration your response to the \'Data collection status\' field' \
                                    ' is invalid, your response must be one of the provided options.'
 
     def test_registration_metadata_question_keys_must_be_value(
@@ -561,7 +561,7 @@ class TestDraftRegistrationCreate(DraftRegistrationTestCase):
             expect_errors=True)
         errors = res.json['errors'][0]
         assert res.status_code == 400
-        assert errors['detail'] == 'For your registration your response to the \'Has data collection begun for this project?\' ' \
+        assert errors['detail'] == 'For your registration your response to the \'Data collection status\' ' \
                                    'field is invalid, your response must be one of the provided options.'
 
     def test_question_in_registration_metadata_must_be_in_schema(
@@ -602,7 +602,7 @@ class TestDraftRegistrationCreate(DraftRegistrationTestCase):
             expect_errors=True)
         errors = res.json['errors'][0]
         assert res.status_code == 400
-        assert errors['detail'] == 'For your registration your response to the \'Has data collection begun for this project?\'' \
+        assert errors['detail'] == 'For your registration your response to the \'Data collection status\'' \
                                    ' field is invalid, your response must be one of the provided options.'
 
     def test_registration_responses_must_be_a_dictionary(
