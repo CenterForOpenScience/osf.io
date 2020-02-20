c28b5311d41a498271d3d1d0e5f3ee823e93c52e
John Tordoff <john@cos.io>
2020-02-20 15:51:23 -0500

    fix management command calls for upgrade to Django 2

--- osf/models/user.py
+++ osf/models/user.py
@@ -348,9 +348,6 @@ class OSFUser(DirtyFieldsMixin, GuidMixin, BaseModel, AbstractBaseUser, Permissi
     #     'academiaProfileID': <profile identifier for academia.edu>
     # }
 
-    # date the user object was created
-    created = NonNaiveDateTimeField(db_index=True, auto_now_add=True)
-
     # date the user last sent a request
     date_last_login = NonNaiveDateTimeField(null=True, blank=True)
 
