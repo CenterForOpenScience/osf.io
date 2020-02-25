c807ee47d4c2739e91ff03b1bf9f1013f1a5baaa
west1636 <nishikawa@nii.ac.jp>
2020-02-25 18:03:14 +0900

    revise oauth_scopes.py utils.py

--- website/notifications/utils.py
+++ website/notifications/utils.py
@@ -499,8 +499,8 @@ def format_user_and_project_subscriptions(user):
         {
             'node': {
                 'id': user._id,
-                'title': _('Default Notification Settings'),
-                'help': _('These are default settings for new projects you create or are added to. Modifying these settings will not modify settings on existing projects.')
+                'title': unicode(_('Default Notification Settings')),
+                'help': unicode_('These are default settings for new projects you create or are added to. Modifying these settings will not modify settings on existing projects.'))
             },
             'kind': 'heading',
             'children': format_user_subscriptions(user)
@@ -508,8 +508,9 @@ def format_user_and_project_subscriptions(user):
         {
             'node': {
                 'id': '',
-                'title': ('Project Notifications'),
-                'help': _('These are settings for each of your projects. Modifying these settings will only modify the settings for the selected project.')
+                'title': unicode(_('Project Notifications')),
+                'help': 'These are settings for each of your projects. Modifying ' +
+                        'these settings will only modify the settings for the selected project.'
             },
             'kind': 'heading',
             'children': format_data(user, get_configured_projects(user))
