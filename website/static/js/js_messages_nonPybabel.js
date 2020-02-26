8bd6d29dbfafedfd0645b1517b21a49e866bef1a
west1636 <nishikawa@nii.ac.jp>
2020-02-26 09:36:42 +0900

    delete js_messages_nonPybabel.js

--- website/static/js/js_messages_nonPybabel.js
+++
@@ -1,9 +0,0 @@
-var rdmGettext = require('js/rdmGettext');
-var gt = rdmGettext.rdmGettext();
-var _ = function(msgid) { return gt.gettext(msgid); };
-
-var newProjectCategory = [_('Analysis'),_('Communication'),_('Data'),_('Hypothesis'),_('Instrumentation'),_('Methods and Measures'),_('Procedure'),_('Project'),_('Software'),_('Other')];
-
-var NODE_SUBSCRIPTIONS_AVAILABLE = [_('Comments added'),_('Files updated')]
-
-var USER_SUBSCRIPTIONS_AVAILABLE = [_('Replies to your comments'),_('Comments added'),_('Files updated'),_('Mentions added'),_('Preprint submissions updated')]
