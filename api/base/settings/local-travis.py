f8269b2836630d1638508e92d7c6252175ba1c37
John Tordoff <john@cos.io>
2020-03-11 11:50:26 -0400

    see if this fixes travis

--- api/base/settings/local-travis.py
+++ api/base/settings/local-travis.py
@@ -15,7 +15,7 @@ REST_FRAMEWORK['DEFAULT_THROTTLE_RATES'] = {
     'test-user': '2/hour',
     'test-anon': '1/hour',
     'send-email': '2/minute',
-    'burst': '10/second',
+    'burst': '1000000/second',
 }
 
 ALLOWED_HOSTS.append('localhost')
