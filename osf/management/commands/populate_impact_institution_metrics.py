dc2eef00361bf35bead92387ad5b5ffafbe19c93
UdayVarkhedkar <uvarkhedkar@gmail.com>
2020-03-16 14:09:33 -0400

    Updates populate_impact_institution_metrics to be more developer friendly and versatile

--- osf/management/commands/populate_impact_institution_metrics.py
+++ osf/management/commands/populate_impact_institution_metrics.py
@@ -9,46 +9,105 @@ from osf.metrics import (
 
 from osf.models import Institution, OSFUser
 
+"""
+This management command can be run to populate impact with fake
+institutional metrics data.
 
-# Edit the following guids based off your local environment
-USERS = [OSFUser.load('bd53u'), OSFUser.load('hy84n'), OSFUser.load('7zyg2')]
-INSTITUTION = Institution.load('cos')
+All flags are optional with the script defaulting to 3 users and 1 institution
+from your local database with metrics for the past 7 days.
 
+--users: Specify user guids
+--num_users: Specify the number of users to use from the database (if
+user guids aren't specified)
+--institutions: Specify institution guids
+--num_institutions: Specify the number of institutions to use from the database
+(if institution guids aren't specified)
+--days: Specify the number of days to write metrics data for
 
-def populate_institution_metrics(dates):
+Example: docker-compose run --rm web python3 manage.py populate_impact_institution_metrics --days 3 --institutions cos --users hy84n bd53u
+"""
+
+def populate_institution_metrics(users, institutions, dates):
     institution_public_project_count = 25 + (int(25 * random()))
     institution_private_project_count = 25 + (int(25 * random()))
+
     for date in dates:
-        institution_public_project_count = institution_public_project_count - int(5 * random())
-        institution_private_project_count = institution_private_project_count - int(5 * random())
 
-        InstitutionProjectCounts.record_institution_project_counts(
-            institution=INSTITUTION,
-            public_project_count=institution_public_project_count,
-            private_project_count=institution_private_project_count,
-            timestamp=date
-        )
+        for institution in institutions:
+            institution_public_project_count = institution_public_project_count - int(5 * random())
+            institution_private_project_count = institution_private_project_count - int(5 * random())
 
-        for user in USERS:
-            user_public_project_count = (int(10 * random()))
-            user_private_project_count = (int(10 * random()))
-
-            UserInstitutionProjectCounts.record_user_institution_project_counts(
-                institution=INSTITUTION,
-                user=user,
-                public_project_count=user_public_project_count,
-                private_project_count=user_private_project_count,
+            InstitutionProjectCounts.record_institution_project_counts(
+                institution=institution,
+                public_project_count=institution_public_project_count,
+                private_project_count=institution_private_project_count,
                 timestamp=date
             )
+
+            for user in users:
+                user_public_project_count = (int(10 * random()))
+                user_private_project_count = (int(10 * random()))
+
+                UserInstitutionProjectCounts.record_user_institution_project_counts(
+                    institution=institution,
+                    user=user,
+                    public_project_count=user_public_project_count,
+                    private_project_count=user_private_project_count,
+                    timestamp=date
+                )
 
 
 class Command(BaseCommand):
 
     def add_arguments(self, parser):
         super(Command, self).add_arguments(parser)
+        parser.add_argument(
+            '--users',
+            nargs='*',
+            help='Specify user guids'
+        ),
+        parser.add_argument(
+            '--num_users',
+            nargs=1,
+            type=int,
+            default=3,
+            help='Specify number of users to use if not specifying users'
+        ),
+        parser.add_argument(
+            '--institutions',
+            nargs='*',
+            help='Specify insitutions guids'
+        ),
+        parser.add_argument(
+            '--num_institutions',
+            nargs=1,
+            type=int,
+            default=1,
+            help='Specify number of institutions to use if not specifying institutions'
+        ),
+        parser.add_argument(
+            '--days',
+            type=int,
+            default=7,
+            help='Specify number of past days to write metrics data for'
+        )
 
     def handle(self, *args, **options):
-        today = dt.datetime.today()
-        last_seven_days = [(today - dt.timedelta(days=num_days)) for num_days in range(0, 7)]
+        days = options.get('days')
+        num_users = options.get('num_users')
+        num_institutions = options.get('num_institutions')
 
-        populate_institution_metrics(last_seven_days)
+        if options.get('users'):
+            users = [OSFUser.load(user_id) for user_id in options.get('users')]
+        else:
+            users = OSFUser.objects.all()[:num_users]
+
+        if options.get('institutions'):
+            institutions = [Institution.load(institution_id) for institution_id in options.get('institutions')]
+        else:
+            institutions = Institution.objects.all()[:num_institutions]
+
+        today = dt.datetime.today()
+        last_x_days = [(today - dt.timedelta(days=num_days)) for num_days in range(0, days)]
+
+        populate_institution_metrics(users, institutions, last_x_days)
