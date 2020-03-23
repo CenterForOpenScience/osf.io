c14a76cfe7abd6d852edf8f7bb9b80ba24238d6b
corbinSanders <corbin@cos.io>
2020-03-23 10:50:19 -0400

    Fixing api permissions

--- api/metrics/permissions.py
+++ api/metrics/permissions.py
@@ -1,6 +1,8 @@
 from rest_framework import permissions
 from api.base.utils import get_user_auth
 
+from osf.models import Institution
+
 
 class IsPreprintMetricsUser(permissions.BasePermission):
 
@@ -14,6 +16,14 @@ class IsInstitutionalMetricsUser(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         auth = get_user_auth(request)
-        if auth.user.has_perm('view_institutional_metrics', obj.institution):
+        if type(obj) != Institution:
+            obj = obj.institution
+        if not auth.user:
+            return False
+        if auth.user.has_perm('view_institutional_metrics', obj):
             return True
         return False
+
+    def has_permission(self, request, view):
+        institution = view.get_institution()
+        return self.has_object_permission(request, view, institution)
