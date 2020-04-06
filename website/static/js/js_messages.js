d199d4c0d0cf7c333755b9ea544f76a2b6111c6f
west1636 <nishikawa@nii.ac.jp>
2020-04-06 19:04:28 +0900

    (try) #18781 revise apiApplication.js,apiPersonalToken.js po,pot etc

--- website/static/js/js_messages.js
+++ website/static/js/js_messages.js
@@ -11,3 +11,5 @@ var USER_SUBSCRIPTIONS_AVAILABLE = [_('Replies to your comments'),_('Comments ad
 var noLicense = _('No license');
 
 var beforeFork = _('Because you have not configured the {addon} add-on, your authentication will not be transferred to the forked {category}. You may authorize and configure the {addon} add-on in the new fork on the settings page.');
+
+var validationMessages = [_('This field is required.'),_('Please enter a value greater than or equal to {0}.'),_('Please enter a value less than or equal to {0}.'),_('Please enter at least {0} characters.'),_('Please enter no more than {0} characters.'),_('Please check this value.'),_('The value must increment by {0}.'),_('Please enter a proper email address.'),_('Please enter a proper date.'),_('Please enter a proper date.'),_('Please enter a number.'),_('Please enter a digit.'),_('Please specify a valid phone number.'),_('Values must equal.'),_('Please choose another value.'),_('Please make sure the value is unique.')];
