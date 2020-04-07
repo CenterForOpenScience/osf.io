4b718c327c5705fb02470dbd2fb95746c6e852b9
west1636 <nishikawa@nii.ac.jp>
2020-04-07 10:22:09 +0900

    change ja notation provisionally コントリビュータ→メンバー #18780 revise po ,pot etc

--- website/static/js/js_messages.js
+++ website/static/js/js_messages.js
@@ -4,6 +4,8 @@ var editBtn = _('Edit');
 
 var newProjectCategory = [_('Analysis'),_('Communication'),_('Data'),_('Hypothesis'),_('Instrumentation'),_('Methods and Measures'),_('Procedure'),_('Project'),_('Software'),_('Other')];
 
+var newProjectCategoryLowCase = [_('analysis'),_('communication'),_('data'),_('hypothesis'),_('instrumentation'),_('methods and measures'),_('procedure'),_('project'),_('software'),_('other')];
+
 var NODE_SUBSCRIPTIONS_AVAILABLE = [_('Comments added'),_('Files updated')];
 
 var USER_SUBSCRIPTIONS_AVAILABLE = [_('Replies to your comments'),_('Comments added'),_('Files updated'),_('Mentions added'),_('Preprint submissions updated')];
