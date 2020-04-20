3c4071ab59d1b305117d5b6efb3c0cdb2fd3def8
UdayVarkhedkar <uvarkhedkar@gmail.com>
2020-04-20 14:41:18 -0400

    Adds test for sync_registration_creator_bibliographic_status management command

--- osf_tests/management_commands/test_sync_registration_creator_bibliographic_status.py
+++ osf_tests/management_commands/test_sync_registration_creator_bibliographic_status.py
@@ -36,8 +36,8 @@ def registration(project, user):
     return registration
 
 @pytest.fixture()
-def registration_creator_contrib(project_contributor, registration):
-    contributor = registration.contributor_set.get(user=project_contributor.user)
+def registration_creator_contrib(user, registration):
+    contributor = registration.contributor_set.get(user=user)
     contributor.visible = True
     contributor.save()
     return contributor
@@ -46,7 +46,7 @@ def registration_creator_contrib(project_contributor, registration):
 @pytest.mark.django_db
 class TestRegistrationCreatorBibliographicStatusSync:
 
-    def test_sync_registration_creator_bibliographic_status(self, user, project, registration, project_contributor, registration_creator_contrib):
+    def test_sync_different_registration_creator_bibliographic_status(self, user, project, registration, project_contributor, registration_creator_contrib):
         # Assert out-of-sync bibliographic status
         assert project_contributor.visible is False
         assert registration_creator_contrib.visible is True
@@ -55,4 +55,18 @@ class TestRegistrationCreatorBibliographicStatusSync:
         sync_registration_creator_bibliographic_status(registration_guid)
         updated_registration = Registration.load(registration_guid)
         updated_registration_creator_contrib = updated_registration.contributor_set.get(user=registration.creator)
+        assert updated_registration_creator_contrib.visible is False
+        assert updated_registration_creator_contrib.visible == project_contributor.visible
+
+    def test_sync_same_registration_creator_bibliographic_status(self, user, project, registration, registration_creator_contrib):
+        # Assert aligned bibliographic status
+        project_contributor = registration.registered_from.contributor_set.get(user=user)
+        assert project_contributor.visible is True
+        assert registration_creator_contrib.visible is True
+
+        registration_guid = registration._id
+        sync_registration_creator_bibliographic_status(registration_guid)
+        updated_registration = Registration.load(registration_guid)
+        updated_registration_creator_contrib = updated_registration.contributor_set.get(user=registration.creator)
+        assert updated_registration_creator_contrib.visible is True
         assert updated_registration_creator_contrib.visible == project_contributor.visible
