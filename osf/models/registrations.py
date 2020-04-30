d3c036fcc9edf9f468a08d2415959d4807d0c365
corbinSanders <corbin@cos.io>
2020-04-30 10:45:37 -0400

    Fixing how contributors are copied at registration time

--- osf/models/registrations.py
+++ osf/models/registrations.py
@@ -911,7 +911,7 @@ class DraftRegistration(ObjectIDMixin, RegistrationResponseMixin, DirtyFieldsMix
             provider=provider,
         )
         draft.save()
-        draft.copy_editable_fields(node, Auth(user), save=True)
+        draft.copy_editable_fields(node, Auth(user), save=True, contributors=False)
         draft.update(data)
         return draft
 
@@ -1010,8 +1010,7 @@ class DraftRegistration(ObjectIDMixin, RegistrationResponseMixin, DirtyFieldsMix
         self.registered_node = register
         self.add_status_log(auth.user, DraftRegistrationLog.REGISTERED)
 
-        if isinstance(node, Node):
-            self.copy_contributors_from(node)
+        self.copy_contributors_from(node)
 
         if save:
             self.save()
