2196dadd73d94c27a82407f5d35c9573d61e56f8
west1636 <nishikawa@nii.ac.jp>
2020-05-08 13:18:42 +0900

    (try3)(admin)add translation by custom filter(move templatetags folder)

--- admin/base/templatetags/filters.py
+++ admin/base/templatetags/filters.py
@@ -2,9 +2,14 @@
 from django import template
 from django.utils.safestring import mark_safe
 import json
+from django.utils.translation import gettext_lazy as _
 
 register = template.Library()
 
 @register.filter
 def jsonify(o):
     return mark_safe(json.dumps(o))
+
+@register.filter
+def transValue(value1):
+    return _(value1)
