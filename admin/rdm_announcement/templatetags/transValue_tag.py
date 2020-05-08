8e760c5afaa0cb65802f5eadd28b622428a07a97
west1636 <nishikawa@nii.ac.jp>
2020-05-08 11:38:13 +0900

    (try2)(admin)add translation by custom filter(move templatetags folder)

--- admin/rdm_announcement/templatetags/transValue_tag.py
+++
@@ -1,8 +0,0 @@
-from django import template
-from django.utils.translation import gettext_lazy as _
-
-register = template.Library()
-
-@register.filter
-def transValue(value1):
-    return _(value1)
