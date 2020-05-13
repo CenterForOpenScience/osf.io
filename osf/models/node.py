4df457bd1b23191d2692fc225d8340330e0d0214
John Tordoff <john@cos.io>
2020-05-13 11:37:05 -0400

    add refactor Django code for backward compatible syntax changes

--- osf/models/node.py
+++ osf/models/node.py
@@ -15,7 +15,7 @@ from django_bulk_update.helper import bulk_update
 from django.contrib.auth.models import AnonymousUser, Permission
 from django.contrib.contenttypes.fields import GenericRelation
 from django.core.paginator import Paginator
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.db import models, connection
 from django.db.models.signals import post_save
 from django.dispatch import receiver
