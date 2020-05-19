9d6079751bbd96d98eede4fcd9578cf7f1269b46
UdayVarkhedkar <uvarkhedkar@gmail.com>
2020-05-19 12:51:46 -0400

    Adds docstrings for new renderers for csv exports

--- api/institutions/renderers.py
+++ api/institutions/renderers.py
@@ -2,6 +2,9 @@ from rest_framework_csv.renderers import CSVRenderer
 
 
 class MetricsCSVRenderer(CSVRenderer):
+    """
+    CSVRenderer with updated render method to export `data` dictionary of API Response to CSV
+    """
 
     def render(self, data, media_type=None, renderer_context={}, writer_opts=None):
         """
@@ -13,6 +16,10 @@ class MetricsCSVRenderer(CSVRenderer):
         return super().render(data, media_type=media_type, renderer_context=renderer_context, writer_opts=writer_opts)
 
 class InstitutionUserMetricsCSVRenderer(MetricsCSVRenderer):
+    """
+    MetricsCSVRenderer with headers and labels specific to the InstitutionUserMetrics Endpoint
+    """
+
     header = ['id', 'attributes.user_name', 'attributes.public_projects', 'attributes.private_projects', 'type']
     labels = {
         'attributes.private_projects': 'private_projects',
@@ -21,6 +28,10 @@ class InstitutionUserMetricsCSVRenderer(MetricsCSVRenderer):
     }
 
 class InstitutionDepartmentMetricsCSVRenderer(MetricsCSVRenderer):
+    """
+    MetricsCSVRenderer with headers and labels specific to the InstitutionDepartmentMetrics Endpoint
+    """
+
     header = ['id', 'attributes.name', 'attributes.number_of_users', 'type']
     labels = {
         'attributes.name': 'name',
