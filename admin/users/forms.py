c4048ea62df3a27e835b87ea28ec9cef6f73f682
west1636 <nishikawa@nii.ac.jp>
2020-06-11 15:39:28 +0900

    (general admin) add translations

--- admin/users/forms.py
+++ admin/users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-
+from django.utils.translation import ugettext_lazy as _
 
 class EmailResetForm(forms.Form):
     emails = forms.ChoiceField(label='Email')
@@ -18,8 +18,8 @@ class WorkshopForm(forms.Form):
 
 class UserSearchForm(forms.Form):
     guid = forms.CharField(label='guid', min_length=5, max_length=5, required=False)  # TODO: Move max to 6 when needed
-    name = forms.CharField(label='name', required=False)
-    email = forms.EmailField(label='email', required=False)
+    name = forms.CharField(label=_('name'), required=False)
+    email = forms.EmailField(label=_('email'), required=False)
 
 class MergeUserForm(forms.Form):
     user_guid_to_be_merged = forms.CharField(label='user_guid_to_be_merged', min_length=5, max_length=5, required=True)  # TODO: Move max to 6 when needed
