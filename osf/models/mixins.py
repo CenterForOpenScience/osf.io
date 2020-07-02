21086076958e7d38449341132839f36856958391
corbinSanders <corbin@cos.io>
2020-07-02 11:41:52 -0400

    Spam should never be checked on admin app based requests

--- osf/models/mixins.py
+++ osf/models/mixins.py
@@ -2041,6 +2041,8 @@ class SpamOverrideMixin(SpamMixin):
             return False
         if user.spam_status == SpamStatus.HAM:
             return False
+        if request_headers['Host'].startswith('admin') or ':8001' in request_headers['Host']:
+            return False
 
         content = self._get_spam_content(saved_fields)
         if not content:
