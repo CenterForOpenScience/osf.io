69a56cf0a7f018adf13130f28867d1a78628cd2d
corbinSanders <corbin@cos.io>
2020-07-06 13:24:24 -0400

    Adding limit to sql query

--- osf/management/commands/populate_branched_from_node.py
+++ osf/management/commands/populate_branched_from_node.py
@@ -7,18 +7,24 @@ from django.db import connection, transaction
 
 logger = logging.getLogger(__name__)
 
-POPULATE_BRANCHED_FROM_NODE = """UPDATE osf_abstractnode a
+POPULATE_BRANCHED_FROM_NODE = """WITH cte AS (
+    SELECT id
+    FROM osf_abstractnode
+    WHERE type = 'osf.registration' AND
+    branched_from_node IS null
+    LIMIT %s
+)
+UPDATE osf_abstractnode a
  SET branched_from_node = CASE WHEN
     EXISTS(SELECT id FROM osf_nodelog WHERE action='project_created_from_draft_reg' AND node_id = a.id) THEN False
     ELSE True
 END
-WHERE
-a.type = 'osf.registration' AND
-a.branched_from_node IS null
+FROM cte
+WHERE cte.id = a.id
 """
 
 @celery_app.task(name='management.commands.populate_branched_from')
-def populate_branched_from(page_size, dry_run):
+def populate_branched_from(page_size=10000, dry_run=False):
     with transaction.atomic():
         with connection.cursor() as cursor:
             cursor.execute(POPULATE_BRANCHED_FROM_NODE, [page_size])
