72cc46c511d25623beba2b651c128265e3561f48
John Tordoff <Johnetordoff@users.noreply.github.com>
2020-07-23 14:38:38 -0400

    [ENG-1960] Add provider field to registration schemas (#9436)
    ## Purpose
    
    Allows schemas to be associated with a registration provider. Allow admin app to add/remove registration schemas with registration providers.
    
    ## Changes
    
    - new view RegistrationProviderSchemaList
    - new tests for that view
    - new migrations
    - created html page with view to control registration schemas
    - adds tests
    - added 'change schema' page
    - added 'change schema' button
    
    ## QA Notes
    
      - Does this change require a data migration? If so, what data will we migrate?
    Small migration
      - What is the level of risk?
    Low risk 
        - Any permissions code touched?
    No
        - Is this an additive or subtractive change, other?
    Additive
      - How can QA verify? (Through UI, API, AdminApp or AdminAdminApp?)
    No QA element yet
        - If verifying through API, what's the new version? Please include the endpoints in PR notes or Dev docs.
    Endpoint is `v2/providers/registrations/<provider_id>/schemas/`
      - What features or workflows might this change impact?
    No workflows effected.
      - How will this impact performance?
    No impact
    
    ## Documentation
    
    ⚠️ Check to update api dev docs when finished.
    
    ## Side Effects
    
    None that I know of.
    
    
    ## Ticket
    
    https://openscience.atlassian.net/browse/ENG-1960
    https://openscience.atlassian.net/browse/ENG-1973

--- admin_tests/registration_providers/test_views.py
+++ admin_tests/registration_providers/test_views.py
@@ -6,7 +6,7 @@ from osf_tests.factories import (
     AuthUserFactory,
     RegistrationProviderFactory,
 )
-from osf.models import RegistrationProvider
+from osf.models import RegistrationProvider, RegistrationSchema
 from admin_tests.utilities import setup_view, setup_form_view
 from admin.registration_providers import views
 from admin.registration_providers.forms import RegistrationProviderForm
@@ -105,3 +105,49 @@ class TestDeleteRegistrationProvider(DeleteProviderMixinBase):
         view = setup_view(view, req)
         view.kwargs = {'registration_provider_id': provider.id}
         return view
+
+
+@pytest.mark.urls('admin.base.urls')
+class TestChangeSchemas:
+
+    @pytest.fixture()
+    def user(self):
+        return AuthUserFactory()
+
+    @pytest.fixture()
+    def req(self, user):
+        req = RequestFactory().get('/fake_path')
+        req.user = user
+        return req
+
+    @pytest.fixture()
+    def provider(self):
+        return RegistrationProviderFactory()
+
+    @pytest.fixture()
+    def schema(self):
+        schema = RegistrationSchema(name='foo', schema={'foo': 42}, schema_version=1)
+        schema.save()
+        return schema
+
+    @pytest.fixture()
+    def view(self, req, provider):
+        view = views.ChangeSchema()
+        view = setup_view(view, req)
+        view.kwargs = {'registration_provider_id': provider.id}
+        return view
+
+    def test_get(self, view, req):
+        res = view.get(req)
+        assert res.status_code == 200
+
+    def test_post(self, view, req, schema, provider):
+        schema_id = schema.id
+        req.POST = {
+            'csrfmiddlewaretoken': 'fake csfr',
+            str(schema_id): ['on']
+        }
+
+        res = view.post(req)
+        assert res.status_code == 302
+        assert provider.schemas.get(id=schema_id)
