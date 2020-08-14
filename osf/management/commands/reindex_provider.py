d3d435446c519ddb3a6ccc1f293086fe4ddc0fa5
John Tordoff <john@cos.io>
2020-08-14 14:00:09 -0400

    modify preprint provider re-index command to handle all providers

--- osf/management/commands/reindex_provider.py
+++ osf/management/commands/reindex_provider.py
@@ -1,16 +1,22 @@
-"""Resend all preprints for given providers to SHARE."""
+"""Resend all resources (nodes, registrations, preprints) for given providers to SHARE."""
 import logging
 
 from django.core.management.base import BaseCommand
-from osf.models import PreprintProvider
+from osf.models import AbstractProvider, AbstractNode, Preprint
 from api.share.utils import update_share
 
 logger = logging.getLogger(__name__)
 
+
 def reindex_provider(provider):
     logger.info('Sending {} preprints to SHARE...'.format(provider.preprints.count()))
-    for preprint in provider.preprints.all():
+    for preprint in Preprint.objects.filter(provider=provider):
         update_share(preprint)
+
+    logger.info('Sending {} AbstractNodes to SHARE...'.format(AbstractNode.objects.filter(provider=provider).count()))
+    for abstract_node in AbstractNode.objects.filter(provider=provider):
+        update_share(abstract_node)
+
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
@@ -19,6 +25,6 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         provider_ids = options.get('providers', [])
-        for provider in PreprintProvider.objects.filter(_id__in=provider_ids):
+        for provider in AbstractProvider.objects.filter(_id__in=provider_ids):
             logger.info('Reindexing {}...'.format(provider._id))
             reindex_provider(provider)
