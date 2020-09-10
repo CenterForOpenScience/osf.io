ed38a298aa1e83d7e23675132da4e2ff49e44423
John Tordoff <Johnetordoff@users.noreply.github.com>
2020-09-10 09:41:19 -0400

    [ENG-2121] Add node storage limits to model (#9474)
    ## Purpose
    
    Creates an indicator property that shows when a node is over or at it's storage limit.
    
    ## Changes
    
    - adds field for custom  storage limits
    - add StorageUsage enum with methods for determining status
    - adds tests
    
    ## QA Notes
    
      - Does this change require a data migration? If so, what data will we migrate?
    small auto-migration
      - What is the level of risk?
    Low low
        - Any permissions code touched?
    No
        - Is this an additive or subtractive change, other?
    addtive
      - How can QA verify? (Through UI, API, AdminApp or AdminAdminApp?)
    Dev test
      - What features or workflows might this change impact?
    Not
      - How will this impact performance?
    Not
    
    ## Documentation
    
    No new docs
    
    ## Side Effects
    
    None that I know of.
    
    ## Ticket
    
    https://openscience.atlassian.net/browse/ENG-2121

--- osf/models/node.py
+++ osf/models/node.py
@@ -367,6 +367,9 @@ class AbstractNode(DirtyFieldsMixin, TypedModel, AddonModelMixin, IdentifierMixi
                                         validators=[validate_doi],
                                         null=True, blank=True)
 
+    custom_storage_usage_limit_public = models.DecimalField(decimal_places=9, max_digits=100, null=True, blank=True)
+    custom_storage_usage_limit_private = models.DecimalField(decimal_places=9, max_digits=100, null=True, blank=True)
+
     class Meta:
         base_manager_name = 'objects'
         index_together = (('is_public', 'is_deleted', 'type'))
@@ -390,6 +393,17 @@ class AbstractNode(DirtyFieldsMixin, TypedModel, AddonModelMixin, IdentifierMixi
             if parent:
                 return parent
         return None
+
+    @property
+    def storage_limit_status(self):
+        """ This should indicate if a node is at or over a certain storage threshold indicating a status. If nodes have
+        a custom limit this should indicate that."""
+
+        return settings.StorageLimits.from_node_usage(
+            self.storage_usage,
+            self.custom_storage_usage_limit_private,
+            self.custom_storage_usage_limit_public
+        )
 
     @property
     def nodes(self):
