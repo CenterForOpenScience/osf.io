d258ed63330cc17cb5ae39dcea825bf6455112c2
yacchin1205 <968739+yacchin1205@users.noreply.github.com>
2020-09-14 22:32:36 +0900

    Fix flake8

--- addons/nextcloud/settings/__init__.py
+++ addons/nextcloud/settings/__init__.py
@@ -4,5 +4,5 @@ from addons.nextcloud.settings.defaults import *  # noqa
 logger = logging.getLogger(__name__)
 try:
     from addons.nextcloud.settings.local import *  # noqa
-except ImportError as error:
+except ImportError:
     logger.warn('No local.py settings file found')
