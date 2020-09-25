1b59793641173c7be561dccef4307ce14b43d956
UdayVarkhedkar <uvarkhedkar@gmail.com>
2020-09-25 14:58:58 -0400

    Enforces storage limits on public to private project conversion

---
+++ api_tests/nodes/views/test_node_storage.py
@@ -0,0 +1,103 @@
+import pytest
+
+from api.base.settings.defaults import API_BASE
+from api.caching import settings as cache_settings
+from api.caching.utils import storage_usage_cache
+from osf_tests.factories import (
+    ProjectFactory,
+    AuthUserFactory
+)
+from osf.utils.permissions import READ, WRITE
+from website import settings
+from website.project.utils import sizeof_fmt
+
+
+@pytest.mark.django_db
+@pytest.mark.enable_quickfiles_creation
+class TestNodeStorage:
+    @pytest.fixture()
+    def admin_contributor(self):
+        return AuthUserFactory()
+
+    @pytest.fixture()
+    def write_contributor(self):
+        return AuthUserFactory()
+
+    @pytest.fixture()
+    def read_contributor(self):
+        return AuthUserFactory()
+
+    @pytest.fixture()
+    def non_contributor(self):
+        return AuthUserFactory()
+
+    @pytest.fixture()
+    def project(self, admin_contributor, write_contributor, read_contributor):
+        project = ProjectFactory(
+            creator=admin_contributor
+        )
+        project.add_contributor(write_contributor, WRITE, visible=False)
+        project.add_contributor(read_contributor, READ)
+        project.save()
+        return project
+
+    @pytest.fixture()
+    def url(self, project):
+        return '/{}nodes/{}/storage/'.format(API_BASE, project._id)
+
+    @pytest.fixture()
+    def embed_url(self, project):
+        return '/{}nodes/{}/?embed=storage'.format(API_BASE, project._id)
+
+    def test_node_storage(self, app, url, embed_url, project, admin_contributor,
+            write_contributor, read_contributor, non_contributor):
+
+        # Test GET unauthenticated
+        res = app.get(url, expect_errors=True)
+        assert res.status_code == 401
+
+        # Test GET non_contributor
+        res = app.get(url, auth=non_contributor.auth, expect_errors=True)
+        assert res.status_code == 403
+
+        # Test GET read contrib
+        res = app.get(url, auth=read_contributor.auth, expect_errors=True)
+        assert res.status_code == 403
+
+        # Test GET write contrib
+        # Initial request results in a 202 response, intitiating calculation of storage usage
+        res = app.get(url, auth=write_contributor.auth, expect_errors=True)
+        assert res.status_code == 202
+        data = res.json['data']
+        assert data['attributes']['storage_limit_status'] is None
+        assert data['attributes']['storage_usage'] is None
+
+        # Test POST not allowed
+        res = app.post_json_api(url, auth=write_contributor.auth, expect_errors=True)
+        assert res.status_code == 405
+
+        # Tests Node Storage fo Nodes without Storage Usage
+        res = app.get(url, auth=write_contributor.auth, expect_errors=True)
+        assert res.status_code == 200
+        data = res.json['data']
+        assert data['attributes']['storage_limit_status'] == 'DEFAULT'
+        assert data['attributes']['storage_usage'] == '0.0B'
+
+        key = cache_settings.STORAGE_USAGE_KEY.format(target_id=project._id)
+        storage_usage = (settings.STORAGE_LIMIT_PRIVATE + 1) * settings.GBs
+        formatted_storage_usage = sizeof_fmt(storage_usage)
+        storage_usage_cache.set(key, storage_usage, settings.STORAGE_USAGE_CACHE_TIMEOUT)
+
+        # Tests Node Storage for Nodes with Storage Usage
+        res = app.get(url, auth=admin_contributor.auth)
+        assert res.status_code == 200
+        data = res.json['data']
+        assert data['attributes']['storage_limit_status'] == 'OVER_PRIVATE'
+        assert data['attributes']['storage_usage'] == formatted_storage_usage
+
+        # Tests Node Storage Embed
+        res = app.get(embed_url, auth=admin_contributor.auth)
+        assert res.status_code == 200
+        data = res.json['data']
+        assert data['embeds']['storage']['data']['attributes']['storage_limit_status'] == 'OVER_PRIVATE'
+        assert data['embeds']['storage']['data']['attributes']['storage_usage'] == formatted_storage_usage
