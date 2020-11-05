dc72d77d1ec08b3214db683fe4051c1f98641ecb
west1636 <nishikawa@nii.ac.jp>
2020-11-05 09:17:21 +0900

    Revert "Revert "Revert "(test)動作確認用5"""
    This reverts commit 4b1d75ddb66421293819de920614b559c7d94af4.

--- website/settings/defaults.py
+++ website/settings/defaults.py
@@ -2003,8 +2003,7 @@ BABEL_DOMAIN = 'messages'
 BABEL_LANGUAGES = {
     'en': 'English',
     'ja': '日本語',
-    'ja_jp': '日本語',
-    'fr': 'francais'
+    'ja_jp': '日本語'
 }
 BABEL_DEFAULT_LOCALE = 'ja'
 
