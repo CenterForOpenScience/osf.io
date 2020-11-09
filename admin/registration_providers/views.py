f8a8d52e420b09c21dde29c0d148fedc7446edbb
corbinSanders <corbin@cos.io>
2020-11-09 12:43:43 -0500

    Adding ability to add and remove moderators cleanly.

--- admin/registration_providers/views.py
+++ admin/registration_providers/views.py
@@ -412,7 +412,7 @@ class ChangeSchema(TemplateView):
         return redirect('registration_providers:detail', registration_provider_id=registration_provider.id)
 
 
-class AddModerators(TemplateView):
+class AddAdminOrModerator(TemplateView):
     permission_required = 'osf.change_registrationprovider'
     template_name = 'registration_providers/edit_moderators.html'
 
@@ -423,6 +423,7 @@ class AddModerators(TemplateView):
         context = super().get_context_data(**kwargs)
         context['registration_provider'] = registration_provider
         context['moderators'] = registration_provider.get_group('moderator').user_set.all()
+        context['admins'] = registration_provider.get_group('admin').user_set.all()
         return context
 
     def post(self, request, *args, **kwargs):
@@ -430,19 +431,24 @@ class AddModerators(TemplateView):
         data = dict(request.POST)
         del data['csrfmiddlewaretoken']  # just to remove the key from the form dict
 
-        moderator = OSFUser.load(data['add-moderators-form'][0])
-        if moderator is None:
+        target_user = OSFUser.load(data['add-moderators-form'][0])
+        if target_user is None:
             messages.error(request, f'User for guid: {data["add-moderators-form"][0]} could not be found')
-            return redirect('registration_providers:add_moderators', registration_provider_id=registration_provider.id)
+            return redirect('registration_providers:add_admin_or_moderator', registration_provider_id=registration_provider.id)
 
-        registration_provider.add_to_group(moderator, 'moderator')
+        if 'admin' in data:
+            registration_provider.add_to_group(target_user, 'admin')
+            target_type = 'admin'
+        else:
+            registration_provider.add_to_group(target_user, 'moderator')
+            target_type = 'moderator'
 
-        messages.success(request, f'The following moderator was successfully added: {moderator.fullname} ({moderator.username})')
+        messages.success(request, f'The following {target_type} was successfully added: {target_user.fullname} ({target_user.username})')
 
-        return redirect('registration_providers:add_moderators', registration_provider_id=registration_provider.id)
+        return redirect('registration_providers:add_admin_or_moderator', registration_provider_id=registration_provider.id)
 
 
-class RemoveModerators(TemplateView):
+class RemoveAdminsAndModerators(TemplateView):
     permission_required = 'osf.change_registrationprovider'
     template_name = 'registration_providers/edit_moderators.html'
 
@@ -453,6 +459,7 @@ class RemoveModerators(TemplateView):
         context = super().get_context_data(**kwargs)
         context['registration_provider'] = registration_provider
         context['moderators'] = registration_provider.get_group('moderator').user_set.all()
+        context['admins'] = registration_provider.get_group('admin').user_set.all()
         return context
 
     def post(self, request, *args, **kwargs):
@@ -460,10 +467,20 @@ class RemoveModerators(TemplateView):
         data = dict(request.POST)
         del data['csrfmiddlewaretoken']  # just to remove the key from the form dict
 
-        moderators = OSFUser.objects.filter(id__in=list(data.keys()))
+        to_be_removed = list(data.keys())
+        removed_admins = [admin.replace('Admin-', '') for admin in to_be_removed if 'Admin-' in admin]
+        removed_moderators = [moderator.replace('Moderator-', '') for moderator in to_be_removed if 'Moderator-' in moderator]
+        moderators = OSFUser.objects.filter(id__in=removed_moderators)
+        admins = OSFUser.objects.filter(id__in=removed_admins)
         registration_provider.get_group('moderator').user_set.remove(*moderators)
+        registration_provider.get_group('admin').user_set.remove(*admins)
 
-        moderator_names = ' ,'.join(moderators.values_list('fullname', flat=True))
-        messages.success(request, f'The following moderators were successfully removed: {moderator_names}')
+        if moderators:
+            moderator_names = ' ,'.join(moderators.values_list('fullname', flat=True))
+            messages.success(request, f'The following moderators were successfully removed: {moderator_names}')
 
-        return redirect('registration_providers:remove_moderators', registration_provider_id=registration_provider.id)
+        if admins:
+            admin_names = ' ,'.join(admins.values_list('fullname', flat=True))
+            messages.success(request, f'The following admins were successfully removed: {admin_names}')
+
+        return redirect('registration_providers:remove_admins_and_moderators', registration_provider_id=registration_provider.id)
