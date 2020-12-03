6a5fe1cb64ddb6de733d9808f08aec624ecaba65
Jon Walz <jonwalz@Admins-MBP.lan>
2020-12-03 16:16:48 -0500

    Update notify_base to support custom branding; add custom branding to context for registries moderation

--- osf/utils/notifications.py
+++ osf/utils/notifications.py
@@ -9,7 +9,7 @@ def get_email_template_context(resource):
     url_segment = 'preprints' if is_preprint else 'registries'
     document_type = resource.provider.preprint_word if is_preprint else 'registration'
 
-    return {
+    base_context = {
         'domain': DOMAIN,
         'reviewable': resource,
         'workflow': resource.provider.reviews_workflow,
@@ -19,6 +19,12 @@ def get_email_template_context(resource):
         'document_type': document_type
     }
 
+    if document_type == 'registration' and resource.provider.brand:
+        brand = resource.provider.brand
+        base_context['logo_url']: brand.hero_logo_image
+        base_context['top_bar_color']: brand.primary_color
+
+    return base_context
 
 def notify_submit(resource, user, *args, **kwargs):
     context = get_email_template_context(resource)
--- website/notifications/tasks.py
+++ website/notifications/tasks.py
@@ -66,16 +66,22 @@ def _send_reviews_moderator_emails(send_type):
         info = group['info']
         notification_ids = [message['_id'] for message in info]
         provider = AbstractProvider.objects.get(id=group['provider_id'])
+        additional_context = dict()
         if isinstance(provider, RegistrationProvider):
             provider_type = 'registration'
             submissions_url = f'{settings.DOMAIN}registries/{provider._id}/moderation/submissions',
             withdrawals_url = f'{submissions_url}?state=pending_withdraw'
+            if provider.brand:
+                additional_context = {
+                    'logo_url': provider.brand.hero_logo_image,
+                    'top_bar_color': provider.brand.primary_color
+                }
         else:
             provider_type = 'preprint'
             submissions_url = f'{settings.DOMAIN}reviews/preprints/{provider._id}',
             withdrawals_url = ''
-        if not user.is_disabled:
 
+        if not user.is_disabled:
             mails.send_mail(
                 to_addr=user.username,
                 mimetype='html',
@@ -88,7 +94,8 @@ def _send_reviews_moderator_emails(send_type):
                 reviews_withdrawal_url=withdrawals_url,
                 is_reviews_moderator_notification=True,
                 is_admin=provider.get_group(ADMIN).user_set.filter(id=user.id).exists(),
-                provider_type=provider_type
+                provider_type=provider_type,
+                **additional_context
             )
         remove_notifications(email_notification_ids=notification_ids)
 
