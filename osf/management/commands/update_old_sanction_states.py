7c7d1c61d281dce7bcd5697485d4c6fa6c690495
Jon Walz <jonwalz@Admins-MBP.lan>
2020-12-07 16:03:14 -0500

    Management command to fix old sanctions

---
+++ osf/management/commands/update_old_sanction_states.py
@@ -0,0 +1,25 @@
+from django.core.management.base import BaseCommand
+
+from osf.models import Embargo, Retraction
+
+
+def update_old_sanction_states():
+    '''Fix out-of-date states for Embargo and Retraction objects.
+
+    https://github.com/CenterForOpenScience/osf.io/pull/3919 homogenized a lot
+    of the code behind the Embargo and Retraction objecs as part of introducing
+    the RegistrationApproval. It abruptly changed the acceptable states for
+    these models without updating existing values for them. This command
+    will bring them up-to-date.
+    '''
+
+    Embargo.objects.filter(state_iexact='active').update(state=Embargo.APPROVED)
+    Embargo.objects.filter(state_iexact='cancelled').update(state=Embargo.REJECTED)
+    Retraction.objects.filter(state_iexact='retracted').update(state=Retraction.APPROVED)
+    Retraction.objects.filter(state_iexact='cancelled').update(state=Retraction.REJECTED)
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        update_old_sanction_states()
