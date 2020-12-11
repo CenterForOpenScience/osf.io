33d3aefdd02a685c6dd900dc12a9a70564f6f03a
corbinSanders <corbin@cos.io>
2020-12-11 14:41:44 -0500

    Specifying size for text inputs

--- admin/brands/forms.py
+++ admin/brands/forms.py
@@ -10,7 +10,7 @@ class BrandForm(forms.ModelForm):
         widgets = {
             'primary_color': TextInput(attrs={'class': 'colorpicker'}),
             'secondary_color': TextInput(attrs={'class': 'colorpicker'}),
-            'hero_logo_image': TextInput(attrs={'placeholder': 'Logo image should be max height of 100px'}),
-            'topnav_logo_image': TextInput(attrs={'placeholder': 'Logo should be max height of 40px'}),
-            'hero_background_image': TextInput(attrs={'placeholder': 'Background image should be max height of 300px'}),
+            'hero_logo_image': TextInput(attrs={'placeholder': 'Logo image should be max height of 100px', 'size': 200}),
+            'topnav_logo_image': TextInput(attrs={'placeholder': 'Logo should be max height of 40px', 'size': 200}),
+            'hero_background_image': TextInput(attrs={'placeholder': 'Background image should be max height of 300px', 'size': 200}),
         }
