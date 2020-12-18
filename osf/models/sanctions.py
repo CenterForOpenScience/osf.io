ce28c08606037393b6081fcdf82bd5c83ae4d0fe
Jon Walz <jonwalz@Admins-MBP.lan>
2020-12-18 14:06:39 -0500

    Expose embargo_end_date before embargo is approved

--- osf/models/sanctions.py
+++ osf/models/sanctions.py
@@ -465,9 +465,7 @@ class Embargo(SanctionCallbackMixin, EmailApprovableSanction):
 
     @property
     def embargo_end_date(self):
-        if self.state == self.APPROVED:
-            return self.end_date
-        return False
+        return self.end_date
 
     # NOTE(hrybacki): Old, private registrations are grandfathered and do not
     # require to be made public or embargoed. This field differentiates them
