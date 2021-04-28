a2406382c16d0970e072f701361b1caf01742550
Jon Walz <jonwalz@Admins-MBP.lan>
2021-04-28 16:29:03 -0400

    make everything work

--- osf/management/commands/backfill_egap_provider_metadata.py
+++ osf/management/commands/backfill_egap_provider_metadata.py
@@ -5,7 +5,8 @@ import logging
 import pytz
 
 from django.core.management.base import BaseCommand
-from osf.models import RegistrationProvider
+from django.db.models import Max
+from osf.models import Registration
 
 logger = logging.getLogger(__name__)
 
@@ -14,12 +15,12 @@ logger = logging.getLogger(__name__)
 # https://github.com/CenterForOpenScience/osf.io/blob/50467ce8f156cea162666df6587614a7e95d4859/scripts/EGAP/egap-registration-3.json
 EGAP_ID_KEY = 'q3'
 EGAP_PUBLICATION_DATE_KEY = 'q4'
-LAST_SUPPORTED_VERSION = 3
+LAST_RELEVANT_VERSION = 3
 
 WITH_OFFSET_DATE_FORMAT = '%Y-%m-%d %H:%M:%S %z'
 NO_OFFSET_DATE_FORMAT = '%m/%d/%Y - %H:%M'
-# ALL 'Timestamp of Original Registration' values should match one of the above
-# formats, but I spotted this one at least once in the wild.
+# ALL "Timestamp of Original Registration" values *should* match one of
+# the above formats, but I spotted this one at least once in the wild.
 HYBRID_DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
 
 def _get_date_registered(timestamp_string):
@@ -41,18 +42,26 @@ def _get_date_registered(timestamp_string):
         )
 
     # Mimic behavior of import_EGAP script for consistency
-    if registered_date.tz_info is None:
-        registered_date.replace(tzinfo=pytz.UTC)
+    if not registered_date.tzinfo:
+        registered_date = registered_date.replace(tzinfo=pytz.UTC)
     return registered_date
 
-def main(dry_run, batch_size=-1):
-    egap_registrations = RegistrationProvider.objects.get(_id='egap').registrations.filter(
-        additional_metadata__is_null=True,
-        registered_schema__schema_version__lte=LAST_SUPPORTED_VERSION
-    )[:batch_size]
+def backfill_egap_metadata(dry_run=False, batch_size=None):
+    egap_registrations = Registration.objects.annotate(
+        # "Max" is a stupid way to extract the schema_version because
+        # registered_schema is implemented as ManyToMany instead of FK
+        schema_version=Max('registered_schema__schema_version')
+    ).filter(
+        provider___id='egap',
+        additional_metadata=None,  # JSON fields are weird
+        schema_version__lte=LAST_RELEVANT_VERSION
+    )
+    if batch_size:
+        egap_registrations = egap_registrations[:batch_size]
+    count = egap_registrations.count()
 
     logger.info(
-        f'Backfilling EGAP ID and registered_date for {egap_registrations.count()} registrations'
+        f'Backfilling EGAP ID and registered_date for {count} registrations'
     )
     for registration in egap_registrations:
         egap_id = registration.registration_responses.get(EGAP_ID_KEY)
@@ -70,7 +79,7 @@ def main(dry_run, batch_size=-1):
         )
         if egap_registration_date is not None:
             logger.info(
-                'f{"[DRY RUN]: " if dry_run else ""}'
+                f'{"[DRY RUN]: " if dry_run else ""}'
                 'Copying Timestamp or Original Registration to registered_date for '
                 f'Registration with GUID {registration._id}'
             )
@@ -78,7 +87,7 @@ def main(dry_run, batch_size=-1):
 
         if not dry_run:
             registration.save()
-    return egap_registrations.count()
+    return count
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
@@ -93,10 +102,10 @@ class Command(BaseCommand):
         parser.add_argument(
             '--batch_size',
             type=int,
-            default=-1
+            default=0
         )
 
     def handle(self, *args, **options):
         dry_run = options.get('dry_run')
         batch_size = options.get('batch_size')
-        main(dry_run=dry_run, batch_size=batch_size)
+        backfill_egap_metadata(dry_run=dry_run, batch_size=batch_size)
