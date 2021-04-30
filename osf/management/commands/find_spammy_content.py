2268f56fc591175906e12434b6b6f095998d4781
corbinSanders <corbin@cos.io>
2021-04-30 13:18:25 -0400

    Adding management command to mass spam content. Adding admin app functionality to run command. Adding tests

---
+++ osf/management/commands/find_spammy_content.py
@@ -0,0 +1,72 @@
+import io
+import csv
+import re
+from datetime import timedelta
+import logging
+
+from django.utils import timezone
+from osf.models import Node, Preprint, Registration
+
+logger = logging.getLogger(__name__)
+
+def manage_spammy_content(regex, days=1, models=[Node, Preprint, Registration], return_csv=True, ban=False, response_object=None):
+    script_start_time = timezone.now()
+    logger.info(f'Script started time: {script_start_time}')
+    data = []
+    count = 0
+    field_names = ['Object Type', 'GUID', 'Spam Content', 'Created', 'User ID', 'Username', 'Fullname']
+
+    for model in models:
+        data.extend(find_spammy_content(regex, days, model, ban))
+        count = count + len(data)
+
+    script_finish_time = timezone.now()
+    logger.info('Script finished time: {}'.format(script_finish_time))
+    logger.info('Run time {}'.format(script_finish_time - script_start_time))
+
+    if ban:
+        logger.info(f'Banned {count} users')
+        return count
+
+    if return_csv:
+        if response_object:
+            output = response_object
+        else:
+            output = io.StringIO()
+        writer = csv.writer(output, field_names)
+        writer.writerow([field for field in field_names])
+        for item in data:
+            writer.writerow([
+                item['type'],
+                item['guid'],
+                item['content'],
+                item['created'],
+                item['uid'],
+                item['username'],
+                item['fullname']
+            ])
+        return writer
+    else:
+        return data
+
+def find_spammy_content(regex, days, model, ban):
+    check_fields = model.SPAM_CHECK_FIELDS.copy()
+    created_items = model.objects.filter(created__gte=timezone.now() - timedelta(days=days))
+    data = []
+    for item in created_items:
+        item_data = {}
+        spam_fields = item.get_spam_fields(check_fields)
+        spam_content = item._get_spam_content(spam_fields)
+        matches = re.search(regex, spam_content)
+        if matches:
+            item_data['type'] = model.__name__
+            item_data['guid'] = item._id
+            item_data['content'] = spam_content
+            item_data['created'] = item.created
+            item_data['uid'] = item.creator._id
+            item_data['username'] = item.creator.username
+            item_data['fullname'] = item.creator.fullname
+            data.append(item_data)
+            if ban:
+                item.suspend_spam_user(item.creator)
+    return data
---
+++ osf_tests/management_commands/test_find_spammy_content.py
@@ -0,0 +1,110 @@
+import pytest
+from datetime import timedelta
+from django.utils import timezone
+
+from osf_tests.factories import (
+    UserFactory,
+    NodeFactory,
+    RegistrationFactory,
+    PreprintFactory
+)
+
+from osf.models import (
+    Node,
+    Registration,
+    Preprint
+)
+
+from osf.management.commands.find_spammy_content import manage_spammy_content
+
+
+@pytest.mark.django_db
+class TestFindSpammyContent:
+
+    @pytest.fixture
+    def user(self):
+        return UserFactory()
+
+    @pytest.fixture
+    def user_two(self):
+        return UserFactory()
+
+    @pytest.fixture
+    def kombat_node(self, user):
+        return NodeFactory(title='Mortal Kombat Spam', creator=user)
+
+    @pytest.fixture
+    def node_two(self, user_two):
+        return NodeFactory(title='Not Combat Spam', creator=user_two)
+
+    @pytest.fixture
+    def spam_node_but_old(self, user_two):
+        return NodeFactory(title='Kombat spam', creator=user_two, created=timezone.now() - timedelta(days=2))
+
+    @pytest.fixture
+    def kombat_registration(self, user):
+        return RegistrationFactory(title='Mortal Kombat Spam', creator=user)
+
+    @pytest.fixture
+    def registration_two(self, user_two):
+        return RegistrationFactory(title='Not Combat Spam', creator=user_two)
+
+    @pytest.fixture
+    def kombat_preprint(self, user):
+        return PreprintFactory(title='Mortal Kombat Spam', creator=user)
+
+    @pytest.fixture
+    def preprint_two(self, user_two):
+        return PreprintFactory(title='Not Combat Spam', creator=user_two)
+
+    def test_get_all_node_spam(self, kombat_node, node_two):
+        spam_data = manage_spammy_content('Kombat', models=[Node, ], return_csv=False)
+        spam_guids = [spam['guid'] for spam in spam_data]
+        assert kombat_node._id in spam_guids
+        assert node_two not in spam_guids
+
+    def test_get_all_registration_spam(self, kombat_registration, registration_two):
+        spam_data = manage_spammy_content('Kombat', models=[Registration, ], return_csv=False)
+        spam_guids = [spam['guid'] for spam in spam_data]
+        assert kombat_registration._id in spam_guids
+        assert registration_two not in spam_guids
+
+    def test_get_all_preprint_spam(self, kombat_preprint, preprint_two):
+        spam_data = manage_spammy_content('Kombat', models=[Preprint, ], return_csv=False)
+        spam_guids = [spam['guid'] for spam in spam_data]
+        assert kombat_preprint._id in spam_guids
+        assert preprint_two not in spam_guids
+
+    def test_get_recent_spam(self, kombat_node, spam_node_but_old):
+        spam_data = manage_spammy_content('Kombat', models=[Node, ], return_csv=False)
+        spam_guids = [spam['guid'] for spam in spam_data]
+        assert kombat_node._id in spam_guids
+        assert spam_node_but_old not in spam_guids
+
+    def test_ban_all_node_spam(self, user, user_two, kombat_node, node_two):
+        manage_spammy_content('Kombat', models=[Node, ], ban=True)
+        assert kombat_node.is_spam
+        assert not node_two.is_spam
+        assert user.is_disabled
+        assert not user_two.is_disabled
+
+    def test_ban_all_preprint_spam(self, user, user_two, kombat_preprint, preprint_two):
+        manage_spammy_content('Kombat', models=[Preprint, ], ban=True)
+        assert kombat_preprint.is_spam
+        assert not preprint_two.is_spam
+        assert user.is_disabled
+        assert not user_two.is_disabled
+
+    def test_ban_all_registration_spam(self, user, user_two, kombat_registration, registration_two):
+        manage_spammy_content('Kombat', models=[Registration, ], ban=True)
+        assert kombat_registration.is_spam
+        assert not registration_two.is_spam
+        assert user.is_disabled
+        assert not user_two.is_disabled
+
+    def test_ban_recent_spam(self, kombat_node, spam_node_but_old, user, user_two):
+        manage_spammy_content('Kombat', models=[Node, ], ban=True)
+        assert kombat_node.is_spam
+        assert not spam_node_but_old.is_spam
+        assert user.is_disabled
+        assert not user_two.is_disabled
