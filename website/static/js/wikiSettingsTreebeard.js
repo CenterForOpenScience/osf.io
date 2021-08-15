b15f543f7af2218622e4deca2e29c3faa535db7c
John Tordoff <>
2021-08-15 18:52:23 -0400

    add more aria-labels to settings page for a11y

--- website/static/js/wikiSettingsTreebeard.js
+++ website/static/js/wikiSettingsTreebeard.js
@@ -120,7 +120,7 @@ function ProjectWiki(data) {
                     filter : false,
                     custom : function() {
                         return  m('div[style="padding-right:10px"]',
-                            [m('select.form-control', {
+                            [m('select.form-control[aria-label="Select Wiki Privacy"]', {
                                 onchange: function(ev) {
                                     beforeChangePermissions(item, ev.target.value);
                                 },
