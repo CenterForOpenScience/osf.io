cd8fefb78b40b9e740a02f0566fb2204202016ec
John Tordoff <>
2021-08-17 10:23:51 -0400

    add aria-label to search input

--- website/static/js/pages/project-settings-page.js
+++ website/static/js/pages/project-settings-page.js
@@ -55,8 +55,6 @@ if ($('#wiki-grid').length) {
             extra: { url: wikiSettingsURL, status: status, error: error }
         });
     });
-    // For a11y compatibility
-    $('#wiki-grid > .gridWrapper > .tb-table > div').attr('id', 'tb-body-wiki');
 }
 
 $(document).ready(function() {
