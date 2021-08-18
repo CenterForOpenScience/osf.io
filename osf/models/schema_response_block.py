39fbba119510ba2431240214f5fa8255e4a70509
Jon Walz <jonwalz@Admins-MBP.lan>
2021-08-18 10:15:13 -0400

    Add schema FK to schema_responses, rename GenericRelation on the node, re-order shtuffs

--- osf/models/schema_response_block.py
+++ osf/models/schema_response_block.py
@@ -4,12 +4,11 @@ from osf.models.base import BaseModel, ObjectIDMixin
 
 class SchemaResponseBlock(ObjectIDMixin, BaseModel):
 
-    # Match length for RegistrationSchemaBlockKey.registration_response_key
-    schema_key = models.CharField(max_length=255)
-    answer = models.TextField()
-
     # The RegistrationSchemaBlock that defines the question being answered
     source_block = models.ForeignKey('osf.RegistrationSchemaBlock', null=False)
-
     # The SchemaResponses version where this response originated
     source_revision = models.ForeignKey('osf.SchemaResponses', null=False)
+
+    # Should match source_block.registration_response_key
+    schema_key = models.CharField(max_length=255)
+    answer = models.TextField()
--- osf/models/schema_responses.py
+++ osf/models/schema_responses.py
@@ -2,19 +2,19 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from osf.models.base import BaseModel, ObjectIDMixin
-from osf.models.schema_response_block import SchemaResponseBlock
 from osf.utils.fields import NonNaiveDateTimeField
 
 
 class SchemaResponses(ObjectIDMixin, BaseModel):
 
+    schema = models.ForeignKey('osf.registrationschema')
+    all_responses = models.ManyToManyField('osf.schemaresponseblock')
+    initiator = models.ForeignKey('osf.osfuser', null=False)
+
+    justification = models.CharField(max_length=2048, null=True)
+    submitted_timestamp = NonNaiveDateTimeField(null=True)
+
     # Allow schema responses for non-Registrations
     object_id = models.PositiveIntegerField()
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     parent = GenericForeignKey('content_type', 'object_id')
-
-    all_responses = models.ManyToManyField(SchemaResponseBlock)
-
-    justification = models.CharField(max_length=2048, null=True)
-    initiator = models.ForeignKey('OSFUser', null=False)
-    submitted_timestamp = NonNaiveDateTimeField(null=True)
