357839a10191f3584e745a8a4bd552d4f3a3c8c3
Jon Walz <jonwalz@Admins-MBP.lan>
2021-08-19 13:27:56 -0400

    Add revision_responses and revised_responses properties

--- osf/models/schema_responses.py
+++ osf/models/schema_responses.py
@@ -18,3 +18,18 @@ class SchemaResponses(ObjectIDMixin, BaseModel):
     object_id = models.PositiveIntegerField()
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     parent = GenericForeignKey('content_type', 'object_id')
+
+    @property
+    def revision_responses(self):
+        '''Surfaces responses from response_blocks in a dictionary format'''
+        formatted_responses = {
+            response_block.schema_key: response_block.response
+            for response_block in self.response_blocks.all()
+        }
+        return formatted_responses
+
+    @property
+    def revised_responses(self):
+        '''Surfaces the keys of responses_blocks added in this revision.'''
+        revised_keys = self.revised_response_blocks.values_list('schema_key', flat=True)
+        return list(revised_keys)
