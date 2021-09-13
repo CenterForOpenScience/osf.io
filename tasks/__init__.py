c34df8359e65e75e326b1cdcff55a78d8bcd525a
Jon Walz <jonwalz@Admins-MBP.lan>
2021-09-13 09:24:19 -0400

    Add schema_resposne API tests to GHA

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -357,6 +357,7 @@ API_TESTS2 = [
     'api_tests/nodes',
     'api_tests/osf_groups',
     'api_tests/requests',
+    'api_tests/schema_responses'
     'api_tests/subscriptions',
     'api_tests/waffle',
     'api_tests/wb',
