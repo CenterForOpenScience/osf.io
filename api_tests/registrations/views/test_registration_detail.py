31ec14cc6c40153582b4602d84b613aedec487f2
Jon Walz <jonwalz@Admins-MBP.lan>
2021-10-07 12:00:15 -0400

    Manage SchemaResponses on root + tests

--- api_tests/registrations/views/test_registration_detail.py
+++ api_tests/registrations/views/test_registration_detail.py
@@ -9,7 +9,8 @@ from api.base.settings.defaults import API_BASE
 from api.taxonomies.serializers import subjects_as_relationships_version
 from api_tests.subjects.mixins import UpdateSubjectsMixin
 from osf.utils import permissions
-from osf.models import Registration, NodeLog, NodeLicense
+from osf.utils.workflows import ApprovalStates
+from osf.models import Registration, NodeLog, NodeLicense, SchemaResponse
 from framework.auth import Auth
 from website.project.signals import contributor_added
 from api_tests.utils import disconnected_from_listeners
@@ -29,6 +30,7 @@ from osf_tests.factories import (
     CommentFactory,
     InstitutionFactory,
 )
+from osf_tests.utils import get_default_test_schema
 
 from api_tests.nodes.views.test_node_detail import TestNodeUpdateLicense
 from tests.utils import assert_latest_log
@@ -1468,3 +1470,72 @@ class TestProviderSpecificMetadata():
         registration.refresh_from_db()
         assert registration.description == 'Setting all of the things'
         assert registration.additional_metadata['foo'] == 'baz'
+
+
+@pytest.mark.django_db
+class TestRegistrationResponses:
+
+    INITIAL_SCHEMA_RESPONSES = {
+        'q1': 'Some answer',
+        'q2': 'Some even longer answer',
+        'q3': 'A',
+        'q4': ['D', 'G'],
+        'q5': None,
+        'q6': None
+    }
+
+    @pytest.fixture
+    def registration(self):
+        return RegistrationFactory(schema=get_default_test_schema())
+
+    @pytest.fixture
+    def approved_schema_response(self, registration):
+        response = registration.schema_responses.last()
+        response.state = ApprovalStates.IN_PROGRESS
+        response.update_responses(self.INITIAL_SCHEMA_RESPONSES)
+        response.state = ApprovalStates.APPROVED
+        response.save()
+        return response
+
+    @pytest.fixture
+    def in_progress_schema_response(self, approved_schema_response):
+        response = SchemaResponse.create_from_previous_response(
+            previous_response=approved_schema_response,
+            initiator=approved_schema_response.initiator
+        )
+        response.update_responses({'q1': 'updated', 'q2': 'answers'})
+        return response
+
+    @pytest.fixture
+    def nested_registration(self, registration):
+        return RegistrationFactory(
+            project=ProjectFactory(parent=registration.registered_from),
+            parent=registration,
+            is_public=True,
+        )
+
+    def get_registration_detail_url(self, registration):
+        return f'/{API_BASE}registrations/{registration._id}/'
+
+    def test_registration_responses_surfaces_latest_approved_responses(
+            self, app, registration, approved_schema_response, in_progress_schema_response):
+        assert not registration.registration_responses
+
+        resp = app.get(
+            self.get_registration_detail_url(registration), auth=registration.creator.auth
+        )
+
+        responses = resp.json['data']['attributes']['registration_responses']
+        assert responses == approved_schema_response.all_responses
+
+    def test_nested_registration_surfaces_root_schema_responses(
+            self, app, nested_registration, approved_schema_response):
+        assert not nested_registration.registration_responses
+        assert not nested_registration.schema_responses.exists()
+
+        resp = app.get(
+            self.get_registration_detail_url(nested_registration)
+        )
+
+        responses = resp.json['data']['attributes']['registration_responses']
+        assert responses == approved_schema_response.all_responses
