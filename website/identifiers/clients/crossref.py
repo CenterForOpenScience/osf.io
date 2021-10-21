37c5dfa0d1c7d802eed3b44b53dfbe6ed5fe2d87
John Tordoff <>
2021-10-21 13:51:48 -0400

    fix circular dependency which was causing crossref to error

--- website/identifiers/clients/crossref.py
+++ website/identifiers/clients/crossref.py
@@ -120,7 +120,7 @@ class CrossRefClient(AbstractIdentifierClient):
                     element.program(xmlns=CROSSREF_ACCESS_INDICATORS)
                 )
 
-            if preprint.article_doi and include_relation:
+            if preprint.article_doi != self.build_doi(preprint) and include_relation:
                 posted_content.append(
                     element.program(
                         element.related_item(
