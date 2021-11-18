e08dce115da9bdebb4cfef6a85860a48509a5384
John Tordoff <>
2021-11-18 14:18:58 -0500

    fix revision justifications and prevent unchanged responses from being submit

--- api_tests/actions/views/test_schema_response_action_list.py
+++ api_tests/actions/views/test_schema_response_action_list.py
@@ -391,6 +391,19 @@ class TestSchemaResponseActionListPOSTPermissions:
 @pytest.mark.enable_quickfiles_creation
 class TestSchemaResponseActionListPOSTBehavior:
 
+    def test_POST_submit__denies_unchanged_submission(self, app):
+        auth, schema_response, _, _ = configure_test_preconditions(
+            schema_response_state=ApprovalStates.IN_PROGRESS, role='admin'
+        )
+        schema_response.updated_response_blocks.all().delete()
+        assert schema_response.updated_response_keys == set()
+        payload = make_payload(schema_response, trigger=Triggers.SUBMIT)
+        resp = app.post_json_api(
+            make_api_url(schema_response), payload, auth=auth, expect_errors=True
+        )
+        assert resp.status_code == 400
+        assert resp.json['errors'][0]['detail'] == 'Cannot submit SchemaResponses without updated registration responses.'
+
     def test_POST_submit__writes_action_and_advances_state(self, app):
         auth, schema_response, _, _ = configure_test_preconditions(
             schema_response_state=ApprovalStates.IN_PROGRESS, role='admin'
--- api_tests/schema_responses/views/test_schema_response_detail.py
+++ api_tests/schema_responses/views/test_schema_response_detail.py
@@ -495,6 +495,13 @@ class TestSchemaResponseDetailPATCHBehavior:
         schema_response.refresh_from_db()
         assert schema_response.revision_justification == 'why not?'
 
+        payload['data']['attributes']['revision_justification'] = None
+        resp = app.patch_json_api(make_api_url(schema_response), payload, auth=admin_user.auth, expect_errors=True)
+
+        schema_response.refresh_from_db()
+        assert resp.status_code == 400
+        assert resp.json['errors'][0]['detail'] == 'This field may not be null.'
+
     @pytest.mark.parametrize('response_state', IMMUTABLE_STATES)
     def test_PATCH_fails_in_unsupported_state(
             self, app, schema_response, payload, admin_user, response_state):
