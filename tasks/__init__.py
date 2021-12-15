dbb0c24f0a7a61da4e002a098e8e848445e038a0
Fitz Elliott <fitz@cos.io>
2021-12-15 14:19:30 -0500

    debug: dare I hope

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -832,10 +832,10 @@ def assets(ctx, dev=False, watch=False, colors=False):
         command += ' --production'
     ctx.run('yarn -v', echo=True)
     print('honk')
+    ctx.run('perl -i -pwe "s{require}{meow}gs" /usr/local/lib/node_modules/yarn/lib/cli.js', echo=True)
+    print('zoig')
     ctx.run('cat /usr/local/lib/node_modules/yarn/lib/cli.js', echo=True)
     print('berf')
-    ctx.run('perl -v', echo=True)
-    print('zoig')
     ctx.run(command, echo=True)
     # ctx.run(command + ' || cat /home/runner/work/osf.io/osf.io/yarn-error.log', echo=True)
     print('quack')
