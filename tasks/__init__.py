083e616b38c6847978d3c0b571ed8d71d63bb0c6
Fitz Elliott <fitz@cos.io>
2021-12-15 20:56:06 -0500

    debug: semi-oho

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -838,7 +838,7 @@ def assets(ctx, dev=False, watch=False, colors=False):
     # ctx.run('cp cli.js /usr/local/lib/node_modules/yarn/lib/cli.js', echo=True)
 
     print('mistump')
-    ctx.run('perl -i -pwe "s{invariant\(hash, \'Commit hash required\'\);}{console\.trace\(\)\ console.log(\'blah blah blah\: \' \+ this\.reference\); invariant\(hash, \'Commit hash required\'\);}" /usr/local/lib/node_modules/yarn/lib/cli.js', echo=True)
+    ctx.run('perl -i -pwe "s{invariant\(hash, \'Commit hash required\'\);}{console\.trace\(\)\; console.log(\'blah blah blah\: \' \+ this\.reference\); invariant\(hash, \'Commit hash required\'\);}" /usr/local/lib/node_modules/yarn/lib/cli.js', echo=True)
     print('zoig')
     ctx.run('perl -i -pwe "s[fetchFromExternal\(\) \{][fetchFromExternal\(\) \{\n      console.log\(\'blah blah blah\: \', this, arguments\);]" /usr/local/lib/node_modules/yarn/lib/cli.js', echo=True)
     print('mlersp')
