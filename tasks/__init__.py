98937efa06e79bfccd396e8746598def4d33c2fc
Fitz Elliott <fitz@cos.io>
2021-12-15 21:55:55 -0500

    debug: moregarbage

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -838,7 +838,9 @@ def assets(ctx, dev=False, watch=False, colors=False):
     # ctx.run('cp cli.js /usr/local/lib/node_modules/yarn/lib/cli.js', echo=True)
 
     print('mistump')
-    ctx.run('perl -i -pwe "s{invariant\(hash, \'Commit hash required\'\);}{console\.trace\(\)\; console.log(\'blah blah blah\: \' \+ this\.reference\); invariant\(hash, \'Commit hash required\'\);}" /usr/local/lib/node_modules/yarn/lib/cli.js', echo=True)
+    ctx.run('perl -i -pwe "s{invariant\(hash, \'Commit hash required\'\);}{console\.trace\(\)\; console.log(\'blah blah blah\: hash::\' \+ this\.hash \+ \'  ref::\' \+ this\.reference\); invariant\(hash, \'Commit hash required\'\);}" /usr/local/lib/node_modules/yarn/lib/cli.js', echo=True)
+    print('wwweww')
+    ctx.run('perl -i -pwe "s{invariant\(hash, \'Commit hash required\'\);}{ if(\!hash)\{ console.log(\'quok quok quok\: hash::\' \+ this\.hash \+ \'  ref::\' \+ this\.reference\);\} invariant\(hash, \'Commit hash required\'\);}" /usr/local/lib/node_modules/yarn/lib/cli.js', echo=True)
     print('zoig')
     ctx.run('perl -i -pwe "s[fetchFromExternal\(\) \{][fetchFromExternal\(\) \{\n      console.log\(\'blah blah blah\: this::\' + this + \' args::\' + arguments\);]" /usr/local/lib/node_modules/yarn/lib/cli.js', echo=True)
     print('mlersp')
