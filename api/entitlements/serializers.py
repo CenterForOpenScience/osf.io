9f85ee85e6b6145782a6af6246de3936082d2440
Phat Nguyen <ntphat@tma.com.vn>
2022-02-11 16:32:20 +0700

    202201-LargeFile - 4.2 - remove read_only from LoginAvailabilitySerializer

--- api/entitlements/serializers.py
+++ api/entitlements/serializers.py
@@ -2,5 +2,5 @@ from rest_framework import serializers as ser
 
 
 class LoginAvailabilitySerializer(ser.Serializer):
-    institution_id = ser.IntegerField(required=True, read_only=True)
+    institution_id = ser.IntegerField(required=True)
     entitlements = ser.ListField(required=True, child=ser.CharField())
