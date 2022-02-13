f0563ad7c2d241870b6518f2eeb21cb85255ca3f
Phat Nguyen <ntphat@tma.com.vn>
2022-02-13 16:08:16 +0700

    202201-LargeFile - 4.1.2 - remove rdm_useremails templates and views

--- admin/rdm_useremails/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class SearchForm(forms.Form):
-    guid = forms.CharField(label='guid', min_length=5, max_length=5, required=False)  # TODO: Move max to 6 when needed
-    name = forms.CharField(label='name:', required=False)
-    email = forms.EmailField(label='email', required=False)
--- admin/rdm_useremails/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.SearchView.as_view(), name='search'),
-    url(r'^result/$', views.ResultView.as_view(), name='result'),
-    url(r'^settings/$', views.SettingsView.as_view(), name='settings'),
-]
