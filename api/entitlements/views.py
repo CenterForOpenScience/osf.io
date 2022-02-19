4587f72c77a6c70b6d987a27a590318911df3b38
lmtan <lmtan@tma.com.vn>
2022-02-19 09:14:37 +0700

    202201-LargeFile- 4.2 - format code

--- api/entitlements/views.py
+++ api/entitlements/views.py
@@ -23,8 +23,10 @@ class LoginAvailability(APIView):
             data = serializer.validated_data
             institution_id = data.get('institution_id')
             entitlements = data.get('entitlements')
-            entitlement_list = InstitutionEntitlement.objects.filter(institution_id=institution_id,
-                                                                     entitlement__in=entitlements)
+            entitlement_list = InstitutionEntitlement.objects.filter(
+                institution_id=institution_id,
+                entitlement__in=entitlements,
+            )
             login_availability = all(list(entitlement_list.values_list('login_availability', flat=True)))
             return Response({'login_availability': login_availability}, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
