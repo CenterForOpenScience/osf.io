f01d604faa0740e1093c2e2c5983353b15f8595a
Thuc Nguyen <nqtthuc@tma.com.vn>
2022-03-12 16:08:42 +0700

    202201-AccountRegister-4.1.3-update ut in test_202201

--- tests/test_202201/website/search/test_util.py
+++ tests/test_202201/website/search/test_util.py
@@ -1,6 +1,69 @@
 from nose import tools as nt
 from tests.base import OsfTestCase
 from website.search import util
+from tests.utils import run_celery_tasks
+from osf_tests.factories import AuthUserFactory, InstitutionFactory, ProjectFactory
+import website.search.search as search
+from website.search_migration.migrate import migrate
+import time
+import pytest
+
+
+@pytest.mark.enable_search
+@pytest.mark.enable_enqueue_task
+class TestContributorSearch(OsfTestCase):
+
+    def setUp(self):
+        super(TestContributorSearch, self).setUp()
+
+        with run_celery_tasks():
+            self.firstname = 'jane'
+            self.fullname1 = self.firstname + ' 1'
+            self.fullname2 = self.firstname + ' 2'
+            self.fullname3 = self.firstname + ' 3'
+            self.dummy1_fullname = self.firstname + ' dummy1'
+            self.dummy2_fullname = self.firstname + ' dummy2'
+            self.dummy3_fullname = self.firstname + ' dummy3'
+            self.dummy4_fullname = self.firstname + ' dummy4'
+
+            self.inst1 = inst1 = InstitutionFactory()
+            self.inst2 = inst2 = InstitutionFactory()
+
+            def create_user(name, insts):
+                user = AuthUserFactory(fullname=name)
+                for inst in insts:
+                    user.affiliated_institutions.add(inst)
+                user.save()
+                return user
+
+            self.user1 = create_user(self.fullname1, (self.inst1,))
+            self.user2 = create_user(self.fullname2, (self.inst1, self.inst2))
+            self.user3 = create_user(self.fullname3, ())
+
+            create_user(self.dummy1_fullname, (self.inst1,))
+            create_user(self.dummy2_fullname, (self.inst1,))
+            create_user(self.dummy3_fullname, (self.inst2,))
+            create_user(self.dummy4_fullname, (self.inst2,))
+
+            self.inst1_users = [u.fullname for u in inst1.osfuser_set.all()]
+            self.inst2_users = [u.fullname for u in inst2.osfuser_set.all()]
+
+            # dummy_project
+            ProjectFactory(creator=self.user1, is_public=False)
+
+    def test_search_contributors_by_email(self):
+        email2 = self.user2.emails.get()
+        email2.address = 'test@example.com'
+        email2.save()
+        migrate(delete=False, remove=False,
+                index=None, app=self.app.app)
+        time.sleep(10)
+        contribs = search.search_contributor(
+            email2.address,
+            current_user=self.user1
+        )
+        nt.assert_equal(set([u['fullname'] for u in contribs['users']]),
+                     set([self.user2.fullname]))
 
 
 class TestSearchUtils(OsfTestCase):
