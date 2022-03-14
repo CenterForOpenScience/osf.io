c1eff62297bc8372752db4df1ff2af7cf18450ad
John Tordoff <>
2022-03-14 09:17:14 -0400

    drop periodic-ness and move to async

--- osf/management/commands/fix_quickfiles_waterbulter_logs.py
+++ osf/management/commands/fix_quickfiles_waterbulter_logs.py
@@ -4,6 +4,8 @@ from django.core.management.base import BaseCommand
 from django.db import transaction
 from osf.models import Node, NodeLog
 from framework.celery_tasks import app as celery_app
+from framework.celery_tasks.handlers import enqueue_task
+
 
 logger = logging.getLogger(__name__)
 
@@ -24,46 +26,45 @@ def swap_guid_view_download(url, node):
     return f'/{url}'
 
 
-@celery_app.task(name='osf.management.commands.fix_quickfiles_waterbutler_logs')
-def fix_quickfiles_waterbutler_logs(batch_size=100, dry_run=False):
+@celery_app.task()
+def fix_logs(node):
     '''
     Fixes view/download links for waterbutler based file logs, and also fixes old 10 digit node params for moved/renamed
     files.
     '''
     with transaction.atomic():
-        nodes = Node.objects.filter(
-            logs__action=NodeLog.MIGRATED_QUICK_FILES
-        ).filter(
-            logs__action__in=['addon_file_renamed', 'addon_file_moved', 'addon_file_copied', 'osf_storage_file_added', 'file_tag_removed', 'file_tag_added', 'osf_storage_file_removed']
-        )[:batch_size]
-        i = 0
-        for i, node in enumerate(nodes):
+        for log in node.logs.filter(action__in=['addon_file_renamed', 'addon_file_moved', 'addon_file_copied']):
+            log.params['params_node'].update({'id': node._id})
+            url = swap_guid(log.params['source']['url'], node)
+            log.params['destination'].update({'url': url})
 
-            for log in node.logs.filter(action__in=['addon_file_renamed', 'addon_file_moved', 'addon_file_copied']):
-                log.params['params_node'].update({'id': node._id})
-                url = swap_guid(log.params['source']['url'], node)
-                log.params['destination'].update({'url': url})
+            if log.action == 'addon_file_renamed':
+                log.params['source'].update({'url': url})
 
-                if log.action == 'addon_file_renamed':
-                    log.params['source'].update({'url': url})
+            log.save()
 
-                log.save()
+        for log in node.logs.filter(action__in=['osf_storage_file_added', 'file_tag_removed', 'file_tag_added', 'osf_storage_file_removed', 'osf_storage_file_updated']):
+            log.params['params_node'].update({'id': node._id})
+            url = swap_guid_view_download(log.params['urls']['view'], node)
+            log.params['urls'] = {
+                'view': url,
+                'download': f'{url}?action=download'
+            }
+            log.save()
 
-            for log in node.logs.filter(action__in=['osf_storage_file_added', 'file_tag_removed', 'file_tag_added', 'osf_storage_file_removed']):
-                log.params['params_node'].update({'id': node._id})
-                url = swap_guid_view_download(log.params['urls']['view'], node)
-                log.params['urls'] = {
-                    'view': url,
-                    'download': f'{url}?action=download'
-                }
-                log.save()
+        node.save()
 
-            node.save()
+        logger.info(f'{node._id} Quickfiles logs fixed')
 
-        logger.info(f'{i} Quickfiles logs fixed')
 
-        if dry_run:
-            raise RuntimeError('Dry run, transaction rolled back')
+def fix_quickfiles_waterbutler_logs(batch_size=100, dry_run=False):
+    nodes = Node.objects.filter(
+        logs__action=NodeLog.MIGRATED_QUICK_FILES
+    ).filter(
+        logs__action__in=['addon_file_renamed', 'addon_file_moved', 'addon_file_copied', 'osf_storage_file_added', 'file_tag_removed', 'file_tag_added', 'osf_storage_file_removed']
+    )
+    for node in nodes:
+        enqueue_task(fix_logs.s(node))
 
 
 class Command(BaseCommand):
