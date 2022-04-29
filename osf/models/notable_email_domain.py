1463ce8ce48f8abf161499ef44bef28b2e518c48
Longze Chen <cslzchen@gmail.com>
2022-04-29 16:12:22 -0400

    Migrations tweaked and tested

--- osf/models/notable_email_domain.py
+++ osf/models/notable_email_domain.py
@@ -6,17 +6,19 @@ from osf.models.base import BaseModel
 from osf.utils.fields import LowercaseCharField
 
 
-class NotableEmailDomain(BaseModel):
-    class Note(IntEnum):
-        EXCLUDE_FROM_ACCOUNT_CREATION = 0
-        ASSUME_HAM_UNTIL_REPORTED = 1
+class Note(IntEnum):
+    EXCLUDE_FROM_ACCOUNT_CREATION = 0
+    ASSUME_HAM_UNTIL_REPORTED = 1
 
-        @classmethod
-        def choices(cls):
-            return [
-                (int(enum_item), enum_item.name)
-                for enum_item in cls
-            ]
+    @classmethod
+    def choices(cls):
+        return [
+            (int(enum_item), enum_item.name)
+            for enum_item in cls
+        ]
+
+
+class NotableEmailDomain(BaseModel):
 
     domain = LowercaseCharField(max_length=255, unique=True, db_index=True)
 
