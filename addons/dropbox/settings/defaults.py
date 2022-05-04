63cdad69046ce224ec1a8018ad97643a9e47a799
Matt Frazier <maf7sm@virginia.edu>
2022-05-04 17:07:46 -0400

    Update DropBox OAuth process

--- addons/dropbox/settings/defaults.py
+++ addons/dropbox/settings/defaults.py
@@ -3,6 +3,9 @@ DROPBOX_KEY = None
 DROPBOX_SECRET = None
 
 DROPBOX_AUTH_CSRF_TOKEN = 'dropbox-auth-csrf-token'
+DROPBOX_OAUTH_AUTH_ENDPOINT = 'https://www.dropbox.com/oauth2/authorize'
+DROPBOX_OAUTH_TOKEN_ENDPOINT = 'https://www.dropbox.com/oauth2/token'
+REFRESH_TIME = 14399  # 4 hours
 
 # Max file size permitted by frontend in megabytes
 MAX_UPLOAD_SIZE = 5 * 1024
