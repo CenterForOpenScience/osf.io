54f41e0e25fb6328ad4634375d3d841b775ed365
John Tordoff <>
2022-06-01 11:30:21 -0400

    remove set preprint id category population

--- osf/migrations/0106_set_preprint_identifier_category.py
+++ osf/migrations/0106_set_preprint_identifier_category.py
@@ -7,22 +7,6 @@ from django.db import migrations, models
 from django.contrib.contenttypes.models import ContentType
 
 
-def set_preprint_identifier_catetory_to_legacy(apps, *args, **kwargs):
-
-    PreprintService = apps.get_model('osf', 'PreprintService')
-    Identifier = apps.get_model('osf', 'Identifier')
-
-    preprint_content_type = ContentType.objects.get_for_model(PreprintService)
-    Identifier.objects.filter(content_type_id=preprint_content_type.id, category='doi').update(category='legacy_doi')
-
-def return_preprint_identifier_category_to_doi(apps, *args, **kwargs):
-    PreprintService = apps.get_model('osf', 'PreprintService')
-    Identifier = apps.get_model('osf', 'Identifier')
-
-    preprint_content_type = ContentType.objects.get_for_model(PreprintService)
-    Identifier.objects.filter(content_type_id=preprint_content_type.id, category='legacy_doi').update(category='doi')
-
-
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -35,5 +19,4 @@ class Migration(migrations.Migration):
             name='category',
             field=models.CharField(max_length=20),
         ),
-        migrations.RunPython(set_preprint_identifier_catetory_to_legacy, return_preprint_identifier_category_to_doi)
     ]
