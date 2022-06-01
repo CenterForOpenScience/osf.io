81e9f5e4959a1f75ae66325903dbb2b98a18c646
John Tordoff <>
2022-06-01 09:34:00 -0400

    remove setting share titles

--- osf/migrations/0043_set_share_title.py
+++ osf/migrations/0043_set_share_title.py
@@ -7,29 +7,6 @@ from __future__ import unicode_literals
 
 from django.db import migrations
 
-# _id => share_title
-SHARE_TITLES = {
-    'osf': 'OSF',
-    'lawarxiv': 'LawArXiv',
-    'mindrxiv': 'MindRxiv',
-    'bitss': 'BITSS',
-    'agrixiv': 'AgriXiv',
-    'engrxiv': 'engrXiv',
-    'lissa': 'LIS Scholarship Archive',
-    'psyarxiv': 'PsyArXiv',
-    'socarxiv': 'SocArXiv',
-}
-
-def set_share_titles(state, *args, **kwargs):
-    PreprintProvider = state.get_model('osf', 'preprintprovider')
-    for provider in PreprintProvider.objects.filter(_id__in=list(SHARE_TITLES.keys())):
-        provider.share_title = SHARE_TITLES[provider._id]
-        provider.save()
-
-
-def unset_share_titles(state, *args, **kwargs):
-    PreprintProvider = state.get_model('osf', 'preprintprovider')
-    PreprintProvider.objects.filter(_id__in=list(SHARE_TITLES.keys())).update(share_title='')
 
 class Migration(migrations.Migration):
 
@@ -38,7 +15,4 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(
-            set_share_titles, unset_share_titles
-        ),
     ]
