535c79a01c70e525aac119ec49dff9b86250819b
John Tordoff <>
2022-06-01 09:36:02 -0400

    remove backfill for unique notifications

--- osf/migrations/0021_unique_notificationsettings__ids.py
+++ osf/migrations/0021_unique_notificationsettings__ids.py
@@ -3,29 +3,9 @@
 #   1. The next script would fail if duplicate records existed
 #   2. This should only need to be run once
 from __future__ import unicode_literals
-import logging
 
-from django.db import connection, migrations
+from django.db import migrations
 
-logger = logging.getLogger(__file__)
-
-def remove_duplicate_notificationsubscriptions(state, schema):
-    NotificationSubscription = state.get_model('osf', 'notificationsubscription')
-    # Deletes the newest from each set of duplicates
-    sql = """
-    SELECT MAX(id)
-    FROM osf_notificationsubscription
-    GROUP BY _id HAVING COUNT(*) > 1;
-    """
-    with connection.cursor() as cursor:
-        cursor.execute(sql)
-        ids = list(sum(cursor.fetchall(), ()))
-        logger.info('Deleting duplicate NotificationSubscriptions with `id`s {}'.format(ids))
-        # Use Django to cascade delete through tables
-        NotificationSubscription.objects.filter(id__in=ids).delete()
-
-def noop(*args):
-    logger.info('Removal of duplicates cannot be reversed, skipping.')
 
 class Migration(migrations.Migration):
 
@@ -34,7 +14,4 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(
-            remove_duplicate_notificationsubscriptions, noop
-        ),
     ]
