713017664cfd4259809107d8dcc9e49c1701e63f
John Tordoff <>
2022-06-01 11:40:40 -0400

    remove custom schema population

--- osf/migrations/0139_rename_aspredicted_schema.py
+++ osf/migrations/0139_rename_aspredicted_schema.py
@@ -4,28 +4,6 @@ from __future__ import unicode_literals
 
 from django.db import migrations
 
-OLD_NAME = 'AsPredicted Preregistration'
-NEW_NAME = 'Preregistration Template from AsPredicted.org'
-
-def rename_schema(model, from_name, to_name):
-    try:
-        schema = model.objects.get(name=from_name)
-    except model.DoesNotExist:
-        return
-
-    schema.name = to_name
-    schema.schema['name'] = to_name
-    schema.schema['title'] = to_name
-    schema.schema['pages'][0]['title'] = to_name
-    return schema.save()
-
-def rename_aspredicted_schema(state, schema):
-    RegistrationSchema = state.get_model('osf.registrationschema')
-    return rename_schema(RegistrationSchema, OLD_NAME, NEW_NAME)
-
-def undo_aspredicted_rename(state, schema):
-    RegistrationSchema = state.get_model('osf.registrationschema')
-    return rename_schema(RegistrationSchema, NEW_NAME, OLD_NAME)
 
 class Migration(migrations.Migration):
 
@@ -34,5 +12,4 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(rename_aspredicted_schema, undo_aspredicted_rename)
     ]
--- osf/migrations/0196_update_schemas.py
+++ osf/migrations/0196_update_schemas.py
@@ -3,15 +3,6 @@ from __future__ import unicode_literals
 from django.db import migrations
 from osf.utils.migrations import UpdateRegistrationSchemasAndSchemaBlocks
 
-def make_egap_active_but_invisible(state, schema):
-        RegistrationSchema = state.get_model('osf', 'registrationschema')
-        egap_registration = RegistrationSchema.objects.get(name='EGAP Registration', schema_version=2)
-        egap_registration.visible = False
-        egap_registration.active = True
-        egap_registration.save()
-
-def noop(*args, **kwargs):
-    pass
 
 class Migration(migrations.Migration):
 
@@ -20,6 +11,4 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        UpdateRegistrationSchemasAndSchemaBlocks(),
-        migrations.RunPython(make_egap_active_but_invisible, noop),
     ]
