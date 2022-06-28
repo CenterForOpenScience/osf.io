2f5ee6d701eb2bad9044bd3ccb3dd72ce1ed4634
Longze Chen <cslzchen@gmail.com>
2022-06-28 14:52:46 -0400

    Update instn auth api to support eppn and email migration

--- framework/auth/__init__.py
+++ framework/auth/__init__.py
@@ -114,6 +114,43 @@ def register_unconfirmed(username, password, fullname, campaign=None, accepted_t
     return user
 
 
+def get_or_create_institutional_user(fullname, email, eppn=None):
+    """
+    Get or create an institutional user by fullname, email address and eppn (optional). Returns a tuple of three
+    objects ``(user, is_created, email_to_add)``: the user to authenticate, whether the user is newly created or
+    not, and an extra email to add to the user later (after the user passes status check).
+
+    :param str fullname: user's full name
+    :param str email: user's email, which comes from the email attribute during SSO
+    :param str eppn: user's eppn, which comes from the identity attribute during SSO
+    """
+
+    from osf.models import OSFUser
+    email_user = get_user(email=email)
+    # If eppn is provided by CAS, check if an account already exists using eppn as email.
+    if eppn:
+        eppn_user = get_user(email=eppn)
+        if eppn_user:
+            if not email_user:
+                # CASE 1: If the email doesn't belong to any user, return both the eppn_user and the email
+                return eppn_user, False, email
+            if email_user == eppn_user:
+                # CASE 2: Return the user only since email already belongs to the same user
+                return email_user, False, None
+            # TODO: log warning and inform sentry since this indicates potential duplicate users exist
+            # CASE 3: When eppn and email point to different users, the email takes priority
+            return email_user, False, None
+    # If eppn is empty, use email_user if found.
+    if email_user:
+        # CASE 4: user only found via email
+        return email_user, False, None
+    # CASE 5/5: existing user not found
+    # Institution users are created as confirmed with a strong and random password. Users don't need the password
+    # since they sign in via institution SSO. They can still reset their password to enable email/password login.
+    user = OSFUser.create_confirmed(email, str(uuid.uuid4()), fullname)
+    return user, True, None
+
+
 def get_or_create_user(fullname, address, reset_password=True, is_spam=False):
     """
     Get or create user by fullname and email address.
