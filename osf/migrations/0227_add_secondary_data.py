4e63d3a57487411b7f8a69b8e0b888961749284a
Longze Chen <cslzchen@gmail.com>
2022-07-13 12:10:27 -0400

    [ENG-3863] Move schema ensuring and schema blocks update to post-migrate signals (#9974)
    * Set noop to all ensure_schema migrations
    * Set noop to shema and shema block updates
    * Add ensuring schema and updating schema blocks to post-migrate signals
    * Set noop to one-time data migrations, which include
      * Preprint permissions
      * Schema to schema blocks
      * Draft node permissions
    * Remove manual post-migration signals from PND
    * Move generate_object_id into migration utils
    * Replace duplicate function with inline imports

--- osf/migrations/0227_add_secondary_data.py
+++ osf/migrations/0227_add_secondary_data.py
@@ -3,16 +3,6 @@ import logging
 from django.db import migrations
 
 logger = logging.getLogger(__file__)
-from osf.models import RegistrationSchema
-from osf.utils.migrations import ensure_schemas
-from website.project.metadata.schemas import ensure_schema_structure, from_json
-from osf.utils.migrations import UpdateRegistrationSchemasAndSchemaBlocks
-
-
-def add_schema(apps, schema_editor):
-    schema = ensure_schema_structure(from_json('secondary-data.json'))
-
-    RegistrationSchema.objects.filter(name=schema['name']).update(visible=False, active=True)
 
 
 class Migration(migrations.Migration):
@@ -21,7 +11,4 @@ class Migration(migrations.Migration):
         ('osf', '0226_auto_20210224_1610'),
     ]
 
-    operations = [
-        UpdateRegistrationSchemasAndSchemaBlocks(),
-        migrations.RunPython(add_schema, ensure_schemas),
-    ]
+    operations = []
