5746a84f4a6ed6f14fd68e7c563750eaa4f11690
Longze Chen <cslzchen@gmail.com>
2022-07-13 12:10:27 -0400

    [ENG-3863] Move schema ensuring and schema blocks update to post-migrate signals (#9974)
    * Set noop to all ensure_schema migrations
    * Set noop to shema and shema block updates
    * Add ensuring schema and updating schema blocks to post-migrate signals
    * Set noop to one-time data migrations, which include
      * Preprint permissions
      * Schema to schema blocks
      * Draft node permissions
    * Remove manual post-migration signals from PND
    * Move generate_object_id into migration utils
    * Replace duplicate function with inline imports

--- osf/apps.py
+++ osf/apps.py
@@ -5,31 +5,42 @@ from osf.migrations import (
     update_permission_groups,
     update_waffle_flags,
     create_cache_table,
-    update_license
+    update_license,
+    add_registration_schemas
 )
 
 logger = logging.getLogger(__file__)
 
 
 class AppConfig(BaseAppConfig):
+
     name = 'osf'
     app_label = 'osf'
     managed = True
 
     def ready(self):
         super(AppConfig, self).ready()
+
+        post_migrate.connect(
+            add_registration_schemas,
+            dispatch_uid='osf.apps.add_registration_schemas'
+        )
+
         post_migrate.connect(
             update_permission_groups,
             dispatch_uid='osf.apps.update_permissions_groups'
         )
+
         post_migrate.connect(
             update_license,
             dispatch_uid='osf.apps.ensure_licenses',
         )
+
         post_migrate.connect(
             update_waffle_flags,
             dispatch_uid='osf.apps.update_waffle_flags'
         )
+
         post_migrate.connect(
             create_cache_table,
             dispatch_uid='osf.apps.create_cache_table'
--- osf/migrations/0152_ensure_schemas.py
+++ osf/migrations/0152_ensure_schemas.py
@@ -1,7 +1,6 @@
 from __future__ import unicode_literals
 
 from django.db import migrations
-from osf.utils.migrations import ensure_schemas
 
 
 class Migration(migrations.Migration):
@@ -10,6 +9,4 @@ class Migration(migrations.Migration):
         ('osf', '0151_auto_20181215_1911'),
     ]
 
-    operations = [
-        migrations.RunPython(ensure_schemas, ensure_schemas),
-    ]
+    operations = []
