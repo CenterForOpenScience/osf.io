119ccbf83315bec4eb1b466f7a32ff79739a6c71
Longze Chen <cslzchen@gmail.com>
2022-07-13 09:04:50 -0400

    Add ensuring schema and updating schema blocks to post-migrate signals

--- osf/apps.py
+++ osf/apps.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 
 from django.apps import AppConfig as BaseAppConfig
 from django.db.models.signals import post_migrate
-from osf.migrations import update_permission_groups
+from osf.migrations import add_registration_schemas, update_permission_groups
 
 
 class AppConfig(BaseAppConfig):
@@ -12,7 +12,13 @@ class AppConfig(BaseAppConfig):
 
     def ready(self):
         super(AppConfig, self).ready()
+
         post_migrate.connect(
             update_permission_groups,
             dispatch_uid='osf.apps.update_permissions_groups'
+        )
+
+        post_migrate.connect(
+            add_registration_schemas,
+            dispatch_uid='osf.apps.add_registration_schemas'
         )
