2a3b42966449c95344028651327e7131681a7b49
John Tordoff <>
2022-07-14 10:16:47 -0400

    upgrade password change views to Django 3

--- admin/common_auth/urls.py
+++ admin/common_auth/urls.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 from django.conf.urls import url
 from django.urls import reverse_lazy
-from django.contrib.auth.views import password_change, password_change_done
+from django.contrib.auth.views import PasswordChangeView, PasswordChangeDoneView
 
 from admin.common_auth import views
 
@@ -12,10 +12,10 @@ urlpatterns = [
     url(r'^login/?$', views.LoginView.as_view(), name='login'),
     url(r'^logout/$', views.logout_user, name='logout'),
     url(r'^register/$', views.RegisterUser.as_view(), name='register'),
-    url(r'^password_change/$', password_change,
+    url(r'^password_change/$', PasswordChangeView.as_view(),
         {'post_change_redirect': reverse_lazy('auth:password_change_done')},
         name='password_change'),
-    url(r'^password_change/done/$', password_change_done,
+    url(r'^password_change/done/$', PasswordChangeDoneView.as_view(),
         {'template_name': 'password_change_done.html'},
         name='password_change_done'),
     url(r'^settings/desk/$', views.DeskUserCreateFormView.as_view(), name='desk'),
