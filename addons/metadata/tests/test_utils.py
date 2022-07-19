455b263e47c8e3b110ad56e9641ad47b6ad77947
yacchin1205 <968739+yacchin1205@users.noreply.github.com>
2022-07-19 15:51:27 +0900

    Fix report format

--- addons/metadata/tests/test_utils.py
+++ addons/metadata/tests/test_utils.py
@@ -52,3 +52,40 @@ class TestMakeReportAsCsv(OsfTestCase):
         filename, result = make_report_as_csv(format, data, schema)
         assert_equal(filename, 'report.csv')
         assert_equal(result, '"TEST,DATA"')
+
+    def test_choose_tooltip(self):
+        format = RegistrationReportFormat.objects.create(
+            csv_template='{{example}},{{example_tooltip}},{{example_tooltip_0}},{{example_tooltip_1}},{{example_tooltip_2}},{{example_tooltip_3}}',
+        )
+        data = {
+            'example': {
+                'value': '2',
+            }
+        }
+        schema = {'pages': [
+            {
+                'questions': [
+                    {
+                        'qid': 'example',
+                        'type': 'choose',
+                        'options': [
+                            {
+                                'text': '1',
+                                'tooltip': '一|one',
+                            },
+                            {
+                                'text': '2',
+                                'tooltip': '二|two',
+                            },
+                            {
+                                'text': '3',
+                                'tooltip': '三|three',
+                            },
+                        ],
+                    }
+                ],
+            }
+        ]}
+        filename, result = make_report_as_csv(format, data, schema)
+        assert_equal(filename, 'report.csv')
+        assert_equal(result, '2,二|two,二,two,,')
