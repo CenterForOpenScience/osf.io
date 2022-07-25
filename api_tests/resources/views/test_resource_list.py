49a6fbd34e9dc548b4c4b881e48f02777e53bc86
Jon Walz <jonwalz@Admins-MBP.lan>
2022-07-25 09:43:12 -0400

    Extend UserRoles functionality

--- api_tests/resources/views/test_resource_list.py
+++ api_tests/resources/views/test_resource_list.py
@@ -43,13 +43,13 @@ def payload(registration):
 @pytest.mark.django_db
 class TestResourceListPOSTPermissions:
 
-    @pytest.mark.parametrize('user_role', UserRoles.write_users())
+    @pytest.mark.parametrize('user_role', UserRoles.write_roles())
     def test_status_code__write_user(self, app, registration, payload, user_role):
         test_auth = configure_test_auth(registration, user_role)
         resp = app.post_json_api(POST_URL, payload, auth=test_auth, expect_errors=True)
         assert resp.status_code == 201
 
-    @pytest.mark.parametrize('user_role', UserRoles.excluding(*UserRoles.write_users()))
+    @pytest.mark.parametrize('user_role', UserRoles.excluding(*UserRoles.write_roles()))
     def test_status_code__non_admin(self, app, registration, payload, user_role):
         test_auth = configure_test_auth(registration, user_role)
         resp = app.post_json_api(POST_URL, payload, auth=test_auth, expect_errors=True)
