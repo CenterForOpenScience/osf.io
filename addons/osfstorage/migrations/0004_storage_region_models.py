53e73bf774786b3aaf7b58bb25b4fb328109c244
John Tordoff <>
2022-07-26 09:52:06 -0400

    squash single file

--- addons/osfstorage/migrations/0004_storage_region_models.py
+++ addons/osfstorage/migrations/0004_storage_region_models.py
@@ -17,6 +17,7 @@ class Migration(migrations.Migration):
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('addons_osfstorage', '0003_auto_20170713_1125'),
+        ('osf', '0001_initial'),
     ]
 
     operations = [
