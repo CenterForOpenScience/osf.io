d733370c97a57e696fac03a705f52d052697aaa3
Abram Booth <boothaa@gmail.com>
2022-08-15 11:11:01 -0400

    add api for reports and counted usage

--- api/metrics/serializers.py
+++ api/metrics/serializers.py
@@ -1,5 +1,9 @@
-from api.base.serializers import BaseAPISerializer
 from rest_framework import serializers as ser
+
+from api.base.serializers import BaseAPISerializer
+from api.base.utils import absolute_reverse
+from osf.metrics.counted_usage import CountedUsageV1
+from website import settings as website_settings
 
 
 class PreprintMetricSerializer(BaseAPISerializer):
@@ -13,3 +17,135 @@ class PreprintMetricSerializer(BaseAPISerializer):
 class RawMetricsSerializer():
 
     query = ser.DictField()
+
+
+def validate_action_label(label):
+    try:
+        CountedUsageV1.ActionLabel(label)
+    except ValueError:
+        valid_labels = ', '.join(label.value for label in CountedUsageV1.ActionLabel)
+        raise ser.ValidationError(
+            f'Invalid value in action_labels! Valid labels: {valid_labels}',
+        )
+
+
+class PageviewInfoSerializer(ser.Serializer):
+    page_url = ser.URLField(max_length=4095, required=True)
+    page_title = ser.CharField(max_length=4095, required=False)
+    referer_url = ser.URLField(max_length=4095, required=False, allow_blank=True)
+
+
+class CountedUsageSerializer(ser.Serializer):
+    provider_id = ser.CharField(max_length=255, default='osf')
+    item_guid = ser.CharField(max_length=255, required=False)
+    item_public = ser.BooleanField(required=False)
+    client_session_id = ser.CharField(max_length=255, required=False)
+
+    action_labels = ser.ListField(
+        child=ser.CharField(validators=[validate_action_label]),
+        allow_empty=False,
+        max_length=255,
+    )
+
+    # when the "usage" is viewing a web page, include pageview info
+    pageview_info = PageviewInfoSerializer(required=False)
+
+    def validate(self, data):
+        no_guid = not data.get('item_guid')
+        no_page_url = not data.get('pageview_info', {}).get('page_url')
+        if no_guid and no_page_url:
+            raise ser.ValidationError(f'Either item_guid or pageview_info.page_url is required({data})')
+        return data
+
+    def create(self, validated_data):
+        return CountedUsageV1.record(
+            platform_iri=website_settings.DOMAIN,
+            provider_id=validated_data.get('provider_id'),
+            item_guid=validated_data.get('item_guid'),
+            item_public=validated_data.get('item_public'),
+            session_id=validated_data['session_id'],  # must be provided by the view
+            action_labels=validated_data.get('action_labels'),
+            pageview_info=validated_data.get('pageview_info'),
+        )
+
+
+class ReportNameSerializer(ser.BaseSerializer):
+    def to_representation(self, instance):
+        latest_link = absolute_reverse(
+            'metrics:latest-report-detail',
+            kwargs={'report_name': instance},
+        )
+        recent_link = absolute_reverse(
+            'metrics:recent-report-list',
+            kwargs={'report_name': instance},
+        )
+        return {
+            'id': instance,
+            'type': 'metrics-report-name',
+            'links': {
+                'latest': latest_link,
+                'recent': recent_link,
+            },
+        }
+
+
+class DailyReportSerializer(ser.BaseSerializer):
+    def to_representation(self, instance):
+        # TODO: detangle datamodel (osf.metrics.reports) from api serialization
+        # (don't use `to_dict` here)
+        report_as_dict = instance.to_dict()
+        report_name = self.context['report_name']
+        return {
+            'id': instance.meta.id,
+            'type': f'daily-report:{report_name}',
+            'attributes': {
+                **report_as_dict,
+                'report_date': report_as_dict['report_date'].date().isoformat(),
+            },
+        }
+
+
+class NodeAnalyticsSerializer(ser.BaseSerializer):
+    def to_representation(self, instance):
+        aggs = instance.aggregations
+        popular_pages = [
+            {
+                'path': bucket['key'],
+                'title': bucket['page-titles'].buckets[0]['key'],
+                'count': bucket['doc_count'],
+            }
+            for bucket in aggs['popular-pages'].buckets
+        ]
+        unique_visits = [
+            {
+                'date': bucket['key_as_string'],
+                'count': bucket['doc_count'],
+            }
+            for bucket in aggs['unique-visits'].buckets
+        ]
+        time_of_day = [
+            {
+                'hour': bucket['key'],
+                'count': bucket['doc_count'],
+            }
+            for bucket in aggs['time-of-day'].buckets
+        ]
+        referer_domain = [
+            {
+                'referer_domain': bucket['key'],
+                'count': bucket['doc_count'],
+            }
+            for bucket in aggs['referer-domain'].buckets
+        ]
+        node_guid = self.context['node_guid']
+        timespan = self.context['timespan']
+        return {
+            'id': f'{node_guid}:{timespan}',
+            'type': 'node-analytics',
+            'attributes': {
+                'popular_pages': popular_pages,
+                'unique_visits': unique_visits,
+                'time_of_day': time_of_day,
+                'referer_domain': referer_domain,
+            },
+        }
---
+++ api_tests/metrics/test_counted_usage.py
@@ -0,0 +1,222 @@
+from datetime import datetime, timezone
+
+import pytest
+from unittest import mock
+
+from osf_tests.factories import AuthUserFactory
+
+
+COUNTED_USAGE_URL = '/_/metrics/events/counted_usage/'
+
+def counted_usage_payload(**attributes):
+    return {
+        'data': {
+            'type': 'counted-usage',
+            'attributes': attributes,
+        },
+    }
+
+
+def assert_saved_with(mock_save, *, expected_doc_id, expected_attrs):
+    assert mock_save.call_count == 1
+    args, kwargs = mock_save.call_args
+    actual_instance = args[0]
+    actual_attrs = actual_instance.to_dict()
+    for attr_name, expected_value in expected_attrs.items():
+        actual_value = actual_attrs.get(attr_name, None)
+        assert actual_value == expected_value, repr(actual_value)
+    assert actual_instance.meta.id == expected_doc_id
+
+
+@pytest.mark.django_db
+class TestRestrictions:
+    def test_http_method(self, app):
+        resp = app.put(COUNTED_USAGE_URL, expect_errors=True)
+        assert resp.status_code == 405
+        resp = app.get(COUNTED_USAGE_URL, expect_errors=True)
+        assert resp.status_code == 405
+        resp = app.patch(COUNTED_USAGE_URL, expect_errors=True)
+        assert resp.status_code == 405
+        resp = app.delete(COUNTED_USAGE_URL, expect_errors=True)
+        assert resp.status_code == 405
+
+    @pytest.mark.parametrize('attrs', [
+        {},
+        {'item_guid': 'foo', 'action_labels': []},
+        {'action_labels': []},
+        {'pageview_info': {'page_url': 'http://example.foo/blahblah/blee'}},
+    ])
+    def test_required_attributes(self, app, attrs):
+        payload = counted_usage_payload(**attrs)
+        resp = app.post_json_api(COUNTED_USAGE_URL, payload, expect_errors=True)
+        assert resp.status_code == 400
+
+
+@pytest.mark.django_db
+class TestComputedFields:
+    @pytest.fixture(autouse=True)
+    def mock_domain(self):
+        domain = 'http://example.foo/'
+        with mock.patch('api.metrics.serializers.website_settings.DOMAIN', new=domain):
+            yield domain
+
+    @pytest.fixture
+    def mock_save(self):
+        with mock.patch('elasticsearch_dsl.Document.save', autospec=True) as mock_save:
+            yield mock_save
+
+    @pytest.fixture
+    def now(self):
+        timestamp = datetime(1981, 1, 1, 0, 1, 31, tzinfo=timezone.utc)
+        with mock.patch('django.utils.timezone.now', return_value=timestamp):
+            yield timestamp
+
+    @pytest.fixture()
+    def user(self):
+        with mock.patch('osf.models.base.generate_guid', return_value='guidy'):
+            return AuthUserFactory()
+
+    def test_by_client_session_id(self, app, mock_save, now, user):
+        payload = counted_usage_payload(
+            client_session_id='hello',
+            item_guid='zyxwv',
+            action_labels=['view', 'api'],
+            item_public=True,
+            pageview_info={'page_url': 'http://example.foo/blahblah/blee'},
+        )
+        headers = {
+            'User-Agent': 'haha',
+        }
+        resp = app.post_json_api(COUNTED_USAGE_URL, payload, headers=headers, auth=user.auth)
+        assert resp.status_code == 201
+        assert_saved_with(
+            mock_save,
+            # doc_id: sha256(b'http://example.foo/|osf|zyxwv|5b7c8b0a740a5b23712258a9d1164d2af008df02a8e3d339f16ead1d19595b34|1981-01-01|3').hexdigest()
+            expected_doc_id='6fac366c096998fd23ae2ca654e8573cd4cb2e17b9691d45b22a381fa5f67c09',
+            expected_attrs={
+                'platform_iri': 'http://example.foo/',
+                'provider_id': 'osf',
+                'item_guid': 'zyxwv',
+                # session_id: sha256(b'hello|1981-01-01').hexdigest()
+                'session_id': '5b7c8b0a740a5b23712258a9d1164d2af008df02a8e3d339f16ead1d19595b34',
+                'item_public': True,
+                'action_labels': ['view', 'api'],
+                'pageview_info': {
+                    'page_url': 'http://example.foo/blahblah/blee',
+                    'page_path': '/blahblah/blee',
+                    'hour_of_day': 0,
+                },
+            },
+        )
+
+    def test_by_client_session_id_anon(self, app, mock_save, now):
+        payload = counted_usage_payload(
+            client_session_id='hello',
+            item_guid='zyxwv',
+            action_labels=['view', 'web'],
+            item_public=True,
+            pageview_info={
+                'page_url': 'http://example.foo/bliz/',
+                'referer_url': 'http://elsewhere.baz/index.php',
+            },
+        )
+        headers = {
+            'User-Agent': 'haha',
+        }
+        resp = app.post_json_api(COUNTED_USAGE_URL, payload, headers=headers)
+        assert resp.status_code == 201
+        assert_saved_with(
+            mock_save,
+            # doc_id: sha256(b'http://example.foo/|osf|zyxwv|5b7c8b0a740a5b23712258a9d1164d2af008df02a8e3d339f16ead1d19595b34|1981-01-01|3').hexdigest()
+            expected_doc_id='6fac366c096998fd23ae2ca654e8573cd4cb2e17b9691d45b22a381fa5f67c09',
+            expected_attrs={
+                'platform_iri': 'http://example.foo/',
+                'provider_id': 'osf',
+                'item_guid': 'zyxwv',
+                # session_id: sha256(b'hello|1981-01-01').hexdigest()
+                'session_id': '5b7c8b0a740a5b23712258a9d1164d2af008df02a8e3d339f16ead1d19595b34',
+                'item_public': True,
+                'action_labels': ['view', 'web'],
+                'pageview_info': {
+                    'page_url': 'http://example.foo/bliz/',
+                    'page_path': '/bliz/',
+                    'referer_url': 'http://elsewhere.baz/index.php',
+                    'referer_domain': 'elsewhere.baz',
+                    'hour_of_day': 0,
+                },
+            },
+        )
+
+    def test_by_user_auth(self, app, mock_save, now, user):
+        payload = counted_usage_payload(
+            item_guid='yxwvu',
+            action_labels=['view', 'web'],
+            item_public=False,
+            pageview_info={
+                'page_url': 'http://osf.io/mst3k',
+                'referer_url': 'http://osf.io/registries/discover',
+            },
+        )
+        headers = {
+            'User-Agent': 'haha',
+        }
+        resp = app.post_json_api(COUNTED_USAGE_URL, payload, headers=headers, auth=user.auth)
+        assert resp.status_code == 201
+        assert_saved_with(
+            mock_save,
+            # doc_id: sha256(b'http://example.foo/|osf|yxwvu|ec768abb16c3411570af99b9d635c2c32d1ca31d1b25eec8ee73759e7242e74a|1981-01-01|3').hexdigest()
+            expected_doc_id='da08c656cb1ef4198b986122803da3a1943ff64de01e34233a88e07f59d5b5ea',
+            expected_attrs={
+                'platform_iri': 'http://example.foo/',
+                'provider_id': 'osf',
+                'item_guid': 'yxwvu',
+                # session_id: sha256(b'guidy|1981-01-01|0').hexdigest()
+                'session_id': 'ec768abb16c3411570af99b9d635c2c32d1ca31d1b25eec8ee73759e7242e74a',
+                'item_public': False,
+                'action_labels': ['view', 'web'],
+                'pageview_info': {
+                    'page_url': 'http://osf.io/mst3k',
+                    'page_path': '/mst3k',
+                    'referer_url': 'http://osf.io/registries/discover',
+                    'referer_domain': 'osf.io',
+                    'hour_of_day': 0,
+                },
+            },
+        )
+
+    def test_by_useragent_header(self, app, mock_save, now):
+        payload = counted_usage_payload(
+            item_guid='yxwvu',
+            action_labels=['view', 'api'],
+            item_public=False,
+            pageview_info={
+                'page_url': 'http://blibble.blop',
+                'referer_url': 'http://blopple.blip/',
+            },
+        )
+        headers = {
+            'User-Agent': 'haha',
+        }
+        resp = app.post_json_api(COUNTED_USAGE_URL, payload, headers=headers)
+        assert resp.status_code == 201
+        assert_saved_with(
+            mock_save,
+            # doc_id: sha256(b'http://example.foo/|osf|yxwvu|97098dd3f7cd26053c0d0264d1c84eaeea8e08d2c55ca34017ffbe53c749ba5a|1981-01-01|3').hexdigest()
+            expected_doc_id='92dd6ec64045abb294958bebe523c68c5f8589ca44bbdfba5272ef299e7286a9',
+            expected_attrs={
+                'platform_iri': 'http://example.foo/',
+                'provider_id': 'osf',
+                'item_guid': 'yxwvu',
+                # session_id: sha256(b'localhost:80|haha|1981-01-01|0').hexdigest()
+                'session_id': '97098dd3f7cd26053c0d0264d1c84eaeea8e08d2c55ca34017ffbe53c749ba5a',
+                'item_public': False,
+                'action_labels': ['view', 'api'],
+                'pageview_info': {
+                    'page_url': 'http://blibble.blop',
+                    'page_path': '',
+                    'referer_url': 'http://blopple.blip/',
+                    'referer_domain': 'blopple.blip',
+                    'hour_of_day': 0,
+                },
+            },
+        )
