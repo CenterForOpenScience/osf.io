0ad3f7c8543cc0960b85f11023c9a568698e7797
Longze Chen <cslzchen@gmail.com>
2022-09-01 11:37:57 -0400

    Fix system check warnings for django admin
    Co-authored-by: John Tordoff <johnetordoff@gmail.com>

--- api/base/settings/defaults.py
+++ api/base/settings/defaults.py
@@ -247,6 +247,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
