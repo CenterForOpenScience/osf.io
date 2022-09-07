4f040921beab96c725a3ea1eb694ed34f23a5629
lmtan <lmtan@tma.com.vn>
2022-09-07 19:21:31 +0700

    (2)4.2 - Delete unnecessary method

--- admin/user_identification_information_admin/views.py
+++ admin/user_identification_information_admin/views.py
@@ -1,19 +1,10 @@
 from django.http import Http404
 
 from admin.user_identification_information.views import (
-    UserIdentificationInformationListView,
     UserIdentificationListView,
     UserIdentificationDetailView,
     ExportFileCSVView,
 )
-
-
-class UserIdentificationInformationAdminListView(UserIdentificationInformationListView):
-
-    def get_context_data(self, **kwargs):
-        if self.is_super_admin:
-            raise Http404('Page not found')
-        return super(UserIdentificationInformationListView, self).get_context_data(**kwargs)
 
 
 class UserIdentificationAdminListView(UserIdentificationListView):
