0bc3d5cc592c7b5c7371dae227243dd28bbc564e
John Tordoff <>
2022-09-13 09:01:00 -0400

    upgrade Django factory models for Django 3

--- osf_tests/factories.py
+++ osf_tests/factories.py
@@ -470,6 +470,7 @@ class RegistrationFactory(BaseNodeFactory):
         reg.files_count = reg.registered_from.files.filter(deleted_on__isnull=True).count()
         draft_registration.registered_node = reg
         draft_registration.save()
+        reg.creator = user  # keep auth if passed
         reg.save()
 
         if has_doi:
@@ -702,12 +703,18 @@ class PreprintFactory(DjangoModelFactory):
     creator = factory.SubFactory(AuthUserFactory)
 
     doi = factory.Sequence(lambda n: '10.123/{}'.format(n))
-    provider = factory.SubFactory(PreprintProviderFactory)
 
     @classmethod
     def _build(cls, target_class, *args, **kwargs):
         creator = kwargs.pop('creator', None) or UserFactory()
         provider = kwargs.pop('provider', None) or PreprintProviderFactory()
+
+        # pre Django 3 behavior
+        reviews_workflow = kwargs.pop('reviews_workflow', None)
+        if reviews_workflow:
+            provider.reviews_workflow = reviews_workflow
+            provider.save()
+
         project = kwargs.pop('project', None) or None
         title = kwargs.pop('title', None) or 'Untitled'
         description = kwargs.pop('description', None) or 'None'
