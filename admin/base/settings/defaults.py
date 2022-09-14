95a64e00c6bc1eac7e0019b820ea66e7b7883a50
John Tordoff <>
2022-09-14 11:11:19 -0400

    Fix middleware order and replace deprecated staticfiles with static

--- admin/base/settings/defaults.py
+++ admin/base/settings/defaults.py
@@ -153,8 +153,8 @@ MIDDLEWARE = (
 
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
