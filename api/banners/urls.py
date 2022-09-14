03491349558230bb8ea342258ed559c84fe66b9f
John Tordoff <>
2022-09-14 10:43:23 -0400

    Replace deprecated url() with re_path() for api

--- api/banners/urls.py
+++ api/banners/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
+from django.conf.urls import re_path
 
 from api.banners import views
 
 app_name = 'osf'
 
 urlpatterns = [
-    url(r'^current/$', views.CurrentBanner.as_view(), name=views.CurrentBanner.view_name),
-    url(r'^(?P<filename>[^/]+)/$', views.BannerMedia.as_view(), name=views.BannerMedia.view_name),
+    re_path(r'^current/$', views.CurrentBanner.as_view(), name=views.CurrentBanner.view_name),
+    re_path(r'^(?P<filename>[^/]+)/$', views.BannerMedia.as_view(), name=views.BannerMedia.view_name),
 ]
--- api/search/urls.py
+++ api/search/urls.py
@@ -1,19 +1,19 @@
-from django.conf.urls import url
+from django.conf.urls import re_path
 
 from api.search import views
 
 app_name = 'osf'
 
 urlpatterns = [
-    url(r'^$', views.Search.as_view(), name=views.Search.view_name),
-    url(r'^components/$', views.SearchComponents.as_view(), name=views.SearchComponents.view_name),
-    url(r'^files/$', views.SearchFiles.as_view(), name=views.SearchFiles.view_name),
-    url(r'^projects/$', views.SearchProjects.as_view(), name=views.SearchProjects.view_name),
-    url(r'^registrations/$', views.SearchRegistrations.as_view(), name=views.SearchRegistrations.view_name),
-    url(r'^users/$', views.SearchUsers.as_view(), name=views.SearchUsers.view_name),
-    url(r'^institutions/$', views.SearchInstitutions.as_view(), name=views.SearchInstitutions.view_name),
-    url(r'^collections/$', views.SearchCollections.as_view(), name=views.SearchCollections.view_name),
+    re_path(r'^$', views.Search.as_view(), name=views.Search.view_name),
+    re_path(r'^components/$', views.SearchComponents.as_view(), name=views.SearchComponents.view_name),
+    re_path(r'^files/$', views.SearchFiles.as_view(), name=views.SearchFiles.view_name),
+    re_path(r'^projects/$', views.SearchProjects.as_view(), name=views.SearchProjects.view_name),
+    re_path(r'^registrations/$', views.SearchRegistrations.as_view(), name=views.SearchRegistrations.view_name),
+    re_path(r'^users/$', views.SearchUsers.as_view(), name=views.SearchUsers.view_name),
+    re_path(r'^institutions/$', views.SearchInstitutions.as_view(), name=views.SearchInstitutions.view_name),
+    re_path(r'^collections/$', views.SearchCollections.as_view(), name=views.SearchCollections.view_name),
 
     # not currently supported by v1, but should be supported by v2
-    # url(r'^nodes/$', views.SearchProjects.as_view(), name=views.SearchProjects.view_name),
+    # re_path(r'^nodes/$', views.SearchProjects.as_view(), name=views.SearchProjects.view_name),
 ]
