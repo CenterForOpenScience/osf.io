cb4264b99518cf2d764a06b65acb5ea5f8aa7dd5
Longze Chen <cslzchen@gmail.com>
2022-09-22 22:28:26 -0400

    Replace deprecated url() with re_path() for admin

--- admin/collection_providers/urls.py
+++ admin/collection_providers/urls.py
@@ -1,15 +1,15 @@
-from django.conf.urls import url
+from django.conf.urls import re_path
 from . import views
 
 app_name = 'admin'
 
 urlpatterns = [
-    url(r'^create/$', views.CreateCollectionProvider.as_view(), name='create'),
-    url(r'^$', views.CollectionProviderList.as_view(), name='list'),
-    url(r'^import/$', views.ImportCollectionProvider.as_view(), name='import'),
-    url(r'^(?P<collection_provider_id>[a-z0-9]+)/$', views.CollectionProviderDetail.as_view(), name='detail'),
-    url(r'^(?P<collection_provider_id>[a-z0-9]+)/delete/$', views.DeleteCollectionProvider.as_view(), name='delete'),
-    url(r'^(?P<collection_provider_id>[a-z0-9]+)/export/$', views.ExportColectionProvider.as_view(), name='export'),
-    url(r'^(?P<collection_provider_id>[a-z0-9]+)/import/$', views.ImportCollectionProvider.as_view(), name='import'),
-    url(r'^(?P<collection_provider_id>[a-z0-9]+)/cannot_delete/$', views.CannotDeleteProvider.as_view(), name='cannot_delete'),
+    re_path(r'^create/$', views.CreateCollectionProvider.as_view(), name='create'),
+    re_path(r'^$', views.CollectionProviderList.as_view(), name='list'),
+    re_path(r'^import/$', views.ImportCollectionProvider.as_view(), name='import'),
+    re_path(r'^(?P<collection_provider_id>[a-z0-9]+)/$', views.CollectionProviderDetail.as_view(), name='detail'),
+    re_path(r'^(?P<collection_provider_id>[a-z0-9]+)/delete/$', views.DeleteCollectionProvider.as_view(), name='delete'),
+    re_path(r'^(?P<collection_provider_id>[a-z0-9]+)/export/$', views.ExportColectionProvider.as_view(), name='export'),
+    re_path(r'^(?P<collection_provider_id>[a-z0-9]+)/import/$', views.ImportCollectionProvider.as_view(), name='import'),
+    re_path(r'^(?P<collection_provider_id>[a-z0-9]+)/cannot_delete/$', views.CannotDeleteProvider.as_view(), name='cannot_delete'),
 ]
