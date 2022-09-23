2646600e93697f196c12e50de7c04c040ff0d52d
Longze Chen <cslzchen@gmail.com>
2022-09-22 22:02:56 -0400

    Replace deprecated NullBooleanField + add migrations

--- osf/models/registrations.py
+++ osf/models/registrations.py
@@ -102,9 +102,7 @@ class Registration(AbstractNode):
     )
     registered_date = NonNaiveDateTimeField(db_index=True, null=True, blank=True)
 
-    # This is a NullBooleanField because of inheritance issues with using a BooleanField
-    # TODO: Update to BooleanField(default=False, null=True) when Django is updated to >=2.1
-    external_registration = models.NullBooleanField(default=False)
+    external_registration = models.BooleanField(null=True, default=False)
     registered_user = models.ForeignKey(OSFUser,
                                         related_name='related_to',
                                         on_delete=models.SET_NULL,
@@ -136,7 +134,7 @@ class Registration(AbstractNode):
                                                     null=True, blank=True,
                                                     on_delete=models.SET_NULL)
     files_count = models.PositiveIntegerField(blank=True, null=True)
-    branched_from_node = models.NullBooleanField(blank=True, null=True)
+    branched_from_node = models.BooleanField(null=True, blank=True)
 
     moderation_state = models.CharField(
         max_length=30,
