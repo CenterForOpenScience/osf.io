c22327fffdbd1c8721353cf5ab2105b04ec2cf4f
Longze Chen <cslzchen@gmail.com>
2022-09-22 22:28:26 -0400

    Replace deprecated url() with re_path() for admin

--- admin/common_auth/urls.py
+++ admin/common_auth/urls.py
@@ -1,15 +1,15 @@
 from __future__ import absolute_import
 
-from django.conf.urls import url
+from django.conf.urls import re_path
 
 from admin.common_auth import views
 
 app_name = 'admin'
 
 urlpatterns = [
-    url(r'^login/?$', views.LoginView.as_view(), name='login'),
-    url(r'^logout/$', views.logout_user, name='logout'),
-    url(r'^register/$', views.RegisterUser.as_view(), name='register'),
-    url(r'^settings/desk/$', views.DeskUserCreateFormView.as_view(), name='desk'),
-    url(r'^settings/desk/update/$', views.DeskUserUpdateFormView.as_view(), name='desk_update'),
+    re_path(r'^login/?$', views.LoginView.as_view(), name='login'),
+    re_path(r'^logout/$', views.logout_user, name='logout'),
+    re_path(r'^register/$', views.RegisterUser.as_view(), name='register'),
+    re_path(r'^settings/desk/$', views.DeskUserCreateFormView.as_view(), name='desk'),
+    re_path(r'^settings/desk/update/$', views.DeskUserUpdateFormView.as_view(), name='desk_update'),
 ]
--- admin/registration_providers/urls.py
+++ admin/registration_providers/urls.py
@@ -1,20 +1,20 @@
-from django.conf.urls import url
+from django.conf.urls import re_path
 from . import views
 
 app_name = 'admin'
 
 urlpatterns = [
-    url(r'^create/$', views.CreateRegistrationProvider.as_view(), name='create'),
-    url(r'^$', views.RegistrationProviderList.as_view(), name='list'),
-    url(r'^import/$', views.ImportRegistrationProvider.as_view(), name='import'),
-    url(r'^process_custom_taxonomy/$', views.ProcessCustomTaxonomy.as_view(), name='process_custom_taxonomy'),
-    url(r'^(?P<registration_provider_id>[a-z0-9]+)/$', views.RegistrationProviderDetail.as_view(), name='detail'),
-    url(r'^(?P<registration_provider_id>[a-z0-9]+)/delete/$', views.DeleteRegistrationProvider.as_view(), name='delete'),
-    url(r'^(?P<registration_provider_id>[a-z0-9]+)/export/$', views.ExportRegistrationProvider.as_view(), name='export'),
-    url(r'^(?P<registration_provider_id>[a-z0-9]+)/import/$', views.ImportRegistrationProvider.as_view(), name='import'),
-    url(r'^(?P<registration_provider_id>[a-z0-9]+)/schemas/$', views.ChangeSchema.as_view(), name='schemas'),
-    url(r'^(?P<registration_provider_id>[a-z0-9]+)/cannot_delete/$', views.CannotDeleteProvider.as_view(), name='cannot_delete'),
-    url(r'^(?P<registration_provider_id>[a-z0-9]+)/share_source/$', views.ShareSourceRegistrationProvider.as_view(), name='share_source'),
-    url(r'^(?P<registration_provider_id>[a-z0-9]+)/remove_admins_and_moderators/$', views.RemoveAdminsAndModerators.as_view(), name='remove_admins_and_moderators'),
-    url(r'^(?P<registration_provider_id>[a-z0-9]+)/add_admin_or_moderator/$', views.AddAdminOrModerator.as_view(), name='add_admin_or_moderator'),
+    re_path(r'^create/$', views.CreateRegistrationProvider.as_view(), name='create'),
+    re_path(r'^$', views.RegistrationProviderList.as_view(), name='list'),
+    re_path(r'^import/$', views.ImportRegistrationProvider.as_view(), name='import'),
+    re_path(r'^process_custom_taxonomy/$', views.ProcessCustomTaxonomy.as_view(), name='process_custom_taxonomy'),
+    re_path(r'^(?P<registration_provider_id>[a-z0-9]+)/$', views.RegistrationProviderDetail.as_view(), name='detail'),
+    re_path(r'^(?P<registration_provider_id>[a-z0-9]+)/delete/$', views.DeleteRegistrationProvider.as_view(), name='delete'),
+    re_path(r'^(?P<registration_provider_id>[a-z0-9]+)/export/$', views.ExportRegistrationProvider.as_view(), name='export'),
+    re_path(r'^(?P<registration_provider_id>[a-z0-9]+)/import/$', views.ImportRegistrationProvider.as_view(), name='import'),
+    re_path(r'^(?P<registration_provider_id>[a-z0-9]+)/schemas/$', views.ChangeSchema.as_view(), name='schemas'),
+    re_path(r'^(?P<registration_provider_id>[a-z0-9]+)/cannot_delete/$', views.CannotDeleteProvider.as_view(), name='cannot_delete'),
+    re_path(r'^(?P<registration_provider_id>[a-z0-9]+)/share_source/$', views.ShareSourceRegistrationProvider.as_view(), name='share_source'),
+    re_path(r'^(?P<registration_provider_id>[a-z0-9]+)/remove_admins_and_moderators/$', views.RemoveAdminsAndModerators.as_view(), name='remove_admins_and_moderators'),
+    re_path(r'^(?P<registration_provider_id>[a-z0-9]+)/add_admin_or_moderator/$', views.AddAdminOrModerator.as_view(), name='add_admin_or_moderator'),
 ]
