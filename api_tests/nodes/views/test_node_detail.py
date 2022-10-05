d4fdf89a6f195533a723ec2a97d1f9690c7ec637
Longze Chen <cslzchen@gmail.com>
2022-10-05 13:18:14 -0400

    Revert "[Temp Commit] Skip an assertion on linked_by_nodes in tests"
    This reverts commit 5d9769a3d97108ef43066bcc4d117f0e942c5afd.

--- api_tests/nodes/views/test_node_detail.py
+++ api_tests/nodes/views/test_node_detail.py
@@ -496,8 +496,7 @@ class TestNodeDetail:
         res = app.get(url)
         assert 'count' in res.json['data']['relationships']['linked_by_nodes']['links']['related']['meta']
         assert 'count' in res.json['data']['relationships']['linked_by_registrations']['links']['related']['meta']
-        # Skipped for now due to an unknown bug after django upgrade
-        # assert res.json['data']['relationships']['linked_by_nodes']['links']['related']['meta']['count'] == 0
+        assert res.json['data']['relationships']['linked_by_nodes']['links']['related']['meta']['count'] == 0
         assert res.json['data']['relationships']['linked_by_registrations']['links']['related']['meta']['count'] == 0
 
     def test_node_shows_correct_forks_count_including_private_forks(self, app, user, project_private, url_private, user_two):
