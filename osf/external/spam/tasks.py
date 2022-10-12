b7e109836f7b5417af70287594fba7f8c6ce24c6
Yuhuai Liu <yuhuai@cos.io>
2022-10-12 01:27:27 -0400

    CR follow up

--- osf/external/spam/tasks.py
+++ osf/external/spam/tasks.py
@@ -9,7 +9,6 @@ DOMAIN_REGEX = re.compile(r'(?P<protocol>\w+://)?(?P<www>www\.)?(?P<domain>[\w-]
 def reclassify_domain_references(notable_domain_id):
     from osf.models.notable_domain import DomainReference, NotableDomain
     from osf.models.spam import SpamStatus
-    spammy_domains_set = set(NotableDomain.objects.filter(note=NotableDomain.Note.EXCLUDE_FROM_ACCOUNT_CREATION_AND_CONTENT).values_list('domain', flat=True))
     domain = NotableDomain.load(notable_domain_id)
     references = DomainReference.objects.filter(domain=domain)
     with transaction.atomic():
@@ -20,7 +19,7 @@ def reclassify_domain_references(notable_domain_id):
             elif domain.note == NotableDomain.Note.UNKNOWN or domain.note == NotableDomain.Note.IGNORED:
                 if item.referrer.spam_status == SpamStatus.SPAM:
                     item.referrer.spam_data['domains'].remove(domain.domain)
-                    if set(item.referrer.spam_data['domains']).isdisjoint(spammy_domains_set):
+                    if len(item.referrer.spam_data['domains']) == 0:
                         item.referrer.unspam(save=False)
             item.save()
             item.referrer.save()
@@ -37,12 +36,11 @@ def check_resource_for_domains(guid, content):
         domain, _ = NotableDomain.objects.get_or_create(domain=domain)
         if domain.note == NotableDomain.Note.EXCLUDE_FROM_ACCOUNT_CREATION_AND_CONTENT:
             spammy_domains.append(domain.domain)
-            mark_spam = True
         DomainReference.objects.get_or_create(
             domain=domain,
             referrer_object_id=resource.id,
             referrer_content_type=referrer_content_type,
             defaults={'is_triaged': domain.note != NotableDomain.Note.UNKNOWN}
         )
-    if mark_spam:
+    if spammy_domains:
         resource.confirm_spam(save=True, domains=list(spammy_domains))
