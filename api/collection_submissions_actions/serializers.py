b82272351d8b7e343f8c078c0cc3f18765ebdb5d
John Tordoff <>
2022-10-28 12:32:21 -0400

    remove unnecessary permissions from CollectionSubmissionActionSerializer

--- api/collection_submissions_actions/serializers.py
+++ api/collection_submissions_actions/serializers.py
@@ -1,5 +1,4 @@
 from api.actions.serializers import BaseActionSerializer
-from osf.utils import permissions
 from osf.utils.workflows import CollectionSubmissionsTriggers
 from rest_framework import serializers as ser
 from api.actions.serializers import TargetRelationshipField
@@ -11,8 +10,6 @@ class CollectionSubmissionActionSerializer(BaseActionSerializer):
     class Meta:
         type_ = 'collection-submissions-actions'
 
-    permissions = ser.ChoiceField(choices=permissions.API_CONTRIBUTOR_PROVIDER_PERMISSIONS, required=False)
-    visible = ser.BooleanField(default=True, required=False)
     trigger = ser.ChoiceField(choices=CollectionSubmissionsTriggers.char_field_choices())
     from_state = ser.ChoiceField(choices=ApprovalStates.char_field_choices(), read_only=True)
     to_state = ser.ChoiceField(choices=ApprovalStates.char_field_choices(), read_only=True)
