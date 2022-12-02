30c60af06cee251672b72c58fbe4a809f16419f4
John Tordoff <>
2022-12-02 09:40:32 -0500

    more logic clean for semi-moderated flow

--- osf/models/collection_submission.py
+++ osf/models/collection_submission.py
@@ -137,7 +137,7 @@ class CollectionSubmission(TaxonomizableMixin, BaseModel):
                 osf_contact_email=settings.OSF_CONTACT_EMAIL,
             )
 
-    def _validate_remove(self, event_data):
+    def _validate_moderated_remove(self, event_data):
         user = event_data.kwargs['user']
         force = event_data.kwargs.get('force')
         if force:
@@ -148,12 +148,24 @@ class CollectionSubmission(TaxonomizableMixin, BaseModel):
 
         is_admin = self.guid.referent.has_permission(user, ADMIN)
         is_moderator = user.has_perm('withdraw_submissions', self.collection.provider)
-        if self.is_moderated or self.is_semi_moderated:
-            if not is_moderator and not is_admin:
-                raise PermissionsError(f'{user} must have moderator or admin permissions.')
+        if not is_moderator and not is_admin:
+            raise PermissionsError(f'{user} must have moderator or admin permissions.')
         else:
             if not is_admin:
                 raise PermissionsError(f'{user} must have moderator or admin permissions.')
+
+    def _validate_unmoderated_remove(self, event_data):
+        user = event_data.kwargs['user']
+        force = event_data.kwargs.get('force')
+        if force:
+            return
+
+        if user is None:
+            raise PermissionsError(f'{user} must have moderator or admin permissions.')
+
+        is_admin = self.guid.referent.has_permission(user, ADMIN)
+        if not is_admin:
+            raise PermissionsError(f'{user} must have moderator or admin permissions.')
 
     def _notify_removed(self, event_data):
         force = event_data.kwargs.get('force')
