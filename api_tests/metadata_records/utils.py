409f0687edd608086af85f70646c022ad225aa6d
Abram Booth <boothaa@gmail.com>
2023-01-19 08:19:31 -0500

    fix: consistent guid metadata validation

--- api_tests/metadata_records/utils.py
+++ api_tests/metadata_records/utils.py
@@ -6,42 +6,28 @@ class ExpectedMetadataRecord:
         if attrname.startswith('_'):
             super().__setattr__(attrname, attrvalue)
         else:
-            self._expected_values[attrname] = self._freeze(attrvalue)
+            self._expected_values[attrname] = attrvalue
+
+    def __getattr__(self, attrname):
+        return self._expected_values[attrname]
 
     def assert_expectations(self, db_record, api_record):
         db_record.refresh_from_db()
         for attrname, expected_value in self._expected_values.items():
             actual_db_value = self._getattr_dbrecord(attrname, db_record)
-            actual_api_value = self._getattr_apirecord(attrname, api_record)
             assert actual_db_value == expected_value
-            assert actual_api_value == expected_value
+            if api_record is not None:
+                actual_api_value = self._getattr_apirecord(attrname, api_record)
+                assert actual_api_value == expected_value
 
     def _getattr_dbrecord(self, attrname, dbrecord):
         if attrname == 'id':
             return dbrecord.guid._id
-        return self._freeze(getattr(dbrecord, attrname))
+        return getattr(dbrecord, attrname)
 
     def _getattr_apirecord(self, attrname, apirecord):
         if attrname == 'id':
             return apirecord['id']
         if attrname == 'funding_info':
             attrname = 'funders'
-        return self._freeze(apirecord['attributes'][attrname])
-
-    def _freeze(self, unfrozen):
-        frozen = None
-        if isinstance(unfrozen, dict):
-            frozen = frozenset(
-                (k, self._freeze(v))
-                for k, v in unfrozen.items()
-            )
-        elif isinstance(unfrozen, list):
-            frozen = frozenset(
-                self._freeze(v)
-                for v in unfrozen
-            )
-
-        if frozen is None:
-            return unfrozen
-        assert len(frozen) == len(unfrozen), 'unexpected duplicates!'
-        return frozen
+        return apirecord['attributes'][attrname]
