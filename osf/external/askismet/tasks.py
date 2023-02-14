da0082084438722317285796f35bfa5bf53adf19
John Tordoff <>
2023-02-14 08:31:19 -0500

    remove old akismet task

--- osf/external/askismet/tasks.py
+++ osf/external/askismet/tasks.py
@@ -4,44 +4,6 @@ from osf.external.askismet.client import AkismetClient
 
 
 @celery_app.task()
-def check_resource_with_akismet(guid, content, author, author_email, request_kwargs):
-    from osf.models import Guid
-    resource = Guid.load(guid).referent
-
-    client = AkismetClient()
-
-    is_spam, pro_tip = client.check_content(
-        user_ip=request_kwargs.get('remote_addr'),
-        user_agent=request_kwargs.get('user_agent'),
-        referrer=request_kwargs.get('referer'),
-        comment_content=content,
-        comment_author=author,
-        comment_author_email=author_email
-    )
-
-    resource.spam_pro_tip = pro_tip
-    resource.spam_data['headers'] = {
-        'Remote-Addr': request_kwargs.get('remote_addr'),
-        'User-Agent': request_kwargs.get('user_agent'),
-        'Referer': request_kwargs.get('referer'),
-    }
-    resource.spam_data['content'] = content
-    resource.spam_data['author'] = author
-    resource.spam_data['author_email'] = author_email
-
-    if is_spam:
-        resource.flag_spam()
-        if not resource.spam_data.get('who_flagged'):
-            resource.spam_data['who_flagged'] = 'akismet'
-        elif resource.spam_data['who_flagged'] == 'oopspam':
-            resource.spam_data['who_flagged'] = 'both'
-
-    resource.save()
-
-    return is_spam
-
-
-@celery_app.task()
 def submit_spam(guid):
     from osf.models import Guid
     resource = Guid.load(guid).referent
