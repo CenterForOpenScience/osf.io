38ba47ca32eedd07921afc278bd053eacd152ef1
Yuhuai Liu <yuhuai@cos.io>
2023-02-14 16:53:33 -0500

    add UserSessions model

--- osf/models/session.py
+++ osf/models/session.py
@@ -1,6 +1,7 @@
 from osf.models.base import BaseModel, ObjectIDMixin
 from osf.utils.datetime_aware_jsonfield import DateTimeAwareJSONField
-
+from django.db import models
+from osf.utils.fields import NonNaiveDateTimeField
 
 class Session(ObjectIDMixin, BaseModel):
     data = DateTimeAwareJSONField(default=dict, blank=True)
@@ -12,3 +13,10 @@ class Session(ObjectIDMixin, BaseModel):
     @property
     def is_external_first_login(self):
         return 'auth_user_external_first_login' in self.data
+    
+class UserSessions(BaseModel):
+    class Meta:
+        unique_together = ('user', 'session_key')
+    user = models.ForeignKey('OSFUser', on_delete=models.CASCADE)
+    session_key = models.CharField(max_length=255)
+    expire_date = NonNaiveDateTimeField()
