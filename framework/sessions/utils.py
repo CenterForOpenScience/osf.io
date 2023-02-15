acd3bda63f7435aafa1cf47b6dbc62a6138bc6ad
Longze Chen <cslzchen@gmail.com>
2023-02-15 15:46:37 -0500

    pair: rework session get and create [skip ci]

--- framework/sessions/utils.py
+++ framework/sessions/utils.py
@@ -1,4 +1,6 @@
-from datetime import datetime
+from importlib import import_module
+
+from django.conf import settings as django_conf_settings
 
 
 def remove_sessions_for_user(user):
@@ -9,22 +11,10 @@ def remove_sessions_for_user(user):
     :return:
     """
 
-    # Note 1: the query is very expensive since the auth_user_id needs to be decoded
-    # Note 2: this needs to be async
-    # Note 3: it only works for DB backend
-    # Note 4: a better solution is to have a separate table map user and their sessions
-    # Note 5: an even better solution is to extend Django session and add user id as a field, see
-    #         https://docs.djangoproject.com/en/3.2/topics/http/sessions/#extending-database-backed-session-engines
-    from django.contrib.sessions.models import Session
-    if user._id:
-        # find all unexpired sessions
-        session_keys = []
-        sessions = Session.objects.filter(expire_date__gte=datetime.now())
-        # for each, decode and return a queryset of all whose session_data['auth_user_id'] is user_id
-        for session in sessions:
-            if user._id == session.get_decoded().get('auth_user_id'):
-                session_keys.append(session.session_key)
-        Session.objects.filter(pk__in=session_keys).delete()
+    from osf.models import UserSessionMap
+    # SessionStore = import_module(django_conf_settings.SESSION_ENGINE).SessionStore
+    # session_keys = UserSessionMap.objects.filter(user__id=user.id).values_list('session_key', flat=True)
+    pass
 
 
 def remove_session(session):
