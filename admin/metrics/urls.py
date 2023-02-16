1d29438dd474e4e3731adcc611f600eac4e7686f
Fitz Elliott <fitz@cos.io>
2023-02-16 00:50:38 -0500

    update metrics page to pull from local metrics
     * Replace the extant metrics pages with the alternative version that
       pulls from es6.

--- admin/metrics/urls.py
+++ admin/metrics/urls.py
@@ -7,5 +7,4 @@ app_name = 'admin'
 
 urlpatterns = [
     re_path(r'^$', login(views.MetricsView.as_view()), name='metrics'),
-    re_path(r'^mw$', login(views.MetricsView2.as_view()), name='metrics-mw'),
 ]
--- admin/metrics/views.py
+++ admin/metrics/views.py
@@ -11,18 +11,7 @@ class MetricsView(PermissionRequiredMixin, TemplateView):
     raise_exception = True
 
     def get_context_data(self, **kwargs):
-
-        kwargs.update(KEEN_CREDENTIALS.copy())
-        return super(MetricsView, self).get_context_data(**kwargs)
-
-
-class MetricsView2(PermissionRequiredMixin, TemplateView):
-    template_name = 'metrics/osf_metrics-mw.html'
-    permission_required = 'osf.view_metrics'
-    raise_exception = True
-
-    def get_context_data(self, **kwargs):
         kwargs.update(KEEN_CREDENTIALS.copy())
         api_report_url = '{}_/metrics/reports/'.format(settings.API_DOMAIN)
         kwargs.update({'metrics_url': api_report_url})
-        return super(MetricsView2, self).get_context_data(**kwargs)
+        return super(MetricsView, self).get_context_data(**kwargs)
