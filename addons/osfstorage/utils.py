084c5a32746c4c2025a1abcf0c71b16472065dbd
Yuhuai Liu <yuhuai@cos.io>
2023-02-28 14:38:24 -0500

    change session usage in addons/

--- addons/osfstorage/utils.py
+++ addons/osfstorage/utils.py
@@ -20,20 +20,15 @@ logger = logging.getLogger(__name__)
 LOCATION_KEYS = ['service', settings.WATERBUTLER_RESOURCE, 'object']
 
 def enqueue_update_analytics(node, file, version_idx, action='download'):
-    enqueue_postcommit_task(update_analytics_async, (node._id, file._id, version_idx, session._id, session.data, action), {}, celery=True)
+    enqueue_postcommit_task(update_analytics_async, (node._id, file._id, version_idx, session.session_key, action), {}, celery=True)
 
 @app.task(max_retries=5, default_retry_delay=60)
-def update_analytics_async(node_id, file_id, version_idx, session_id=None, session_data=None, action='download'):
-    if not session_data:
-        session_data = {}
+def update_analytics_async(node_id, file_id, version_idx, session_key=None, action='download'):
     node = Guid.load(node_id).referent
     file = BaseFileNode.load(file_id)
-    session_obj = Session.load(session_id)
-    if not session_obj:
-        session_obj = Session(data=session_data)
-    update_analytics(node, file, version_idx, session_obj, action)
+    update_analytics(node, file, version_idx, session_key, action)
 
-def update_analytics(node, file, version_idx, session_obj, action='download'):
+def update_analytics(node, file, version_idx, session_key, action='download'):
     """
     :param Node node: Root node to update
     :param str file_id: The _id field of a filenode
@@ -53,8 +48,8 @@ def update_analytics(node, file, version_idx, session_obj, action='download'):
     }
     resource = node.guids.first()
 
-    update_counter(resource, file, version=None, action=action, node_info=node_info, session_obj=session_obj)
-    update_counter(resource, file, version_idx, action, node_info=node_info, session_obj=session_obj)
+    update_counter(resource, file, version=None, action=action, node_info=node_info, session_key=session_key)
+    update_counter(resource, file, version_idx, action, node_info=node_info, session_key=session_key)
 
 
 def serialize_revision(node, record, version, index, anon=False):
