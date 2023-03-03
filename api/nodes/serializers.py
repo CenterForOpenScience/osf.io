bec0b0c9289ef8ee6505c1c40577e9e4dcc5f704
Jon Walz <walzj@COSs-MBP.lan>
2023-03-03 14:45:59 -0500

    Re-implement improved Node/Registration anonymization from PR 10009

--- api/nodes/serializers.py
+++ api/nodes/serializers.py
@@ -251,8 +251,6 @@ class NodeSerializer(TaxonomizableSerializerMixin, JSONAPISerializer):
     # list if it doesn't expose user data
     non_anonymized_fields = [
         'access_requests_enabled',
-        'affiliated_institutions',
-        'affiliate_user_institutions',
         'analytics_key',
         'category',
         'children',
@@ -269,7 +267,6 @@ class NodeSerializer(TaxonomizableSerializerMixin, JSONAPISerializer):
         'fork',
         'forked_from',
         'id',
-        'identifiers',
         'license',
         'linked_by_nodes',
         'linked_by_registrations',
