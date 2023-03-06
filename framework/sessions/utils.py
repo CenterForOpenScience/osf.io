bd5c4dcbbc483856dca558677eee52494f3138bc
Longze Chen <cslzchen@gmail.com>
2023-03-06 13:32:16 -0500

    CR update: framework/sessions/utils.py

--- framework/sessions/utils.py
+++ framework/sessions/utils.py
@@ -2,16 +2,12 @@ from importlib import import_module
 
 from django.conf import settings as django_conf_settings
 
+SessionStore = import_module(django_conf_settings.SESSION_ENGINE).SessionStore
+
 
 def remove_sessions_for_user(user):
-    """
-    Permanently remove all stored sessions for the user from the DB.
-
-    :param user: User
-    :return:
-    """
+    """Permanently remove all stored sessions for the given user from DB and/or Cache."""
     from osf.models import UserSessionMap
-    SessionStore = import_module(django_conf_settings.SESSION_ENGINE).SessionStore
     session_keys = UserSessionMap.objects.filter(user__id=user.id).values_list('session_key', flat=True)
     for key in session_keys:
         session = SessionStore(session_key=key)
@@ -19,11 +15,6 @@ def remove_sessions_for_user(user):
 
 
 def remove_session(session):
-    """
-    Remove a session from database
-
-    :param session: Session
-    :return:
-    """
+    """Remove a given session from DB and/or Cache."""
     if session:
         session.flush()
