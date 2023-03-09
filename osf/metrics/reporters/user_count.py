30d256bff6380b64a175e4088b5866091e13af79
Abram Booth <boothaa@gmail.com>
2023-03-09 08:17:40 -0500

    update user-count reporter to institution remodel

--- osf/metrics/reporters/user_count.py
+++ osf/metrics/reporters/user_count.py
@@ -13,7 +13,7 @@ class UserCountReporter(DailyReporter):
             deactivated=OSFUser.objects.filter(date_disabled__isnull=False, date_disabled__date__lte=report_date).count(),
             merged=OSFUser.objects.filter(date_registered__date__lte=report_date, merged_by__isnull=False).count(),
             new_users_daily=OSFUser.objects.filter(is_active=True, date_confirmed__date=report_date).count(),
-            new_users_with_institution_daily=OSFUser.objects.filter(is_active=True, date_confirmed__date=report_date, affiliated_institutions__isnull=False).count(),
+            new_users_with_institution_daily=OSFUser.objects.filter(is_active=True, date_confirmed__date=report_date, institutionaffiliation__isnull=False).count(),
             unconfirmed=OSFUser.objects.filter(date_registered__date__lte=report_date, date_confirmed__isnull=True).count(),
         )
 
