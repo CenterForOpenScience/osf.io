46f917a36f650094c1d479686356e964d625fa85
soum-fujimoto <fujimoto@soum.co.jp>
2023-03-22 08:03:55 +0000

    Adjust token refresh timing to avoid race condition occure on update the token.

--- addons/dropbox/settings/defaults.py
+++ addons/dropbox/settings/defaults.py
@@ -5,7 +5,8 @@ DROPBOX_SECRET = None
 DROPBOX_AUTH_CSRF_TOKEN = 'dropbox-auth-csrf-token'
 DROPBOX_OAUTH_AUTH_ENDPOINT = 'https://www.dropbox.com/oauth2/authorize'
 DROPBOX_OAUTH_TOKEN_ENDPOINT = 'https://www.dropbox.com/oauth2/token'
-REFRESH_TIME = 14399  # 4 hours
+# Token expires in 4 hours, No refresh at least 2 hours to avoid race condition
+REFRESH_TIME = 7199  # 2 hours
 
 # Max file size permitted by frontend in megabytes
 MAX_UPLOAD_SIZE = 5 * 1024
