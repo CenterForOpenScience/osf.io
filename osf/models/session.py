3b30568accce9ae846c2da8633fd15655c907311
Longze Chen <cslzchen@gmail.com>
2023-05-08 15:28:23 -0400

    Redo migrations

--- osf/models/session.py
+++ osf/models/session.py
@@ -1,21 +1,7 @@
-
 from django.db import models
 
-from osf.models.base import BaseModel, ObjectIDMixin
-from osf.utils.datetime_aware_jsonfield import DateTimeAwareJSONField
+from osf.models.base import BaseModel
 from osf.utils.fields import NonNaiveDateTimeField
-
-
-class Session(ObjectIDMixin, BaseModel):
-    data = DateTimeAwareJSONField(default=dict, blank=True)
-
-    @property
-    def is_authenticated(self):
-        return 'auth_user_id' in self.data
-
-    @property
-    def is_external_first_login(self):
-        return 'auth_user_external_first_login' in self.data
 
 
 class UserSessionMap(BaseModel):
