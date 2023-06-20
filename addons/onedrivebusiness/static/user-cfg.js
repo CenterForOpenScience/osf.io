af2b91e8abcab5fbe90a184331455f6c64359d3e
hide_24 <hidetoshi.yoshimoto@gmail.com>
2023-06-20 14:28:37 +0900

    Merge pull request #408 from yacchin1205/fix/onedrivebusiness-template-name
    [GRDM-37474] OneDrive for Office365アドオンを有効化した場合のユーザーアドオンアカウント構成画面の修正

--- addons/onedrivebusiness/static/user-cfg.js
+++ addons/onedrivebusiness/static/user-cfg.js
@@ -1,5 +1,4 @@
 'use strict';
 
-var OneDriveBusinessUserConfig = require('./onedrivebusinessUserConfig.js').OneDriveBusinessUserConfig;
-var url = '/api/v1/settings/onedrivebusiness/accounts/';
-new OneDriveBusinessUserConfig('#onedrivebusinessAddonScope', url);
+const OneDriveBusinessUserConfig = require('./onedrivebusinessUserConfig.js').OneDriveBusinessUserConfig;
+new OneDriveBusinessUserConfig('#onedrivebusinessAddonScope');
