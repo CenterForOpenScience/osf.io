96e4c20e46af8c6529b07430137544eae40b8260
Longze Chen <cslzchen@gmail.com>
2023-10-12 17:48:55 -0400

    Fix things caused by resolving merge conflicts

--- addons/boa/settings/__init__.py
+++ addons/boa/settings/__init__.py
@@ -1,9 +1,10 @@
 import logging
-from .defaults import *  # noqa
+
+from addons.boa.settings.defaults import *  # noqa
 
 logger = logging.getLogger(__name__)
 
 try:
-    from .local import *  # noqa
+    from addons.boa.settings.local import *  # noqa
 except ImportError:
     logger.warn('No local.py settings file found')
