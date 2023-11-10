82df759bce282e83f827d8c6572eb649832312f8
Fitz Elliott <fitz@cos.io>
2023-11-10 13:32:57 -0500

    d'oh! stop importing removed setting

--- addons/boa/apps.py
+++ addons/boa/apps.py
@@ -1,7 +1,6 @@
 import os
 
 from addons.base.apps import BaseAddonAppConfig
-from addons.boa.settings import MAX_UPLOAD_SIZE
 
 HERE = os.path.dirname(os.path.abspath(__file__))
 TEMPLATE_PATH = os.path.join(HERE, 'templates')
