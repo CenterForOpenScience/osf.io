82edf2fc541b323207aad3abfda1e30717ee9710
Longze Chen <cslzchen@gmail.com>
2023-12-08 12:08:42 -0500

    Update cedarmetadatatemplate model and redo migration

--- osf/models/cedar_metadata.py
+++ osf/models/cedar_metadata.py
@@ -1,10 +1,11 @@
 from django.db import models
+
+from osf.models.base import BaseModel, ObjectIDMixin
 from osf.utils.datetime_aware_jsonfield import DateTimeAwareJSONField
-from .base import BaseModel, ObjectIDMixin
 
 
 class CedarMetadataTemplate(ObjectIDMixin, BaseModel):
-    title = models.CharField(max_length=255, default=None)
+    schema_name = models.CharField(max_length=255, default=None)
     cedar_id = models.CharField(max_length=255, default=None)
     template = DateTimeAwareJSONField(default=dict)
     active = models.BooleanField(default=True)
@@ -14,4 +15,4 @@ class CedarMetadataTemplate(ObjectIDMixin, BaseModel):
         unique_together = ('cedar_id', 'template_version')
 
     def __unicode__(self):
-        return f'({self.title}, version {self.tempate_version})'
+        return f'(name=[{self.schema_name}], version=[{self.template_version}], id=[{self.cedar_id}])'
