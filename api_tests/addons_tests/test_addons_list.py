182a2257d1d1b42fdd11281bc3d7ef794c237dda
John Tordoff <>
2024-01-03 09:52:55 -0500

    fix `category` attribute and tweak serialization

--- api_tests/addons_tests/test_addons_list.py
+++ api_tests/addons_tests/test_addons_list.py
@@ -8,14 +8,14 @@ class TestAddonsList:
 
     def test_filter_by_category(self, app):
         url = '/{}addons/'.format(API_BASE)
-        url_storage = '{}?filter[categories]=storage'.format(url)
-        url_citations = '{}?filter[categories]=citations'.format(url)
+        url_storage = '{}?filter[category]=storage'.format(url)
+        url_citations = '{}?filter[category]=citations'.format(url)
 
         data_storage = app.get(url_storage).json['data']
         data_citations = app.get(url_citations).json['data']
 
         for addon in data_storage:
-            assert 'storage' in addon['attributes']['categories']
+            assert 'storage' in addon['attributes']['category']
 
         for addon in data_citations:
-            assert 'citations' in addon['attributes']['categories']
+            assert 'citations' in addon['attributes']['category']
