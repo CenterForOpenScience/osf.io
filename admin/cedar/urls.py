ead4aa798ed3b5a987aceb621cb107474385dfdc
Yuhuai Liu <yuhuai@cos.io>
2024-01-03 01:42:52 -0500

    cedar template admin configuration feature

---
+++ admin/cedar/urls.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+
+from django.conf.urls import re_path
+
+from admin.cedar import views
+
+app_name = 'admin'
+
+urlpatterns = [
+    re_path(r'^$', views.CedarMetadataTemplateListView.as_view(), name='list'),
+    re_path(r'^(?P<id>[0-9_]+)/$', views.CedarMetadataTemplateDetailView.as_view(),
+        name='detail'),
+]
