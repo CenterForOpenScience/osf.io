f74ad2c4213e54b00c73630940ffd04f0b910076
Fitz Elliott <fitz@cos.io>
2024-03-06 22:46:22 -0500

    fix two changed behaviors in jwt library
     * jwt exception DecodeError is now InvalidSignatureError
    
     * `'require_exp': True` option is now `'require': ['exp']`

--- addons/base/views.py
+++ addons/base/views.py
@@ -274,7 +274,7 @@ def get_auth(auth, **kwargs):
         data = jwt.decode(
             jwe.decrypt(request.args.get('payload', '').encode('utf-8'), WATERBUTLER_JWE_KEY),
             settings.WATERBUTLER_JWT_SECRET,
-            options={'require_exp': True},
+            options={'require': ['exp']},
             algorithms=[settings.WATERBUTLER_JWT_ALGORITHM],
         )['data']
     except (jwt.InvalidTokenError, KeyError) as err:
