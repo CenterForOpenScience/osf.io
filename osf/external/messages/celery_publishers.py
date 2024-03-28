dcbc57ae151083e6355a43574498240447c51e53
John Tordoff <>
2024-03-28 15:45:19 -0400

    add waffling for celery messaging queue, add tests

--- osf/external/messages/celery_publishers.py
+++ osf/external/messages/celery_publishers.py
@@ -1,44 +1,42 @@
+import waffle
 from kombu import Exchange
 from framework.celery_tasks import app as celery_app
-from framework.celery_tasks.handlers import enqueue_task
+from website import settings
+from osf import features
 
 
 def publish_deactivated_user(user):
-    enqueue_task(
-        _publish_user_status_change.s(
+    if settings.USE_CELERY and waffle.switch_is_active(features.ENABLE_GV):
+        _publish_user_status_change(
             body={
                 'action': 'deactivate',
                 'user_uri': user.get_semantic_iri(),
-            },
+            }
         )
-    )
 
 
 def publish_reactivate_user(user):
-    enqueue_task(
-        _publish_user_status_change.s(
+    if settings.USE_CELERY and waffle.switch_is_active(features.ENABLE_GV):
+        _publish_user_status_change(
             body={
                 'action': 'reactivate',
                 'user_uri': user.get_semantic_iri(),
             },
         )
-    )
 
 
 def publish_merged_user(user):
     assert user.merged_by, 'User received merge signal, but has no `merged_by` reference.'
-    enqueue_task(
-        _publish_user_status_change.s(
+    if settings.USE_CELERY and waffle.switch_is_active(features.ENABLE_GV):
+        _publish_user_status_change(
             body={
                 'action': 'merge',
                 'into_user_uri': user.merged_by.get_semantic_iri(),
                 'from_user_uri': user.get_semantic_iri(),
             },
         )
-    )
 
 
-@celery_app.task()
 def _publish_user_status_change(body: dict):
     with celery_app.producer_pool.acquire() as producer:
         producer.publish(
