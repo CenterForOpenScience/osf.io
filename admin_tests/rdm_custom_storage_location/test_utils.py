ec996ea05a7752bd4af71de4f4e81927971985c7
soum-fujimoto <fujimoto@soum.co.jp>
2024-04-03 10:06:46 +0000

    Remove Box, Google Drive, Nextcloud, owncloud from Institutional Storage

--- admin_tests/rdm_custom_storage_location/test_utils.py
+++ admin_tests/rdm_custom_storage_location/test_utils.py
@@ -11,16 +11,12 @@ class TestUtils:
         assert provider_list
         provider_list_short_name = [p.short_name for p in provider_list]
         nt.assert_in('s3', provider_list_short_name, 's3')
-        nt.assert_in('box', provider_list_short_name, 'box')
         nt.assert_in('dropboxbusiness', provider_list_short_name, 'dropboxbusiness')
-        nt.assert_in('googledrive', provider_list_short_name, 'googledrive')
-        nt.assert_in('nextcloud', provider_list_short_name, 'nextcloud')
         nt.assert_in('nextcloudinstitutions', provider_list_short_name, 'nextcloudinstitutions')
         nt.assert_in('osfstorage', provider_list_short_name, 'osfstorage')
         nt.assert_in('onedrivebusiness', provider_list_short_name, 'onedrivebusiness')
         nt.assert_in('swift', provider_list_short_name, 'swift')
         nt.assert_in('ociinstitutions', provider_list_short_name, 'ociinstitutions')
-        nt.assert_in('owncloud', provider_list_short_name, 'owncloud')
         nt.assert_in('s3compat', provider_list_short_name, 's3compat')
         nt.assert_in('s3compatinstitutions', provider_list_short_name, 's3compatinstitutions')
 
