a23cf785384a9c83289f54d967635e9f49375854
John Tordoff <>
2024-05-21 12:26:46 -0400

    pass basic auth and cookie for v1 route tests

--- tests/test_csrf.py
+++ tests/test_csrf.py
@@ -1,5 +1,5 @@
 from tests.base import OsfTestCase
-from osf_tests.factories import UserFactory
+from osf_tests.factories import AuthUserFactory
 from django.conf import settings as api_settings
 from website import settings
 
@@ -7,10 +7,10 @@ from website import settings
 class TestCSRF(OsfTestCase):
 
     def test_csrf_cookie_gets_set_on_authd_request(self):
-        user = UserFactory()
+        user = AuthUserFactory()
         # use session auth
         session_cookie = user.get_or_create_cookie()
         self.app.set_cookie(settings.COOKIE_NAME, session_cookie.decode())
-        res = self.app.get('/settings/')
+        res = self.app.get('/settings/', auth=user.auth)
         assert res.status_code == 200
         assert api_settings.CSRF_COOKIE_NAME in self.app.cookies
