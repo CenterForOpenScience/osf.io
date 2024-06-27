b77d2dff7f054389c05ec1946ddc095b7923f6c7
John Tordoff <>
2024-06-27 13:02:15 -0400

    make code review fixes

--- osf/models/mixins.py
+++ osf/models/mixins.py
@@ -297,11 +297,7 @@ class AffiliatedInstitutionMixin(models.Model):
 
     affiliated_institutions = models.ManyToManyField('Institution', related_name='nodes')
 
-    def add_affiliated_institution(self, inst, user, save=False, log=True):
-        from .institution import Institution
-        if not isinstance(inst, Institution):
-            raise ValidationValueError(f'Institution not provided')
-
+    def add_affiliated_institution(self, inst, user, log=True):
         if not user.is_affiliated_with_institution(inst):
             raise UserNotAffiliatedError(f'User is not affiliated with {inst}')
 
@@ -319,13 +315,6 @@ class AffiliatedInstitutionMixin(models.Model):
                 params=params,
                 auth=Auth(user)
             )
-
-        if save:
-            self.save()
-
-            self.update_search()
-            return True
-        return False
 
     def remove_affiliated_institution(self, inst, user, save=False, log=True):
         if self.is_affiliated_with_institution(inst):
@@ -346,41 +335,6 @@ class AffiliatedInstitutionMixin(models.Model):
             self.update_search()
             return True
         return False
-
-    def update_institutional_affiliation(self, auth, institution_ids):
-        user = auth.user
-        current_institutions = set(self.affiliated_institutions.values_list('_id', flat=True))
-
-        institutions_to_add = set(institution_ids) - current_institutions
-        institutions_to_remove = current_institutions - set(institution_ids)
-
-        Institution = apps.get_model('osf.Institution')
-
-        from osf.exceptions import UserNotAffiliatedError
-
-        for institution_id in institutions_to_add:
-            try:
-                institution = Institution.objects.get(_id=institution_id)
-                self.add_affiliated_institution(institution, user, log=True)
-            except Institution.DoesNotExist:
-                raise ValidationError(f'User is not affiliated with {institution.name},'
-                                             f' it was not found in records')
-            except UserNotAffiliatedError:
-                raise ValidationError(f'User is not affiliated with {institution.name},')
-
-        for institution_id in institutions_to_remove:
-            try:
-                institution = Institution.objects.get(_id=institution_id)
-                self.remove_affiliated_institution(institution, user, log=True)
-            except Institution.DoesNotExist:
-                raise ValidationError(f'User is not affiliated with {institution.name},'
-                                             f' it was not found in records')
-            except UserNotAffiliatedError:
-                raise ValidationError(f'User is not affiliated with {institution.name},')
-
-        self.save()
-
-        self.update_search()
 
     def is_affiliated_with_institution(self, institution):
         return self.affiliated_institutions.filter(id=institution.id).exists()
--- osf_tests/test_institutional_affiliation.py
+++ osf_tests/test_institutional_affiliation.py
@@ -4,8 +4,7 @@ from osf_tests.factories import (
     UserFactory,
     InstitutionFactory,
 )
-from osf.exceptions import UserNotAffiliatedError, ValidationError
-from framework.auth import Auth
+from osf.exceptions import UserNotAffiliatedError
 
 
 @pytest.mark.django_db
@@ -34,10 +33,6 @@ class TestPreprintInstitutionalAffiliation:
         preprint.add_permission(user, 'admin')
         return preprint
 
-    def test_add_institution_that_does_not_exist(self, preprint, user):
-        with pytest.raises(ValidationError):
-            preprint.add_affiliated_institution(None, user)
-
     def test_remove_nonexistent_affiliation(self, preprint, institution, user):
         assert not preprint.remove_affiliated_institution(institution, user)
 
@@ -54,22 +49,6 @@ class TestPreprintInstitutionalAffiliation:
         was_removed = preprint.remove_affiliated_institution(institution, user)
         assert was_removed
         assert not preprint.is_affiliated_with_institution(institution)
-
-    def test_update_institutional_affiliation(self, preprint, institution, user):
-        new_institution = InstitutionFactory()
-        user.add_or_update_affiliated_institution(new_institution)
-
-        institution_ids = [institution._id, new_institution._id]
-        preprint.update_institutional_affiliation(Auth(user), institution_ids)
-
-        assert preprint.is_affiliated_with_institution(institution)
-        assert preprint.is_affiliated_with_institution(new_institution)
-
-        institution_ids = [new_institution._id]
-        preprint.update_institutional_affiliation(Auth(user), institution_ids)
-
-        assert not preprint.is_affiliated_with_institution(institution)
-        assert preprint.is_affiliated_with_institution(new_institution)
 
     def test_permission_errors_during_affiliation_update(self, preprint, institution, user_without_affiliation):
         with pytest.raises(UserNotAffiliatedError):
