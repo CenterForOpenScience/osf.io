799cb57b7da84ca9efd8680697cc90c6f41004ad
Matt Frazier <maf7sm@virginia.edu>
2024-07-09 18:03:45 -0400

    Fix signature

--- website/identifiers/tasks.py
+++ website/identifiers/tasks.py
@@ -6,7 +6,7 @@ from framework.celery_tasks.handlers import queued_task
 from framework import sentry
 
 @celery_app.task(bind=True, max_retries=5, acks_late=True)
-def task__update_doi_metadata_on_change(target_guid):
+def task__update_doi_metadata_on_change(self, target_guid):
     sentry.log_message('Updating DOI for guid', extra_data={'guid': target_guid}, level=logging.INFO)
     Guid = apps.get_model('osf.Guid')
     target_object = Guid.load(target_guid).referent
