1eeb452f5fdecd2e7a0eb2d12685d1dbe011414b
Jon Walz <walzj@COSs-MBP.lan>
2024-07-12 11:34:19 -0400

    Resolve conflicts

--- api/nodes/views.py
+++ api/nodes/views.py
@@ -1,4 +1,7 @@
 import re
+import typing
+
+import dataclasses
 
 from distutils.version import StrictVersion
 from django.apps import apps
@@ -1471,28 +1474,53 @@ class NodeAddonFolderList(JSONAPIBaseView, generics.ListAPIView, NodeMixin, Addo
             raise HTTP_CODE_MAP.get(exc.code, exc)
 
 
-class NodeStorageProvider(object):
+@dataclasses.dataclass
+class NodeStorageProvider:
 
-    def __init__(self, node, provider_name, storage_addon=None):
-        self.path = '/'
-        self.node = node
-        self.kind = 'folder'
-        self.name = provider_name
-        self.provider = provider_name
-        self.node_id = node._id
-        self.pk = node._id
-        self.id = node.id
-        self.root_folder = self._get_root_folder(storage_addon)
+    resource: typing.Any
+    provider_name: str = None
+    provider_settings: typing.Any = None  # NodeSettings or EphemeralSettings
+    path: str = '/'
+    kind: str = 'folder'
+
+    @property
+    def node(self):
+        return self.resource
 
     @property
     def target(self):
-        return self.node
+        return self.resource
 
-    def _get_root_folder(self, storage_addon):
-        if isinstance(self.target, Preprint):
+    @property
+    def provider(self):
+        return self.provider_name or self.provider_settings.short_name
+
+    @property
+    def name(self):
+        if self.provider_settings:
+            return self.provider_settings.display_name
+        return self.provider_name
+
+    @property
+    def node_id(self):
+        return self.resource._id
+
+    @property
+    def pk(self):
+        return self.resource._id
+
+    @property
+    def id(self):
+        return self.resource.id
+
+    @property
+    def root_folder(self):
+        if isinstance(self.resource, Preprint):
             return self.target.root_folder
-        else:
-            return storage_addon.root_node if storage_addon else None
+        if self.provider_settings:
+            return self.provider_settings.root_node
+        return None
+
 
 class NodeStorageProvidersList(JSONAPIBaseView, generics.ListAPIView, NodeMixin):
     """The documentation for this endpoint can be found [here](https://developer.osf.io/#operation/nodes_providers_list).
