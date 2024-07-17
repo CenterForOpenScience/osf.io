12e28b1a2be470e34f1b53e7d1e0c7447aff7a18
Fitz Elliott <fitz@cos.io>
2024-07-17 14:24:03 -0400

    remove obsolete gevent monkeypatches

--- api/base/wsgi.py
+++ api/base/wsgi.py
@@ -6,15 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
-from website import settings
 from api.base import settings as api_settings
-
-if not settings.DEBUG_MODE:
-    from gevent import monkey
-    monkey.patch_all()
-    # PATCH: avoid deadlock on getaddrinfo, this patch is necessary while waiting for
-    # the final gevent 1.1 release (https://github.com/gevent/gevent/issues/349)
-    #  'foo'.encode('idna')  # noqa
 
 import os  # noqa
 from django.core.wsgi import get_wsgi_application  # noqa
