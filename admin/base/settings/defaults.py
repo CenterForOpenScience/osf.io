cf3f0a014f8ad12c30e9fb8bd9c0f02f0641703f
Oleh Paduchak <opaduchak@exoft.net>
2024-08-01 16:01:55 +0300

    fixed django assets location

--- admin/base/settings/defaults.py
+++ admin/base/settings/defaults.py
@@ -111,6 +111,21 @@ MIGRATION_MODULES = {
 if SECURE_MODE and DEBUG_MODE:
     INSTALLED_APPS += ('sslserver',)
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        }
+    }
+]
 
 MIDDLEWARE = (
     # TokuMX transaction support
