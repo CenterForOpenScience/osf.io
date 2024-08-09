406c027f73e86140c4452868824ae44af6075e13
Fitz Elliott <fitz@cos.io>
2024-08-09 13:00:15 -0400

    Hotfixes for: double-encoded urls + dep upgrades
    Squashed commit of the following:
    
    commit 4f6adcead99386f5be8d58300aa151ea57679e27
    Merge: f3925d325e 4edcf8be77
    Author: Fitz Elliott <fitz@cos.io>
    Date:   Fri Aug 9 12:07:43 2024 -0400
    
        Merge branch 'hotfix/flake-error' into hotfix/pyupgrade-hotfixes-2
    
    commit 4edcf8be77a62c2518fe7f61ccdda8c7870f6ae4
    Author: Fitz Elliott <fitz@cos.io>
    Date:   Fri Aug 9 12:07:11 2024 -0400
    
        flake error - remove unused import
    
    commit f3925d325e41f2837d240166cd850bbe66d372bb
    Merge: f79ea48055 e7b5e4c15d
    Author: Fitz Elliott <fitz@cos.io>
    Date:   Thu Aug 8 01:53:00 2024 -0400
    
        Merge branch 'hotfix/dep-bumps-2' into hotfix/pyupgrade-hotfixes-2
    
    commit e7b5e4c15d5afa35cbb3a8d02386f8cf2bbddc80
    Author: Fitz Elliott <fitz@cos.io>
    Date:   Thu Aug 8 01:52:29 2024 -0400
    
        bump django dep
    
    commit d810142cd14fd69f722c1258a6f590e3d1bcdb46
    Author: Fitz Elliott <fitz@cos.io>
    Date:   Thu Aug 8 01:48:58 2024 -0400
    
        update latent transitive deps
    
    commit f79ea4805502185170be070162fb286008de7732
    Merge: f3c5b998c8 8296391bc5
    Author: Fitz Elliott <fitz@cos.io>
    Date:   Thu Aug 8 01:28:50 2024 -0400
    
        Merge branch 'hotfix/double-url-encoding' into hotfix/pyupgrade-hotfixes-2
    
         [ENG-6055]
         Closes #10694
    
    commit 8296391bc51881b0c0afbf593ce627dc949dcfdc
    Author: Andriy Sheredko <sheredko.andriy@gmail.com>
    Date:   Fri Aug 2 16:23:52 2024 +0300
    
        - ENG-6055 fix for double encoding files and not be able to download them
    
    commit f3c5b998c899ab324a5daec0311f636b4bffe384
    Merge: d2b27f0e5e 2070b6e6f7
    Author: Fitz Elliott <fitz@cos.io>
    Date:   Thu Aug 8 01:24:01 2024 -0400
    
        Merge branch 'hotfix/dep-bumps' into hotfix/pyupgrade-hotfixes-2
    
         * Bump deps for django & werkzeug
    
    commit 2070b6e6f76533549a7f04cb84bc07042f21ba0a
    Author: Fitz Elliott <fitz@cos.io>
    Date:   Fri Aug 2 21:30:52 2024 -0400
    
        make gha workflow use pyproject.toml
    
    commit c5c0c8e01dd14cadffe373a38530ab6d907d9a6f
    Author: Fitz Elliott <fitz@cos.io>
    Date:   Fri Aug 2 21:20:39 2024 -0400
    
        remove old requirements files & annotate poetry deps
    
    commit c9b21179791625d9ea581ca349c233e5d564fb0a
    Author: Fitz Elliott <fitz@cos.io>
    Date:   Fri Aug 2 20:23:49 2024 -0400
    
        bump werkzeug dep
    
    commit c5fd2f38c6d8d5dafa0ff7ccbe5e4bbe1a5c54f6
    Author: Fitz Elliott <fitz@cos.io>
    Date:   Fri Aug 2 20:21:45 2024 -0400
    
        bump django dep
    
    commit 0e4950e0f2d8c3f69356529c38578d11a2e793d5
    Author: Fitz Elliott <fitz@cos.io>
    Date:   Fri Aug 2 20:19:39 2024 -0400
    
        update latent transitive deps

--- api/base/utils.py
+++ api/base/utils.py
@@ -1,5 +1,5 @@
 from furl import furl
-from urllib.parse import urlunsplit, urlsplit, parse_qs, urlencode, quote
+from urllib.parse import urlunsplit, urlsplit, parse_qs, urlencode
 from packaging.version import Version
 from hashids import Hashids
 
@@ -236,7 +236,7 @@ def waterbutler_api_url_for(node_id, provider, path='/', _internal=False, base_u
     # NOTE: furl encoding to be verified later
     url = furl(website_settings.WATERBUTLER_INTERNAL_URL if _internal else (base_url or website_settings.WATERBUTLER_URL))
     segments = ['v1', 'resources', node_id, 'providers', provider] + path.split('/')[1:]
-    url.add(path=[quote(x) for x in segments])
+    url.add(path=segments)
     url.args.update(kwargs)
     return url.url
 
