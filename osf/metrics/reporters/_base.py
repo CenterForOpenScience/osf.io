647cf1b54a19060b2f0fcbc730ec9763347a4010
abram axel booth <boothaa@gmail.com>
2024-08-23 13:35:55 -0400

    stop trying to send metrics reports to keen

--- osf/metrics/reporters/_base.py
+++ osf/metrics/reporters/_base.py
@@ -1,12 +1,6 @@
-from collections import defaultdict
-from datetime import datetime
 import logging
-import pytz
-
-from keen.client import KeenClient
 
 from osf.metrics.utils import YearMonth
-from website.settings import KEEN as keen_settings
 
 
 logger = logging.getLogger(__name__)
@@ -33,49 +27,10 @@ class DailyReporter:
         """
         raise NotImplementedError(f'{self.__name__} must implement `report`')
 
-    def keen_events_from_report(self, report):
-        """given one of this reporter's own reports, build equivalent keen events
-        (for back-compat; to be deleted once we don't need keen anymore)
-
-        return a mapping from keen collection name to iterable of events
-        e.g. {'my_keen_collection': [event1, event2, ...]}
-        """
-        raise NotImplementedError(f'{self.__name__} should probably implement keen_events_from_report')
-
-    def run_and_record_for_date(self, report_date, *, also_send_to_keen=False):
+    def run_and_record_for_date(self, report_date):
         reports = self.report(report_date)
 
         # expecting each reporter to spit out only a handful of reports per day;
         # not bothering with bulk-create
         for report in reports:
             report.save()
-
-        if also_send_to_keen:
-            self.send_to_keen(reports)
-
-    def send_to_keen(self, reports):
-        keen_project = keen_settings['private']['project_id']
-        write_key = keen_settings['private']['write_key']
-        if not (keen_project and write_key):
-            logger.warning(f'keen not configured; not sending events for {self.__class__.__name__}')
-            return
-
-        keen_events_by_collection = defaultdict(list)
-        for report in reports:
-            keen_event_timestamp = datetime(
-                report.report_date.year,
-                report.report_date.month,
-                report.report_date.day,
-                tzinfo=pytz.utc,
-            )
-
-            for collection_name, keen_events in self.keen_events_from_report(report).items():
-                for event in keen_events:
-                    event['keen'] = {'timestamp': keen_event_timestamp.isoformat()}
-                keen_events_by_collection[collection_name].extend(keen_events)
-
-        client = KeenClient(
-            project_id=keen_project,
-            write_key=write_key,
-        )
-        client.add_events(keen_events_by_collection)
