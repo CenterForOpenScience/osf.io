79f4cd65f397631f81568b4e90fe38bb4582ab4d
abram axel booth <boothaa@gmail.com>
2024-09-19 12:07:36 -0400

    PublicItemUsageReport(er) + tests

--- osf/metrics/reporters/__init__.py
+++ osf/metrics/reporters/__init__.py
@@ -9,6 +9,7 @@ from .new_user_domain import NewUserDomainReporter
 from .node_count import NodeCountReporter
 from .osfstorage_file_count import OsfstorageFileCountReporter
 from .preprint_count import PreprintCountReporter
+from .public_item_usage import PublicItemUsageReporter
 from .user_count import UserCountReporter
 from .spam_count import SpamCountReporter
 
@@ -28,3 +29,4 @@ class AllDailyReporters(enum.Enum):
 class AllMonthlyReporters(enum.Enum):
     SPAM_COUNT = SpamCountReporter
     INSTITUTIONAL_USERS = InstitutionalUsersReporter
+    ITEM_USAGE = PublicItemUsageReporter
