0fabf7ae310e83fb1d413fcaa529c4f674dd0436
Brian J. Geiger <bgeiger@pobox.com>
2024-11-04 15:19:16 -0500

    Add onedrive config

--- osf/external/gravy_valet/translations.py
+++ osf/external/gravy_valet/translations.py
@@ -12,6 +12,7 @@ from addons.github.apps import GitHubAddonConfig
 from addons.gitlab.apps import GitLabAddonConfig
 from addons.googledrive.apps import GoogleDriveAddonConfig
 from addons.s3.apps import S3AddonAppConfig
+from addons.onedrive.apps import OneDriveAddonAppConfig
 from . import request_helpers as gv_requests
 
 
@@ -26,6 +27,7 @@ class _LegacyConfigsForWBKey(enum.Enum):
     gitlab = GitLabAddonConfig
     googledrive = GoogleDriveAddonConfig
     s3 = S3AddonAppConfig
+    onedrive = OneDriveAddonAppConfig
 
 
 def make_ephemeral_user_settings(gv_account_data, requesting_user):
