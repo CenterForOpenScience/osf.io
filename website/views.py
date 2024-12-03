4a6baae9c1ebd0f1626390d24b7549b71bea04cf
Ostap Zherebetskyi <ozherebetskyi@exoft.net>
2024-12-03 19:40:16 +0200

    restore comment

--- website/views.py
+++ website/views.py
@@ -303,6 +303,7 @@ def resolve_guid(guid, suffix=None):
             if version:
                 resource = base_guid_obj.versions.filter(version=version).first().referent
             else:
+                # redirection to the same route, but with the specified version
                 version = base_guid_obj.versions.order_by('-version').first().version
                 new_guid = f'{base_guid}_v{version}'
                 return redirect(f'/{new_guid}/{suffix}' if suffix else f'/{new_guid}/', code=302)
