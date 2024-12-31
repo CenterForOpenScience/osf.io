26a2854249c5c7e679c6affcc66689b4e41b6a47
Ostap Zherebetskyi <ozherebetskyi@exoft.net>
2024-12-31 15:03:25 +0200

    update metrics

--- addons/osfstorage/utils.py
+++ addons/osfstorage/utils.py
@@ -21,7 +21,7 @@ def enqueue_update_analytics(node, file, version_idx, action='download'):
 
 @app.task(max_retries=5, default_retry_delay=60)
 def update_analytics_async(node_id, file_id, version_idx, session_key=None, action='download'):
-    node = Guid.load_referent(node_id)
+    node, _ = Guid.load_referent(node_id)
     file = BaseFileNode.load(file_id)
     update_analytics(node, file, version_idx, session_key, action)
 
