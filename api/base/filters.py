a61659f8fa53081baa1fd90ae9d9c05a75056cc1
Longze Chen <cslzchen@gmail.com>
2025-01-08 22:02:58 -0500

    Add a TODO comment to fix broken filters

--- api/base/filters.py
+++ api/base/filters.py
@@ -292,6 +292,7 @@ class FilterMixin:
                             field_name: self._parse_date_param(field, source_field_name, op, value),
                         })
                     elif not isinstance(value, int) and source_field_name in ['_id', 'guid._id']:
+                        # TODO: this is broken since value can be a multi-value str separated by comma
                         base_guid, version = Guid.split_guid(value)
                         if base_guid is None and version is None:
                             raise InvalidFilterValue(
