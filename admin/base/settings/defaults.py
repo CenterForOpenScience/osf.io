b2680033198f3a0ae7444f8b7a1a23bbfd04f69c
Bohdan Odintsov <bodintsov@exoft.net>
2025-01-15 15:15:24 +0200

    added testcases to check email send once

--- admin/base/settings/defaults.py
+++ admin/base/settings/defaults.py
@@ -40,7 +40,9 @@ CSRF_COOKIE_SECURE = osf_settings.SECURE_MODE
 CSRF_COOKIE_HTTPONLY = False
 
 ALLOWED_HOSTS = [
-    '.osf.io'
+    '.osf.io',
+    'localhost',
+    '127.0.0.1'
 ]
 
 AUTH_PASSWORD_VALIDATORS = [
