68803f7b8dd9e726922ce8175df2a0d70e006454
Longze Chen <cslzchen@gmail.com>
2025-01-17 09:23:37 -0500

    Use `\w` for versioned guid since it's equivalent to [a-Az-Z0-9_]

--- api/chronos/urls.py
+++ api/chronos/urls.py
@@ -7,7 +7,6 @@ app_name = 'osf'
 urlpatterns = [
     re_path(r'^journals/$', views.ChronosJournalList.as_view(), name=views.ChronosJournalList.view_name),
     re_path(r'^journals/(?P<journal_id>[-0-9A-Za-z]+)/$', views.ChronosJournalDetail.as_view(), name=views.ChronosJournalDetail.view_name),
-    # ^\w+(_[vV]\d+)*)
-    re_path(r'^(?P<preprint_id>\w+(_[vV]\d+)*)/submissions/$', views.ChronosSubmissionList.as_view(), name=views.ChronosSubmissionList.view_name),
-    re_path(r'^(?P<preprint_id>\w+(_[vV]\d+)*)/submissions/(?P<submission_id>[-0-9A-Za-z]+)/$', views.ChronosSubmissionDetail.as_view(), name=views.ChronosSubmissionDetail.view_name),
+    re_path(r'^(?P<preprint_id>\w+)/submissions/$', views.ChronosSubmissionList.as_view(), name=views.ChronosSubmissionList.view_name),
+    re_path(r'^(?P<preprint_id>\w+)/submissions/(?P<submission_id>[-0-9A-Za-z]+)/$', views.ChronosSubmissionDetail.as_view(), name=views.ChronosSubmissionDetail.view_name),
 ]
