caa787e102867bfb410a7c080cb2d3b1e2a8d048
bodintsov <bodintsov@exoft.net>
2025-02-12 16:20:47 +0200

    [ENG-6208] implemented unwithdraw specific preprint version (#10964)
    ## Purpose
    
    Allow to unwithdraw preprint versions in the admin app
    
    ## Ticket
    
    (https://openscience.atlassian.net/browse/ENG-6208)

--- admin/preprints/urls.py
+++ admin/preprints/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     re_path(r'^(?P<guid>\w+)/reject_withdrawal/$', views.PreprintRejectWithdrawalRequest.as_view(), name='reject-withdrawal'),
     re_path(r'^(?P<guid>\w+)/resync_crossref/$', views.PreprintResyncCrossRefView.as_view(), name='resync-crossref'),
     re_path(r'^(?P<guid>\w+)/make_published/$', views.PreprintMakePublishedView.as_view(), name='make-published'),
+    re_path(r'^(?P<guid>\w+)/unwithdraw/$', views.PreprintUnwithdrawView.as_view(), name='unwithdraw'),
 ]
