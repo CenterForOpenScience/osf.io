356a32d71390dc6d08ac008eff61bf408657d3dc
Ihor Sokhan <isokhan@exoft.net>
2025-02-21 15:59:49 +0200

    remove property duplicate

--- osf/models/preprint.py
+++ osf/models/preprint.py
@@ -690,14 +690,6 @@ class Preprint(DirtyFieldsMixin, VersionedGuidMixin, IdentifierMixin, Reviewable
     def is_latest_version(self):
         return self.guids.exists()
 
-    @property
-    def has_registration_doi(self):
-        return self.identifiers.filter(
-            category='doi',
-            deleted__isnull=True,
-            object_id__isnull=False,
-        ).exists()
-
     def get_preprint_versions(self, include_rejected=True):
         guids = self.versioned_guids.first().guid.versions.all()
         preprint_versions = Preprint.objects.filter(id__in=[vg.object_id for vg in guids]).order_by('-id')
