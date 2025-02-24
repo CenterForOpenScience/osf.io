8a9dc3927e58f0fa54736283b9988693ee6ff6ae
ihorsokhanexoft <isokhan@exoft.net>
2025-02-24 21:36:30 +0200

    [ENG-7283] fixed publication doi related error (#10994)
    ## Purpose
    An unexpected error was raised because incorrect Publication DOI was passed
    
    ## Changes
    If possible try to strip redundant data and save the correct format
    
    ## Ticket
    https://openscience.atlassian.net/browse/ENG-7283

--- api_tests/preprints/views/test_preprint_detail.py
+++ api_tests/preprints/views/test_preprint_detail.py
@@ -273,7 +273,7 @@ class TestPreprintUpdate:
 
     @pytest.fixture()
     def preprint(self, user):
-        return PreprintFactory(creator=user)
+        return PreprintFactory(creator=user, provider=PreprintProviderFactory(doi_prefix='10.1234/test'))
 
     @pytest.fixture()
     def url(self, preprint):
@@ -598,7 +598,7 @@ class TestPreprintUpdate:
 
         update_payload = build_preprint_update_payload(
             preprint._id,
-            attributes={'doi': '10.1234/test'}
+            attributes={'doi': '10.1235/test'}
         )
         res = app.patch_json_api(url, update_payload, auth=user.auth)
         assert res.status_code == 200
@@ -614,10 +614,80 @@ class TestPreprintUpdate:
         assert ' is already associated with this preprint' in error_data[0]['detail']
 
         preprint.reload()
-        assert preprint.article_doi == '10.1234/test'
+        assert preprint.article_doi == '10.1235/test'
 
         preprint_detail = app.get(url, auth=user.auth).json['data']
-        assert preprint_detail['links']['doi'] == 'https://doi.org/10.1234/test'
+        assert preprint_detail['links']['doi'] == 'https://doi.org/10.1235/test'
+
+    @responses.activate
+    @mock.patch('osf.models.preprint.update_or_enqueue_on_preprint_updated', mock.Mock())
+    def test_update_article_doi_different_formats(self, app, user, preprint, url):
+        responses.add(
+            responses.Response(
+                responses.POST,
+                CROSSREF_URL,
+                content_type='text/html;charset=ISO-8859-1',
+                status=200,
+            ),
+        )
+        dois = [
+            ('10.1235/', False),
+            ('10.1235/test', False),
+            ('doi.org/10.1235/test', False),
+            ('https://10.1235/test', False),
+            ('https://doi.org/10.1235/', False),
+            ('https://doi.org/10.1235/test', False),
+            ('10.1235', True),
+            ('9.1234/test', True),
+            ('https://doi.org/9.1234/test', True),
+            ('https://doi.org/9.1234/', True),
+            ('https://9.1234/test', True),
+            ('doi.org/9.1234/test', True),
+            ('9.1234', True),
+        ]
+        for doi, expect_error in dois:
+            update_payload = build_preprint_update_payload(
+                preprint._id,
+                attributes={'doi': doi}
+            )
+            res = app.patch_json_api(url, update_payload, auth=user.auth, expect_errors=expect_error)
+            assert res.status_code == 400 if expect_error else 200
+
+    @responses.activate
+    @mock.patch('osf.models.preprint.update_or_enqueue_on_preprint_updated', mock.Mock())
+    def test_article_doi_regex(self, app, user, preprint, url):
+        responses.add(
+            responses.Response(
+                responses.POST,
+                CROSSREF_URL,
+                content_type='text/html;charset=ISO-8859-1',
+                status=200,
+            ),
+        )
+        original_and_stripped_dois = [
+            ('10.1235/', '10.1235/'),
+            ('10.1235/test', '10.1235/test'),
+            ('doi.org/10.1235/test', '10.1235/test'),
+            ('https://10.1235/test', '10.1235/test'),
+            ('https://doi.org/10.1235/', '10.1235/'),
+            ('https://doi.org/10.1235/test', '10.1235/test'),
+            ('10.1312321321/test/tes2/test/13', '10.1312321321/test/tes2/test/13'),
+            ('10.1235/word1/random/preprint/id/any/other/string///doi', '10.1235/word1/random/preprint/id/any/other/string///doi'),
+            ('http://doi.com/10.1236/test/url/http', '10.1236/test/url/http'),
+            ('10.123/456/789', '10.123/456/789'),
+            ('10.word/', '10.word/'),
+            ('word.10.1234/word2', '10.1234/word2'),
+            ('10.word/word2/', '10.word/word2/')
+        ]
+        for doi, stripped_doi in original_and_stripped_dois:
+            update_payload = build_preprint_update_payload(
+                preprint._id,
+                attributes={'doi': doi}
+            )
+            app.patch_json_api(url, update_payload, auth=user.auth)
+
+            preprint_detail = app.get(url, auth=user.auth).json['data']
+            assert preprint_detail['links']['doi'] == f'https://doi.org/{stripped_doi}'
 
     def test_title_has_a_512_char_limit(self, app, user, preprint, url):
         new_title = 'a' * 513
@@ -838,7 +908,7 @@ class TestPreprintUpdate:
             ),
         )
         update_doi_payload = build_preprint_update_payload(
-            preprint._id, attributes={'doi': '10.1234/ASDFASDF'})
+            preprint._id, attributes={'doi': '10.1235/ASDFASDF'})
 
         app.patch_json_api(url, update_doi_payload, auth=user.auth)
 
