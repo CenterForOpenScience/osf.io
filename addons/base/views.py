e59d51ae6c2992bb632d1cbfb0a95b3b3ee218ef
Oleh Paduchak <opaduchak@exoft.net>
2025-03-10 15:46:43 +0200

    fixed broken VOL for viewing files

--- addons/base/views.py
+++ addons/base/views.py
@@ -863,7 +863,7 @@ def addon_view_or_download_file(auth, path, provider, **kwargs):
         raise HTTPError(http_status.HTTP_400_BAD_REQUEST)
 
     if hasattr(target, 'get_addon'):
-        node_addon = target.get_addon(provider)
+        node_addon = target.get_addon(provider, auth=auth)
         if flag_is_active(request, features.ENABLE_GV):
             if not isinstance(node_addon, EphemeralNodeSettings) and provider != 'osfstorage':
                 object_text = markupsafe.escape(getattr(target, 'project_or_component', 'this object'))
