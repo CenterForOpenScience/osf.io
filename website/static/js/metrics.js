822b829aaa8eac861f09ede4a7c5acf8e03beabc
mkovalua <mkoval@exoft.net>
2025-03-14 13:18:40 +0200

    update getUniqueId

--- website/static/js/metrics.js
+++ website/static/js/metrics.js
@@ -8,22 +8,18 @@ var $osf = require('js/osfHelpers');
 
 var MetricsTracker = (function() {
     function getUniqueId() {
-          if (typeof window.crypto !== 'undefined' && window.crypto.getRandomValues) {
-            return ([1e7] + '-1e3-4e3-8e3-1e11').replace(/[xy]/g, c => {
-              let r = window.crypto.getRandomValues(new Uint8Array(1))[0] & 15;
-              let v = c === 'x' ? r : (r & 0x3 | 0x8);
-              return v.toString(16);
-            });
+        if (typeof window.crypto !== 'undefined' && window.crypto.getRandomValues) {
+            return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+              (c ^ window.crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+            );
           } else {
             let str = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx';
             return str.replace(/[xy]/g, function(c) {
-              let r = Math.random() * 16 | 0;
-              let v = c === 'x' ? r : (r & 0x3 | 0x8);
+              let r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);
               return v.toString(16);
             });
           }
     }
-
 
     function _createOrUpdateKeenSession() {
         var expDate = new Date();
