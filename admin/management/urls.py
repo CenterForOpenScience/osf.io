f9cdc627ac827c1a2ab6c812172026cf71cbcc7b
mkovalua <mkoval@exoft.net>
2025-03-25 15:45:33 +0200

    [ENG-7296] feature/ENG-7296--v2 resyncing empty metadata dataarchive registration with Datacite (#11050)
    <!-- Before submit your Pull Request, make sure you picked
         the right branch:
    
         - For hotfixes, select "master" as the target branch
         - For new features, select "develop" as the target branch
         - For release feature fixes, select the relevant release branch (release/X.Y.Z) as the target branch -->
    
    ## Purpose
    Possible updates mentioned in the ticket
    https://github.com/CenterForOpenScience/osf.io/pull/11014#pullrequestreview-2705866778
    
    ## Ticket
    https://openscience.atlassian.net/browse/ENG-7296

--- admin/management/urls.py
+++ admin/management/urls.py
@@ -15,5 +15,7 @@ urlpatterns = [
     re_path(r'^daily_reporters_go', views.DailyReportersGo.as_view(), name='daily_reporters_go'),
     re_path(r'^monthly_reporters_go', views.MonthlyReportersGo.as_view(), name='monthly_reporters_go'),
     re_path(r'^ingest_cedar_metadata_templates', views.IngestCedarMetadataTemplates.as_view(), name='ingest_cedar_metadata_templates'),
-    re_path(r'^bulk_resync', views.BulkResync.as_view(), name='bulk-resync')
+    re_path(r'^bulk_resync', views.BulkResync.as_view(), name='bulk-resync'),
+    re_path(r'^empty_metadata_dataarchive_registration_bulk_resync', views.EmptyMetadataDataarchiveRegistrationBulkResync.as_view(),
+            name='empty-metadata-dataarchive-registration-bulk-resync')
 ]
--- osf/management/commands/sync_doi_metadata.py
+++ osf/management/commands/sync_doi_metadata.py
@@ -3,11 +3,11 @@ import time
 import datetime
 import logging
 
+from django.contrib.contenttypes.models import ContentType
 from django.core.management.base import BaseCommand
 from datacite.errors import DataCiteServerError
 from website.identifiers.clients.exceptions import CrossRefRateLimitError
-from osf.models import Identifier
-
+from osf.models import GuidMetadataRecord, Identifier, Registration
 from framework.celery_tasks import app
 
 logger = logging.getLogger(__name__)
@@ -67,6 +67,46 @@ def sync_doi_metadata(modified_date, batch_size=100, dry_run=True, sync_private=
 
         if (identifier.referent.is_public and not identifier.referent.deleted and not identifier.referent.is_retracted) or sync_private:
             sync_identifier_doi.apply_async(kwargs={'identifier_id': identifier.id})
+
+
+@app.task(name='osf.management.commands.sync_doi_empty_metadata_dataarchive_registrations_command', max_retries=5, default_retry_delay=RATE_LIMIT_RETRY_DELAY)
+def sync_doi_empty_metadata_dataarchive_registrations(modified_date, batch_size=100, dry_run=True, sync_private=False, rate_limit=100):
+    registrations_ids = list(
+        Registration.objects.filter(
+            provider___id='dataarchive',
+            is_public=True,
+            deleted__isnull=True,
+        ).values_list('id', flat=True)
+    )
+    identifiers = Identifier.objects.filter(
+        object_id__in=registrations_ids,
+        content_type_id=ContentType.objects.get_for_model(Registration).id,
+        category='doi',
+        deleted__isnull=True,
+        modified__lte=modified_date
+    )
+    if batch_size:
+        identifiers = identifiers[:batch_size]
+        rate_limit = batch_size if batch_size > rate_limit else rate_limit
+
+    logger.info(f'{"[DRY RUN]: " if dry_run else ""}'
+                f'{identifiers.count()} identifiers to mint')
+
+    for record_number, identifier in enumerate(identifiers, 1):
+
+        # in order to not reach rate limits that CrossRef and DataCite have, we make delay
+        if not record_number % rate_limit:
+            time.sleep(RATE_LIMIT_RETRY_DELAY)
+
+        if identifier.referent.is_retracted or sync_private:
+            metadata_record = GuidMetadataRecord.objects.for_guid(
+                identifier.referent
+            )
+            if metadata_record.resource_type_general == '':
+                if dry_run:
+                    logger.info(f"[DRY RUN]:  doi minting for {identifier.value} started")
+                    continue
+                sync_identifier_doi.apply_async(kwargs={'identifier_id': identifier.id})
 
 
 class Command(BaseCommand):
