054ce019631cb89ab4312514d0abe03f19e2746b
Ostap Zherebetskyi <ozherebetskyi@exoft.net>
2025-04-03 16:47:22 +0300

    remove old Flask registration_callback code and related decorators

--- website/routes.py
+++ website/routes.py
@@ -1716,14 +1716,6 @@ def make_url_map(app):
             json_renderer,
         ),
         Rule(
-            [
-                '/registration/<pid>/callbacks/',
-            ],
-            'put',
-            project_views.register.registration_callbacks,
-            json_renderer,
-        ),
-        Rule(
             '/settings/addons/',
             'post',
             profile_views.user_choose_addons,
