6bfab18999932c248385a42ce7aa4c5dfed6cab5
mkovalua <mkoval@exoft.net>
2025-04-08 18:33:36 +0300

    fix migrations conflicts

--- osf/migrations/0029_remove_abstractnode_keenio_read_key.py
+++ osf/migrations/0029_remove_abstractnode_keenio_read_key.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('osf', '0027_alter_guidversionsthrough_object_id'),
+        ('osf', '0028_collection_grade_levels_choices_and_more'),
     ]
 
     operations = [
