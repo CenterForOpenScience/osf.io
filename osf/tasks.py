fe53f50977f5d51475b61e4b71c54f5a9407d93f
Oleh Paduchak <opaduchak@exoft.net>
2025-04-10 12:56:37 +0300

    fixed task implementation to fail when needed

--- osf/tasks.py
+++ osf/tasks.py
@@ -1,9 +1,17 @@
 import logging
+import re
+
+from django.db.models import Model
 
 from framework.celery_tasks import app
 
-
 logger = logging.getLogger(__name__)
+iri_regex = re.compile(r'http://[^/]+/(?P<id>\w{5})/?')
+
+def get_object_by_url(url: str, model: type[Model]) -> Model | None:
+    if match := iri_regex.match(url):
+        return model.objects.get(guids___id=match['id'])
+    raise Exception(f"Invalid URL received: {url=}")
 
 @app.task(max_retries=5, name='osf.tasks.log_gv_addon', default_retry_delay=10)
 def log_gv_addon(node_url: str, action: str, user_url: str, addon: str):
@@ -14,11 +22,14 @@ def log_gv_addon(node_url: str, action: str, user_url: str, addon: str):
     })
     if action not in PERMITTED_GV_ACTIONS:
         logger.error(f"{action} is not permitted to be logged from GV")
+        return
+    try:
+        node = get_object_by_url(node_url, Node)
+        user = get_object_by_url(user_url, OSFUser)
+    except Exception as e:
+        logger.error(e)
+        return
 
-    node_guid = node_url.split('/')[-1]
-    user_guid = user_url.split('/')[-1]
-    node = Node.objects.get(guids___id=node_guid)
-    user = OSFUser.objects.get(guids___id=user_guid)
     node.add_log(
         action=action,
         auth=user,
