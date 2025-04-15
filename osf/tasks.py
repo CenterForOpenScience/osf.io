f076506114ed601d6c04b8c308dad9216cc92de4
Oleh Paduchak <opaduchak@exoft.net>
2025-04-15 12:29:09 +0300

    fixed arror handling accordng to comments

--- osf/tasks.py
+++ osf/tasks.py
@@ -4,30 +4,36 @@ import re
 from django.db.models import Model
 
 from framework.celery_tasks import app
+from framework.sentry import log_message
 
 logger = logging.getLogger(__name__)
 iri_regex = re.compile(r'http://[^/]+/(?P<id>\w{5})/?')
 
-def get_object_by_url(url: str, model: type[Model]) -> Model | None:
-    if match := iri_regex.match(url):
+def get_object_by_url[T: Model](url: str, model: type[T]) -> T | None:
+    if not (match := iri_regex.match(url)):
+        log_message(f"received invalid {model.__name__} {url=}. ", skip_session=True)
+        return None
+    try:
         return model.objects.get(guids___id=match['id'])
-    raise Exception(f"Invalid URL received: {url=}")
+    except model.DoesNotExist:
+        log_message(f"Could not find {model.__name__} with id={match['id']}", skip_session=True)
+        return None
 
 @app.task(max_retries=5, name='osf.tasks.log_gv_addon', default_retry_delay=10)
 def log_gv_addon(node_url: str, action: str, user_url: str, addon: str):
     from osf.models import NodeLog, OSFUser, Node
+
     PERMITTED_GV_ACTIONS = frozenset({
         NodeLog.ADDON_ADDED,
         NodeLog.ADDON_REMOVED
     })
     if action not in PERMITTED_GV_ACTIONS:
-        logger.error(f"{action} is not permitted to be logged from GV")
+        log_message(f"{action} is not permitted to be logged from GV", skip_session=True)
         return
-    try:
-        node = get_object_by_url(node_url, Node)
-        user = get_object_by_url(user_url, OSFUser)
-    except Exception as e:
-        logger.error(e)
+
+    node = get_object_by_url(node_url, Node)
+    user = get_object_by_url(user_url, OSFUser)
+    if not (node or user):
         return
 
     node.add_log(
