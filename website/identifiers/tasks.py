3a674955e8b717cef11b575d32bb5301d32bcb22
Vlad0n20 <vlad.onischuk1234@gmail.com>
2025-05-05 14:25:22 +0300

    Update PR accordng to comments

--- website/identifiers/tasks.py
+++ website/identifiers/tasks.py
@@ -1,9 +1,12 @@
 import logging
 from django.apps import apps
 
+from osf.external.gravy_valet.exceptions import GVException
 from framework.celery_tasks import app as celery_app
 from framework.celery_tasks.handlers import queued_task
 from framework import sentry
+
+logger = logging.getLogger(__name__)
 
 @celery_app.task(bind=True, max_retries=5, acks_late=True)
 def task__update_doi_metadata_on_change(self, target_guid):
@@ -20,9 +23,7 @@ def update_doi_metadata_on_change(target_guid):
 
 @celery_app.task(bind=True, max_retries=5, acks_late=True)
 def task__update_doi_metadata_with_verified_links(self, target_guid):
-    sentry.log_message('Updating DOI with verified links for guid',
-                      extra_data={'guid': target_guid},
-                      level=logging.INFO)
+    logger.info(f'Updating DOI with verified links for guid - {target_guid}')
 
     Guid = apps.get_model('osf.Guid')
     target_object = Guid.load(target_guid).referent
@@ -30,14 +31,10 @@ def task__update_doi_metadata_with_verified_links(self, target_guid):
 
         target_object.request_identifier_update(category='doi')
 
-        sentry.log_message('DOI metadata with verified links updated for guid',
-                         extra_data={'guid': target_guid},
-                         level=logging.INFO)
-    except Exception as exc:
-        sentry.log_message('Failed to update DOI metadata with verified links',
-                         extra_data={'guid': target_guid, 'error': str(exc)},
-                         level=logging.ERROR)
-        raise self.retry(exc=exc)
+        logger.info(f'DOI metadata with verified links updated for guid - {target_guid}')
+    except GVException as e:
+        logger.info(f'Failed to update DOI metadata with verified links for guid - {target_guid}')
+        raise self.retry(exc=e)
 
 @queued_task
 @celery_app.task(ignore_results=True)
