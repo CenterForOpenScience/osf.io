581cf5a7bd1c6d799ce14966d383ec8e2ff446be
Ihor Sokhan <isokhan@exoft.net>
2025-06-18 11:06:19 +0300

    simplified code readability

--- website/archiver/tasks.py
+++ website/archiver/tasks.py
@@ -289,19 +289,19 @@ def archive_node(stat_results, job_pk):
 
     if (NO_ARCHIVE_LIMIT not in job.initiator.system_tags) and (disk_usage_in_gb > limit):
         raise ArchiverSizeExceeded(result=stat_result)
-    else:
-        if not stat_result.targets:
-            job.status = ARCHIVER_SUCCESS
-            job.save()
-        for result in stat_result.targets:
-            if not result['num_files']:
-                job.update_target(result['target_name'], ARCHIVER_SUCCESS)
-            else:
-                archive_addon.delay(
-                    addon_short_name=result['target_name'],
-                    job_pk=job_pk
-                )
-        project_signals.archive_callback.send(dst)
+
+    if not stat_result.targets:
+        job.status = ARCHIVER_SUCCESS
+        job.save()
+    for result in stat_result.targets:
+        if not result['num_files']:
+            job.update_target(result['target_name'], ARCHIVER_SUCCESS)
+        else:
+            archive_addon.delay(
+                addon_short_name=result['target_name'],
+                job_pk=job_pk
+            )
+    project_signals.archive_callback.send(dst)
 
 
 def archive(job_pk):
