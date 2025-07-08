65e88b58d2030ca8315e44bbffd378589937a4d6
hide24 <hidetoshi.yoshimoto@gmail.com>
2025-07-08 23:25:21 +0900

    manage migrations

--- addons/wiki/migrations/0013_wikipage_parent.py
+++ addons/wiki/migrations/0013_wikipage_parent.py
@@ -15,6 +15,11 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='wikipage',
             name='parent',
+            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='addons_wiki.WikiPage'),
+        ),
+        migrations.AddField(
+            model_name='wikipage',
+            name='sort_order',
             field=models.IntegerField(blank=True, null=True),
         ),
-    ]
+   ]
