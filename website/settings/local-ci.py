29f9d8995ce2edc783de0acae17129ec7257071d
chiku <samugari.penguin@gmail.com>
2025-07-09 17:08:23 +0900

    Make `PRIMARY_WEB_APP` option work on CI.

--- website/settings/local-ci.py
+++ website/settings/local-ci.py
@@ -27,6 +27,7 @@ PREPRINT_PROVIDER_DOMAINS = {
     'suffix': ':4201/'
 }
 USE_EXTERNAL_WEB_APP = True
+PRIMARY_WEB_APP = 'ember_osf_web'
 EXTERNAL_WEB_APPS = {
     'ember_osf_web': {
         'server': 'http://localhost:4200',
