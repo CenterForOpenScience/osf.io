03cad99e8475d99ded53df47e1b9cb8aa1598a35
John Tordoff <Johnetordoff@users.noreply.github.com>
2025-07-11 08:40:35 -0400

    fix issues with migrate schema response task deletion

--- api_tests/users/views/test_user_settings_reset_password.py
+++ api_tests/users/views/test_user_settings_reset_password.py
@@ -8,9 +8,8 @@ from osf_tests.factories import (
 )
 from django.middleware import csrf
 
-
-@pytest.mark.django_db
 @pytest.mark.usefixtures('mock_send_grid')
+@pytest.mark.usefixtures('mock_notification_send')
 class TestResetPassword:
 
     @pytest.fixture()
@@ -29,14 +28,14 @@ class TestResetPassword:
     def csrf_token(self):
         return csrf._mask_cipher_secret(csrf._get_new_csrf_string())
 
-    def test_get(self, mock_send_grid, app, url, user_one):
+    def test_get(self, mock_notification_send, app, url, user_one):
         encoded_email = urllib.parse.quote(user_one.email)
         url = f'{url}?email={encoded_email}'
         res = app.get(url)
         assert res.status_code == 200
 
         user_one.reload()
-        assert mock_send_grid.call_args[1]['to_addr'] == user_one.username
+        assert mock_notification_send.called
 
     def test_get_invalid_email(self, mock_send_grid, app, url):
         url = f'{url}?email={'invalid_email'}'
@@ -109,10 +108,11 @@ class TestResetPassword:
         res = app.post_json_api(url, payload, expect_errors=True, headers={'X-THROTTLE-TOKEN': 'test-token', 'X-CSRFToken': csrf_token})
         assert res.status_code == 400
 
-    def test_throttle(self, app, url, user_one):
+    def test_throrrle(self, app, url, user_one, csrf_token):
+        app.set_cookie(CSRF_COOKIE_NAME, csrf_token)
         encoded_email = urllib.parse.quote(user_one.email)
         url = f'{url}?email={encoded_email}'
-        app.get(url)
+        res = app.get(url)
         user_one.reload()
         payload = {
             'data': {
@@ -123,9 +123,8 @@ class TestResetPassword:
                 }
             }
         }
-
-        res = app.post_json_api(url, payload, expect_errors=True)
-        assert res.status_code == 429
+        res = app.post_json_api(url, payload, expect_errors=True, headers={'X-CSRFToken': csrf_token})
+        assert res.status_code == 200
 
         res = app.get(url, expect_errors=True)
         assert res.json['message'] == 'You have recently requested to change your password. Please wait a few minutes before trying again.'
