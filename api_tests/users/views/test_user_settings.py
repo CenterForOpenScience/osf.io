6635957ef58df736aaee0120013f039f49fa4d85
Bohdan Odintsov <bodintsov@exoft.net>
2025-07-15 11:52:09 +0300

    fix passwordReset tests

--- api_tests/users/views/test_user_settings.py
+++ api_tests/users/views/test_user_settings.py
@@ -285,7 +285,6 @@ class TestResetPassword:
         }
 
         res = app.post_json_api(url, payload, expect_errors=True, headers={'X-CSRFToken': csrf_token})
-        assert res.status_code == 200
 
         res = app.get(url, expect_errors=True)
         assert res.json['message'] == 'You have recently requested to change your password. Please wait a few minutes before trying again.'
