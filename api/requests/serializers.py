55e155d384adbc298497ea9be0ec1140c6e2be92
John Tordoff <Johnetordoff@users.noreply.github.com>
2025-07-15 13:26:10 -0400

    add for institutional access emails

--- api/requests/serializers.py
+++ api/requests/serializers.py
@@ -14,12 +14,11 @@ from osf.models import (
     NodeRequest,
     PreprintRequest,
     Institution,
-    OSFUser,
+    OSFUser, NotificationType,
 )
 from osf.utils.workflows import DefaultStates, RequestTypes, NodeRequestTypes
 from osf.utils import permissions as osf_permissions
 from website import language, settings
-from website.mails import send_mail, NODE_REQUEST_INSTITUTIONAL_ACCESS_REQUEST
 
 from rest_framework.exceptions import PermissionDenied, ValidationError
 
@@ -188,18 +187,20 @@ class NodeRequestCreateSerializer(NodeRequestSerializer):
 
             comment = validated_data.get('comment', '').strip() or language.EMPTY_REQUEST_INSTITUTIONAL_ACCESS_REQUEST_TEXT
 
-            send_mail(
-                to_addr=recipient.username,
-                mail=NODE_REQUEST_INSTITUTIONAL_ACCESS_REQUEST,
+            NotificationType.objects.get(
+                name=NotificationType.Type.NODE_REQUEST_INSTITUTIONAL_ACCESS_REQUEST.value,
+            ).emit(
                 user=recipient,
-                sender=sender,
-                bcc_addr=[sender.username] if validated_data['bcc_sender'] else None,
-                reply_to=sender.username if validated_data['reply_to'] else None,
-                recipient=recipient,
-                comment=comment,
-                institution=institution,
-                osf_url=settings.DOMAIN,
-                node=node_request.target,
+                event_context={
+                    'sender': sender,
+                    'bcc_addr': [sender.username] if validated_data['bcc_sender'] else None,
+                    'reply_to': sender.username if validated_data['reply_to'] else None,
+                    'recipient': recipient,
+                    'comment': comment,
+                    'institution': institution,
+                    'osf_url': settings.DOMAIN,
+                    'node': node_request.target._id,
+                },
             )
 
         return node_request
