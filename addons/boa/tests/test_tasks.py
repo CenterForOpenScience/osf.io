496fc2379f873cd0c71cf15170e51c3ae30b0ff4
John Tordoff <Johnetordoff@users.noreply.github.com>
2025-08-04 11:54:24 -0400

    fix boa send_mail

--- addons/boa/tests/test_tasks.py
+++ addons/boa/tests/test_tasks.py
@@ -9,10 +9,11 @@ from urllib.error import HTTPError
 from addons.boa import settings as boa_settings
 from addons.boa.boa_error_code import BoaErrorCode
 from addons.boa.tasks import submit_to_boa, submit_to_boa_async, handle_boa_error
+from osf.models import NotificationType
 from osf_tests.factories import AuthUserFactory, ProjectFactory
 from tests.base import OsfTestCase
+from tests.utils import capture_notifications
 from website import settings as osf_settings
-from website.mails import ADDONS_BOA_JOB_COMPLETE, ADDONS_BOA_JOB_FAILURE
 
 DEFAULT_REFRESH_JOB_INTERVAL = boa_settings.REFRESH_JOB_INTERVAL
 DEFAULT_MAX_JOB_WAITING_TIME = boa_settings.MAX_JOB_WAITING_TIME
@@ -53,42 +54,25 @@ class TestBoaErrorHandling(OsfTestCase):
         assert BoaErrorCode.JOB_TIME_OUT_ERROR == 7
 
     def test_handle_boa_error(self):
-        with mock.patch('addons.boa.tasks.send_mail', return_value=None) as mock_send_mail, \
-                mock.patch('addons.boa.tasks.sentry.log_message', return_value=None) as mock_sentry_log_message, \
-                mock.patch('addons.boa.tasks.logger.error', return_value=None) as mock_logger_error:
-            return_value = handle_boa_error(
-                self.error_message,
-                BoaErrorCode.UNKNOWN,
-                self.user_username,
-                self.user_fullname,
-                self.project_url,
-                self.file_full_path,
-                query_file_name=self.query_file_name,
-                file_size=self.file_size,
-                output_file_name=self.output_file_name,
-                job_id=self.job_id
-            )
-            mock_send_mail.assert_called_with(
-                to_addr=self.user_username,
-                mail=ADDONS_BOA_JOB_FAILURE,
-                fullname=self.user_fullname,
-                code=BoaErrorCode.UNKNOWN,
-                message=self.error_message,
-                query_file_name=self.query_file_name,
-                file_size=self.file_size,
-                max_file_size=boa_settings.MAX_SUBMISSION_SIZE,
-                query_file_full_path=self.file_full_path,
-                output_file_name=self.output_file_name,
-                job_id=self.job_id,
-                max_job_wait_hours=self.max_job_wait_hours,
-                project_url=self.project_url,
-                boa_job_list_url=boa_settings.BOA_JOB_LIST_URL,
-                boa_support_email=boa_settings.BOA_SUPPORT_EMAIL,
-                osf_support_email=osf_settings.OSF_SUPPORT_EMAIL,
-            )
-            mock_sentry_log_message.assert_called_with(self.error_message, skip_session=True)
-            mock_logger_error.assert_called_with(self.error_message)
-            assert return_value == BoaErrorCode.UNKNOWN
+        with mock.patch('addons.boa.tasks.sentry.log_message', return_value=None) as mock_sentry_log_message:
+            with mock.patch('addons.boa.tasks.logger.error', return_value=None) as mock_logger_error:
+                with capture_notifications() as notifications:
+                    return_value = handle_boa_error(
+                        self.error_message,
+                        BoaErrorCode.UNKNOWN,
+                        self.user_username,
+                        self.user_fullname,
+                        self.project_url,
+                        self.file_full_path,
+                        file_size=self.file_size,
+                        output_file_name=self.output_file_name,
+                        job_id=self.job_id
+                    )
+                assert len(notifications) == 1
+                assert notifications[0]['type'] == NotificationType.Type.ADDONS_BOA_JOB_FAILURE
+                mock_sentry_log_message.assert_called_with(self.error_message, skip_session=True)
+                mock_logger_error.assert_called_with(self.error_message)
+                assert return_value == BoaErrorCode.UNKNOWN
 
 
 class TestSubmitToBoa(OsfTestCase):
