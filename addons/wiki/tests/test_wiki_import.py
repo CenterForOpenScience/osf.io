867b923b65998297c486bddceee61fd57891c02e
NishikawaKoharu <nishikawa@omik.co.jp>
2025-08-05 21:27:10 +0900

    TravisCI8

--- addons/wiki/tests/test_wiki_import.py
+++ addons/wiki/tests/test_wiki_import.py
@@ -950,7 +950,7 @@ class TestWikiViews(OsfTestCase, unittest.TestCase):
         mock_get_sharejs_uuid.return_value = None
 
         delete_url = self.project.api_url_for('project_wiki_delete', wname='funpage')
-        self.app.delete(delete_url, auth=self.auth, expect_errors=True)
+        self.app.delete(delete_url, auth=self.consolidate_auth, expect_errors=True)
         #res = self.app.get(delete_url, expect_errors=True)
         res = self.app.get(delete_url)
         assert_equal(res.status_code, 404)
@@ -962,7 +962,7 @@ class TestWikiViews(OsfTestCase, unittest.TestCase):
         mock_get_sharejs_uuid.return_value = None
 
         delete_url = self.project.api_url_for('project_wiki_delete', wname='funpage')
-        self.app.delete(delete_url, auth=self.auth)
+        self.app.delete(delete_url, auth=self.consolidate_auth)
         #res = self.app.get(delete_url, expect_errors=True)
         res = self.app.get(delete_url, expect_errors=True)
         assert_equal(res.status_code, 404)
@@ -992,7 +992,7 @@ class TestWikiViews(OsfTestCase, unittest.TestCase):
         with mock.patch.object(timezone, 'now', return_value=mock_now):
             self.app.delete(
                 url,
-                auth=self.auth
+                auth=self.consolidate_auth
             )
         self.project.reload()
         page.reload()
@@ -1013,7 +1013,7 @@ class TestWikiViews(OsfTestCase, unittest.TestCase):
 
     def test_project_wiki_edit_post(self):
         url = self.project.web_url_for('project_wiki_edit_post', wname='home')
-        res = self.app.post_json(url, {'markdown': 'new content'}, auth=self.user).follow()
+        res = self.app.post_json(url, {'markdown': 'new content'}, auth=self.consolidate_auth).follow()
         assert_equal(res.status_code, 200)
 
     @mock.patch('addons.wiki.models.WikiPage.objects.get_for_node')
@@ -1034,7 +1034,7 @@ class TestWikiViews(OsfTestCase, unittest.TestCase):
         mock_create_for_node.return_value = None
 
         url = self.project.api_url_for('project_wiki_validate_name', wname='Capslock', p_wname='home', node=None)
-        res = self.app.get(url, auth=self.auth)
+        res = self.app.get(url, auth=self.consolidate_auth)
 
         # モックが呼ばれたか
         mock_create_for_node.assert_called()
@@ -1057,7 +1057,7 @@ class TestWikiViews(OsfTestCase, unittest.TestCase):
         mock_create_for_node.return_value = None
 
         url = self.project.api_url_for('project_wiki_validate_name', wname='Capslock', p_wname='test', node=None)
-        res = self.app.get(url, auth=self.auth, expect_errors=True)
+        res = self.app.get(url, auth=self.consolidate_auth, expect_errors=True)
 
         assert_equal(res.status_code, 404)
 
@@ -1213,7 +1213,7 @@ class TestWikiViews(OsfTestCase, unittest.TestCase):
         mock_check_file_object_in_node.return_value = True
         dir_id = self.root_import_folder1._id
         url = self.project.api_url_for('project_wiki_import', dir_id=dir_id)
-        res = self.app.post_json(url, { 'data': [{'test': 'test1'}] }, auth=self.auth)
+        res = self.app.post_json(url, { 'data': [{'test': 'test1'}] }, auth=self.consolidate_auth)
         response_json = res.json
         task_id = response_json['taskId']
         uuid_obj = uuid.UUID(task_id)
@@ -2550,7 +2550,7 @@ class TestWikiViews(OsfTestCase, unittest.TestCase):
         WikiImportTask.objects.create(node=self.project, task_id='task-id-11', status=WikiImportTask.STATUS_COMPLETED, creator=self.user)
         WikiImportTask.objects.create(node=self.project, task_id='task-id-2222', status=WikiImportTask.STATUS_RUNNING, creator=self.user)
         url = self.project.api_url_for('project_clean_celery_tasks')
-        res = self.app.post(url, auth=self.auth)
+        res = self.app.post(url, auth=self.consolidate_auth)
         task_completed = WikiImportTask.objects.get(task_id='task-id-11')
         task_running = WikiImportTask.objects.get(task_id='task-id-2222')
         assert_equal(task_completed.status, 'Completed')
@@ -2693,7 +2693,7 @@ class TestWikiViews(OsfTestCase, unittest.TestCase):
         node = self.node
         kwargs = {'node': node}
 
-        with TestWikiViews.mock_dependencies(wiki_page=None, wiki_version=None, request_args={}, format_version_side_effect=None), mock.patch('ddons.wiki.utils.to_mongo_key', return_value='not_home'):
+        with TestWikiViews.mock_dependencies(wiki_page=None, wiki_version=None, request_args={}, format_version_side_effect=None), mock.patch('addons.wiki.utils.to_mongo_key', return_value='not_home'):
             with assert_raises(self.WIKI_PAGE_NOT_FOUND_ERROR):
                 views.project_wiki_view(auth, 'NotHome', **kwargs)
 
