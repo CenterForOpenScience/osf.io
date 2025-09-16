9e9beb9344fe664e3fedc071c6deb267c2d21e29
Oleh Paduchak <opaduchak@exoft.net>
2025-09-16 18:47:25 +0300

    fix migration job

---
+++ osf/management/commands/fix_versioned_guids.py
@@ -0,0 +1,69 @@
+import logging
+
+from django.contrib.contenttypes.models import ContentType
+from django.core.management.base import BaseCommand
+from django.db import transaction
+from django.db.models import Prefetch
+
+from osf.models import GuidVersionsThrough, Guid, Preprint
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    help = 'Fix'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--dry-run',
+            action='store_true',
+            dest='dry_run',
+            help='Run the command without saving changes',
+        )
+
+    @transaction.atomic
+    def handle(self, *args, **options):
+        dry_run = options.get('dry_run', False)
+        fix_versioned_guids(dry_run=dry_run)
+        if dry_run:
+            transaction.set_rollback(True)
+
+
+def fix_versioned_guids(dry_run: bool):
+    content_type = ContentType.objects.get_for_model(Preprint)
+    versions_queryset = GuidVersionsThrough.objects.order_by('-version')
+    processed_count = 0
+    updated_count = 0
+    skipped_count = 0
+    errors_count = 0
+    for guid in (
+        Guid.objects.filter(content_type=content_type)
+        .prefetch_related(Prefetch('versions', queryset=versions_queryset))
+        .iterator(chunk_size=500)
+    ):
+        processed_count += 1
+        if not guid.versions:
+            skipped_count += 1
+            continue
+        last_version: GuidVersionsThrough = guid.versions.first()
+        last_version_object_id = last_version.object_id
+        if guid.object_id != last_version_object_id:
+            try:
+                guid.object_id = last_version_object_id
+                guid.referent = last_version.referent
+                if not dry_run:
+                    guid.save()
+                updated_count += 1
+            except Exception as e:
+                logger.error(f"Error occurred during patching {guid._id=}", exc_info=e)
+                errors_count += 1
+        else:
+            skipped_count += 1
+    if dry_run:
+        logger.error(
+            f"Processed: {processed_count}, Would update: {updated_count}, Skipped: {skipped_count}, Errors: {errors_count}"
+        )
+    else:
+        logger.error(
+            f"Processed: {processed_count}, Updated: {updated_count}, Skipped: {skipped_count}, Errors: {errors_count}"
+        )
