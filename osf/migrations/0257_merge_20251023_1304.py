d5fa9748d913ce1fc92ffb621f947b054985c572
hide24 <hidetoshi.yoshimoto@gmail.com>
2025-10-23 13:12:23 +0900

    merge migration

---
+++ osf/migrations/0257_merge_20251023_1304.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('osf', '0256_ensure_schema_mappings'),
+        ('osf', '0254_reorder_japan_grant_number'),
+    ]
+
+    operations = [
+    ]
