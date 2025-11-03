529edc8a2383ff03f6a385e8d2f9c073733547ef
John Tordoff <Johnetordoff@users.noreply.github.com>
2025-11-03 12:08:24 -0500

    remove second `'api_tests/notifications', as previous PR as merged

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -392,7 +392,6 @@ API_TESTS3 = [
     'api_tests/subscriptions',
     'api_tests/wikis',
     'api_tests/wb',
-    'api_tests/notifications',
 ]
 API_TESTS4 = [
     'api_tests/cedar_metadata_records',
