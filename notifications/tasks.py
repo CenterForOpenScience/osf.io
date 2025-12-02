58ab036102c826f8ad48b57e40dd7e3df7d4de44
Ostap Zherebetskyi <ozherebetskyi@exoft.net>
2025-12-02 13:42:48 +0200

    add admin-moderator group

--- notifications/tasks.py
+++ notifications/tasks.py
@@ -164,11 +164,13 @@ def send_moderator_email_task(self, user_id, notification_ids, provider_content_
 
         current_moderators = provider.get_group('moderator')
         if current_moderators is None or not current_moderators.user_set.filter(id=user.id).exists():
-            log_message(f"User is not a moderator for provider {provider._id} - skipping email")
-            email_task.status = 'FAILURE'
-            email_task.error_message = f'User is not a moderator for provider {provider._id}'
-            email_task.save()
-            return
+            current_admins = provider.get_group('admins')
+            if current_admins is None or not current_admins.user_set.filter(id=user.id).exists():
+                log_message(f"User is not a moderator for provider {provider._id} - skipping email")
+                email_task.status = 'FAILURE'
+                email_task.error_message = f'User is not a moderator for provider {provider._id}'
+                email_task.save()
+                return
 
         additional_context = {}
         if isinstance(provider, RegistrationProvider):
