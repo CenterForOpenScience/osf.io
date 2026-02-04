88103afc14ae2e820be6facb8a1cb56889b73956
Longze Chen <cslzchen@gmail.com>
2026-02-04 09:53:25 -0500

    [ENG-10083] Add type<-->is_digest "relation" and update subscription (#11575)
    * Add is_digest_type property to NotificationType and log message in emit method
    * Fix is_digest handling in NotificationType.emit method
    * Log message update in NotificationType.emit method for is_digest handling
    * fix unit tests
    
    ---------
    
    Co-authored-by: Ostap Zherebetskyi <ozherebetskyi@exoft.net>

--- api_tests/share/test_share_preprint.py
+++ api_tests/share/test_share_preprint.py
@@ -18,6 +18,7 @@ from osf_tests.factories import (
 from website import settings
 from website.preprints.tasks import on_preprint_updated
 from ._utils import expect_preprint_ingest_request
+from tests.utils import capture_notifications
 
 
 @pytest.mark.django_db
@@ -72,54 +73,59 @@ class TestPreprintShare:
             preprint.save()
 
     def test_save_published_called(self, mock_share_responses, preprint, user, auth):
-        with expect_preprint_ingest_request(mock_share_responses, preprint):
-            preprint.set_published(True, auth=auth, save=True)
+        with capture_notifications():
+            with expect_preprint_ingest_request(mock_share_responses, preprint):
+                preprint.set_published(True, auth=auth, save=True)
 
     # This covers an edge case where a preprint is forced back to unpublished
     # that it sends the information back to share
     def test_save_unpublished_called_forced(self, mock_share_responses, auth, preprint):
-        with expect_preprint_ingest_request(mock_share_responses, preprint):
-            preprint.set_published(True, auth=auth, save=True)
-        with expect_preprint_ingest_request(mock_share_responses, preprint, delete=True):
-            preprint.is_published = False
-            preprint.save(**{'force_update': True})
+        with capture_notifications():
+            with expect_preprint_ingest_request(mock_share_responses, preprint):
+                preprint.set_published(True, auth=auth, save=True)
+            with expect_preprint_ingest_request(mock_share_responses, preprint, delete=True):
+                preprint.is_published = False
+                preprint.save(**{'force_update': True})
 
     def test_save_published_subject_change_called(self, mock_share_responses, auth, preprint, subject, subject_two):
-        preprint.set_published(True, auth=auth, save=True)
-        with expect_preprint_ingest_request(mock_share_responses, preprint):
-            preprint.set_subjects([[subject_two._id]], auth=auth)
+        with capture_notifications():
+            preprint.set_published(True, auth=auth, save=True)
+            with expect_preprint_ingest_request(mock_share_responses, preprint):
+                preprint.set_subjects([[subject_two._id]], auth=auth)
 
     def test_save_unpublished_subject_change_not_called(self, mock_share_responses, auth, preprint, subject_two):
         with expect_preprint_ingest_request(mock_share_responses, preprint, delete=True):
             preprint.set_subjects([[subject_two._id]], auth=auth)
 
     def test_send_to_share_is_true(self, mock_share_responses, auth, preprint):
-        preprint.set_published(True, auth=auth, save=True)
-        with expect_preprint_ingest_request(mock_share_responses, preprint):
-            on_preprint_updated(preprint._id, saved_fields=['title'])
+        with capture_notifications():
+            preprint.set_published(True, auth=auth, save=True)
+            with expect_preprint_ingest_request(mock_share_responses, preprint):
+                on_preprint_updated(preprint._id, saved_fields=['title'])
 
     def test_preprint_contributor_changes_updates_preprints_share(self, mock_share_responses, user, auth):
-        preprint = PreprintFactory(is_published=True, creator=user)
-        preprint.set_published(True, auth=auth, save=True)
-        user2 = AuthUserFactory()
+        with capture_notifications():
+            preprint = PreprintFactory(is_published=True, creator=user)
+            preprint.set_published(True, auth=auth, save=True)
+            user2 = AuthUserFactory()
 
-        with expect_preprint_ingest_request(mock_share_responses, preprint):
-            preprint.add_contributor(contributor=user2, auth=auth, save=True)
+            with expect_preprint_ingest_request(mock_share_responses, preprint):
+                preprint.add_contributor(contributor=user2, auth=auth, save=True)
 
-        with expect_preprint_ingest_request(mock_share_responses, preprint):
-            preprint.move_contributor(contributor=user, index=0, auth=auth, save=True)
+            with expect_preprint_ingest_request(mock_share_responses, preprint):
+                preprint.move_contributor(contributor=user, index=0, auth=auth, save=True)
 
-        data = [{'id': user._id, 'permissions': ADMIN, 'visible': True},
-                {'id': user2._id, 'permissions': WRITE, 'visible': False}]
+            data = [{'id': user._id, 'permissions': ADMIN, 'visible': True},
+                    {'id': user2._id, 'permissions': WRITE, 'visible': False}]
 
-        with expect_preprint_ingest_request(mock_share_responses, preprint):
-            preprint.manage_contributors(data, auth=auth, save=True)
+            with expect_preprint_ingest_request(mock_share_responses, preprint):
+                preprint.manage_contributors(data, auth=auth, save=True)
 
-        with expect_preprint_ingest_request(mock_share_responses, preprint):
-            preprint.update_contributor(user2, READ, True, auth=auth, save=True)
+            with expect_preprint_ingest_request(mock_share_responses, preprint):
+                preprint.update_contributor(user2, READ, True, auth=auth, save=True)
 
-        with expect_preprint_ingest_request(mock_share_responses, preprint):
-            preprint.remove_contributor(contributor=user2, auth=auth)
+            with expect_preprint_ingest_request(mock_share_responses, preprint):
+                preprint.remove_contributor(contributor=user2, auth=auth)
 
     @pytest.mark.skip('Synchronous retries not supported if celery >=5.0')
     def test_call_async_update_on_500_failure(self, mock_share_responses, preprint, auth):
@@ -129,10 +135,11 @@ class TestPreprintShare:
             preprint.update_search()
 
     def test_no_call_async_update_on_400_failure(self, mock_share_responses, preprint, auth):
-        mock_share_responses.replace(responses.POST, shtrove_ingest_url(), status=400)
-        preprint.set_published(True, auth=auth, save=True)
-        with expect_preprint_ingest_request(mock_share_responses, preprint, count=1, error_response=True):
-            preprint.update_search()
+        with capture_notifications():
+            mock_share_responses.replace(responses.POST, shtrove_ingest_url(), status=400)
+            preprint.set_published(True, auth=auth, save=True)
+            with expect_preprint_ingest_request(mock_share_responses, preprint, count=1, error_response=True):
+                preprint.update_search()
 
     def test_delete_from_share(self, mock_share_responses):
         preprint = PreprintFactory()
