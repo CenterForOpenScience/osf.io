b6eba82b5750a5be29009b3a947a0958437eae27
mkovalua <mkoval@exoft.net>
2026-02-12 16:57:23 +0200

    [ENG-8740] Ability to delete a file from a registration via admin (#11285)
    * [ENG-8514] Remove CSRF protection from reset password api v2 POST (#11246)
    
    * remove csrf protection from reset password endpoint
    
    * update test
    
    * add background color prop to Brand (#11254)
    
    * fix flaky test_serialized_metadata
    
    * fix throttle test
    
    * reset throttle cache
    
    * don't ignore components when create a view-only link
    
    * fixed updating subscribe_osf_general_email subscription
    
    * [ENG-7277] Update doc to include version as relationship (#11262)
    
    * added tests
    
    * [ENG-8691] Wrong server on reset password email (#11271)
    
    * use same logic for admin and web password reset
    
    * fix tests
    
    * enable filtering for linked-nodes endpoint (#11273)
    
    * remove deleted users from institutional dashboard (#11261)
    
    * fixed tests
    
    * [ENG-7803] Update text on VOL modal for registration (#11279)
    
    * add a file removal feature for a node (registration) using admin panel
    
    * delete file only for archived registration when it is not possible from user side to avoid inconsistency
    
    * remove file from scheme response blocks
    
    * delete file from registration and metadata and keep it for original project
    
    ---------
    
    Co-authored-by: antkryt <ant.krytskyi@gmail.com>
    Co-authored-by: John Tordoff <Johnetordoff@users.noreply.github.com>
    Co-authored-by: Ihor Sokhan <isokhan@exoft.net>
    Co-authored-by: Vlad0n20 <137097005+Vlad0n20@users.noreply.github.com>

--- admin/nodes/views.py
+++ admin/nodes/views.py
@@ -6,6 +6,7 @@ from framework import status
 from django.utils import timezone
 from django.core.exceptions import PermissionDenied, ValidationError
 from django.urls import NoReverseMatch
+from django.db import transaction
 from django.db.models import F, Case, When, IntegerField
 from django.contrib import messages
 from django.contrib.auth.mixins import PermissionRequiredMixin
@@ -28,14 +29,17 @@ from api.share.utils import update_share
 from api.caching.tasks import update_storage_usage_cache
 
 from osf.exceptions import NodeStateError, RegistrationStuckError
+from osf.management.commands.change_node_region import _update_schema_meta
 from osf.models import (
+    Guid,
     OSFUser,
     NodeLog,
     AbstractNode,
     Registration,
     RegistrationProvider,
     RegistrationApproval,
-    SpamStatus
+    SpamStatus,
+    TrashedFile
 )
 from osf.models.admin_log_entry import (
     update_admin_log,
@@ -810,6 +814,36 @@ class NodeMakePublic(NodeMixin, View):
             node.set_privacy('public')
         except NodeStateError as e:
             messages.error(request, str(e))
+        return redirect(self.get_success_url())
+
+
+class NodeRemoveFileView(NodeMixin, View):
+    """ Allows an authorized user to remove file from node.
+    """
+    permission_required = 'osf.change_node'
+
+    def post(self, request, *args, **kwargs):
+        def _remove_file_from_schema_response_blocks(registration, removed_file_id):
+            file_input_keys = registration.registration_schema.schema_blocks.filter(
+                block_type='file-input'
+            ).values_list('registration_response_key', flat=True)
+            for schema_response in registration.schema_responses.all():
+                for block in schema_response.response_blocks.filter(schema_key__in=file_input_keys):
+                    if not block.response:
+                        continue
+                    block.response = [entry for entry in block.response if entry.get('file_id') not in removed_file_id]
+                    block.save()
+
+        node = self.get_object()
+        guid_id = request.POST.get('remove-file-guid', '').strip()
+        guid = Guid.load(guid_id)
+
+        # delete file from registration and metadata and keep it for original project
+        if guid and (file := guid.referent) and (file.target == node) and not isinstance(file, TrashedFile):
+            with transaction.atomic():
+                file.delete()
+                _update_schema_meta(file.target)
+                _remove_file_from_schema_response_blocks(node, [file._id, file.copied_from._id])
         return redirect(self.get_success_url())
 
 
