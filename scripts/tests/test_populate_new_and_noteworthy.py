29f5ffadaa8add9132821309140d4c4f5d6f595a
Vlad0n20 <vlad.onischuk1234@gmail.com>
2026-02-13 15:31:46 +0200

    Fix tests

--- scripts/tests/test_populate_new_and_noteworthy.py
+++ scripts/tests/test_populate_new_and_noteworthy.py
@@ -1,10 +1,13 @@
+from unittest import mock
+
 from tests.base import OsfTestCase
 from osf_tests.factories import ProjectFactory
 
 from osf.models import Node
-from website.settings import NEW_AND_NOTEWORTHY_LINKS_NODE
 
 from scripts import populate_new_and_noteworthy_projects as script
+
+TEST_NEW_AND_NOTEWORTHY_GUID = 'nguid'
 
 
 class TestPopulateNewAndNoteworthy(OsfTestCase):
@@ -13,14 +16,14 @@ class TestPopulateNewAndNoteworthy(OsfTestCase):
         super().setUp()
 
         self.new_and_noteworthy_links_node = ProjectFactory()
-        self.new_and_noteworthy_links_node._id = NEW_AND_NOTEWORTHY_LINKS_NODE
+        self.new_and_noteworthy_links_node._id = TEST_NEW_AND_NOTEWORTHY_GUID
         self.new_and_noteworthy_links_node.save()
 
-        self.nn1 = ProjectFactory(is_public=True)
-        self.nn2 = ProjectFactory(is_public=True)
-        self.nn3 = ProjectFactory(is_public=True)
-        self.nn4 = ProjectFactory(is_public=True)
-        self.nn5 = ProjectFactory(is_public=True)
+        self.nn1 = ProjectFactory(is_public=True, title='Noteworthy Alpha')
+        self.nn2 = ProjectFactory(is_public=True, title='Noteworthy Bravo')
+        self.nn3 = ProjectFactory(is_public=True, title='Noteworthy Charlie')
+        self.nn4 = ProjectFactory(is_public=True, title='Noteworthy Foxtrot')
+        self.nn5 = ProjectFactory(is_public=True, title='Noteworthy Golf')
 
         self.all_ids = {self.nn1._id, self.nn2._id, self.nn3._id, self.nn4._id, self.nn5._id}
 
@@ -32,6 +35,7 @@ class TestPopulateNewAndNoteworthy(OsfTestCase):
         new_noteworthy = script.get_new_and_noteworthy_nodes(self.new_and_noteworthy_links_node)
         assert set(new_noteworthy) == self.all_ids
 
+    @mock.patch.object(script, 'NEW_AND_NOTEWORTHY_LINKS_NODE', TEST_NEW_AND_NOTEWORTHY_GUID)
     def test_populate_new_and_noteworthy(self):
         assert self.new_and_noteworthy_links_node._nodes.count() == 0
 
