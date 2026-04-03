b11d69cce470c8062081ce2cdbef7bc36b9eb5ea
Vlad0n20 <vlad.onischuk1234@gmail.com>
2026-04-03 17:22:16 +0300

    add required_metadata_template to API

--- api_tests/providers/collections/views/test_collection_provider_detail.py
+++ api_tests/providers/collections/views/test_collection_provider_detail.py
@@ -2,6 +2,7 @@ import pytest
 
 from api.base.settings.defaults import API_BASE
 from api_tests.providers.mixins import ProviderExistsMixin
+from osf.models import CedarMetadataTemplate
 from osf_tests.factories import (
     CollectionProviderFactory,
     BrandFactory,
@@ -55,3 +56,40 @@ class TestCollectionProviderExists(ProviderExistsMixin):
         data = res.json['data']
 
         assert data['relationships']['brand']['data']['id'] == str(brand.id)
+
+
+@pytest.mark.django_db
+class TestCollectionProviderRequiredMetadataTemplate:
+
+    @pytest.fixture()
+    def provider(self):
+        return CollectionProviderFactory()
+
+    @pytest.fixture()
+    def provider_url(self, provider):
+        return f'/{API_BASE}providers/collections/{provider._id}/'
+
+    @pytest.fixture()
+    def cedar_template(self):
+        return CedarMetadataTemplate.objects.create(
+            schema_name='Test Schema',
+            cedar_id='https://repo.metadatacenter.org/templates/test',
+            template_version=1,
+            template={},
+            active=True,
+        )
+
+    def test_required_metadata_template_is_null_by_default(self, app, provider, provider_url):
+        res = app.get(provider_url)
+        assert res.status_code == 200
+        assert res.json['data']['relationships']['required_metadata_template']['data'] is None
+
+    def test_required_metadata_template_when_set(self, app, provider, provider_url, cedar_template):
+        provider.required_metadata_template = cedar_template
+        provider.save()
+
+        res = app.get(provider_url)
+        assert res.status_code == 200
+        rel = res.json['data']['relationships']['required_metadata_template']
+        assert rel['data']['id'] == cedar_template._id
+        assert rel['data']['type'] == 'cedar-metadata-templates'
--- api_tests/providers/registrations/views/test_registration_provider_detail.py
+++ api_tests/providers/registrations/views/test_registration_provider_detail.py
@@ -2,6 +2,7 @@ import pytest
 
 from api.base.settings.defaults import API_BASE
 from api_tests.providers.mixins import ProviderDetailViewTestBaseMixin
+from osf.models import CedarMetadataTemplate
 from osf_tests.factories import (
     BrandFactory,
     RegistrationProviderFactory,
@@ -58,3 +59,40 @@ class TestRegistrationProviderExists(ProviderDetailViewTestBaseMixin):
 
         assert data['relationships']['brand']['data']['id'] == str(brand.id)
         assert data['attributes']['branded_discovery_page'] == provider_with_brand.branded_discovery_page
+
+
+@pytest.mark.django_db
+class TestRegistrationProviderRequiredMetadataTemplate:
+
+    @pytest.fixture()
+    def provider(self):
+        return RegistrationProviderFactory()
+
+    @pytest.fixture()
+    def provider_url(self, provider):
+        return f'/{API_BASE}providers/registrations/{provider._id}/'
+
+    @pytest.fixture()
+    def cedar_template(self):
+        return CedarMetadataTemplate.objects.create(
+            schema_name='Test Schema',
+            cedar_id='https://repo.metadatacenter.org/templates/test',
+            template_version=1,
+            template={},
+            active=True,
+        )
+
+    def test_required_metadata_template_is_null_by_default(self, app, provider, provider_url):
+        res = app.get(provider_url)
+        assert res.status_code == 200
+        assert res.json['data']['relationships']['required_metadata_template']['data'] is None
+
+    def test_required_metadata_template_when_set(self, app, provider, provider_url, cedar_template):
+        provider.required_metadata_template = cedar_template
+        provider.save()
+
+        res = app.get(provider_url)
+        assert res.status_code == 200
+        rel = res.json['data']['relationships']['required_metadata_template']
+        assert rel['data']['id'] == cedar_template._id
+        assert rel['data']['type'] == 'cedar-metadata-templates'
