6b2b6751be8d751eee0f3a3993759c6799e807ae
mkovalua <mkoval@exoft.net>
2026-04-06 20:26:37 +0300

    add terminal command run option with 'python3 manage.py remove_orcid_from_social'

--- osf/management/commands/remove_orcid_from_user_social.py
+++ osf/management/commands/remove_orcid_from_user_social.py
@@ -1,6 +1,13 @@
+from django.core.management.base import BaseCommand
 from django.db.models.expressions import RawSQL
 from osf.models import OSFUser
 
 
 def remove_orcid_from_user_social():
     OSFUser.objects.filter(social__has_key='orcid').update(social=RawSQL("""social #- '{orcid}'""", []))
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        breakpoint()
+        remove_orcid_from_user_social()
