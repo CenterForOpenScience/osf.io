77eeddce9bac10dbd54930e69bed11705a005f1a
mkovalua <mkoval@exoft.net>
2026-04-06 21:21:05 +0300

    [ENG-10586] remove orcid from user social (#11684)
    * delete user social orcid using management command ( need recatalog_metadata --users run to sync the data to SHARE )
    
    * add terminal command run option with 'python3 manage.py remove_orcid_from_social'

--- osf/management/commands/remove_orcid_from_user_social.py
+++ osf/management/commands/remove_orcid_from_user_social.py
@@ -1,6 +1,12 @@
+from django.core.management.base import BaseCommand
 from django.db.models.expressions import RawSQL
 from osf.models import OSFUser
 
 
 def remove_orcid_from_user_social():
     OSFUser.objects.filter(social__has_key='orcid').update(social=RawSQL("""social #- '{orcid}'""", []))
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        remove_orcid_from_user_social()
