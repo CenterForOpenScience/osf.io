fd62d6c18c06d66f1e89975608c1b39cbd41dcd9
mkovalua <mkoval@exoft.net>
2026-04-06 21:55:04 +0300

    show how much records are updated and how much time it took

--- osf/management/commands/remove_orcid_from_user_social.py
+++ osf/management/commands/remove_orcid_from_user_social.py
@@ -1,13 +1,20 @@
 from django.core.management.base import BaseCommand
 from django.db.models.expressions import RawSQL
 from osf.models import OSFUser
+from datetime import datetime
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def remove_orcid_from_user_social():
-    OSFUser.objects.filter(social__has_key='orcid').update(social=RawSQL("""social #- '{orcid}'""", []))
+    start = datetime.now()
+    orcid_records = OSFUser.objects.filter(social__has_key='orcid')
+    logger.info(f'extracted orcid records count {orcid_records.count()}')
+    deleted_records = orcid_records.update(social=RawSQL("""social #- '{orcid}'""", []))
+    logger.info(f'deleted orcid records count {deleted_records} in {datetime.now() - start}')
 
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        breakpoint()
         remove_orcid_from_user_social()
