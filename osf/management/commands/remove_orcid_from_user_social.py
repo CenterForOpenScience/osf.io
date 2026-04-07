c5912dd2ee8dc7643702741c707327616ca388d3
mkovalua <mkoval@exoft.net>
2026-04-07 16:34:52 +0300

    [ENG-10586] (#11685)
    * delete user social orcid using management command ( need recatalog_metadata --users run to sync the data to SHARE )
    
    * add terminal command run option with 'python3 manage.py remove_orcid_from_social'
    
    * show how much records are updated and how much time it took
    
    * delete orcid from user social in batch

--- osf/management/commands/remove_orcid_from_user_social.py
+++ osf/management/commands/remove_orcid_from_user_social.py
@@ -1,10 +1,22 @@
 from django.core.management.base import BaseCommand
 from django.db.models.expressions import RawSQL
 from osf.models import OSFUser
+from datetime import datetime
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def remove_orcid_from_user_social():
-    OSFUser.objects.filter(social__has_key='orcid').update(social=RawSQL("""social #- '{orcid}'""", []))
+    start = datetime.now()
+    orcid_records = OSFUser.objects.filter(social__has_key='orcid')
+    logger.info(f'extracted orcid records count {orcid_records.count()}')
+    total_deleted_records = 0
+    while OSFUser.objects.filter(social__has_key='orcid').exists():
+        total_deleted_records += OSFUser.objects.filter(
+            id__in=OSFUser.objects.filter(social__has_key='orcid')[:10_000].values_list('id', flat=True)
+        ).update(social=RawSQL("""social #- '{orcid}'""", []))
+    logger.info(f'deleted orcid records count {total_deleted_records} in {datetime.now() - start}')
 
 
 class Command(BaseCommand):
