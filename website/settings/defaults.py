6c8ca7aa9cd77d83a41dbcef6b75900bfbf52218
Longze Chen <cslzchen@gmail.com>
2026-04-24 14:30:30 -0400

    Add local mode and update web_url_for to support angular domain

--- website/settings/defaults.py
+++ website/settings/defaults.py
@@ -84,13 +84,14 @@ LOAD_BALANCER = False
 DEV_MODE = False
 DEBUG_MODE = False
 SECURE_MODE = not DEBUG_MODE  # Set secure cookie
+LOCAL_MODE = False  # handles angular and web with different domains in local env
 
 PROTOCOL = 'https://' if SECURE_MODE else 'http://'
 DOMAIN = PROTOCOL + 'localhost:5000/'
 INTERNAL_DOMAIN = DOMAIN
 API_DOMAIN = PROTOCOL + 'localhost:8000/'
 RESET_PASSWORD_URL = PROTOCOL + 'localhost:5000/resetpassword/' # TODO set angular reset password url
-LOCAL_ANGULAR_URL = 'localhost:4200'
+LOCAL_ANGULAR_DOMAIN = PROTOCOL + 'localhost:4200/'  # Only used when LOCAL_MODE is True
 
 PREPRINT_PROVIDER_DOMAINS = {
     'enabled': False,
