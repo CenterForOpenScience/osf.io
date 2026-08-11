8d975bff00174bb8b2739c094e9809b660765d99
abram axel booth <boothaa@gmail.com>
2026-08-11 10:08:21 -0400

    fix(celery): remove broken import
    osf.management.commands.migrate_osfmetrics_fix_6to8
    was previously removed

--- website/settings/defaults.py
+++ website/settings/defaults.py
@@ -632,7 +632,6 @@ class CeleryConfig:
         'scripts.remove_after_use.merge_notification_subscription_provider_ct',
         'scripts.disable_removed_beat_tasks',
         'osf.management.commands.delete_withdrawn_or_failed_registration_files',
-        'osf.management.commands.migrate_osfmetrics_fix_6to8',
         'osf.email.notification_campaign',
     )
 
